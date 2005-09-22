3de88c299fac178dc1a8a304a43072de36884171
Unknown Perforce User <anandp>
2005-09-22 17:10:12 -0500

    (split)refactoring error - server should call close(), not closeNow()
    Copied from Perforce
     Change: 12965

--- src/java/com/zimbra/cs/ozserver/TestProtocolHandler.java
+++ src/java/com/zimbra/cs/ozserver/TestProtocolHandler.java
@@ -84,7 +84,7 @@ class TestProtocolHandler implements OzProtocolHandler {
         if (!matched) {
             // A command has to fit in the standard buffer size
             connection.writeAscii("500 command too long!", true); // TODO test this
-            connection.closeNow();
+            connection.close();
             return;
         }
         String cmd = OzUtil.asciiByteArrayToString(content);
@@ -94,7 +94,7 @@ class TestProtocolHandler implements OzProtocolHandler {
             gotoReadingCommandState(connection);
         } else if (cmd.equals("quit")) {
             connection.writeAscii("200 buh bye", true);
-            connection.closeNow();
+            connection.close();
         } else if (cmd.equals("sum")) {
             gotoReadingSumDataState(connection);
         } else if (cmd.startsWith("nsum")) {
