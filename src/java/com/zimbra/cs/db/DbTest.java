ba8d7b83c07e2677b902d7f4c2e886fcdfdd2df4
Unknown Perforce User <dkarp>
2005-10-26 01:23:24 -0500

    (split)Missed a file.
    Copied from Perforce
     Change: 15067

--- src/java/com/zimbra/cs/db/DbTest.java
+++ src/java/com/zimbra/cs/db/DbTest.java
@@ -284,7 +284,7 @@ public class DbTest {
 			if (name.indexOf('/') != -1)
 				System.out.println(name + ": invalid folder name");
 			else {
-				Folder folder = mailbox.createFolder(null, name, location, MailItem.TYPE_UNKNOWN);
+				Folder folder = mailbox.createFolder(null, name, location, MailItem.TYPE_UNKNOWN, null);
 				if (folder == null)
 					System.out.println("error: failure during request");
 			}
