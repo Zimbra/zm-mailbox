d03eaedb486087e6a0eb6223163011e25dc8dfcb
Unknown Perforce User <bburtin>
2006-01-24 16:09:11 -0600

    (split)Consolidated DummySSLSocketFactory and DummyTrustManager.  After I check in the IMAP SSL client support we'll have 3 callsites for this code.
    Copied from Perforce
     Change: 20271

---
+++ src/java/com/zimbra/cs/util/DummySSLSocketFactory.java
@@ -0,0 +1,94 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 ("License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.zimbra.com/license
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * The Original Code is: Zimbra Collaboration Suite Server.
+ * 
+ * The Initial Developer of the Original Code is Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2005 Zimbra, Inc.
+ * All Rights Reserved.
+ * 
+ * Contributor(s): 
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.util;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Allows Java code to make SSL connections without certificates.  This
+ * class is insecure and should only be used for testing.  See SSLNOTES.txt
+ * in the JavaMail distribution for more details.
+ *  
+ * @author bburtin
+ */
+public class DummySSLSocketFactory extends SSLSocketFactory {
+    static Log sLog = LogFactory.getLog(DummySSLSocketFactory.class);
+    private SSLSocketFactory factory;
+    
+    public DummySSLSocketFactory() {
+        try {
+            SSLContext sslcontext = SSLContext.getInstance("TLS");
+            sslcontext.init(null,
+                            new TrustManager[] { new DummyTrustManager()},
+                            null);
+            factory = sslcontext.getSocketFactory();
+        } catch(Exception ex) {
+            sLog.warn("Unable to initialize SSL", ex);
+        }
+    }
+    
+    public static SocketFactory getDefault() {
+        return new DummySSLSocketFactory();
+    }
+    
+    public Socket createSocket(Socket socket, String s, int i, boolean flag) throws IOException {
+        return factory.createSocket(socket, s, i, flag);
+    }
+    
+    public Socket createSocket(InetAddress inaddr, int i,
+                               InetAddress inaddr1, int j) throws IOException {
+        return factory.createSocket(inaddr, i, inaddr1, j);
+    }
+    
+    public Socket createSocket(InetAddress inaddr, int i) throws IOException {
+        return factory.createSocket(inaddr, i);
+    }
+
+    public Socket createSocket(String s, int i, InetAddress inaddr, int j) throws IOException {
+        return factory.createSocket(s, i, inaddr, j);
+    }
+
+    public Socket createSocket(String s, int i) throws IOException {
+        return factory.createSocket(s, i);
+    }
+
+    public String[] getDefaultCipherSuites() {
+        return factory.getDefaultCipherSuites();
+    }
+
+    public String[] getSupportedCipherSuites() {
+        return factory.getSupportedCipherSuites();
+    }
+}
--- src/java/com/zimbra/cs/util/SmtpInject.java
+++ src/java/com/zimbra/cs/util/SmtpInject.java
@@ -28,21 +28,14 @@ package com.zimbra.cs.util;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
 import java.util.Arrays;
 import java.util.Properties;
 
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
-
-import javax.net.SocketFactory;
-import javax.net.ssl.*;
-import javax.net.ssl.X509TrustManager;
-import java.security.cert.X509Certificate;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -119,9 +112,8 @@ public class SmtpInject {
         } else {
             file = cl.getOptionValue("f"); 
         }
-        byte[] data = null;
         try {
-            data = ByteUtil.getContent(new File(file));
+            ByteUtil.getContent(new File(file));
         } catch (IOException ioe) {
             usage(ioe.getMessage());
         }
@@ -207,7 +199,7 @@ public class SmtpInject {
         
         // Disable certificate checking so we can test against
         // self-signed certificates
-        java.security.Security.setProperty("ssl.SocketFactory.provider", SmtpInject.DummySSLSocketFactory.class.getName());
+        java.security.Security.setProperty("ssl.SocketFactory.provider", DummySSLSocketFactory.class.getName());
 
         Session session = Session.getInstance(props, null);
         session.setDebug(trace);
@@ -233,73 +225,6 @@ public class SmtpInject {
         } catch (IOException ioe) {
             ioe.printStackTrace();
             System.exit(1);
-        }
-    }
-
-    public static class DummySSLSocketFactory extends SSLSocketFactory {
-        private SSLSocketFactory factory;
-        
-        public DummySSLSocketFactory() {
-            try {
-                SSLContext sslcontext = SSLContext.getInstance("TLS");
-                sslcontext.init(null,
-                                new TrustManager[] { new SmtpInject.DummyTrustManager()},
-                                null);
-                factory = (SSLSocketFactory)sslcontext.getSocketFactory();
-            } catch(Exception ex) {
-                // ignore
-            }
-        }
-        
-        public static SocketFactory getDefault() {
-            return new DummySSLSocketFactory();
-        }
-        
-        public Socket createSocket(Socket socket, String s, int i, boolean flag) throws IOException {
-            return factory.createSocket(socket, s, i, flag);
-        }
-        
-        public Socket createSocket(InetAddress inaddr, int i,
-                                   InetAddress inaddr1, int j) throws IOException {
-            return factory.createSocket(inaddr, i, inaddr1, j);
-        }
-        
-        public Socket createSocket(InetAddress inaddr, int i) throws IOException {
-            return factory.createSocket(inaddr, i);
-        }
-
-        public Socket createSocket(String s, int i, InetAddress inaddr, int j) throws IOException {
-            return factory.createSocket(s, i, inaddr, j);
-        }
-
-        public Socket createSocket(String s, int i) throws IOException {
-            return factory.createSocket(s, i);
-        }
-
-        public String[] getDefaultCipherSuites() {
-            return factory.getDefaultCipherSuites();
-        }
-
-        public String[] getSupportedCipherSuites() {
-            return factory.getSupportedCipherSuites();
-        }
-    }
-
-    /**
-     * DummyTrustManager - NOT SECURE
-     */
-    public static class DummyTrustManager implements X509TrustManager {
-        
-        public void checkClientTrusted(X509Certificate[] cert, String authType) {
-            // everything is trusted
-        }
-        
-        public void checkServerTrusted(X509Certificate[] cert, String authType) {
-            // everything is trusted
-        }
-        
-        public X509Certificate[] getAcceptedIssuers() {
-            return new X509Certificate[0];
         }
     }
 }
