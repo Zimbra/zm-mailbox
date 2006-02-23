05afc248aab849af179b1b350d5a85aa60bda0a2
Unknown Perforce User <dkarp>
2006-02-23 15:48:11 -0600

    (split)http://bugzilla.zimbra.com/show_bug.cgi?id=6173 bug: 6173
    Don't strip users out of the set of Reply-To addresses even if they're also in the From: line.  When there were multiple addresses on the Reply-To line, we were losing the sender, which is bad.
    
    Copied from Perforce
     Change: 21620

--- src/java/com/zimbra/cs/service/mail/ToXML.java
+++ src/java/com/zimbra/cs/service/mail/ToXML.java
@@ -491,9 +491,8 @@ public class ToXML {
                 part = "";
 
             EmailElementCache eecache = new EmailElementCache();
-            HashSet unique = new HashSet();
-            addEmails(m, eecache, Mime.parseAddressHeader(mm, "From"), EmailElementCache.EMAIL_TYPE_FROM, unique);
-            addEmails(m, eecache, Mime.parseAddressHeader(mm, "Reply-To"), EmailElementCache.EMAIL_TYPE_REPLY_TO, unique);
+            addEmails(m, eecache, Mime.parseAddressHeader(mm, "From"), EmailElementCache.EMAIL_TYPE_FROM);
+            addEmails(m, eecache, Mime.parseAddressHeader(mm, "Reply-To"), EmailElementCache.EMAIL_TYPE_REPLY_TO);
             addEmails(m, eecache, Mime.parseAddressHeader(mm, "To"), EmailElementCache.EMAIL_TYPE_TO);
             addEmails(m, eecache, Mime.parseAddressHeader(mm, "Cc"), EmailElementCache.EMAIL_TYPE_CC);
             addEmails(m, eecache, Mime.parseAddressHeader(mm, "Bcc"), EmailElementCache.EMAIL_TYPE_BCC);
@@ -661,9 +660,8 @@ public class ToXML {
                 part = "";
 
             EmailElementCache eecache = new EmailElementCache();
-            HashSet unique = new HashSet();
-            addEmails(m, eecache, Mime.parseAddressHeader(mm, "From"), EmailElementCache.EMAIL_TYPE_FROM, unique);
-            addEmails(m, eecache, Mime.parseAddressHeader(mm, "Reply-To"), EmailElementCache.EMAIL_TYPE_REPLY_TO, unique);
+            addEmails(m, eecache, Mime.parseAddressHeader(mm, "From"), EmailElementCache.EMAIL_TYPE_FROM);
+            addEmails(m, eecache, Mime.parseAddressHeader(mm, "Reply-To"), EmailElementCache.EMAIL_TYPE_REPLY_TO);
             addEmails(m, eecache, Mime.parseAddressHeader(mm, "To"), EmailElementCache.EMAIL_TYPE_TO);
             addEmails(m, eecache, Mime.parseAddressHeader(mm, "Cc"), EmailElementCache.EMAIL_TYPE_CC);
             addEmails(m, eecache, Mime.parseAddressHeader(mm, "Bcc"), EmailElementCache.EMAIL_TYPE_BCC);
