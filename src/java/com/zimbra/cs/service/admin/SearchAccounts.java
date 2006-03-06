131607f8a08863a2b60b7ad976061da9f9c18c7a
Unknown Perforce User <jhahm>
2006-03-06 16:42:05 -0600

    (split)http://bugzilla.zimbra.com/show_bug.cgi?id=6077 Bug: 6077
    Calendar resource objects returned by SearchAccounts admin command must use <calresource> element, not <account>.
    
    Copied from Perforce
     Change: 22070

--- src/java/com/zimbra/cs/service/admin/SearchAccounts.java
+++ src/java/com/zimbra/cs/service/admin/SearchAccounts.java
@@ -34,6 +34,7 @@ import java.util.Map.Entry;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.AccountServiceException;
 import com.zimbra.cs.account.Alias;
+import com.zimbra.cs.account.CalendarResource;
 import com.zimbra.cs.account.DistributionList;
 import com.zimbra.cs.account.Domain;
 import com.zimbra.cs.account.NamedEntry;
@@ -122,6 +123,8 @@ public class SearchAccounts extends AdminDocumentHandler {
             NamedEntry entry = (NamedEntry) accounts.get(i);
             if (entry instanceof Account) {
                 ToXML.encodeAccount(response, (Account) entry, applyCos);
+            } else if (entry instanceof CalendarResource) {
+                ToXML.encodeCalendarResource(response, (CalendarResource) entry, applyCos);
             } else if (entry instanceof DistributionList) {
                 doDistributionList(response, (DistributionList) entry);
             } else if (entry instanceof Alias) {
