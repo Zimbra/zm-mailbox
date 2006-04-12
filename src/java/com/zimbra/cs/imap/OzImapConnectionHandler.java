e0250075fa56bf72a03969124a40940409363766
Unknown Perforce User <dkarp>
2006-04-12 15:12:47 -0500

    (split)http://bugzilla.zimbra.com/show_bug.cgi?id=6582 bug: 6582
    When we're in Windows Mobile 5 IMAP hack mode and we see a multipart/alternative part, replace the multipart by promoting its primary subpart.  There should be no multipart/alternatives in the resulting BODYSTRUCTUREs.
    
    Unfortunately, we run up against broken JavaMail caching, so in order to get this to work we need to serialize the resulting message to a buffer and then read that buffer back into a new MimeMessage.  This is absolutely terrible, but necessary unless and until I can figure out how to work around JavaMail.
    
    Copied from Perforce
     Change: 24260

--- src/java/com/zimbra/cs/imap/OzImapConnectionHandler.java
+++ src/java/com/zimbra/cs/imap/OzImapConnectionHandler.java
@@ -1430,7 +1430,12 @@ public class OzImapConnectionHandler implements OzConnectionHandler, ImapSession
                             InputStream is = raw != null ? new ByteArrayInputStream(raw) : mMailbox.getMessageById(getContext(), i4msg.id).getRawMessage();
                             mm = new Mime.FixedMimeMessage(JMSession.getSession(), is);
                             if (mSession.isHackEnabled(EnabledHack.WM5))
-                                new ImapMessage.WindowsMobile5Converter().accept(mm);
+                                if (new ImapMessage.WindowsMobile5Converter().accept(mm)) {
+                                    // FIXME: terrible hack to work around JavaMail repulsiveness
+                                    ByteArrayOutputStream baosCopy = new ByteArrayOutputStream();
+                                    mm.writeTo(baosCopy);
+                                    mm = new Mime.FixedMimeMessage(JMSession.getSession(), new ByteArrayInputStream(baosCopy.toByteArray()));
+                                }
                             is.close();
                         } catch (IOException e) {
                             throw ServiceException.FAILURE("error fetching raw content for message " + i4msg.id, e);
