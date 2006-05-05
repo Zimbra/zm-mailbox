69146164da64dac688653de6cc768b1c33d6d6ee
Unknown Perforce User <anandp>
2006-05-04 21:31:06 -0500

    (split)http://bugzilla.zimbra.com/show_bug.cgi?id=7488 bug 7488
    Copied from Perforce
     Change: 25025

--- src/java/com/zimbra/cs/service/admin/ConfigureZimlet.java
+++ src/java/com/zimbra/cs/service/admin/ConfigureZimlet.java
@@ -1,9 +1,9 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1
+ * Version: ZPL 1.2
  * 
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 ("License"); you may not use this file except in
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.2 ("License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.zimbra.com/license
  * 
--- src/java/com/zimbra/cs/zimlet/ZimletProperty.java
+++ src/java/com/zimbra/cs/zimlet/ZimletProperty.java
@@ -1,8 +1,8 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1
+ * Version: ZPL 1.2
  * 
- * The contents of this file are subject to the Mozilla Public License
+ * The contents of this file are subject to the Zimbra Public License
  * Version 1.1 ("License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.zimbra.com/license
