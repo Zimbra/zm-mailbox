24b53fd013358c0d4eba773143403c5ec848213c
Unknown Perforce User <anandp>
2006-05-04 21:38:42 -0500

    (split)http://bugzilla.zimbra.com/show_bug.cgi?id=7488 bug 7488
    Copied from Perforce
     Change: 25026

--- src/java/com/zimbra/cs/zimlet/Version.java
+++ src/java/com/zimbra/cs/zimlet/Version.java
@@ -2,7 +2,7 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Version: ZPL 1.2
  * 
- * The contents of this file are subject to the ZimbraPublic License
+ * The contents of this file are subject to the Zimbra Public License
  * Version 1.2 ("License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.zimbra.com/license
