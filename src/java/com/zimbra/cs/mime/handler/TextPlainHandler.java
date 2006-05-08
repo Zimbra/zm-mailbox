20ed7b520362d2dc4d82c0469c64aa48298b43e9
Unknown Perforce User <dkarp>
2006-05-08 14:31:58 -0500

    (split)http://bugzilla.zimbra.com/show_bug.cgi?id=6810 bug: 6810
    Copied from Perforce
     Change: 25106

--- src/java/com/zimbra/cs/mime/handler/TextPlainHandler.java
+++ src/java/com/zimbra/cs/mime/handler/TextPlainHandler.java
@@ -33,12 +33,9 @@ import java.io.IOException;
 
 import javax.activation.DataSource;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.lucene.document.Document;
 
 import com.zimbra.cs.convert.AttachmentInfo;
-import com.zimbra.cs.convert.ConversionException;
 import com.zimbra.cs.mime.Mime;
 import com.zimbra.cs.mime.MimeHandler;
 import com.zimbra.cs.mime.MimeHandlerException;
@@ -49,11 +46,10 @@ import com.zimbra.cs.mime.MimeHandlerException;
  *  class that creates a Lucene document from a Java Mail Message
  */
 public class TextPlainHandler extends MimeHandler {
-    
-    private static Log mLog = LogFactory.getLog(TextPlainHandler.class);
+
     private String mContent;
 
-    public void addFields(Document doc) throws MimeHandlerException {
+    public void addFields(Document doc) {
         // we add no type-specific fields to the doc
     }
 
@@ -81,7 +77,7 @@ public class TextPlainHandler extends MimeHandler {
     /* (non-Javadoc)
      * @see com.zimbra.cs.mime.MimeHandler#convert(com.zimbra.cs.convert.AttachmentInfo, java.lang.String)
      */
-    public String convert(AttachmentInfo doc, String baseURL) throws IOException, ConversionException {
+    public String convert(AttachmentInfo doc, String baseURL) {
         throw new IllegalStateException("No need to convert plain text");
     }
 
