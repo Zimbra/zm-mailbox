fb3fcfaf4b4c203d9270c9a167f0a440c92487ea
Unknown Perforce User <anandp>
2006-05-10 17:20:45 -0500

    (split)bug: none zimlets are zpl 1.2
    Copied from Perforce
     Change: 25213

--- src/java/com/zimbra/cs/zimlet/ZimletUserProperties.java
+++ src/java/com/zimbra/cs/zimlet/ZimletUserProperties.java
@@ -1,9 +1,9 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1
+ * Version: ZPL 1.2
  * 
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 ("License"); you may not use this file except in
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.2 ("License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.zimbra.com/license
  * 
@@ -12,7 +12,7 @@
  * the License for the specific language governing rights and limitations
  * under the License.
  * 
- * The Original Code is: Zimbra Collaboration Suite Server.
+ * The Original Code is: Zimlets
  * 
  * The Initial Developer of the Original Code is Zimbra, Inc.
  * Portions created by Zimbra are Copyright (C) 2005 Zimbra, Inc.
