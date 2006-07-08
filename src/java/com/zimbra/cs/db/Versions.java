7c4a6cf60e505d49761aa6438f0c6b03b152dfd6
Unknown Perforce User <dkarp>
2006-07-08 14:26:40 -0500

    (split)http://bugzilla.zimbra.com/show_bug.cgi?id=8884 bug: 8884
    This migration script, migrate20060708-FlagCalendarFolder.pl, will add the "Checked" flag to the main Calendar folder (id: 10) in all existing mailboxes.  This will cause the minical and calendar view to display appointments in the main calendar by default.
    
    Database version is now 25.  Please either reset-the-world or run this upgrade script.
    
    Copied from Perforce
     Change: 28147

--- src/java/com/zimbra/cs/db/Versions.java
+++ src/java/com/zimbra/cs/db/Versions.java
@@ -61,7 +61,7 @@ public class Versions {
 	 * UPDATE THESE TO REQUIRE RESET-WORLD TO BE RUN
 	 *  
 	 */
-	public static final String DB_VERSION = "24";
+	public static final String DB_VERSION = "25";
 
 	/**
 	 * The INDEX_VERSION is stored into the config table of the DB when the DB is created.  
