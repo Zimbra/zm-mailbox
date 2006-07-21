94792bd29a3d97ed92bb74efa8465abf51b26326
Unknown Perforce User <dkarp>
2006-07-20 19:17:21 -0500

    (split)http://bugzilla.zimbra.com/show_bug.cgi?id=4207 bug: 4207
    Don't use SoapProvisioning to proxy DeleteAccount.  Just use the standard high-level proxying.  If we end up executing on the wrong host (e.g. because the server's offline or never existed in the first place), just do the LDAP op and orphan the remote Mailbox.
    
    Copied from Perforce
     Change: 28782

--- src/java/com/zimbra/cs/service/admin/DeleteAccount.java
+++ src/java/com/zimbra/cs/service/admin/DeleteAccount.java
@@ -32,12 +32,8 @@ import java.util.Map;
 
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.AccountServiceException;
-import com.zimbra.cs.account.AuthToken;
-import com.zimbra.cs.account.AuthTokenException;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.Provisioning.AccountBy;
-import com.zimbra.cs.account.soap.SoapProvisioning;
-import com.zimbra.cs.httpclient.URLUtil;
 import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.cs.service.ServiceException;
 import com.zimbra.cs.util.ZimbraLog;
@@ -63,40 +59,28 @@ public class DeleteAccount extends AdminDocumentHandler {
      * Deletes an account and its mailbox.
      */
     public Element handle(Element request, Map<String, Object> context) throws ServiceException {
-        
         ZimbraSoapContext lc = getZimbraSoapContext(context);
         Provisioning prov = Provisioning.getInstance();
-        
+
         String id = request.getAttribute(AdminService.E_ID);
-        
+
         // Confirm that the account exists and that the mailbox is located
         // on the current host
         Account account = prov.get(AccountBy.id, id);
         if (account == null)
             throw AccountServiceException.NO_SUCH_ACCOUNT(id);
-        
+
         if (!canAccessAccount(lc, account))
             throw ServiceException.PERM_DENIED("can not access account");
-        
-        if (!Provisioning.onLocalServer(account)) {
-            SoapProvisioning sp = new SoapProvisioning();
-            try {
-                sp.setAuthToken(AuthToken.getZimbraAdminAuthToken().getEncoded());
-            } catch (AuthTokenException e) {
-                throw ServiceException.FAILURE("unable to get admin authtoken" , e);
-            }
-            sp.soapSetURI(URLUtil.getAdminURL(prov.getServer(account)));
-            sp.deleteAccount(account.getId());
-        } else {
-            Mailbox mbox = Mailbox.getMailboxByAccount(account);
-        
-            prov.deleteAccount(id);
+
+        Mailbox mbox = Provisioning.onLocalServer(account) ? Mailbox.getMailboxByAccount(account) : null;
+        prov.deleteAccount(id);
+        if (mbox != null)
             mbox.deleteMailbox();
-        }
-        
+
         ZimbraLog.security.info(ZimbraLog.encodeAttrs(
             new String[] {"cmd", "DeleteAccount","name", account.getName(), "id", account.getId()}));
-        
+
         Element response = lc.createElement(AdminService.DELETE_ACCOUNT_RESPONSE);
         return response;
     }
