17f54200273f298cf2e252578480e55e25795af4
Unknown Perforce User <dkarp>
2006-07-24 21:26:54 -0500

    (split)http://bugzilla.zimbra.com/show_bug.cgi?id=9360 bug: 9360
    Forgot a file...
    
    Copied from Perforce
     Change: 28919

---
+++ src/java/com/zimbra/cs/service/account/AccountDocumentHandler.java
@@ -0,0 +1,47 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 ("License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.zimbra.com/license
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * The Original Code is: Zimbra Collaboration Suite Server.
+ * 
+ * The Initial Developer of the Original Code is Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2005, 2006 Zimbra, Inc.
+ * All Rights Reserved.
+ * 
+ * Contributor(s): 
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.service.account;
+
+import java.util.Map;
+
+import com.zimbra.cs.service.ServiceException;
+import com.zimbra.soap.DocumentHandler;
+import com.zimbra.soap.Element;
+
+public abstract class AccountDocumentHandler extends DocumentHandler {
+
+    protected Element proxyIfNecessary(Element request, Map<String, Object> context) throws ServiceException {
+        try {
+            // by default, try to execute on the appropriate host
+            return super.proxyIfNecessary(request, context);
+        } catch (ServiceException e) {
+            // if something went wrong proxying the request, just execute it locally
+            if (ServiceException.PROXY_ERROR.equals(e.getCode()))
+                return null;
+            // but if it's a real error, it's a real error
+            throw e;
+        }
+    }
+}
