a7a70346a5e127c1432ef836a1b1b3631300a517
Unknown Perforce User <jylee>
2006-09-06 19:42:28 -0500

    (split)http://bugzilla.zimbra.com/show_bug.cgi?id=10509 bug 10509 use HttpClient instead of URLConnection for proxying client requests.  removed content caching.
    Copied from Perforce
     Change: 31296

--- src/java/com/zimbra/cs/zimlet/ProxyServlet.java
+++ src/java/com/zimbra/cs/zimlet/ProxyServlet.java
@@ -27,21 +27,23 @@ package com.zimbra.cs.zimlet;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ByteArrayInputStream;
-import java.util.zip.GZIPInputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.HttpState;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.ByteArrayRequestEntity;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
 
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.AuthToken;
@@ -57,26 +59,14 @@ import com.zimbra.cs.util.ZimbraLog;
  * @author jylee
  *
  */
+@SuppressWarnings("serial")
 public class ProxyServlet extends ZimbraServlet {
-	private static final String ACCEPT_ENCODING = "Accept-Encoding";
-	private static final String CONTENT_ENCODING = "Content-Encoding";
-	private static final String ENCODING_GZIP = "gzip";
-	
+	private static final String CONTENT_TYPE = "Content-Type";
 	private static final String TARGET_PARAM = "target";
 	private static final String USER_PARAM = "user";
 	private static final String PASS_PARAM = "pass";
 	private static final String AUTH_PARAM = "auth";
 	private static final String AUTH_BASIC = "basic";
-	
-	private static final boolean COMPRESS_CONTENTS = true;
-	private static final long CACHE_TTL = 3600 * 1000;  // 1 hour
-	
-	private Map<Integer,URLContents> mContentCache;
-	
-	public void init() throws ServletException {
-		super.init();
-		mContentCache = new HashMap<Integer,URLContents>();
-	}
 	
 	private Set<String> getAllowedDomains(AuthToken auth) throws ServiceException {
         Provisioning prov = Provisioning.getInstance();
@@ -107,58 +97,6 @@ public class ProxyServlet extends ZimbraServlet {
 		return false;
 	}
 	
-	private static class URLContents {
-		public String url;
-		public String contentType;
-		public boolean isCompressed;
-		public byte[] data;
-		public int returnCode;
-		public long createTime;
-
-		public URLContents(URL url, int error) {
-			this.url = url.toString();
-			this.returnCode = error;
-			this.createTime = System.currentTimeMillis();
-		}
-		public URLContents(URLConnection conn, boolean compress) throws IOException {
-			this.url = conn.getURL().toString();
-			this.contentType = conn.getContentType();
-			this.isCompressed = compress;
-			this.returnCode = HttpServletResponse.SC_OK;
-			this.createTime = System.currentTimeMillis();
-			InputStream stream = null;
-			if (conn instanceof HttpURLConnection) {
-				HttpURLConnection hconn = (HttpURLConnection) conn;
-				
-				this.returnCode = hconn.getResponseCode();
-				if (returnCode != HttpServletResponse.SC_OK) {
-					stream = hconn.getErrorStream();
-				}
-			}
-			if (stream == null) stream = conn.getInputStream();
-			byte[] rawData = ByteUtil.getContent(stream, conn.getContentLength());
-			//ZimbraLog.zimlet.info("******\n"+new String(rawData)+"\n******");
-			if (compress) {
-				this.data = ByteUtil.compress(rawData);
-			} else {
-				this.data = rawData;
-			}
-		}
-	}
-
-	private URLContents checkCachedURLContent(URL url) {
-		int key = url.hashCode();
-		URLContents content = mContentCache.get(key);
-		if (content != null) {
-			long now = System.currentTimeMillis();
-			if (content.createTime + CACHE_TTL < now) {
-				mContentCache.remove(key);
-				content = null;
-			}
-		}
-		return content;
-	}
-	
 	private boolean canProxyHeader(String header) {
 		if (header == null) return false;
 		header = header.toLowerCase();
@@ -178,7 +116,7 @@ public class ProxyServlet extends ZimbraServlet {
 	
 	private byte[] copyPostedData(HttpServletRequest req) throws IOException {
         int size = req.getContentLength();
-		if (req.getMethod().equals("GET") || size <= 0) {
+		if (req.getMethod().equalsIgnoreCase("GET") || size <= 0) {
 			return null;
 		}
 		InputStream is = req.getInputStream();
@@ -197,96 +135,6 @@ public class ProxyServlet extends ZimbraServlet {
             if (baos != null)
                 baos.close();
     	}
-	}
-	
-	private URLContents fetchURLContent(HttpServletRequest req, URL url, byte[] body) throws IOException {
-		URLConnection conn = url.openConnection();
-		
-		// handle basic auth
-		String auth, user, pass;
-		auth = req.getParameter(AUTH_PARAM);
-		user = req.getParameter(USER_PARAM);
-		pass = req.getParameter(PASS_PARAM);
-		if (auth != null && user != null && pass != null) {
-			if (!auth.equals(AUTH_BASIC)) {
-				ZimbraLog.zimlet.info("unsupported auth type: "+auth);
-				return new URLContents(url, HttpServletResponse.SC_BAD_REQUEST);
-			}
-			String cred = user + ":" + pass;
-			conn.setRequestProperty("Authorization", "Basic " + new String(Base64.encodeBase64(cred.getBytes())));
-		}
-		
-		Enumeration headers = req.getHeaderNames();
-		while (headers.hasMoreElements()) {
-			String hdr = (String) headers.nextElement();
-			if (canProxyHeader(hdr)) {
-				//ZimbraLog.zimlet.info(hdr + ": " + req.getHeader(hdr));
-				conn.setRequestProperty(hdr, req.getHeader(hdr));
-			}
-		}
-		if (conn instanceof HttpURLConnection) {
-			HttpURLConnection httpconn = (HttpURLConnection) conn;
-			
-			httpconn.setRequestMethod(req.getMethod());
-			if (body != null) {
-				conn.setDoOutput(true);
-				httpconn.getOutputStream().write(body);
-			}
-			int status = httpconn.getResponseCode();
-			if (status != HttpURLConnection.HTTP_OK) {
-				ZimbraLog.zimlet.info("remote host(" + url + ") returned error on proxy request: "+status);
-			}
-		}
-
-		return new URLContents(conn, COMPRESS_CONTENTS);
-	}
-	
-	private Set<String> getCacheableContentTypes(AuthToken auth) throws ServiceException {
-        Provisioning prov = Provisioning.getInstance();
-        Account acct = prov.get(AccountBy.id, auth.getAccountId());        
-		return prov.getCOS(acct).getMultiAttrSet(Provisioning.A_zimbraProxyCacheableContentTypes);
-	}
-	
-	private boolean canCacheProxyContent(HttpServletRequest req, URL url, URLContents content, AuthToken authToken) 
-			throws ServiceException {
-		if (content.contentType == null) {
-			return false;
-		}
-		// don't cache protected resources.
-		String auth = req.getParameter(AUTH_PARAM);
-		if (auth != null) {
-			return false;
-		}
-		// cache only the approved content types
-		Set<String> contentTypes = getCacheableContentTypes(authToken);
-		for (String ct : contentTypes) {
-			if (content.contentType.equalsIgnoreCase(ct)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	private URLContents getURLContent(HttpServletRequest req, URL url, AuthToken authToken, byte[] body) 
-			throws IOException {
-		// check the cache first
-		URLContents content = checkCachedURLContent(url);
-		if (content == null) {
-			
-			// fetch from the internet
-			content = fetchURLContent(req, url, body);
-
-			try {
-				// cache the result for later use
-				if (canCacheProxyContent(req, url, content, authToken)) {
-					ZimbraLog.zimlet.info("adding new proxy cache content for " + url.toString());
-					mContentCache.put(url.hashCode(), content);
-				}
-			} catch (ServiceException se) {
-				ZimbraLog.zimlet.info("error trying to cache the proxy content");
-			}
-		}
-		return content;
 	}
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
@@ -320,27 +168,63 @@ public class ProxyServlet extends ZimbraServlet {
 			resp.sendError(HttpServletResponse.SC_FORBIDDEN);
 			return;
 		}
-		
-		// fetch the contents
-		URLContents content = getURLContent(req, url, authToken, body);
-		if (content.returnCode != HttpServletResponse.SC_OK) {
-			resp.setStatus(content.returnCode);
-		}
-		
-		InputStream data = new ByteArrayInputStream(content.data);
 
-		// uncompress the result if the browser doesn't accept gzip encoding
-		if (content.isCompressed) {
-			String acceptEncoding = req.getHeader(ACCEPT_ENCODING);
-			if (acceptEncoding == null || acceptEncoding.indexOf(ENCODING_GZIP) == -1) {
-				// uncompress
-				ZimbraLog.zimlet.debug("compression not supported");
-				data = new GZIPInputStream(data);
+		HttpMethod method = null;
+		try {
+			HttpClient client = new HttpClient();
+			String reqMethod = req.getMethod();
+			
+			if (reqMethod.equalsIgnoreCase("GET"))
+				method = new GetMethod(target);
+			else if (reqMethod.equalsIgnoreCase("POST")) {
+				PostMethod post = new PostMethod(target);
+				post.setRequestEntity(new ByteArrayRequestEntity(body, req.getContentType()));
+				method = post;
 			} else {
-				resp.addHeader(CONTENT_ENCODING, ENCODING_GZIP);
+				ZimbraLog.zimlet.info("unsupported request method: "+reqMethod);
+				resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+				return;
 			}
+
+			// handle basic auth
+			String auth, user, pass;
+			auth = req.getParameter(AUTH_PARAM);
+			user = req.getParameter(USER_PARAM);
+			pass = req.getParameter(PASS_PARAM);
+			if (auth != null && user != null && pass != null) {
+				if (!auth.equals(AUTH_BASIC)) {
+					ZimbraLog.zimlet.info("unsupported auth type: "+auth);
+					resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+					return;
+				}
+				HttpState state = new HttpState();
+				state.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(user, pass));
+				client.setState(state);
+				method.setDoAuthentication(true);
+			}
+			
+			Enumeration headers = req.getHeaderNames();
+			while (headers.hasMoreElements()) {
+				String hdr = (String) headers.nextElement();
+				if (canProxyHeader(hdr)) {
+					//ZimbraLog.zimlet.info(hdr + ": " + req.getHeader(hdr));
+					method.addRequestHeader(hdr, req.getHeader(hdr));
+				}
+			}
+			
+			try {
+				client.executeMethod(method);
+			} catch (HttpException ex) {
+				ZimbraLog.zimlet.info("exception while proxying "+target, ex);
+				resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+				return;
+			}
+			
+			resp.setContentType(method.getResponseHeader(CONTENT_TYPE).getValue());
+			ByteUtil.copy(method.getResponseBodyAsStream(), false, resp.getOutputStream(), false);
+		} finally {
+			if (method != null)
+				method.releaseConnection();
 		}
-		resp.setContentType(content.contentType);
-		ByteUtil.copy(data, true, resp.getOutputStream(), false);
 	}
 }
