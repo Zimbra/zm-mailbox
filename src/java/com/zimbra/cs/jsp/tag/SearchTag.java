b26f3749875c71a589905655b2568b556c4c12e3
Unknown Perforce User <schemers>
2006-09-18 13:41:00 -0500

    bug: none (update license)
    Copied from Perforce
     Change: 31783

--- src/java/com/zimbra/cs/jsp/tag/SearchTag.java
+++ src/java/com/zimbra/cs/jsp/tag/SearchTag.java
@@ -6,6 +6,14 @@
  * Version 1.1 ("License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.zimbra.com/license
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 ("License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.zimbra.com/license
  * 
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
@@ -15,7 +23,7 @@
  * The Original Code is: Zimbra Collaboration Suite Server.
  * 
  * The Initial Developer of the Original Code is Zimbra, Inc.
- * Portions created by Zimbra are Copyright (C) 2005 Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2006 Zimbra, Inc.
  * All Rights Reserved.
  * 
  * Contributor(s): 
@@ -25,13 +33,11 @@
 package com.zimbra.cs.jsp.tag;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.jsp.JspContext;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
 
-import com.zimbra.cs.jsp.bean.ZSearchHitBean;
 import com.zimbra.cs.jsp.bean.ZSearchResultBean;
 import com.zimbra.cs.service.ServiceException;
 import com.zimbra.cs.zclient.ZMailbox;
