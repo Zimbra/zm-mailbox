3faf201118d675d7423f3151c475a9396129cd0d
Unknown Perforce User <dkarp>
2006-10-03 13:26:17 -0500

    (split)http://bugzilla.zimbra.com/show_bug.cgi?id=2548 bug: 2548
    Allow the caller to set the following metadata during a SaveDraft call:
    
       [l="{folder}"] [f="{flags}"] [t="{tag-list}"] [color="{color}"]
    
    This metadata setting is performed after the draft is saved; if it fails for any reason, the draft will still be saved.  The response contains all the relevant information on the resulting draft, so the caller can figure out the state of the world post-call.
    
    Proxying should now work on SaveDraft.  If an existing draft is specified, we'll proxy to that mailbox.  If it's a new draft, we'll proxy to the specified target folder.  If neither's specified, it's executed in the auth token owner's mailbox.
    
    Copied from Perforce
     Change: 32732

--- src/java/com/zimbra/cs/service/mail/SaveDraft.java
+++ src/java/com/zimbra/cs/service/mail/SaveDraft.java
@@ -28,21 +28,27 @@
  */
 package com.zimbra.cs.service.mail;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Map;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
+import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.account.Account;
+import com.zimbra.cs.mailbox.MailItem;
+import com.zimbra.cs.mailbox.MailServiceException;
 import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.cs.mailbox.Message;
 import com.zimbra.cs.mailbox.Mailbox.OperationContext;
 import com.zimbra.cs.mime.ParsedMessage;
+import com.zimbra.cs.operation.ItemActionOperation;
 import com.zimbra.cs.operation.SaveDraftOperation;
 import com.zimbra.cs.operation.Operation.Requester;
 import com.zimbra.cs.service.FileUploadServlet;
 import com.zimbra.cs.service.ServiceException;
+import com.zimbra.cs.service.util.ItemId;
 import com.zimbra.cs.session.Session;
 import com.zimbra.cs.util.AccountUtil;
 import com.zimbra.soap.Element;
@@ -53,11 +59,22 @@ import com.zimbra.soap.ZimbraSoapContext;
  */
 public class SaveDraft extends MailDocumentHandler {
 
+    private static final String[] TARGET_DRAFT_PATH = new String[] { MailService.E_MSG, MailService.A_ID };
+    private static final String[] TARGET_FOLDER_PATH = new String[] { MailService.E_MSG, MailService.A_FOLDER };
+    private static final String[] RESPONSE_ITEM_PATH = new String[] { };
+    protected String[] getProxiedIdPath(Element request) {
+        return getXPath(request, TARGET_DRAFT_PATH) != null ? TARGET_DRAFT_PATH : TARGET_FOLDER_PATH;
+    }
+    protected boolean checkMountpointProxy(Element request) {
+        return getXPath(request, TARGET_DRAFT_PATH) == null;
+    }
+    protected String[] getResponseItemPath()  { return RESPONSE_ITEM_PATH; }
+
     public Element handle(Element request, Map<String, Object> context) throws ServiceException {
-        ZimbraSoapContext lc = getZimbraSoapContext(context);
-        Account acct = getRequestedAccount(lc);
-        Mailbox mbox = getRequestedMailbox(lc);
-        OperationContext octxt = lc.getOperationContext();
+        ZimbraSoapContext zsc = getZimbraSoapContext(context);
+        Account acct = getRequestedAccount(zsc);
+        Mailbox mbox = getRequestedMailbox(zsc);
+        OperationContext octxt = zsc.getOperationContext();
         Session session = getSession(context);
 
         Element msgElem = request.getElement(MailService.E_MSG);
@@ -66,15 +83,26 @@ public class SaveDraft extends MailDocumentHandler {
         int origId = (int) msgElem.getAttributeLong(MailService.A_ORIG_ID, 0);
         String replyType = msgElem.getAttribute(MailService.A_REPLY_TYPE, null);
 
+        // allow the caller to update the draft's metadata at the same time as they save the draft
+        String folderId = msgElem.getAttribute(MailService.A_FOLDER, null);
+        ItemId iidFolder = new ItemId(folderId == null ? "-1" : folderId, zsc);
+        if (!iidFolder.belongsTo(mbox))
+            throw ServiceException.INVALID_REQUEST("cannot move item between mailboxes", null);
+        else if (folderId != null && iidFolder.getId() <= 0)
+            throw MailServiceException.NO_SUCH_FOLDER(iidFolder.getId());
+        String flags = msgElem.getAttribute(MailService.A_FLAGS, null);
+        String tags  = msgElem.getAttribute(MailService.A_TAGS, null);
+        byte color = (byte) msgElem.getAttributeLong(MailService.A_COLOR, -1);
+
         // check to see whether the entire message has been uploaded under separate cover
         String attachment = msgElem.getAttribute(MailService.A_ATTACHMENT_ID, null);
 
         ParseMimeMessage.MimeMessageData mimeData = new ParseMimeMessage.MimeMessageData();
         MimeMessage mm;
         if (attachment != null)
-            mm = SendMsg.parseUploadedMessage(lc, attachment, mimeData);
+            mm = SendMsg.parseUploadedMessage(zsc, attachment, mimeData);
         else
-            mm = ParseMimeMessage.parseMimeMsgSoap(lc, mbox, msgElem, null, mimeData);
+            mm = ParseMimeMessage.parseMimeMsgSoap(zsc, mbox, msgElem, null, mimeData);
 
         long date = System.currentTimeMillis();
         try {
@@ -101,9 +129,22 @@ public class SaveDraft extends MailDocumentHandler {
         if (mimeData.uploads != null)
             FileUploadServlet.deleteUploads(mimeData.uploads);
 
-        Element response = lc.createElement(MailService.SAVE_DRAFT_RESPONSE);
+        // try to set the metadata on the new/revised draft
+        if (folderId != null || flags != null || tags != null || color >= 0) {
+            try {
+                // best not to fail if there's an error here...
+                ItemActionOperation.UPDATE(zsc, session, octxt, mbox, Requester.SOAP, Arrays.asList(msg.getId()),
+                                           MailItem.TYPE_MESSAGE, null, iidFolder, flags, tags, color);
+                // and make sure the Message object reflects post-update reality
+                msg = mbox.getMessageById(octxt, msg.getId());
+            } catch (ServiceException e) {
+                ZimbraLog.soap.warn("error setting metadata for draft " + msg.getId() + "; skipping that operation", e);
+            }
+        }
+
+        Element response = zsc.createElement(MailService.SAVE_DRAFT_RESPONSE);
         // FIXME: inefficient -- this recalculates the MimeMessage (but SaveDraft is called rarely)
-        ToXML.encodeMessageAsMP(response, lc, msg, null, false, true);
+        ToXML.encodeMessageAsMP(response, zsc, msg, null, false, true);
         return response;
     }
 }
