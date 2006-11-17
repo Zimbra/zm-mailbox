f64f543ada9fd7cab56332d4c5009f464a2cd3a3
Unknown Perforce User <jhahm>
2006-11-16 20:26:41 -0600

    (split)http://bugzilla.zimbra.com/show_bug.cgi?id=11928 bug: 11928
    Some new files were left out of the last checkin.
    
    Copied from Perforce
     Change: 35769

---
+++ src/java/com/zimbra/cs/mailbox/Task.java
@@ -0,0 +1,76 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 ("License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.zimbra.com/license
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * The Original Code is: Zimbra Collaboration Suite Server.
+ * 
+ * The Initial Developer of the Original Code is Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2005, 2006 Zimbra, Inc.
+ * All Rights Reserved.
+ * 
+ * Contributor(s): 
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.cs.mailbox;
+
+import javax.mail.internet.MimeMessage;
+
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.mailbox.Mailbox.OperationContext;
+import com.zimbra.cs.mailbox.calendar.Invite;
+import com.zimbra.cs.redolog.op.CreateCalendarItemPlayer;
+import com.zimbra.cs.redolog.op.CreateCalendarItemRecorder;
+import com.zimbra.cs.service.ServiceException;
+
+public class Task extends CalendarItem {
+    public Task(Mailbox mbox, UnderlyingData data) throws ServiceException {
+        super(mbox, data);
+        if (mData.type != TYPE_TASK)
+            throw new IllegalArgumentException();
+    }
+
+    protected String processPartStat(Invite invite,
+                                     MimeMessage mmInv,
+                                     boolean forCreate,
+                                     String defaultPartStat)
+    throws ServiceException {
+        Mailbox mbox = getMailbox();
+        OperationContext octxt = mbox.getOperationContext();
+        CreateCalendarItemPlayer player =
+            octxt != null ? (CreateCalendarItemPlayer) octxt.getPlayer() : null;
+
+        String partStat = defaultPartStat;
+        if (player != null) {
+            String p = player.getCalendarItemPartStat();
+            if (p != null) partStat = p;
+        }
+
+        CreateCalendarItemRecorder recorder =
+            (CreateCalendarItemRecorder) mbox.getRedoRecorder();
+        recorder.setCalendarItemPartStat(partStat);
+
+        Account account = getMailbox().getAccount();
+        invite.updateMyPartStat(account, partStat);
+        if (forCreate) {
+            Invite defaultInvite = getDefaultInviteOrNull();
+            if (defaultInvite != null && !defaultInvite.equals(invite) &&
+                !partStat.equals(defaultInvite.getPartStat())) {
+                defaultInvite.updateMyPartStat(account, partStat);
+                saveMetadata();
+            }
+        }
+        return partStat;
+    }
+}
