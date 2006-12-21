4a8c02d8fd32d523859d05b902439f625bd9a4d9
Unknown Perforce User <bburtin>
2006-12-21 14:32:19 -0600

    bug: none
    Moved getExtension() from StringUtil to FileUtil, so that it's in the
    same class as trimFilename().
    
    Copied from Perforce
     Change: 38840

--- src/java/com/zimbra/common/util/StringUtil.java
+++ src/java/com/zimbra/common/util/StringUtil.java
@@ -402,10 +402,10 @@ public class StringUtil {
     /**
      * Returns the simple class name (the name after the last dot)
      * from a fully-qualified class name.  Behavior is the same as
-     * {@link #getExtension}. 
+     * {@link FileUtil#getExtension}. 
      */
     public static String getSimpleClassName(String className) {
-        return getExtension(className);
+        return FileUtil.getExtension(className);
     }
     
     /**
@@ -416,33 +416,7 @@ public class StringUtil {
         if (o == null) {
             return null;
         }
-        return getExtension(o.getClass().getName());
-    }
-    
-    /**
-     * Returns the extension portion of the given filename.
-     * <ul>
-     *   <li>If <code>filename</code> contains one or more dots, returns
-     *     all characters after the last dot.</li>
-     *   <li>If <code>filename</code> contains no dot, returns <code>filename</code>.</li>
-     *   <li>If <code>filename</code> is <code>null</code>, returns
-     *     <code>null</code>.</li>
-     *   <li>If <code>filename</code> ends with a dot, returns an
-     *     empty <code>String</code>.</li>
-     * </ul> 
-     */
-    public static String getExtension(String filename) {
-        if (filename == null) {
-            return null;
-        }
-        int lastDot = filename.lastIndexOf(".");
-        if (lastDot == -1) {
-            return filename;
-        }
-        if (lastDot == filename.length() - 1) {
-            return "";
-        }
-        return filename.substring(lastDot + 1, filename.length());
+        return FileUtil.getExtension(o.getClass().getName());
     }
     
     /**
