b03e63dabb66d57441b1bdae8254a2e5d2ad968f
Unknown Perforce User <tim>
2007-01-02 13:44:19 -0600

    (split)http://bugzilla.zimbra.com/show_bug.cgi?id=7074 bug: 7074
    Somehow, this file got missed in my last checkin.
    
    Copied from Perforce
     Change: 39720

---
+++ src/java/com/zimbra/cs/redolog/op/SaveChat.java
@@ -0,0 +1,84 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 ("License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.zimbra.com/license
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * The Original Code is: Zimbra Collaboration Suite Server.
+ * 
+ * The Initial Developer of the Original Code is Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2004, 2005, 2006 Zimbra, Inc.
+ * All Rights Reserved.
+ * 
+ * Contributor(s): 
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.redolog.op;
+
+import java.io.IOException;
+
+import com.zimbra.cs.mailbox.Mailbox;
+import com.zimbra.cs.mailbox.MailboxManager;
+import com.zimbra.cs.mime.ParsedMessage;
+import com.zimbra.cs.redolog.RedoLogInput;
+import com.zimbra.cs.redolog.RedoLogOutput;
+
+public class SaveChat extends CreateChat {
+    
+    private int mImapId;           // new IMAP id for this message
+    
+    public SaveChat() {}
+    
+    public SaveChat(int mailboxId, int chatId, 
+                String digest,
+                int msgSize,
+                int folderId,
+                int flags,
+                String tags)
+    {
+        super(mailboxId, digest, msgSize, folderId, flags, tags);
+        setMessageId(chatId);
+    }
+
+    public int getImapId() {
+        return mImapId;
+    }
+
+    public void setImapId(int imapId) {
+        mImapId = imapId;
+    }
+
+    protected String getPrintableData() {
+        return super.getPrintableData() + ",imap=" + mImapId;
+    }
+    
+    public int getOpCode() {
+        return OP_SAVE_DRAFT;
+    }
+    
+    protected void serializeData(RedoLogOutput out) throws IOException {
+        out.writeInt(mImapId);
+        super.serializeData(out);
+    }
+
+    protected void deserializeData(RedoLogInput in) throws IOException {
+        mImapId = in.readInt();
+        super.deserializeData(in);
+    }
+    
+    public void redo() throws Exception {
+        int mboxId = getMailboxId();
+        Mailbox mbox = MailboxManager.getInstance().getMailboxById(mboxId);
+
+        ParsedMessage pm = new ParsedMessage(getMessageBody(), getTimestamp(), mbox.attachmentsIndexingEnabled());
+        mbox.updateChat(getOperationContext(), pm, getMessageId());
+    }
+}
