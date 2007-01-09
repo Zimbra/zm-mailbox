ecad39b35aa643a39b7a1c04523638041aa557da
Unknown Perforce User <jhahm>
2007-01-08 18:53:15 -0600

    http://bugzilla.zimbra.com/show_bug.cgi?id=13389 bug: 13389
    Add "FORCE INDEX (i_folder_id_date)" hint to mail_item table query if querying for messages (type = 5) in exactly one folder (folder_id = <val>), no other conditions are specified, sorting by date, and a limit is given.  In all other cases, no hint string is inserted and we let the database engine's optimizer pick the execution plan.  The hint string is also omitted if localconfig key search_disable_database_hints is set to false.  (It defaults to true.)
    
    The FORCE INDEX hint may cause a problem in non-mysql database.
    
    Slightly rewrote the queries for retrieving distinct set of flags and tags used by a mailbox.  The rewritten queries allow mysql to use i_flags_date or i_tags_date index to efficiently figure out the set, eliminating the need to scan all mail_item rows for the mailbox.
    
    Copied from Perforce
     Change: 40406

--- src/java/com/zimbra/common/localconfig/LC.java
+++ src/java/com/zimbra/common/localconfig/LC.java
@@ -222,6 +222,8 @@ public class LC {
 
     public static final KnownKey timezone_file;
 
+    public static final KnownKey search_disable_database_hints;
+
     static {
         final String ZM_MYCNF_CAVEAT = "This value is stored here for use by zmmycnf program.  Changing this setting does not immediately reflect in MySQL server.  You will have to, with abundant precaution, re-generate my.cnf and restart MySQL server for the change to take effect.";
         final String FS = File.separator;
@@ -865,5 +867,10 @@ public class LC {
         timezone_file.setDefault("${zimbra_home}" + FS + "conf" + FS + "timezones.ics");
         timezone_file.setDoc
         ("iCalendar file listing well-known time zones");
+
+        search_disable_database_hints = new KnownKey("search_disable_database_hints");
+        search_disable_database_hints.setDefault("false");
+        search_disable_database_hints.setDoc
+        ("If true, do not use database hints in queries generated during search");
     }
 }
