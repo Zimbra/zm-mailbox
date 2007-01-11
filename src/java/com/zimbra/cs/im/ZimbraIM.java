73da1812d15a783aeea92e1d68ac81dc8550e275
Unknown Perforce User <tim>
2007-01-11 17:43:09 -0600

    (split)http://bugzilla.zimbra.com/show_bug.cgi?id=7074 bug: 7074
    Populate WF's list of local domains by calling Provisioning.getAllDomains() at startup.  Validate incoming client connections against the list of domains and reject the client with 'host-unknown' if the client is from a domain we don't know about.  There is currently no listener on the list of domains -- this means that you need to restart the server when you add/remove domains for now.
    
    Copied from Perforce
     Change: 40873

--- src/java/com/zimbra/cs/im/ZimbraIM.java
+++ src/java/com/zimbra/cs/im/ZimbraIM.java
@@ -1,10 +1,14 @@
 package com.zimbra.cs.im;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jivesoftware.wildfire.XMPPServer;
 import org.jivesoftware.wildfire.interceptor.InterceptorManager;
 
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.account.Domain;
 import com.zimbra.cs.account.Provisioning;
 
 public class ZimbraIM {
@@ -15,15 +19,23 @@ public class ZimbraIM {
         try {
             System.setProperty("wildfireHome", "/opt/zimbra");
             
+            ArrayList<String> domainStrs = new ArrayList<String>();
+            
             String defaultDomain = Provisioning.getInstance().getConfig().getAttr(Provisioning.A_zimbraDefaultDomainName, null);
             if (defaultDomain != null) {
                 ZimbraLog.im.info("Setting default XMPP domain to: "+defaultDomain);
-                System.setProperty("wildfireDomain", defaultDomain);
-            } else {
-                ZimbraLog.im.warn("No default domain has been set, domain must be set in JiveProperties table or IM will not work"+defaultDomain);
+                domainStrs.add(defaultDomain);
+            } 
+            List<Domain> domains = Provisioning.getInstance().getAllDomains();
+            for (Domain d : domains) {
+                domainStrs.add(d.getName());
             }
+            
+            // set the special msgs ClassLoader -- so that WF looks in our conf/msgs directory
+            // for its localization .properties bundles.
             org.jivesoftware.util.LocaleUtils.sMsgsClassLoader = com.zimbra.cs.util.L10nUtil.getMsgClassLoader();             
-            XMPPServer srv = new XMPPServer();
+            
+            XMPPServer srv = new XMPPServer(domainStrs);
             InterceptorManager.getInstance().addInterceptor(new com.zimbra.cs.im.PacketInterceptor());
             
             sRunning = true;
