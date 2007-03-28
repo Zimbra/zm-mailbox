7e54d76196859e44b6d2f28d65c4a99cf153cf04
Unknown Perforce User <schemers>
2007-03-28 18:36:19 -0500

    (split)http://bugzilla.zimbra.com/show_bug.cgi?id=14164 bug: 14164 - add an "Add Attendees" toolbar button to pick attendees from contacts/gal
    Copied from Perforce
     Change: 48491

--- src/java/com/zimbra/cs/service/admin/CreateAccount.java
+++ src/java/com/zimbra/cs/service/admin/CreateAccount.java
@@ -28,16 +28,16 @@
  */
 package com.zimbra.cs.service.admin;
 
-import java.util.Map;
-
-import com.zimbra.cs.service.account.ToXML;
 import com.zimbra.common.service.ServiceException;
-import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.common.soap.AdminConstants;
 import com.zimbra.common.soap.Element;
+import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.service.account.ToXML;
 import com.zimbra.soap.ZimbraSoapContext;
+
+import java.util.Map;
 
 /**
  * @author schemers
