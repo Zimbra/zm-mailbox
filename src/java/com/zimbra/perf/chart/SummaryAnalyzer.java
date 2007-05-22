02e5467f19d182e8fb76c4201632ac0bcbe9cf31
Unknown Perforce User <jhahm>
2007-05-21 22:30:43 -0500

    http://bugzilla.zimbra.com/show_bug.cgi?id=12095 bug: 12095
    Moved chart generation code into ZimbraCommon.  Much code cleanup.
    
    /opt/zimbra/bin/zmstat-chart-config script is provided to generate chart configuration for the host.  All configuration data is embedded in the script, so no external xml file needs to be provided.  The generated .xml file can be edited to suit your needs.  The generated file should be comprehensive enough for most installs.  (It includes detailed GC and heap info.)
    
    /opt/zimbra/bin/zmstat-chart script does chart generation.
    
    Copied from Perforce
     Change: 53402

---
+++ src/java/com/zimbra/perf/chart/SummaryAnalyzer.java
@@ -0,0 +1,243 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 ("License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.zimbra.com/license
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * The Original Code is: Zimbra Collaboration Suite Server.
+ * 
+ * The Initial Developer of the Original Code is Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2007 Zimbra, Inc.
+ * All Rights Reserved.
+ * 
+ * Contributor(s): 
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.perf.chart;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import com.zimbra.common.util.CsvReader;
+
+
+public class SummaryAnalyzer {
+
+    private Map<String, List<PlotSettings>> mOutfilePlotsMap = null;
+
+    private List<ReportEntry> mEntryList = new ArrayList<ReportEntry>();
+
+    public SummaryAnalyzer(List<ChartSettings> chartSettings) {
+        mOutfilePlotsMap = new HashMap<String, List<PlotSettings>>(chartSettings.size());
+        for (ChartSettings cs : chartSettings) {
+            mOutfilePlotsMap.put(cs.getOutfile(), cs.getPlots());
+        }
+    }
+
+    private static String buildKey(String infile, String data, String aggregateFun) {
+        return infile + ":" + data + ":" + aggregateFun;
+    }
+
+    private static String getVolume(String dataColumn) {
+        return dataColumn.split(":")[0];
+    }
+    
+    private void buildSingleVolume(String volumeName, String name){
+          String key;
+          key = buildKey(SummaryConstants.IO_IN,volumeName + ":"
+                  + SummaryConstants.IO_UTIL,
+                    PlotSettings.AGG_FUNCTION_AVG);
+          mEntryList.add(new ReportEntry( name + " Disk Util",key));
+          key = buildKey(
+                SummaryConstants.IO_IN, volumeName + ":"
+                        + SummaryConstants.R_THROUGHPUT,
+                PlotSettings.AGG_FUNCTION_AVG);
+          mEntryList.add(new ReportEntry( name + " Read Throughput",key));
+          
+          key = buildKey(
+                SummaryConstants.IO_IN, volumeName + ":"
+                        + SummaryConstants.W_THROUGHPUT,
+                PlotSettings.AGG_FUNCTION_AVG);
+          mEntryList.add(new ReportEntry(name + " Write Throughput",key));
+          key = buildKey(
+                SummaryConstants.IO_IN, volumeName + ":"
+                        + SummaryConstants.R_IOPS,
+                PlotSettings.AGG_FUNCTION_AVG);
+          mEntryList.add(new ReportEntry(name + " Read IOPS",key));
+          key = buildKey(
+                SummaryConstants.IO_IN, volumeName + ":"
+                        + SummaryConstants.W_IOPS,
+                PlotSettings.AGG_FUNCTION_AVG);
+          mEntryList.add(new ReportEntry(name + " write IOPS",key));
+    }
+
+    private List<PlotSettings> getIOPlotSettings(){
+        Iterator<String> it = mOutfilePlotsMap.keySet().iterator();
+        List<PlotSettings> settings = null;
+        String key;
+        while (it.hasNext()) {
+            key = (String) it.next();
+            if (key.indexOf(SummaryConstants.KEY_TO_DISK_UTIL) != -1 ) {
+                settings = mOutfilePlotsMap.get(key);
+                break;
+            }
+        }
+        return settings;
+    }
+
+    private  void buildIOKeys() {
+        List<PlotSettings> settings = getIOPlotSettings();        
+        for (PlotSettings ps : settings) {
+            String legend = ps.getLegend();
+            String volume = getVolume(ps.getDataColumn());    
+            buildSingleVolume(volume, legend);
+        }    
+    }
+
+    private void buildCPUKeys() {
+         String key;
+         key = buildKey(SummaryConstants.CPU_IN,
+                  SummaryConstants.SYS,
+                    PlotSettings.AGG_FUNCTION_AVG);
+         mEntryList.add(new ReportEntry("Mail Server CPU Sys ",key));
+         
+         key = buildKey(SummaryConstants.CPU_IN,
+                  SummaryConstants.USER,
+                    PlotSettings.AGG_FUNCTION_AVG);
+         mEntryList.add(new ReportEntry("Mail Server CPU User ",key));
+         
+         key = buildKey(SummaryConstants.CPU_IN,
+                  SummaryConstants.IDLE,
+                    PlotSettings.AGG_FUNCTION_AVG);
+         mEntryList.add(new ReportEntry("Mail Server CPU Idle ",key));
+         
+         key = buildKey(SummaryConstants.CPU_IN,
+                  SummaryConstants.IOWAIT,
+                    PlotSettings.AGG_FUNCTION_AVG);
+         mEntryList.add(new ReportEntry("Mail Server CPU Iowait ",key));
+
+    }
+
+    private void buildMemKeys() {
+         String key;
+         key = buildKey(SummaryConstants.GC_IN,
+                  SummaryConstants.F_GC,
+                    PlotSettings.AGG_FUNCTION_AVG);
+         mEntryList.add(new ReportEntry("Full GC% ",key));
+         
+         key = buildKey(SummaryConstants.GC_IN,
+                  SummaryConstants.Y_GC,
+                    PlotSettings.AGG_FUNCTION_AVG);
+         mEntryList.add(new ReportEntry("Young GC% ",key));
+    }
+
+    private void buildServerResponseKeys() {
+         String key;
+         key = buildKey(SummaryConstants.SERVER_IN,
+                  SummaryConstants.ADD_COUNT,
+                    PlotSettings.AGG_FUNCTION_AVG);
+         mEntryList.add(new ReportEntry("Mailbox add rate ",key));
+         
+         key = buildKey(SummaryConstants.SERVER_IN,
+                  SummaryConstants.ADD_LATENCY,
+                    PlotSettings.AGG_FUNCTION_AVG);
+         mEntryList.add(new ReportEntry("Mailbox add latency ",key)); 
+         
+         key = buildKey(SummaryConstants.SERVER_IN,
+                  SummaryConstants.GET_LATENCY,
+                    PlotSettings.AGG_FUNCTION_AVG);
+         mEntryList.add(new ReportEntry("Mailbox get latency ",key));
+         
+         key = buildKey(SummaryConstants.SERVER_IN,
+                  SummaryConstants.SOAP_RESPONSE,
+                    PlotSettings.AGG_FUNCTION_AVG);
+         mEntryList.add(new ReportEntry("Soap response time",key));
+         
+         key = buildKey(SummaryConstants.SERVER_IN,
+                  SummaryConstants.POP_RESPONSE,
+                    PlotSettings.AGG_FUNCTION_AVG);
+         mEntryList.add(new ReportEntry("Pop response time",key));
+         
+         key = buildKey(SummaryConstants.SERVER_IN,
+                  SummaryConstants.IMAP_RESPONSE,
+                    PlotSettings.AGG_FUNCTION_AVG);
+         mEntryList.add(new ReportEntry("Imap response time",key));
+    }
+    
+    public class ReportEntry{
+        String entryName;
+        String entryKey;
+        public ReportEntry(String keyName, String keyVal){
+            this.entryName = keyName;
+            this.entryKey = keyVal;
+        }
+        public String getEntryKey() {
+            return entryKey;
+        }
+        public void setEntryKey(String entryKey) {
+            this.entryKey = entryKey;
+        }
+        public String getEntryName() {
+            return entryName;
+        }
+        public void setEntryName(String entryName) {
+            this.entryName = entryName;
+        }
+    }
+
+    public void writeReport(File summaryCsv) throws IOException {
+        buildIOKeys();
+        buildCPUKeys();
+        buildMemKeys();
+        buildServerResponseKeys();
+
+        FileReader reader = null;
+        CsvReader csv = null;
+        FileWriter writer = null;
+        try {
+            reader = new FileReader(summaryCsv);
+            csv = new CsvReader(reader);
+            File summaryTxt = new File(summaryCsv.getParentFile(),
+                    SummaryConstants.SUMMARY_TXT);
+            writer = new FileWriter(summaryTxt);
+            writer.write("===========================================\n");
+            writer.write("#            Perf Stats                   #\n");
+            writer.write("===========================================\n");
+
+            if(csv.hasNext()){
+                for(ReportEntry entry : mEntryList){
+                  if(csv.columnExists(entry.entryKey)){
+                      writer.write(entry.entryName + ":" + csv.getValue(entry.entryKey));
+                  }else{
+                      writer.write(entry.entryName + ":");
+                  }
+                  writer.write("\n");
+                }
+            }
+        } finally {
+            if (writer != null)
+                writer.close();
+            if (csv != null)
+                csv.close();
+            else if (reader != null)
+                reader.close();
+        }
+    }
+}
