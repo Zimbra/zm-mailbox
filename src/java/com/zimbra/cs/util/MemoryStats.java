8822d14b3dc37e0abf464ed59967e97a307b3686
Unknown Perforce User <jhahm>
2007-05-21 22:30:43 -0500

    (split)http://bugzilla.zimbra.com/show_bug.cgi?id=12095 bug: 12095
    Moved chart generation code into ZimbraCommon.  Much code cleanup.
    
    /opt/zimbra/bin/zmstat-chart-config script is provided to generate chart configuration for the host.  All configuration data is embedded in the script, so no external xml file needs to be provided.  The generated .xml file can be edited to suit your needs.  The generated file should be comprehensive enough for most installs.  (It includes detailed GC and heap info.)
    
    /opt/zimbra/bin/zmstat-chart script does chart generation.
    
    Copied from Perforce
     Change: 53402

--- src/java/com/zimbra/cs/util/MemoryStats.java
+++ src/java/com/zimbra/cs/util/MemoryStats.java
@@ -15,7 +15,7 @@
  * The Original Code is: Zimbra Collaboration Suite Server.
  * 
  * The Initial Developer of the Original Code is Zimbra, Inc.
- * Portions created by Zimbra are Copyright (C) 2005 Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2007 Zimbra, Inc.
  * All Rights Reserved.
  * 
  * Contributor(s):
@@ -32,8 +32,10 @@ import java.lang.management.MemoryUsage;
 import java.util.ArrayList;
 import java.util.Formatter;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.zimbra.cs.stats.RealtimeStatsCallback;
 import com.zimbra.cs.stats.ZimbraPerf;
@@ -43,9 +45,46 @@ import com.zimbra.cs.stats.ZimbraPerf;
  * JVM statistics at runtime
  */
 public final class MemoryStats implements RealtimeStatsCallback {
-    
+
+    private static final String MEMPOOL_PREFIX = "mempool_";
+    private static final String USED_SUFFIX = "_used";
+    private static final String FREE_SUFFIX = "_free";
+    private static final String GC_PREFIX = "gcollector_";
+    private static final String COUNT_SUFFIX = "_count";
+    private static final String TIME_SUFFIX = "_time";
+    private static final String MINOR = "minor";
+    private static final String MAJOR = "major";
+
+    private static final String HEAP_USED = "heap_used";
+    private static final String HEAP_FREE = "heap_free";
+
+    private static final String GC_MINOR_COUNT = GC_PREFIX + MINOR + COUNT_SUFFIX; 
+    private static final String GC_MINOR_TIME  = GC_PREFIX + MINOR + TIME_SUFFIX; 
+    private static final String GC_MAJOR_COUNT = GC_PREFIX + MAJOR + COUNT_SUFFIX; 
+    private static final String GC_MAJOR_TIME  = GC_PREFIX + MAJOR + TIME_SUFFIX; 
+
+    // complete set of GC memory manager names from JDK memoryManager.hpp/cpp
+    private static final String MEMMGR_GC_MAJOR_MSC = "MarkSweepCompact";
+    private static final String MEMMGR_GC_MAJOR_CMS = "ConcurrentMarkSweep";
+    private static final String MEMMGR_GC_MAJOR_PS_MARKSWEEP = "PS MarkSweep";
+    private static final String MEMMGR_GC_MAJOR_TRAIN = "Train";  // obsoleted in JDK1.6
+    //private static final String MEMMGR_GC_MINOR_COPY = "Copy";
+    //private static final String MEMMGR_GC_MINOR_PARNEW = "ParNew";
+    //private static final String MEMMGR_GC_MINOR_PS_SCAVENGE = "PS Scavenge";
+
+    private static Set<String> sMajorCollectors;
+
     private static MemoryStats sInstance = null;
-    
+
+    static {
+        // Set of major garbage collectors.  All others are minor collectors.
+        sMajorCollectors = new HashSet<String>(4);
+        sMajorCollectors.add(MEMMGR_GC_MAJOR_MSC);
+        sMajorCollectors.add(MEMMGR_GC_MAJOR_CMS);
+        sMajorCollectors.add(MEMMGR_GC_MAJOR_PS_MARKSWEEP);
+        sMajorCollectors.add(MEMMGR_GC_MAJOR_TRAIN);
+    }
+
     /**
      * Output human-readable information about the garbage collectors in the system
      * 
@@ -111,7 +150,7 @@ public final class MemoryStats implements RealtimeStatsCallback {
             sb.append('\n');
         }
         sb.append(new Formatter().format(
-            "RuntimeTotal=%,d RuntimeMax=%,d  RuntimeFree=%,d  TotUsed=%,d  TotReserved=%,d  TotMax=%,d  CollectUsed=%,d\n",
+            "RuntimeTotal=%,d  RuntimeMax=%,d  RuntimeFree=%,d  TotUsed=%,d  TotReserved=%,d  TotMax=%,d  CollectUsed=%,d\n",
             Runtime.getRuntime().totalMemory(), Runtime.getRuntime().maxMemory(), Runtime.getRuntime().freeMemory(), 
             totalUsed, totalReserved, totalMax, collectUsed));
         return sb.toString();
@@ -173,35 +212,41 @@ public final class MemoryStats implements RealtimeStatsCallback {
     public static void startup() {
         sInstance = new MemoryStats();
     }
-    
+
     private static final String getGCCountColName(String gcName) {
-        return "gcollector_"+spaceToUs(gcName)+"_count";
+        return GC_PREFIX + spaceToUs(gcName) + COUNT_SUFFIX;
     }
-    
+
     private static final String getGCTimeColName(String gcName) {
-        return "gcollector_"+spaceToUs(gcName)+"_time";
+        return GC_PREFIX + spaceToUs(gcName) + TIME_SUFFIX;
     }
+
     private static final String getPoolFreeSizeColName(String poolName) {
-        return "mempool_"+spaceToUs(poolName)+"_free";
+        return MEMPOOL_PREFIX + spaceToUs(poolName) + FREE_SUFFIX;
     }
-    
+
     private static final String getPoolUsedSizeColName(String poolName) {
-        return "mempool_"+spaceToUs(poolName)+"_used";
+        return MEMPOOL_PREFIX + spaceToUs(poolName) + USED_SUFFIX;
     }
+
     private static final String spaceToUs(String s) {
         return s.replace(' ', '_');
     }
-    
+
     private MemoryStats() {
         ZimbraPerf.addStatsCallback(this);
-        
+
         // gcollector_NAME_count,  gcollector_NAME_time
         for (String gc : getGarbageCollectorNames()) {
             gc = gc.toLowerCase();
             ZimbraPerf.addRealtimeStatName(getGCCountColName(gc));
             ZimbraPerf.addRealtimeStatName(getGCTimeColName(gc));
         }
-        
+        ZimbraPerf.addRealtimeStatName(GC_MINOR_COUNT);
+        ZimbraPerf.addRealtimeStatName(GC_MINOR_TIME);
+        ZimbraPerf.addRealtimeStatName(GC_MAJOR_COUNT);
+        ZimbraPerf.addRealtimeStatName(GC_MAJOR_TIME);
+
         // mempool_NAME_used, mempool_NAME_free
         for (String pool : getHeapPoolNames()) {
             pool = pool.toLowerCase();
@@ -210,8 +255,8 @@ public final class MemoryStats implements RealtimeStatsCallback {
         }
         
         // heap_used, heap_free
-        ZimbraPerf.addRealtimeStatName("heap_used");
-        ZimbraPerf.addRealtimeStatName("heap_free");
+        ZimbraPerf.addRealtimeStatName(HEAP_USED);
+        ZimbraPerf.addRealtimeStatName(HEAP_FREE);
     }
     
     /* (non-Javadoc)
@@ -221,14 +266,28 @@ public final class MemoryStats implements RealtimeStatsCallback {
         Map<String, Object> toRet = new HashMap<String, Object>();
 
         // GC times
+        long minorCount = 0, minorTime = 0, majorCount = 0, majorTime = 0;
         List<GarbageCollectorMXBean> gcs = ManagementFactory.getGarbageCollectorMXBeans();
         for (GarbageCollectorMXBean gc : gcs) {
-            String gcName = gc.getName().toLowerCase();
-            toRet.put(getGCCountColName(gcName), gc.getCollectionCount());
-            toRet.put(getGCTimeColName(gcName), gc.getCollectionTime());
+            String gcName = gc.getName();
+            long count = gc.getCollectionCount();
+            long time = gc.getCollectionTime();
+            String gcNameLower = gcName.toLowerCase();
+            toRet.put(getGCCountColName(gcNameLower), count);
+            toRet.put(getGCTimeColName(gcNameLower), time);
+            if (sMajorCollectors.contains(gcName)) {
+                majorCount += count;
+                majorTime  += time;
+            } else {
+                minorCount += count;
+                minorTime  += time;
+            }
         }
-        
-        
+        toRet.put(GC_MINOR_COUNT, minorCount);
+        toRet.put(GC_MINOR_TIME,  minorTime);
+        toRet.put(GC_MAJOR_COUNT, majorCount);
+        toRet.put(GC_MAJOR_TIME,  majorTime);
+
         long heapTotal = 0;
         long heapUsed = 0;
         
@@ -241,56 +300,24 @@ public final class MemoryStats implements RealtimeStatsCallback {
 //            }
             String poolName = pool.getName().toLowerCase();
             MemoryUsage usage = pool.getUsage();
-            heapTotal += usage.getMax();
-            heapUsed += usage.getUsed();
-            long curpoolFree = usage.getMax() - usage.getUsed();
-            toRet.put(getPoolUsedSizeColName(poolName), usage.getUsed());
+            long committed = usage.getCommitted();
+            long used = usage.getUsed();
+            if (pool.getType() == MemoryType.HEAP) {
+                heapTotal += committed;
+                heapUsed += used;
+            }
+            long curpoolFree = committed - used;
+            toRet.put(getPoolUsedSizeColName(poolName), used);
             toRet.put(getPoolFreeSizeColName(poolName), curpoolFree);
         }
         
         // heap stats
-        toRet.put("heap_used", heapUsed);
-        toRet.put("heap_free", heapTotal-heapUsed);
+        toRet.put(HEAP_USED, heapUsed);
+        toRet.put(HEAP_FREE, heapTotal - heapUsed);
         
         return toRet;
     }
-    
-    
-//    /**
-//     * @return Summary statistics about the "Major" GCs
-//     * 
-//     */
-//    public static Pair<Long /*num GCs*/, Long /*time elapsed*/> getMajorGCInfo() {
-//        List<GarbageCollectorMXBean> gcs = ManagementFactory.getGarbageCollectorMXBeans();
-//        long num = 0;
-//        long time = 0;
-//        
-//        for (GarbageCollectorMXBean gc : gcs) {
-//            if (!"Copy".equals(gc.getName())) {
-//                num+=gc.getCollectionCount(); 
-//                time+=gc.getCollectionTime();
-//            }
-//        }
-//        return new Pair<Long, Long>(num, time);
-//    }
-//    
-//    /**
-//     * @return
-//     */
-//    public static Pair<Long /*num GCs*/, Long /*time elapsed*/> getMinorGCInfo() {
-//        List<GarbageCollectorMXBean> gcs = ManagementFactory.getGarbageCollectorMXBeans();
-//        long num = 0;
-//        long time = 0;
-//        
-//        for (GarbageCollectorMXBean gc : gcs) {
-//            if ("Copy".equals(gc.getName())) {
-//                num+=gc.getCollectionCount(); 
-//                time+=gc.getCollectionTime();
-//            }
-//        }
-//        return new Pair<Long, Long>(num, time);
-//    }
-    
+
     private void doShutdown() { }
-    
+
 }
