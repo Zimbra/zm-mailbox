614dd21c9e13e807f7d934b5ae439ca6193959df
Unknown Perforce User <pshao>
2007-06-05 13:51:59 -0500

    bug: 16820
    move VoliceConstants.java to ZimbraCommon com.zimbra.common.soap so it can be accessed from zclient for the basic client.  Will fix build error in next checkin.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=16820
    
    Copied from Perforce
     Change: 54827

---
+++ src/java/com/zimbra/common/soap/VoiceConstants.java
@@ -0,0 +1,121 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 ("License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.zimbra.com/license
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * The Original Code is: Zimbra Collaboration Suite Server.
+ * 
+ * The Initial Developer of the Original Code is Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2004, 2005, 2006 Zimbra, Inc.
+ * All Rights Reserved.
+ * 
+ * Contributor(s): 
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.cs.service.voice;
+
+import org.dom4j.QName;
+import org.dom4j.Namespace;
+
+public class VoiceConstants {
+    
+    public static final String NAMESPACE_STR = "urn:zimbraVoice";
+    public static final Namespace NAMESPACE = Namespace.get(NAMESPACE_STR);
+    
+    // tester for voice soap request conventions
+    public static final QName VOICE_SERVICE_TESTER_REQUEST  = QName.get("VoiceServiceTesterRequest", NAMESPACE);
+    public static final QName VOICE_SERVICE_TESTER_RESPONSE = QName.get("VoiceServiceTesterResponse", NAMESPACE);
+
+    public static final QName GET_VOICE_MAIL_PREFS_REQUEST  = QName.get("GetVoiceMailPrefsRequest", NAMESPACE);
+    public static final QName GET_VOICE_MAIL_PREFS_RESPONSE = QName.get("GetVoiceMailPrefsResponse", NAMESPACE);
+
+    public static final QName MODIFY_VOICE_MAIL_PREFS_REQUEST  = QName.get("ModifyVoiceMailPrefsRequest", NAMESPACE);
+    public static final QName MODIFY_VOICE_MAIL_PREFS_RESPONSE = QName.get("ModifyVoiceMailPrefsResponse", NAMESPACE);
+    
+    public static final QName GET_VOICE_INFO_REQUEST  = QName.get("GetVoiceInfoRequest", NAMESPACE);
+    public static final QName GET_VOICE_INFO_RESPONSE = QName.get("GetVoiceInfoResponse", NAMESPACE);
+
+    public static final QName GET_VOICE_FOLDER_REQUEST  = QName.get("GetVoiceFolderRequest", NAMESPACE);
+    public static final QName GET_VOICE_FOLDER_RESPONSE = QName.get("GetVoiceFolderResponse", NAMESPACE);
+    
+    public static final QName SEARCH_VOICE_REQUEST = QName.get("SearchVoiceRequest", NAMESPACE);
+    public static final QName SEARCH_VOICE_RESPONSE = QName.get("SearchVoiceResponse", NAMESPACE);
+
+    public static final QName GET_VOICE_MSG_REQUEST  = QName.get("GetVoiceMsgRequest", NAMESPACE);
+    public static final QName GET_VOICE_MSG_RESPONSE = QName.get("GetVoiceMsgResponse", NAMESPACE);
+
+    public static final QName VOICE_MSG_ACTION_REQUEST  = QName.get("VoiceMsgActionRequest", NAMESPACE);
+    public static final QName VOICE_MSG_ACTION_RESPONSE = QName.get("VoiceMsgActionResponse", NAMESPACE);
+
+    public static final QName GET_VOICE_FEATURES_REQUEST  = QName.get("GetVoiceFeaturesRequest", NAMESPACE);
+    public static final QName GET_VOICE_FEATURES_RESPONSE = QName.get("GetVoiceFeaturesResponse", NAMESPACE);
+
+    public static final QName MODIFY_VOICE_FEATURES_REQUEST  = QName.get("ModifyVoiceFeaturesRequest", NAMESPACE);
+    public static final QName MODIFY_VOICE_FEATURES_RESPONSE = QName.get("ModifyVoiceFeaturesResponse", NAMESPACE);
+
+    public static final QName RESET_VOICE_FEATURES_REQUEST  = QName.get("ResetVoiceFeaturesRequest", NAMESPACE);
+    public static final QName RESET_VOICE_FEATURES_RESPONSE = QName.get("ResetVoiceFeaturesResponse", NAMESPACE);
+
+    public static final QName UPLOAD_VOICE_MAIL_REQUEST  = QName.get("UploadVoiceMailRequest", NAMESPACE);
+    public static final QName UPLOAD_VOICE_MAIL_RESPONSE = QName.get("UploadVoiceMailResponse", NAMESPACE);
+
+    
+    public static final String E_PHONE         = "phone";
+    public static final String E_CALLLOG       = "cl";
+    public static final String E_CALLPARTY     = "cp";
+    public static final String E_VOICEMSG      = "vm";
+    public static final String E_UPLOAD        = "upload";
+ 
+    public static final String A_NAME          = "name";
+    public static final String A_VMSG_DURATION = "du";
+    public static final String A_PHONE         = "phone";
+    public static final String A_PHONENUM      = "n";
+    public static final String A_CITY          = "ci";
+    public static final String A_STATE         = "st";
+    public static final String A_COUNTRY       = "co";
+    
+    public static final String FLAG_HI_PRIORITY   = "h";  // 'h'igh priority
+    public static final String FLAG_UNFORWARDABLE = "p";  // 'p'rivate
+    
+    //
+    // call feature elements
+    //
+    // public static final String E_CALL_FEATURE    = "callfeature";
+    public static final String E_CALL_FEATURES   = "callfeatures";
+    public static final String E_CALL_FEATURE    = "callfeature";
+    
+    public static final String E_ANON_CALL_REJECTION        = "anoncallrejection";
+    public static final String E_CALLER_ID_BLOCKING         = "calleridblocking";
+    public static final String E_CALL_FORWARD               = "callforward";
+    // public static final String E_CALL_FORWARD_COMBINATION   = "callforwardcombination"; // Deprecated in jr-vnes v1.6
+    public static final String E_CALL_FORWARD_BUSY_LINE     = "callforwardbusyline";
+    public static final String E_CALL_FORWARD_NO_ANSWER     = "callforwardnoanswer";
+    public static final String E_CALL_WAITING               = "callwaiting";
+    public static final String E_DO_NOT_DISTURB             = "donotdisturb";
+    public static final String E_SELECTIVE_CALL_FORWARD     = "selectivecallforward";
+    public static final String E_VOICE_MAIL_PREFS           = "voicemailprefs";
+    
+    public static final String A_ACTIVE          = "a";
+    public static final String A_FORWARD_TO      = "ft";
+    public static final String A_NUM_RING_CYCLES = "nr";
+    public static final String A_PHONE_NUMBER    = "pn";
+    public static final String A_SUBSCRIBED      = "s";
+    
+    public static final String C_UNKNOWN         = "unknown";  // for cases gateway do not return us a value it should,
+    // we output unknown to our client instead of throwing 
+    // an exception
+
+
+
+ }
