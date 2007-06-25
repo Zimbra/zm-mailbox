718081578e722c756848cd6ade9a5444ad991c9e
Unknown Perforce User <jylee>
2007-06-25 16:51:26 -0500

    (split)bug 17875 extended SoapTransport.invoke() to accept optional parameters change id and type.
    http://bugzilla.zimbra.com/show_bug.cgi?id=17875
    
    Copied from Perforce
     Change: 57077

--- src/java/com/zimbra/qa/unittest/TestContacts.java
+++ src/java/com/zimbra/qa/unittest/TestContacts.java
@@ -24,12 +24,21 @@
  */
 package com.zimbra.qa.unittest;
 
+import com.zimbra.common.localconfig.LC;
+import com.zimbra.common.soap.Element;
+import com.zimbra.common.soap.MailConstants;
+import com.zimbra.common.soap.SoapHttpTransport;
+import com.zimbra.common.util.CliUtil;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.account.Server;
 import com.zimbra.cs.account.Provisioning.AccountBy;
+import com.zimbra.cs.account.soap.SoapProvisioning;
+import com.zimbra.cs.account.soap.SoapProvisioning.DelegateAuthResponse;
 import com.zimbra.cs.db.DbMailItem;
 import com.zimbra.cs.db.DbResults;
 import com.zimbra.cs.db.DbUtil;
+import com.zimbra.cs.httpclient.URLUtil;
 import com.zimbra.cs.mailbox.MailItem;
 import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.cs.mailbox.MailboxManager;
@@ -52,5 +61,38 @@ extends TestCase {
         DbResults results = DbUtil.executeQuery(sql);
         int count = results.getInt(1);
         assertEquals("Found non-null volumeId values for contacts", 0, count);
+    }
+    
+    public static void main(String[] args) throws Exception {
+    	String username = "user1";
+    	String id = args[0];
+    	String name = args[1];
+    	String value = args[2];
+    	String rev = args[3];
+		SoapProvisioning prov = new SoapProvisioning();            
+		CliUtil.toolSetup();
+		String server = LC.zimbra_zmprov_default_soap_server.value();
+		prov.soapSetURI(URLUtil.getAdminURL(server));
+		prov.soapAdminAuthenticate(LC.zimbra_ldap_user.value(), LC.zimbra_ldap_password.value());
+		DelegateAuthResponse dar = prov.delegateAuth(AccountBy.name, username, 60*60*24);
+		Account acct = prov.get(AccountBy.name, username);
+		Server remoteServer = prov.getServer(acct);
+		String url = URLUtil.getSoapURL(remoteServer, true);
+		SoapHttpTransport transport = new SoapHttpTransport(url);
+		transport.setAuthToken(dar.getAuthToken());
+		transport.setTargetAcctName(username);
+		try {
+			Element req = new Element.XMLElement(MailConstants.CONTACT_ACTION_REQUEST);
+			Element action = req.addElement(MailConstants.E_ACTION);
+			action.addAttribute(MailConstants.A_OPERATION, "update");
+			action.addAttribute(MailConstants.A_ID, id);
+			Element a = action.addElement(MailConstants.E_A);
+			a.addAttribute(MailConstants.A_ATTRIBUTE_NAME, name);
+			a.addText(value);
+			transport.invoke(req, false, true, true, null, rev, null);
+		} finally {
+			if (transport != null)
+				transport.shutdown();
+		}
     }
 }
