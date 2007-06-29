7494c673954eb01e37af182fc96eb23f5544e419
Unknown Perforce User <dkarp>
2007-06-29 15:22:59 -0500

    bug: 14812, 10704
    First pass at support for "last SOAP activity time" and "total message count since last login".  These are returned as the <accessed> and <recent> values in the GetInfoResponse, and contain session-relative data if the caller has an active session on the mailbox and mailbox-relative data if the user has no such session.
    
    The "last SOAP activity time" is the time in milliseconds of the last write operation on the mailbox by the mailbox's owner; this is maintained in the session and written at the time of (a) the session's first write and (b) the session's expiry.  The "total message count since last login" does not include drafts and save-to-sent messages but does include messages received during the duration of the current session.  (I fully expect these definititions to change on us several times before 5.0 ships.)
    
    Two new columns (LAST_SOAP_ACCESS and NEW_MESSAGES) have been added to ZIMBRA.MAILBOX in order to keep track of this.  The database version is now 42; please reset-the-world or run the migrate20070630-LastSoapAccess.pl migration script to bring your install up to date.
    
    Moved ZimbraSoapContext.getOperationContext() to a static method on DocumentHandler in order to further disentangle ZimbraSoapContext from the ZimbraServer code.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=14812
    http://bugzilla.zimbra.com/show_bug.cgi?id=10704
    
    Copied from Perforce
     Change: 57694

--- src/java/com/zimbra/common/soap/AccountConstants.java
+++ src/java/com/zimbra/common/soap/AccountConstants.java
@@ -103,6 +103,8 @@ public class AccountConstants {
     public static final String E_ATTR = "attr";
     public static final String E_ATTRS = "attrs";
     public static final String E_QUOTA_USED = "used";
+    public static final String E_LAST_ACCESS = "accessed";
+    public static final String E_RECENT_MSGS = "recent";
     public static final String E_ZIMLET = "zimlet";
     public static final String E_ZIMLETS = "zimlets";
     public static final String E_ZIMLET_CONTEXT = "zimletContext";
