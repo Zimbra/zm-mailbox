744b8fc77e1ef62c0463cd4fd47f35c26d10530e
Unknown Perforce User <jylee>
2007-07-03 02:21:03 -0500

    (split)bug 18083 skip URL normalization only when the wiki URL already starts with http://. otherwise normalize the URL based on the folder's REST URL.
    http://bugzilla.zimbra.com/show_bug.cgi?id=18083
    
    Copied from Perforce
     Change: 58098

--- src/java/com/zimbra/cs/wiki/Wiki.java
+++ src/java/com/zimbra/cs/wiki/Wiki.java
@@ -174,7 +174,7 @@ public abstract class Wiki {
 			return Wiki.getInstance(ctxt, ownerAccount.getId(), getFolderPath(ctxt, referenceAccount));
 		}
 		public String getFullUrl(WikiContext ctxt, String referenceAccount) throws ServiceException {
-			if (mUrl != null)
+			if (mUrl != null && mUrl.startsWith("http://"))
 				return mUrl;
 			Account ownerAccount = getOwnerAccount(referenceAccount);
 			return UserServlet.getRestUrl(ownerAccount)
