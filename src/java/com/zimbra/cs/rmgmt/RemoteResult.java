ea1aff94ca88f7fe899cc9a5ee538789b02295bd
Unknown Perforce User <ccao>
2007-07-31 19:48:16 -0500

    (split)bug: 18479 1) New zmssl.cnf.new config file . It should replace the zmssl.cnf.in 2) New Script file to manage the certificates: zmcertmgr.sh. It should obsolete zmcreateca, zmcreatecert and zmcertinstall . 3) Java extension zimbra_cert_manager is developed to support multiple soap admin calls from the admin console and then invoke the shell commands 4) zmrcd is modified to allow the zmcertmgr to run. Might need to use sudo . 5) The admin extension zimlets is developed to allow user create self-signed and commercially-signed certificates through AC
    http://bugzilla.zimbra.com/show_bug.cgi?id=18479
    
    Copied from Perforce
     Change: 61407

--- src/java/com/zimbra/cs/rmgmt/RemoteResult.java
+++ src/java/com/zimbra/cs/rmgmt/RemoteResult.java
@@ -30,4 +30,20 @@ public class RemoteResult {
     byte[] mStderr;
     int mExitStatus;
     String mExitSignal;
+    
+    public String getMExitSignal() {
+        return mExitSignal;
+    }
+    
+    public int getMExitStatus() {
+        return mExitStatus;
+    }
+    
+    public byte[] getMStderr() {
+        return mStderr;
+    }
+    
+    public byte[] getMStdout() {
+        return mStdout;
+    }    
 }
