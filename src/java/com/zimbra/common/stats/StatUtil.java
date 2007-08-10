94eb8896768beebec9f0118b5efd367b211c45a7
Unknown Perforce User <bburtin>
2007-08-10 16:17:11 -0500

    bug: 19165 - Collect more detailed user activity stats
    Initial checkin for user activity tracking.  We're now timing the
    count and average execution time of each unique SOAP, POP3
    and IMAP request/command.  Results are stored once a minute
    in /opt/zimbra/log/soap.csv, pop.csv and imap.csv.  A subsequent
    checkin will add more details in cases where the name of the
    command by itself does not provide enough context about what
    the user is doing.
    
    * Added the ActivityTracker class for tracking requests, counts
    and average execution times.
    * Added ActivityTracker callsites to SoapEngine, Pop3Handler
    and ImapHandler.
    * Updated Pop3Handler to store the command name and start
    time in instance variables for access by the timing code.
    * Added StatUtil to ZimbraCommon.  As time goes on we can
    move general-purpose code out of ZimbraPerf and put it there.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=19165
    
    Copied from Perforce
     Change: 62933

---
+++ src/java/com/zimbra/common/stats/StatUtil.java
@@ -0,0 +1,38 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 ("License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.zimbra.com/license
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * The Original Code is: Zimbra Collaboration Suite Server.
+ * 
+ * The Initial Developer of the Original Code is Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2007 Zimbra, Inc.
+ * All Rights Reserved.
+ * 
+ * Contributor(s): 
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.common.stats;
+
+import java.util.Date;
+
+
+public class StatUtil {
+
+    /**
+     * Returns the current time formatted as <tt>MM/dd/yyyy hh:mm:ss</tt>.
+     */
+    public static String getTimestampString() {
+        return String.format("%1$tm/%1$td/%1$tY %1$tH:%1$tM:%1$tS", new Date());
+    }
+}
