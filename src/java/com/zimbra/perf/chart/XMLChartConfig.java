7e645cd5a80a655f8427a8c361cd86bffb4b664a
Unknown Perforce User <jhahm>
2007-08-20 19:55:02 -0500

    bug: 12095
    Integrating from main to FRANK
    
    Change 53402 by jhahm@jhahm-mac on 2007/05/21 20:25:59 *pending*
    
    	bug: 12095
    
    	Moved chart generation code into ZimbraCommon.  Much code cleanup.
    
    	/opt/zimbra/bin/zmstat-chart-config script is provided to generate 
    	chart configuration for the host.  All configuration data is 
    	embedded in the script, so no external xml file needs to be 
    	provided.  The generated .xml file can be edited to suit your needs.  
    	The generated file should be comprehensive enough for most installs.  
    	(It includes detailed GC and heap info.)
    
    	/opt/zimbra/bin/zmstat-chart script does chart generation.
    
    Affected files ...
    
    ... //depot/main/ZimbraCommon/src/java/com/zimbra/perf/chart/ChartFromCsv.java#2 delete
    ... //depot/main/ZimbraCommon/src/java/com/zimbra/perf/chart/ChartSettings.java#2 edit
    ... //depot/main/ZimbraCommon/src/java/com/zimbra/perf/chart/ChartUtil.java#1 add
    ... //depot/main/ZimbraCommon/src/java/com/zimbra/perf/chart/PlotSettings.java#2 edit
    ... //depot/main/ZimbraCommon/src/java/com/zimbra/perf/chart/SummaryAnalyzer.java#1 add
    ... //depot/main/ZimbraCommon/src/java/com/zimbra/perf/chart/SummaryConstants.java#1 add
    ... //depot/main/ZimbraCommon/src/java/com/zimbra/perf/chart/TestResultAnalyzer.java#2 delete
    ... //depot/main/ZimbraCommon/src/java/com/zimbra/perf/chart/TestResultConstants.java#2 delete
    ... //depot/main/ZimbraCommon/src/java/com/zimbra/perf/chart/XMLChartConfig.java#2 edit
    ... //depot/main/ZimbraPerf/testbin/creatchartconfig.pl#1 delete
    ... //depot/main/ZimbraPerf/testbin/zmchart.pl#1 delete
    ... //depot/main/ZimbraServer/src/bin/zmstat-chart#1 add
    ... //depot/main/ZimbraServer/src/bin/zmstat-chart-config#1 add
    ... //depot/main/ZimbraServer/src/java/com/zimbra/cs/util/MemoryStats.java#2 edit
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=12095
    
    Copied from Perforce
     Change: 63890

--- src/java/com/zimbra/perf/chart/XMLChartConfig.java
+++ src/java/com/zimbra/perf/chart/XMLChartConfig.java
@@ -25,6 +25,7 @@
 
 package com.zimbra.perf.chart;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -37,203 +38,213 @@ import org.dom4j.io.SAXReader;
 
 public class XMLChartConfig {
 
-	// elements
-	public static final String E_CHARTS = "charts";
+    // elements
+    public static final String E_CHARTS = "charts";
+    public static final String E_CHART = "chart";
+    public static final String E_PLOT = "plot";
 
-	public static final String E_CHART = "chart";
+    // <chart> attributes
+    public static final String A_CHART_TITLE = "title";
+    public static final String A_CHART_CATEGORY = "category";
+    public static final String A_CHART_OUTFILE = "outfile";
+    public static final String A_CHART_XAXIS = "xAxis";
+    public static final String A_CHART_YAXIS = "yAxis";
+    public static final String A_CHART_ALLOW_LOG_SCALE = "allowLogScale";
+    public static final String A_CHART_PLOT_ZERO = "plotZero";
+    public static final String A_CHART_WIDTH = "width";
+    public static final String A_CHART_HEIGHT = "height";
 
-	public static final String E_PLOT = "plot";
+    // <plot> attributes
+    public static final String A_PLOT_LEGEND = "legend";
+    public static final String A_PLOT_INFILE = "infile";
+    public static final String A_PLOT_DATA_COLUMN = "data";
+    public static final String A_PLOT_SHOW_RAW = "showRaw";
+    public static final String A_PLOT_SHOW_MOVING_AVG = "showMovingAvg";
+    public static final String A_PLOT_MOVING_AVG_POINTS = "movingAvgPoints";
+    public static final String A_PLOT_MULTIPLIER = "multiplier";
+    public static final String A_PLOT_DIVISOR = "divisor";
+    public static final String A_PLOT_NON_NEGATIVE = "nonNegative";
+    public static final String A_PLOT_PERCENT_TIME = "percentTime";
+    public static final String A_PLOT_DATA_FUNCTION = "dataFunction";
+    public static final String A_PLOT_AGGREGATE_FUNCTION = "aggregateFunction";
+    public static final String A_PLOT_OPTIONAL = "optional";
 
-	// <chart> attributes
-	public static final String A_CHART_TITLE = "title";
+    private static String getAttr(Element elem, String name)
+            throws DocumentException {
+        String val = elem.attributeValue(name, null);
+        if (val != null)
+            return val;
+        else
+            throw new DocumentException("Missing required attribute " + name
+                    + " in element " + elem.getName());
+    }
 
-	public static final String A_CHART_OUTFILE = "outfile";
+    private static String getAttr(Element elem, String name, String defaultValue) {
+        String val = elem.attributeValue(name, null);
+        if (val != null)
+            return val;
+        else
+            return defaultValue;
+    }
 
-	public static final String A_CHART_XAXIS = "xAxis";
+    private static String getInheritedAttr(Element elem, String name,
+            String defaultValue) {
+        String val = null;
+        for (Element e = elem; val == null && e != null; e = e.getParent()) {
+            val = e.attributeValue(name, null);
+        }
+        return val != null ? val : defaultValue;
+    }
 
-	public static final String A_CHART_YAXIS = "yAxis";
+    private static Boolean getInheritedAttrBoolean(Element elem, String name,
+            boolean defaultValue) {
+        String val = getInheritedAttr(elem, name, null);
+        if (val != null) {
+            val = val.toLowerCase();
+            return val.equalsIgnoreCase("true") || val.equals("1");
+        } else
+            return defaultValue;
+    }
 
-	public static final String A_CHART_ALLOW_LOG_SCALE = "allowLogScale";
+    private static int getInheritedAttrInt(Element elem, String name,
+            int defaultValue) throws DocumentException {
+        String val = getInheritedAttr(elem, name, null);
+        if (val != null) {
+            try {
+                int i = Integer.parseInt(val);
+                return i;
+            } catch (NumberFormatException ex) {
+                throw new DocumentException("Invalid integer value " + val
+                        + " for attribute " + name + " in element "
+                        + elem.getName());
+            }
+        } else
+            return defaultValue;
+    }
 
-	public static final String A_CHART_PLOT_ZERO = "plotZero";
+    // allows 'k', 'm', 'g' suffix for kilo, mega and giga
+    private static double getInheritedAttrDouble(Element elem, String name,
+            double defaultValue) throws DocumentException {
+        String val = getInheritedAttr(elem, name, null);
+        if (val != null) {
+            int len = val.length();
+            if (len == 0) return defaultValue;
+            char unit = val.toLowerCase().charAt(len - 1);
+            int multiplier = 1;
+            if (unit == 'k')
+                multiplier = 1024;
+            else if (unit == 'm')
+                multiplier = 1024 * 1024;
+            else if (unit == 'g')
+                multiplier = 1024 * 1024 * 1024;
+            String digits;
+            if (multiplier == 1)
+                digits = val;
+            else
+                digits = val.substring(0, len - 1);
+            try {
+                double d = Double.parseDouble(digits);
+                return d * multiplier;
+            } catch (NumberFormatException ex) {
+                throw new DocumentException("Invalid double value " + val
+                        + " for attribute " + name + " in element "
+                        + elem.getName());
+            }
+        } else
+            return defaultValue;
+    }
 
-	public static final String A_CHART_WIDTH = "width";
+    /**
+     * Loads chart settings from the specified XML file.
+     * 
+     * @throws IOException
+     *             if there was an error reading the file
+     * @throws DocumentException
+     *             if there was an error parsing the file
+     * @throws IllegalArgumentException
+     *             if any attribute has invalid value
+     */
+    public static List<ChartSettings> load(File xmlFile)
+            throws IOException, DocumentException {
+        List<ChartSettings> charts = new ArrayList<ChartSettings>();
+        SAXReader reader = new SAXReader();
+        Document document = reader.read(xmlFile);
+        Element chartsElem = document.getRootElement();
+        if (!chartsElem.getName().equals(E_CHARTS))
+            throw new DocumentException("Missing <" + E_CHARTS
+                    + "> root element");
+        for (Iterator iter = chartsElem.elementIterator(E_CHART); iter
+                .hasNext();) {
+            Element chartElem = (Element) iter.next();
+            String chartTitle = getAttr(chartElem, A_CHART_TITLE);
+            String category = getAttr(chartElem, A_CHART_CATEGORY, "unknown");
+            String outfile = getAttr(chartElem, A_CHART_OUTFILE);
 
-	public static final String A_CHART_HEIGHT = "height";
+            // inheritable attributes
+            String xAxis = getInheritedAttr(chartElem, A_CHART_XAXIS,
+                    ChartSettings.DEFAULT_CHART_XAXIS);
+            String yAxis = getInheritedAttr(chartElem, A_CHART_YAXIS, "");
+            boolean allowLogScale = getInheritedAttrBoolean(chartElem,
+                    A_CHART_ALLOW_LOG_SCALE,
+                    ChartSettings.DEFAULT_CHART_ALLOW_LOG_SCALE);
+            boolean plotZero = getInheritedAttrBoolean(chartElem,
+                    A_CHART_PLOT_ZERO, ChartSettings.DEFAULT_CHART_PLOT_ZERO);
+            if (!allowLogScale)
+                plotZero = true;
+            int width = getInheritedAttrInt(chartElem, A_CHART_WIDTH,
+                    ChartSettings.DEFAULT_CHART_WIDTH);
+            int height = getInheritedAttrInt(chartElem, A_CHART_HEIGHT,
+                    ChartSettings.DEFAULT_CHART_HEIGHT);
 
-	// <plot> attributes
-	public static final String A_PLOT_LEGEND = "legend";
+            ChartSettings chart = new ChartSettings(chartTitle, category, outfile, xAxis,
+                    yAxis, allowLogScale, plotZero, width, height);
 
-	public static final String A_PLOT_INFILE = "infile";
+            for (Iterator plotIter = chartElem.elementIterator(E_PLOT); plotIter
+                    .hasNext();) {
+                Element plotElem = (Element) plotIter.next();
+                String dataCol = getAttr(plotElem, A_PLOT_DATA_COLUMN);
 
-	public static final String A_PLOT_TSTAMP_COLUMN = "tstamp";
+                // inheritable attributes
+                String legend = getInheritedAttr(plotElem, A_PLOT_LEGEND, null);
+                String infile = getInheritedAttr(plotElem, A_PLOT_INFILE, null);
 
-	public static final String A_PLOT_DATA_COLUMN = "data";
+                boolean showRaw = getInheritedAttrBoolean(plotElem,
+                        A_PLOT_SHOW_RAW, PlotSettings.DEFAULT_PLOT_SHOW_RAW);
+                boolean showMovingAvg = getInheritedAttrBoolean(plotElem,
+                        A_PLOT_SHOW_MOVING_AVG,
+                        PlotSettings.DEFAULT_PLOT_SHOW_MOVING_AVG);
+                int movingAvgPoints = getInheritedAttrInt(plotElem,
+                        A_PLOT_MOVING_AVG_POINTS,
+                        PlotSettings.DEFAULT_PLOT_MOVING_AVG_POINTS);
+                double multiplier = getInheritedAttrDouble(plotElem,
+                        A_PLOT_MULTIPLIER, PlotSettings.DEFAULT_PLOT_MULTIPLIER);
+                double divisor = getInheritedAttrDouble(plotElem,
+                        A_PLOT_DIVISOR, PlotSettings.DEFAULT_PLOT_DIVISOR);
 
-	public static final String A_PLOT_SHOW_RAW = "showRaw";
+                boolean nonNegative = getInheritedAttrBoolean(plotElem,
+                        A_PLOT_NON_NEGATIVE,
+                        PlotSettings.DEFAULT_PLOT_NON_NEGATIVE);
+                boolean percentTime = getInheritedAttrBoolean(plotElem,
+                        A_PLOT_PERCENT_TIME,
+                        PlotSettings.DEFAULT_PLOT_PERCENT_TIME);
+                String dataFunction = getAttr(plotElem, A_PLOT_DATA_FUNCTION,
+                        PlotSettings.DEFAULT_PLOT_DATA_FUNCTION);
+                String aggFunction = getAttr(plotElem, A_PLOT_AGGREGATE_FUNCTION,
+                        PlotSettings.DEFAULT_PLOT_AGGREGATE_FUNCTION);
 
-	public static final String A_PLOT_SHOW_SUM = "showSum";
+                boolean optional = getInheritedAttrBoolean(plotElem,
+                        A_PLOT_OPTIONAL,
+                        PlotSettings.DEFAULT_PLOT_OPTIONAL);
 
-	public static final String A_PLOT_SHOW_MOVING_AVG = "showMovingAvg";
-
-	public static final String A_PLOT_MOVING_AVG_POINTS = "movingAvgPoints";
-
-	public static final String A_PLOT_MULTIPLIER = "multiplier";
-
-	public static final String A_PLOT_DIVISOR = "divisor";
-
-	public static final String A_PLOT_AGGREGATE_FUNCTION = "aggregateFunction";
-
-	private static String getAttr(Element elem, String name)
-			throws DocumentException {
-		String val = elem.attributeValue(name, null);
-		if (val != null)
-			return val;
-		else
-			throw new DocumentException("Missing required attribute " + name
-					+ " in element " + elem.getName());
-	}
-
-	private static String getAttr(Element elem, String name, String defaultValue) {
-		String val = elem.attributeValue(name, null);
-		if (val != null)
-			return val;
-		else
-			return defaultValue;
-	}
-
-	private static String getInheritedAttr(Element elem, String name,
-			String defaultValue) {
-		String val = null;
-		for (Element e = elem; val == null && e != null; e = e.getParent()) {
-			val = e.attributeValue(name, null);
-		}
-		return val != null ? val : defaultValue;
-	}
-
-	private static Boolean getInheritedAttrBoolean(Element elem, String name,
-			boolean defaultValue) {
-		String val = getInheritedAttr(elem, name, null);
-		if (val != null) {
-			val = val.toLowerCase();
-			return val.equalsIgnoreCase("true") || val.equals("1");
-		} else
-			return defaultValue;
-	}
-
-	private static int getInheritedAttrInt(Element elem, String name,
-			int defaultValue) throws DocumentException {
-		String val = getInheritedAttr(elem, name, null);
-		if (val != null) {
-			try {
-				int i = Integer.parseInt(val);
-				return i;
-			} catch (NumberFormatException ex) {
-				throw new DocumentException("Invalid integer value " + val
-						+ " for attribute " + name + " in element "
-						+ elem.getName());
-			}
-		} else
-			return defaultValue;
-	}
-
-	private static double getInheritedAttrDouble(Element elem, String name,
-			double defaultValue) throws DocumentException {
-		String val = getInheritedAttr(elem, name, null);
-		if (val != null) {
-			try {
-				double d = Double.parseDouble(val);
-				return d;
-			} catch (NumberFormatException ex) {
-				throw new DocumentException("Invalid double value " + val
-						+ " for attribute " + name + " in element "
-						+ elem.getName());
-			}
-		} else
-			return defaultValue;
-	}
-
-	/**
-	 * Loads chart settings from the specified XML file.
-	 * 
-	 * @throws IOException
-	 *             if there was an error reading the file
-	 * @throws DocumentException
-	 *             if there was an error parsing the file
-	 * @throws IllegalArgumentException
-	 *             if any attribute has invalid value
-	 */
-	public static List<ChartSettings> load(String xmlFilename)
-			throws IOException, DocumentException {
-		List<ChartSettings> charts = new ArrayList<ChartSettings>();
-		SAXReader reader = new SAXReader();
-		Document document = reader.read(xmlFilename);
-		Element chartsElem = document.getRootElement();
-		if (!chartsElem.getName().equals(E_CHARTS))
-			throw new DocumentException("Missing <" + E_CHARTS
-					+ "> root element");
-		for (Iterator iter = chartsElem.elementIterator(E_CHART); iter
-				.hasNext();) {
-			Element chartElem = (Element) iter.next();
-			String chartTitle = getAttr(chartElem, A_CHART_TITLE);
-			String outfile = getAttr(chartElem, A_CHART_OUTFILE);
-
-			// inheritable attributes
-			String xAxis = getInheritedAttr(chartElem, A_CHART_XAXIS,
-					ChartSettings.DEFAULT_CHART_XAXIS);
-			String yAxis = getInheritedAttr(chartElem, A_CHART_YAXIS, "");
-			boolean allowLogScale = getInheritedAttrBoolean(chartElem,
-					A_CHART_ALLOW_LOG_SCALE,
-					ChartSettings.DEFAULT_CHART_ALLOW_LOG_SCALE);
-			boolean plotZero = getInheritedAttrBoolean(chartElem,
-					A_CHART_PLOT_ZERO, ChartSettings.DEFAULT_CHART_PLOT_ZERO);
-			if (!allowLogScale)
-				plotZero = true;
-			int width = getInheritedAttrInt(chartElem, A_CHART_WIDTH,
-					ChartSettings.DEFAULT_CHART_WIDTH);
-			int height = getInheritedAttrInt(chartElem, A_CHART_HEIGHT,
-					ChartSettings.DEFAULT_CHART_HEIGHT);
-
-			ChartSettings chart = new ChartSettings(chartTitle, outfile, xAxis,
-					yAxis, allowLogScale, plotZero, width, height);
-
-			for (Iterator plotIter = chartElem.elementIterator(E_PLOT); plotIter
-					.hasNext();) {
-				Element plotElem = (Element) plotIter.next();
-				String dataCol = getAttr(plotElem, A_PLOT_DATA_COLUMN);
-
-				String function = getAttr(plotElem, A_PLOT_AGGREGATE_FUNCTION,
-						PlotSettings.DEFAULT_PLOT_AGGREGATE_FUNCTION);
-
-				// inheritable attributes
-				String legend = getInheritedAttr(plotElem, A_PLOT_LEGEND, null);
-				String infile = getInheritedAttr(plotElem, A_PLOT_INFILE, null);
-				String tstampCol = getInheritedAttr(plotElem,
-						A_PLOT_TSTAMP_COLUMN,
-						PlotSettings.DEFAULT_PLOT_TSTAMP_COLUMN);
-
-				boolean showRaw = getInheritedAttrBoolean(plotElem,
-						A_PLOT_SHOW_RAW, PlotSettings.DEFAULT_PLOT_SHOW_RAW);
-				boolean showSum = getInheritedAttrBoolean(plotElem,
-						A_PLOT_SHOW_SUM, PlotSettings.DEFAULT_PLOT_SHOW_SUM);
-				boolean showMovingAvg = getInheritedAttrBoolean(plotElem,
-						A_PLOT_SHOW_MOVING_AVG,
-						PlotSettings.DEFAULT_PLOT_SHOW_MOVING_AVG);
-				int movingAvgPoints = getInheritedAttrInt(plotElem,
-						A_PLOT_MOVING_AVG_POINTS,
-						PlotSettings.DEFAULT_PLOT_MOVING_AVG_POINTS);
-				double multiplier = getInheritedAttrDouble(plotElem,
-						A_PLOT_MULTIPLIER, PlotSettings.DEFAULT_PLOT_MULTIPLIER);
-				double divisor = getInheritedAttrDouble(plotElem,
-						A_PLOT_DIVISOR, PlotSettings.DEFAULT_PLOT_DIVISOR);
-
-				PlotSettings plot = new PlotSettings(legend, infile, tstampCol,
-						dataCol, showRaw, showSum, showMovingAvg,
-						movingAvgPoints, multiplier, divisor, function);
-				chart.addPlot(plot);
-			}
-			charts.add(chart);
-		}
-		return charts;
-	}
+                PlotSettings plot = new PlotSettings(
+                        legend, infile, dataCol, showRaw, showMovingAvg,
+                        movingAvgPoints, multiplier, divisor,
+                        nonNegative, percentTime,
+                        dataFunction, aggFunction, optional);
+                chart.addPlot(plot);
+            }
+            charts.add(chart);
+        }
+        return charts;
+    }
 }
