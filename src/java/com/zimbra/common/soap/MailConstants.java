b94fc5b611daf05fc65990597403a489368e2dc9
Unknown Perforce User <tim>
2007-08-20 19:56:57 -0500

    bug: 7074
    instant-notify requests (<NoOpRequest wait=1>) were being cancelled by blocking-NoOp requests sent by the Zimbra Desktop Client.  Add a parameter to the NoOpRequest so that the client can request 'browser mode' which whill enforce the only-allow-one-blocked-request rule which is necessary to avoid browser lockups with hanging-NoOp.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=7074
    
    Copied from Perforce
     Change: 63892

--- src/java/com/zimbra/common/soap/MailConstants.java
+++ src/java/com/zimbra/common/soap/MailConstants.java
@@ -484,6 +484,7 @@ public class MailConstants {
     public static final String A_WAIT = "wait";
     public static final String A_TIMEOUT = "timeout";
     public static final String A_WAIT_DISALLOWED= "waitDisallowed";
+    public static final String A_LIMIT_TO_ONE_BLOCKED = "limitToOneBlocked";
 
     public static final String A_CAL_RULE_FREQ = "freq";
     public static final String A_CAL_RULE_COUNT_NUM = "num";
