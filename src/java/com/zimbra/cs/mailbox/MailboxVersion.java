32837f26cdd4eb2e90e67b7abb2589ce058c4d16
Unknown Perforce User <anandp>
2007-08-22 12:35:44 -0500

    (split)bug: none copyright years
    Copied from Perforce
     Change: 64158

--- src/java/com/zimbra/cs/mailbox/MailboxVersion.java
+++ src/java/com/zimbra/cs/mailbox/MailboxVersion.java
@@ -15,7 +15,7 @@
  * The Original Code is: Zimbra Collaboration Suite Server.
  * 
  * The Initial Developer of the Original Code is Zimbra, Inc.
- * Portions created by Zimbra are Copyright (C) 2006 Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2006, 2007 Zimbra, Inc.
  * All Rights Reserved.
  * 
  * Contributor(s): 
