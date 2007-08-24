52968ceeef995ff1b396f90739171ea8c6a0498b
Unknown Perforce User <dac>
2007-08-24 02:00:17 -0500

    (split)bug: none Clean up indentation, IntelliJ warnings.
    Copied from Perforce
     Change: 64378

--- src/java/com/zimbra/cs/tcpserver/TcpServer.java
+++ src/java/com/zimbra/cs/tcpserver/TcpServer.java
@@ -23,19 +23,11 @@
  * ***** END LICENSE BLOCK *****
  */
 
-/*
- * Created on 2004. 10. 21.
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
 package com.zimbra.cs.tcpserver;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -48,63 +40,45 @@ import com.zimbra.cs.server.ServerConfig;
 import EDU.oswego.cs.dl.util.concurrent.BoundedLinkedQueue;
 import EDU.oswego.cs.dl.util.concurrent.PooledExecutor;
 
-/**
- * @author jhahm
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
 public abstract class TcpServer implements Runnable, Server {
-
-	protected abstract ProtocolHandler newProtocolHandler();
-
-	private Log mLog;
-
-	private PooledExecutor mPooledExecutor;
-
-	private String mName;
-	private InetAddress mBindAddress;
-	private int mListenPort;
-	private ServerSocket mServerSocket;
-
-	private boolean mShutdownRequested;
-        private boolean mSSLEnabled;
-
-        private ServerConfig mConfig;
+    private final Log mLog;
+    private final PooledExecutor mPooledExecutor;
+    private final String mName;
+    private final ServerSocket mServerSocket;
+    private final List<ProtocolHandler> mActiveHandlers;
+    private boolean mShutdownRequested;
+    private boolean mSSLEnabled;
+    private ServerConfig mConfig;
 
     public TcpServer(String name, ServerConfig config) throws ServiceException {
         this(name, config.getNumThreads(), config.getServerSocket());
         mConfig = config;
     }
-    
+
     public TcpServer(String name, int numThreads, ServerSocket serverSocket) {
-    	    this(name, numThreads, Thread.NORM_PRIORITY, serverSocket);
+        this(name, numThreads, Thread.NORM_PRIORITY, serverSocket);
     }
 
     public TcpServer(String name, int numThreads, int threadPriority, ServerSocket serverSocket) {
-		mName = name;
+        mName = name;
         mServerSocket = serverSocket;
-		mLog = LogFactory.getLog(TcpServer.class.getName() + "/" + serverSocket.getLocalPort()); 
-		mShutdownRequested = false;
+        mLog = LogFactory.getLog(TcpServer.class.getName() + "/" + serverSocket.getLocalPort());
 
-		if (numThreads <= 0) {
-			mLog.warn("number of handler threads " + numThreads + " invalid; will use 10 threads instead");
-			numThreads = 10;
-		}
+        if (numThreads <= 0) {
+            mLog.warn("number of handler threads " + numThreads + " invalid; will use 10 threads instead");
+            numThreads = 10;
+        }
 
-		mPooledExecutor = new PooledExecutor(new BoundedLinkedQueue(), numThreads);
-		mPooledExecutor.setMinimumPoolSize(numThreads);
-		mPooledExecutor.setThreadFactory(new TcpThreadFactory(mName, false, threadPriority));
-		mPooledExecutor.waitWhenBlocked();
+        mPooledExecutor = new PooledExecutor(new BoundedLinkedQueue(), numThreads);
+        mPooledExecutor.setMinimumPoolSize(numThreads);
+        mPooledExecutor.setThreadFactory(new TcpThreadFactory(mName, false, threadPriority));
+        mPooledExecutor.waitWhenBlocked();
 
-		mActiveHandlers = new LinkedList();
-        
-	}
-
-	// TODO a linked list is probably the wrong datastructure here
-	// TODO write tests with multiple concurrent client
-	// TODO write some tests for shutdown/startup
-	private List mActiveHandlers;
+        // TODO a linked list is probably the wrong datastructure here
+        // TODO write tests with multiple concurrent client
+        // TODO write some tests for shutdown/startup
+        mActiveHandlers = new LinkedList<ProtocolHandler>();
+    }
 
     public ServerConfig getConfig() {
         return mConfig;
@@ -117,7 +91,7 @@ public abstract class TcpServer implements Runnable, Server {
         }
         return -1;
     }
-    
+
     public void setSSLEnabled(boolean ssl) {
         mSSLEnabled = ssl;
     }
@@ -125,105 +99,106 @@ public abstract class TcpServer implements Runnable, Server {
     public boolean isSSLEnabled() {
         return mSSLEnabled;
     }
-    
-        public void addActiveHandler(ProtocolHandler handler) {
-		synchronized (mActiveHandlers) {
-			mActiveHandlers.add(handler);
-		}
-	}
 
-	public void removeActiveHandler(ProtocolHandler handler) {
-		synchronized (mActiveHandlers) {
-			mActiveHandlers.remove(handler);
-		}
-	}
-
-	protected int numActiveHandlers() {
-		synchronized (mActiveHandlers) {
-			return mActiveHandlers.size();
-		}
-	}
-	
-	private void shutdownActiveHandlers(boolean graceful) {
-		synchronized (mActiveHandlers) {
-			for (Iterator iter = mActiveHandlers.iterator(); iter.hasNext();) {
-				ProtocolHandler handler = (ProtocolHandler)iter.next();
-                if (graceful)
-                    handler.gracefulShutdown("graceful shutdown requested");
-                else
-    				handler.hardShutdown("hard shutdown requested");
-			}
-		}
-	}
-
-        public void start() {
-            Thread t = new Thread(this);
-            if (mName != null) t.setName(mName);
-            t.start();
+    public void addActiveHandler(ProtocolHandler handler) {
+        synchronized (mActiveHandlers) {
+            mActiveHandlers.add(handler);
         }
-    
-        public void shutdown(int forceShutdownAfterSeconds) {
+    }
 
-		mLog.info(mName + " initiating shutdown");
-		mShutdownRequested = true;
+    public void removeActiveHandler(ProtocolHandler handler) {
+        synchronized (mActiveHandlers) {
+            mActiveHandlers.remove(handler);
+        }
+    }
 
-		try {
-			mServerSocket.close();
-			Thread.yield();
-		} catch (IOException ioe) {
-			mLog.warn(mName + " error closing server socket", ioe);
-		}
+    protected int numActiveHandlers() {
+        synchronized (mActiveHandlers) {
+            return mActiveHandlers.size();
+        }
+    }
 
-		mPooledExecutor.shutdownAfterProcessingCurrentlyQueuedTasks();
+    private void shutdownActiveHandlers(boolean graceful) {
+        synchronized (mActiveHandlers) {
+            for (ProtocolHandler handler : mActiveHandlers) {
+                if (graceful) {
+                    handler.gracefulShutdown("graceful shutdown requested");
+                } else {
+                    handler.hardShutdown("hard shutdown requested");
+                }
+            }
+        }
+    }
+
+    public void start() {
+        Thread t = new Thread(this);
+        if (mName != null) t.setName(mName);
+        t.start();
+    }
+
+    public void shutdown(int forceShutdownAfterSeconds) {
+        mLog.info(mName + " initiating shutdown");
+        mShutdownRequested = true;
+
+        try {
+            mServerSocket.close();
+            Thread.yield();
+        } catch (IOException ioe) {
+            mLog.warn(mName + " error closing server socket", ioe);
+        }
+
+        mPooledExecutor.shutdownAfterProcessingCurrentlyQueuedTasks();
 
         shutdownActiveHandlers(true);
 
         if (numActiveHandlers() == 0) {
-			mLog.info(mName + " shutting down idle thread pool");
-			mPooledExecutor.shutdownNow();
-			return;
-		}
+            mLog.info(mName + " shutting down idle thread pool");
+            mPooledExecutor.shutdownNow();
+            return;
+        }
 
-		mLog.info(mName + " waiting " + forceShutdownAfterSeconds + " seconds for thread pool shutdown");
-		try {
-			mPooledExecutor.awaitTerminationAfterShutdown(forceShutdownAfterSeconds * 1000);
-		} catch (InterruptedException ie) {
-			mLog.warn(mName + " interrupted while waiting for graceful shutdown", ie);
-		}
+        mLog.info(mName + " waiting " + forceShutdownAfterSeconds + " seconds for thread pool shutdown");
+        try {
+            mPooledExecutor.awaitTerminationAfterShutdown(forceShutdownAfterSeconds * 1000);
+        } catch (InterruptedException ie) {
+            mLog.warn(mName + " interrupted while waiting for graceful shutdown", ie);
+        }
 
-		if (numActiveHandlers() == 0) {
-			mLog.info(mName + " thread pool terminated");
-			return;
-		}
+        if (numActiveHandlers() == 0) {
+            mLog.info(mName + " thread pool terminated");
+            return;
+        }
 
-		shutdownActiveHandlers(false);
+        shutdownActiveHandlers(false);
 
-		mLog.info(mName + " shutdown complete");
-	}
+        mLog.info(mName + " shutdown complete");
+    }
 
-	public void run() {
-		Thread.currentThread().setName(mName);
+    public void run() {
+        Thread.currentThread().setName(mName);
 
-		mLog.info("starting accept loop");
-		try {
-			while (!mShutdownRequested) {
-				Socket connection = mServerSocket.accept();
-				ProtocolHandler handler = newProtocolHandler();
-				handler.setConnection(connection);
-				try {
-					mPooledExecutor.execute(handler);
-				} catch (InterruptedException ie) {
-					mLog.warn("handler thread pool execution request interrupted", ie);
-				}
-			}
-		} catch (IOException ioe) {
-			if (!mShutdownRequested) {
-				// TODO !mShutdownRequested check should be wrapped in a synchronized
-				// to guarantee we see the variable change right away.  This may cause
-				// bogus "accept loop failed" messages to be logged.
-				mLog.fatal("accept loop failed", ioe);
-			}
-		}
-		mLog.info("finished accept loop");
-	}
+        mLog.info("starting accept loop");
+        try {
+            while (!mShutdownRequested) {
+                Socket connection = mServerSocket.accept();
+                ProtocolHandler handler = newProtocolHandler();
+                handler.setConnection(connection);
+                try {
+                    mPooledExecutor.execute(handler);
+                } catch (InterruptedException ie) {
+                    mLog.warn("handler thread pool execution request interrupted", ie);
+                }
+            }
+        } catch (IOException ioe) {
+            if (!mShutdownRequested) {
+                // TODO !mShutdownRequested check should be wrapped in a synchronized
+                // to guarantee we see the variable change right away.  This may cause
+                // bogus "accept loop failed" messages to be logged.
+                mLog.fatal("accept loop failed", ioe);
+            }
+        }
+        mLog.info("finished accept loop");
+    }
+
+    protected abstract ProtocolHandler newProtocolHandler();
 }
