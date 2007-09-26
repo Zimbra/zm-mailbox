5c7cbf7495f7868c28593896b882148a7ab4cc15
Unknown Perforce User <jhahm>
2007-09-26 15:18:12 -0500

    bug: 15549
    When Apple iCal is subscribing to a calendar via REST interface, convert timezone names to Olson TZIDs as much as possible because iCal doesn't understand anything but Olson TZIDs.  This hack is done only for timezones for which the server has name mappings.  Not all custom timezones will be mapped.  Those that aren't mapped will still display at incorrect times in iCal, and we'll just have to wait for iCal to implement proper timezone handling.
    
    The name mapping hack is done based on User-Agent header in the REST request.
    
    Added localconfig key calendar_entourage_compatible_timezones.  This defaults true.  When enabled (current behavior), TZID values in various iCalendar properties are double-quoted.  Quoting is not necessary in the spec but is required to work around a bug in Microsoft Entourage.  This quoting issue doesn't affect Apple iCal.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=15549
    
    Copied from Perforce
     Change: 68595

--- src/java/com/zimbra/common/localconfig/LC.java
+++ src/java/com/zimbra/common/localconfig/LC.java
@@ -213,6 +213,7 @@ public class LC {
     public static final KnownKey wiki_user;
 
     public static final KnownKey calendar_outlook_compatible_allday_events;
+    public static final KnownKey calendar_entourage_compatible_timezones;
 
     public static final KnownKey nio_enabled;
     public static final KnownKey nio_debug_enabled;
@@ -825,6 +826,11 @@ public class LC {
         calendar_outlook_compatible_allday_events = new KnownKey("calendar_outlook_compatible_allday_events");
         calendar_outlook_compatible_allday_events.setDefault("true");
         calendar_outlook_compatible_allday_events.setDoc("Use Outlook-compatible all-day events.  True by default.");
+
+        calendar_entourage_compatible_timezones = new KnownKey("calendar_entourage_compatible_timezones");
+        calendar_entourage_compatible_timezones.setDefault("true");
+        calendar_entourage_compatible_timezones.setDoc(
+                "Quote TZID parameter in iCalendar properties, to workaround bug in MS Entourage.  True by default.");
 
         nio_imap_enabled = new KnownKey("nio_imap_enabled");
         nio_imap_enabled.setDefault("false");
