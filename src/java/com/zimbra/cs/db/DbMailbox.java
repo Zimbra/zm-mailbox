1c3b0e144315e301c631ed3c71e4118dddfa243e
Unknown Perforce User <bburtin>
2007-10-01 16:27:50 -0500

    (split)bug: 19404 - MySQL deadlock when updating mailbox metadata
    Reversed order of SQL statements in DbMailbox.updateConfig()
    so that we try to INSERT first, then UPDATE.  Hopefully this
    will avoid the MySQL deadlock.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=19404
    
    Copied from Perforce
     Change: 69203

--- src/java/com/zimbra/cs/db/DbMailbox.java
+++ src/java/com/zimbra/cs/db/DbMailbox.java
@@ -413,23 +413,30 @@ public class DbMailbox {
                     stmt.executeUpdate();
                     stmt.close();
                 } else {
-                    // New scheme: try update first, then insert if no rows were updated
-                    stmt = conn.prepareStatement("UPDATE mailbox_metadata" +
-                        " SET metadata = ?" +
-                        " WHERE mailbox_id = ? AND " + Db.equalsSTRING("section"));
-                    stmt.setString(1, config.toString());
-                    stmt.setInt(2, mbox.getId());
-                    stmt.setString(3, section.toUpperCase());
-                    int numRows = stmt.executeUpdate();
-                    stmt.close();
-                    
-                    if (numRows == 0) {
-                        stmt = conn.prepareStatement("INSERT INTO mailbox_metadata (mailbox_id, section, metadata)" +
-                        " VALUES (?, ?, ?)");
+                    // We try INSERT first even though it's the less common case, to avoid MySQL
+                    // deadlock.  See bug 19404 for more info.
+                    try {
+                        stmt = conn.prepareStatement("INSERT INTO mailbox_metadata (mailbox_id, section, metadata) " +
+                        "VALUES (?, ?, ?)");
                         stmt.setInt(1, mbox.getId());
                         stmt.setString(2, section);
                         stmt.setString(3, config.toString());
                         stmt.executeUpdate();
+                    } catch (SQLException e) {
+                        if (Db.errorMatches(e, Db.Error.DUPLICATE_ROW)) {
+                            stmt = conn.prepareStatement("UPDATE mailbox_metadata " +
+                                "SET metadata = ? " +
+                                "WHERE mailbox_id = ? AND section = ?");
+                            stmt.setString(1, config.toString());
+                            stmt.setInt(2, mbox.getId());
+                            stmt.setString(3, section);
+                            int numRows = stmt.executeUpdate();
+                            if (numRows != 1) {
+                                String msg = String.format(
+                                    "Unexpected number of rows (%d) updated for section %s", numRows, section);
+                                throw ServiceException.FAILURE(msg, e);
+                            }
+                        }
                     }
                 }
             }
