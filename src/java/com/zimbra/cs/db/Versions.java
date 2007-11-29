35ced27fd03696c5593920e331be59e393ae8c9e
Unknown Perforce User <jhahm>
2007-11-28 19:51:58 -0600

    (split)bug: 22055
    Updated database schema to change account_id column in mailbox and deleted_account table from CHAR(36) to VARCHAR(127).  Added migration script.  Database schema version is now 49.  ZDesktop schema has been updated too.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=22055
    
    Copied from Perforce
     Change: 75097

--- src/java/com/zimbra/cs/db/Versions.java
+++ src/java/com/zimbra/cs/db/Versions.java
@@ -53,7 +53,7 @@ public class Versions {
 	 * UPDATE THESE TO REQUIRE RESET-WORLD TO BE RUN
 	 *  
 	 */
-	public static final String DB_VERSION = "48";
+	public static final String DB_VERSION = "49";
 
 	/**
 	 * The INDEX_VERSION is stored into the config table of the DB when the DB is created.  
