771024c51435e24e5761e82d46211c4bc0226213
Unknown Perforce User <jylee>
2008-01-30 17:11:25 -0600

    (split)bug 15613 checkpoint.  webdav message for propogating zimbra users freebusy to exchange.
    http://bugzilla.zimbra.com/show_bug.cgi?id=15613
    
    Copied from Perforce
     Change: 81974

--- src/java/com/zimbra/cs/fb/ExchangeFreeBusyProvider.java
+++ src/java/com/zimbra/cs/fb/ExchangeFreeBusyProvider.java
@@ -16,11 +16,16 @@
  */
 package com.zimbra.cs.fb;
 
+import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import org.apache.commons.httpclient.auth.AuthPolicy;
+import org.apache.commons.httpclient.Credentials;
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
@@ -28,12 +33,16 @@ import org.apache.commons.httpclient.HttpState;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PutMethod;
 
 import org.dom4j.DocumentException;
 
+import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
 import com.zimbra.common.util.DateUtil;
 import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.cs.mailbox.calendar.IcalXmlStrMap;
 
 public class ExchangeFreeBusyProvider {
@@ -46,6 +55,7 @@ public class ExchangeFreeBusyProvider {
 		public Scheme scheme;
 		public String hostname;
 		public int port;
+		public String ou;
 		public String authUsername;
 		public String authPassword;
 	}
@@ -63,17 +73,12 @@ public class ExchangeFreeBusyProvider {
 		mResolvers.add(priority, r);
 	}
 	public static FreeBusy getFreeBusy(String emailAddr, long start, long end) {
-		ServerInfo serverInfo = null;
-		for (ExchangeUserResolver r : mResolvers) {
-			serverInfo = r.getServerInfo(emailAddr);
-			if (serverInfo != null)
-				break;
-		}
+		ServerInfo serverInfo = getServerInfo(emailAddr);
 		if (serverInfo == null) {
 			ZimbraLog.misc.warn("no exchange server info for user "+emailAddr);
 			return LocalFreeBusyProvider.createDummyFreeBusy(start, end);
 		}
-		String url = constructUrl(serverInfo, emailAddr, start, end);
+		String url = constructGetUrl(serverInfo, emailAddr, start, end);
 		ZimbraLog.misc.debug("fetching fb from url="+url);
 		HttpMethod method = new GetMethod(url);
 		method.setDoAuthentication(true);
@@ -104,13 +109,7 @@ public class ExchangeFreeBusyProvider {
 		return new ExchangeUserFreeBusy(response, emailAddr, FB_INTERVAL, start, end);
 	}
 	
-	private static String constructUrl(ServerInfo info, String emailAddr, long start, long end) {
-		// http://exchange/public/?params..
-		//   cmd      = freebusy
-		//   start    = [ISO8601date]
-		//   end      = [ISO8601date]
-		//   interval = [minutes]
-		//   u        = SMTP:[emailAddr]
+	private static String constructUrl(ServerInfo info) {
 		StringBuilder buf = new StringBuilder();
 		if (info.scheme == ServerInfo.Scheme.http)
 			buf.append("http://");
@@ -121,12 +120,69 @@ public class ExchangeFreeBusyProvider {
 			info.scheme == ServerInfo.Scheme.https && info.port != 443) {
 			buf.append(":").append(info.port);
 		}
+		return buf.toString();
+	}
+	
+	private static String constructGetUrl(ServerInfo info, String emailAddr, long start, long end) {
+		// http://exchange/public/?params..
+		//   cmd      = freebusy
+		//   start    = [ISO8601date]
+		//   end      = [ISO8601date]
+		//   interval = [minutes]
+		//   u        = SMTP:[emailAddr]
+		StringBuilder buf = new StringBuilder(constructUrl(info));
 		buf.append("/public/?cmd=freebusy");
 		buf.append("&start=").append(DateUtil.toISO8601(new Date(start)));
 		buf.append("&endt=").append(DateUtil.toISO8601(new Date(end)));
 		buf.append("&interval=").append(FB_INTERVAL);
 		buf.append("&u=SMTP:").append(emailAddr);
 		return buf.toString();
+	}
+	
+	public static boolean isExchangeUser(String emailAddr) {
+		return getServerInfo(emailAddr) != null;
+	}
+	public static ServerInfo getServerInfo(String emailAddr) {
+		ServerInfo serverInfo = null;
+		for (ExchangeUserResolver r : mResolvers) {
+			serverInfo = r.getServerInfo(emailAddr);
+			if (serverInfo != null)
+				break;
+		}
+		return serverInfo;
+	}
+	// XX move to generic interface
+	public static void publishFreeBusyToExchange(Mailbox mbox) {
+		try {
+			Provisioning prov = Provisioning.getInstance();
+			String emailAddr = prov.get(Provisioning.AccountBy.id, mbox.getAccountId()).getName();
+			Calendar cal = GregorianCalendar.getInstance();
+			cal.setTimeInMillis(System.currentTimeMillis());
+			cal.set(Calendar.DAY_OF_MONTH, 1);
+			cal.set(Calendar.HOUR_OF_DAY, 0);
+			cal.set(Calendar.MINUTE, 0);
+			cal.set(Calendar.SECOND, 0);
+			long start = cal.getTimeInMillis();
+			int month = cal.get(Calendar.MONTH);
+			if (month == 11) {
+				cal.set(Calendar.MONTH, 0);
+				cal.set(Calendar.YEAR, cal.get(Calendar.YEAR) + 1);
+			} else {
+				cal.set(Calendar.MONTH, month + 1);
+			}
+			long end = cal.getTimeInMillis();
+			FreeBusy fb = mbox.getFreeBusy(start, end);
+			publishFreeBusyToExchange(emailAddr, fb);
+		} catch (ServiceException e) {
+			ZimbraLog.misc.warn("cannot publish free/busy to exchange", e);
+		}
+	}
+	public static void publishFreeBusyToExchange(String emailAddr, FreeBusy fb) {
+		Thread t = new Thread(new ExchangeFreeBusySyncThread(emailAddr, fb));
+		t.start();
+	}
+	public static boolean isEnabled() {
+		return true;
 	}
 	
 	public static class ExchangeUserFreeBusy extends FreeBusy {
@@ -197,5 +253,79 @@ public class ExchangeFreeBusyProvider {
 	    	
 	    	return ret;
 	    }
+	}
+	
+	public static class ExchangeFreeBusySyncThread implements Runnable {
+		private String mEmail;
+		private FreeBusy mFb;
+		private boolean mShutdown;
+		
+		ExchangeFreeBusySyncThread(String email, FreeBusy fb) {
+			mEmail = email;
+			mFb = fb;
+			mShutdown = false;
+		}
+		public void run() {
+			boolean retry = false;
+			while (!mShutdown) {
+				try {
+					ServerInfo serverInfo = getServerInfo(mEmail);
+					if (serverInfo == null) {
+						ZimbraLog.misc.warn("no exchange server info for user "+mEmail);
+						shutdown();
+						continue;
+					}
+					ExchangeMessage msg = new ExchangeMessage(serverInfo.ou, mEmail);
+					String url = constructUrl(serverInfo) + msg.getUrl();
+					Credentials cred = new UsernamePasswordCredentials(serverInfo.authUsername, serverInfo.authPassword);
+
+					HttpMethod method = msg.createMethod(url, mFb);
+					try {
+						int status = sendRequest(method, cred);
+						if (status != HttpServletResponse.SC_OK) {
+							method = new PutMethod(url);
+							retry = true;
+							status = sendRequest(method, cred);
+							if (status != HttpServletResponse.SC_CREATED &&
+									status != HttpServletResponse.SC_OK) {
+								retry = false;
+								ZimbraLog.misc.error("cannot create resource at "+url);
+							}
+						}
+					} catch (IOException e) {
+						ZimbraLog.misc.warn("error parsing fb response from exchange", e);
+					} finally {
+						method.releaseConnection();
+					}
+
+					if (!retry)
+						shutdown();
+				} catch (Exception e) {
+					ZimbraLog.misc.error("error while syncing freebusy to exchange", e);
+					shutdown();
+				}
+			}
+		}
+		public void shutdown() {
+			mShutdown = true;
+		}
+		private int sendRequest(HttpMethod method, Credentials credential) throws IOException {
+			int status = 0;
+			method.setDoAuthentication(true);
+			method.setRequestHeader("User-Agent", USER_AGENT);
+			HttpState state = new HttpState();
+			state.setCredentials(AuthScope.ANY, credential);
+			HttpClient client = new HttpClient();
+			client.setState(state);
+	        ArrayList<String> authPrefs = new ArrayList<String>();
+	        authPrefs.add(AuthPolicy.BASIC);
+	        client.getParams().setParameter(AuthPolicy.AUTH_SCHEME_PRIORITY, authPrefs);
+			try {
+				status = client.executeMethod(method);
+			} finally {
+				method.releaseConnection();
+			}
+			return status;
+		}
 	}
 }
---
+++ src/java/com/zimbra/cs/fb/ExchangeMessage.java
@@ -0,0 +1,257 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2008 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Yahoo! Public License
+ * Version 1.0 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.fb;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.ByteArrayRequestEntity;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.Namespace;
+import org.dom4j.QName;
+import org.dom4j.tree.DefaultDocument;
+
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.dav.DomUtil;
+import com.zimbra.cs.mailbox.calendar.IcalXmlStrMap;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+import java.util.regex.Pattern;
+
+public class ExchangeMessage {
+	/*
+	 * public folder URL:
+	 * urlencode( first part + encrypt(/o=<organization>/ou=<organization unit>) + second part + encrypt(/cn=RECIPIENTS/cn=<mailboxid>.EML) )
+	 * 
+	 * first part:
+	 * /public/NON_IPM_SUBTREE/SCHEDULE+ FREE BUSY/EX:
+	 * 
+	 * second part:
+	 * /USER-
+	 * encrypt:
+	 * s/\//_xF8FF_/g
+	 * 
+	 * urlencode:
+	 * s/\+/%2B/g
+	 * s/ /%20/g
+	 * 
+	 * result:
+     * http://exchange/public/NON_IPM_SUBTREE/SCHEDULE%2B%20FREE%20BUSY/EX:_xF8FF_o=First%20Organization_xF8FF_ou=First%20Administrative%20Group/USER-_xF8FF_cn=RECIPIENTS_xF8FF_cn=USER1.EML
+	 */
+	public static final String PUBURL_FIRST_PART = "/public/NON_IPM_SUBTREE/SCHEDULE+ FREE BUSY/EX:";
+	public static final String PUBURL_SECOND_PART = "/USER-";
+	public static final String PUBURL_RCPT = "/cn=RECIPIENTS/cn=";
+	public static final String PUBURL_EML = ".EML";
+	public static final String ENCODED_SLASH = "_xF8FF_";
+	public static final String ENCODED_SPACE = "%20";
+	public static final String ENCODED_PLUS = "%2B";
+	
+	public static final String MV_INT = "mv.int";
+	public static final String MV_BIN = "mv.bin.base64";
+	
+	public static final int MINS_IN_DAY = 60 * 24;
+	
+    public static final Namespace NS_DAV = Namespace.get("DAV:");
+    public static final Namespace NS_XML = Namespace.get("xml:");
+    public static final Namespace NS_MSFT = Namespace.get("http://schemas.microsoft.com/mapi/proptag/");
+    public static final Namespace NS_WEB_FOLDERS = Namespace.get("urn:uuid:c2f41010-65b3-11d1-a29f-00aa00c14882/");
+	
+    public static final QName EL_SET = QName.get("set", NS_DAV);
+    public static final QName EL_PROP = QName.get("prop", NS_DAV);
+    public static final QName EL_PROPERTYUPDATE = QName.get("propertyupdate", NS_DAV);
+    public static final QName EL_V = QName.get("v", NS_XML);
+    
+    public static final QName ATTR_DT = QName.get("dt", NS_WEB_FOLDERS);
+    
+    public static final QName PR_FREEBUSY_ALL_EVENTS       = QName.get("0x68501102", NS_MSFT);
+    public static final QName PR_FREEBUSY_ALL_MONTHS       = QName.get("0x684F1003", NS_MSFT);
+    public static final QName PR_FREEBUSY_BUSY_EVENTS      = QName.get("0x68541102", NS_MSFT);
+    public static final QName PR_FREEBUSY_BUSY_MONTHS      = QName.get("0x68531003", NS_MSFT);
+    public static final QName PR_FREEBUSY_EMAIL_ADDRESS    = QName.get("0x6849001F", NS_MSFT);
+    public static final QName PR_FREEBUSY_END_RANGE        = QName.get("0x68480003", NS_MSFT);
+    public static final QName PR_FREEBUSY_ENTRYIDS         = QName.get("0x36E41102", NS_MSFT);
+    public static final QName PR_FREEBUSY_LAST_MODIFIED    = QName.get("0x68680040", NS_MSFT);
+    public static final QName PR_FREEBUSY_NUM_MONTHS       = QName.get("0x68690003", NS_MSFT);
+    public static final QName PR_FREEBUSY_OOF_EVENTS       = QName.get("0x68561102", NS_MSFT);
+    public static final QName PR_FREEBUSY_OOF_MONTHS       = QName.get("0x68551003", NS_MSFT);
+    public static final QName PR_FREEBUSY_START_RANGE      = QName.get("0x68470003", NS_MSFT);
+    public static final QName PR_FREEBUSY_TENTATIVE_EVENTS = QName.get("0x68521102", NS_MSFT);
+    public static final QName PR_FREEBUSY_TENTATIVE_MONTHS = QName.get("0x68511003", NS_MSFT);
+    public static final QName PR_PROCESS_MEETING_REQUESTS  = QName.get("0x686D000B", NS_MSFT);
+    public static final QName PR_DECLINE_RECURRING_MEETING_REQUESTS = QName.get("0x686E000B", NS_MSFT);
+    public static final QName PR_DECLINE_CONFLICTING_MEETING_REQUESTS = QName.get("0x686F000B", NS_MSFT);
+    public static final QName PR_CAL_END_TIME              = QName.get("0x10C40040", NS_MSFT);
+    public static final QName PR_CAL_RECURRING_ID          = QName.get("0x10C50040", NS_MSFT);
+    public static final QName PR_CAL_REMINDER_NEXT_TIME    = QName.get("0x10CA0040", NS_MSFT);
+    public static final QName PR_CAL_START_TIME            = QName.get("0x10C30040", NS_MSFT);
+    public static final QName PR_SUBJECT_A                 = QName.get("0x0037001E", NS_MSFT);
+    
+    public static final QName PR_68410003 = QName.get("0x68410003", NS_MSFT);
+    public static final QName PR_6842000B = QName.get("0x6842000B", NS_MSFT);
+    public static final QName PR_6843000B = QName.get("0x6843000B", NS_MSFT);
+    public static final QName PR_6846000B = QName.get("0x6846000B", NS_MSFT);
+    public static final QName PR_684B000B = QName.get("0x684B000B", NS_MSFT);
+    
+    private static Pattern SLASH = Pattern.compile("\\/");
+    private static Pattern SPACE = Pattern.compile(" ");
+    private static Pattern PLUS  = Pattern.compile("\\+");
+    
+    private String mOu;
+    private String mMail;
+    
+    public ExchangeMessage(String ou, String mail) {
+    	mOu = ou;
+    	mMail = mail;
+    }
+    
+    public String getUrl() {
+    	StringBuilder buf = new StringBuilder(PUBURL_FIRST_PART);
+    	buf.append(SLASH.matcher(mOu).replaceAll(ENCODED_SLASH));
+    	buf.append(PUBURL_SECOND_PART);
+    	buf.append(SLASH.matcher(PUBURL_RCPT).replaceAll(ENCODED_SLASH));
+    	buf.append(mMail);
+    	buf.append(PUBURL_EML);
+    	
+    	String ret = buf.toString();
+    	ret = SPACE.matcher(ret).replaceAll(ENCODED_SPACE);
+    	ret = PLUS.matcher(ret).replaceAll(ENCODED_PLUS);
+    	return ret;
+    }
+    
+    public Document createRequest(FreeBusy fb) {
+    	Element root = DocumentHelper.createElement(EL_PROPERTYUPDATE);
+    	Element prop = root.addElement(EL_SET).addElement(EL_PROP);
+    	addElement(prop, PR_SUBJECT_A, PUBURL_RCPT + mMail);
+    	addElement(prop, PR_FREEBUSY_START_RANGE, "");
+    	addElement(prop, PR_FREEBUSY_END_RANGE, "");
+    	addElement(prop, PR_FREEBUSY_EMAIL_ADDRESS, mOu + PUBURL_RCPT + mMail);
+    	
+    	
+    	Element allMonths = addElement(prop, PR_FREEBUSY_ALL_MONTHS, null, ATTR_DT, MV_INT);
+    	Element allEvents = addElement(prop, PR_FREEBUSY_ALL_EVENTS, null, ATTR_DT, MV_BIN);
+    	Element busyMonths = addElement(prop, PR_FREEBUSY_BUSY_MONTHS, null, ATTR_DT, MV_INT);
+    	Element busyEvents = addElement(prop, PR_FREEBUSY_BUSY_EVENTS, null, ATTR_DT, MV_BIN);
+    	Element tentativeMonths = addElement(prop, PR_FREEBUSY_TENTATIVE_MONTHS, null, ATTR_DT, MV_INT);
+    	Element tentativeEvents = addElement(prop, PR_FREEBUSY_TENTATIVE_EVENTS, null, ATTR_DT, MV_BIN);
+    	Element oofMonths = addElement(prop, PR_FREEBUSY_OOF_MONTHS, null, ATTR_DT, MV_INT);
+    	Element oofEvents = addElement(prop, PR_FREEBUSY_OOF_EVENTS, null, ATTR_DT, MV_BIN);
+    	
+    	addElement(prop, PR_68410003, "0");
+    	addElement(prop, PR_6842000B, "1");
+    	addElement(prop, PR_6843000B, "1");
+    	addElement(prop, PR_6846000B, "1");
+    	addElement(prop, PR_684B000B, "1");
+    	
+    	Iterator<FreeBusy.Interval> iter = fb.iterator();
+    	while (iter.hasNext()) {
+    		FreeBusy.Interval interval = iter.next();
+    		String status = interval.getStatus();
+    		Element elMonth = null;
+    		Element elEvent = null;
+    		if (status.equals(IcalXmlStrMap.FBTYPE_BUSY)) {
+    			elMonth = busyMonths;
+    			elEvent = busyEvents;
+    		} else if (status.equals(IcalXmlStrMap.FBTYPE_BUSY_TENTATIVE)) {
+    			elMonth = tentativeMonths;
+    			elEvent = tentativeEvents;
+    		} else if (status.equals(IcalXmlStrMap.FBTYPE_BUSY_UNAVAILABLE)) {
+    			elMonth = oofMonths;
+    			elEvent = oofEvents;
+    		}
+    		if (elMonth != null) {
+    			long start = interval.getStart();
+    			long end = interval.getEnd();
+    			try {
+    				String busyMonth = Long.toString(millisToMonths(start));
+    				String busyTime = encodeFb(start, end);
+    				addElement(elMonth, EL_V, busyMonth);
+    				addElement(elEvent, EL_V, busyTime);
+    				addElement(allMonths, EL_V, busyMonth);
+    				addElement(allEvents, EL_V, busyTime);
+    			} catch (IOException e) {
+    				ZimbraLog.misc.warn("error converting millis to minutes "+start+", "+end, e);
+    				continue;
+    			}
+    		}
+    	}
+    	return new DefaultDocument(root);
+    }
+    
+    public HttpMethod createMethod(String uri, FreeBusy fb) throws IOException {
+    	// PROPPATCH
+    	PostMethod method = new PostMethod(uri) {
+    		private String PROPPATCH = "PROPPATCH";
+    		public String getName() {
+    			return PROPPATCH;
+    		}
+    	};
+		Document doc = createRequest(fb);
+		byte[] buf = DomUtil.getBytes(doc);
+		ByteArrayRequestEntity re = new ByteArrayRequestEntity(buf, "text/xml");
+		method.setRequestEntity(re);
+		return method;
+    }
+    private Element addElement(Element parent, QName name, String text, QName attr, String attrVal) {
+    	Element el = parent.addElement(name);
+    	if (text != null)
+    		el.setText(text);
+    	if (attr != null && attrVal != null)
+    		el.addAttribute(attr, attrVal);
+    	return el;
+    }
+    private Element addElement(Element parent, QName name, String text) {
+    	return addElement(parent, name, text, null, null);
+    }
+    private String encodeFb(long s, long e) throws IOException {
+    	int start = millisToMinutes(s);
+    	int end = millisToMinutes(e);
+    	// swap bytes and convert to little endian.  then lay out bytes
+    	// two bytes each for start time, then end time
+    	byte[] buf = new byte[4];
+    	int i = 0;
+    	buf[i++] = (byte)(start & 0xFF);
+    	buf[i++] = (byte)(start >> 8 & 0xFF);
+    	buf[i++] = (byte)(end & 0xFF);
+    	buf[i++] = (byte)(end >> 8 & 0xFF);
+    	
+    	// base64 encode
+    	byte[] encoded = Base64.encodeBase64(buf);
+    	return new String(encoded, "UTF-8");
+    }
+    private int millisToMinutes(long millis) throws IOException {
+    	// millis since epoch to minutes since 1st of the month
+    	Calendar c = new GregorianCalendar();
+    	c.setTime(new Date(millis));
+    	int days = c.get(Calendar.DAY_OF_MONTH) - 1;
+    	int hours =  24 * days + c.get(Calendar.HOUR_OF_DAY);
+    	return 60 * hours + c.get(Calendar.MINUTE);
+    }
+    private long millisToMonths(long millis) {
+    	// number of freebusy months = year * 16 + month
+    	// why * 16 not * 12, ask msft.
+    	Calendar c = new GregorianCalendar();
+    	c.setTime(new Date(millis));
+    	return c.get(Calendar.YEAR) * 16 + c.get(Calendar.MONTH) + 1;  // january is 0
+    }
+}
