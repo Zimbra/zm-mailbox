08a4cc77675bf5e892b9be51f1d541c33ec1fd29
Unknown Perforce User <dac>
2008-02-05 19:25:22 -0600

    bug: 24324 In IMAP import, cache downloaded messages exceeding LC 'data_source_max_literal_mem_size' bytes (default 2MB) in a temporary file rather than in memory. The temporary file will be cleaned up as soon as the message is stored locally.
    http://bugzilla.zimbra.com/show_bug.cgi?id=24324
    
    Copied from Perforce
     Change: 82577

---
+++ src/java/com/zimbra/cs/mailtest/Io.java
@@ -0,0 +1,34 @@
+package com.zimbra.cs.mailtest;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.EOFException;
+import java.io.DataInputStream;
+
+/**
+ * I/O utility methods.
+ */
+public final class Io {
+    public static void readFully(InputStream is, byte[] b, int off, int len)
+            throws IOException {
+        new DataInputStream(is).readFully(b, off, len);
+    }
+
+    public static void readFully(InputStream is, byte[] b) throws IOException {
+        readFully(is, b, 0, b.length);
+    }
+    
+    public static void copyBytes(InputStream is, OutputStream os, int len)
+            throws IOException {
+        byte[] b = new byte[4096];
+        while (len > 0) {
+            int n = is.read(b, 0, Math.min(b.length, len));
+            if (n == -1) {
+                throw new EOFException("Unexpected end of stream");
+            }
+            os.write(b, 0, n);
+            len -= n;
+        }
+    }
+}
