4a641c2b42a32dba6fd052bef861c73e04ea3967
Unknown Perforce User <dkarp>
2008-02-12 13:29:35 -0600

    bug: 24325
    Propagate to FRANKLIN:
    
    Change 83443 by dkarp@dkarp-copy on 2008/02/12 11:27:53
    
    	SoapFaultException.getCode() and getReason() now look for their 
    	target values as attributes rather than as element text so that 
    	they also work with JSON serialization.
    
    	Fault argument constants are now fetched from ZimbraNamespace.
    
    	Several places in SoapFaultException now use a shared constant 
    	String[] for XPath-ing rather than allocating and initializing a 
    	new one for each call.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=24325
    
    Copied from Perforce
     Change: 83444

--- src/java/com/zimbra/common/soap/AdminConstants.java
+++ src/java/com/zimbra/common/soap/AdminConstants.java
@@ -485,4 +485,8 @@ public class AdminConstants {
     public static final String A_PROPAGATE = "propagate";
     public static final String A_START = "start";
     public static final String A_END = "end";
+    
+    //extension targets
+    public static final String T_ADMIN_MAIN = "admin-main";
+    public static final String T_ADMIN_DOMAIN = "admin-domain";     
 }
