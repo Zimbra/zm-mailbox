3c728c4f0246cc5a3ba2db396bd5e8ea19af6925
Unknown Perforce User <dac>
2008-03-21 21:13:01 -0500

    (split)bug: 26234 Enable additional debug output
    http://bugzilla.zimbra.com/show_bug.cgi?id=26234
    
    Copied from Perforce
     Change: 89672

--- src/java/com/zimbra/cs/mina/MinaCodecFactory.java
+++ src/java/com/zimbra/cs/mina/MinaCodecFactory.java
@@ -62,6 +62,10 @@ class MinaCodecFactory implements ProtocolCodecFactory {
         public void decode(IoSession session,
                            org.apache.mina.common.ByteBuffer in,
                            ProtocolDecoderOutput out) throws IOException {
+            if (MinaServer.isDebugEnabled()) {
+                ZimbraLog.nio.debug("Decode request: session = %d, bytes = %d",
+                                    session.hashCode(), in.remaining());
+            }
             java.nio.ByteBuffer bb = in.buf();
             while (bb.hasRemaining()) {
                 if (req == null) {
@@ -78,6 +82,9 @@ class MinaCodecFactory implements ProtocolCodecFactory {
                 if (!req.isComplete()) break;
                 out.write(req);
                 req = null;
+            }
+            if (MinaServer.isDebugEnabled()) {
+                ZimbraLog.nio.debug("Decode finished: session = %d", session.hashCode());
             }
         }
     }
