bc7021c6226a43431d57b2c630a5ab6603ac84f6
Unknown Perforce User <dkarp>
2008-03-25 10:40:47 -0500

    (split)bug: 26304
    GetItem by path now traverses mountpoints within the path.
    
    "name" is now deprecated in GetItem.  Callers should use "path" regardless of whether they include an enclosing "l".
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=26304
    
    Copied from Perforce
     Change: 90038

--- src/java/com/zimbra/cs/service/mail/GetItem.java
+++ src/java/com/zimbra/cs/service/mail/GetItem.java
@@ -21,8 +21,12 @@ import java.util.Map;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.MailConstants;
 import com.zimbra.common.soap.Element;
+import com.zimbra.common.util.Pair;
+import com.zimbra.cs.mailbox.Folder;
 import com.zimbra.cs.mailbox.MailItem;
 import com.zimbra.cs.mailbox.Mailbox;
+import com.zimbra.cs.mailbox.MailServiceException;
+import com.zimbra.cs.mailbox.Mountpoint;
 import com.zimbra.cs.mailbox.Mailbox.OperationContext;
 import com.zimbra.cs.service.util.ItemId;
 import com.zimbra.cs.service.util.ItemIdFormatter;
@@ -33,13 +37,13 @@ public class GetItem extends MailDocumentHandler {
     private static final String[] TARGET_ITEM_PATH = new String[] { MailConstants.E_ITEM, MailConstants.A_ID };
     private static final String[] TARGET_FOLDER_PATH = new String[] { MailConstants.E_ITEM, MailConstants.A_FOLDER };
     private static final String[] RESPONSE_ITEM_PATH = new String[] { };
-    protected String[] getProxiedIdPath(Element request) {
+    @Override protected String[] getProxiedIdPath(Element request) {
         if (getXPath(request, TARGET_ITEM_PATH) != null)    return TARGET_ITEM_PATH;
         if (getXPath(request, TARGET_FOLDER_PATH) != null)  return TARGET_FOLDER_PATH;
         return null;
     }
-    protected boolean checkMountpointProxy(Element request)  { return getXPath(request, TARGET_ITEM_PATH) == null; }
-    protected String[] getResponseItemPath()  { return RESPONSE_ITEM_PATH; }
+    @Override protected boolean checkMountpointProxy(Element request)  { return getXPath(request, TARGET_ITEM_PATH) == null; }
+    @Override protected String[] getResponseItemPath()  { return RESPONSE_ITEM_PATH; }
 
     @Override public Element handle(Element request, Map<String, Object> context) throws ServiceException {
         ZimbraSoapContext zsc = getZimbraSoapContext(context);
@@ -49,22 +53,32 @@ public class GetItem extends MailDocumentHandler {
 
         Element target = request.getElement(MailConstants.E_ITEM);
         String id = target.getAttribute(MailConstants.A_ID, null);
-        String folder = target.getAttribute(MailConstants.A_FOLDER, null);
-        String path = target.getAttribute(MailConstants.A_PATH, null);
+        String folderStr = target.getAttribute(MailConstants.A_FOLDER, null);
+        String path = target.getAttribute(MailConstants.A_PATH, target.getAttribute(MailConstants.A_NAME, null));
+
+        if (folderStr != null && path == null)
+            throw ServiceException.INVALID_REQUEST("missing required attribute: " + MailConstants.A_PATH, null);
 
         MailItem item;
         if (id != null) {
             // get item by id
             item = mbox.getItemById(octxt, new ItemId(id, zsc).getId(), MailItem.TYPE_UNKNOWN);
-        } else if (folder != null) {
-            // get item by name within containing folder id
-            String name = target.getAttribute(MailConstants.A_NAME);
-            item = mbox.getItemByPath(octxt, name, new ItemId(folder, zsc).getId());
         } else if (path != null) {
-            // get item by user-root-relative absolute path
-            item = mbox.getItemByPath(octxt, path); 
+            // get item by name within containing folder id (from root by default)
+            int folderId = folderStr == null ? Mailbox.ID_FOLDER_USER_ROOT : new ItemId(folderStr, zsc).getId();
+            try {
+                item = mbox.getItemByPath(octxt, path, folderId);
+            } catch (MailServiceException.NoSuchItemException nsie) {
+                Pair<Folder, String> match = mbox.getFolderByPathLongestMatch(octxt, folderId, path);
+                if (match.getFirst() instanceof Mountpoint) {
+                    Mountpoint mpt = (Mountpoint) match.getFirst();
+                    target.addAttribute(MailConstants.A_FOLDER, mpt.getRemoteId()).addAttribute(MailConstants.A_PATH, match.getSecond());
+                    return proxyRequest(request, context, mpt.getOwnerId());
+                }
+                throw nsie;
+            }
         } else {
-            throw ServiceException.INVALID_REQUEST("must specify 'id' or 'path' or 'l' / 'name'", null);
+            throw ServiceException.INVALID_REQUEST("must specify 'id' or 'path'", null);
         }
 
         Element response = zsc.createElement(MailConstants.GET_ITEM_RESPONSE);
