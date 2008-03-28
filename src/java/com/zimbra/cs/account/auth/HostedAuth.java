869c35ee837b02d45cba0d264bdc6f67e0adeda8
Unknown Perforce User <pshao>
2008-03-28 17:16:50 -0500

    (split)bug: 24454
    integrate 
    //depot/FRANKLIN/ZimbraCustomerServices/ZimbraHostedAuthModule/src/java/com/zimbra/cs/service/hosted/HostedAuth.java
    to
    //depot/main/ZimbraServer/src/java/com/zimbra/cs/account/auth/HostedAuth.java
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=24454
    
    Copied from Perforce
     Change: 90601

---
+++ src/java/com/zimbra/cs/account/auth/HostedAuth.java
@@ -0,0 +1,91 @@
+package com.zimbra.cs.service.hosted;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.AccountServiceException.AuthFailedServiceException;
+import com.zimbra.cs.account.ldap.ZimbraCustomAuth;
+
+public class HostedAuth extends ZimbraCustomAuth {
+	public static String HEADER_AUTH_METHOD = "Auth-Method";
+	public static String HEADER_AUTH_USER = "Auth-User";
+	public static String HEADER_AUTH_PASSWORD = "Auth-Pass";
+	public static String HEADER_AUTH_PROTOCOL = "Auth-Protocol";
+	public static String HEADER_AUTH_LOGIN_ATTEMPT = "Auth-Login-Attempt";
+	public static String HEADER_CLIENT_IP = "Client-IP";
+	public static String HEADER_AUTH_STATUS = "Auth-Status";
+	public static String HEADER_AUTH_SERVER = "Auth-Server";
+	public static String HEADER_AUTH_PORT = "Auth-Port";
+	
+	public static String AUTH_STATUS_OK = "OK";
+	
+	/**
+	 *  This custom auth module takes arguments in the following form:
+	 * {URL} [GET|POST - default is GET] [encryption method - defautl is plain] [auth protocol - default is imap] 
+	 * e.g.: http://auth.customer.com:80 GET plain imap
+	 **/
+	public void authenticate(Account acct, String password,
+			Map<String, Object> context, List<String> args) throws Exception {
+		HttpClient client = new HttpClient();
+		HttpMethod method = null;
+		
+		String authMethod = "plain";
+		String authProtocol = "imap";
+		String targetURL = args.get(0);
+		
+		if (args.size()>2) {
+			authMethod = args.get(2);
+		}
+		
+		if (args.size()>3) {
+			authMethod = args.get(3);
+		}
+		
+		if(args.size()>1) {
+			if(args.get(1).equalsIgnoreCase("GET"))
+				method = new GetMethod(targetURL);
+			else
+				method = new PostMethod(targetURL);
+		} else
+			method = new GetMethod(targetURL);
+		
+		method.addRequestHeader(HEADER_AUTH_METHOD,authMethod);
+		method.addRequestHeader(HEADER_AUTH_USER,acct.getName());
+		method.addRequestHeader(HEADER_AUTH_PASSWORD,password);
+		method.addRequestHeader(HEADER_AUTH_PROTOCOL,authProtocol);
+        try {
+            client.executeMethod(method);
+        } catch (HttpException ex) {
+        	throw AuthFailedServiceException.AUTH_FAILED(acct.getName(),acct.getName(), "HTTP request to remote authentication server failed",ex); 
+        } catch (IOException ex) {
+        	throw AuthFailedServiceException.AUTH_FAILED(acct.getName(), acct.getName(), "HTTP request to remote authentication server failed",ex); 
+        }
+        int status = method.getStatusCode(); 
+        if(status != HttpStatus.SC_OK) {
+        	throw AuthFailedServiceException.AUTH_FAILED(acct.getName(), "HTTP request to remote authentication server failed. Remote response code: " + Integer.toString(status));
+        }
+        
+        String responseMessage;
+        if(method.getResponseHeader(HEADER_AUTH_STATUS) != null) {
+        	responseMessage = method.getResponseHeader(HEADER_AUTH_STATUS).getValue();
+        } else {
+        	throw AuthFailedServiceException.AUTH_FAILED(acct.getName(), "Empty response from remote authentication server.");
+        }
+        if(responseMessage.equalsIgnoreCase(AUTH_STATUS_OK)) {
+        	return;
+        } else {
+        	throw AuthFailedServiceException.AUTH_FAILED(acct.getName(), responseMessage);
+        }
+        
+	}
+
+}
