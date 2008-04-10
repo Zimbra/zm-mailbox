0b1735e2371f5113da793651f4a2e6e080319aad
Unknown Perforce User <pshao>
2008-04-10 16:53:09 -0500

    (split)bug: 21204, 24757
    added a seperate unit test class for testing enabled/exclude ciphers for imaps/pop3s/https, instead of piggybacking on TestPop3ImapAuth.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=21204
    http://bugzilla.zimbra.com/show_bug.cgi?id=24757
    
    Copied from Perforce
     Change: 92664

--- src/java/com/zimbra/qa/unittest/TestPop3ImapAuth.java
+++ src/java/com/zimbra/qa/unittest/TestPop3ImapAuth.java
@@ -238,28 +238,6 @@ extends TestCase {
         send(socket, null, IMAP_LOGOUT_RESPONSE2);
     }
     
-    
-    public void testCiphers() throws Exception {
-        Socket socket = DummySSLSocketFactory.getDefault().createSocket(HOSTNAME, mPop3SslPort);
-        SSLSocket sslSocket = (SSLSocket)socket;
-
-        // use an excluded cipher suite 
-        sslSocket.setEnabledCipherSuites(new String[] {"SSL_RSA_WITH_DES_CBC_SHA"});
-
-        boolean good = false;
-        try {
-            send(socket, "", POP3_CONNECT_RESPONSE);
-            send(socket, POP3_USER, POP3_USER_RESPONSE);
-            send(socket, POP3_PASS, POP3_PASS_RESPONSE);
-            send(socket, POP3_QUIT, POP3_QUIT_RESPONSE);
-        } catch (javax.net.ssl.SSLHandshakeException e) {
-            good = true;
-        } finally {
-            socket.close();
-        }
-        assertTrue(good);
-    }
-    
     public void tearDown()
     throws Exception {
         setPop3Cleartext(mOrigPop3CleartextLoginEnabled);
