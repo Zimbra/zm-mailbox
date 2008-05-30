9b3b8d9adba0432e6ac85500647410b48afb4c04
Unknown Perforce User <pshao>
2008-05-29 22:22:21 -0500

    (split)bug: 9620
    added support for calresource, cos, distributionlist, domain, server targets in CheckPermission, used to support only account target.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=9620
    
    Copied from Perforce
     Change: 100152

--- src/java/com/zimbra/cs/service/admin/CheckPermission.java
+++ src/java/com/zimbra/cs/service/admin/CheckPermission.java
@@ -28,43 +28,71 @@ import com.zimbra.common.soap.Element;
 import com.zimbra.cs.account.AccessManager;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.AccountServiceException;
+import com.zimbra.cs.account.NamedEntry;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.Provisioning.AccountBy;
+import com.zimbra.cs.account.Provisioning.CalendarResourceBy;
+import com.zimbra.cs.account.Provisioning.CosBy;
+import com.zimbra.cs.account.Provisioning.DistributionListBy;
+import com.zimbra.cs.account.Provisioning.DomainBy;
+import com.zimbra.cs.account.Provisioning.ServerBy;
 import com.zimbra.cs.account.accesscontrol.Right;
 import com.zimbra.cs.account.accesscontrol.RightManager;
 import com.zimbra.soap.ZimbraSoapContext;
 
 public class CheckPermission extends AdminDocumentHandler {
 
+    // proxy only if target is an account
     private static final String[] TARGET_ACCOUNT_PATH = new String[] { AdminConstants.E_ACCOUNT };
     protected String[] getProxiedAccountElementPath()  { return TARGET_ACCOUNT_PATH; }
 
-    /**
-     * must be careful and only return accounts a domain admin can see
-     */
-    public boolean domainAuthSufficient(Map context) {
-        return true;
-    }
 
     public Element handle(Element request, Map<String, Object> context) throws ServiceException {
         ZimbraSoapContext zsc = getZimbraSoapContext(context);
         Provisioning prov = Provisioning.getInstance();
 
-        Element a = request.getElement(AdminConstants.E_ACCOUNT);
-        String acctBy = a.getAttribute(AdminConstants.A_BY);
-        String acctValue = a.getText();
-
-        Account account = prov.get(AccountBy.fromString(acctBy), acctValue, zsc.getAuthToken());
-
-        if (account == null)
-            throw AccountServiceException.NO_SUCH_ACCOUNT(acctValue);
+        Element eTarget = request.getElement(AdminConstants.E_TARGET);
+        String targetType = eTarget.getAttribute(AdminConstants.A_TYPE);
+        String targetBy = eTarget.getAttribute(AdminConstants.A_BY);
+        String targetValue = eTarget.getText();
         
-        if (!canAccessAccount(zsc, account))
-            throw ServiceException.PERM_DENIED("can not access account");
+        NamedEntry entry = null;
+        if (targetType.equals(AdminConstants.A_ACCOUNT)) {
+            entry = prov.get(AccountBy.fromString(targetBy), targetValue, zsc.getAuthToken());
+            if (entry == null)
+                throw AccountServiceException.NO_SUCH_ACCOUNT(targetValue);
+                
+            /*
+            if (!canAccessAccount(zsc, (Account)entry))
+                throw ServiceException.PERM_DENIED("can not access account");
+            */
+        } else if (targetType.equals(AdminConstants.A_CALENDAR_RESOURCE)) {
+            entry = prov.get(CalendarResourceBy.fromString(targetBy), targetValue);
+            if (entry == null)
+                throw AccountServiceException.NO_SUCH_CALENDAR_RESOURCE(targetValue);
+        } else if (targetType.equals(AdminConstants.A_COS)) {
+            entry = prov.get(CosBy.fromString(targetBy), targetValue);
+            if (entry == null)
+                throw AccountServiceException.NO_SUCH_COS(targetValue);
+        } else if (targetType.equals(AdminConstants.A_DISTRIBUTION_LIST)) {
+            entry = prov.get(DistributionListBy.fromString(targetBy), targetValue);
+            if (entry == null)
+                throw AccountServiceException.NO_SUCH_DISTRIBUTION_LIST(targetValue);
+        } else if (targetType.equals(AdminConstants.A_DOMAIN)) {
+            entry = prov.get(DomainBy.fromString(targetBy), targetValue);
+            if (entry == null)
+                throw AccountServiceException.NO_SUCH_DOMAIN(targetValue);
+        } else if (targetType.equals(AdminConstants.A_SERVER)) {
+            entry = prov.get(ServerBy.fromString(targetBy), targetValue);
+            if (entry == null)
+                throw AccountServiceException.NO_SUCH_SERVER(targetValue);
+        } else
+            throw ServiceException.INVALID_REQUEST("invalid target type: " + targetType, null);
+
         
-        Element p = request.getElement(AdminConstants.E_PRINCIPAL);
-        String principalBy = p.getAttribute(AdminConstants.A_BY);
-        String principalValue = p.getText();
+        Element ePrincipal = request.getElement(AdminConstants.E_PRINCIPAL);
+        String principalBy = ePrincipal.getAttribute(AdminConstants.A_BY);
+        String principalValue = ePrincipal.getText();
 
         // look for the principal if it is not identified by name
         AccountBy by = AccountBy.fromString(principalBy);
@@ -78,8 +106,8 @@ public class CheckPermission extends AdminDocumentHandler {
         Element r = request.getElement(AdminConstants.E_RIGHT);
         Right right = RightManager.getInstance().getRight(r.getText());
         
-        if (!AccessManager.getInstance().canPerform(principalValue, account, right, false, false))
-            throw ServiceException.PERM_DENIED("credential " + principalValue + " is not allowed for right " + right.getCode() + " on target " + account.getName());
+        if (!AccessManager.getInstance().canPerform(principalValue, entry, right, false, false))
+            throw ServiceException.PERM_DENIED("credential " + principalValue + " is not allowed for right " + right.getCode() + " on target " + entry.getName());
 
         Element response = zsc.createElement(AdminConstants.CHECK_PERMISSION_RESPONSE);
 
