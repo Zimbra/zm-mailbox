d52f9f1fb4947a991415d85ae1c2c9827ae3f63d
Unknown Perforce User <pshao>
2008-06-03 15:55:06 -0500

    bug: 28571
    integrate change 100767 from main to FRANKLIN 
    ------------------------------------------------------
    Change 100767 by pshao@pshao-mac on 2008/06/03 13:53:56
    
            bug: 28571
    
            - if GetProfileResponse from the SSML gateway does not have ums element, don't pull quota  and vm counts from the voicemail gateway.
            - added SOAP attr vm="0|1" on the <phone> element in GetVoiceInfoResponse and GetVoiceFolderResponse.  vm=0 indicates the phone does not have voicemail service, and client can render the UI accordingly.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=28571
    
    Copied from Perforce
     Change: 100768

--- src/java/com/zimbra/common/localconfig/LC.java
+++ src/java/com/zimbra/common/localconfig/LC.java
@@ -150,6 +150,7 @@ public class LC {
 
     public static final KnownKey zimbra_converter_enabled_uuencode;
     public static final KnownKey zimbra_converter_enabled_tnef;
+    public static final KnownKey convertd_version;
 
     public static final KnownKey zimbra_ssl_enabled;
     
@@ -555,6 +556,8 @@ public class LC {
         zimbra_converter_enabled_tnef = new KnownKey("zimbra_converter_enabled_tnef");
         zimbra_converter_enabled_tnef.setDefault("true");
 
+	convertd_version = new KnownKey("convertd_version");
+	convertd_version.setDefault("1");
         
         zimbra_ssl_enabled = new KnownKey("zimbra_ssl_enabled", "true");
         stats_img_folder = new KnownKey("stats_img_folder");
