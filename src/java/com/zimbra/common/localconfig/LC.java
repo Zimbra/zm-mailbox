5bfccb12de129ce651866ce54b32a02f993b7e7f
Unknown Perforce User <jhahm>
2008-07-02 18:48:44 -0500

    bug: 29596
    Integrating from FRANKLIN to main
    
    Change 105370 by jhahm@jhahm-mac on 2008/07/02 16:46:27
    
            bug: 29596
    
            Added localconfig key calendar_ics_export_buffer_size.  This is the 
            size of the in-memory buffer used during ics export.  If data being 
            exported is smaller than this size, it is buffered in memory before 
            being written out to the client over the network.  If data exceeds 
            this size, it is buffered to a temporary file.  The buffering is 
            done to avoid locking the mailbox during network write, which can be 
            slow depending on the client or the network connection.
    
            This key defaults to 131072 bytes (128KB).
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=29596
    
    Copied from Perforce
     Change: 105374

--- src/java/com/zimbra/common/localconfig/LC.java
+++ src/java/com/zimbra/common/localconfig/LC.java
@@ -17,9 +17,6 @@
 
 package com.zimbra.common.localconfig;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import com.zimbra.common.util.Constants;
 
 /**
@@ -153,7 +150,6 @@ public class LC {
     public static final KnownKey zimbra_converter_enabled_uuencode;
     public static final KnownKey zimbra_converter_enabled_tnef;
     public static final KnownKey zimbra_converter_depth_max;
-    public static final KnownKey convertd_version;
 
     public static final KnownKey zimbra_ssl_enabled;
     
@@ -556,8 +552,6 @@ public class LC {
         zimbra_converter_depth_max = new KnownKey("zimbra_converter_depth_max");
         zimbra_converter_depth_max.setDefault("100");
 
-        convertd_version = new KnownKey("convertd_version");
-        convertd_version.setDefault("1");
         
         zimbra_ssl_enabled = new KnownKey("zimbra_ssl_enabled", "true");
         stats_img_folder = new KnownKey("stats_img_folder");
