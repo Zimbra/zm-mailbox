c14c077841a8681e74dd4cfd9d0081050a2ebdfb
Unknown Perforce User <dkarp>
2008-08-19 14:00:38 -0500

    (split)bug: 20177
    Handle qualified IDs and proxying for GetNote request.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=20177
    
    Copied from Perforce
     Change: 111795

--- src/java/com/zimbra/cs/service/mail/GetNote.java
+++ src/java/com/zimbra/cs/service/mail/GetNote.java
@@ -28,13 +28,15 @@ import com.zimbra.common.soap.Element;
 import com.zimbra.cs.mailbox.MailServiceException;
 import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.cs.mailbox.Note;
+import com.zimbra.cs.service.util.ItemId;
 import com.zimbra.cs.service.util.ItemIdFormatter;
 import com.zimbra.soap.ZimbraSoapContext;
 
-/**
- * @author dkarp
- */
 public class GetNote extends MailDocumentHandler {
+
+    private static final String[] TARGET_NOTE_PATH = new String[] { MailConstants.E_NOTE, MailConstants.A_ID };
+    protected String[] getProxiedIdPath(Element request)     { return TARGET_NOTE_PATH; }
+    protected boolean checkMountpointProxy(Element request)  { return false; }
 
 	public Element handle(Element request, Map<String, Object> context) throws ServiceException {
 		ZimbraSoapContext zsc = getZimbraSoapContext(context);
@@ -43,12 +45,12 @@ public class GetNote extends MailDocumentHandler {
         ItemIdFormatter ifmt = new ItemIdFormatter(zsc);
 		
 		Element enote = request.getElement(MailConstants.E_NOTE);
-		int noteId = (int) enote.getAttributeLong(MailConstants.A_ID);
-		
-		Note note = mbox.getNoteById(octxt, noteId);
-        
+        ItemId iid = new ItemId(enote.getAttribute(MailConstants.A_ID), zsc);
+
+		Note note = mbox.getNoteById(octxt, iid.getId());
+
 		if (note == null)
-			throw MailServiceException.NO_SUCH_NOTE(noteId);
+			throw MailServiceException.NO_SUCH_NOTE(iid.getId());
 		
 		Element response = zsc.createElement(MailConstants.GET_NOTE_RESPONSE);
 		ToXML.encodeNote(response, ifmt, note);
