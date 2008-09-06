2810b94825e8808744462e33dc9c0497af20c405
Unknown Perforce User <pshao>
2008-09-05 21:25:10 -0500

    (split)bug: 30128
    added  server/global config multi-valued attrs:
        zimbraReverseProxyImapEnabledCapability
        zimbraReverseProxyImapSSLEnabledCapability
        zimbraReverseProxyPop3EnabledCapability
        zimbraReverseProxyPop3SSLEnabledCapability
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=30128
    
    Copied from Perforce
     Change: 113507

--- src/java/com/zimbra/cs/account/Provisioning.java
+++ src/java/com/zimbra/cs/account/Provisioning.java
@@ -1477,6 +1477,11 @@ public abstract class Provisioning {
     public static final String A_zimbraReverseProxyDomainNameAttribute  = "zimbraReverseProxyDomainNameAttribute";
     public static final String A_zimbraReverseProxyHttpEnabled          = "zimbraReverseProxyHttpEnabled";
     public static final String A_zimbraReverseProxyAdminIPAddress       = "zimbraReverseProxyAdminIPAddress";
+    public static final String A_zimbraReverseProxyImapEnabledCapability    = "zimbraReverseProxyImapEnabledCapability";
+    public static final String A_zimbraReverseProxyImapSSLEnabledCapability = "zimbraReverseProxyImapSSLEnabledCapability";
+    public static final String A_zimbraReverseProxyPop3EnabledCapability    = "zimbraReverseProxyPop3EnabledCapability";
+    public static final String A_zimbraReverseProxyPop3SSLEnabledCapability = "zimbraReverseProxyPop3SSLEnabledCapability";
+    
     public static final String A_zimbraVirtualIPAddress                 = "zimbraVirtualIPAddress";
     
     /*
