cabbea085110217975b100b8f38824b510d5e416
Unknown Perforce User <jjzhuang>
2008-09-29 04:01:06 -0500

    bug: 31879
    back out 116540 because it caused a race condition of potential circular static init between LC and ZimbraLog.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=31879
    
    Copied from Perforce
     Change: 116874

--- src/java/com/zimbra/common/localconfig/LC.java
+++ src/java/com/zimbra/common/localconfig/LC.java
@@ -355,8 +355,6 @@ public class LC {
     public static final KnownKey zimbra_auth_provider;
     public static final KnownKey zimbra_authtoken_cookie_domain;
     
-    public static final KnownKey zimbra_log_context_filter;
-    
     public static final KnownKey debug_update_config_use_old_scheme;
     
     public static final KnownKey debug_xmpp_disable_client_tls;
@@ -1092,8 +1090,6 @@ public class LC {
         zimbra_auth_provider.setDefault("zimbra");
         zimbra_authtoken_cookie_domain = new KnownKey("zimbra_authtoken_cookie_domain");
         zimbra_authtoken_cookie_domain.setDefault("");
-        
-        zimbra_log_context_filter = new KnownKey("zimbra_log_context_filter", "", "comma delimited list of log context items to filter out");
         
         debug_update_config_use_old_scheme = new KnownKey("debug_update_config_use_old_scheme");
         debug_update_config_use_old_scheme.setDefault("false");
