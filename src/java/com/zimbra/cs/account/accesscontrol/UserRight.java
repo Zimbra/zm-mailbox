4c1dd91016d058212653811dc2299bd7a518929c
Unknown Perforce User <pshao>
2008-10-27 12:04:32 -0500

    (split)bug: 22819
    added sendAs account level right.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=22819
    
    Copied from Perforce
     Change: 120586

--- src/java/com/zimbra/cs/account/accesscontrol/UserRight.java
+++ src/java/com/zimbra/cs/account/accesscontrol/UserRight.java
@@ -6,13 +6,15 @@ public class UserRight extends Right {
     
     // known rights
     public static UserRight RT_invite;
-    public static UserRight RT_viewFreeBusy;
     public static UserRight RT_loginAs;
-
+    public static UserRight RT_sendAs;
+    public static UserRight RT_viewFreeBusy;
+    
     static void initKnownUserRights(RightManager rm) throws ServiceException {
         RT_invite = rm.getUserRight("invite");
-        RT_viewFreeBusy = rm.getUserRight("viewFreeBusy");
         RT_loginAs = rm.getUserRight("loginAs");
+        RT_sendAs = rm.getUserRight("sendAs");
+        RT_viewFreeBusy = rm.getUserRight("viewFreeBusy");
     }
     
     UserRight(String name) {
