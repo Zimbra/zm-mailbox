f37bca388eead1bba50decb0e7c1e4cdc76a0dae
Unknown Perforce User <tim>
2008-11-07 13:27:44 -0600

    bug: 27700
    Renamed "IMJoinChatRequest" API to be "IMJoinConferenceRoomRequest" to clean up the API.  Now "Chat" always refers to an active conversation you personally have going, while a "Conference Room" is used consistently to refer to a MUC room on a MUC service.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=27700
    
    Copied from Perforce
     Change: 122139

--- src/java/com/zimbra/common/soap/IMConstants.java
+++ src/java/com/zimbra/common/soap/IMConstants.java
@@ -38,8 +38,8 @@ public class IMConstants {
     public static final QName IM_MODIFY_CHAT_RESPONSE  = QName.get("IMModifyChatResponse", NAMESPACE);
     public static final QName IM_SEND_MESSAGE_REQUEST  = QName.get("IMSendMessageRequest", NAMESPACE);
     public static final QName IM_SEND_MESSAGE_RESPONSE = QName.get("IMSendMessageResponse", NAMESPACE);
-    public static final QName IM_JOIN_CHAT_REQUEST = QName.get("IMJoinChatRequest", NAMESPACE);
-    public static final QName IM_JOIN_CHAT_RESPONSE = QName.get("IMJoinChatResponse", NAMESPACE);
+    public static final QName IM_JOIN_CONFERENCE_ROOM_REQUEST = QName.get("IMJoinConferenceRoomRequest", NAMESPACE);
+    public static final QName IM_JOIN_CONFERENCE_ROOM_RESPONSE = QName.get("IMJoinConferenceRoomResponse", NAMESPACE);
     
     public static final QName IM_GET_PRIVACY_LIST_REQUEST = QName.get("IMGetPrivacyListRequest", NAMESPACE);
     public static final QName IM_GET_PRIVACY_LIST_RESPONSE = QName.get("IMGetPrivacyListResponse", NAMESPACE);
