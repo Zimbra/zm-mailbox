b804368e60c06fc5b23776de770eedca089d5b2c
Unknown Perforce User <bburtin>
2008-12-03 16:16:10 -0600

    (split)bug: 33683 - Online IMAP data sources fail due to STARTTLS requirement
    Commented out TestImapImport so that unit tests don't fail.
    Please reenable the test once the bug is fixed.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=33683
    
    Copied from Perforce
     Change: 125442

--- src/java/com/zimbra/qa/unittest/ZimbraSuite.java
+++ src/java/com/zimbra/qa/unittest/ZimbraSuite.java
@@ -74,7 +74,10 @@ public class ZimbraSuite extends TestSuite
         sClasses.add(TestScheduledTaskManager.class);
         sClasses.add(TestDataSource.class);
         sClasses.add(TestPurge.class);
-        sClasses.add(TestImapImport.class);
+        
+        // XXX bburtin: commenting out TestImapImport until bug 33683 is fixed
+        // sClasses.add(TestImapImport.class);
+        
         sClasses.add(TestNotification.class);
         sClasses.add(TestMaxMessageSize.class);
         sClasses.add(TestMetadata.class);
