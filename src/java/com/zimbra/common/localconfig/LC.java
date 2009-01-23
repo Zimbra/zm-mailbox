87ff5cd78340ac3cc2faa12d45b60d2fab6f52bf
Unknown Perforce User <jjzhuang>
2009-01-23 02:05:53 -0600

    bug: 34752,34753
    disable sync of Drafts folder, prevent subfolder creation in yahoo mailbox, remove option to sync-inbox-only.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=34752
    http://bugzilla.zimbra.com/show_bug.cgi?id=34753
    
    Copied from Perforce
     Change: 142333

--- src/java/com/zimbra/common/localconfig/LC.java
+++ src/java/com/zimbra/common/localconfig/LC.java
@@ -380,6 +380,8 @@ public class LC {
     public static final KnownKey javamail_imap_enable_starttls;
     public static final KnownKey javamail_smtp_enable_starttls;
     
+    public static final KnownKey yauth_baseuri;
+    
     public static final KnownKey purge_initial_sleep_time;
     
     public static final KnownKey httpclient_idle_connection_timeout;
@@ -1150,6 +1152,10 @@ public class LC {
 
         javamail_smtp_enable_starttls = new KnownKey("javamail_smtp_enable_starttls");
         javamail_smtp_enable_starttls.setDefault("true");
+        
+	    yauth_baseuri = new KnownKey("yauth_baseuri");
+	    yauth_baseuri.setDefault("https://login.yahoo.com/WSLogin/V1");
+	    yauth_baseuri.setDoc("base uri for yauth");
         
         purge_initial_sleep_time = new KnownKey(
             "purge_initial_sleep_ms", Long.toString(30 * Constants.MILLIS_PER_MINUTE),
