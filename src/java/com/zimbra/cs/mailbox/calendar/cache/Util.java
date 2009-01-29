a1ae6b18ed96ee9247b44cf7e3d062e5423ced39
Unknown Perforce User <jhahm>
2009-01-28 20:41:36 -0600

    (split)bug: 22577
    Fix a problem with calculating the cache time range end time when the last month being cached is a February.  Because the month view and minical displays 6 weeks all the time the cache range must be aligned to this boundary, and depending on the day of week for February 1st this means including up to 2 weeks of March in February month view.  But the old code was only caching up to the first week of march.  This would end up bypassing the cache which is inefficient.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=22577
    
    Copied from Perforce
     Change: 143220

--- src/java/com/zimbra/cs/mailbox/calendar/cache/Util.java
+++ src/java/com/zimbra/cs/mailbox/calendar/cache/Util.java
@@ -41,6 +41,8 @@ public class Util {
             return val2 == null;
     }
 
+    private static final int WEEEKS_IN_MONTH_VIEW = 6;
+
     /**
      * Get start/end times in millis for months range around now.
      * 
@@ -71,22 +73,49 @@ public class Util {
         calStart.add(Calendar.MONTH, monthFrom);
         // go back to Sunday of the week
         int moreDays = calStart.get(Calendar.DAY_OF_WEEK) - Calendar.SUNDAY;
-        // go back one more day, for GMT+nnnn timezones
-        moreDays++;
+        // go back one more day, for GMT+nnnn timezones (those ahead of GMT)
+        ++moreDays;
         calStart.add(Calendar.DAY_OF_YEAR, -1 * moreDays);
         long start = calStart.getTimeInMillis();
 
         Calendar calEnd = (Calendar) cal.clone();
         calEnd.add(Calendar.MONTH, monthFrom + numMonths);
-        calEnd.add(Calendar.DAY_OF_YEAR, -1);  // last day of the last month in range
+        calEnd.add(Calendar.DAY_OF_YEAR, -1);  // beginning (midnight) of last day of the last month in range
+
+        // Adjust for the extra weeks at the bottom of month view.  On a typical calendar showing
+        // one week per line with Sunday of the first day of the week, a month can occupy 4 to 6
+        // lines depending on the day of the week of the 1st of the month and the number of days
+        // in the month.
+        //
+        // There is only one way for the 4 line case: February of non-leap year and the 1st is a
+        // Sunday.  Most months take 5 lines.  The extreme case of 6-line month is a 31-day month
+        // with the 1st falling on Saturday:
+        //
+        // pp pp pp pp pp pp 01
+        // 02 03 04 05 06 07 08
+        // 09 10 11 12 13 14 15
+        // 16 17 18 19 20 21 22
+        // 23 24 25 26 27 28 29
+        // 30 31 nn nn nn nn nn
+        //
+        // The dates marked "pp" are from the previous month and "nn" dates are from the next month.
+        // We can tell how many lines the current month is occupying by adding up the number of days
+        // in the month and the number of "nn" dates, which is the extra days needed to get to the
+        // end of that week.  In the minimum case the sum will be 28 and the maximum case will have
+        // the sum greater than 35.
+        int daysInFinalMonth = calEnd.getActualMaximum(Calendar.DAY_OF_MONTH);
+        int extraDaysToEndOfWeek = Calendar.SATURDAY - calEnd.get(Calendar.DAY_OF_WEEK);
+        int linesOccupied = (daysInFinalMonth + extraDaysToEndOfWeek + 6) / 7;
+        int extraWeeks = Math.max(WEEEKS_IN_MONTH_VIEW - linesOccupied, 0);
+
         // go forward to Saturday of the week
-        moreDays = Calendar.SATURDAY - calEnd.get(Calendar.DAY_OF_WEEK);
-        // go forward one more week because minical shows 5 weeks all the time
-        moreDays += 7;
-        // go forward one more day, for GMT-nnnn timezones
-        moreDays++;
+        moreDays = extraDaysToEndOfWeek;
+        // go forward two more weeks because minical shows 6 weeks all the time
+        moreDays += extraWeeks * 7;
+        // go forward one more day, for GMT-nnnn timezones (those behind GMT)
+        ++moreDays;
         // go forward one more day to cover through the end of the day
-        moreDays++;
+        ++moreDays;
         calEnd.add(Calendar.DAY_OF_YEAR, moreDays);
         long end = Math.max(calEnd.getTimeInMillis(), start);
 
