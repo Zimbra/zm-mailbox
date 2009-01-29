42d77bdff515139a4f4a604a61485c10147046cc
Unknown Perforce User <bburtin>
2009-01-29 15:28:50 -0600

    (split)bug 34814 - Too many blob file descriptors open on dogfood
    MessageCache code now closes the InputStream after
    constructing the MimeMessage.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=34814
    
    Copied from Perforce
     Change: 143302

--- src/java/com/zimbra/cs/mailbox/MessageCache.java
+++ src/java/com/zimbra/cs/mailbox/MessageCache.java
@@ -271,6 +271,10 @@ public class MessageCache {
                 throw ServiceException.FAILURE("IOException while retrieving content for item " + item.getId(), e);
             } catch (MessagingException e) {
                 throw ServiceException.FAILURE("MessagingException while creating MimeMessage for item " + item.getId(), e);
+            } finally {
+                if (!(is instanceof BlobInputStream)) {
+                    ByteUtil.closeStream(is);
+                }
             }
         } else {
             if (ZimbraLog.cache.isDebugEnabled())
@@ -305,7 +309,7 @@ public class MessageCache {
     private static void cacheItem(String key, CacheNode cnode) {
         if (cnode.mSize >= mMaxCacheSize)
             return;
-
+        
         synchronized (mCache) {
             if (ZimbraLog.cache.isDebugEnabled())
                 ZimbraLog.cache.debug("msgcache: caching " + (cnode.mContent != null ? "raw" : "mime") + " message: " + key);
