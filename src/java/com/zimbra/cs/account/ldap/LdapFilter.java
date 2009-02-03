68b358e85a3c4067f41db64606714e342c7263f6
Unknown Perforce User <pshao>
2009-02-03 12:37:21 -0600

    (split)bug: none
    moved printFilters method to LdapFilter.
    
    Copied from Perforce
     Change: 143962

--- src/java/com/zimbra/cs/account/ldap/LdapFilter.java
+++ src/java/com/zimbra/cs/account/ldap/LdapFilter.java
@@ -1,5 +1,8 @@
 package com.zimbra.cs.account.ldap;
 
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.account.Provisioning;
+
 public class LdapFilter {
 
     private static final String FILTER_ACCOUNT_OBJECTCLASS = "(objectclass=zimbraAccount)";
@@ -190,5 +193,192 @@ public class LdapFilter {
         return "(objectclass=zimbraXMPPComponent)";
     }
     
-
+    
+    private static void printFilter(String doc, String usage, String base, String filter) {
+        System.out.println();
+        System.out.println(doc);
+        System.out.println("usage:  " + usage);
+        System.out.println("base:   " + base);
+        System.out.println("filter: " + filter);
+    }
+    
+    private static void printFilters(LdapProvisioning prov) throws ServiceException {
+        // account
+        printFilter("all non system accounts", 
+                    "create account(count account for license)",
+                    prov.getDIT().domainToAccountSearchDN("example.com"),
+                    LdapFilter.allNonSystemAccounts());
+        
+        printFilter("account by foreign principal", 
+                    "general",
+                    prov.getDIT().mailBranchBaseDN(),
+                    LdapFilter.accountByForeignPrincipal("{foreign principal}"));
+        
+        printFilter("account by id",
+                    "general",
+                    prov.getDIT().mailBranchBaseDN(),
+                    LdapFilter.accountById("{account id}"));
+        
+        printFilter("account by email",
+                    "general",
+                    prov.getDIT().mailBranchBaseDN(),
+                    LdapFilter.accountByName("{account email}"));
+        
+        printFilter("admin account by RDN",
+                    "admin account access",
+                    prov.getDIT().adminBaseDN(),
+                    LdapFilter.adminAccountByRDN(prov.getDIT().accountNamingRdnAttr(), "{admin name}"));
+        
+        // calendar resource
+        printFilter("calendar resource by foreign principal",
+                    "general",
+                    prov.getDIT().mailBranchBaseDN(),
+                    LdapFilter.calendarResourceByForeignPrincipal("{foreign principal}"));
+        
+        printFilter("calendar resource by id",
+                    "general",
+                    prov.getDIT().mailBranchBaseDN(),
+                    LdapFilter.calendarResourceById("{calendar resource id}"));
+        
+        printFilter("calendar resource by name",
+                    "general",
+                    prov.getDIT().mailBranchBaseDN(),
+                    LdapFilter.calendarResourceByName("{calendar resource name}"));
+        
+        // cos
+        printFilter("all coses",
+                    "admin console, zmprov",
+                    prov.getDIT().cosBaseDN(),
+                    LdapFilter.allCoses());
+        
+        printFilter("cos by id",
+                    "general",
+                    prov.getDIT().cosBaseDN(),
+                    LdapFilter.cosById("{cos id}"));
+        
+        printFilter("coses have certain server in its server pool",
+                    "delete server (to remove the server from all coses)",
+                    prov.getDIT().cosBaseDN(),
+                    LdapFilter.cosesByMailHostPool("{server name}"));
+        
+        // data source
+        printFilter("all data sources",
+                    "general",
+                    "account DN",
+                    LdapFilter.allDataSources());
+        
+        printFilter("data source by id",
+                    "general",
+                    "account DN",
+                    LdapFilter.dataSourceById("{data source id}"));
+        
+        printFilter("data source by name",
+                    "general",
+                    "account DN",
+                    LdapFilter.dataSourceByName("{data source name}"));
+        
+        // distributuion list
+        printFilter("distributuion list by id",
+                    "general",
+                    prov.getDIT().mailBranchBaseDN(),
+                    LdapFilter.distributionListById("{dist list id}"));
+        
+        printFilter("distributuion list by name",
+                    "general",
+                    prov.getDIT().mailBranchBaseDN(),
+                    LdapFilter.distributionListByName("{dist list name}"));
+        
+        // domain
+        printFilter("all domains",
+                    "admin console, zmprov",
+                    prov.getDIT().domainBaseDN(),
+                    LdapFilter.allDomains());
+        
+        printFilter("domain by id",
+                    "general",
+                    prov.getDIT().domainBaseDN(),
+                    LdapFilter.domainById("{domain id}"));
+        
+        printFilter("domain by krb5 realm",
+                    "general",
+                    prov.getDIT().domainBaseDN(),
+                    LdapFilter.domainByKrb5Realm("{krb5 realm}"));
+        
+        printFilter("domain by name",
+                    "general",
+                    prov.getDIT().domainBaseDN(),
+                    LdapFilter.domainByName("{domain name}"));
+        
+        printFilter("domain by virtual hostname",
+                    "general",
+                    prov.getDIT().domainBaseDN(),
+                    LdapFilter.domainByVirtualHostame("{virtual hostname}"));
+        
+        printFilter("test if a domain label(dot separated segments) exists",
+                    "create domain",
+                    "{parent domain DN}",
+                    LdapFilter.domainLabel());
+        
+        // identity
+        printFilter("all identities",
+                    "general",
+                    "account DN",
+                    LdapFilter.allIdentities());
+        
+        printFilter("identity by name",
+                    "general",
+                    "account DN",
+                    LdapFilter.identityByName("{identity name}"));
+        
+        
+        // mime 
+        printFilter("all mime entries",
+                    "general",
+                    prov.getDIT().mimeBaseDN(),
+                    LdapFilter.allMimeEntries());
+        
+        printFilter("mime entry by mime type",
+                    "general",
+                    prov.getDIT().mimeBaseDN(),
+                    LdapFilter.mimeEntryByMimeType("{mime type}"));
+        
+        // signature
+        printFilter("all signatures",
+                    "general",
+                    "account DN",
+                    LdapFilter.allSignatures());
+        
+        printFilter("signature by id",
+                    "general",
+                    "account DN",
+                    LdapFilter.signatureById("{signature id}"));
+        
+        // server
+        printFilter("all servers", 
+                    "admin console, zmprov",
+                    prov.getDIT().serverBaseDN(), 
+                    LdapFilter.allServers());
+        
+        printFilter("server by id",
+                    "general",
+                    prov.getDIT().serverBaseDN(),
+                    LdapFilter.serverById("{server id}"));
+        
+        printFilter("server by service",
+                    "general",
+                    prov.getDIT().serverBaseDN(),
+                    LdapFilter.serverByService("{service}"));
+        
+        // zimlet
+        printFilter("all zimlets",
+                    "general",
+                    prov.getDIT().zimletBaseDN(),
+                    LdapFilter.allZimlets());
+        
+    }
+    
+    public static void main(String[] args) throws Exception {
+        LdapProvisioning prov = (LdapProvisioning)Provisioning.getInstance();
+        printFilters(prov);
+    }
 }
