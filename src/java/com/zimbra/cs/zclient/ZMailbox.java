b98bd7a3b7a4b0f9a83f03c52eb8b460555ad83c
Unknown Perforce User <ssugumaran>
2009-02-12 14:23:16 -0600

    (split)bug: 34769 inline cal related search request to speed up calendar app loading
    works when loaded with ? app=calendar
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=34769
    
    Copied from Perforce
     Change: 145503

--- src/java/com/zimbra/cs/zclient/ZMailbox.java
+++ src/java/com/zimbra/cs/zclient/ZMailbox.java
@@ -107,6 +107,7 @@ import java.net.URLEncoder;
 import java.security.cert.CertificateException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -987,6 +988,121 @@ public class ZMailbox implements ToZJSONObject {
             return debug.getEnvelope();
         } catch (IOException e) {
             throw ZClientException.IO_ERROR("invoke "+e.getMessage(), e);
+        }
+    }
+
+    /**
+     * used when bootstrapping AJAX client.
+     *
+     * @param url url to connect to
+     * @param authToken auth token to use
+     * @param itemsPerPage number of search items to return
+     * @param searchTypes what to search for
+     * @return top-level JSON respsonse
+     * @throws ServiceException on error
+     */
+    public static Element getBootstrapCalSearchJSON(String url, String remoteAddr, ZAuthToken authToken, String itemsPerPage, String searchTypes) throws ServiceException {
+        ZMailbox.Options options = new ZMailbox.Options(authToken, url);
+        JsonDebugListener debug = new JsonDebugListener();
+        options.setNoSession(false);
+        options.setAuthAuthToken(false);
+        options.setDebugListener(debug);
+        options.setClientIp(remoteAddr);
+        options.setResponseProtocol(SoapProtocol.SoapJS);
+
+        ZMailbox mbox = getMailbox(options);
+        try {
+            TimeZone tz = mbox.getPrefs().getTimeZone();
+
+            Calendar currentDay = tz == null ? Calendar.getInstance() : Calendar.getInstance(tz);
+            currentDay.setTimeInMillis(System.currentTimeMillis());
+            currentDay.set(Calendar.HOUR_OF_DAY, 0);
+            currentDay.set(Calendar.MINUTE, 0);
+            currentDay.set(Calendar.SECOND, 0);
+            currentDay.set(Calendar.MILLISECOND, 0);
+            currentDay.set(Calendar.DAY_OF_MONTH, 1);
+            
+            StringBuilder sb = new StringBuilder();
+            getCheckedCalendarFoldersRecursive(mbox.getUserRoot(), sb);
+            String checkedCalendars = sb.toString();
+
+            Calendar other = Calendar.getInstance(currentDay.getTimeZone());
+            other.setTimeInMillis(currentDay.getTimeInMillis());
+            //7 days for reminder search and 1 day for timezone difference
+            other.add(Calendar.DAY_OF_MONTH, -8);
+
+            long calStart = other.getTimeInMillis();
+
+            other.setTimeInMillis(currentDay.getTimeInMillis());
+            //no of days shown for minical
+            other.add(Calendar.DAY_OF_MONTH, 42);
+
+            long calEnd = other.getTimeInMillis();
+
+            //BatchRequest
+            Element batch = new Element.JSONElement(ZimbraNamespace.E_BATCH_REQUEST);
+
+            //GetMiniCalRequest
+            Element miniCalRequest = batch.addElement(MailConstants.GET_MINI_CAL_REQUEST);
+            miniCalRequest.addAttribute(MailConstants.A_CAL_START_TIME, calStart);
+            miniCalRequest.addAttribute(MailConstants.A_CAL_END_TIME, calEnd);
+
+            String [] sArray = null;
+            StringBuilder searchQuery = new StringBuilder();
+
+            if (checkedCalendars!=null) {
+                if(checkedCalendars.indexOf(",") == -1){
+                    sArray = new String[]{checkedCalendars};
+                }else{
+                    sArray = checkedCalendars.split(",");
+                }
+                for(int i=0; i<sArray.length; i++) {
+                    Element folder = miniCalRequest.addElement(MailConstants.E_FOLDER);
+                    folder.addAttribute(MailConstants.A_ID, sArray[i]);
+                    if (searchQuery.length() > 1) searchQuery.append(" or ");
+                    searchQuery.append("inid:").append(sArray[i]);
+                }
+            }
+
+            //SearchRequest
+            Element search = batch.addElement(MailConstants.SEARCH_REQUEST);
+            if (itemsPerPage != null && itemsPerPage.length() > 0)
+                search.addAttribute(MailConstants.A_QUERY_LIMIT, itemsPerPage);
+            if (searchTypes != null && searchTypes.length() > 0)
+                search.addAttribute(MailConstants.A_SEARCH_TYPES, searchTypes);
+            search.addAttribute(MailConstants.A_CAL_EXPAND_INST_START, calStart);
+            search.addAttribute(MailConstants.A_CAL_EXPAND_INST_END, calEnd);
+            search.addAttribute(MailConstants.A_QUERY_OFFSET, 0);
+
+            Element queryEl = search.addElement(MailConstants.E_QUERY);
+            queryEl.setText(searchQuery.toString());
+
+            Element resp = mbox.mTransport.invoke(batch);
+
+            Element e = debug.getEnvelope();
+
+            //search params included in response
+            Element responseParams = e.addElement(MailConstants.E_SEARCH);
+            responseParams.addAttribute(MailConstants.A_CAL_START_TIME, calStart);
+            responseParams.addAttribute(MailConstants.A_CAL_END_TIME, calEnd);
+            Element queryElement = responseParams.addElement(MailConstants.A_QUERY);
+            queryElement.setText(searchQuery.toString());
+            responseParams.addAttribute(MailConstants.A_FOLDER, checkedCalendars);
+            
+            return e;
+
+        } catch (IOException e) {
+            throw ZClientException.IO_ERROR("invoke "+e.getMessage(), e);
+        }
+    }
+
+    private static void getCheckedCalendarFoldersRecursive(ZFolder f, StringBuilder sb) {
+        if (f.getDefaultView() == ZFolder.View.appointment && f.isCheckedInUI()) {
+            if (sb.length() > 0) sb.append(',');
+            sb.append(f.getId());
+        }
+        for (ZFolder child : f.getSubFolders()) {
+            getCheckedCalendarFoldersRecursive(child, sb);
         }
     }
 
