38a4158028f607f1d227728441cf0fba6681fbef
Unknown Perforce User <jylee>
2009-02-25 20:35:29 -0600

    (split)bug 35591 add xprop X-APPLE-NEEDS-REPLY when the user's PARTSTAT=NEEDS-ACTION and RSVP=TRUE in the invite.
    http://bugzilla.zimbra.com/show_bug.cgi?id=35591
    
    Copied from Perforce
     Change: 147314

--- src/java/com/zimbra/cs/dav/resource/CalendarObject.java
+++ src/java/com/zimbra/cs/dav/resource/CalendarObject.java
@@ -27,6 +27,7 @@ import java.util.Iterator;
 
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.AuthToken;
 import com.zimbra.cs.dav.DavContext;
 import com.zimbra.cs.dav.DavElements;
@@ -38,8 +39,10 @@ import com.zimbra.cs.mailbox.Folder;
 import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.cs.mailbox.Mailbox.OperationContext;
 import com.zimbra.cs.mailbox.calendar.ICalTimeZone;
+import com.zimbra.cs.mailbox.calendar.IcalXmlStrMap;
 import com.zimbra.cs.mailbox.calendar.Invite;
 import com.zimbra.cs.mailbox.calendar.TimeZoneMap;
+import com.zimbra.cs.mailbox.calendar.ZAttendee;
 import com.zimbra.cs.mailbox.calendar.ZCalendar;
 import com.zimbra.cs.mime.Mime;
 import com.zimbra.cs.service.AuthProvider;
@@ -169,6 +172,7 @@ public interface CalendarObject {
                 buf.append(wr.toCharArray());
                 wr.close();
             }
+            Account acct = ctxt.getAuthAccount();
             for (Invite inv : mInvites) {
                 CharArrayWriter wr = new CharArrayWriter();
                 try {
@@ -177,6 +181,12 @@ public interface CalendarObject {
                     Folder folder = mbox.getFolderById(octxt, mFolderId);
                     boolean allowPrivateAccess = CalendarItem.allowPrivateAccess(
                             folder, ctxt.getAuthAccount(), octxt.isUsingAdminPrivileges());
+                    ZAttendee attendee = inv.getMatchingAttendee(acct);
+                    if (attendee != null && attendee.hasRsvp() && attendee.getRsvp() && IcalXmlStrMap.PARTSTAT_NEEDS_ACTION.equals(attendee.getPartStat())) {
+                    	ZCalendar.ZProperty prop = new ZCalendar.ZProperty("X-APPLE-NEEDS-REPLY");
+                    	prop.setValue("TRUE");
+                    	inv.addXProp(prop);
+                    }
                     ZCalendar.ZComponent vcomp = inv.newToVComponent(false, allowPrivateAccess);
                     if (filter != null && !filter.match(vcomp))
                         continue;
