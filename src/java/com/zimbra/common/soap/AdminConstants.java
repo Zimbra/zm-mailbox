cbdb7bc9b483fa4b11543f24ed88497a46d09c42
Unknown Perforce User <pshao>
2009-03-12 19:47:30 -0500

    bug: 18277
    Generate rights for core admin commands to rights.txt
    Generate rights for admin extensions to rights-ext.txt.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=18277
    
    Copied from Perforce
     Change: 149367

--- src/java/com/zimbra/common/soap/AdminConstants.java
+++ src/java/com/zimbra/common/soap/AdminConstants.java
@@ -619,6 +619,7 @@ public class AdminConstants {
     public static final String E_MAX         = "max";
     public static final String E_MIN         = "min";
     public static final String E_NOTE        = "note";
+    public static final String E_PACKAGE     = "package";
     public static final String E_RIGHTS      = "rights";
     public static final String E_R           = "r";
     public static final String E_RIGHT       = "right";
