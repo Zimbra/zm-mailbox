0b832eabf804897e53bf10274780a4d1bd46337c
Quanah Gibson-Mount <quanah@zimbra.com>
2009-03-14 20:34:34 -0500

    bug: 34634 Update copyright and licenses
    http://bugzilla.zimbra.com/show_bug.cgi?id=34634
    
    Copied from Perforce
     Change: 149622

--- src/native/Process.c
+++ src/native/Process.c
@@ -1,9 +1,8 @@
 /* -*- Mode: c; c-basic-offset: 4 -*- */
 /*
  * ***** BEGIN LICENSE BLOCK *****
- * 
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2005 Zimbra, Inc.
+ * Copyright (C) 2005, 2007 Zimbra, Inc.
  * 
  * The contents of this file are subject to the Yahoo! Public License
  * Version 1.0 ("License"); you may not use this file except in
@@ -12,7 +11,6 @@
  * 
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
- * 
  * ***** END LICENSE BLOCK *****
  */
 #include <jni.h>
