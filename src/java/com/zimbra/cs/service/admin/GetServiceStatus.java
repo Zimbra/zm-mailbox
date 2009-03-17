43d9289cca0184ea3d85533ea9c25c2e037ee05c
Unknown Perforce User <pfnguyen>
2009-03-16 21:38:57 -0500

    (split)bug:36039
    GetServiceStatus.java: use zmrrdfetch to retrieve server status data
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=36039
    
    Copied from Perforce
     Change: 149869

--- src/java/com/zimbra/cs/service/admin/GetServiceStatus.java
+++ src/java/com/zimbra/cs/service/admin/GetServiceStatus.java
@@ -18,31 +18,39 @@
  */
 package com.zimbra.cs.service.admin;
 
-import java.util.Iterator;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.AdminConstants;
 import com.zimbra.common.soap.Element;
 import com.zimbra.common.soap.SoapFaultException;
+import com.zimbra.common.util.CsvReader;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.Server;
 import com.zimbra.cs.account.Provisioning.ServerBy;
 import com.zimbra.cs.account.accesscontrol.AdminRight;
-import com.zimbra.cs.db.DbPool;
-import com.zimbra.cs.db.DbServiceStatus;
-import com.zimbra.cs.db.DbPool.Connection;
 import com.zimbra.soap.ZimbraSoapContext;
 
 /**
  * @author schemers
  */
 public class GetServiceStatus extends AdminDocumentHandler {
+    private final static String ZMRRDFETCH = LC.zimbra_home.value() + "/libexec/zmrrdfetch";
+    private final static String ZMSTATUSLOG_CSV = "zmstatuslog";
 
-	public Element handle(Element request, Map<String, Object> context) throws SoapFaultException, ServiceException {
+    public Element handle(Element request, Map<String, Object> context) throws SoapFaultException, ServiceException {
         ZimbraSoapContext zsc = getZimbraSoapContext(context);
-        
+
         // allow only system admin for now
         checkRight(zsc, context, null, AdminRight.PR_SYSTEM_ADMIN_ONLY);
 
@@ -62,38 +70,110 @@ public class GetServiceStatus extends AdminDocumentHandler {
         Server local = prov.getLocalServer();
         String[] services = local.getMultiAttr(Provisioning.A_zimbraServiceEnabled);
         if (services != null) {
-            for (int i = 0; i < services.length; i++) {
-                if ("logger".equals(services[i])) {
-                    loggerEnabled = true;
-                    break;
-                }
+            for (int i = 0; i < services.length && !loggerEnabled; i++) {
+                loggerEnabled = "logger".equals(services[i]);
             }
         }
         if (loggerEnabled) {
-    	    Connection conn = null;
-            try { 
-                conn = DbPool.getLoggerConnection();
-                List stats = DbServiceStatus.getStatus(conn.getConnection());
-                doServiceStatus(response, stats);            
-            } finally {
-                DbPool.quietClose(conn);
+            BufferedReader in = null;
+            try {
+                ProcessBuilder pb = new ProcessBuilder(ZMRRDFETCH, "-f", ZMSTATUSLOG_CSV);
+                Process p = pb.start();
+                in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                Map<String,CsvReader> hostStatus = new HashMap<String,CsvReader>();
+                StringWriter currentWriter = null;
+                String currentHost = null;
+                String line;
+                while ((line = in.readLine()) != null) {
+                    if ("".equals(line.trim())) continue;
+                    if (line.startsWith("Host: ")) {
+                        if (currentHost != null)
+                            hostStatus.put(currentHost, new CsvReader(
+                                    new StringReader(currentWriter.toString())));
+                        currentHost   = line.substring("Host: ".length());
+                        currentWriter = new StringWriter();
+                    } else {
+                        if (currentWriter != null)
+                            currentWriter.write(line + "\n");
+                    }
+                }
+                if (currentHost != null && currentWriter != null)
+                    hostStatus.put(currentHost, new CsvReader(
+                            new StringReader(currentWriter.toString())));
+                List<ServiceStatus> status = ServiceStatus.parseData(hostStatus);
+                for (ServiceStatus stat : status) {
+                    Element s = response.addElement(AdminConstants.E_STATUS);
+                    s.addAttribute(AdminConstants.A_SERVER, stat.server);
+                    s.addAttribute(AdminConstants.A_SERVICE, stat.service);
+                    s.addAttribute(AdminConstants.A_T, stat.time);
+                    s.setText(stat.status);
+                }
+            }
+            catch (IOException e) {
+                try {
+                    if (in != null) in.close();
+                }
+                catch (IOException x) { }
+                ServiceException.FAILURE("Unable to read logger stats", e);
             }
         }
-	    return response;
-	}
+        return response;
+    }
 
-	// <status server="..." service="..." t="...">{status}<status/>
-    public static void doServiceStatus(Element e, List stats) {
-        for (Iterator it = stats.iterator(); it.hasNext(); ) {
-            DbServiceStatus stat = (DbServiceStatus) it.next();
-            Element s = e.addElement(AdminConstants.E_STATUS);
-            s.addAttribute(AdminConstants.A_SERVER, stat.getServer());
-            s.addAttribute(AdminConstants.A_SERVICE, stat.getService());
-            s.addAttribute(AdminConstants.A_T, stat.getTime());
-            s.setText(Integer.toString(stat.getStatus()));
+    private static class ServiceStatus {
+        String server;
+        String service;
+        String time;    // seconds since epoch
+        String status;  // "1" or "0"
+        
+        static List<ServiceStatus> parseData(Map<String,CsvReader> data)
+        throws IOException {
+            List<ServiceStatus> results = new ArrayList<ServiceStatus>();
+            for (String host : data.keySet()) {
+                CsvReader r = data.get(host);
+                ArrayList<String> columns = new ArrayList<String>(
+                        Arrays.asList(r.getColNames()));
+                columns.remove("timestamp");
+                Map<String,String> row = new HashMap<String,String>();
+                String lastTS = null;
+                
+                while (r.hasNext()) {
+                    String ts = r.getValue("timestamp");
+                    boolean rowHasData = false;
+                    for (String column : columns) {
+                        String value = r.getValue(column);
+                        rowHasData = rowHasData || value != null;
+                    }
+                    if (rowHasData) {
+                        lastTS = ts;
+                        row.clear();
+                        for (String column : columns) {
+                            String value = r.getValue(column);
+                            if (value != null)
+                                row.put(column, value);
+                        }
+                    }
+                }
+                if (lastTS != null) {
+                    for (String service : row.keySet()) {
+                        String status = row.get(service);
+                        if (status != null) {
+                            ServiceStatus s = new ServiceStatus();
+                            s.server  = host;
+                            s.time    = lastTS;
+                            s.service = service;
+                            // 0.8 check because of rrd rounding,
+                            // happens when a service has just started
+                            s.status = Float.parseFloat(status) > 0.8 ? "1" : "0";
+                            results.add(s);
+                        }
+                    }
+                }
+            }
+            return results;
         }
     }
-    
+
     @Override
     protected void docRights(List<AdminRight> relatedRights, List<String> notes) {
         notes.add(sDocRightNotesSystemAdminsOnly);
