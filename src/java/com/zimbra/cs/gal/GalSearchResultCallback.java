a2078ea14a5977537ca554154437b23558baf938
Unknown Perforce User <jylee>
2009-03-20 20:40:47 -0500

    (split)bug 14531 checkpoint
    http://bugzilla.zimbra.com/show_bug.cgi?id=14531
    
    Copied from Perforce
     Change: 150568

---
+++ src/java/com/zimbra/cs/gal/GalSearchResultCallback.java
@@ -0,0 +1,62 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2009 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Yahoo! Public License
+ * Version 1.0 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.gal;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.soap.Element;
+import com.zimbra.common.soap.MailConstants;
+import com.zimbra.cs.mailbox.Contact;
+import com.zimbra.cs.service.mail.ToXML;
+import com.zimbra.cs.service.util.ItemIdFormatter;
+
+public class GalSearchResultCallback {
+    private Element mResponse;
+	private ItemIdFormatter mFormatter;
+    
+    public GalSearchResultCallback(GalSearchParams params, Element resp) {
+    	mResponse = resp;
+    	mFormatter = new ItemIdFormatter(params.getSoapContext());
+    }
+    
+    public void reset() {
+    	mResponse = null;
+    	mFormatter = null;
+    }
+    
+    public Element getResponse() {
+    	return mResponse;
+    }
+    
+    public void handleContact(Contact c) throws ServiceException {
+		ToXML.encodeContact(mResponse, mFormatter, c, true, null);
+    }
+    
+    public void handleElement(Element e) {
+    	mResponse.addElement(e.detach());
+    }
+    
+    public void setNewToken(int newToken) {
+    	mResponse.addAttribute(MailConstants.A_TOKEN, newToken);
+    }
+    public void setSortBy(String sortBy) {
+        mResponse.addAttribute(MailConstants.A_SORTBY, sortBy);
+    }
+    public void setQueryOffset(int offset) {
+        mResponse.addAttribute(MailConstants.A_QUERY_OFFSET, offset);
+    }
+    public void setHasMoreResult(boolean more) {
+        mResponse.addAttribute(MailConstants.A_QUERY_MORE, more);
+    }
+}
