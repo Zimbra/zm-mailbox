a9ed34a75c1567e3368bc4a1f2e2ca68d3cd641d
Unknown Perforce User <pshao>
2009-03-30 13:21:15 -0500

    bug: 36683
    - trim extra space around getAttrs, setAttrs in GetEffectiveRightsRequest.
    
    - corrected an typo on the variable name (not bug).
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=36683
    
    Copied from Perforce
     Change: 151618

--- src/java/com/zimbra/common/soap/AdminConstants.java
+++ src/java/com/zimbra/common/soap/AdminConstants.java
@@ -636,7 +636,7 @@ public class AdminConstants {
     public static final String A_ALLOW       = "allow";
     public static final String A_CAN_DELEGATE= "canDelegate";
     public static final String A_DENY        = "deny";
-    public static final String A_EXPAND_ALL_ATRTS = "expandAllAttrs";
+    public static final String A_EXPAND_ALL_ATTRS = "expandAllAttrs";
     public static final String A_RIGHT       = "right";
     public static final String A_TARGET_TYPE = "targetType";
     
