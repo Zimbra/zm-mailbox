8ecb5ae84862358961baf261ddd9cae88be12a22
Unknown Perforce User <tfr>
2009-04-06 20:40:14 -0500

    (split)bug: 36786 exception caused when warning an Execption w/o a cause
    http://bugzilla.zimbra.com/show_bug.cgi?id=36786
    
    Copied from Perforce
     Change: 152793

--- src/java/com/zimbra/cs/service/formatter/TarFormatter.java
+++ src/java/com/zimbra/cs/service/formatter/TarFormatter.java
@@ -193,7 +193,7 @@ public class TarFormatter extends Formatter {
                     for (MailItem mi : context.respListItems)
                         tos = saveItem(context, mi, fldrs, cnts, names, false, tos);
                 } catch (Exception e) {
-                    ZimbraLog.misc.warn("%s: %s", e, e.getCause().toString());
+                    warn(e);
                 }
             } else if (context.target != null && !(context.target instanceof
                 Folder)) {
@@ -201,7 +201,7 @@ public class TarFormatter extends Formatter {
                     tos = saveItem(context, context.target, fldrs, cnts, names,
                         false, tos);
                 } catch (Exception e) {
-                    ZimbraLog.misc.warn("%s: %s", e, e.getCause().toString());
+                    warn(e);
                 }
             } else {
                 ZimbraQueryResults results = null;
@@ -248,7 +248,7 @@ public class TarFormatter extends Formatter {
                                 false, tos);
                     }
                 } catch (Exception e) {
-                    ZimbraLog.misc.warn("%s: %s", e, e.getCause().toString());
+                    warn(e);
                 } finally {
                     if (results != null)
                         results.doneWithSearchResults();
@@ -785,6 +785,13 @@ public class TarFormatter extends Formatter {
     }
 
     private String string(String s) { return s == null ? new String() : s; }
+
+    private void warn(Exception e) {
+        if (e.getCause() == null)
+            ZimbraLog.misc.warn("TarFormatter warning: %s", e);
+        else
+            ZimbraLog.misc.warn("TarFormatter warning: %s: %s", e, e.getCause().toString());
+    }
 
     private void addItem(Context context, Folder fldr,
         Map<Object, Folder> fmap, Map<String, Integer> digestMap,
