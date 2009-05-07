014f16bcfdde4a247f6d3a53e15776185a60a69e
Unknown Perforce User <bburtin>
2009-05-07 18:12:06 -0500

    bug: 36173 - make zimbraSmtpHostname fault tolerant
    When we fail to connect to the SMTP server,
    we now cycle through the list of known SMTP hosts
    and try the others.  The bad host is taken out of the
    server's cached host list and not retried for another
    60 seconds.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=36173
    
    Copied from Perforce
     Change: 157119

--- src/java/com/zimbra/common/localconfig/LC.java
+++ src/java/com/zimbra/common/localconfig/LC.java
@@ -477,6 +477,8 @@ public class LC {
     
     public static final KnownKey contact_ranking_enabled;
     
+    public static final KnownKey smtp_host_retry_millis;
+    
     static {
         final String ZM_MYCNF_CAVEAT = "This value is stored here for use by zmmycnf program.  " +
                 "Changing this setting does not immediately reflect in MySQL server.  " +
@@ -1387,6 +1389,8 @@ public class LC {
         zmailbox_message_cachesize = new KnownKey("zmailbox_message_cachesize", "1", "max number of messages cached in zmailbox");
               
         contact_ranking_enabled = new KnownKey("contact_ranking_enabled", "true", "Enable contact ranking table");
+        
+        smtp_host_retry_millis = new KnownKey("smtp_host_retry_millis", "60000");
         
 		// NOTE: When adding a new KnownKey, you do not need to call
 		//       setDoc. The documentation string will come from the
