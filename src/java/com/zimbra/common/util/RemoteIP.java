20789835c1800e17a862e2fb1ce26465d54fc7b4
Unknown Perforce User <pshao>
2009-05-26 17:05:02 -0500

    bug: none
    delete dead comment
    
    Copied from Perforce
     Change: 159347

--- src/java/com/zimbra/common/util/RemoteIP.java
+++ src/java/com/zimbra/common/util/RemoteIP.java
@@ -64,26 +64,6 @@ public class RemoteIP {
             ZimbraLog.addIpToContext(mClientIP);
     }
     
-    /*
-    private static class TrustedNetwork {
-        
-        
-        // returns if an ip is in trusted network
-        public static boolean isIpTrusted(String ip) {
-            if (StringUtil.isNullOrEmpty(ip))
-                return false;
-            
-            // For now the only trusted ip is localhost
-            return isLocalhost(ip);
-        }
-
-        public static boolean isLocalhost(String ip) {
-            return IP_LOCALHOST.equals(ip);
-        }
-
-    }
-    */
-    
     public static class TrustedIPs {
         private static final String IP_LOCALHOST = "127.0.0.1"; 
         
