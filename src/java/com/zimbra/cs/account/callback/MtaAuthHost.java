dc0c4436cfcae5d69b7e2648a2d3ad2ee9ef2552
Unknown Perforce User <pshao>
2009-05-28 23:46:45 -0500

    (split)bug: 23404
    fixed issue with MtaAuthHost callback:
    
    - it had been treated as a singled-valued attr by the callback
    - when removing a ZimbraMtaAuthHost value, if the server no longer exists, just proceed to remove the ZimbraMtaAuthHost value, and any corresponding ZimbraMtaAuthUrl with the same host name. 
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=23404
    
    Copied from Perforce
     Change: 159685

--- src/java/com/zimbra/cs/account/callback/MtaAuthHost.java
+++ src/java/com/zimbra/cs/account/callback/MtaAuthHost.java
@@ -15,9 +15,16 @@
 
 package com.zimbra.cs.account.callback;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.account.AttributeCallback;
 import com.zimbra.cs.account.Entry;
 import com.zimbra.cs.account.Provisioning;
@@ -30,13 +37,71 @@ public class MtaAuthHost extends AttributeCallback {
      */
     public void preModify(Map context, String attrName, Object value,
                           Map attrsToModify, Entry entry, boolean isCreate) throws ServiceException {
-        if (!(value instanceof String))
-            throw ServiceException.INVALID_REQUEST(Provisioning.A_zimbraMtaAuthHost + " is a single-valued attribute", null);
+
+        MultiValueMod mod = multiValueMod(attrsToModify, Provisioning.A_zimbraMtaAuthHost);
+        Set<String> values = mod.valuesSet();
         
-        // Set zimbraMtaAuthURL when zimbraMtaAuthHost is set
-        String authHost = (String) value;
-        String url = URLUtil.getMtaAuthURL(authHost);
-        attrsToModify.put(Provisioning.A_zimbraMtaAuthURL, url);
+        if (mod.adding()) {
+            // Add zimbraMtaAuthURL for each auth host being added
+            List<String> urlsToAdd = new ArrayList<String>();
+            for (String authHost : values) {
+                String authUrl = URLUtil.getMtaAuthURL(authHost);
+                urlsToAdd.add(authUrl);
+            }
+            if (urlsToAdd.size() > 0)
+                attrsToModify.put("+" + Provisioning.A_zimbraMtaAuthURL, urlsToAdd.toArray(new String[urlsToAdd.size()]));
+            
+        } else if (mod.replacing()) {
+            // Replace zimbraMtaAuthURL for each auth host being replaced
+            List<String> urls = new ArrayList<String>();
+            for (String authHost : values) {
+                String authUrl = URLUtil.getMtaAuthURL(authHost);
+                urls.add(authUrl);
+            }
+            if (urls.size() > 0)
+                attrsToModify.put(Provisioning.A_zimbraMtaAuthURL, urls.toArray(new String[urls.size()]));
+            
+        } else if (mod.removing()) {
+            // Remove zimbraMtaAuthURL for each auth host being removed,
+            // if the auth host server to be remove no longer exists, just catch the Exception and 
+            // remove the corresponding auth url if there is one
+            
+            if (!isCreate && entry != null) {
+                Set<String> curUrls = entry.getMultiAttrSet(Provisioning.A_zimbraMtaAuthURL);
+                List<String> urlsToRemove = new ArrayList<String>();
+                
+                for (String authHost : values) {
+                    try {
+                        String authUrl = URLUtil.getMtaAuthURL(authHost);
+                        if (curUrls.contains(authUrl))
+                            urlsToRemove.add(authUrl);
+                        
+                    } catch (ServiceException e) {
+                        for (String curUrl : curUrls) {
+                            try {
+                                URL url = new URL(curUrl);
+                                String urlHost = url.getHost();
+                                // just compare the urlHost with the string of the host
+                                // we are removing, there is no way to get the A_zimbraServiceHostname
+                                // of the server because the server no longer exists
+                                if (authHost.equals(urlHost))
+                                    urlsToRemove.add(curUrl);
+                            } catch (MalformedURLException mue) {
+                                // hmm, mailformed url? just remove it
+                                urlsToRemove.add(curUrl);
+                            }
+                        }
+                    }
+                }
+                if (urlsToRemove.size() > 0)
+                    attrsToModify.put("-" + Provisioning.A_zimbraMtaAuthURL, urlsToRemove.toArray(new String[urlsToRemove.size()]));
+            }
+
+        } else if (mod.deleting()) {
+            // delete all the zimbraMtaAuthURL values 
+            attrsToModify.put(Provisioning.A_zimbraMtaAuthURL, null);
+        }
+
     }
 
     /**
