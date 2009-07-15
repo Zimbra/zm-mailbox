4b4a8aea9a4c0a61229a7149f101480103afeffa
Unknown Perforce User <bburtin>
2009-07-15 16:44:01 -0500

    Integrated change 138154 from main to FRANKLIN.
    ---------------
    
    Change 138154 by bburtin@bburtin-ubuntu-main on 2008/12/19 11:39:21
    
    	bug 32027 - Log context not cleared in some cases
    
    	Added call to ZimbraLog.clearContext() from SetHeaderFilter.doFilter(), which
    	runs before every servlet request.  Updated ZimbraLog.clearContext() to
    	not implicitly instantiate the set of account names.
    
    
    Affected files ...
    
    ... //depot/main/ZimbraCommon/src/java/com/zimbra/common/util/ZimbraLog.java#44 edit
    ... //depot/main/ZimbraWebClient/src/com/zimbra/webClient/filters/SetHeaderFilter.java#29 edit
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=32027
    
    Copied from Perforce
     Change: 165742

--- src/java/com/zimbra/common/util/ZimbraLog.java
+++ src/java/com/zimbra/common/util/ZimbraLog.java
@@ -580,7 +580,10 @@ public class ZimbraLog {
         if (contextMap != null) {
             contextMap.clear();
         }
-        getAccountNamesForThread().clear();
+        Set<String> accountNames = sAccountNames.get();
+        if (accountNames != null) {
+            accountNames.clear();
+        }
         sContextString.remove();
     }
 
