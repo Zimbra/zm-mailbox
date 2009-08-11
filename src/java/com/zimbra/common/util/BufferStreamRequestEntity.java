78b0e0be7bcb394c297a8bc4687a3bcd160d8a08
Unknown Perforce User <tfr>
2009-08-10 19:13:02 -0500

    bug: 39909 optimize DataSource import
    http://bugzilla.zimbra.com/show_bug.cgi?id=39909
    
    Copied from Perforce
     Change: 168921

--- src/java/com/zimbra/common/util/BufferStreamRequestEntity.java
+++ src/java/com/zimbra/common/util/BufferStreamRequestEntity.java
@@ -24,17 +24,21 @@ import org.apache.commons.httpclient.methods.RequestEntity;
 
 public class BufferStreamRequestEntity extends BufferStream implements
     RequestEntity {
-    String contentType = null;
+    String contentType;
     private InputStream is = null;
 
-    public BufferStreamRequestEntity() { this(null, 0); }
+    public BufferStreamRequestEntity() { this(0); }
+
+    public BufferStreamRequestEntity(long sizeHint) {
+        this(sizeHint, Integer.MAX_VALUE);
+    }
 
     public BufferStreamRequestEntity(long sizeHint, int maxBuffer) {
-        this(null, sizeHint, maxBuffer);
+        this(sizeHint, maxBuffer, Long.MAX_VALUE);
     }
 
     public BufferStreamRequestEntity(long sizeHint, int maxBuffer, long maxSize) {
-        this(null, sizeHint, maxBuffer, maxSize);
+        this(null, null, sizeHint, maxBuffer, maxSize);
     }
 
     public BufferStreamRequestEntity(InputStream is) { this(is, 0); }
@@ -44,12 +48,38 @@ public class BufferStreamRequestEntity extends BufferStream implements
     }
 
     public BufferStreamRequestEntity(InputStream is, long sizeHint, int maxBuffer) {
-        this(is, sizeHint, maxBuffer, Long.MAX_VALUE);
+        this(is, null, sizeHint, maxBuffer, Long.MAX_VALUE);
     }
 
-    public BufferStreamRequestEntity(InputStream is, long sizeHint, int
-        maxBuffer, long maxSize) {
+    public BufferStreamRequestEntity(InputStream is, String contentType) {
+        this(is, contentType, 0);
+    }
+
+    public BufferStreamRequestEntity(InputStream is, String contentType, long
+        sizeHint) {
+        this(is, contentType, sizeHint, Integer.MAX_VALUE);
+    }
+
+    public BufferStreamRequestEntity(InputStream is, String contentType,
+        long sizeHint, int maxBuffer) {
+        this(is, contentType, sizeHint, maxBuffer, Long.MAX_VALUE);
+    }
+
+    public BufferStreamRequestEntity(String contentType) { this(contentType, 0); }
+
+    public BufferStreamRequestEntity(String contentType, long sizeHint) {
+        this(contentType, sizeHint, Integer.MAX_VALUE);
+    }
+
+    public BufferStreamRequestEntity(String contentType, long sizeHint, int
+        maxBuffer) {
+        this(null, contentType, sizeHint, maxBuffer, Long.MAX_VALUE);
+    }
+
+    public BufferStreamRequestEntity(InputStream is, String contentType, long
+        sizeHint, int maxBuffer, long maxSize) {
         super(sizeHint, maxBuffer, maxSize);
+        this.contentType = contentType;
         this.is = is;
     }
 
