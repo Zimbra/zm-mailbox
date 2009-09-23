1311f874f92f0b3ba6cc3e8ec7026805a4c5e735
Unknown Perforce User <tfr>
2009-09-23 18:38:40 -0500

    (split)bug: 41168 incorrect folder check with contacts & chats
    http://bugzilla.zimbra.com/show_bug.cgi?id=41168
    
    Copied from Perforce
     Change: 212185

--- src/java/com/zimbra/cs/service/formatter/ArchiveFormatter.java
+++ src/java/com/zimbra/cs/service/formatter/ArchiveFormatter.java
@@ -957,7 +957,7 @@ public abstract class ArchiveFormatter extends Formatter {
                     
                     try {
                         oldChat = mbox.getChatById(oc, chat.getId());
-                        if (oldChat.getFolderId() != fldr.getFolderId())
+                        if (oldChat.getFolderId() != fldr.getId())
                             oldChat = null;
                     } catch (Exception e) {
                     }
@@ -999,7 +999,7 @@ public abstract class ArchiveFormatter extends Formatter {
                     
                     try {
                         oldContact = mbox.getContactById(oc, ct.getId());
-                        if (oldContact.getFolderId() != fldr.getFolderId())
+                        if (oldContact.getFolderId() != fldr.getId())
                             oldContact = null;
                     } catch (Exception e) {
                     }
