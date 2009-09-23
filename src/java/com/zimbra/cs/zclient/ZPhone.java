501ee3a58c331c49f40e7bfc30ac8db66aac841c
Unknown Perforce User <lars>
2009-09-23 09:09:47 -0500

    (split)bug: 40923
    implemented "add to forward/reject list" in html
    working on a prettier solution...
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=40923
    
    Copied from Perforce
     Change: 212125

--- src/java/com/zimbra/cs/zclient/ZPhone.java
+++ src/java/com/zimbra/cs/zclient/ZPhone.java
@@ -23,49 +23,49 @@ import java.util.regex.Pattern;
 
 public class ZPhone implements ToZJSONObject {
 
-	public static final String INVALID_PHNUM_OWN_PHONE_NUMBER = "voice.INVALID_PHNUM_OWN_PHONE_NUMBER";
-	public static final String INVALID_PHNUM_INTERNATIONAL_NUMBER = "voice.INVALID_PHNUM_INTERNATIONAL_NUMBER";
-	public static final String INVALID_PHNUM_BAD_NPA = "voice.INVALID_PHNUM_BAD_NPA";
-	public static final String INVALID_PHNUM_BAD_LINE = "voice.INVALID_PHNUM_BAD_LINE";
-	public static final String INVALID_PHNUM_EMERGENCY_ASSISTANCE = "voice.INVALID_PHNUM_EMERGENCY_ASSISTANCE";
-	public static final String INVALID_PHNUM_DIRECTORY_ASSISTANCE = "voice.INVALID_PHNUM_DIRECTORY_ASSISTANCE";
-	public static final String INVALID_PHNUM_BAD_FORMAT = "voice.INVALID_PHNUM_BAD_FORMAT";
-	public static final String VALID = "voice.OK";
+    public static final String INVALID_PHNUM_OWN_PHONE_NUMBER = "voice.INVALID_PHNUM_OWN_PHONE_NUMBER";
+    public static final String INVALID_PHNUM_INTERNATIONAL_NUMBER = "voice.INVALID_PHNUM_INTERNATIONAL_NUMBER";
+    public static final String INVALID_PHNUM_BAD_NPA = "voice.INVALID_PHNUM_BAD_NPA";
+    public static final String INVALID_PHNUM_BAD_LINE = "voice.INVALID_PHNUM_BAD_LINE";
+    public static final String INVALID_PHNUM_EMERGENCY_ASSISTANCE = "voice.INVALID_PHNUM_EMERGENCY_ASSISTANCE";
+    public static final String INVALID_PHNUM_DIRECTORY_ASSISTANCE = "voice.INVALID_PHNUM_DIRECTORY_ASSISTANCE";
+    public static final String INVALID_PHNUM_BAD_FORMAT = "voice.INVALID_PHNUM_BAD_FORMAT";
+    public static final String VALID = "voice.OK";
 	
-	public static final Pattern CHECK_INTERNATIONAL = Pattern.compile("^0\\d*");
-	public static final Pattern CHECK_NPA = Pattern.compile("^1?(900)|(500)|(700)|(976)");
-	public static final Pattern CHECK_LINE = Pattern.compile("^1?\\d{3}555\\d*");
-	public static final Pattern CHECK_EMERGENCY_ASSISTANCE = Pattern.compile("^1?911\\d*");
-	public static final Pattern CHECK_DIRECTORY_ASSISTANCE = Pattern.compile("^1?411\\d*");
-	public static final Pattern CHECK_FORMAT = Pattern.compile("^1?[2-9]\\d{9}$");
+    public static final Pattern CHECK_INTERNATIONAL = Pattern.compile("^0\\d*");
+    public static final Pattern CHECK_NPA = Pattern.compile("^1?(900)|(500)|(700)|(976)");
+    public static final Pattern CHECK_LINE = Pattern.compile("^1?\\d{3}555\\d*");
+    public static final Pattern CHECK_EMERGENCY_ASSISTANCE = Pattern.compile("^1?911\\d*");
+    public static final Pattern CHECK_DIRECTORY_ASSISTANCE = Pattern.compile("^1?411\\d*");
+    public static final Pattern CHECK_FORMAT = Pattern.compile("^1?[2-9]\\d{9}$");
 
-	private String mName;
-	private String mCallerId;
+    private String mName;
+    private String mCallerId;
 
-	public ZPhone(String name, String callerId) throws ServiceException {
-		mName = name;
-		mCallerId = callerId != null && (callerId.equals(name) || callerId.equals("Unavailable")) ? null : callerId;
-	}
+    public ZPhone(String name, String callerId) throws ServiceException {
+        mName = name;
+        mCallerId = callerId != null && (callerId.equals(name) || callerId.equals("Unavailable")) ? null : callerId;
+    }
 
-	public ZPhone(String name) throws ServiceException {
-		this(name, null);
-	}
+    public ZPhone(String name) throws ServiceException {
+        this(name, null);
+    }
 
-	public String getName() {
-		return mName;
-	}
+    public String getName() {
+        return mName;
+    }
 
-	public String getDisplay() {
-		return ZPhone.getDisplay(mName);
-	}
+    public String getDisplay() {
+        return ZPhone.getDisplay(mName);
+    }
 
-	public String getValidity() {
-		return ZPhone.validate(mName);
-	}
+    public String getValidity() {
+        return ZPhone.validate(mName);
+    }
 
-	public String getCallerId() {
-		return mCallerId;
-	}
+    public String getCallerId() {
+        return mCallerId;
+    }
 
     public ZJSONObject toZJSONObject() throws JSONException {
         ZJSONObject zjo = new ZJSONObject();
@@ -89,10 +89,10 @@ public class ZPhone implements ToZJSONObject {
         }
         if (doIt) {
             StringBuilder builder = new StringBuilder();
-		    if (offset>0) {
-				builder.append(name, 0, offset);
-				builder.append("-");
-		    }
+            if (offset>0) {
+                builder.append(name, 0, offset);
+                builder.append("-");
+            }
             builder.append('(');
             builder.append(name, offset, offset + 3);
             builder.append(") ");
@@ -106,9 +106,9 @@ public class ZPhone implements ToZJSONObject {
 	}
 
     public static String getName(String display) {
-		if (display == null) {
-			return display;
-		}
+        if (display == null) {
+            return display;
+        }
         StringBuilder builder = new StringBuilder(display.length());
         for (int i = 0, count = display.length(); i < count; i++) {
             char ch = display.charAt(i);
@@ -120,34 +120,38 @@ public class ZPhone implements ToZJSONObject {
     }
 
     public static String validate(String number) {
-		number = ZPhone.getName(number);
-	    
-		if (number.charAt(0) == '1')
-		    number = number.substring(1);
-		
-		if (ZPhone.CHECK_INTERNATIONAL.matcher(number).matches()) {
-		    return ZPhone.INVALID_PHNUM_INTERNATIONAL_NUMBER;
-		}
+        number = ZPhone.getName(number);
+
+        if (number.length() == 0) {
+            return ZPhone.INVALID_PHNUM_BAD_FORMAT;
+        }
+
+        if (number.charAt(0) == '1')
+            number = number.substring(1);
+
+        if (ZPhone.CHECK_INTERNATIONAL.matcher(number).matches()) {
+            return ZPhone.INVALID_PHNUM_INTERNATIONAL_NUMBER;
+        }
 									
-		if (ZPhone.CHECK_NPA.matcher(number).matches()) {
-		    return ZPhone.INVALID_PHNUM_BAD_NPA;
-		}
+        if (ZPhone.CHECK_NPA.matcher(number).matches()) {
+            return ZPhone.INVALID_PHNUM_BAD_NPA;
+        }
 																	
-		if (ZPhone.CHECK_LINE.matcher(number).matches()) {
-			return ZPhone.INVALID_PHNUM_BAD_LINE;
-		}
+        if (ZPhone.CHECK_LINE.matcher(number).matches()) {
+            return ZPhone.INVALID_PHNUM_BAD_LINE;
+        }
 																				
-		if (ZPhone.CHECK_EMERGENCY_ASSISTANCE.matcher(number).matches()) {
-			return ZPhone.INVALID_PHNUM_EMERGENCY_ASSISTANCE;
-		}
+        if (ZPhone.CHECK_EMERGENCY_ASSISTANCE.matcher(number).matches()) {
+            return ZPhone.INVALID_PHNUM_EMERGENCY_ASSISTANCE;
+        }
 																						
-		if (ZPhone.CHECK_DIRECTORY_ASSISTANCE.matcher(number).matches()) {
-			return ZPhone.INVALID_PHNUM_DIRECTORY_ASSISTANCE;
-		}
+        if (ZPhone.CHECK_DIRECTORY_ASSISTANCE.matcher(number).matches()) {
+            return ZPhone.INVALID_PHNUM_DIRECTORY_ASSISTANCE;
+        }
 																										    
-		if (!ZPhone.CHECK_FORMAT.matcher(number).matches()) {
-			return ZPhone.INVALID_PHNUM_BAD_FORMAT;
-		}
-		return ZPhone.VALID;
+        if (!ZPhone.CHECK_FORMAT.matcher(number).matches()) {
+            return ZPhone.INVALID_PHNUM_BAD_FORMAT;
+        }
+        return ZPhone.VALID;
     }
 }
