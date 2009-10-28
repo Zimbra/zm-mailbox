98d36415ef180cdd12e902cc3492edde246476b9
Unknown Perforce User <lars>
2009-10-28 04:44:00 -0500

    (split)bug: 41993
    All TNs matching the regex ^(1?\\d{3})?555\\d*$ will now be disallowed (including 555-1212 and 555-01xx)
    + added a few enhancements (e.g. replaced a little ugly code with non-ugly code)
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=41993
    
    Copied from Perforce
     Change: 216424

--- src/java/com/zimbra/cs/zclient/ZPhone.java
+++ src/java/com/zimbra/cs/zclient/ZPhone.java
@@ -34,8 +34,7 @@ public class ZPhone implements ToZJSONObject {
 	
     public static final Pattern CHECK_INTERNATIONAL = Pattern.compile("^0\\d*");
     public static final Pattern CHECK_NPA = Pattern.compile("^1?(900)|(500)|(700)|(976)");
-    public static final Pattern CHECK_LINE = Pattern.compile("^(1?\\d{3})?5551212\\d*");
-    public static final Pattern CHECK_FIVES = Pattern.compile("^(1?\\d{3})?55501\\d{2}\\d*");
+    public static final Pattern CHECK_LINE = Pattern.compile("^(1?\\d{3})?555\\d*");
     public static final Pattern CHECK_EMERGENCY_ASSISTANCE = Pattern.compile("^1?911\\d*");
     public static final Pattern CHECK_DIRECTORY_ASSISTANCE = Pattern.compile("^1?411\\d*");
     public static final Pattern CHECK_FORMAT = Pattern.compile("^1?[2-9]\\d{9}$");
@@ -90,8 +89,8 @@ public class ZPhone implements ToZJSONObject {
             doIt = true;
             offset = 1;
         } else if (name.length() == 7) {
-                doIt = true;
-                offset = -3;
+            doIt = true;
+            offset = -3;
         }
         if (doIt) {
             StringBuilder builder = new StringBuilder();
@@ -148,10 +147,6 @@ public class ZPhone implements ToZJSONObject {
         }
 	
         if (ZPhone.CHECK_LINE.matcher(number).matches()) {
-            return ZPhone.INVALID_PHNUM_BAD_LINE;
-        }
-	
-        if (ZPhone.CHECK_FIVES.matcher(number).matches()) {
             return ZPhone.INVALID_PHNUM_BAD_LINE;
         }
 	
