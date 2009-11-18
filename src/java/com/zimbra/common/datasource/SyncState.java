55da9225410533c29d3952e0140b38dbea168100
Unknown Perforce User <bburtin>
2009-11-18 13:35:41 -0600

    bug: 10704 - Factor out core code from ZimbraServer
    Moved SyncState to com.zimbra.common.datasource, in
    preparation for moving DataSource to ZimbraCommon
    with the rest of the Provisioning classes.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=10704
    
    Copied from Perforce
     Change: 218724

---
+++ src/java/com/zimbra/common/datasource/SyncState.java
@@ -0,0 +1,56 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2008, 2009 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Yahoo! Public License
+ * Version 1.0 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.common.datasource;
+
+public class SyncState {
+    private long lastUid = -1;
+    private long exists = -1;
+    private long unseen = -1;
+    private int lastModSeq;
+
+    public SyncState() {}
+
+    public long getLastUid() { return lastUid; }
+    public long getExists() { return exists; }
+    public long getUnseen() { return unseen; }
+    public int getLastModSeq() { return lastModSeq; }
+
+    public void setExists(long exists) {
+        this.exists = exists;
+    }
+
+    public void setUnseen(long unseen) {
+        this.unseen = unseen;
+    }
+
+    public void setLastUid(long lastUid) {
+        this.lastUid = lastUid;
+    }
+
+    public void updateLastUid(long uid) {
+        if (uid > lastUid) {
+            lastUid = uid;
+        }
+    }
+
+    public void setLastModSeq(int lastModSeq) {
+        this.lastModSeq = lastModSeq;
+    }
+    
+    public String toString() {
+        return String.format("{lastUid=%d,exists=%d,unseen=%d,lastModSeq=%d}",
+                             lastUid, exists, unseen, lastModSeq);
+    }
+}
