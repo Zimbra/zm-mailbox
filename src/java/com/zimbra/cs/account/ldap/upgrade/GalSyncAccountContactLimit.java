127b4d7fde2b5a3bcd105d9fd2da7463da68246d
Unknown Perforce User <pshao>
2009-12-11 12:45:56 -0600

    (split)bug: 43147
    - search domains with zimbraGalAccountId set and asking for only minimum attrs from ldap
    - use one LDAP context for modifying.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=43147
    
    Copied from Perforce
     Change: 221585

--- src/java/com/zimbra/cs/account/ldap/upgrade/GalSyncAccountContactLimit.java
+++ src/java/com/zimbra/cs/account/ldap/upgrade/GalSyncAccountContactLimit.java
@@ -1,11 +1,19 @@
 package com.zimbra.cs.account.ldap.upgrade;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
 
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.account.Account;
-import com.zimbra.cs.account.Domain;
 import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.account.ldap.LdapUtil;
+import com.zimbra.cs.account.ldap.ZimbraLdapContext;
+import com.zimbra.cs.account.ldap.LdapUtil.SearchLdapVisitor;
 
 public class GalSyncAccountContactLimit extends LdapUpgrade {
 
@@ -14,21 +22,82 @@ public class GalSyncAccountContactLimit extends LdapUpgrade {
 
     @Override
     void doUpgrade() throws ServiceException {
-        Provisioning prov = Provisioning.getInstance();
-        HashMap<String,String> map = new HashMap<String,String>();
-        map.put(Provisioning.A_zimbraContactMaxNumEntries, "0");
-        for (Domain domain : prov.getAllDomains()) {
-            System.out.println("Checking domain "+domain.getName());
-            String[] accountIds = domain.getMultiAttr(Provisioning.A_zimbraGalAccountId);
-            if (accountIds != null && accountIds.length > 0) {
-                for (String accountId : accountIds) {
-                    Account a = prov.getAccountById(accountId);
-                    if (a != null) {
-                        System.out.println("Modifying account "+a.getName());
-                        prov.modifyAttrs(a, map);
+        
+        Set<String> galSyncAcctIds = getAllGalSyncAcctIds();
+        
+        ZimbraLdapContext zlc = new ZimbraLdapContext(true);
+        try {
+            upgradeGalSyncAcct(zlc, galSyncAcctIds);
+        } finally {
+            ZimbraLdapContext.closeContext(zlc);
+        }
+    }
+    
+    private Set<String> getAllGalSyncAcctIds() throws ServiceException {
+        final Set<String> galSyncAcctIds = new HashSet<String>();
+        
+        SearchLdapVisitor visitor = new SearchLdapVisitor() {
+            public void visit(String dn, Map<String, Object> attrs, Attributes ldapAttrs) {
+                System.out.println("Domain " + attrs.get(Provisioning.A_zimbraDomainName));
+                
+                Object values = attrs.get(Provisioning.A_zimbraGalAccountId);
+                if (values instanceof String) {
+                    System.out.println(" GAL sync account " + (String)values);
+                    galSyncAcctIds.add((String)values);
+                } else if (values instanceof String[]) {
+                    for (String value : (String[])values) {
+                       System.out.println(" GAL sync account " + value);
+                       galSyncAcctIds.add(value);
                     }
                 }
             }
+        };
+        
+        String query = "(&(objectClass=zimbraDomain)(zimbraGalAccountId=*))";
+        String bases[] = mProv.getSearchBases(Provisioning.SA_DOMAIN_FLAG);
+        String attrs[] = new String[] {Provisioning.A_zimbraDomainName,
+                                       Provisioning.A_zimbraGalAccountId};
+        
+        for (String base : bases) {
+            LdapUtil.searchLdapOnMaster(base, query, attrs, visitor);
         }
+        
+        return galSyncAcctIds;
+    }
+    
+    private void upgradeGalSyncAcct(ZimbraLdapContext zlc, Set<String> galSyncAcctIds) throws ServiceException {
+        System.out.println();
+        System.out.println("Upgrading zimbraContactMaxNumEntries on GAL sync accounts ...");
+        System.out.println();
+        
+        HashMap<String,String> attrs = new HashMap<String,String>();
+        attrs.put(Provisioning.A_zimbraContactMaxNumEntries, "0");
+        
+        // got all GAL sync account ids, upgrade them
+        for (String id : galSyncAcctIds) {
+            Account acct = mProv.getAccountById(id);
+            if (acct != null) {
+                // upgrade only when the gal sync account does not have zimbraContactMaxNumEntries 
+                // set on the account entry
+                String curValue = acct.getAttr(Provisioning.A_zimbraContactMaxNumEntries, false);
+                
+                if (curValue == null) {
+                    try {
+                        System.out.println("Account: " + acct.getId() + "(" + acct.getName() + ") - "+ "modifying zimbraContactMaxNumEntries to 0");
+                        modifyAttrs(acct, zlc, attrs);
+                    } catch (ServiceException e) {
+                        System.out.println("Caught ServiceException while modifying GAL sync account entry " + acct.getName());
+                        e.printStackTrace();
+                    } catch (NamingException e) {
+                        System.out.println("Caught NamingException while modifying GAL sync account entry " + acct.getName());
+                        e.printStackTrace();
+                    }
+                } else
+                    System.out.println("Account: " + acct.getId() + "(" + acct.getName() + ") - "+ "already has value " + curValue + ", skipping");
+            } else {
+                System.out.println("Account: " + id + " - no such account");
+            }
+        }
+
     }
 }
