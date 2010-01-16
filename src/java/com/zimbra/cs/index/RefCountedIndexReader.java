475ed8a8dd41e6f634aadd66464b1265aa642fd3
Unknown Perforce User <pshao>
2010-01-15 18:40:41 -0600

    (split)bug: 41873
    Instrumented RefCountedIndexReader for debugging instance leaking.
    
    to enable: 
    zmlocalconfig -e debug_enable_ref_counted_index_reader_stats=true
    zmprov aal user1 zimbra.index debug
    
    This will print RefCountedIndexReader info (ref count, access time, stack where ref count is inc/dec, ...) for user1 into /opt/zimbra/zmstat/RefCountedIndexReader.csv
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=41873
    
    Copied from Perforce
     Change: 224502

--- src/java/com/zimbra/cs/index/RefCountedIndexReader.java
+++ src/java/com/zimbra/cs/index/RefCountedIndexReader.java
@@ -15,9 +15,20 @@
 package com.zimbra.cs.index;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 import org.apache.lucene.index.IndexReader;
 
+import com.zimbra.cs.localconfig.DebugConfig;
+import com.zimbra.common.stats.StatsDumper;
+import com.zimbra.common.stats.StatsDumperDataSource;
+import com.zimbra.common.util.Constants;
+import com.zimbra.common.util.Log;
+import com.zimbra.common.util.LogFactory;
+import com.zimbra.common.util.StringUtil;
 import com.zimbra.common.util.ZimbraLog;
 
 /**
@@ -29,11 +40,128 @@ final class RefCountedIndexReader {
     private int mCount = 1;
     private long mAccessTime;
     private boolean mRequiresReopen = false;
+    
+    // debugging stuff
+    private boolean mDebug = false;
+    private static RefCountedIndexReaderStats sStatsInstance = null;
+    private static final String DEBUG_DELIM = ", ";
+    private enum DebugAction {
+        CRT("*"),  // creating 
+        ADD("+"),  // add ref
+        DEC("-") ; // dev ref
+        
+        private String mSymbol; 
+        DebugAction(String symbol) {
+            mSymbol = symbol;
+        }
+        
+        String symbol() {
+            return mSymbol;
+        }
+    }
+    
+    static {
+        if (DebugConfig.enableRefCountedIndexReaderStats) {
+            sStatsInstance = new RefCountedIndexReaderStats();
+            StatsDumper.schedule(sStatsInstance, 5 * Constants.MILLIS_PER_SECOND);
+        }
+    }
+    
+    private static final class RefCountedIndexReaderStats implements StatsDumperDataSource {
+        public String getFilename() {
+            return "RefCountedIndexReader.csv";
+        }
+        
+        public String getHeader() {
+            List<String> columns = new ArrayList<String>();
+            columns.add("ThreadId");
+            columns.add("Notes");
+            columns.add("Hashcode");
+            columns.add("RefCount");
+            columns.add("LastAccessTime");
+            columns.add("StackFrames");
+            return StringUtil.join(",", columns);
+        }
+        
+        synchronized public Collection<String> getDataLines() {
+            Collection<String> curDataLines = mDataLines;
+            mDataLines = new ArrayList<String>(); // "empty it out" after we've logged 
+            return curDataLines;
+        }
+        
+        public boolean hasTimestampColumn() {
+            return true;
+        }
+        
+        Collection<String> mDataLines = new ArrayList<String>();
+        
+        synchronized void addStatLine(String line) {
+            mDataLines.add(line);
+        }
+        
+        static void addStats(String line) {
+            if (sStatsInstance == null)
+                return;
 
+            sStatsInstance.addStatLine(line);
+        }
+    }
+    
+    void statMe(DebugAction action) {
+        if (!mDebug)
+            return;
+
+        // the thread must be holding the lock on the RefCountedIndexReader object
+        // log it if that's not the case (should not happen)
+        StringBuilder line = new StringBuilder();
+            
+        Thread curThread = Thread.currentThread();
+            
+        // ThreadId,
+        line.append(curThread.getId() + DEBUG_DELIM);
+            
+        // Notes,
+        line.append("[");
+        line.append(action.symbol());
+        if (action != DebugAction.CRT && !Thread.holdsLock(this)) 
+            line.append(" nolock!");     // no lock warning, not likely to happen, just checking
+        line.append("]" + DEBUG_DELIM);
+
+        // Hashcode,
+        line.append(hashCode() + DEBUG_DELIM);
+        
+        // RefCount,
+        line.append("["+ mCount + "]" + DEBUG_DELIM);
+        
+        // LastAccessTime,
+        String accessTime = String.format("%1$tm%1$td-%1$tH:%1$tM:%1$tS.%1$tL", new Date(mAccessTime));
+        line.append(accessTime + DEBUG_DELIM);
+            
+        // StackFrames
+        StackTraceElement[] stack = curThread.getStackTrace();
+        int maxFrames = Math.min(20, stack.length);
+            
+        // skip the top two frames, they are Thread:1409 and RefCountedIndexReader:108 
+        for (int i = 2; i < maxFrames; i++) {
+            String className = stack[i].getClassName();
+            int lastDot = className.lastIndexOf('.');
+            if (lastDot != -1)
+                className = className.substring(lastDot+1);
+            line.append(className + ":" + stack[i].getLineNumber() + " - ");
+        }
+            
+        RefCountedIndexReaderStats.addStats(line.toString());
+    }
+    
     RefCountedIndexReader(ILuceneIndex idx, IndexReader reader) {
         mIdx = idx;
         mReader= reader;
         mAccessTime = System.currentTimeMillis();
+        
+        if (DebugConfig.enableRefCountedIndexReaderStats && ZimbraLog.index.isDebugEnabled())
+            mDebug = true;
+        
+        statMe(DebugAction.CRT);
     }
     
     synchronized IndexReader getReader() {
@@ -43,6 +171,8 @@ final class RefCountedIndexReader {
     synchronized void addRef() {
         mAccessTime = System.currentTimeMillis();
         mCount++;
+        
+        statMe(DebugAction.ADD);
     }
 
     synchronized void forceClose() {
@@ -55,6 +185,8 @@ final class RefCountedIndexReader {
         if (0 == mCount) {
             closeIt();
         }
+        
+        statMe(DebugAction.DEC);
     }
     
     synchronized boolean markForReopen() {
