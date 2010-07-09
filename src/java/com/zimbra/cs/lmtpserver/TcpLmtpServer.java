295d46188267736bf3bfd0b6b78bdec1f28610ac
Unknown Perforce User <dac>
2010-07-08 22:29:22 -0500

    (split)bug: 9470 Add missing file.
    http://bugzilla.zimbra.com/show_bug.cgi?id=9470
    
    Copied from Perforce
     Change: 241786

---
+++ src/java/com/zimbra/cs/lmtpserver/TcpLmtpServer.java
@@ -0,0 +1,33 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
+ *
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.lmtpserver;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.tcpserver.ProtocolHandler;
+import com.zimbra.cs.tcpserver.TcpServer;
+
+public class TcpLmtpServer extends TcpServer implements LmtpServer {
+    public TcpLmtpServer(LmtpConfig config) throws ServiceException {
+        super("LmtpServer", config);
+    }
+
+    protected ProtocolHandler newProtocolHandler() {
+        return new TcpLmtpHandler(this);
+    }
+
+    public LmtpConfig getConfig() {
+        return (LmtpConfig) super.getConfig();
+    }
+}
