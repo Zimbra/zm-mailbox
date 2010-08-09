6ec1a4aca077f69dab4224d971d60ed754e6a726
Unknown Perforce User <ysasaki>
2010-08-09 14:26:12 -0500

    bug:47613
    Add 'allowRemoteContent' contact attribute
    
    https://reviewboard.eng.vmware.com/r/166287/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=47613
    
    Copied from Perforce
     Change: 246444

--- src/java/com/zimbra/common/mailbox/ContactConstants.java
+++ src/java/com/zimbra/common/mailbox/ContactConstants.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2009, 2010 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -19,7 +19,9 @@ import java.util.Arrays;
 
 import com.zimbra.common.service.ServiceException;
 
-public class ContactConstants {
+public final class ContactConstants {
+    private ContactConstants() {
+    }
 
     /** "File as" setting: &nbsp;<tt>Last, First</tt> */
     public static final int FA_LAST_C_FIRST = 1;
@@ -43,86 +45,92 @@ public class ContactConstants {
     public static final int FA_MAXIMUM = FA_EXPLICIT;
 
     // these are the "well known attrs". keep in sync with Attr enum below
-    public static final String A_assistantPhone = "assistantPhone";
-    public static final String A_birthday = "birthday";
-    public static final String A_callbackPhone = "callbackPhone";
-    public static final String A_carPhone = "carPhone";
-    public static final String A_company = "company";
-    public static final String A_companyPhone = "companyPhone";
-    public static final String A_department = "department";
-    public static final String A_dlist = "dlist";
-    public static final String A_email = "email";
-    public static final String A_email2 = "email2";
-    public static final String A_email3 = "email3";
-    public static final String A_fileAs = "fileAs";
-    public static final String A_firstName = "firstName";
-    public static final String A_fullName = "fullName";
-    public static final String A_homeCity = "homeCity";
-    public static final String A_homeCountry = "homeCountry";
-    public static final String A_homeFax = "homeFax";
-    public static final String A_homePhone = "homePhone";
-    public static final String A_homePhone2 = "homePhone2";
-    public static final String A_homePostalCode = "homePostalCode";
-    public static final String A_homeState = "homeState";
-    public static final String A_homeStreet = "homeStreet";
-    public static final String A_homeURL = "homeURL";
-    public static final String A_image = "image";
-    public static final String A_initials = "initials";
+    public static final String A_assistantPhone = Attr.assistantPhone.name();
+    public static final String A_birthday = Attr.birthday.name();
+    public static final String A_callbackPhone = Attr.callbackPhone.name();
+    public static final String A_carPhone = Attr.callbackPhone.name();
+    public static final String A_company = Attr.company.name();
+    public static final String A_companyPhone = Attr.companyPhone.name();
+    public static final String A_department = Attr.department.name();
+    public static final String A_dlist = Attr.dlist.name();
+    public static final String A_email = Attr.email.name();
+    public static final String A_email2 = Attr.email2.name();
+    public static final String A_email3 = Attr.email3.name();
+    public static final String A_fileAs = Attr.fileAs.name();
+    public static final String A_firstName = Attr.firstName.name();
+    public static final String A_fullName = Attr.fullName.name();
+    public static final String A_homeCity = Attr.homeCity.name();
+    public static final String A_homeCountry = Attr.homeCountry.name();
+    public static final String A_homeFax = Attr.homeFax.name();
+    public static final String A_homePhone = Attr.homePhone.name();
+    public static final String A_homePhone2 = Attr.homePhone2.name();
+    public static final String A_homePostalCode = Attr.homePostalCode.name();
+    public static final String A_homeState = Attr.homeState.name();
+    public static final String A_homeStreet = Attr.homeStreet.name();
+    public static final String A_homeURL = Attr.homeURL.name();
+    public static final String A_image = Attr.image.name();
+    public static final String A_initials = Attr.initials.name();
     public static final String A_isMyCard = "isMyCard";
-    public static final String A_jobTitle = "jobTitle";
-    public static final String A_lastName = "lastName";
-    public static final String A_middleName = "middleName";
-    public static final String A_mobilePhone = "mobilePhone";
-    public static final String A_namePrefix = "namePrefix";
-    public static final String A_nameSuffix = "nameSuffix";
-    public static final String A_nickname = "nickname";
-    public static final String A_notes = "notes";
-    public static final String A_office = "office";
-    public static final String A_otherCity = "otherCity";
-    public static final String A_otherCountry = "otherCountry";
-    public static final String A_otherFax = "otherFax";
-    public static final String A_otherPhone = "otherPhone";
-    public static final String A_otherPostalCode = "otherPostalCode";
-    public static final String A_otherState = "otherState";
-    public static final String A_otherStreet = "otherStreet";
-    public static final String A_otherURL = "otherURL";
-    public static final String A_pager = "pager";
-    public static final String A_workCity = "workCity";
-    public static final String A_workCountry = "workCountry";
-    public static final String A_workFax = "workFax";
-    public static final String A_workPhone = "workPhone";
-    public static final String A_workPhone2 = "workPhone2";
-    public static final String A_workPostalCode = "workPostalCode";
-    public static final String A_workState = "workState";
-    public static final String A_workStreet = "workStreet";
-    public static final String A_workURL = "workURL";
-    public static final String A_type = "type";
-    public static final String A_imAddress1 = "imAddress1";
-    public static final String A_imAddress2 = "imAddress2";
-    public static final String A_imAddress3 = "imAddress3";
+    public static final String A_jobTitle = Attr.jobTitle.name();
+    public static final String A_lastName = Attr.lastName.name();
+    public static final String A_middleName = Attr.middleName.name();
+    public static final String A_mobilePhone = Attr.mobilePhone.name();
+    public static final String A_namePrefix = Attr.namePrefix.name();
+    public static final String A_nameSuffix = Attr.nameSuffix.name();
+    public static final String A_nickname = Attr.nickname.name();
+    public static final String A_notes = Attr.notes.name();
+    public static final String A_office = Attr.office.name();
+    public static final String A_otherCity = Attr.otherCity.name();
+    public static final String A_otherCountry = Attr.otherCountry.name();
+    public static final String A_otherFax = Attr.otherFax.name();
+    public static final String A_otherPhone = Attr.otherPhone.name();
+    public static final String A_otherPostalCode = Attr.otherPostalCode.name();
+    public static final String A_otherState = Attr.otherState.name();
+    public static final String A_otherStreet = Attr.otherStreet.name();
+    public static final String A_otherURL = Attr.otherURL.name();
+    public static final String A_pager = Attr.pager.name();
+    public static final String A_workCity = Attr.workCity.name();
+    public static final String A_workCountry = Attr.workCountry.name();
+    public static final String A_workFax = Attr.workFax.name();
+    public static final String A_workPhone = Attr.workPhone.name();
+    public static final String A_workPhone2 = Attr.workPhone2.name();
+    public static final String A_workPostalCode = Attr.workPostalCode.name();
+    public static final String A_workState = Attr.workState.name();
+    public static final String A_workStreet = Attr.workStreet.name();
+    public static final String A_workURL = Attr.workURL.name();
+    public static final String A_type = Attr.type.name();
+    public static final String A_imAddress1 = Attr.imAddress1.name();
+    public static final String A_imAddress2 = Attr.imAddress2.name();
+    public static final String A_imAddress3 = Attr.imAddress3.name();
+
+    /**
+     * Whether to allow displaying remote images in HTML mail.
+     */
+    public static final String A_allowRemoteContent = Attr.allowRemoteContent.name();
+
     // Comcast specific fields
-    public static final String A_homeAddress = "homeAddress";
-    public static final String A_workAddress = "workAddress";
-    public static final String A_workEmail1 = "workEmail1";
-    public static final String A_workEmail2 = "workEmail2";
-    public static final String A_workEmail3 = "workEmail3";
-    public static final String A_workMobile = "workMobile";
-    public static final String A_workIM1 = "workIM1";
-    public static final String A_workIM2 = "workIM2";
-    public static final String A_workAltPhone = "workAltPhone";
-    public static final String A_otherDepartment = "otherDepartment";
-    public static final String A_otherOffice = "otherOffice";
-    public static final String A_otherProfession = "otherProfession";
-    public static final String A_otherAddress = "otherAddress";
-    public static final String A_otherMgrName = "otherMgrName";
-    public static final String A_otherAsstName = "otherAsstName";
-    public static final String A_otherAnniversary = "otherAnniversary";
-    public static final String A_otherCustom1 = "otherCustom1";
-    public static final String A_otherCustom2 = "otherCustom2";
-    public static final String A_otherCustom3 = "otherCustom3";
-    public static final String A_otherCustom4 = "otherCustom4";
+    public static final String A_homeAddress = Attr.homeAddress.name();
+    public static final String A_workAddress = Attr.workAddress.name();
+    public static final String A_workEmail1 = Attr.workEmail1.name();
+    public static final String A_workEmail2 = Attr.workEmail2.name();
+    public static final String A_workEmail3 = Attr.workEmail3.name();
+    public static final String A_workMobile = Attr.workMobile.name();
+    public static final String A_workIM1 = Attr.workIM1.name();
+    public static final String A_workIM2 = Attr.workIM2.name();
+    public static final String A_workAltPhone = Attr.workAltPhone.name();
+    public static final String A_otherDepartment = Attr.otherDepartment.name();
+    public static final String A_otherOffice = Attr.otherOffice.name();
+    public static final String A_otherProfession = Attr.otherProfession.name();
+    public static final String A_otherAddress = Attr.otherAddress.name();
+    public static final String A_otherMgrName = Attr.otherMgrName.name();
+    public static final String A_otherAsstName = Attr.otherAsstName.name();
+    public static final String A_otherAnniversary = Attr.otherAnniversary.name();
+    public static final String A_otherCustom1 = Attr.otherCustom1.name();
+    public static final String A_otherCustom2 = Attr.otherCustom2.name();
+    public static final String A_otherCustom3 = Attr.otherCustom3.name();
+    public static final String A_otherCustom4 = Attr.otherCustom4.name();
     // end
-    
+
     // vCard fields that need to be preserved
     public static final String A_vCardUID    = "vcardUID";
     public static final String A_vCardURL    = "vcardURL";
@@ -130,8 +138,11 @@ public class ContactConstants {
 
     public static final String TYPE_GROUP = "group";
 
- // these are the "well known attrs". keep in sync with ContactConstants.A_* above.
+    /**
+     * "well known attrs". keep in sync with ContactConstants.A_* above.
+     */
     public enum Attr {
+        allowRemoteContent,
         assistantPhone,
         birthday,
         callbackPhone,
@@ -217,7 +228,8 @@ public class ContactConstants {
             try {
                 return Attr.valueOf(s);
             } catch (IllegalArgumentException e) {
-                throw ServiceException.INVALID_REQUEST("invalid attr: "+s+", valid values: "+Arrays.asList(Attr.values()), e);
+                throw ServiceException.INVALID_REQUEST("invalid attr: " + s +
+                        ", valid values: " + Arrays.asList(Attr.values()), e);
             }
         }
     }
