b0e1e918027a431cd87fc48e923c5a44dd538144
Unknown Perforce User <jflanigan>
2010-08-31 09:15:09 -0500

    Integrate change 248819 from ZDESKTOP-608        bug: 49429
           Use new retry handler with input stream reqs so HttpClient doesn't 
           hide IO exceptions for unbuffered requests.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=49429
    
    Copied from Perforce
     Change: 248821

--- src/java/com/zimbra/common/httpclient/HttpClientUtil.java
+++ src/java/com/zimbra/common/httpclient/HttpClientUtil.java
@@ -1,11 +1,15 @@
 package com.zimbra.common.httpclient;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.HttpState;
+import org.apache.commons.httpclient.methods.EntityEnclosingMethod;
+import org.apache.commons.httpclient.methods.InputStreamRequestEntity;
+import org.apache.commons.httpclient.params.HttpMethodParams;
 
 import com.zimbra.common.util.ZimbraHttpConnectionManager;
 
@@ -20,5 +24,14 @@ public class HttpClientUtil {
 
     public static int executeMethod(HttpClient client, HttpMethod method, HttpState state) throws HttpException, IOException {
         return client.executeMethod(HttpProxyConfig.getProxyConfig(client.getHostConfiguration(), method.getURI().toString()), method, state);
+    }
+    
+    public static <T extends EntityEnclosingMethod> T addInputStreamToHttpMethod(T method, InputStream is, long size, String contentType) {
+        if (size < 0) {
+            size = InputStreamRequestEntity.CONTENT_LENGTH_AUTO;
+        }
+        method.setRequestEntity(new InputStreamRequestEntity(is, size, contentType));
+        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new InputStreamRequestHttpRetryHandler());
+        return method;
     }
 }
