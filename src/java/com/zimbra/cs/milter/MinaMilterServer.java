bb66719044c3d9a4da7a5bb99d0eebf88e9916e4
Unknown Perforce User <jmhe>
2010-09-13 12:31:49 -0500

    bug: 46311 - allow the option to run milter server as a standalone service - add LC keys for overloading bind_port and bind_address on a particular node milter is installed
    http://bugzilla.zimbra.com/show_bug.cgi?id=46311
    
    Copied from Perforce
     Change: 250235

--- src/java/com/zimbra/cs/milter/MinaMilterServer.java
+++ src/java/com/zimbra/cs/milter/MinaMilterServer.java
@@ -23,11 +23,16 @@ import org.apache.mina.filter.codec.ProtocolEncoder;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.util.Log;
 import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.account.AttributeManager;
+import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.account.ldap.LdapProvisioning;
+import com.zimbra.cs.account.ldap.ZimbraLdapContext;
 import com.zimbra.cs.mina.MinaCodecFactory;
 import com.zimbra.cs.mina.MinaHandler;
 import com.zimbra.cs.mina.MinaServer;
 import com.zimbra.cs.mina.MinaSession;
 import com.zimbra.cs.server.ServerConfig;
+import com.zimbra.cs.server.ServerManager;
 
 public class MinaMilterServer extends MinaServer implements MilterServer {
 
@@ -58,5 +63,48 @@ public class MinaMilterServer extends MinaServer implements MilterServer {
     
     @Override public Log getLog() {
         return ZimbraLog.milter;
+    }
+    
+    /* for running standalone milter server */
+    
+    private static ExecutorService milterNioHandlerPool;
+    private static MilterServer milterServer;
+    
+    private static class MilterShutdownHook extends Thread {
+        private MilterServer server;
+
+        public MilterShutdownHook(MilterServer server) {
+            this.server = server;
+        }
+        
+        public void run() {
+            try {
+                ZimbraLog.milter.info("Shutting down milter server");
+                server.stop();
+            } catch (ServiceException e) {
+                ZimbraLog.milter.error("Server shutdown error: " + e.getMessage());    
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        try {
+            Provisioning prov = Provisioning.getInstance();
+            if (prov instanceof LdapProvisioning) {
+                ZimbraLdapContext.waitForServer();
+            }
+            
+            MilterConfig config = new MilterConfig();
+            milterNioHandlerPool = ServerManager.newNioHandlerPool(config);
+            milterServer = new MinaMilterServer(config, milterNioHandlerPool);
+            
+            MilterShutdownHook shutdownHook = new MilterShutdownHook(milterServer);
+            Runtime.getRuntime().addShutdownHook(shutdownHook);
+            
+            ZimbraLog.milter.info("Starting milter server");
+            milterServer.start();
+        } catch (ServiceException e) {
+            ZimbraLog.milter.error("Unable to start milter server: " + e.getMessage());
+        }
     }
 }
