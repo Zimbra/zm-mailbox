276cb710cba7899709135c1eab91c458b8d66a7b
Unknown Perforce User <ysasaki>
2010-10-01 13:58:51 -0500

    bug:20984 HttpClient's connection timeout has no effect - zmlicense --ping was hanging too long
    http://bugzilla.zimbra.com/show_bug.cgi?id=20984
    
    Copied from Perforce
     Change: 252398

--- src/java/com/zimbra/common/net/ProtocolSocketFactoryWrapper.java
+++ src/java/com/zimbra/common/net/ProtocolSocketFactoryWrapper.java
@@ -25,28 +25,32 @@ import java.net.Socket;
 
 class ProtocolSocketFactoryWrapper implements ProtocolSocketFactory {
     private final SocketFactory factory;
-    
+
     ProtocolSocketFactoryWrapper(SocketFactory factory) {
         this.factory = factory;
     }
 
-    public Socket createSocket(String host, int port, InetAddress localAddress, int localPort) throws IOException {
+    @Override
+    public Socket createSocket(String host, int port, InetAddress localAddress,
+            int localPort) throws IOException {
         return factory.createSocket(host, port, localAddress, localPort);
     }
 
+    @Override
     public Socket createSocket(String host, int port, InetAddress localAddress,
-                               int localPort, HttpConnectionParams params) throws IOException {
+            int localPort, HttpConnectionParams params) throws IOException {
         int timeout = params != null ? params.getConnectionTimeout() : 0;
         if (timeout > 0) {
             Socket sock = factory.createSocket();
             sock.bind(new InetSocketAddress(localAddress, localPort));
-            sock.connect(new InetSocketAddress(host, port));
+            sock.connect(new InetSocketAddress(host, port), timeout);
             return sock;
         } else {
             return factory.createSocket(host, port, localAddress, localPort);
         }
     }
 
+    @Override
     public Socket createSocket(String host, int port) throws IOException {
         return factory.createSocket(host, port);
     }
