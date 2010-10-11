53dfb1cdd77836f8de3c6bf1929ed00631e50e72
Unknown Perforce User <ysasaki>
2010-10-11 16:46:21 -0500

    (split)bug:48037 Add charset parameter to VCF formatter + minor code cleanup
    https://reviewboard.eng.vmware.com/r/180196/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=48037
    
    Copied from Perforce
     Change: 253363

--- src/java/com/zimbra/cs/service/formatter/VcfFormatter.java
+++ src/java/com/zimbra/cs/service/formatter/VcfFormatter.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -15,6 +15,7 @@
 package com.zimbra.cs.service.formatter;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.List;
 import javax.mail.Part;
@@ -33,32 +34,44 @@ import com.zimbra.common.mime.MimeConstants;
 
 public class VcfFormatter extends Formatter {
 
+    @Override
     public String getType() {
         return "vcf";
     }
 
+    @Override
     public String[] getDefaultMimeTypes() {
-        return new String[] { MimeConstants.CT_TEXT_VCARD, MimeConstants.CT_TEXT_VCARD_LEGACY, MimeConstants.CT_TEXT_VCARD_LEGACY2 };
+        return new String[] {
+                MimeConstants.CT_TEXT_VCARD,
+                MimeConstants.CT_TEXT_VCARD_LEGACY,
+                MimeConstants.CT_TEXT_VCARD_LEGACY2
+        };
     }
 
+    @Override
     public String getDefaultSearchTypes() {
         return MailboxIndex.SEARCH_FOR_CONTACTS;
     }
-    
+
+    @Override
     public boolean canBeBlocked() {
         return false;
     }
 
+    @Override
     public void formatCallback(Context context) throws IOException, ServiceException {
+        Charset charset = context.getCharset();
         Iterator<? extends MailItem> iterator = null;
         try {
             iterator = getMailItems(context, getDefaultStartTime(), getDefaultEndTime(), Integer.MAX_VALUE);
 
-            String filename = context.target instanceof Contact ? ((Contact) context.target).getFileAsString() : "contacts";
-            String cd = Part.ATTACHMENT + "; filename=" + HttpUtil.encodeFilename(context.req, filename + ".vcf");
+            String filename = context.target instanceof Contact ?
+                    ((Contact) context.target).getFileAsString() : "contacts";
+            String cd = Part.ATTACHMENT + "; filename=" +
+            HttpUtil.encodeFilename(context.req, filename + ".vcf");
             context.resp.addHeader("Content-Disposition", cd);
             context.resp.setContentType(MimeConstants.CT_TEXT_VCARD_LEGACY);  // for backward compatibility
-            context.resp.setCharacterEncoding(MimeConstants.P_CHARSET_UTF8);
+            context.resp.setCharacterEncoding(charset.name());
 
             int count = 0;
             while (iterator.hasNext()) {
@@ -66,27 +79,33 @@ public class VcfFormatter extends Formatter {
                 if (!(item instanceof Contact))
                     continue;
                 VCard vcf = VCard.formatContact((Contact) item);
-                context.resp.getOutputStream().write(vcf.formatted.getBytes(MimeConstants.P_CHARSET_UTF8));
+                context.resp.getOutputStream().write(vcf.formatted.getBytes(charset));
                 count++;
             }
-//            if (count == 0)
-//                throw new UserServletException(HttpServletResponse.SC_NO_CONTENT, "no matching contacts");
         } finally {
             if (iterator instanceof QueryResultIterator)
                 ((QueryResultIterator) iterator).finished();
         }
     }
 
+    @Override
     public boolean supportsSave() {
         return true;
     }
 
-    public void saveCallback(Context context, String contentType, Folder folder, String filename) throws ServiceException, IOException, UserServletException {
+    @Override
+    public void saveCallback(Context context, String contentType, Folder folder, String filename)
+        throws ServiceException, IOException, UserServletException {
+
         byte[] body = context.getPostBody();
-        List<VCard> cards = VCard.parseVCard(new String(body, MimeConstants.P_CHARSET_UTF8));
-        
-        if (cards == null || cards.size() == 0 || (cards.size() == 1 && cards.get(0).fields.isEmpty()))
-            throw new UserServletException(HttpServletResponse.SC_BAD_REQUEST, "no contact fields found in vcard");
+        List<VCard> cards = VCard.parseVCard(new String(body, context.getCharset()));
+
+        if (cards == null || cards.size() == 0 ||
+                (cards.size() == 1 && cards.get(0).fields.isEmpty())) {
+            throw new UserServletException(HttpServletResponse.SC_BAD_REQUEST,
+                    "no contact fields found in vcard");
+        }
+
         for (VCard vcf : cards) {
             if (vcf.fields.isEmpty())
                 continue;
