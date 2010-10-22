c78a70c61ad776e4984067f82820a6e05059ec84
Unknown Perforce User <jflanigan>
2010-10-22 13:42:15 -0500

    (split)Integrate change 254969 from ZDESKTOP-608        bug: 51538
           Additional cases for retry handling.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=51538
    
    Copied from Perforce
     Change: 254971

--- src/java/com/zimbra/cs/db/RetryConnectionFactory.java
+++ src/java/com/zimbra/cs/db/RetryConnectionFactory.java
@@ -17,8 +17,25 @@ public class RetryConnectionFactory extends DriverManagerConnectionFactory {
 
     @Override 
     public java.sql.Connection createConnection() throws SQLException {
-        java.sql.Connection conn = super.createConnection();
-        Db.getInstance().postCreate(conn);
-        return new RetryConnection(conn);
+        AbstractRetry<java.sql.Connection> exec = new AbstractRetry<java.sql.Connection>() {
+            @Override
+            public ExecuteResult<java.sql.Connection> execute() throws SQLException {
+                java.sql.Connection conn = superCreateConnection();
+                Db.getInstance().postCreate(conn);
+                return new ExecuteResult<java.sql.Connection>(new RetryConnection(conn));
+            }
+
+            @Override
+            protected boolean retryException(SQLException sqle) {
+                //TODO: add new error codes in Db and consult those instead. Currently tightly coupled to SQLite
+                return (super.retryException(sqle) || sqle.getMessage().contains("SQLITE_CANTOPEN"));
+            }
+            
+        };
+        return exec.doRetry().getResult();
+    }
+    
+    private java.sql.Connection superCreateConnection() throws SQLException {
+        return super.createConnection();
     }
 }
