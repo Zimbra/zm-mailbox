08c29cfc1d0d616f240a5b1ffbddb3b25379551f
Unknown Perforce User <ysasaki>
2010-11-09 18:03:58 -0600

    bug:20795 Mark soap_fault_include_stack_trace as reloadable for QA
    http://bugzilla.zimbra.com/show_bug.cgi?id=20795
    
    Copied from Perforce
     Change: 257202

--- src/java/com/zimbra/common/localconfig/LC.java
+++ src/java/com/zimbra/common/localconfig/LC.java
@@ -163,10 +163,10 @@ public final class LC {
 
     public static final KnownKey stats_img_folder = KnownKey.newKey("${zimbra_home}/logger/db/work");
 
-    public static final KnownKey soap_fault_include_stack_trace = KnownKey.newKey(true);
+    public static final KnownKey soap_fault_include_stack_trace = KnownKey.newKey(true).reloadable();
     public static final KnownKey soap_response_buffer_size = KnownKey.newKey("")
         .desc("the size of the content buffer for sending SOAP http responses. if not set, use jetty default.");
-    public static final KnownKey soap_response_chunked_transfer_encoding_disabled = KnownKey.newKey(false);
+    public static final KnownKey soap_response_chunked_transfer_encoding_enabled = KnownKey.newKey(true).reloadable();
     public static final KnownKey zimbra_servlet_output_stream_buffer_size = KnownKey.newKey(5120);
     public static final KnownKey servlet_max_concurrent_requests_per_session = KnownKey.newKey(0)
         .reloadable()
