f5968c42e39ae8acac7f0026852400cf019b37dc
Unknown Perforce User <jflanigan>
2010-12-09 10:42:46 -0600

    bug: 54227
    Set response Qname. This is normally taken care of when service is registered but not if we make a new instance and use it without registering.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=54227
    
    Copied from Perforce
     Change: 261922

--- src/java/com/zimbra/common/soap/MailConstants.java
+++ src/java/com/zimbra/common/soap/MailConstants.java
@@ -192,6 +192,7 @@ public class MailConstants {
     public static final QName SEND_INVITE_REPLY_REQUEST = QName.get("SendInviteReplyRequest", NAMESPACE);
     public static final QName ICAL_REPLY_REQUEST = QName.get("ICalReplyRequest", NAMESPACE);
     public static final QName GET_FREE_BUSY_REQUEST = QName.get("GetFreeBusyRequest", NAMESPACE);
+    public static final QName GET_FREE_BUSY_RESPONSE = QName.get("GetFreeBusyResponse", NAMESPACE);
     public static final QName GET_WORKING_HOURS_REQUEST = QName.get("GetWorkingHoursRequest", NAMESPACE);
     public static final QName GET_ICAL_REQUEST = QName.get("GetICalRequest", NAMESPACE);
     public static final QName ANNOUNCE_ORGANIZER_CHANGE_REQUEST = QName.get("AnnounceOrganizerChangeRequest", NAMESPACE);
