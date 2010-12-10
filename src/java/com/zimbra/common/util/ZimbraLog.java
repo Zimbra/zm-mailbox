9b41c919c22b989c5e73a6d67bc848b75b379a3d
Unknown Perforce User <ysasaki>
2010-12-10 15:53:49 -0600

    bug:54328 Fix ArrayIndexOutOfBoundsException in InternetAddress.isValidDotAtom() - integrating from HELIX
    http://bugzilla.zimbra.com/show_bug.cgi?id=54328
    
    Copied from Perforce
     Change: 262260

--- src/java/com/zimbra/common/util/ZimbraLog.java
+++ src/java/com/zimbra/common/util/ZimbraLog.java
@@ -137,8 +137,7 @@ public final class ZimbraLog {
     /**
      * the "zimbra.index.add" logger. For the add-to-the-index part of indexing
      */
-    public static final Log index_add = LogFactory.getLog("zimbra.index.indexadd");
-
+    public static final Log indexing = LogFactory.getLog("zimbra.index.indexing");
 
     /**
      * Fhe "zimbra.searchstat" logger.  For logging statistics about what kinds of searches are run
