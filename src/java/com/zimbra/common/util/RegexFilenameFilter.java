bd876c7cbea1c1d8dc110513e97c3defa00bd426
Unknown Perforce User <jflanigan>
2010-12-17 13:04:05 -0600

    bug: 47552
    Backup of ZD mailboxes. Backup can be scheduled to run automatically or invoked manually through a SOAP service.
    
    Configuration is based on the following new preferences:
    zimbraPrefOfflineBackupInterval - Interval in ms to create a backup. UI should allow user to select values such as once a day, once a week, once a month, etc
    zimbraPrefOfflineBackupAccountId - Multi-value attribute specifying account ids for backup. 
    zimbraPrefOfflineBackupPath - Directory in which to store backup files. 
    zimbraPrefOfflineBackupKeep - Number of previous backups to keep; minimum value of 1
    
    SOAP service immediately produces backup regardless of schedule.
    
    Example 1, backup a single account by ID:
    <AccountBackupRequest id="ffffffff-ffff-ffff-ffff-ffffffffffff" xmlns="urn:zimbraOffline"/>
    <AccountBackupResponse id="ffffffff-ffff-ffff-ffff-ffffffffffff" xmlns="urn:zimbraOffline"/>
    
    Example 2, backup all accounts previously specified in zimbraPrefOfflineBackupAccountId:
    <AccountBackupRequest xmlns="urn:zimbraOffline"/>
    <AccountBackupResponse xmlns="urn:zimbraOffline"/>
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=47552
    
    Copied from Perforce
     Change: 263707

---
+++ src/java/com/zimbra/common/util/RegexFilenameFilter.java
@@ -0,0 +1,33 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2010 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.common.util;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class RegexFilenameFilter implements FilenameFilter {
+
+    protected String regex;
+    
+    public RegexFilenameFilter(String regex) {
+        super();
+        this.regex = regex;
+    }
+
+    @Override
+    public boolean accept(File dir, String name) {
+        return name.matches(regex);
+    }
+}
