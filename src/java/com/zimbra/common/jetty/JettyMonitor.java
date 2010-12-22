d81082fd17bbefb5ce311b4f927b2b8c6885a094
Unknown Perforce User <bburtin>
2010-12-22 13:35:47 -0600

    bug: 30046 - Log a warning when all protocol handler threads are in use
    Now tracking stats for the total number of threads in the Jetty
    thread pool and the number of idle threads.  Turns out that Jetty
    reserves some of the threads for internal use, so getting the number
    of concurrent HTTP requests is trickier than I expected.  Hopefully
    this is good enough.
    
    Added two columns to mailboxd.csv:
    
    http_threads
    http_idle_threads
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=30046
    
    Copied from Perforce
     Change: 264402

---
+++ src/java/com/zimbra/common/jetty/JettyMonitor.java
@@ -0,0 +1,36 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2010 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.common.jetty;
+
+import org.mortbay.thread.ThreadPool;
+
+/**
+ * Holds on to a reference to the Jetty thread pool.  Called
+ * by the Jetty startup code (see jetty.xml).
+ */
+public class JettyMonitor {
+
+    private static ThreadPool threadPool = null;
+    
+    public synchronized static void setThreadPool(ThreadPool pool) {
+        System.out.println(JettyMonitor.class.getSimpleName() + " monitoring thread pool " + pool);
+        threadPool = pool;
+    }
+    
+    public synchronized static ThreadPool getThreadPool() {
+        return threadPool;
+    }
+}
