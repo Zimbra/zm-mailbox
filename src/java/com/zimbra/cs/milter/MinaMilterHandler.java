3a655e1c8172f09a173d23c3cf476e7d0992d447
Unknown Perforce User <ysasaki>
2011-01-04 12:20:51 -0600

    bug:9470 Reject new connections if NIO server is overloaded - NIO servers reject new connections if the number of concurrent connections exceeded the configured value. - Reworked NIO server thread related config. - Removed some blocking operations in NIO server code.
    https://reviewboard.eng.vmware.com/r/197358/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=9470
    
    Copied from Perforce
     Change: 267771

--- src/java/com/zimbra/cs/milter/MinaMilterHandler.java
+++ src/java/com/zimbra/cs/milter/MinaMilterHandler.java
@@ -1,13 +1,13 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2010 Zimbra, Inc.
- * 
+ * Copyright (C) 2010, 2011 Zimbra, Inc.
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -22,48 +22,35 @@ import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.mina.MinaHandler;
 import com.zimbra.cs.mina.MinaSession;
 
-public class MinaMilterHandler extends MilterHandler implements MinaHandler {
-    private MinaMilterServer server;
-    private MinaSession session;
-    private MilterConfig config;
-    
+final class MinaMilterHandler extends MilterHandler implements MinaHandler {
+    private final MinaSession session;
+
     MinaMilterHandler(MinaMilterServer server, MinaSession session) {
         super(server);
-        this.server = server;
         this.session = session;
-        config = server.getConfig();
     }
-        
-    @Override public void connectionClosed() throws IOException {
+
+    @Override
+    public void connectionClosed() throws IOException {
         ZimbraLog.milter.info(sessPrefix + "Connection closed");
         session.close();
     }
 
-    @Override public void connectionIdle() throws IOException {
+    @Override
+    public void connectionIdle() throws IOException {
         notifyIdleConnection();
     }
 
-    @Override public void connectionOpened() throws IOException {
-        if (server.getStats().getActiveSessions() >= config.getNioMaxSessions()) {
-            ZimbraLog.milter.warn("Dropping connection (max sessions exceeded)");
-            dropConnection();
-        } else {
-            newSession();
-            ZimbraLog.milter.info(sessPrefix + "Connection opened");
-        }
+    @Override
+    public void connectionOpened() throws IOException {
+        newSession();
+        ZimbraLog.milter.info(sessPrefix + "Connection opened");
     }
 
-    @Override public void dropConnection(long timeout) throws IOException {
-        if (!session.isClosed()) {
-            if (!session.drainWriteQueue(timeout))
-                ZimbraLog.milter.warn(sessPrefix + "Force closing connection with unsent data");
-            session.close();
-        }
-    }
-
-    @Override public void messageReceived(Object msg) throws IOException {
+    @Override
+    public void messageReceived(Object msg) throws IOException {
         MilterPacket command = (MilterPacket) msg;
-        
+
         try {
             MilterPacket response = processCommand(command);
             if (response != null) {
@@ -74,25 +61,31 @@ public class MinaMilterHandler extends MilterHandler implements MinaHandler {
             dropConnection(); // aborting the session
         }
     }
-    
-    @Override protected boolean setupConnection(Socket connection) throws IOException {
+
+    @Override
+    protected boolean setupConnection(Socket connection) throws IOException {
         throw new UnsupportedOperationException();
     }
 
-    @Override protected boolean authenticate() throws IOException {
+    @Override
+    protected boolean authenticate() throws IOException {
         throw new UnsupportedOperationException();
     }
 
-    @Override protected boolean processCommand() throws Exception {
+    @Override
+    protected boolean processCommand() throws Exception {
         throw new UnsupportedOperationException();
     }
 
-    @Override protected void dropConnection() {
-        if (!session.isClosed())
+    @Override
+    public void dropConnection() {
+        if (!session.isClosed()) {
             session.close();
+        }
     }
 
-    @Override protected void notifyIdleConnection() {
+    @Override
+    protected void notifyIdleConnection() {
         ZimbraLog.milter.debug(sessPrefix + "Dropping connection for inactivity");
         dropConnection();
     }
