6195a04e49e21d3827c2bebfb0684016dab14589
Unknown Perforce User <jflanigan>
2011-01-04 14:49:57 -0600

    Integrate change 267804 from HELIX        bug: 54895
           Only send UNSELECT and CLOSE if in selected state. 
           Also use normal timeout (60s) rather than idle timeout (1800s) when 
           issuing UNSELECT. This allows us to read untagged BAD (which may or 
           may not have more lines) for a reasonable period before timing out.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=54895
    
    Copied from Perforce
     Change: 267805

--- src/java/com/zimbra/cs/milter/MinaMilterDecoder.java
+++ src/java/com/zimbra/cs/milter/MinaMilterDecoder.java
@@ -2,20 +2,20 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2010 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
  */
 package com.zimbra.cs.milter;
 
-import org.apache.mina.common.ByteBuffer;
-import org.apache.mina.common.IoSession;
+import org.apache.mina.core.buffer.IoBuffer;
+import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.CumulativeProtocolDecoder;
 import org.apache.mina.filter.codec.ProtocolDecoderOutput;
 
@@ -23,15 +23,16 @@ import com.zimbra.cs.mina.MinaStats;
 
 public class MinaMilterDecoder extends CumulativeProtocolDecoder {
     private final MinaStats stats;
-    
+
     MinaMilterDecoder(MinaStats stats) {
         this.stats = stats;
     }
-    
-    @Override public boolean doDecode(IoSession session, ByteBuffer in, ProtocolDecoderOutput out) {
+
+    @Override
+    public boolean doDecode(IoSession session, IoBuffer in, ProtocolDecoderOutput out) {
         if (!in.prefixedDataAvailable(4))
             return false;
-        
+
         int len = in.getInt();
         byte cmd = in.get();
         byte[] data = null;
@@ -41,7 +42,7 @@ public class MinaMilterDecoder extends CumulativeProtocolDecoder {
         }
         MilterPacket packet = new MilterPacket(len, cmd, data);
         out.write(packet);
-        
+
         if (stats != null) {
             stats.receivedBytes.addAndGet(len + 4);
         }
