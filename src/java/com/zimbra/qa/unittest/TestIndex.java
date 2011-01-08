e01b8dc1ba8fe7c93b18eab602a379454a4562ff
Unknown Perforce User <bburtin>
2011-01-07 18:33:44 -0600

    (split)bug: 54613 - filename:Blob* matches all briefcase items
    BlobDataSource.getName() now returns null.  Added a unit
    test that searches for documents by filename.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=54613
    
    Copied from Perforce
     Change: 268782

--- src/java/com/zimbra/qa/unittest/TestIndex.java
+++ src/java/com/zimbra/qa/unittest/TestIndex.java
@@ -16,12 +16,14 @@ package com.zimbra.qa.unittest;
 
 import java.util.List;
 
+import junit.framework.TestCase;
+
+import com.zimbra.common.mime.MimeConstants;
 import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.cs.zclient.ZMailbox;
 import com.zimbra.cs.zclient.ZMessage;
-import com.zimbra.common.mime.MimeConstants;
-
-import junit.framework.TestCase;
+import com.zimbra.cs.zclient.ZSearchParams;
 
 
 public class TestIndex extends TestCase {
@@ -80,6 +82,19 @@ public class TestIndex extends TestCase {
         msgId = sendMessage(subject, attachedMsg.getBytes(), "attachment.msg", MimeConstants.CT_MESSAGE_RFC822).getId();
         checkQuery("in:inbox subject:\"" + subject + "\" pigs", msgId);
         checkQuery("in:inbox subject:\"" + subject + "\" gun", null);
+    }
+    
+    /**
+     * Verifies the fix to bug 54613.
+     */
+    public void testFilenameSearch()
+    throws Exception {
+        ZMailbox mbox = TestUtil.getZMailbox(USER_NAME);
+        String filename = NAME_PREFIX + " testFilenameSearch.txt";
+        TestUtil.createDocument(mbox, Integer.toString(Mailbox.ID_FOLDER_BRIEFCASE),
+            filename, "text/plain", "This is the data for testFilenameSearch.".getBytes());
+        assertEquals(0, TestUtil.search(mbox, "filename:Blob*", ZSearchParams.TYPE_DOCUMENT).size());
+        assertEquals(1, TestUtil.search(mbox, "filename:\"" + filename + "\"", ZSearchParams.TYPE_DOCUMENT).size());
     }
     
     /**
