0face3a20931c7aeefae3e42b437d8d422f62cec
Unknown Perforce User <pshao>
2011-01-12 18:52:06 -0600

    (split)bug: 54308
    refactored checkHardRules to its own class.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=54308
    
    Copied from Perforce
     Change: 269813

--- src/java/com/zimbra/cs/account/accesscontrol/AccessControlUtil.java
+++ src/java/com/zimbra/cs/account/accesscontrol/AccessControlUtil.java
@@ -18,7 +18,6 @@ import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.AuthToken;
-import com.zimbra.cs.account.Entry;
 import com.zimbra.cs.account.GuestAccount;
 import com.zimbra.cs.account.Provisioning;
 
@@ -30,42 +29,6 @@ public class AccessControlUtil {
     
     static boolean isDelegatedAdmin(Account acct, boolean asAdmin) {
         return (asAdmin && acct != null && acct.getBooleanAttr(Provisioning.A_zimbraIsDelegatedAdminAccount, false));
-    }
-    
-    /**
-     * entry point for each and every ACL checking calls.
-     * 
-     * Currently, the only check is if the authed account is a system admin.
-     * 
-     * @param authedAcct
-     * @param target
-     * @return
-     * @throws ServiceException
-     */
-    public static Boolean checkHardRules(Account authedAcct, boolean asAdmin, Entry target, Right right) throws ServiceException {
-        if (AccessControlUtil.isGlobalAdmin(authedAcct, asAdmin)) {
-            return Boolean.TRUE;
-        } else {
-            boolean isAdminRight = (right == null || !right.isUserRight());
-            
-            // We are checking an admin right
-            if (isAdminRight) {
-                // 1. ensure the authed account must be a delegated admin
-                if (!AccessControlUtil.isDelegatedAdmin(authedAcct, asAdmin))
-                    throw ServiceException.PERM_DENIED("not an eligible admin account");
-                
-                // 2. don't allow a delegated-only admin to access a global admin's account,
-                //    no matter how much rights it has
-                if (target instanceof Account) {
-                    if (AccessControlUtil.isGlobalAdmin((Account)target, true))
-                        // return Boolean.FALSE;
-                        throw ServiceException.PERM_DENIED("delegated admin is not allowed to access a global admin's account");
-                }
-            }
-        }
-        
-        // hard rules are not applicable
-        return null;
     }
     
     static public Account authTokenToAccount(AuthToken authToken, Right rightNeeded) {
---
+++ src/java/com/zimbra/cs/account/accesscontrol/HardRules.java
@@ -0,0 +1,45 @@
+package com.zimbra.cs.account.accesscontrol;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.Entry;
+
+public class HardRules {
+
+    /**
+     * entry point for each and every ACL checking calls.
+     * 
+     * Currently, the only check is if the authed account is a system admin.
+     * 
+     * @param authedAcct
+     * @param target
+     * @return
+     * @throws ServiceException
+     */
+    public static Boolean checkHardRules(Account authedAcct, boolean asAdmin, Entry target, Right right) throws ServiceException {
+        if (AccessControlUtil.isGlobalAdmin(authedAcct, asAdmin)) {
+            return Boolean.TRUE;
+        } else {
+            boolean isAdminRight = (right == null || !right.isUserRight());
+            
+            // We are checking an admin right
+            if (isAdminRight) {
+                // 1. ensure the authed account must be a delegated admin
+                if (!AccessControlUtil.isDelegatedAdmin(authedAcct, asAdmin))
+                    throw ServiceException.PERM_DENIED("not an eligible admin account");
+                
+                // 2. don't allow a delegated-only admin to access a global admin's account,
+                //    no matter how much rights it has
+                if (target instanceof Account) {
+                    if (AccessControlUtil.isGlobalAdmin((Account)target, true))
+                        // return Boolean.FALSE;
+                        throw ServiceException.PERM_DENIED("delegated admin is not allowed to access a global admin's account");
+                }
+            }
+        }
+        
+        // hard rules are not applicable
+        return null;
+    }
+
+}
