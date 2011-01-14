c1ddaf13b8653b5936e5c549f8633a7354ce9d92
Unknown Perforce User <jylee>
2011-01-14 11:24:15 -0600

    (split)bug 55545 when migrating the documents update the mailbox version first before attempting the migration.  that way if the same mailbox is opened again during the migration it won't attempt to go through the migration then, resulting infinite recursion.
    http://bugzilla.zimbra.com/show_bug.cgi?id=55545
    
    Copied from Perforce
     Change: 270162

--- src/java/com/zimbra/cs/mailbox/Mailbox.java
+++ src/java/com/zimbra/cs/mailbox/Mailbox.java
@@ -485,8 +485,11 @@ public class Mailbox {
 
             // bug 39647: wiki to document migration
             if (!getVersion().atLeast(1, 10)) {
-                migrateWikiFolders();
+                // update the version first so that the same mailbox
+                // don't have to go through the migration again
+                // if it was called to open() during the migration.
                 updateVersion(new MailboxVersion((short) 1, (short) 10));
+                migrateWikiFolders();
             }
 
             // done!
