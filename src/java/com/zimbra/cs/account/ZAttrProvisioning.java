80b22cab201b1ede335383c4f3e8c0614a382a03
Unknown Perforce User <pshao>
2011-01-17 16:37:22 -0600

    (split)bug: 40184
    default zimbraFilterBatchSize to 10000 for now until batching in UI is implemented. 
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=40184
    
    Copied from Perforce
     Change: 270611

--- src/java/com/zimbra/cs/account/ZAttrProvisioning.java
+++ src/java/com/zimbra/cs/account/ZAttrProvisioning.java
@@ -28,7 +28,7 @@ public class ZAttrProvisioning {
 
     ///// BEGIN-AUTO-GEN-REPLACE
 
-    /* build: 7.0.0_BETA1_1111 pshao 20110112-1619 */
+    /* build: 7.0.0_BETA1_1111 pshao 20110117-1427 */
 
     public static enum AccountCalendarUserType {
         RESOURCE("RESOURCE"),
