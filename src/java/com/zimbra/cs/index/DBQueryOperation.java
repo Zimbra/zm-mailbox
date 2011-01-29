8cbe5242182c5f018d9fd6d1dec21302e6d13a8d
Unknown Perforce User <ysasaki>
2011-01-29 13:36:07 -0600

    (split)bug:56048 Set QueryTarget when adding ItemID to DBQueryOperation - When adding an ItemID to a DBQueryOperation, set QueryTarget (LOCAL or REMOTE) accordingly, so that imcompatible clauses (i.e. LOCAL AND REMOTE) are pruned on the optimization phase.
    https://reviewboard.eng.vmware.com/r/204585/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=56048
    
    Copied from Perforce
     Change: 273086

--- src/java/com/zimbra/cs/index/DBQueryOperation.java
+++ src/java/com/zimbra/cs/index/DBQueryOperation.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -272,13 +272,15 @@ public class DBQueryOperation extends QueryOperation {
         mAllResultsQuery = false;
         if (itemId.belongsTo(mbox)) {
             // LOCAL
+            assert mQueryTarget.isCompatibleLocal() : topLevelAndedConstraint() + "," + itemId;
+            mQueryTarget = QueryTarget.LOCAL;
             topLevelAndedConstraint().addItemIdClause(itemId.getId(), truth);
         } else {
             // REMOTE
+            assert mQueryTarget != QueryTarget.LOCAL : topLevelAndedConstraint() + "," + itemId;
+            mQueryTarget = new QueryTarget(itemId.getAccountId());
             topLevelAndedConstraint().addRemoteItemIdClause(itemId, truth);
-
         }
-
     }
 
     public void addDateClause(long lowestDate, boolean lowestEq, long highestDate, boolean highestEq, boolean truth)  {
