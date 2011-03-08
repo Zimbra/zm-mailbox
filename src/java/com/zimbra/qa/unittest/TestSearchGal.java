8a734d0681e134942f7d246887a7730083bbd911
Unknown Perforce User <pshao>
2011-03-07 22:51:52 -0600

    (split)bug: 57425
    refactor the pass thru part of FilteredGalSearchResultCallback to a seperate abstract class GalSearchResultCallback.PassThruGalSearchResultCallback so it can be extended by other classes.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=57425
    
    Copied from Perforce
     Change: 280460

--- src/java/com/zimbra/qa/unittest/TestSearchGal.java
+++ src/java/com/zimbra/qa/unittest/TestSearchGal.java
@@ -1,3 +1,18 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2006, 2007, 2009, 2010, 2011 Zimbra, Inc.
+ *
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+
 package com.zimbra.qa.unittest;
 
 import java.util.ArrayList;
@@ -69,7 +84,7 @@ public class TestSearchGal {
     }
     */
     
-    static void authUser(SoapTransport transport, String acctName) throws Exception {
+    public static void authUser(SoapTransport transport, String acctName) throws Exception {
         
         Element request = Element.create(transport.getRequestProtocol(), AccountConstants.AUTH_REQUEST);
         request.addElement(AccountConstants.E_ACCOUNT).addAttribute(AccountConstants.A_BY, AccountBy.name.name()).setText(acctName);
@@ -80,7 +95,7 @@ public class TestSearchGal {
         transport.setAuthToken(authToken);
     }
     
-    static void disableGalSyncAccount(String domainName) throws Exception {
+    public static void disableGalSyncAccount(String domainName) throws Exception {
         Provisioning prov = Provisioning.getInstance();
         
         Domain domain = prov.get(DomainBy.name, domainName);
@@ -99,7 +114,7 @@ public class TestSearchGal {
         both
     }
   
-    static void enableGalSyncAccount(String domainName) throws Exception {
+    public static void enableGalSyncAccount(String domainName) throws Exception {
         enableGalSyncAccount(domainName, GSAType.zimbra);
     }
     
