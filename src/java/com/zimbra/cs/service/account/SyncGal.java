379332eca9cbdaab75664ae6e3cb7ff2cf0cfde3
Unknown Perforce User <jpowers>
2011-03-11 16:02:31 -0600

    (split)Bug 57716
    Added a workaround similar to the one for the archived tarball issue so that the timer task is canceled for SyncGal requests. This will allow requests with longer sync times to complete without getting canceled.
    
    for archive formatter reference:
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=57716
    
    Copied from Perforce
     Change: 281205

--- src/java/com/zimbra/cs/service/account/SyncGal.java
+++ src/java/com/zimbra/cs/service/account/SyncGal.java
@@ -20,6 +20,12 @@ package com.zimbra.cs.service.account;
 
 import java.util.Map;
 
+import org.mortbay.io.EndPoint;
+import org.mortbay.io.nio.SelectChannelEndPoint;
+import org.mortbay.jetty.HttpConnection;
+import org.mortbay.thread.Timeout;
+
+import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.mailbox.ContactConstants;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.AccountConstants;
@@ -40,6 +46,8 @@ import com.zimbra.soap.ZimbraSoapContext;
 public class SyncGal extends GalDocumentHandler {
 
     public Element handle(Element request, Map<String, Object> context) throws ServiceException {
+        disableJettyTimeout();
+        
         ZimbraSoapContext zsc = getZimbraSoapContext(context);
         Account account = getRequestedAccount(getZimbraSoapContext(context));
 
@@ -135,6 +143,37 @@ public class SyncGal extends GalDocumentHandler {
                 }
             }
             super.handleContact(c);
+        }
+    }
+    
+    
+    /**
+     * Implemented for bug 56458..
+     * 
+     * Disable the Jetty timeout for the SelectChannelConnector and the SSLSelectChannelConnector
+     * for this request.
+     * 
+     * By default (and our normal configuration) Jetty has a 30 second idle timeout (10 if the server is busy) for 
+     * connection endpoints. There's another task that keeps track of what connections have timeouts and periodically
+     * works over a queue and closes endpoints that have been timed out. This plays havoc with downloads to slow connections
+     * and whenever we have a long pause while working to create an archive.
+     * 
+     * This method removes this request from the queue to check timeout via the mechanisms that are normally used
+     * after jetty completes a request. Given that we don't send a content-length down to the browser for
+     * archive responses, we have to close the socket to tell the browser its done. Since we have to do that.. leaving this 
+     * endpoint without a timeout is safe. If the connection was being reused (ie keep-alive) this could have issues, but its not 
+     * in this case.
+     */
+    private void disableJettyTimeout() {
+        if (LC.zimbra_gal_sync_disable_timeout.booleanValue()) {
+            EndPoint endPoint = HttpConnection.getCurrentConnection().getEndPoint();
+            if (endPoint instanceof SelectChannelEndPoint) {
+                SelectChannelEndPoint scEndPoint = (SelectChannelEndPoint) endPoint;
+                Timeout.Task task = scEndPoint.getTimeoutTask();
+                if (task != null) {
+                    task.cancel();
+                }
+            }
         }
     }
 }
