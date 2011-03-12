6ad05c91931e65fc764a4aa5d3b73533e8dea998
Unknown Perforce User <pjoseph>
2011-03-11 18:35:32 -0600

    (split)bug: 57799 rename as ApplianceProxyServlet and readd proxy servlet between jetty and tomcat
    http://bugzilla.zimbra.com/show_bug.cgi?id=57799
    
    Copied from Perforce
     Change: 281270

---
+++ src/java/com/zimbra/cs/appliance/servapp/ApplianceProxyServlet.java
@@ -0,0 +1,224 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Appliance
+ * Copyright (C) 2011 VMware, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.appliance.servapp;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.httpclient.Cookie;
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.HttpState;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.InputStreamRequestEntity;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.HttpException;
+import com.zimbra.cs.appliance.httpclient.HttpProxyUtil;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.ByteArrayRequestEntity;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+/**
+ * @author pjoseph
+ */
+@SuppressWarnings("serial")
+public class ApplianceProxyServlet extends HttpServlet {
+    private static final String TARGET_PARAM = "target";
+    private static final String USER_PARAM = "cimuser";
+    private static final String PASS_PARAM = "cimpass";
+    private static final String FORMAT_PARAM = "fmt";
+    private static final String FILENAME_PARAM = "filename";
+    private static final String AUTH_PARAM = "cimauth";
+    private static final String AUTH_BASIC = "basic";
+   
+
+    
+    private boolean canProxyHeader(String header) {
+        if (header == null) return false;
+        header = header.toLowerCase();
+        if (header.startsWith("accept") ||
+            header.equals("content-length") ||
+            header.equals("connection") ||
+            header.equals("keep-alive") ||
+            header.equals("expires") ||
+            header.equals("pragma") ||
+            header.equals("host") ||
+            //header.equals("user-agent") ||
+            header.equals("cache-control") ||
+            header.equals("cookie") ||
+            header.equals("transfer-encoding")) {
+            return false;
+        }
+        return true;
+    }
+    
+    private byte[] copyPostedData(HttpServletRequest req) throws IOException {
+        int size = req.getContentLength();
+        if (req.getMethod().equalsIgnoreCase("GET") || size <= 0) {
+            return null;
+        }
+        InputStream is = req.getInputStream();
+        ByteArrayOutputStream baos = null;
+        try {
+            if (size < 0)
+                size = 0; 
+            baos = new ByteArrayOutputStream(size);
+            byte[] buffer = new byte[8192];
+            int num;
+            while ((num = is.read(buffer)) != -1) {
+                baos.write(buffer, 0, num);
+            }
+            return baos.toByteArray();
+        } finally {
+            com.zimbra.cs.appliance.util.ByteUtil.closeStream(baos);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        doProxy(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        doProxy(req, resp);
+    }
+
+    private static final String DEFAULT_CTYPE = "text/xml";
+
+    private void doProxy(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        // get the posted body before the server read and parse them.
+        byte[] body = copyPostedData(req);
+
+        // sanity check
+        String target = req.getParameter(TARGET_PARAM);
+        if (target == null) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        URL url = new URL(target);
+
+
+        HttpMethod method = null;
+        try {
+            HttpClient client = new HttpClient();
+            com.zimbra.cs.appliance.httpclient.HttpProxyUtil.configureProxy(client,url.toString());
+            String reqMethod = req.getMethod();
+            if (reqMethod.equalsIgnoreCase("GET"))
+                method = new GetMethod(target);
+            else if (reqMethod.equalsIgnoreCase("POST")) {
+                PostMethod post = new PostMethod(target);
+                if (body != null)
+                    post.setRequestEntity(new ByteArrayRequestEntity(body, req.getContentType()));
+                
+                Enumeration<String> params = req.getParameterNames();
+                if(params != null) {
+                	while(params.hasMoreElements()) {
+                		String paramName = params.nextElement();
+                		if(TARGET_PARAM.equalsIgnoreCase(paramName)) {
+                			continue;
+                		}
+                		post.addParameter(paramName, req.getParameter(paramName));
+                	}
+                	
+                }
+                method = post;
+            } else {
+                resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+                return;
+            }
+
+            // handle basic auth
+            String auth, user, pass;
+            auth = req.getHeader(AUTH_PARAM);
+            user = req.getHeader(USER_PARAM);
+            pass = req.getHeader(PASS_PARAM);
+            if (auth != null && user != null && pass != null) {
+                if (!auth.equals(AUTH_BASIC)) {
+                    resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                    return;
+                }
+                HttpState state = client.getState();
+                client.getParams().setAuthenticationPreemptive(true);
+                AuthScope authScope = new AuthScope(url.getHost(),url.getPort());
+                state.setCredentials(authScope, new UsernamePasswordCredentials(user, pass));
+            }
+            
+            Enumeration headers = req.getHeaderNames();
+            while (headers.hasMoreElements()) {
+                String hdr = (String) headers.nextElement();
+                if (canProxyHeader(hdr)) {
+                	if (hdr.equalsIgnoreCase("x-host"))
+                		method.getParams().setVirtualHost(req.getHeader(hdr));
+                	else
+                		method.addRequestHeader(hdr, req.getHeader(hdr));
+                }
+            }
+            
+            try {
+                client.executeMethod(method);
+            } catch (HttpException ex) {
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+                return;
+            }
+
+            int status = method.getStatusLine() == null ? HttpServletResponse.SC_INTERNAL_SERVER_ERROR : method.getStatusCode();
+
+            // workaround for Alexa Thumbnails paid web service, which doesn't bother to return a content-type line
+            Header ctHeader = method.getResponseHeader("Content-Type");
+            String contentType = ctHeader == null || ctHeader.getValue() == null ? DEFAULT_CTYPE : ctHeader.getValue();
+
+            InputStream targetResponseBody = method.getResponseBodyAsStream();
+            
+            resp.setStatus(status);
+            resp.setContentType(contentType);
+            //no caching!   
+            resp.setHeader("Expires", "Tue, 24 Jan 2000 17:46:50 GMT");
+            // Set standard HTTP/1.1 no-cache headers.
+            resp.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
+            // Set standard HTTP/1.0 no-cache header.
+            resp.setHeader("Pragma", "no-cache");
+            
+            for (Header h : method.getResponseHeaders()) {
+                if (canProxyHeader(h.getName())) {
+                	resp.addHeader(h.getName(), h.getValue());
+                }
+            }
+            if (targetResponseBody != null) {
+            	com.zimbra.cs.appliance.util.ByteUtil.copy(targetResponseBody, true, resp.getOutputStream(), true);
+            }
+        } finally {
+            if (method != null)
+                method.releaseConnection();
+        }
+    }
+}
---
+++ src/java/com/zimbra/cs/appliance/util/BufferStream.java
@@ -0,0 +1,443 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2009, 2010 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.appliance.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.SequenceInputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
+
+public class BufferStream extends OutputStream {
+    private byte buf[] = null;
+    private File file = null;
+    private FileOutputStream fos = null;
+    private int maxMem;
+    private long maxSize;
+    private boolean sequenced = true;
+    private long size = 0;
+
+    public BufferStream() { this(0); }
+
+    public BufferStream(long sizeHint) { this(sizeHint, Integer.MAX_VALUE); }
+
+    public BufferStream(long sizeHint, int maxMem) {
+        this(sizeHint, maxMem, Long.MAX_VALUE);
+    }
+
+    public BufferStream(long sizeHint, int maxMem, long maxSize) {
+        if (maxMem > maxSize)
+            maxMem = (int)maxSize;
+        this.maxMem = maxMem;
+        this.maxSize = maxSize;
+        if (sizeHint > 0)
+            buf = new byte[(int)Math.min(sizeHint, maxMem)];
+    }
+
+    private int buffer(int len) {
+        if (buf == null) {
+            if (maxMem == 0)
+                return 0;
+            buf = new byte[Math.min(maxMem, Math.max(len, 2 * 1024))];
+            return buf.length;
+        } else if (len <= buf.length - size) {
+            return (int)(buf.length - size);
+        } else if (buf.length < maxMem) {
+            long tot = size + len;
+            byte newBuf[];
+
+            if (tot < buf.length << 1)
+                tot = Math.min(buf.length << 1, maxMem);
+            newBuf = new byte[(int)tot];
+            System.arraycopy(buf, 0, newBuf, 0, (int)size);
+            buf = newBuf;
+            return (int)(buf.length - size);
+        } else {
+            return 0;
+        }
+    }
+    
+    public void close() {
+        release();
+        buf = null;
+        maxSize = maxMem = 0;
+    }
+
+    public void copyTo(OutputStream os) throws IOException {
+        if (buf != null)
+            os.write(buf, 0, size <= buf.length ? (int)size : buf.length);
+        if (file != null) {
+            byte tmp[] = new byte[32 * 1024];
+            FileInputStream fis = new FileInputStream(file);
+            int in;
+
+            try {
+                while ((in = fis.read(tmp)) != -1)
+                    os.write(tmp, 0, in);
+            } finally {
+                fis.close();
+            }
+        }
+    }
+    
+    protected void finalize() throws Throwable {
+        release();
+        super.finalize();
+    }
+    
+    public byte[] getBuffer() {
+        try {
+            sync();
+        } catch (IOException e) {
+        }
+        if (buf != null && buf.length > size) {
+            byte newBuf[] = new byte[(int)size];
+            
+            System.arraycopy(buf, 0, newBuf, 0, (int)size);
+            buf = newBuf;
+        }
+        return buf;
+    }
+
+    public File getFile() throws IOException { 
+        sync();
+        return file;
+    }
+    
+    public InputStream getInputStream() throws IOException {
+        sync();
+        if (size > maxSize)
+            throw new EOFException("data exceeds copy capacity");
+        if (buf == null)
+            return file == null ? new ByteArrayInputStream(new byte[0]) :
+                new FileInputStream(file);
+
+        InputStream in = new ByteArrayInputStream(buf, 0, (int)Math.min(
+            buf.length, size));
+        
+        return file == null ? in : new SequenceInputStream(in, new
+            FileInputStream(file));
+    }
+    
+    public int getMaxMem() { return maxMem; }
+    
+    public long getMaxSize() { return maxSize; }
+    
+    public byte[] getRawBuffer() {
+        try {
+            sync();
+        } catch (IOException e) {
+        }
+        return buf;
+    }
+
+    public long getSize() {
+        try {
+            sync();
+        } catch (IOException e) {
+        }
+        return size;
+    }
+    
+    public boolean isPartial() { return size > maxMem && file == null; }
+
+    public boolean isSequenced() { return sequenced; }
+
+    public boolean isSpooled() { return file != null; }
+
+    public static BufferStream newFixedBufferStream(int len) {
+        return new BufferStream(len, len, len);
+    }
+
+    public long readFrom(InputStream is) throws IOException {
+        return readFrom(is, Long.MAX_VALUE);
+    }
+    
+    public long readFrom(InputStream is, long len) throws IOException {
+        int in;
+        int left = buffer(len == Long.MAX_VALUE ? 0 : (int)Math.min(
+            Integer.MAX_VALUE, len));
+        long out = 0;
+        
+        while (len > 0 && left > 0) {
+            if ((in = is.read(buf, (int)size, left)) == -1)
+                return out;
+            left -= in;
+            len -= in;
+            out += in;
+            size += in;
+        }
+        if (len == 0)
+            return out;
+        if (is.available() == 0) {
+            if ((in = is.read()) == -1)
+                return out;
+            write(in);
+            len--;
+        }
+        while (len > 0 && (left = buffer((int)Math.min(len, 8 * 1024))) > 0) {
+            if ((in = is.read(buf, (int)size, (int)Math.min(len, left))) == -1)
+                return out;
+            out += in;
+            size += in;
+            len -= in;
+        }
+        if (len == 0)
+            return out;
+
+        byte tmp[] = new byte[(int)Math.min(len, 32 * 1024)];
+        
+        while (len > 0) {
+            if ((in = is.read(tmp, 0, (int)Math.min(len, tmp.length))) == -1)
+                return out;
+            write(tmp, 0, in);
+            out += in;
+            len -= in;
+        }
+        return out;
+    }
+    
+    public void release() {
+        if (file != null) {
+            try {
+                fos.close();
+            } catch (Exception e) {
+            }
+            fos = null;
+            if (!file.delete())
+                file.deleteOnExit();
+            file = null;
+            maxSize = maxMem;
+        }
+    }
+    
+    public void reset() {
+        try {
+            sync();
+        } catch (IOException e) {
+        }
+        release();
+        size = 0;
+    }
+    
+    public void setSequenced(boolean sequenced) { this.sequenced = sequenced; }
+
+    public long size() { return getSize(); }
+    
+    protected boolean spool(int len) {
+        if (size + len > maxSize) {
+            release();
+            return false;
+        } else if (file == null) {
+            try {
+                file = File.createTempFile("cstrm", null, new File(
+                    "tmp"));
+                fos = new FileOutputStream(file);
+                if (!sequenced) {
+                    fos.write(buf);
+                    buf = null;
+                    maxMem = 0;
+                }
+            } catch (IOException e) {
+                file = null;
+                maxSize = maxMem;
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public void sync() throws IOException {
+        if (file != null) {
+            try {
+                fos.flush();
+            } catch (IOException e) {
+                release();
+                throw e;
+            }
+        }
+    }
+    
+    public byte[] toByteArray() {
+        try {
+            sync();
+        } catch (IOException e) {
+        }
+        if (size <= maxMem) {
+            byte tmp[] = getBuffer();
+            
+            return tmp == null ? new byte[0] : tmp;
+        } else if (file == null || size > Integer.MAX_VALUE) {
+            throw new RuntimeException("BufferStream overflow");
+        } else {
+            byte newBuf[] = new byte[(int)size];
+            FileInputStream fis = null;
+            
+            System.arraycopy(buf, 0, newBuf, 0, buf.length);
+            try {
+                int in;
+                int left = (int)(size - buf.length);
+                
+                fis = new FileInputStream(file);
+                while (left > 0) {
+                    if ((in = fis.read(newBuf, buf.length, left)) == -1)
+                        throw new RuntimeException("BufferStream truncated");
+                    left -= in;
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("BufferStream lost");
+            } finally {
+                if (fis != null) {
+                    try {
+                        fis.close();
+                    } catch (IOException e) {
+                    }
+                }
+            }
+            return newBuf;
+        }
+    }
+    
+    public ByteBuffer toByteBuffer() {
+        try {
+            sync();
+        } catch (IOException e) {
+        }
+        if (size == 0) {
+            return ByteBuffer.allocate(0);
+        } else if (size <= maxMem) {
+            return ByteBuffer.wrap(buf, 0, (int)size);
+        } else {
+            return ByteBuffer.wrap(toByteArray());
+        }
+    }
+    
+    public String toString() {
+        try {
+            return toString(Charset.defaultCharset().toString());
+        } catch (Exception e) {
+            return new String(buf);
+        }
+    }
+    
+    public String toString(String cset) throws IOException,
+        UnsupportedEncodingException {
+        sync();
+        if (buf == null)
+            return new String();
+        else if (file == null)
+            return new String(buf, 0, (int)size, cset);
+        else
+            throw new IOException("BufferStream data too large");
+    }
+    
+    public void truncate(long len) throws IOException {
+        sync();
+        if (len > size) {
+            throw new IOException("BufferStream expansion");
+        } else if (file != null) {
+            if (len > maxMem)
+                fos.getChannel().truncate(len - maxMem);
+            else
+                release();
+        }
+        size = len;
+    }
+    
+    public void write(int data) {
+        if (buffer(1) > 0) {
+            buf[(int)size] = (byte)data;
+        } else if (spool(1)) {
+            try {
+                fos.write(data);
+            } catch (Exception e) {
+                maxSize = size;
+                release();
+            }
+        }
+        size++;
+    }
+    
+    public void write(byte data[], int off, int len) {
+        int left = buffer(len);
+        
+        if (left > 0) {
+            if (left > len)
+                left = len;
+            System.arraycopy(data, off, buf, (int)size, left);
+            len -= left;
+            size += left;
+        }
+        if (len > 0) {
+            if (spool(len)) {
+                try {
+                    fos.write(data, off, len);
+                } catch (IOException e) {
+                    maxSize = size;
+                    release();
+                }
+            }
+            size += len;
+        }
+    }
+
+    public long writeTo(OutputStream os) throws IOException {
+        return writeTo(os, Long.MAX_VALUE);
+    }
+    
+    public long writeTo(OutputStream os, long len) throws IOException {
+        int in;
+        long out = 0;
+
+        sync();
+        if (len == Long.MAX_VALUE)
+            len = size;
+        else if (len > size)
+            throw new IOException("BufferStream underflow");
+        if (buf != null) {
+            in = (int)Math.min(Math.min(size, len), buf.length);
+            os.write(buf, 0, in);
+            len -= in;
+            out = in;
+        }
+        if (len == 0)
+            return out;
+
+        FileInputStream fis = null;
+        byte tmp[] = new byte[(int)Math.min(len, 32 * 1024)];
+        
+        try {
+            fis = new FileInputStream(file);
+            while (len > 0 && (in = fis.read(tmp, 0, (int)Math.min(len,
+                tmp.length))) != -1) {
+                os.write(tmp, 0, in);
+                len -= in;
+                out += in;
+            }
+        } finally {
+            if (fis != null)
+                fis.close();
+        }
+        return out;
+    }
+}
