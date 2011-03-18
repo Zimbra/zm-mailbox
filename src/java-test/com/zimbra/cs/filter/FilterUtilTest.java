9194e0b769300c593125a70ed20853d1200bae13
Vishal Mahajan <vmahajan@zimbra.com>
2011-03-17 23:59:37 -0500

    (split)bug: 54360
    In the unit test code replacing latin literal char with utf-8 char to fix the java compiler warning.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=54360
    
    Copied from Perforce
     Change: 282349

--- src/java-test/com/zimbra/cs/filter/FilterUtilTest.java
+++ src/java-test/com/zimbra/cs/filter/FilterUtilTest.java
@@ -11,7 +11,7 @@ public class FilterUtilTest {
     @Test
     public void truncateBody() throws Exception {
         // truncate a body containing a multi-byte char
-        String body = FilterUtil.truncateBodyIfRequired("André", 5);
+        String body = FilterUtil.truncateBodyIfRequired("Andr\u00e9", 5);
 
         Assert.assertTrue("truncated body should not have a partial char at the end", "Andr".equals(body));
     }
