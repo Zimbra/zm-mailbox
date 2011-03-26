5b44e9b32870f793e4127ed8d39bf378fc5d62f8
Unknown Perforce User <ysasaki>
2011-03-26 02:26:49 -0500

    bug:57606 zmprov recalculateMailboxCounts MAIL_ADDRESS - Added "action" attribute to RecalculateMailboxCountsRequest. The possible values are:  * ALL - FOLDER_TAG and MAIL_ADDRESS. This is the default.  * FOLDER_TAG - recalculate Folder and Tag counts  * MAIL_ADDRESS - rebuild Mail Address Directory - Cleaned up ProvUtil class.
    https://reviewboard.eng.vmware.com/r/220213/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=57606
    
    Copied from Perforce
     Change: 283863

--- src/java/com/zimbra/soap/admin/message/RecalculateMailboxCountsRequest.java
+++ src/java/com/zimbra/soap/admin/message/RecalculateMailboxCountsRequest.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2011 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -23,20 +23,35 @@ import com.zimbra.soap.admin.type.MailboxByAccountIdSelector;
 @XmlRootElement(name=AdminConstants.E_RECALCULATE_MAILBOX_COUNTS_REQUEST)
 public class RecalculateMailboxCountsRequest {
 
+    public enum Action {
+        ALL, FOLDER_TAG, MAIL_ADDRESS
+    }
+
     @XmlElement(name=AdminConstants.E_MAILBOX, required=true)
     private final MailboxByAccountIdSelector mbox;
+
+    @XmlElement(name=AdminConstants.A_ACTION)
+    private final Action action;
 
     /**
      * no-argument constructor wanted by JAXB
      */
     @SuppressWarnings("unused")
     private RecalculateMailboxCountsRequest() {
-        this(null);
+        mbox = null;
+        action = null;
     }
 
-    public RecalculateMailboxCountsRequest(MailboxByAccountIdSelector mbox) {
+    public RecalculateMailboxCountsRequest(MailboxByAccountIdSelector mbox, Action action) {
         this.mbox = mbox;
+        this.action = action;
     }
 
-    public MailboxByAccountIdSelector getMbox() { return mbox; }
+    public MailboxByAccountIdSelector getMbox() {
+        return mbox;
+    }
+
+    public Action getAction() {
+        return action;
+    }
 }
