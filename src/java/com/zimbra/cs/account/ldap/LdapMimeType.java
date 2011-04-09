c413e117484a046c7b42c32a53ae72ec0f98c996
Unknown Perforce User <pshao>
2011-04-09 16:14:07 -0500

    (split)bug: 31288
    refactor non-JNDI code to base class.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=31288
    
    Copied from Perforce
     Change: 290632

--- src/java/com/zimbra/cs/account/ldap/LdapMimeType.java
+++ src/java/com/zimbra/cs/account/ldap/LdapMimeType.java
@@ -19,65 +19,17 @@
  */
 package com.zimbra.cs.account.ldap;
 
-import java.util.Set;
-import java.util.TreeSet;
-
 import javax.naming.NamingException;
 import javax.naming.directory.Attributes;
 
-import com.zimbra.cs.account.Entry;
 import com.zimbra.cs.account.Provisioning;
-import com.zimbra.cs.mime.MimeTypeInfo;
+import com.zimbra.cs.prov.LdapMimeTypeBase;
 
-class LdapMimeType extends Entry implements LdapEntry, MimeTypeInfo {
-
-    private String mDn;
-    
+class LdapMimeType extends LdapMimeTypeBase {
+       
     LdapMimeType(String dn, Attributes attrs, Provisioning prov) throws NamingException {
         super(LdapUtil.getAttrs(attrs), null, prov);
         mDn = dn;
     }
-    
-    public String getLabel() {
-        return mDn;
-    }
 
-    public String[] getMimeTypes() {
-        return super.getMultiAttr(Provisioning.A_zimbraMimeType);
-    }
-
-    public String getHandlerClass() {
-        return super.getAttr(Provisioning.A_zimbraMimeHandlerClass, null);
-    }
-
-    public boolean isIndexingEnabled() {
-        return super.getBooleanAttr(Provisioning.A_zimbraMimeIndexingEnabled, true);
-    }
-
-    public String getDescription() {
-        return super.getAttr(Provisioning.A_description, "");
-    }
-
-    public Set<String> getFileExtensions() {
-        String[] extensions = super.getMultiAttr(Provisioning.A_zimbraMimeFileExtension);
-        Set<String> extSet = new TreeSet<String>();
-        for (String ext : extensions) {
-            if (ext != null) {
-                extSet.add(ext.toLowerCase());
-            }
-        }
-        return extSet;
-    }
-
-    public String getExtension() {
-        return super.getAttr(Provisioning.A_zimbraMimeHandlerExtension, null);
-    }
-
-    public String getDN() {
-        return mDn;
-    }
-
-    public int getPriority() {
-        return super.getIntAttr(Provisioning.A_zimbraMimePriority, 0);
-    }
 }
--- src/java/com/zimbra/cs/account/ldap/LdapProvisioning.java
+++ src/java/com/zimbra/cs/account/ldap/LdapProvisioning.java
@@ -32,6 +32,8 @@ import com.zimbra.cs.account.AccountCache;
 import com.zimbra.cs.account.AccountServiceException;
 import com.zimbra.cs.account.AccountServiceException.AuthFailedServiceException;
 import com.zimbra.cs.account.DomainCache.GetFromDomainCacheOption;
+import com.zimbra.cs.account.Provisioning.AccountBy;
+import com.zimbra.cs.account.Provisioning.DistributionListBy;
 import com.zimbra.cs.account.Alias;
 import com.zimbra.cs.account.AttributeClass;
 import com.zimbra.cs.account.AttributeManager;
@@ -114,11 +116,13 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 import java.util.Stack;
+import java.util.TreeSet;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
@@ -5226,13 +5230,239 @@ public class LdapProvisioning extends Provisioning {
     @Override
     public void addMembers(DistributionList list, String[] members) throws ServiceException {
         LdapDistributionList ldl = (LdapDistributionList) list;
-        ldl.addMembers(members, this);
+        addListMembers(ldl, members);
     }
 
     @Override
     public void removeMembers(DistributionList list, String[] members) throws ServiceException {
         LdapDistributionList ldl = (LdapDistributionList) list;
-        ldl.removeMembers(members, this);
+        removeListMembers(ldl, members);
+    }
+    
+    private void addListMembers(DistributionList dl, String[] members) throws ServiceException {
+        Set<String> existing = dl.getMultiAttrSet(Provisioning.A_zimbraMailForwardingAddress);
+        Set<String> mods = new HashSet<String>();
+        
+        // all addrs of thie DL
+        AddrsOfEntry addrsOfDL = getAllAddressesOfEntry(dl.getName());
+        
+        for (int i = 0; i < members.length; i++) { 
+            String memberName = members[i].toLowerCase();
+            memberName = IDNUtil.toAsciiEmail(memberName);
+            
+            if (addrsOfDL.isIn(memberName))
+                throw ServiceException.INVALID_REQUEST("Cannot add self as a member: " + memberName, null);
+            
+            if (!existing.contains(memberName)) {
+                mods.add(memberName);
+
+                // clear the DL cache on accounts/dl
+
+                // can't do prov.getFromCache because it only caches by primary name 
+                Account acct = get(AccountBy.name, memberName);
+                if (acct != null)
+                    clearUpwardMembershipCache(acct);
+                else {
+                    // for DistributionList/ACLGroup, get it from cache because 
+                    // if the dl is not in cache, after loading it prov.getAclGroup 
+                    // always compute the upward membership.  Sounds silly if we are 
+                    // about to clean the cache.  If memberName is indeed an alias 
+                    // of one of the cached DL/ACLGroup, it will get expired after 15 
+                    // mins, just like the multi-node case. 
+                    //
+                    // Note: do not call clearUpwardMembershipCache for AclGroup because
+                    // the upward membership cache for that is computed and cache only when 
+                    // the entry is loaded/being cached, instead of lazily computed like we 
+                    // do for account.
+                    removeGroupFromCache(DistributionListBy.name, memberName);
+                }
+            }
+        }
+
+        if (mods.isEmpty()) {
+            // nothing to do...
+            return;
+        }
+
+        PermissionCache.invalidateCache();
+        
+        Map<String,String[]> modmap = new HashMap<String,String[]>();
+        modmap.put("+" + Provisioning.A_zimbraMailForwardingAddress, (String[])mods.toArray(new String[0]));
+        modifyAttrs(dl, modmap, true);
+    }
+
+    private void removeListMembers(DistributionList dl, String[] members) throws ServiceException {
+        Set<String> curMembers = dl.getMultiAttrSet(Provisioning.A_zimbraMailForwardingAddress);
+        
+        // bug 46219, need a case insentitive Set
+        Set<String> existing = new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
+        existing.addAll(curMembers);
+        
+        Set<String> mods = new HashSet<String>();
+        HashSet<String> failed = new HashSet<String>();
+
+        for (int i = 0; i < members.length; i++) { 
+            String memberName = members[i].toLowerCase();
+            memberName = IDNUtil.toAsciiEmail(memberName);
+            if (memberName.length() == 0) {
+                throw ServiceException.INVALID_REQUEST("invalid member email address: " + memberName, null);
+            }
+            // We do not do any further validation of the remove address for
+            // syntax - removes should be liberal so any bad entries added by
+            // some other means can be removed
+            //
+            // members[] can contain:
+            //   - the primary address of an account or another DL
+            //   - an alias of an account or another DL
+            //   - junk (allAddrs will be returned as null)
+            AddrsOfEntry addrsOfEntry = getAllAddressesOfEntry(memberName);
+            List<String> allAddrs = addrsOfEntry.getAll();
+                
+            if (mods.contains(memberName)) {
+                // already been added in mods (is the primary or alias of previous entries in members[])
+            } else if (existing.contains(memberName)) {
+                if (allAddrs.size() > 0)
+                    mods.addAll(allAddrs);
+                else
+                    mods.add(memberName);  // just get rid of it regardless what it is
+            } else {
+                boolean inList = false;
+                if (allAddrs.size() > 0) {
+                    // go through all addresses of the entry see if any is on the DL
+                    for (Iterator it = allAddrs.iterator(); it.hasNext() && !inList; ) {
+                        String addr = (String)it.next();
+                        if (existing.contains(addr)) {
+                            mods.addAll(allAddrs);
+                            inList = true;
+                        }
+                    }
+                }
+                if (!inList)
+                    failed.add(memberName);
+            }
+            
+            // clear the DL cache on accounts/dl
+            String primary = addrsOfEntry.getPrimary();
+            if (primary != null) {
+                if (addrsOfEntry.isAccount()) {
+                    Account acct = getFromCache(AccountBy.name, primary);
+                    if (acct != null)
+                        clearUpwardMembershipCache(acct);
+                } else {
+                    removeGroupFromCache(DistributionListBy.name, primary);
+                }
+            }
+        }
+
+        if (!failed.isEmpty()) {
+            StringBuilder sb = new StringBuilder();
+            Iterator<String> iter = failed.iterator();
+            while (true) {
+                sb.append(iter.next());
+                if (!iter.hasNext())
+                    break;
+                sb.append(",");
+            }
+            throw AccountServiceException.NO_SUCH_MEMBER(dl.getName(), sb.toString());
+        }
+
+        if (mods.isEmpty()) {
+            throw ServiceException.INVALID_REQUEST("empty remove set", null);
+        }
+        
+        PermissionCache.invalidateCache();
+        
+        Map<String,String[]> modmap = new HashMap<String,String[]>();
+        modmap.put("-" + Provisioning.A_zimbraMailForwardingAddress, (String[])mods.toArray(new String[0]));
+        modifyAttrs(dl, modmap);
+
+    }
+
+    private void clearUpwardMembershipCache(Account acct) {
+        acct.setCachedData(LdapProvisioning.DATA_DL_SET, null);
+        acct.setCachedData(LdapProvisioning.DATA_ACLGROUP_LIST, null);
+        acct.setCachedData(LdapProvisioning.DATA_ACLGROUP_LIST_ADMINS_ONLY, null);
+        acct.setCachedData(EntryCacheDataKey.GROUPEDENTRY_DIRECT_GROUPIDS.getKeyName(), null);
+    }
+    
+    private class AddrsOfEntry {
+        List<String> mAllAddrs = new ArrayList<String>(); // including primary
+        String mPrimary = null;  // primary addr
+        boolean mIsAccount = false;
+        
+        void setPrimary(String primary) {
+            mPrimary = primary;
+            add(primary);
+        }
+        
+        void setIsAccount(boolean isAccount) {
+            mIsAccount = isAccount;
+        }
+
+        void add(String addr) {
+            mAllAddrs.add(addr);
+        }
+        
+        void addAll(String[] addrs) {
+            mAllAddrs.addAll(Arrays.asList(addrs));
+        }
+        
+        List<String> getAll() {
+            return mAllAddrs;
+        }
+        
+        String getPrimary() {
+            return mPrimary;
+        }
+        
+        boolean isAccount() {
+            return mIsAccount;
+        }
+        
+        int size() {
+            return mAllAddrs.size();
+        }
+        
+        boolean isIn(String addr) {
+            return mAllAddrs.contains(addr.toLowerCase());
+        }
+    }
+    
+    
+    //
+    // returns the primary address and all aliases of the named account or DL 
+    //
+    private AddrsOfEntry getAllAddressesOfEntry(String name) {
+        
+        String primary = null;
+        String aliases[] = null;
+        AddrsOfEntry addrs = new AddrsOfEntry();
+        
+        try {
+            // bug 56621.  Do not count implicit aliases (aliases resolved by alias domain) 
+            // when dealing with distribution list members.
+            Account acct = getAccountByName(name, false, false);
+            if (acct != null) {
+                addrs.setIsAccount(true);
+                primary = acct.getName();
+                aliases = acct.getMailAlias();
+            } else {
+                DistributionList dl = get(Provisioning.DistributionListBy.name, name);
+                if (dl != null) {
+                    primary = dl.getName();
+                    aliases = dl.getAliases();
+                }
+            }
+        } catch (ServiceException se) {
+            // swallow any exception and go on
+        }
+        
+        if (primary != null)
+            addrs.setPrimary(primary);
+        if (aliases != null)
+            addrs.addAll(aliases);
+               
+        return addrs;
     }
 
     private List<Identity> getIdentitiesByQuery(LdapEntry entry, String query, ZimbraLdapContext initZlc) throws ServiceException {
