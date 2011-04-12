eefbbb5ba2b9d274fa63336346c396f0e6456eff
Unknown Perforce User <pshao>
2011-04-12 18:47:00 -0500

    bug: 58898
    fixed missed files.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=58898
    
    Copied from Perforce
     Change: 292312

--- src/java/com/zimbra/cs/prov/ldap/LdapProv.java
+++ src/java/com/zimbra/cs/prov/ldap/LdapProv.java
@@ -6,11 +6,29 @@ import java.util.Set;
 
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.account.ldap.LdapDIT;
 import com.zimbra.cs.mime.MimeTypeInfo;
 
 public abstract class LdapProv extends Provisioning {
     
     protected LdapDIT mDIT;
+    
+    protected static final long ONE_DAY_IN_MILLIS = 1000*60*60*24;
+    
+    protected static final String[] sInvalidAccountCreateModifyAttrs = {
+            Provisioning.A_zimbraMailAlias,
+            Provisioning.A_zimbraMailDeliveryAddress,
+            Provisioning.A_uid
+    };
+
+    protected static final String[] sMinimalDlAttrs = {
+            Provisioning.A_zimbraMailAlias,
+            Provisioning.A_zimbraId,
+            Provisioning.A_uid,
+            Provisioning.A_zimbraACE,
+            Provisioning.A_zimbraIsAdminGroup,
+            Provisioning.A_zimbraAdminConsoleUIComponents
+    };
     
     protected void setDIT() {
         mDIT = new LdapDIT(this);
