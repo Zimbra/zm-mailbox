c29ff36b3582cad578a262978b9918f1f78760e4
Unknown Perforce User <pshao>
2011-04-14 22:39:56 -0500

    bug: 31288
    http://bugzilla.zimbra.com/show_bug.cgi?id=31288
    
    Copied from Perforce
     Change: 293511

---
+++ src/java/com/zimbra/cs/prov/ldap/ZLdapHelper.java
@@ -0,0 +1,28 @@
+package com.zimbra.cs.prov.ldap;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.ldap.LdapClient;
+import com.zimbra.cs.ldap.LdapUtil;
+import com.zimbra.cs.ldap.SearchLdapOptions;
+import com.zimbra.cs.ldap.ZLdapContext;
+
+/**
+ * An SDK-neutral LdapHelper.  Based on Z* classes and LdapUtil in the com.zimbra.cs.ldap package.
+ * 
+ * @author pshao
+ *
+ */
+public class ZLdapHelper extends LdapHelper {
+    
+    ZLdapHelper(LdapProv ldapProv) {
+        super(ldapProv);
+    }
+
+    @Override
+    public void searchLdap(SearchLdapOptions searchOptions) throws ServiceException {
+        
+        ZLdapContext zlc = LdapClient.toZLdapContext(getProv(), searchOptions.getILdapContext());
+        zlc.search(searchOptions);
+    }
+
+}
