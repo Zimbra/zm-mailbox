3c147f2f320c63531004214226014e9b7e8492a3
Vishal Mahajan <vmahajan@zimbra.com>
2011-04-28 02:23:22 -0500

    Integrating from HELIX..
    Change 300403 by vmahajan@vmahajan_macbook on 2011/04/27 23:55:28
    
            bug: 59403 - wrong zimbraMailURL when mailbox is on different store than the one initially queried via preauth
    
            For redirection use zimbraMailURL of the server where the account is hosted instead of the local server.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=59403
    
    Copied from Perforce
     Change: 300418

---
+++ src/java/com/zimbra/cs/milter/NioMilterHandler.java
@@ -0,0 +1,100 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2010, 2011 Zimbra, Inc.
+ *
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.milter;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.server.NioHandler;
+import com.zimbra.cs.server.NioConnection;
+
+final class NioMilterHandler extends MilterHandler implements NioHandler {
+    private final NioConnection connection;
+
+    NioMilterHandler(NioMilterServer server, NioConnection conn) {
+        super(server);
+        connection = conn;
+    }
+
+    @Override
+    public void connectionClosed() throws IOException {
+        ZimbraLog.milter.info(sessPrefix + "Connection closed");
+        connection.close();
+    }
+
+    @Override
+    public void connectionIdle() throws IOException {
+        notifyIdleConnection();
+    }
+
+    @Override
+    public void connectionOpened() throws IOException {
+        newSession();
+        ZimbraLog.milter.info(sessPrefix + "Connection opened");
+    }
+
+    @Override
+    public void messageReceived(Object msg) throws IOException {
+        MilterPacket command = (MilterPacket) msg;
+
+        try {
+            MilterPacket response = processCommand(command);
+            if (response != null) {
+                connection.send(response);
+            }
+        } catch (ServiceException e) {
+            ZimbraLog.milter.error(sessPrefix + "Server error: " + e.getMessage());
+            dropConnection(); // aborting the session
+        }
+    }
+
+    @Override
+    protected boolean setupConnection(Socket connection) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected boolean authenticate() throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected boolean processCommand() throws Exception {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void dropConnection() {
+        if (connection.isOpen()) {
+            connection.close();
+        }
+    }
+
+    @Override
+    protected void notifyIdleConnection() {
+        ZimbraLog.milter.debug(sessPrefix + "Dropping connection for inactivity");
+        dropConnection();
+    }
+
+    @Override
+    public void setLoggingContext() {
+    }
+
+    @Override
+    public void exceptionCaught(Throwable e) {
+    }
+}
---
+++ src/java/com/zimbra/cs/prov/ldap/LdapObjectClass.java
@@ -0,0 +1,110 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2008, 2009, 2010 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.prov.ldap;
+
+// use LinkedHashSet to preserve the order and uniqueness of entries, 
+// not that order/uniqueness matters to LDAP server, just cleaner this way
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import com.zimbra.cs.account.AttributeClass;
+import com.zimbra.cs.account.Provisioning;
+import com.zimbra.common.service.ServiceException;
+
+public class LdapObjectClass {
+    
+    public static String ZIMBRA_DEFAULT_PERSON_OC = "organizationalPerson";
+    
+    private static void addExtraObjectClasses(Set<String> ocs, Provisioning prov, String extraOCAttr) throws ServiceException {
+        String[] extraObjectClasses = prov.getConfig().getMultiAttr(extraOCAttr);
+        for (String eoc : extraObjectClasses) {
+            ocs.add(eoc);
+        }
+        
+        /*
+        if (additionalObjectClasses != null) {
+            for (int i = 0; i < additionalObjectClasses.length; i++)
+                ocs.add(additionalObjectClasses[i]);
+        }
+        */
+    }
+    
+    public static Set<String> getAccountObjectClasses(Provisioning prov, boolean zimbraDefaultOnly) throws ServiceException {
+        Set<String> ocs = new LinkedHashSet<String>();
+        
+        ocs.add(ZIMBRA_DEFAULT_PERSON_OC);
+        ocs.add(AttributeClass.OC_zimbraAccount);
+        
+        if (!zimbraDefaultOnly)
+            addExtraObjectClasses(ocs, prov, Provisioning.A_zimbraAccountExtraObjectClass);
+        return ocs;
+    }
+    
+    public static Set<String> getAccountObjectClasses(Provisioning prov) throws ServiceException {
+        return getAccountObjectClasses(prov, false);
+    }
+    
+    public static Set<String> getCalendarResourceObjectClasses(Provisioning prov) throws ServiceException {
+        Set<String> ocs = new LinkedHashSet<String>();
+        
+        ocs.add(AttributeClass.OC_zimbraCalendarResource);
+        
+        addExtraObjectClasses(ocs, prov, Provisioning.A_zimbraCalendarResourceExtraObjectClass);
+        return ocs;
+    }
+    
+    public static Set<String> getCosObjectClasses(Provisioning prov) throws ServiceException {
+        Set<String> ocs = new LinkedHashSet<String>();
+        
+        ocs.add(AttributeClass.OC_zimbraCOS);
+        
+        addExtraObjectClasses(ocs, prov, Provisioning.A_zimbraCosExtraObjectClass);
+        return ocs;
+    }
+    
+    public static Set<String> getDomainObjectClasses(Provisioning prov) throws ServiceException {
+        Set<String> ocs = new LinkedHashSet<String>();
+        
+        ocs.add("dcObject");
+        ocs.add("organization");
+        ocs.add(AttributeClass.OC_zimbraDomain); 
+        
+        addExtraObjectClasses(ocs, prov, Provisioning.A_zimbraDomainExtraObjectClass);
+        return ocs;
+    }
+    
+    public static Set<String> getServerObjectClasses(Provisioning prov) throws ServiceException {
+        Set<String> ocs = new LinkedHashSet<String>();
+        
+        ocs.add(AttributeClass.OC_zimbraServer);
+        
+        addExtraObjectClasses(ocs, prov, Provisioning.A_zimbraServerExtraObjectClass);
+        return ocs;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws ServiceException {
+        Provisioning prov = Provisioning.getInstance();
+        Set<String> ocs = LdapObjectClass.getCalendarResourceObjectClasses(prov);
+
+        for (String oc : ocs) {
+            System.out.println(oc);
+        }
+        
+    }
+
+}
