4141adbba7ee82518ad408f1f92f950dc148d31f
Unknown Perforce User <pshao>
2011-04-28 11:18:49 -0500

    (split)bug: 59379
    null offset/limit and set _offset/_limit on the GetDistributionListMembersRequest before calling GAL search.  Otherwise the GSA search will take those as as parameters for the contact search.  Those are meant for offset/limit for paging through members. 
    
    _offset/_limit is required in case the GSA search needs to be proxied to the GSA's home server.
    
    https://reviewboard.eng.vmware.com/r/228392
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=59379
    
    Copied from Perforce
     Change: 300604

--- src/java/com/zimbra/cs/service/account/GetDistributionListMembers.java
+++ src/java/com/zimbra/cs/service/account/GetDistributionListMembers.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.AccountConstants;
 import com.zimbra.common.soap.AdminConstants;
+import com.zimbra.common.soap.MailConstants;
 import com.zimbra.common.soap.Element;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.AccountServiceException;
@@ -24,20 +25,17 @@ public class GetDistributionListMembers extends GalDocumentHandler {
         if (!canAccessAccount(zsc, account))
             throw ServiceException.PERM_DENIED("can not access account");
         
-        Provisioning prov = Provisioning.getInstance();
-        
-        int limit = (int) request.getAttributeLong(AccountConstants.A_MEMBER_LIMIT, 0);
-        if (limit < 0) {
-            throw ServiceException.INVALID_REQUEST("limit" + limit + " is negative", null);
-        }
-        
-        int offset = (int) request.getAttributeLong(AccountConstants.A_MEMBER_OFFSET, 0);
-        if (offset < 0) {
-            throw ServiceException.INVALID_REQUEST("offset" + offset + " is negative", null);
-        }
-        
         Element d = request.getElement(AdminConstants.E_DL);
         String dlName = d.getText();
+        
+        int offset = getOffset(request);
+        int limit = getLimit(request);
+        
+        // null offset/limit and set _offset/_limit before calling searchGal().
+        request.addAttribute(MailConstants.A_QUERY_OFFSET, (String)null);
+        request.addAttribute(MailConstants.A_LIMIT, (String)null);
+        request.addAttribute(AccountConstants.A_OFFSET_INTERNAL, offset);
+        request.addAttribute(AccountConstants.A_LIMIT_INTERNAL, limit);
         
         DLMembersResult dlMembersResult = GalGroupMembers.searchGal(zsc, account, dlName, request);
         
@@ -50,6 +48,79 @@ public class GetDistributionListMembers extends GalDocumentHandler {
             return processDLMembers(zsc, dlName, account , limit, offset, (DLMembers)dlMembersResult);
         }
     }
+    
+    /*
+     * For SOAP API cleanness purpose, we want to support offset/limit attributes.
+     * 
+     * But that will interfere with the GAL search if GSA is in place, 
+     * because the mailbox search code would take them as the offset/limit for 
+     * the contact search.
+     * 
+     * If we simply null the offset/limit when passing the request object to 
+     * GalGroupMembers.searchGal(), the original value passed from the client 
+     * will be lost if searchGal() proxies the request to the GSA's home server. 
+     * 
+     * We could've chosen to use different attributes for offset/limit for this 
+     * SOAP request, but it is not as ideal/pretty from API point of view.
+     *  
+     * To fix it: 
+     * Before calling GalGroupMembers.searchGal(), we read the offset/limit 
+     * from the request, set them on internal attributes _limit/_offset, and 
+     * null the limit/offset on the request object.
+     * 
+     * In the handler, we first look for _limit/_offset, if set(must have been 
+     * proxied to this server by the GSA code), honor them.  
+     * If not set(the request is from the client client), honor limit/offset 
+     * if they are present.
+     * 
+     * 
+     */
+    private int getOffset(Element request) throws ServiceException {
+        
+        int offset = 0;
+        
+        // see if the internal attrs are set, use them if set.
+        String offsetStr = request.getAttribute(AccountConstants.A_OFFSET_INTERNAL, null);
+        if (offsetStr != null) {
+            offset = (int) Element.parseLong(AccountConstants.A_OFFSET_INTERNAL, offsetStr);
+        } else {
+            // otherwise, see if it is set by the client, use them if set
+            offsetStr = request.getAttribute(MailConstants.A_QUERY_OFFSET, null);
+            if (offsetStr != null) {
+                offset = (int) Element.parseLong(MailConstants.A_QUERY_OFFSET, offsetStr);
+            }
+        }
+        
+        if (offset < 0) {
+            throw ServiceException.INVALID_REQUEST("offset" + offset + " is negative", null);
+        }
+        
+        return offset;
+    }
+    
+    private int getLimit(Element request) throws ServiceException {
+        
+        int limit = 0;
+        
+        // see if the internal attrs are set, use them if set.
+        String limitStr = request.getAttribute(AccountConstants.A_LIMIT_INTERNAL, null);
+        if (limitStr != null) {
+            limit = (int) Element.parseLong(AccountConstants.A_LIMIT_INTERNAL, limitStr);
+        } else {
+            // otherwise, see if it is set by the client, use them if set
+            limitStr = request.getAttribute(MailConstants.A_LIMIT, null);
+            if (limitStr != null) {
+                limit = (int) Element.parseLong(MailConstants.A_LIMIT, limitStr);
+            }
+        }
+        
+        if (limit < 0) {
+            throw ServiceException.INVALID_REQUEST("limit" + limit + " is negative", null);
+        }
+        
+        return limit;
+    }
+
     
     protected Element processDLMembers(ZimbraSoapContext zsc, String dlName, Account account, 
             int limit, int offset, DLMembers dlMembers) throws ServiceException {
