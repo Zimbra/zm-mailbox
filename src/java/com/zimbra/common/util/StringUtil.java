bdcdd4898b13167f544cab7f44c104f6ce80b5f4
Unknown Perforce User <pshao>
2011-04-27 19:31:45 -0500

    bug: 59379
    integrate change 300261 from main to HELIX 
    --------------------------------------------------------------
    Change 300261 by pshao@pshao-macbookpro on 2011/04/27 17:30:04
    
            bug: 59379
    
            use memberOffset/memberLimit instead of offset/limit for GetDistributionListMembersRequest.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=59379
    
    Copied from Perforce
     Change: 300264

--- src/java/com/zimbra/common/util/StringUtil.java
+++ src/java/com/zimbra/common/util/StringUtil.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
- *
+ * 
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- *
+ * 
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -859,13 +859,6 @@ public class StringUtil {
      * aren't recompiled.
      */
     public static Matcher newMatcher(String regex, CharSequence s) {
-        return getCachedPattern(regex).matcher(s);
-    }
-
-    /**
-     * Returns a precompiled {@link Pattern} for the given regular expression.
-     */
-    public static Pattern getCachedPattern(String regex) {
         Pattern pattern = null;
         synchronized (patternCache) {
             pattern = patternCache.get(regex);
@@ -877,7 +870,7 @@ public class StringUtil {
                 patternCache.put(regex, pattern);
             }
         }
-        return pattern;
+        return pattern.matcher(s);
     }
     
     /**
