2d513826c121a94bcc36b07ee8c61c35b2782891
Unknown Perforce User <ysasaki>
2011-04-30 00:45:11 -0500

    (split)bug:53237 Fix build failure
    http://bugzilla.zimbra.com/show_bug.cgi?id=53237
    
    Copied from Perforce
     Change: 301496

--- src/java/com/zimbra/cs/mime/handler/UnknownTypeHandler.java
+++ src/java/com/zimbra/cs/mime/handler/UnknownTypeHandler.java
@@ -1,21 +1,16 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2004, 2005, 2006, 2007, 2009, 2010 Zimbra, Inc.
- * 
+ * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011 Zimbra, Inc.
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
- */
-
-/*
- * Created on Apr 1, 2004
- *
  */
 package com.zimbra.cs.mime.handler;
 
@@ -25,66 +20,52 @@ import com.zimbra.cs.convert.AttachmentInfo;
 import com.zimbra.cs.mime.MimeHandler;
 
 /**
- * @author schemers
+ * {@link MimeHandler} for no conversion.
  *
- *  class that creates a Lucene document from a Java Mail Message
+ * @since Apr 1, 2004
+ * @author schemers
  */
 public class UnknownTypeHandler extends MimeHandler {
 
-    private String mContentType;
+    private String contentType;
 
     @Override
     protected boolean runsExternally() {
         return false;
     }
 
-    /* (non-Javadoc)
-     * @see com.zimbra.cs.mime.MimeHandler#populate(org.apache.lucene.document.Document)
-     */
     @Override
     public void addFields(Document doc) {
         // do nothing
     }
 
-    /* (non-Javadoc)
-     * @see com.zimbra.cs.mime.MimeHandler#getContent()
-     */
     @Override
     protected String getContentImpl() {
         return "";
     }
-    
+
     @Override
     public boolean isIndexingEnabled() {
         return true;
     }
-    
+
     @Override
     public String getContentType() {
-        return mContentType;
+        return contentType;
     }
 
-    /* (non-Javadoc)
-     * @see com.zimbra.cs.mime.MimeHandler#convert(com.zimbra.cs.convert.AttachmentInfo, java.lang.String)
-     */
     @Override
     public String convert(AttachmentInfo doc, String baseURL) {
         throw new IllegalStateException("conversion not allowed for content of unknown type");
     }
 
-    /* (non-Javadoc)
-     * @see com.zimbra.cs.mime.MimeHandler#doConversion()
-     */
     @Override
     public boolean doConversion() {
         return false;
     }
-    
-    /**
-     * @see com.zimbra.cs.mime.MimeHandler#setContentType(String)
-     */
+
     @Override
-    protected void setContentType(String ct) {
-        mContentType = ct;
+    public void setContentType(String value) {
+        contentType = value;
     }
 }
