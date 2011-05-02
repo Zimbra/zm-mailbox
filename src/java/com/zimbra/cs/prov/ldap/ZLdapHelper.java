9ec8281dbba38eb89821a3bee142e425ff6720ec
Unknown Perforce User <pshao>
2011-05-02 13:09:01 -0500

    bug: 31288
    Account, identity, data source, signature, mime type, XMPP components, Zimlet related unittests and fixes.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=31288
    
    Copied from Perforce
     Change: 302641

--- src/java/com/zimbra/cs/prov/ldap/ZLdapHelper.java
+++ src/java/com/zimbra/cs/prov/ldap/ZLdapHelper.java
@@ -14,7 +14,13 @@
  */
 package com.zimbra.cs.prov.ldap;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+
 import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.account.AttributeManager;
+import com.zimbra.cs.account.Entry;
 import com.zimbra.cs.ldap.ILdapContext;
 import com.zimbra.cs.ldap.LdapClient;
 import com.zimbra.cs.ldap.LdapException;
@@ -23,6 +29,7 @@ import com.zimbra.cs.ldap.LdapUtil;
 import com.zimbra.cs.ldap.SearchLdapOptions;
 import com.zimbra.cs.ldap.ZAttributes;
 import com.zimbra.cs.ldap.ZLdapContext;
+import com.zimbra.cs.ldap.ZModificationList;
 import com.zimbra.cs.ldap.ZSearchControls;
 import com.zimbra.cs.ldap.ZSearchResultEntry;
 import com.zimbra.cs.ldap.ZSearchResultEnumeration;
@@ -49,6 +56,93 @@ public class ZLdapHelper extends LdapHelper {
         
         ZLdapContext zlc = LdapClient.toZLdapContext(getProv(), ldapContext);
         zlc.searchPaged(searchOptions);
+    }
+    
+
+    /**
+     * Modifies the specified entry.  <code>attrs</code> is a <code>Map</code> consisting of
+     * keys that are <code>String</code>s, and values that are either
+     * <ul>
+     *   <li><code>null</code>, in which case the attr is removed</li>
+     *   <li>a single <code>Object</code>, in which case the attr is modified
+     *     based on the object's <code>toString()</code> value</li>
+     *   <li>an <code>Object</code> array or <code>Collection</code>,
+     *     in which case a multi-valued attr is updated</li>
+     * </ul>
+     */
+    public void modifyAttrs(ZLdapContext zlc, String dn, Map<String, ? extends Object> attrs, Entry entry) 
+    throws ServiceException {
+        ZModificationList modList = zlc.createModiftcationList();
+        
+        AttributeManager attrMgr = AttributeManager.getInst();
+        
+        for (Map.Entry<String, ? extends Object> attr : attrs.entrySet()) {    
+            Object v= attr.getValue();
+            String key = attr.getKey();
+            boolean doAdd = key.charAt(0) == '+';
+            boolean doRemove = key.charAt(0) == '-';
+            
+            if (doAdd || doRemove) {
+                // make sure there aren't other changes without +/- going on at the same time 
+                key = key.substring(1);
+                if (attrs.containsKey(key)) 
+                    throw ServiceException.INVALID_REQUEST("can't mix +attrName/-attrName with attrName", null);
+            }
+             
+            boolean containsBinaryData = attrMgr == null ? false : attrMgr.containsBinaryData(key);
+            boolean isBinaryTransfer = attrMgr == null ? false : attrMgr.isBinaryTransfer(key);
+            
+            // Convert array to List so it can be treated as a Collection
+            if (v instanceof Object[]) {
+                // Note: Object[] cast is required, so that asList() knows to create a List
+                // that contains the contents of the object array, as opposed to a List with one
+                // element, which is the entire Object[].  Ick.
+                v = Arrays.asList((Object[]) v);
+            }
+            
+            if (v instanceof Collection) {
+                Collection c = (Collection) v;
+                if (c.size() == 0) {
+                    // make sure it exists
+                    if (entry.getAttr(key, false) != null) {
+                        modList.removeAttr(key);
+                    }
+                } else {
+                    // Convert values Collection to a String array
+                    String[] sa = new String[c.size()];
+                    int i = 0;
+                    for (Object o : c) {
+                        sa[i++] = (o == null ? null : o.toString());
+                    }
+                    
+                    // Add attrs
+                    if (doAdd) {
+                        modList.addAttr(key, sa, entry, containsBinaryData, isBinaryTransfer);
+                    } else if (doRemove) {
+                        modList.removeAttr(key, sa, entry, containsBinaryData, isBinaryTransfer);
+                    } else {
+                        modList.modifyAttr(key, sa, containsBinaryData, isBinaryTransfer);
+                    }
+                }
+            } else if (v instanceof Map) {
+                throw ServiceException.FAILURE("Map is not a supported value type", null);
+            } else {
+                String s = (v == null ? null : v.toString());
+                if (doAdd) {
+                    modList.addAttr(key, s, entry, containsBinaryData, isBinaryTransfer);
+                }
+                else if (doRemove) {
+                    modList.removeAttr(key, s, entry, containsBinaryData, isBinaryTransfer);
+                }
+                else {
+                    modList.modifyAttr(key, s, entry, containsBinaryData, isBinaryTransfer);
+                }
+            }
+        }
+        
+        if (!modList.isEmpty()) {
+            zlc.modifyAttributes(dn, modList);
+        }
     }
     
 
@@ -89,12 +183,13 @@ public class ZLdapHelper extends LdapHelper {
     }
 
     @Override
-    public ZAttributes getAttributes(String dn, ZLdapContext initZlc, boolean useMaster) 
+    public ZAttributes getAttributes(String dn, ZLdapContext initZlc, LdapServerType ldapServerType) 
     throws LdapEntryNotFoundException, ServiceException {
         ZLdapContext zlc = initZlc;
         try {
-            if (zlc == null)
-                zlc = LdapClient.getContext(LdapServerType.get(useMaster));
+            if (zlc == null) {
+                zlc = LdapClient.getContext(ldapServerType);
+            }
             return zlc.getAttributes(dn);
         /*  all callsites with the following @TODOEXCEPTIONMAPPING pattern can have ease of mind now and remove the 
          * TODOEXCEPTIONMAPPING annotation
@@ -107,21 +202,34 @@ public class ZLdapHelper extends LdapHelper {
             throw ServiceException.FAILURE("unable to lookup COS by name: "+name+" message: "+e.getMessage(), e);
         */    
         } finally {
-            if (initZlc == null)
+            if (initZlc == null) {
                 LdapClient.closeContext(zlc);
+            }
         }
     }
 
     @Override
     public ZSearchResultEnumeration searchDir(String baseDN, String query,
-            ZSearchControls searchControls) throws ServiceException {
+            ZSearchControls searchControls, ZLdapContext initZlc, LdapServerType ldapServerType) 
+    throws ServiceException {
         ZLdapContext zlc = null;
         try {
-            zlc = LdapClient.getContext();
+            if (zlc == null) {
+                zlc = LdapClient.getContext(ldapServerType);
+            }
             return zlc.searchDir(baseDN, query, searchControls);
+        /*    
+        } catch (NameNotFoundException e) {
+            return null;
+        } catch (InvalidNameException e) {
+            return null;
+        */
         } finally {
-            LdapClient.closeContext(zlc);
+            if (initZlc == null) {
+                LdapClient.closeContext(zlc);
+            }
         }
     }
+
 
 }
