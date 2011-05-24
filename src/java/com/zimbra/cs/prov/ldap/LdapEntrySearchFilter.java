65b13a675b3cf0574cc962ae590bdf3cd3968cae
Unknown Perforce User <bburtin>
2011-05-24 14:57:24 -0500

    Integrated change 314346 from HELIX to main.
    --------------
    
    Change 314346 by bburtin@bburtin-ubuntu-helix on 2011/05/24 12:39:44
    
    	bug: none
    
    	Fixed incorrect cast in TestSendAndReceive.testReuseUploadId().
    
    Affected files ...
    
    ... //depot/zimbra/HELIX/ZimbraServer/src/java/com/zimbra/qa/unittest/TestSendAndReceive.java#7 edit
    
    Copied from Perforce
     Change: 314354

---
+++ src/java/com/zimbra/cs/prov/ldap/LdapEntrySearchFilter.java
@@ -0,0 +1,186 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2006, 2007, 2009, 2010 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.cs.prov.ldap;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.account.AttributeClass;
+import com.zimbra.cs.account.AttributeManager;
+import com.zimbra.cs.account.AttributeManager.IDNType;
+import com.zimbra.cs.account.EntrySearchFilter;
+import com.zimbra.cs.account.IDNUtil;
+import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.account.EntrySearchFilter.Multi;
+import com.zimbra.cs.account.EntrySearchFilter.Operator;
+import com.zimbra.cs.account.EntrySearchFilter.Single;
+import com.zimbra.cs.account.EntrySearchFilter.Term;
+import com.zimbra.cs.account.EntrySearchFilter.Visitor;
+import com.zimbra.cs.ldap.LdapUtilCommon;
+
+public class LdapEntrySearchFilter {
+
+    public static class LdapQueryVisitor implements Visitor {
+        StringBuilder mLdapFilter;
+
+        public LdapQueryVisitor() {
+            mLdapFilter = new StringBuilder();
+        }
+
+        public String getFilter() {
+            return mLdapFilter.toString();
+        }
+
+        private static StringBuilder addCond(StringBuilder sb,
+                                             String lhs,
+                                             String op,
+                                             String rhs) {
+            sb.append('(');
+            sb.append(lhs).append(op).append(rhs);
+            sb.append(')');
+            return sb;
+        }
+
+        public void visitSingle(Single term) {
+            Operator op = term.getOperator();
+            boolean negation = term.isNegation();
+
+            // gt = !le, lt = !ge
+            if (op.equals(Operator.gt)) {
+                op = Operator.le;
+                negation = !negation;
+            } else if (op.equals(Operator.lt)) {
+                op = Operator.ge;
+                negation = !negation;
+            }
+
+            if (negation) mLdapFilter.append("(!");
+
+            String attr = term.getLhs();
+            String val = getVal(term);
+            if (op.equals(Operator.has)) {
+                mLdapFilter.append('(').append(attr);
+                mLdapFilter.append("=*").append(val).append("*)");
+            } else if (op.equals(Operator.eq)) {
+                addCond(mLdapFilter, attr, "=", val);
+            } else if (op.equals(Operator.ge)) {
+                addCond(mLdapFilter, attr, ">=", val);
+            } else if (op.equals(Operator.le)) {
+                addCond(mLdapFilter, attr, "<=", val);
+            } else if (op.equals(Operator.startswith)) {
+                mLdapFilter.append('(').append(attr);
+                mLdapFilter.append('=').append(val).append("*)");
+            } else if (op.equals(Operator.endswith)) {
+                mLdapFilter.append('(').append(attr);
+                mLdapFilter.append("=*").append(val);
+                mLdapFilter.append(')');
+            } else {
+                // fallback to EQUALS
+                addCond(mLdapFilter, attr, "=", val);
+            }
+
+            if (negation) mLdapFilter.append(')');
+        }
+
+        public void enterMulti(Multi term) {
+            if (term.isNegation()) mLdapFilter.append("(!");
+            if (term.getTerms().size() > 1) {
+                if (term.isAnd())
+                    mLdapFilter.append("(&");
+                else
+                    mLdapFilter.append("(|");
+            }
+        }
+
+        public void leaveMulti(Multi term) {
+            if (term.getTerms().size() > 1) mLdapFilter.append(')');
+            if (term.isNegation()) mLdapFilter.append(')');
+        }
+        
+        protected String getVal(Single term) {
+            return LdapUtilCommon.escapeSearchFilterArg(term.getRhs());
+        }
+    }
+    
+    private static class LdapQueryVisitorIDN extends LdapQueryVisitor implements Visitor {
+        protected String getVal(Single term) {
+            String rhs = term.getRhs();
+            
+            AttributeManager attrMgr = null;
+            try {
+                attrMgr = AttributeManager.getInstance();
+            } catch (ServiceException e) {
+                ZimbraLog.account.warn("failed to get AttributeManager instance", e);
+            }
+            
+            IDNType idnType = AttributeManager.idnType(attrMgr, term.getLhs());
+            rhs = IDNUtil.toAscii(rhs, idnType);
+            
+            return rhs;
+        }
+    }
+
+    public static EntrySearchFilter sCalendarResourcesFilter;
+    static {
+        Single calResType = new Single(
+                false,
+                Provisioning.A_objectClass,
+                Operator.eq,
+                AttributeClass.OC_zimbraCalendarResource);
+        sCalendarResourcesFilter = new EntrySearchFilter(calResType);
+    }
+
+    public static String toLdapIDNFilter(String filterStr) {
+        String asciiQuery;
+        
+        try {
+            Term term = LdapFilterParser.parse(filterStr); 
+            EntrySearchFilter filter = new EntrySearchFilter(term);
+            asciiQuery = toLdapIDNFilter(filter);
+            ZimbraLog.account.debug("original query=[" + filterStr + "], converted ascii query=[" + asciiQuery + "]");
+        } catch (ServiceException e) {
+            ZimbraLog.account.warn("unable to convert query to ascii, using original query: " + filterStr, e);
+            asciiQuery = filterStr;
+        }
+        
+        return asciiQuery;
+    }
+    
+    /*
+     * serialize EntrySearchFilter to LDAP filter string
+     */
+    private static String toLdapIDNFilter(EntrySearchFilter filter)
+    throws ServiceException {
+        /*
+        if (!filter.usesIndex())
+            throw ServiceException.INVALID_REQUEST(
+                    "Search referring to no indexed attribute is not allowed: " + filter.toString(), null);
+        */
+        LdapQueryVisitor visitor = new LdapQueryVisitorIDN();
+        filter.traverse(visitor);
+        return visitor.getFilter();
+    }
+
+    public static String toLdapCalendarResourcesFilter(EntrySearchFilter filter)
+    throws ServiceException {
+        filter.andWith(sCalendarResourcesFilter);
+        if (!filter.usesIndex())
+            throw ServiceException.INVALID_REQUEST(
+                    "Search referring to no indexed attribute is not allowed: " + filter.toString(), null);
+        LdapQueryVisitor visitor = new LdapQueryVisitor();
+        filter.traverse(visitor);
+        return visitor.getFilter();
+    }
+}
---
+++ src/java/com/zimbra/cs/prov/ldap/LdapSMIMEConfig.java
@@ -0,0 +1,78 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2007, 2008, 2009, 2010 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.cs.prov.ldap;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.Entry;
+import com.zimbra.cs.extension.ExtensionUtil;
+
+public abstract class LdapSMIMEConfig {
+    
+    public static LdapSMIMEConfig getInstance() throws ServiceException {
+        return getInstance(null);
+    }
+    
+    public static LdapSMIMEConfig getInstance(Entry entry) throws ServiceException {
+        String className = "com.zimbra.cs.account.ldap.LdapSMIMEConfigImpl";
+        LdapSMIMEConfig instance = null;
+   
+        try {
+            if (entry == null) {
+                instance = (LdapSMIMEConfig)ExtensionUtil.findClass(className).newInstance();
+            } else {
+                instance = (LdapSMIMEConfig)ExtensionUtil.findClass(className).getConstructor(Entry.class).newInstance(entry);
+            }
+        } catch (ClassNotFoundException e) {     
+            throw ServiceException.FAILURE("cannot instantiate " + className, e);
+        } catch (IllegalArgumentException e) {
+            throw ServiceException.FAILURE("cannot instantiate " + className, e);
+        } catch (SecurityException e) {
+            throw ServiceException.FAILURE("cannot instantiate " + className, e);
+        } catch (InstantiationException e) {
+            throw ServiceException.FAILURE("cannot instantiate " + className, e);
+        } catch (IllegalAccessException e) {
+            throw ServiceException.FAILURE("cannot instantiate " + className, e);
+        } catch (InvocationTargetException e) {
+            throw ServiceException.FAILURE("cannot instantiate " + className, e);
+        } catch (NoSuchMethodException e) {
+            throw ServiceException.FAILURE("cannot instantiate " + className, e);
+        }
+           
+        if (instance == null) {
+            throw ServiceException.FAILURE("cannot instantiate " + className, null);
+        }
+            
+        return instance;
+    }
+        
+    protected LdapSMIMEConfig() {}
+    public abstract Set<String> getAllSMIMEAttributes();
+    public abstract Map<String, Map<String, Object>> get(String configName) throws ServiceException;
+    public abstract void modify(String configName, Map<String, Object> attrs) throws ServiceException;
+    public abstract void remove(String configName) throws ServiceException;
+    
+    public interface ResultCallback {
+        public void add(String field, String cert);
+    }
+    
+    public abstract void lookupPublicKeys(Account acct, String email, ResultCallback resultCallback) throws ServiceException;
+};
