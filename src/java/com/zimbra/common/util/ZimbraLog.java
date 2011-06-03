27d2cba3a1a6aabd33eab58d8d7dd869f15d7749
Unknown Perforce User <pshao>
2011-06-03 14:02:44 -0500

    bug: 59707
    integrate change 315696 from main to HELIX 
    --------------------------------------------------------------
    Change 315696 by pshao@pshao-macbookpro on 2011/06/03 11:54:16
    
            bug: 59707
    
            - deprecate the old set of LC keys and use httpclient_internal_***, httpclient_external_***.  (need to releace note).
    
            - cleanup to follow coding convention.
    
            https://reviewboard.eng.vmware.com/r/237316
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=59707
    
    Copied from Perforce
     Change: 315700

--- src/java/com/zimbra/common/util/ZimbraLog.java
+++ src/java/com/zimbra/common/util/ZimbraLog.java
@@ -1,13 +1,13 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011 Zimbra, Inc.
- *
+ * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
+ * 
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- *
+ * 
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -120,14 +120,25 @@ public final class ZimbraLog {
     public static final Log net = LogFactory.getLog("zimbra.net");
 
     /**
-     * the "zimbra.index" logger. For indexing.
+     * the "zimbra.index" logger. For general indexing-related events.
      */
     public static final Log index = LogFactory.getLog("zimbra.index");
 
     /**
-     * the "zimbra.search" logger. For search.
+     * the "zimbra.index.lucene" logger. For logging of low-level lucene operations (debug-level only)
      */
-    public static final Log search = LogFactory.getLog("zimbra.search");
+    public static final Log index_lucene = LogFactory.getLog("zimbra.index.lucene");
+
+    /**
+     * the "zimbra.index.search" logger. For logging of the search side of indexing
+     */
+    public static final Log index_search = LogFactory.getLog("zimbra.index.search");
+
+    /**
+     * the "zimbra.index.add" logger. For the add-to-the-index part of indexing
+     */
+    public static final Log index_add = LogFactory.getLog("zimbra.index.indexadd");
+
 
     /**
      * Fhe "zimbra.searchstat" logger.  For logging statistics about what kinds of searches are run
@@ -385,9 +396,6 @@ public final class ZimbraLog {
     public static final Log tnef = LogFactory.getLog("zimbra.tnef");
 
 
-    public static final Log nginxlookup = LogFactory.getLog("zimbra.nginxlookup");
-
-
     /**
      * Maps the log category name to its description.
      */
@@ -437,8 +445,7 @@ public final class ZimbraLog {
         // won't be listed in zmprov online help.
         Map<String, String> descriptions = new TreeMap<String, String>();
         descriptions.put(misc.getCategory(), "Miscellaneous");
-        descriptions.put(index.getCategory(), "Indexing operations");
-        descriptions.put(search.getCategory(), "Search operations");
+        descriptions.put(index.getCategory(), "Index operations");
         descriptions.put(redolog.getCategory(), "Redo log operations");
         descriptions.put(lmtp.getCategory(), "LMTP server (incoming mail)");
         descriptions.put(smtp.getCategory(), "SMTP client (outgoing mail)");
@@ -473,7 +480,6 @@ public final class ZimbraLog {
         descriptions.put(store.getCategory(), "Mail store disk operations");
         descriptions.put(purge.getCategory(), "Mailbox purge operations");
         descriptions.put(datasource.getCategory(), "Data Source operations");
-        descriptions.put(nginxlookup.getCategory(), "Nginx lookup operations");
         CATEGORY_DESCRIPTIONS = Collections.unmodifiableMap(descriptions);
     }
 
