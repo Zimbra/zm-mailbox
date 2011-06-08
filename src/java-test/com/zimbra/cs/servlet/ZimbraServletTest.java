809c058835bc45636f9ee2860c533e2044187230
Unknown Perforce User <jylee>
2011-06-08 13:19:20 -0500

    (split)bug 60345 added test case for UTF-8 encoded URL.
    http://bugzilla.zimbra.com/show_bug.cgi?id=60345
    
    Copied from Perforce
     Change: 316243

---
+++ src/java-test/com/zimbra/cs/servlet/ZimbraServletTest.java
@@ -0,0 +1,27 @@
+package com.zimbra.cs.servlet;
+
+import java.net.URL;
+
+import org.junit.Test;
+
+import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.service.MockHttpServletRequest;
+import com.zimbra.cs.service.MockHttpServletResponse;
+import com.zimbra.cs.servlet.ZimbraServlet;
+
+public class ZimbraServletTest {
+    
+    private static String uri = "/Briefcase/上的发生的发";
+
+    /**
+     * bug 60345
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void proxyTest() throws Exception {
+        MockHttpServletRequest req = new MockHttpServletRequest("test".getBytes("UTF-8"), new URL("http://localhost:7070/user1"+uri), "");
+        MockHttpServletResponse resp = new MockHttpServletResponse();
+        ZimbraServlet.proxyServletRequest(req, resp, Provisioning.getInstance().getLocalServer(), uri, null);
+    }
+}
