84946752ce794876895d94b8a270f362bf4c2520
Unknown Perforce User <ysasaki>
2011-06-30 18:11:34 -0500

    bug:42185 Socialcast SIEVE test - Built-in test for Socialcast notifications excluding bulk messages:  * from *@socialcast.com  * has Reply-To header (this should exclude bulk messages)
    https://reviewboard.eng.vmware.com/r/244533/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=42185
    
    Copied from Perforce
     Change: 319360

--- src/java/com/zimbra/common/soap/MailConstants.java
+++ src/java/com/zimbra/common/soap/MailConstants.java
@@ -579,6 +579,7 @@ public final class MailConstants {
     public static final String E_ADDRESS_BOOK_TEST = "addressBookTest";
     public static final String E_INVITE_TEST = "inviteTest";
     public static final String E_CONVERSATION_TEST = "conversationTest";
+    public static final String E_SOCIALCAST_TEST = "socialcastTest";
     public static final String E_LIST_TEST = "listTest";
     public static final String E_BULK_TEST = "bulkTest";
     public static final String E_METHOD = "method";
