be9693e05c90f86595a9397045d8ee54b8f15d43
Unknown Perforce User <jxing>
2011-07-03 22:59:23 -0500

    bug 61084
    integrate from main to HELIX
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=61084
    
    Copied from Perforce
     Change: 319580

--- src/java/com/zimbra/cs/milter/MinaMilterServer.java
+++ src/java/com/zimbra/cs/milter/MinaMilterServer.java
@@ -1,20 +1,22 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2010, 2011 Zimbra, Inc.
- *
+ * Copyright (C) 2010 Zimbra, Inc.
+ * 
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- *
+ * 
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
  */
 package com.zimbra.cs.milter;
 
-import org.apache.mina.core.session.IoSession;
+import java.util.Observable;
+import java.util.concurrent.ExecutorService;
+
 import org.apache.mina.filter.codec.ProtocolCodecFactory;
 import org.apache.mina.filter.codec.ProtocolDecoder;
 import org.apache.mina.filter.codec.ProtocolEncoder;
@@ -25,96 +27,90 @@ import sun.misc.SignalHandler;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.util.Log;
 import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.account.AttributeManager;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.accesscontrol.PermissionCache;
-import com.zimbra.cs.account.ldap.LdapProv;
-import com.zimbra.cs.server.NioConnection;
-import com.zimbra.cs.server.NioHandler;
-import com.zimbra.cs.server.NioServer;
+import com.zimbra.cs.account.ldap.LdapProvisioning;
+import com.zimbra.cs.account.ldap.ZimbraLdapContext;
+import com.zimbra.cs.mina.MinaCodecFactory;
+import com.zimbra.cs.mina.MinaHandler;
+import com.zimbra.cs.mina.MinaServer;
+import com.zimbra.cs.mina.MinaSession;
 import com.zimbra.cs.server.ServerConfig;
+import com.zimbra.cs.server.ServerManager;
 
-public final class NioMilterServer extends NioServer implements MilterServer {
-    private final ProtocolDecoder decoder = new NioMilterDecoder();
-    private final ProtocolEncoder encoder = new NioMilterEncoder();
+public class MinaMilterServer extends MinaServer implements MilterServer {
 
-    public NioMilterServer(ServerConfig config) throws ServiceException {
-        super(config);
-        registerMBean(getName());
+    public MinaMilterServer(ServerConfig config, ExecutorService pool) throws ServiceException {
+        super(config, pool);
+        registerMinaStatsMBean("MinaMilterServer");
+    }
+    
+    @Override public MinaHandler createHandler(MinaSession session) {
+        return new MinaMilterHandler(this, session);
     }
 
-    @Override
-    public String getName() {
-        return "MilterServer";
-    }
-
-    @Override
-    public NioHandler createHandler(NioConnection conn) {
-        return new NioMilterHandler(this, conn);
-    }
-
-    @Override
-    protected ProtocolCodecFactory getProtocolCodecFactory() {
-        return new ProtocolCodecFactory() {
-            @Override
-            public ProtocolDecoder getDecoder(IoSession session) {
-                return decoder;
+    @Override protected ProtocolCodecFactory getProtocolCodecFactory() {
+        return new MinaCodecFactory() {
+            @Override public ProtocolDecoder getDecoder() {
+                return new MinaMilterDecoder(getStats());
             }
-
-            @Override
-            public ProtocolEncoder getEncoder(IoSession session) {
-                return encoder;
+            
+            @Override public ProtocolEncoder getEncoder() {
+                return new MinaMilterEncoder(getStats());
             }
         };
     }
-
-    @Override
-    public MilterConfig getConfig() {
+    
+    @Override public MilterConfig getConfig() {
         return (MilterConfig) super.getConfig();
     }
-
-    @Override
-    public Log getLog() {
+    
+    @Override public Log getLog() {
         return ZimbraLog.milter;
     }
-
+    
     /* for running standalone milter server */
-
+    
+    private static ExecutorService milterNioHandlerPool;
     private static MilterServer milterServer;
-
+    
     private static class MilterShutdownHook extends Thread {
         private MilterServer server;
 
         public MilterShutdownHook(MilterServer server) {
             this.server = server;
         }
-
-        @Override
+        
         public void run() {
             try {
                 ZimbraLog.milter.info("Shutting down milter server");
                 server.stop();
             } catch (ServiceException e) {
-                ZimbraLog.milter.error("Server shutdown error: " + e.getMessage());
+                ZimbraLog.milter.error("Server shutdown error: " + e.getMessage());    
             }
         }
     }
-
+    
     public static void main(String[] args) {
         try {
             Provisioning prov = Provisioning.getInstance();
-            if (prov instanceof LdapProv) {
-                ((LdapProv) prov).waitForLdapServer();
+            if (prov instanceof LdapProvisioning) {
+                ZimbraLdapContext.waitForServer();
             }
-
+            
             MilterConfig config = new MilterConfig();
-            milterServer = new NioMilterServer(config);
+            milterNioHandlerPool = ServerManager.newNioHandlerPool(config);
+            milterServer = new MinaMilterServer(config, milterNioHandlerPool);
 
             // register the signal handler
             ClearCacheSignalHandler.register();
             
             MilterShutdownHook shutdownHook = new MilterShutdownHook(milterServer);
             Runtime.getRuntime().addShutdownHook(shutdownHook);
+            
 
+            
             ZimbraLog.milter.info("Starting milter server");
             milterServer.start();
         } catch (ServiceException e) {
