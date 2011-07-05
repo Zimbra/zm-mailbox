2c417d5609c9839ff713c5b8fa7a6536a5148ea4
Unknown Perforce User <ysasaki>
2011-07-05 13:37:16 -0500

    (split)bug:42185 RuleManager unit test
    http://bugzilla.zimbra.com/show_bug.cgi?id=42185
    
    Copied from Perforce
     Change: 319689

---
+++ src/java-test/com/zimbra/cs/filter/RuleManagerTest.java
@@ -0,0 +1,110 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011 Zimbra, Inc.
+ *
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.filter;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.MockProvisioning;
+import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.mailbox.DeliveryContext;
+import com.zimbra.cs.mailbox.Mailbox;
+import com.zimbra.cs.mailbox.MailboxManager;
+import com.zimbra.cs.mailbox.MailboxTestUtil;
+import com.zimbra.cs.mailbox.Message;
+import com.zimbra.cs.mailbox.OperationContext;
+import com.zimbra.cs.mime.ParsedMessage;
+import com.zimbra.cs.service.util.ItemId;
+
+/**
+ * Unit test for {@link RuleManager}.
+ *
+ * @author ysasaki
+ */
+public final class RuleManagerTest {
+
+    @BeforeClass
+    public static void init() throws Exception {
+        MailboxTestUtil.initServer();
+        Provisioning prov = Provisioning.getInstance();
+        prov.createAccount("test@zimbra.com", "secret", new HashMap<String, Object>());
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        MailboxTestUtil.clearData();
+    }
+
+    @Test
+    public void tagAndFileinto() throws Exception {
+        Account account = Provisioning.getInstance().getAccount(MockProvisioning.DEFAULT_ACCOUNT_ID);
+        Mailbox mbox = MailboxManager.getInstance().getMailboxByAccount(account);
+
+        RuleManager.clearCachedRules(account);
+        account.setMailSieveScript("if socialcast { tag \"priority\"; fileinto \"socialcast\"; }");
+        List<ItemId> ids = RuleManager.applyRulesToIncomingMessage(new OperationContext(mbox), mbox, new ParsedMessage(
+                "From: do-not-reply@socialcast.com\nReply-To: share@socialcast.com\nSubject: test".getBytes(), false),
+                0, account.getName(), new DeliveryContext(), Mailbox.ID_FOLDER_INBOX, true);
+        Assert.assertEquals(1, ids.size());
+        Message msg = mbox.getMessageById(null, ids.get(0).getId());
+        Assert.assertEquals("socialcast", mbox.getFolderById(null, msg.getFolderId()).getName());
+        Assert.assertEquals("priority", msg.getTagList().get(0).getName());
+
+        RuleManager.clearCachedRules(account);
+        account.setMailSieveScript("if socialcast { tag \"priority\"; }\n" +
+                "if header :contains [\"Subject\"] [\"Zimbra\"] { fileinto \"zimbra\"; }");
+        ids = RuleManager.applyRulesToIncomingMessage(new OperationContext(mbox), mbox, new ParsedMessage(
+                "From: do-not-reply@socialcast.com\nReply-To: share@socialcast.com\nSubject: Zimbra".getBytes(), false),
+                0, account.getName(), new DeliveryContext(), Mailbox.ID_FOLDER_INBOX, true);
+        Assert.assertEquals(1, ids.size());
+        msg = mbox.getMessageById(null, ids.get(0).getId());
+        Assert.assertEquals("zimbra", mbox.getFolderById(null, msg.getFolderId()).getName());
+        Assert.assertEquals("priority", msg.getTagList().get(0).getName());
+    }
+
+    @Test
+    public void tagAndTag() throws Exception {
+        Account account = Provisioning.getInstance().getAccount(MockProvisioning.DEFAULT_ACCOUNT_ID);
+        Mailbox mbox = MailboxManager.getInstance().getMailboxByAccount(account);
+
+        RuleManager.clearCachedRules(account);
+        account.setMailSieveScript("if socialcast { tag \"priority\"; tag \"socialcast\"; }");
+        List<ItemId> ids = RuleManager.applyRulesToIncomingMessage(new OperationContext(mbox), mbox, new ParsedMessage(
+                "From: do-not-reply@socialcast.com\nReply-To: share@socialcast.com\nSubject: test".getBytes(), false),
+                0, account.getName(), new DeliveryContext(), Mailbox.ID_FOLDER_INBOX, true);
+        Assert.assertEquals(1, ids.size());
+        Message msg = mbox.getMessageById(null, ids.get(0).getId());
+        Assert.assertEquals("priority", msg.getTagList().get(0).getName());
+        Assert.assertEquals("socialcast", msg.getTagList().get(1).getName());
+
+        RuleManager.clearCachedRules(account);
+        account.setMailSieveScript("if socialcast { tag \"priority\"; }\n" +
+                "if header :contains [\"Subject\"] [\"Zimbra\"] { tag \"zimbra\"; }");
+        ids = RuleManager.applyRulesToIncomingMessage(new OperationContext(mbox), mbox, new ParsedMessage(
+                "From: do-not-reply@socialcast.com\nReply-To: share@socialcast.com\nSubject: Zimbra".getBytes(), false),
+                0, account.getName(), new DeliveryContext(), Mailbox.ID_FOLDER_INBOX, true);
+        Assert.assertEquals(1, ids.size());
+        msg = mbox.getMessageById(null, ids.get(0).getId());
+        Assert.assertEquals("priority", msg.getTagList().get(0).getName());
+        Assert.assertEquals("zimbra", msg.getTagList().get(1).getName());
+    }
+
+}
