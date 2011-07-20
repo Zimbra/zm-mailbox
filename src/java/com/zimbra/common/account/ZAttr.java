dd9e887f6fbd10816b76bc897309867c75f78b0c
Unknown Perforce User <pshao>
2011-07-19 22:03:17 -0500

    bug: 10704
    fixed package name.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=10704
    
    Copied from Perforce
     Change: 321987

--- src/java/com/zimbra/common/account/ZAttr.java
+++ src/java/com/zimbra/common/account/ZAttr.java
@@ -12,7 +12,7 @@
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
  */
-package com.zimbra.cs.account;
+package com.zimbra.common.account;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
--- src/java/com/zimbra/common/account/ZAttrProvisioning.java
+++ src/java/com/zimbra/common/account/ZAttrProvisioning.java
@@ -12,7 +12,7 @@
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
  */
-package com.zimbra.cs.account;
+package com.zimbra.common.account;
 
 import com.zimbra.common.service.ServiceException;
 
