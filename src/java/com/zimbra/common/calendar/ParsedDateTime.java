12e3df78b8151dab8e7629ebf05792d2692be1b3
Unknown Perforce User <jhahm>
2011-07-22 15:58:12 -0500

    bug: 62368
    Integrating from HELIX to main
    
    Change 322446 by jhahm@jhahm-mac on 2011/07/22 13:56:34
    
            bug: 62368
    
            Removed an incorrect assertion in ParsedDateTime.  Changed iCalendar 
            format of all-day values in exception instances in the series update 
            notification to standard, from legacy-Outlook-compatible.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=62368
    
    Copied from Perforce
     Change: 322447

--- src/java/com/zimbra/common/calendar/ParsedDateTime.java
+++ src/java/com/zimbra/common/calendar/ParsedDateTime.java
@@ -600,7 +600,6 @@ public final class ParsedDateTime {
         if (!useOutlookCompatMode && !hasTime()) {
             toRet.addParameter(new ZParameter(ICalTok.VALUE, ICalTok.DATE.toString()));
         } else {
-            assert(isUTC() || tzName != null);
             if (tzName != null) {
                 toRet.addParameter(new ZParameter(ICalTok.TZID, tzName));
             } 
