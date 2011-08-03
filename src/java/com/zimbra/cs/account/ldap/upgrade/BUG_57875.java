e57fbf2acdf9586b0cb02fd1fa2c6e36bb9dfcbe
Unknown Perforce User <pshao>
2011-08-03 00:28:00 -0500

    (split)bug: 57875
    missed couple of files in previous checkin.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=57875
    
    Copied from Perforce
     Change: 323922

---
+++ src/java/com/zimbra/cs/account/ldap/upgrade/BUG_57875.java
@@ -0,0 +1,135 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.account.ldap.upgrade;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.account.Entry.EntryType;
+import com.zimbra.cs.ldap.IAttributes;
+import com.zimbra.cs.ldap.LdapClient;
+import com.zimbra.cs.ldap.LdapServerType;
+import com.zimbra.cs.ldap.LdapUsage;
+import com.zimbra.cs.ldap.SearchLdapOptions;
+import com.zimbra.cs.ldap.ZAttributes;
+import com.zimbra.cs.ldap.ZLdapContext;
+import com.zimbra.cs.ldap.ZLdapFilterFactory;
+import com.zimbra.cs.ldap.ZMutableEntry;
+
+public class BUG_57875 extends UpgradeOp {
+    
+    private static final String ATTR_NAME = Provisioning.A_zimbraGalLdapGroupHandlerClass;
+    private static final String OLD_VALUE = "com.zimbra.cs.gal.ADGalGroupHandler";
+    private static final String NEW_VALUE = "com.zimbra.cs.account.grouphandler.ADGroupHandler";
+    
+    @Override
+    void doUpgrade() throws ServiceException {
+        ZLdapContext zlc = LdapClient.getContext(LdapServerType.MASTER, LdapUsage.UPGRADE);
+        try {
+            upgradeGlobalConfig(zlc);
+            upgradeDomains(zlc);
+            upgradeDataSources(zlc);
+        } finally {
+            LdapClient.closeContext(zlc);
+        }
+    }
+    
+    @Override
+    Description getDescription() {
+        return new Description(this, 
+                new String[] {ATTR_NAME}, 
+                new EntryType[] {EntryType.GLOBALCONFIG, EntryType.DOMAIN, EntryType.DATASOURCE},
+                OLD_VALUE, 
+                NEW_VALUE, 
+                null);
+    }
+    
+    private void upgradeGlobalConfig(ZLdapContext zlc) throws ServiceException {
+        String bases[] = new String[] {prov.getDIT().configBranchBaseDN()};
+        
+        String query = "(&" + ZLdapFilterFactory.getInstance().globalConfig().toFilterString() + 
+        "(" + ATTR_NAME + "=" + OLD_VALUE + ")"+ ")";
+    
+        upgrade(zlc, bases, query);
+    }
+    
+    private void upgradeDomains(ZLdapContext zlc) {
+        String bases[] = prov.getDIT().getSearchBases(Provisioning.SD_DOMAIN_FLAG);
+        String query = "(&" + ZLdapFilterFactory.getInstance().allDomains().toFilterString() + 
+            "(" + ATTR_NAME + "=" + OLD_VALUE + ")"+ ")";
+        
+        upgrade(zlc, bases, query);
+    }
+    
+    private void upgradeDataSources(ZLdapContext zlc) {
+        String bases[] = new String[] {prov.getDIT().mailBranchBaseDN()};
+        
+        String query = "(&" + ZLdapFilterFactory.getInstance().allDataSources().toFilterString() + 
+        "(" + ATTR_NAME + "=" + OLD_VALUE + ")"+ ")";
+    
+        upgrade(zlc, bases, query);
+    }
+   
+    private void upgrade(ZLdapContext modZlc, String bases[], String filter) {
+        SearchLdapOptions.SearchLdapVisitor visitor = new Bug57875Visitor(this, modZlc);
+
+        String attrs[] = new String[] {ATTR_NAME};
+        
+        for (String base : bases) {
+            try {
+                printer.format("\n=== Searching LDAP: base = %s, filter = %s\n", base, filter);
+                prov.searchLdapOnMaster(base, filter, attrs, visitor);
+            } catch (ServiceException e) {
+                // log and continue
+                printer.println("Caught ServiceException while searching " + filter + " under base " + base);
+                printer.printStackTrace(e);
+            }
+        }
+    }
+    
+    private static class Bug57875Visitor extends SearchLdapOptions.SearchLdapVisitor {
+        private UpgradeOp upgradeOp;
+        private ZLdapContext modZlc;
+        
+        Bug57875Visitor(UpgradeOp upgradeOp, ZLdapContext modZlc) {
+            super(false);
+            this.upgradeOp = upgradeOp;
+            this.modZlc = modZlc;
+        }
+        
+        @Override
+        public void visit(String dn, IAttributes ldapAttrs) {
+            try {
+                if (upgradeOp.verbose) {
+                    upgradeOp.printer.println("Found entry " + dn);
+                }
+                
+                doVisit(dn, (ZAttributes) ldapAttrs);
+            } catch (ServiceException e) {
+                upgradeOp.printer.println("entry skipped, encountered error while processing entry at:" + dn);
+                upgradeOp.printer.printStackTrace(e);
+            }
+        }
+        
+        public void doVisit(String dn, ZAttributes ldapAttrs) throws ServiceException {
+            String curValue = ldapAttrs.getAttrString(ATTR_NAME);
+            if (OLD_VALUE.equals(curValue)) {
+                ZMutableEntry entry = LdapClient.createMutableEntry();
+                entry.setAttr(ATTR_NAME, NEW_VALUE);
+                upgradeOp.replaceAttrs(modZlc, dn, entry);
+            }
+        }
+    }
+
+}
---
+++ src/java/com/zimbra/cs/account/ldap/upgrade/Description.java
@@ -0,0 +1,48 @@
+package com.zimbra.cs.account.ldap.upgrade;
+
+import java.util.Arrays;
+
+import com.zimbra.cs.account.Entry.EntryType;
+
+
+public class Description {
+    private UpgradeOp op;
+    private String[] attrs;
+    private EntryType[] entryTypes;
+    private String oldValue;
+    private String newValue;
+    private String notes;
+    
+    Description(UpgradeOp op, String[] attrs, EntryType[] entryTypes,
+            String oldValue, String newValue, String notes) {
+        this.op = op;
+        this.attrs = attrs;
+        this.entryTypes = entryTypes;
+        this.oldValue = oldValue;
+        this.newValue = newValue;
+        this.notes = notes;
+    }
+    
+    void describe() {
+        LdapUpgradePrinter printer = op.printer;
+        
+        printer.format("Bug %s\n", op.getBug());
+        
+        if (attrs != null) {
+            printer.format("Attrs: %s\n", Arrays.deepToString(attrs));
+        }
+        
+        if (entryTypes != null) {
+            printer.format("Entry types: %s\n", Arrays.deepToString(entryTypes));
+        }
+        
+        printer.format("From value: %s\n", oldValue == null ? "" : oldValue);
+        printer.format("To value: %s\n", newValue == null ? "" : newValue);
+        
+        if (notes != null) {
+            printer.format("Notes: %s\n", notes);
+        }
+        
+        printer.println();
+    }
+}
