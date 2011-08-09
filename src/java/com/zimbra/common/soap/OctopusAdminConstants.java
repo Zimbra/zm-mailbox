db7ec992f3f33be875363aa6407a1acc178e8c4b
Unknown Perforce User <gelliot>
2011-08-09 03:16:06 -0500

    bug: 16343
    More JAXB classes to enable WSDL support of SOAP requests/responses.
    Some server-side handlers converted to use JAXB.
    
    Octopus Admin requests use urn:zimbraAdmin instead of urn:zimbraAdminExt
    
    Provisioning can now get Domains and Accounts by JAXB selector classes
    Various .classpath files updated to find ZimbraSoap source
    Some OctopusAdminService GetDevices/UpdateDeviceStatus sub-elements are now in the urn:zimbraMail namespace in order to re-use some JAXB classes.
    
    WsdlGenerator now uses a targetNamespace for the WSDL which differs
    from the namespace used for the imported .xsd file. wsdl2py tool wouldn't process the previous WSDL because of this.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=16343
    
    Copied from Perforce
     Change: 324735

---
+++ src/java/com/zimbra/common/soap/OctopusAdminConstants.java
@@ -0,0 +1,41 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011 Zimbra, Inc.
+ *
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.common.soap;
+
+import org.dom4j.Namespace;
+import org.dom4j.QName;
+
+public final class OctopusAdminConstants {
+
+    // private constructor prevents instantiation
+    private OctopusAdminConstants() {
+    }
+
+    public static final String NAMESPACE_STR = AdminConstants.NAMESPACE_STR;
+    public static final Namespace NAMESPACE = Namespace.get(NAMESPACE_STR);
+
+    public static final String E_GET_DEVICES_REQUEST = "GetDevicesRequest";
+    public static final String E_GET_DEVICES_RESPONSE = "GetDevicesResponse";
+
+    public static final String E_UPDATE_DEVICE_STATUS_REQUEST = "UpdateDeviceStatusRequest";
+    public static final String E_UPDATE_DEVICE_STATUS_RESPONSE = "UpdateDeviceStatusResponse";
+
+    public static final QName GET_DEVICES_REQUEST = QName.get(E_GET_DEVICES_REQUEST, NAMESPACE);
+    public static final QName GET_DEVICES_RESPONSE = QName.get(E_GET_DEVICES_RESPONSE, NAMESPACE);
+
+    public static final QName UPDATE_DEVICE_STATUS_REQUEST = QName.get(E_UPDATE_DEVICE_STATUS_REQUEST, NAMESPACE);
+    public static final QName UPDATE_DEVICE_STATUS_RESPONSE = QName.get(E_UPDATE_DEVICE_STATUS_RESPONSE, NAMESPACE);
+
+}
