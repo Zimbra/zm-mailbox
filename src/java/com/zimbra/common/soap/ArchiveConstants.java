5feefd9fad8ddc69ce82e89683ec2c270bdf4e59
Unknown Perforce User <pshao>
2011-08-11 14:54:25 -0500

    bug: 63134
    - Added CreateArchiveRequest/Response.  see soap-archive.txt.  CreateArchive does not set zimbraArchiveEnabled.
    
    - Added "zmarchiveconfig create" command.
    
    TODO: - depreate <cos>, <password>, <a> blocks in <archive> in EnableArchiveRequest after admin console is switched to use CreateArchiveRequest/Response, and release notes that EnableArchiveRequest/"zmarchiveconfig enable" will no longer create the archive account.
                - keep <name> in <archive>, this is useful if you are going to use a third party system to do the archiving and ZCS is just a mail forker.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=63134
    
    Copied from Perforce
     Change: 325375

--- src/java/com/zimbra/common/soap/ArchiveConstants.java
+++ src/java/com/zimbra/common/soap/ArchiveConstants.java
@@ -23,17 +23,24 @@ public final class ArchiveConstants {
     public static final Namespace NAMESPACE = Namespace.get(NAMESPACE_STR);
 
     public static final String E_NO_OP_REQUEST = "NoOpRequest";
+    
+    public static final String E_CREATE_ARCHIVE_REQUEST = "CreateArchiveRequest";
+    public static final String E_CREATE_ARCHIVE_RESPONSE = "CreateArchiveResponse";
+    
     public static final String E_ENABLE_ARCHIVE_REQUEST = "EnableArchiveRequest";
     public static final String E_ENABLE_ARCHIVE_RESPONSE = "EnableArchiveResponse";
 
     public static final String E_DISABLE_ARCHIVE_REQUEST = "DisableArchiveRequest";
     public static final String E_DISABLE_ARCHIVE_RESPONSE = "DisableArchiveResponse";
 
-    public static final QName ENABLE_ARCHIVE_REQUEST = QName.get( E_ENABLE_ARCHIVE_REQUEST, NAMESPACE);
-    public static final QName ENABLE_ARCHIVE_RESPONSE = QName.get( E_ENABLE_ARCHIVE_RESPONSE, NAMESPACE);
+    public static final QName CREATE_ARCHIVE_REQUEST = QName.get(E_CREATE_ARCHIVE_REQUEST, NAMESPACE);
+    public static final QName CREATE_ARCHIVE_RESPONSE = QName.get(E_CREATE_ARCHIVE_RESPONSE, NAMESPACE);
     
-    public static final QName DISABLE_ARCHIVE_REQUEST = QName.get( E_DISABLE_ARCHIVE_REQUEST, NAMESPACE);
-    public static final QName DISABLE_ARCHIVE_RESPONSE = QName.get( E_DISABLE_ARCHIVE_RESPONSE, NAMESPACE);
+    public static final QName ENABLE_ARCHIVE_REQUEST = QName.get(E_ENABLE_ARCHIVE_REQUEST, NAMESPACE);
+    public static final QName ENABLE_ARCHIVE_RESPONSE = QName.get(E_ENABLE_ARCHIVE_RESPONSE, NAMESPACE);
+    
+    public static final QName DISABLE_ARCHIVE_REQUEST = QName.get(E_DISABLE_ARCHIVE_REQUEST, NAMESPACE);
+    public static final QName DISABLE_ARCHIVE_RESPONSE = QName.get(E_DISABLE_ARCHIVE_RESPONSE, NAMESPACE);
 
     public static final String E_ARCHIVE = "archive";
     public static final String A_CREATE = "create";
