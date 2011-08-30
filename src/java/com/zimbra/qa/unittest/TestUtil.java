b869120d00a5ad46046ed0d503e65445d7b244cd
Unknown Perforce User <rgadipuuri>
2011-08-30 16:55:13 -0500

    (split)bug:63980 reverting back the changes to integrate using HELIX_to_main branch spec
    http://bugzilla.zimbra.com/show_bug.cgi?id=63980
    
    Copied from Perforce
     Change: 328233

--- src/java/com/zimbra/qa/unittest/TestUtil.java
+++ src/java/com/zimbra/qa/unittest/TestUtil.java
@@ -221,7 +221,7 @@ extends Assert {
         return mbox.addMessage(null, pm, dopt, null);
     }
 
-    public static String getTestMessage(String subject)
+    private static String getTestMessage(String subject)
     throws ServiceException, MessagingException, IOException {
         return new MessageBuilder().withSubject(subject).create();
     }
