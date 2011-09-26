59d093ae23c7ac89e72ab31b2390180a3b876b58
Unknown Perforce User <ysasaki>
2011-09-26 13:31:18 -0500

    (split)bug:61435 Swallow NO_SUCH_ACCOUNT in checkSubscription()
    http://bugzilla.zimbra.com/show_bug.cgi?id=61435
    
    Copied from Perforce
     Change: 331970

--- src/java/com/zimbra/cs/imap/ImapHandler.java
+++ src/java/com/zimbra/cs/imap/ImapHandler.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011 VMware, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -2012,6 +2012,10 @@ abstract class ImapHandler extends ProtocolHandler {
             //   The server MUST NOT unilaterally remove an existing mailbox name
             //   from the subscription list even if a mailbox by that name no
             //   longer exists.
+        } catch (AccountServiceException e) { // ignore NO_SUCH_ACCOUNT as well
+            if (!AccountServiceException.NO_SUCH_ACCOUNT.equals(e.getCode())) {
+                throw e;
+            }
         }
         if (pathMatches(path, pattern)) {
             hits.put(path, Boolean.TRUE);
