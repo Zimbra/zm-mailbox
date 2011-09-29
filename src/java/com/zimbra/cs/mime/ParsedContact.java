f156b43e63956414b5fa482cb80dd779fea6f19b
Unknown Perforce User <rgadipuuri>
2011-09-28 23:44:32 -0500

    (split)bug:62528 Changes are made to sync google contact images
    http://bugzilla.zimbra.com/show_bug.cgi?id=62528
    
    Copied from Perforce
     Change: 332543

--- src/java/com/zimbra/cs/mime/ParsedContact.java
+++ src/java/com/zimbra/cs/mime/ParsedContact.java
@@ -423,6 +423,8 @@ public class ParsedContact {
                 }
                 contactAttachments.add(attach);
             }
+        } else {
+            contactAttachments = null;
         }
         
         ContactGroup contactGroup = null;
