ecbab18ae0b27570227e2137ce1e43912dd873e1
Unknown Perforce User <ysasaki>
2011-10-11 20:29:06 -0500

    bug:65317 Accept empty "types" in SearchFolder element
    https://reviewboard.eng.vmware.com/r/271225/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=65317
    
    Copied from Perforce
     Change: 334079

--- src/java/com/zimbra/client/ZSearchFolder.java
+++ src/java/com/zimbra/client/ZSearchFolder.java
@@ -1,13 +1,13 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
- * 
+ * Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011 Zimbra, Inc.
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -15,6 +15,7 @@
 
 package com.zimbra.client;
 
+import com.google.common.base.Joiner;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
 import com.zimbra.common.soap.MailConstants;
@@ -27,77 +28,79 @@ import com.zimbra.soap.type.SrchSortBy;
 
 import org.json.JSONException;
 
-public class ZSearchFolder extends ZFolder {
+public final class ZSearchFolder extends ZFolder {
 
+    private String query;
+    private String types;
+    private SearchSortBy sortBy;
 
-    private String mQuery;
-    private String mTypes;
-    private SearchSortBy mSortBy;
-    
     public ZSearchFolder(Element e, ZFolder parent, ZMailbox mailbox) throws ServiceException {
         super(e, parent, mailbox);
-        mQuery = e.getAttribute(MailConstants.A_QUERY);
-        mTypes = e.getAttribute(MailConstants.A_SEARCH_TYPES, null);
+        query = e.getAttribute(MailConstants.A_QUERY);
+        types = e.getAttribute(MailConstants.A_SEARCH_TYPES, null);
         try {
-            mSortBy = SearchSortBy.fromString(e.getAttribute(MailConstants.A_SORTBY, SearchSortBy.dateDesc.name()));
+            sortBy = SearchSortBy.fromString(e.getAttribute(MailConstants.A_SORTBY, SearchSortBy.dateDesc.name()));
         } catch (ServiceException se) {
-            mSortBy = SearchSortBy.dateDesc;
+            sortBy = SearchSortBy.dateDesc;
         }
     }
 
     public ZSearchFolder(SearchFolder f, ZFolder parent, ZMailbox mailbox) throws ServiceException {
         super(f, parent, mailbox);
-        mQuery = f.getQuery();
-        mTypes = f.getTypes();
+        query = f.getQuery();
+        types = Joiner.on(',').join(f.getTypes());
         try {
-            mSortBy = SearchSortBy.fromString(
-                SystemUtil.coalesce(f.getSortBy(), SrchSortBy.dateDesc).toString());
+            sortBy = SearchSortBy.fromString(SystemUtil.coalesce(f.getSortBy(), SrchSortBy.dateDesc).toString());
         } catch (ServiceException se) {
-            mSortBy = SearchSortBy.dateDesc;
+            sortBy = SearchSortBy.dateDesc;
         }
     }
-    
+
+    @Override
     public void modifyNotification(ZModifyEvent e) throws ServiceException {
         if (e instanceof ZModifySearchFolderEvent) {
             ZModifySearchFolderEvent sfe = (ZModifySearchFolderEvent) e;
             if (sfe.getId().equals(getId())) {
-                mQuery = sfe.getQuery(mQuery);
-                mTypes = sfe.getTypes(mTypes);
-                mSortBy = sfe.getSortBy(mSortBy);
+                query = sfe.getQuery(query);
+                types = sfe.getTypes(types);
+                sortBy = sfe.getSortBy(sortBy);
                 super.modifyNotification(e);
             }
         }
     }
 
+    @Override
     public ZJSONObject toZJSONObject() throws JSONException {
         ZJSONObject jo = super.toZJSONObject();
-        jo.put("query", mQuery);
-        jo.put("types", mTypes);
-        jo.put("sortBy", mSortBy.name());
+        jo.put("query", query);
+        jo.put("types", types);
+        jo.put("sortBy", sortBy.name());
         return jo;
     }
 
     public String getQuery() {
-        return mQuery;
+        return query;
     }
 
     public SearchSortBy getSortBy() {
-        return mSortBy;
+        return sortBy;
     }
 
     public String getTypes() {
-        return mTypes;
+        return types;
     }
 
+    @Override
     public String toString() {
         return String.format("[ZSearchFolder %s]", getPath());
     }
 
+    @Override
     public ZSearchContext getSearchContext() {
-        ZSearchParams params = new ZSearchParams(mQuery);
-        params.setTypes(mTypes);
-        params.setSortBy(mSortBy);
+        ZSearchParams params = new ZSearchParams(query);
+        params.setTypes(types);
+        params.setSortBy(sortBy);
         return new ZSearchContext(params,getMailbox());
     }
-    
+
 }
