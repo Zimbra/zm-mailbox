dc61b156683e57f383a183b559884bea1f0419dd
Unknown Perforce User <pshao>
2011-10-14 13:08:47 -0500

    bug: 65070
    - chanegd default for zimbraPrefHtmlEditorDefaultFontFamily from "Times New Roman" to "times new roman, new york, times, serif"
    
    - added upgrade task: zmldapupgrade -b 65070
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=65070
    
    Copied from Perforce
     Change: 334545

--- src/java/com/zimbra/common/account/ZAttrProvisioning.java
+++ src/java/com/zimbra/common/account/ZAttrProvisioning.java
@@ -28,7 +28,7 @@ public class ZAttrProvisioning {
 
     ///// BEGIN-AUTO-GEN-REPLACE
 
-    /* build: 8.0.0_BETA1_1111 administrator 20111014-0926 */
+    /* build: 8.0.0_BETA1_1111 pshao 20111014-1053 */
 
     public static enum AccountCalendarUserType {
         RESOURCE("RESOURCE"),
@@ -10273,6 +10273,22 @@ public class ZAttrProvisioning {
      */
     @ZAttr(id=231)
     public static final String A_zimbraTimeZoneStandardRRule = "zimbraTimeZoneStandardRRule";
+
+    /**
+     * binary data
+     *
+     * @since ZCS 8.0.0
+     */
+    @ZAttr(id=10000)
+    public static final String A_zimbraUnittestBinary = "zimbraUnittestBinary";
+
+    /**
+     * binary data
+     *
+     * @since ZCS 8.0.0
+     */
+    @ZAttr(id=10001)
+    public static final String A_zimbraUnittestCertificate = "zimbraUnittestCertificate";
 
     /**
      * whether end-user services on SOAP and LMTP interfaces are enabled
