9c000fedbefc7d1a058f13f3df6e38035467ecb5
Unknown Perforce User <dkarp>
2011-10-18 15:43:40 -0500

    bug: 62871 - Server needs to send notifications with Folder ACLs
    If "tr=1" is set on the GetFolderRequest, we traverse all mountpoints and include the counts and children for their targets in the local "link" element.  ("tr" defaults to "0" except when the GetFolderRequest's top-level folder is itself a mountpoint.)  Broken links are rendered with "broken=1" on the resulting <link> element.
    
    Removed the "recursive" switch on GetFolderRequest and replaced it with a "depth" option.  When set to a nonnegative number, "depth" constrains the number of levels of folder hierarchy returned by GetFolder.  When it's negative, all levels are returned.
    
    Tweaked the GetFolder "view" switch to properly handle the case where a matching folder is a child of a non-matching folder.  "view" is now on the GetFolderRequest element, not on the optional "folder" subelement.
    
    Updated the JAXP files to match the new SOAP syntax.  Split its subelement off from Folder, as it has a different set of attrs.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=62871
    
    Copied from Perforce
     Change: 335125

--- src/java/com/zimbra/soap/mail/message/GetFolderRequest.java
+++ src/java/com/zimbra/soap/mail/message/GetFolderRequest.java
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2010 Zimbra, Inc.
+ * Copyright (C) 2010, 2011 Zimbra, Inc.
  * 
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
@@ -12,7 +12,6 @@
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
  */
-
 package com.zimbra.soap.mail.message;
 
 import javax.xml.bind.annotation.XmlAttribute;
@@ -21,58 +20,83 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 import com.zimbra.common.soap.MailConstants;
-import com.zimbra.soap.mail.type.Folder;
+import com.zimbra.soap.mail.type.GetFolderSpec;
 
 /*
-<GetFolderRequest [visible="0|1"] [needGranteeName="0|1"]>
+<GetFolderRequest [visible="0|1"] [needGranteeName="0|1"] [view="{folder-view-constraint}"] [depth="{subfolder-levels}"]>
   [<folder [l="{base-folder-id}"] [path="{fully-qualified-path}"]/>]
 </GetFolderRequest>
 
  */
+
 @XmlRootElement(name=MailConstants.E_GET_FOLDER_REQUEST)
 @XmlType(propOrder = {})
 public class GetFolderRequest {
 
-    @XmlAttribute(name=MailConstants.A_VISIBLE)
+    @XmlAttribute(name=MailConstants.A_VISIBLE, required=false)
     private Boolean isVisible;
-    @XmlAttribute(name=MailConstants.A_NEED_GRANTEE_NAME)
+
+    @XmlAttribute(name=MailConstants.A_NEED_GRANTEE_NAME, required=false)
     private boolean needGranteeName;
-    @XmlElement(name=MailConstants.E_FOLDER)
-    private Folder folder;
-    
+
+    @XmlAttribute(name=MailConstants.A_DEFAULT_VIEW, required=false)
+    private String viewConstraint;
+
+    @XmlAttribute(name=MailConstants.A_FOLDER_DEPTH, required=false)
+    private Integer treeDepth;
+
+    @XmlElement(name=MailConstants.E_FOLDER, required=false)
+    private GetFolderSpec folder;
+
     public GetFolderRequest() {
     }
-    
-    public GetFolderRequest(Folder folder) {
+
+    public GetFolderRequest(GetFolderSpec folder) {
         this(folder, null);
     }
-    
-    public GetFolderRequest(Folder folder, Boolean isVisible) {
+
+    public GetFolderRequest(GetFolderSpec folder, Boolean isVisible) {
         setFolder(folder);
         setVisible(isVisible);
     }
-    
+
     public Boolean isVisible() {
         return isVisible;
     }
-    
+
     public boolean isNeedGranteeName() {
         return needGranteeName;
     }
-    
-    public Folder getFolder() {
+
+    public String getViewConstraint() {
+        return viewConstraint;
+    }
+
+    public Integer getTreeDepth() {
+        return treeDepth;
+    }
+
+    public GetFolderSpec getFolder() {
         return folder;
     }
-    
+
     public void setVisible(Boolean isVisible) {
         this.isVisible = isVisible;
     }
-    
+
     public void setNeedGranteeName(boolean needGranteeName) {
         this.needGranteeName = needGranteeName;
     }
-    
-    public void setFolder(Folder folder) {
+
+    public void setViewConstraint(String viewConstraint) {
+        this.viewConstraint = viewConstraint;
+    }
+
+    public void setTreeDepth(Integer treeDepth) {
+        this.treeDepth = treeDepth;
+    }
+
+    public void setFolder(GetFolderSpec folder) {
         this.folder = folder;
     }
 }
---
+++ src/java/com/zimbra/soap/mail/type/GetFolderSpec.java
@@ -0,0 +1,56 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011 Zimbra, Inc.
+ *
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.soap.mail.type;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+
+import com.zimbra.common.soap.MailConstants;
+
+/*
+ * <folder [l="{base-folder-id}"] [path="{fully-qualified-path}"]/>
+ */
+
+@XmlAccessorType(XmlAccessType.NONE)
+public class GetFolderSpec {
+
+    // MailConstants.A_ID == "id"
+    @XmlAttribute(name=MailConstants.A_ID, required=false)
+    private String id;
+
+    // MailConstants.A_PATH == "path"
+    @XmlAttribute(name=MailConstants.A_PATH, required=false)
+    private String path;
+
+    public GetFolderSpec() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
