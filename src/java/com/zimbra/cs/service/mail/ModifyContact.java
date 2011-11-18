2d8e76ebdc5c938acf30d0db1515f85062667df5
Unknown Perforce User <gelliot>
2011-11-18 12:55:15 -0600

    (split)bug: 67327
    Support for changing tags on contacts via "tn" attribute.
    
    Also fixed JAXB class ContactAttr which mistakenly used "int" for optional attribute "s"
    
    https://reviewboard.eng.vmware.com/r/282679/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=67327
    
    Copied from Perforce
     Change: 339763

--- src/java/com/zimbra/cs/service/mail/ModifyContact.java
+++ src/java/com/zimbra/cs/service/mail/ModifyContact.java
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2004, 2005, 2006, 2007, 2009, 2010 Zimbra, Inc.
+ * Copyright (C) 2004, 2005, 2006, 2007, 2009, 2010, 2011 Zimbra, Inc.
  * 
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
@@ -28,8 +28,10 @@ import com.zimbra.common.soap.Element;
 import com.zimbra.common.util.Pair;
 import com.zimbra.cs.mailbox.Contact;
 import com.zimbra.cs.mailbox.Mailbox;
+import com.zimbra.cs.mailbox.MailItem;
 import com.zimbra.cs.mailbox.OperationContext;
 import com.zimbra.cs.mailbox.Contact.Attachment;
+import com.zimbra.cs.mailbox.util.TagUtil;
 import com.zimbra.cs.mime.ParsedContact;
 import com.zimbra.cs.service.util.ItemId;
 import com.zimbra.cs.service.util.ItemIdFormatter;
@@ -41,9 +43,12 @@ import com.zimbra.soap.ZimbraSoapContext;
 public class ModifyContact extends MailDocumentHandler  {
 
     private static final String[] TARGET_FOLDER_PATH = new String[] { MailConstants.E_CONTACT, MailConstants.A_ID };
+    @Override
     protected String[] getProxiedIdPath(Element request)     { return TARGET_FOLDER_PATH; }
+    @Override
     protected boolean checkMountpointProxy(Element request)  { return false; }
 
+    @Override
     public Element handle(Element request, Map<String, Object> context) throws ServiceException {
         ZimbraSoapContext zsc = getZimbraSoapContext(context);
         Mailbox mbox = getRequestedMailbox(zsc);
@@ -55,9 +60,11 @@ public class ModifyContact extends MailDocumentHandler  {
 
         Element cn = request.getElement(MailConstants.E_CONTACT);
         ItemId iid = new ItemId(cn.getAttribute(MailConstants.A_ID), zsc);
+        String tagsAttr = cn.getAttribute(MailConstants.A_TAG_NAMES, null);
+        String[] tags = (tagsAttr == null) ? null : TagUtil.decodeTags(tagsAttr);
 
         Contact contact = mbox.getContactById(octxt, iid.getId());
-        
+
         ParsedContact pc;
         if (replace) {
             Pair<Map<String,Object>, List<Attachment>> cdata = CreateContact.parseContact(cn, zsc, octxt, contact);
@@ -67,13 +74,16 @@ public class ModifyContact extends MailDocumentHandler  {
                 CreateContact.parseContactMergeMode(cn, zsc, octxt, contact);
             pc = new ParsedContact(contact).modify(cdata.getFirst(), cdata.getSecond());
         }
-        
+
         if (CreateContact.needToMigrateDlist(zsc)) {
             CreateContact.migrateFromDlist(pc);
         }
-        
+
         mbox.modifyContact(octxt, iid.getId(), pc);
-        
+        if (tags != null) {
+            mbox.setTags(octxt, iid.getId(), MailItem.Type.CONTACT, MailItem.FLAG_UNCHANGED, tags);
+        }
+
         Contact con = mbox.getContactById(octxt, iid.getId());
         Element response = zsc.createElement(MailConstants.MODIFY_CONTACT_RESPONSE);
         if (con != null) {
