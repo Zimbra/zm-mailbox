0f546a49d0ed38e7fe448592d83fec50119e6a3f
Unknown Perforce User <pshao>
2011-12-02 16:44:38 -0600

    bug: 57341
    renamed zimbraOctopusSmtp*** to zimbraRelaySmtp***
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=57341
    
    Copied from Perforce
     Change: 341243

--- src/java/com/zimbra/common/account/ZAttrProvisioning.java
+++ src/java/com/zimbra/common/account/ZAttrProvisioning.java
@@ -28,7 +28,7 @@ public class ZAttrProvisioning {
 
     ///// BEGIN-AUTO-GEN-REPLACE
 
-    /* build: unknown unknown unknown unknown */
+    /* build: 8.0.0_BETA1_1111 pshao 20111201-1307 */
 
     public static enum AccountCalendarUserType {
         RESOURCE("RESOURCE"),
@@ -7093,54 +7093,6 @@ public class ZAttrProvisioning {
     public static final String A_zimbraObjectType = "zimbraObjectType";
 
     /**
-     * Account name for authenticating to relay MTA.
-     *
-     * @since ZCS 8.0.0
-     */
-    @ZAttr(id=1343)
-    public static final String A_zimbraOctopusSmtpAuthAccount = "zimbraOctopusSmtpAuthAccount";
-
-    /**
-     * Password for authenticating to relay MTA.
-     *
-     * @since ZCS 8.0.0
-     */
-    @ZAttr(id=1344)
-    public static final String A_zimbraOctopusSmtpAuthPassword = "zimbraOctopusSmtpAuthPassword";
-
-    /**
-     * Whether to use credential to authenticate to relay MTA.
-     *
-     * @since ZCS 8.0.0
-     */
-    @ZAttr(id=1346)
-    public static final String A_zimbraOctopusSmtpAuthRequired = "zimbraOctopusSmtpAuthRequired";
-
-    /**
-     * SMTP hostname for relay MTA used for sending email notifications.
-     *
-     * @since ZCS 8.0.0
-     */
-    @ZAttr(id=1341)
-    public static final String A_zimbraOctopusSmtpHostname = "zimbraOctopusSmtpHostname";
-
-    /**
-     * SMTP port for relay MTA used for sending email notifications.
-     *
-     * @since ZCS 8.0.0
-     */
-    @ZAttr(id=1342)
-    public static final String A_zimbraOctopusSmtpPort = "zimbraOctopusSmtpPort";
-
-    /**
-     * Whether to use TLS when connecting to relay MTA.
-     *
-     * @since ZCS 8.0.0
-     */
-    @ZAttr(id=1345)
-    public static final String A_zimbraOctopusSmtpUseTls = "zimbraOctopusSmtpUseTls";
-
-    /**
      * allowed OpenID Provider Endpoint URLs for authentication
      *
      * @since ZCS 7.1.0
@@ -9187,6 +9139,54 @@ public class ZAttrProvisioning {
      */
     @ZAttr(id=1020)
     public static final String A_zimbraRedoLogRolloverMinFileAge = "zimbraRedoLogRolloverMinFileAge";
+
+    /**
+     * Account name for authenticating to relay MTA.
+     *
+     * @since ZCS 8.0.0
+     */
+    @ZAttr(id=1343)
+    public static final String A_zimbraRelaySmtpAuthAccount = "zimbraRelaySmtpAuthAccount";
+
+    /**
+     * Password for authenticating to relay MTA.
+     *
+     * @since ZCS 8.0.0
+     */
+    @ZAttr(id=1344)
+    public static final String A_zimbraRelaySmtpAuthPassword = "zimbraRelaySmtpAuthPassword";
+
+    /**
+     * Whether to use credential to authenticate to relay MTA.
+     *
+     * @since ZCS 8.0.0
+     */
+    @ZAttr(id=1346)
+    public static final String A_zimbraRelaySmtpAuthRequired = "zimbraRelaySmtpAuthRequired";
+
+    /**
+     * SMTP hostname for relay MTA used for sending email notifications.
+     *
+     * @since ZCS 8.0.0
+     */
+    @ZAttr(id=1341)
+    public static final String A_zimbraRelaySmtpHostname = "zimbraRelaySmtpHostname";
+
+    /**
+     * SMTP port for relay MTA used for sending email notifications.
+     *
+     * @since ZCS 8.0.0
+     */
+    @ZAttr(id=1342)
+    public static final String A_zimbraRelaySmtpPort = "zimbraRelaySmtpPort";
+
+    /**
+     * Whether to use TLS when connecting to relay MTA.
+     *
+     * @since ZCS 8.0.0
+     */
+    @ZAttr(id=1345)
+    public static final String A_zimbraRelaySmtpUseTls = "zimbraRelaySmtpUseTls";
 
     /**
      * Path to remote management command to execute on this server
