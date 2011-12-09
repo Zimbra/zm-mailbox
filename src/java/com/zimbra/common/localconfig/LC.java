d50cc81f7354ecb0a7bbcac20b25a792d7eb9c96
Unknown Perforce User <jflanigan>
2011-12-09 12:01:48 -0600

    bug: 68107
    Increase default IMAP inactive cache size to 10000.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=68107
    
    Copied from Perforce
     Change: 341925

--- src/java/com/zimbra/common/localconfig/LC.java
+++ src/java/com/zimbra/common/localconfig/LC.java
@@ -726,7 +726,7 @@ public final class LC {
 
     public static final KnownKey imap_max_request_size = KnownKey.newKey(10 * 1024);
     @Supported
-    public static final KnownKey imap_inactive_session_cache_size = KnownKey.newKey(1000);
+    public static final KnownKey imap_inactive_session_cache_size = KnownKey.newKey(10000);
     public static final KnownKey imap_max_idle_time = KnownKey.newKey(60);
     public static final KnownKey imap_authenticated_max_idle_time = KnownKey.newKey(1800);
     public static final KnownKey pop3_max_idle_time = KnownKey.newKey(60);
