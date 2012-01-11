fd338723b527e07e19a80f2076eb32cdfff6ed75
Unknown Perforce User <dywang>
2012-01-11 16:55:01 -0600

    bug: 69017 - added soap handlers for RemoveDevice, SuspendDevice, ResumeDevice for admin console. - added unit tests.
    http://bugzilla.zimbra.com/show_bug.cgi?id=69017
    
    Copied from Perforce
     Change: 345184

--- src/java/com/zimbra/common/soap/SyncAdminConstants.java
+++ src/java/com/zimbra/common/soap/SyncAdminConstants.java
@@ -33,6 +33,12 @@ public final class SyncAdminConstants {
     public static final String E_CANCEL_PENDING_REMOTE_WIPE_RESPONSE = "CancelPendingRemoteWipeResponse";
     public static final String E_GET_DEVICE_STATUS_REQUEST = "GetDeviceStatusRequest";
     public static final String E_GET_DEVICE_STATUS_RESPONSE = "GetDeviceStatusResponse";
+    public static final String E_REMOVE_DEVICE_REQUEST = "RemoveDeviceRequest";
+    public static final String E_REMOVE_DEVICE_RESPONSE = "RemoveDeviceResponse";
+    public static final String E_SUSPEND_DEVICE_REQUEST = "SuspendDeviceRequest";
+    public static final String E_SUSPEND_DEVICE_RESPONSE = "SuspendDeviceResponse";
+    public static final String E_RESUME_DEVICE_REQUEST = "ResumeDeviceRequest";
+    public static final String E_RESUME_DEVICE_RESPONSE = "ResumeDeviceResponse";
 
     public static final QName GET_DEVICES_COUNT_REQUEST = QName.get(E_GET_DEVICES_COUNT_REQUEST, NAMESPACE);
     public static final QName GET_DEVICES_COUNT_RESPONSE = QName.get(E_GET_DEVICES_COUNT_RESPONSE, NAMESPACE);
@@ -46,6 +52,12 @@ public final class SyncAdminConstants {
     public static final QName CANCEL_PENDING_REMOTE_WIPE_RESPONSE = QName.get(E_CANCEL_PENDING_REMOTE_WIPE_RESPONSE, NAMESPACE);
     public static final QName GET_DEVICE_STATUS_REQUEST = QName.get(E_GET_DEVICE_STATUS_REQUEST, NAMESPACE);
     public static final QName GET_DEVICE_STATUS_RESPONSE = QName.get(E_GET_DEVICE_STATUS_RESPONSE, NAMESPACE);
+    public static final QName REMOVE_DEVICE_REQUEST = QName.get(E_REMOVE_DEVICE_REQUEST, NAMESPACE);
+    public static final QName REMOVE_DEVICE_RESPONSE = QName.get(E_REMOVE_DEVICE_RESPONSE, NAMESPACE);
+    public static final QName SUSPEND_DEVICE_REQUEST = QName.get(E_SUSPEND_DEVICE_REQUEST, NAMESPACE);
+    public static final QName SUSPEND_DEVICE_RESPONSE = QName.get(E_SUSPEND_DEVICE_RESPONSE, NAMESPACE);
+    public static final QName RESUME_DEVICE_REQUEST = QName.get(E_RESUME_DEVICE_REQUEST, NAMESPACE);
+    public static final QName RESUME_DEVICE_RESPONSE = QName.get(E_RESUME_DEVICE_RESPONSE, NAMESPACE);
 
     public static final String E_LAST_USED_DATE = "lastUsedDate";
 
