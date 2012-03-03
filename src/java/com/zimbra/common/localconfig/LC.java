7e9cdb0373c0e4f2c16a5913f02e4329b52991c1
Unknown Perforce User <pburgu>
2012-03-02 19:35:56 -0600

    bug:69494 Implement throttle for requests from an IP for LMTP service. By default, no throttling applied because the requests mostly come from internal MTAs. To enable, throttling for LMTP service add the hostname/IP of MTA to zimbraThrottleSafeHosts and set the LC key lmtp_throttle_ip_limit.
    http://bugzilla.zimbra.com/show_bug.cgi?id=69494
    
    Copied from Perforce
     Change: 351141

--- src/java/com/zimbra/common/localconfig/LC.java
+++ src/java/com/zimbra/common/localconfig/LC.java
@@ -754,6 +754,8 @@ public final class LC {
     public static final KnownKey pop3_max_idle_time = KnownKey.newKey(60);
     public static final KnownKey pop3_throttle_ip_limit = KnownKey.newKey(200);
     public static final KnownKey pop3_throttle_acct_limit = KnownKey.newKey(200);
+    
+    public static final KnownKey lmtp_throttle_ip_limit = KnownKey.newKey(0);
 
     public static final KnownKey milter_bind_port = KnownKey.newKey(0);
     public static final KnownKey milter_bind_address = KnownKey.newKey(null);
