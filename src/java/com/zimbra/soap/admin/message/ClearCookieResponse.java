dee319c04b3a23824d33eff8071122d32dcfe255
Unknown Perforce User <pshao>
2012-03-16 18:02:06 -0500

    bug: 71479
    Added an optional <authTokenControl voidOnExpired="1"/> element in soap context.  If this element is present, and if the incoming auth token is expired, server will just "void"(i.e. set the mAuthToken field in ZimbraSoapContext to null, as if there is no auth token for the request ) the auth token instead of throwing AUTH_EXPIRED.
    
    The auth token can come in the <authToken> element in soap context, or in a http cookie as a fallback,   <authTokenControl voidOnExpired="1"/> does not distinguish the two cases.   It just applies to the auth token object returned from the AuthProvider.
    
    In other words, if the incoming auth token is expired:
    - If <authTokenControl voidOnExpired="1"/> is not present, server will throw AUTH_EXPIRED exception - nothing changed.
    
    - If <authTokenControl voidOnExpired="1"/> is present:
          - if a valid auth token is required for the request, server will throw AUTH_REQUIRED.
          - otherwise, the request will go through
    
    
    <authTokenControl voidOnExpired="1"/> should be used with the ClearCookieRequest.  Usage is when browser sends in expired auth token cookie beyond the control of the web client code.   This problem is triggered becasue we now set httpOnly attribute on auth token cookie, as a result, client cannot get to or clear cookies using javascript.    We can't rely on the Max-Age directive on the cookie - we set Max-Age only when user opts to "remember me" (i.e. ask browser to persist cookies after browser exits.).
    
    
    https://reviewboard.eng.vmware.com/r/317464/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=71479
    
    Copied from Perforce
     Change: 353623

--- src/java/com/zimbra/soap/admin/message/ClearCookieResponse.java
+++ src/java/com/zimbra/soap/admin/message/ClearCookieResponse.java
@@ -14,9 +14,15 @@
  */
 package com.zimbra.soap.admin.message;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import com.zimbra.common.soap.AdminConstants;
 
+@XmlAccessorType(XmlAccessType.NONE)
 @XmlRootElement(name=AdminConstants.E_CLEAR_COOKIE_RESPONSE)
 public class ClearCookieResponse {
+    
+    public ClearCookieResponse() {
+    }
 }
