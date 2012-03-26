dc7e7056eacd0b0c023885a3019a0fca0bd73479
Unknown Perforce User <dywang>
2012-03-26 13:45:53 -0500

    bug:63365
    Used ResultsPager to enforce limit of search results.
    Introduced LC zimbra_activesync_search_max_results to limit the number of search result server returns to device.
    Changed GalSearch to use offset and limit consistent with MailboxSearch.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=63365
    
    Copied from Perforce
     Change: 355149

--- src/java/com/zimbra/common/localconfig/LC.java
+++ src/java/com/zimbra/common/localconfig/LC.java
@@ -1072,6 +1072,7 @@ public final class LC {
     public static final KnownKey zimbra_activesync_heartbeat_interval_min = KnownKey.newKey(60); //60 Seconds = 1 min
     //make sure it's less than nginx's zimbraReverseProxyUpstreamPollingTimeout, which is now 3600 seconds
     public static final KnownKey zimbra_activesync_heartbeat_interval_max = KnownKey.newKey(3540); //3540 Seconds = 59 mins,
+    public static final KnownKey zimbra_activesync_search_max_results = KnownKey.newKey(500);
 
     public static final KnownKey zimbra_slow_logging_enabled = KnownKey.newKey(false);
     public static final KnownKey zimbra_slow_logging_threshold = KnownKey.newKey(5000);
