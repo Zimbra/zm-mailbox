9c1b4cf928db86c4f5bbca14894f34266d9a6480
Unknown Perforce User <pburgu>
2012-03-29 14:51:05 -0500

    bug:71713 SOAP and JAXB changes
    http://bugzilla.zimbra.com/show_bug.cgi?id=71713
    
    Copied from Perforce
     Change: 362369

--- src/java/com/zimbra/soap/mail/message/CounterAppointmentRequest.java
+++ src/java/com/zimbra/soap/mail/message/CounterAppointmentRequest.java
@@ -18,6 +18,7 @@ package com.zimbra.soap.mail.message;
 import com.google.common.base.Objects;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -37,6 +38,38 @@ import com.zimbra.soap.mail.type.Msg;
 public class CounterAppointmentRequest {
 
     /**
+     * @zm-api-field-tag invite-id-of-default-invite
+     * @zm-api-field-description Invite ID of default invite
+     */
+    @XmlAttribute(name=MailConstants.A_ID /* id */, required=false)
+    private String id;
+
+    /**
+     * @zm-api-field-tag default-invite-component-num
+     * @zm-api-field-description Component number of default component
+     */
+    @XmlAttribute(name=MailConstants.A_CAL_COMP /* comp */, required=false)
+    private Integer componentNum;
+
+    /**
+     * @zm-api-field-tag changed-seq-of-fetched-version
+     * @zm-api-field-description Changed sequence of fetched version.
+     * <br />
+     * Used for conflict detection.  By setting this, the request indicates which version of the appointment it is
+     * attempting to propose.  If the appointment was updated on the server between the fetch and modify, an
+     * <b>INVITE_OUT_OF_DATE exception</b> will be thrown.
+     */
+    @XmlAttribute(name=MailConstants.A_MODIFIED_SEQUENCE /* ms */, required=false)
+    private Integer modifiedSequence;
+
+    /**
+     * @zm-api-field-tag
+     * @zm-api-field-description
+     */
+    @XmlAttribute(name=MailConstants.A_REVISION /* rev */, required=false)
+    private Integer revision;
+
+    /**
      * @zm-api-field-description Details of counter proposal.
      */
     @XmlElement(name=MailConstants.E_MSG /* m */, required=false)
@@ -44,6 +77,15 @@ public class CounterAppointmentRequest {
 
     public CounterAppointmentRequest() {
     }
+    
+    public void setId(String id) { this.id = id; }
+    public void setComponentNum(Integer componentNum) { this.componentNum = componentNum; }
+    public void setModifiedSequence(Integer modifiedSequence) { this.modifiedSequence = modifiedSequence; }
+    public void setRevision(Integer revision) { this.revision = revision; }
+    public String getId() { return id; }
+    public Integer getComponentNum() { return componentNum; }
+    public Integer getModifiedSequence() { return modifiedSequence; }
+    public Integer getRevision() { return revision; }
 
     public void setMsg(Msg msg) { this.msg = msg; }
     public Msg getMsg() { return msg; }
@@ -51,7 +93,11 @@ public class CounterAppointmentRequest {
     public Objects.ToStringHelper addToStringInfo(
                 Objects.ToStringHelper helper) {
         return helper
-            .add("msg", msg);
+            .add("msg", msg)
+            .add("id", id)
+            .add("componentNum", componentNum)
+            .add("modifiedSequence", modifiedSequence)
+            .add("revision", revision);
     }
 
     @Override
