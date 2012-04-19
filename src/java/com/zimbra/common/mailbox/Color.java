79725af62a5eb9efe526513ef18210b0008b5c40
Unknown Perforce User <dkarp>
2012-04-19 17:33:19 -0500

    bug: 69714
    Consolidate all the variants of Mailbox.createFolder() and Folder.create().  Mailbox.createFolder() now takes a Folder.FolderOptions object, which allows the caller the option of setting the new folder's color, flags, attributes, sync URL, default view, creation date, and UUID.  Deprecated the Mailbox.createFolder() versions present in 7.X in order to delete them in the next major ZCS revision.  (No reason to keep them for Octopus, either.)
    
    Tweaked the CreateFolderPath redo op and the path-based Mailbox.createFolder() to support setting creation date on the new folder.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=69714
    
    Copied from Perforce
     Change: 365267

--- src/java/com/zimbra/common/mailbox/Color.java
+++ src/java/com/zimbra/common/mailbox/Color.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2011 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -29,20 +29,27 @@ public class Color {
     public Color(long rgb) {
         setRgb(rgb);
     }
+
     public Color(byte c) {
         setColor(c);
     }
+
     public Color(String color) {
         // string representation of color.  e.g. #00008B, #F0F8FF, etc
         setColor(color);
     }
+
     // internal use only.  changing the visibility to public
     // in order to allow redo players to use it.
     public static Color fromMetadata(long value) {
         return (value & RGB_INDICATOR) != 0 ? new Color(value & RGB_MASK) : new Color((byte)value);
     }
+
     public static byte getMappedColor(String s) {
-        if (s == null) return (byte)ORANGE;
+        if (s == null) {
+            return (byte)ORANGE;
+        }
+
         try {
             long value = s.startsWith("#") ? Long.parseLong(s.substring(1), 16) : Long.parseLong(s);
             for (int i = 0; i < COLORS.length; i++) {
@@ -54,48 +61,61 @@ public class Color {
         catch (NumberFormatException e) {
             // ignore
         }
-        return (byte)ORANGE;
+        return (byte) ORANGE;
     }
+
     public long getValue() {
         return mValue;
     }
+
     public byte getRed() {
-        return (byte)((getRgb() >> 16) & 0xff);
+        return (byte) ((getRgb() >> 16) & 0xff);
     }
+
     public byte getGreen() {
-        return (byte)((getRgb() >> 8) & 0xff);
+        return (byte) ((getRgb() >> 8) & 0xff);
     }
+
     public byte getBlue() {
-        return (byte)(getRgb() & 0xff);
+        return (byte) (getRgb() & 0xff);
     }
+
     public long getRgb() {
-        return hasMapping() ? COLORS[(int)mValue] : mValue & RGB_MASK;
+        return hasMapping() ? COLORS[(int) mValue] : mValue & RGB_MASK;
     }
+
     public byte getMappedColor() {
-        return hasMapping() ? (byte)mValue : ORANGE;
+        return hasMapping() ? (byte) mValue : ORANGE;
     }
+
     public boolean hasMapping() {
         return (mValue & RGB_INDICATOR) == 0;
     }
+
     public void set(Color that) {
         mValue = that.mValue;
     }
+
     public void setRgb(long rgb) {
         mValue = rgb | RGB_INDICATOR;
     }
+
     public void setColor(byte color) {
-        if (color > ORANGE || color < 0)
+        if (color > ORANGE || color < 0) {
             color = ORANGE;
+        }
         mValue = color;
     }
+
     public void setColor(String color) {
-        if (color.length() == 4 || color.length() == 7)
+        if (color.length() == 4 || color.length() == 7) {
             color = color.substring(1);
+        }
         if (color.length() == 3) {
             String r = color.substring(0, 1);
             String g = color.substring(1, 2);
             String b = color.substring(2, 3);
-            color = r+r+g+g+b+b;
+            color = r + r + g + g + b + b;
         }
         if (color.length() == 6) {
             long rgb =
@@ -105,18 +125,23 @@ public class Color {
             setRgb(rgb);
         }
     }
+
     public long toMetadata() {
         return mValue;
     }
 
     private long mValue;
 
-    @Override public boolean equals(Object that) {
-        if (that instanceof Color)
+    @Override
+    public boolean equals(Object that) {
+        if (that instanceof Color) {
             return mValue == ((Color) that).mValue;
+        }
         return false;
     }
-    @Override public String toString() {
+
+    @Override
+    public String toString() {
         return String.format("#%06X", getRgb());
     }
 }
