a80bc5a5e3d2c4de80ae7c025614f9f07add3b07
Unknown Perforce User <jflanigan>
2012-04-25 12:10:54 -0500

    (split)bug: 73396
    Use createElement(QName) instead of createElement(String)
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=73396
    
    Copied from Perforce
     Change: 366089

--- src/java/com/zimbra/cs/service/admin/VerifyStoreManager.java
+++ src/java/com/zimbra/cs/service/admin/VerifyStoreManager.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2012 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -159,7 +159,7 @@ public class VerifyStoreManager extends AdminDocumentHandler {
 
         sm.delete(mblob);
     }
-    
+
     private void checkBlob(ParsedMessage pm, Blob blob, StagedBlob staged, MailboxBlob linked, MailboxBlob fetched, Mailbox mbox)
     throws IOException, Exception {
         byte[] mimeBytes = readInputStream(pm.getRawInputStream());
@@ -249,9 +249,10 @@ public class VerifyStoreManager extends AdminDocumentHandler {
         Provisioning.getInstance().deleteAccount(acct.getId());
     }
 
+    @Override
     public synchronized Element handle(Element request, Map<String, Object> context) throws ServiceException {
         ZimbraSoapContext zsc = getZimbraSoapContext(context);
-        Element response = zsc.createElement(AdminConstants.E_VERIFY_STORE_MANAGER_RESPONSE);
+        Element response = zsc.createElement(AdminConstants.VERIFY_STORE_MANAGER_RESPONSE);
 
         boolean created = createAccountIfNeeded();
         try {
