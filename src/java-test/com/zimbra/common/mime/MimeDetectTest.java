7279242c8d2357000dc84a83dabf91adf129563b
Unknown Perforce User <rgadipuuri>
2012-05-18 16:01:52 -0500

    jira: HD-2356
    Copied from Perforce
     Change: 371514

--- src/java-test/com/zimbra/common/mime/MimeDetectTest.java
+++ src/java-test/com/zimbra/common/mime/MimeDetectTest.java
@@ -1,12 +1,15 @@
 package com.zimbra.common.mime;
 
+import java.io.IOException;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MimeDetectTest {
 
     @Test
-    public void testFileName() {
+    public void testFileName() throws IOException {
+        MimeDetect.getMimeDetect().addGlob("image/jpeg", "*.jpeg", 50);
         Assert.assertEquals("image/jpeg", MimeDetect.getMimeDetect().detect("2011.07.19 089+.JPG"));
         Assert.assertEquals("image/jpeg", MimeDetect.getMimeDetect().detect("2011.07.18 706+.jpg"));
         Assert.assertEquals("image/jpeg", MimeDetect.getMimeDetect().detect("2011.07.18 706+.jPg"));
