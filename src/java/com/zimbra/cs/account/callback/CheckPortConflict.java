a33979bf79e01169388d945231bb7ec90531b7d3
Unknown Perforce User <jylee>
2012-05-24 16:14:14 -0500

    (split)jira: HD-72
    added the new attribute zimbraMessageChannelPort to CheckPortConflict.
    
    https://reviewboard.eng.vmware.com/r/340871
    
    Copied from Perforce
     Change: 372501

--- src/java/com/zimbra/cs/account/callback/CheckPortConflict.java
+++ src/java/com/zimbra/cs/account/callback/CheckPortConflict.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2007, 2008, 2009, 2010 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -33,9 +33,9 @@ import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.Server;
 
 public class CheckPortConflict extends AttributeCallback {
-    
+
     private static final Set<String> sPortAttrs = new HashSet<String>();
-        
+
     static {
         // TODO: use a flag in zimbra-attrs.xml and generate this map automatically
         sPortAttrs.add(Provisioning.A_zimbraAdminPort);
@@ -47,28 +47,30 @@ public class CheckPortConflict extends AttributeCallback {
         sPortAttrs.add(Provisioning.A_zimbraImapSSLProxyBindPort);
 
         sPortAttrs.add(Provisioning.A_zimbraLmtpBindPort);
-            
+
         sPortAttrs.add(Provisioning.A_zimbraMailPort);
         sPortAttrs.add(Provisioning.A_zimbraMailSSLPort);
         sPortAttrs.add(Provisioning.A_zimbraMailSSLClientCertPort);
-            
+
         sPortAttrs.add(Provisioning.A_zimbraPop3BindPort);
         sPortAttrs.add(Provisioning.A_zimbraPop3SSLBindPort);
         sPortAttrs.add(Provisioning.A_zimbraPop3ProxyBindPort);
         sPortAttrs.add(Provisioning.A_zimbraPop3SSLProxyBindPort);
-            
+
         sPortAttrs.add(Provisioning.A_zimbraRemoteManagementPort);
-        
+
         sPortAttrs.add(Provisioning.A_zimbraMemcachedBindPort);
 
         sPortAttrs.add(Provisioning.A_zimbraMailProxyPort);
         sPortAttrs.add(Provisioning.A_zimbraMailSSLProxyPort);
         sPortAttrs.add(Provisioning.A_zimbraMailSSLProxyClientCertPort);
+
+        sPortAttrs.add(Provisioning.A_zimbraMessageChannelPort);
     }
-        
+
     @Override
     public void preModify(CallbackContext context, String attrName, Object attrValue,
-            Map attrsToModify, Entry entry) 
+            Map attrsToModify, Entry entry)
     throws ServiceException {
 
         if (entry != null && !(entry instanceof Server) && !(entry instanceof Config)) return;
@@ -76,23 +78,23 @@ public class CheckPortConflict extends AttributeCallback {
         if (context.isDoneAndSetIfNot(CheckPortConflict.class)) {
             return;
         }
-        
+
         // sanity check, zimbra-attrs.xml and the sPortAttrsToCheck map has to be in sync
         if (!sPortAttrs.contains(attrName) ||
             !AttributeManager.getInstance().isServerInherited(attrName))
             assert(false);
-        
+
         // server == null means the server entry is being created
         if (entry == null || entry instanceof Server)
             checkServer((Server)entry, attrsToModify);
-        else 
+        else
             checkConfig((Config)entry, attrsToModify);
     }
-    
+
     private void checkServer(Server server, Map<String, Object> serverAttrsToModify) throws ServiceException {
         Map<String, String> ports = new HashMap<String, String>();
         Map<String, Object> defaults = Provisioning.getInstance().getConfig().getServerDefaults();
-        
+
         // collect current port values
         if (server != null) {
             for (String attrName : sPortAttrs) {
@@ -100,14 +102,14 @@ public class CheckPortConflict extends AttributeCallback {
                     ports.put(server.getAttr(attrName), attrName);
             }
         }
-        
-        // check conflict for attrs being changed 
+
+        // check conflict for attrs being changed
         for (Map.Entry<String, Object> attrToModify : serverAttrsToModify.entrySet()) {
             String attrName = attrToModify.getKey();
-            
+
             if (!sPortAttrs.contains(attrName))
                 continue;
-            
+
             SingleValueMod mod = singleValueMod(serverAttrsToModify, attrName);
             String newValue = null;
             if (mod.setting())
@@ -120,64 +122,64 @@ public class CheckPortConflict extends AttributeCallback {
                         newValue = (String)defValue;
                     else
                         ZimbraLog.misc.info("default value for " + attrName + " should be a single-valued attribute, invalid default value ignored");
-                } 
+                }
             }
-            
+
             if (conflict(ports, newValue)) {
                 String serverInfo = (server == null) ? "" : " on server " + server.getName();
-                throw ServiceException.INVALID_REQUEST("port " + newValue + " conflict between " + 
+                throw ServiceException.INVALID_REQUEST("port " + newValue + " conflict between " +
                                                        attrName + " and " + ports.get(newValue) + serverInfo, null);
             } else
                 ports.put(newValue, attrName);
         }
     }
-    
+
     private void checkConfig(Config config, Map<String, Object> configAttrsToModify) throws ServiceException {
         BiMap<String, String> newDefaults = HashBiMap.create();
-            
+
         /*
          * First, make sure there is no conflict in the Config entry, even
          * if the value on the config entry might not be effective on a server.
-         */ 
+         */
         for (String attrName : sPortAttrs) {
             if (!configAttrsToModify.containsKey(attrName))
                 newDefaults.put(config.getAttr(attrName), attrName);
         }
-        
-        // check conflict for attrs being changed 
+
+        // check conflict for attrs being changed
         for (Map.Entry<String, Object> attrToModify : configAttrsToModify.entrySet()) {
             String attrName = attrToModify.getKey();
-            
+
             if (!sPortAttrs.contains(attrName))
                 continue;
-            
+
             SingleValueMod mod = singleValueMod(configAttrsToModify, attrName);
             String newValue = null;
             if (mod.setting())
                 newValue = mod.value();
-                        
+
             if (conflict(newDefaults, newValue)) {
-                throw ServiceException.INVALID_REQUEST("port " + newValue + " conflict between " + 
+                throw ServiceException.INVALID_REQUEST("port " + newValue + " conflict between " +
                         attrName + " and " + newDefaults.get(newValue) + " on global config", null);
             } else
                 newDefaults.put(newValue, attrName);
         }
-        
-        /* 
+
+        /*
          * Then, iterate through all servers see if this port change on the Config
-         * entry has impact on a server.  
+         * entry has impact on a server.
          */
         List<Server> servers = Provisioning.getInstance().getAllServers();
         for (Server server : servers) {
             checkServerWithNewDefaults(server, newDefaults, configAttrsToModify);
         }
     }
-    
-    private void checkServerWithNewDefaults(Server server, BiMap<String, String> newDefaults, 
-            Map<String, Object> configAttrsToModify) 
+
+    private void checkServerWithNewDefaults(Server server, BiMap<String, String> newDefaults,
+            Map<String, Object> configAttrsToModify)
     throws ServiceException {
         Map<String, String> ports = new HashMap<String, String>();
-        
+
         for (String attrName : sPortAttrs) {
             String newValue = null;
             String curValue = server.getAttr(attrName, false); // value on the server entry
@@ -185,20 +187,20 @@ public class CheckPortConflict extends AttributeCallback {
                 newValue = newDefaults.inverse().get(attrName);  // will inherit from new default
             else
                 newValue = curValue;
-            
+
             if (conflict(ports, newValue)) {
                 String conflictWith = ports.get(newValue);
                 // throw only when the attr is one of the attrs being modified, otherwise, just let it pass.
                 if (configAttrsToModify.containsKey(attrName) || configAttrsToModify.containsKey(conflictWith))
-                    throw ServiceException.INVALID_REQUEST("port " + newValue + " conflict between " + 
+                    throw ServiceException.INVALID_REQUEST("port " + newValue + " conflict between " +
                         attrName + " and " + ports.get(newValue) + " on server " + server.getName(), null);
             } else
                 ports.put(newValue, attrName);
         }
     }
-        
+
     private boolean conflict(Map<String, String> ports, String port) {
-        
+
         if (StringUtil.isNullOrEmpty(port))
             return false;
         else if (port.equals("0"))
@@ -206,7 +208,7 @@ public class CheckPortConflict extends AttributeCallback {
         else
             return ports.containsKey(port);
     }
-        
+
     @Override
     public void postModify(CallbackContext context, String attrName, Entry entry) {
     }
