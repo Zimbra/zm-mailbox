0f87a366a5414ef90d324db5e6a53a6f8120abf1
Unknown Perforce User <pburgu>
2012-06-14 12:32:53 -0500

    bug:75338 Make number of soap sessions configurable through LC key "zimbra_session_limit_soap". The default value is 5.
    http://bugzilla.zimbra.com/show_bug.cgi?id=75338
    
    Copied from Perforce
     Change: 377970

--- src/java/com/zimbra/common/localconfig/LC.java
+++ src/java/com/zimbra/common/localconfig/LC.java
@@ -284,6 +284,9 @@ public final class LC {
     public static final KnownKey zimbra_session_limit_sync = KnownKey.newKey(5);
 
     @Supported
+    public static final KnownKey zimbra_session_limit_soap = KnownKey.newKey(5);
+
+    @Supported
     public static final KnownKey zimbra_session_timeout_soap = KnownKey.newKey(600);
 
     @Supported
