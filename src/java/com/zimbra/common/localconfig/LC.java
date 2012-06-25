7287cd5ec969fe4166bf91e2ea5df40d631068c1
Unknown Perforce User <pburgu>
2012-06-25 16:49:21 -0500

    bug:75152 Currently, the server performs LDAP based gal sync when the client specifies idOnly=false or when client does not specify this attribute. Changing this behavior may cause problems for old clients. Introduce new LC "syncgal_non_idonly_use_ldap" with default value set to "true". When this LC is set to false, perform the sync based on galsync account when configured. (i.e> Use galsync account even when client specifies idOnly=false.) We did not make the default value for "syncgal_non_idonly_use_ldap" to "false", becuase there could be some old clients (old ZD, ZCO, ZCB, etc), which may not understand the "id" format. (DN for traditional GAL sync vs itemId for account based)
    https://reviewboard.eng.vmware.com/r/349711/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=75152
    
    Copied from Perforce
     Change: 380260

--- src/java/com/zimbra/common/localconfig/LC.java
+++ src/java/com/zimbra/common/localconfig/LC.java
@@ -711,6 +711,8 @@ public final class LC {
     public static final KnownKey calendar_cache_range_months = KnownKey.newKey(3);
     public static final KnownKey calendar_cache_max_stale_items = KnownKey.newKey(10);
     public static final KnownKey calendar_exchange_form_auth_url = KnownKey.newKey("/exchweb/bin/auth/owaauth.dll");
+    
+    public static final KnownKey syncgal_non_idonly_use_ldap = KnownKey.newKey(true);
 
     public static final KnownKey spnego_java_options =  KnownKey.newKey(
             "-Djava.security.krb5.conf=${mailboxd_directory}/etc/krb5.ini " +
