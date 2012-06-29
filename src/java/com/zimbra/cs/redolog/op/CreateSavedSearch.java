9cdf977c844e8e583e29fb1ddf4160f2c8ee51de
Unknown Perforce User <jflanigan>
2012-06-29 16:15:45 -0500

    (split)bug: 75748
    Set types/sort correctly instead of using query for all three values.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=75748
    
    Copied from Perforce
     Change: 381356

--- src/java/com/zimbra/cs/redolog/op/CreateSavedSearch.java
+++ src/java/com/zimbra/cs/redolog/op/CreateSavedSearch.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2004, 2005, 2006, 2007, 2009, 2010 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -51,8 +51,8 @@ public class CreateSavedSearch extends RedoableOp {
         mSearchId = UNKNOWN_ID;
         mName = name != null ? name : "";
         mQuery = query != null ? query : "";
-        mTypes = types != null ? query : "";
-        mSort = sort != null ? query : "";
+        mTypes = types != null ? types : "";
+        mSort = sort != null ? sort : "";
         mFolderId = folderId;
         mFlags = flags;
         mColor = color.getValue();
