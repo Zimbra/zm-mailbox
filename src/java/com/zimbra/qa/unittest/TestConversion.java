1cae0bd21aa0dee8487a12347ce92e98f32a322f
Unknown Perforce User <jflanigan>
2012-07-31 15:56:24 -0500

    (split)bug: 76313
    Make search more explicit to avoid duplicate results from other messages which may exist in mailbox.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=76313
    
    Copied from Perforce
     Change: 389681

--- src/java/com/zimbra/qa/unittest/TestConversion.java
+++ src/java/com/zimbra/qa/unittest/TestConversion.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -29,12 +29,13 @@ public class TestConversion extends TestCase {
 
     private static final String USER_NAME = "user1";
     private static final String NAME_PREFIX = TestConversion.class.getSimpleName();
-    
+
+    @Override
     public void setUp()
     throws Exception {
         cleanUp();
     }
-    
+
     /**
      * Tests downloading attachments from a TNEF message (bug 44263).
      */
@@ -46,37 +47,38 @@ public class TestConversion extends TestCase {
         String msgContent = new String(ByteUtil.getContent(new File(
             LC.zimbra_home.value() + "/unittest/tnef.msg")));
         TestUtil.addMessageLmtp(new String[] { USER_NAME }, USER_NAME, msgContent);
-        
+
         // Test downloading attachments.
-        ZMessage msg = TestUtil.getMessage(mbox, "in:inbox subject:\"Rich text (TNEF) test\"");
+        ZMessage msg = TestUtil.getMessage(mbox, "in:inbox subject:\"" + NAME_PREFIX + " Rich text (TNEF) test\"");
         byte[] data = TestUtil.getContent(mbox, msg.getId(), "upload.gif");
         assertEquals(73, data.length);
         data = TestUtil.getContent(mbox, msg.getId(), "upload2.gif");
         assertEquals(851, data.length);
-        
+
         ZMimePart part = TestUtil.getPart(msg, "upload.gif");
         checkPartSize(73, part.getSize());
         part = TestUtil.getPart(msg, "upload2.gif");
         checkPartSize(851, part.getSize());
     }
-    
+
     /**
      * The part size is calculated from the base64 content, so it may be off by a few bytes.
      */
     private void checkPartSize(long expected, long actual) {
         assertTrue("expected " + expected + " +/- 4 bytes, got " + actual, Math.abs(expected - actual) <= 4);
     }
-    
+
+    @Override
     public void tearDown()
     throws Exception {
         cleanUp();
     }
-    
+
     private void cleanUp()
     throws Exception {
         TestUtil.deleteTestData(USER_NAME, NAME_PREFIX);
     }
-    
+
     public static void main(String[] args)
     throws Exception {
         TestUtil.cliSetup();
