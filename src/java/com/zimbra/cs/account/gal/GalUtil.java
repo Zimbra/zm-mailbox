ad2e182f5db70de68c878e806c59682c545ac52e
Unknown Perforce User <jflanigan>
2012-08-03 15:11:54 -0500

    (split)bug: 76660
           Don't pass null key into filterFactory.encodeValue(); just add null  directly to vars map. The null is handled correctly in LdapUtil.expandStr()
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=76660
    
    Copied from Perforce
     Change: 390548

--- src/java/com/zimbra/cs/account/gal/GalUtil.java
+++ src/java/com/zimbra/cs/account/gal/GalUtil.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2008, 2009, 2010 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -22,16 +22,16 @@ import com.zimbra.cs.ldap.LdapUtil;
 import com.zimbra.cs.ldap.ZLdapFilterFactory;
 
 public class GalUtil {
-    
-    public static String expandFilter(String tokenize, String filterTemplate, String key, String token) 
+
+    public static String expandFilter(String tokenize, String filterTemplate, String key, String token)
     throws ServiceException {
         return expandFilter(tokenize, filterTemplate, key, token, null);
     }
-    
-    public static String expandFilter(String tokenize, String filterTemplate, String key, String token, String extraQuery) 
+
+    public static String expandFilter(String tokenize, String filterTemplate, String key, String token, String extraQuery)
     throws ServiceException {
         String query;
-        
+
         if (key != null) {
             while (key.startsWith("*")) {
                 key = key.substring(1);
@@ -45,19 +45,19 @@ public class GalUtil {
         if (query.indexOf("**") > 0) {
         	query = query.replaceAll("\\*\\*", "*");
         }
-        
+
         if (token != null && token.length() > 0) {
         	String arg = LdapUtil.escapeSearchFilterArg(token);
         	query = "(&(|(modifyTimeStamp>="+arg+")(createTimeStamp>="+arg+"))"+query+")";
         }
-        
+
         if (extraQuery != null) {
             query = "(&" + query + extraQuery + ")";
         }
-        
+
         return query;
     }
-    
+
     public static String tokenizeKey(GalParams galParams, GalOp galOp) {
     	if (galParams == null)
     		return null;
@@ -65,24 +65,24 @@ public class GalUtil {
             return galParams.tokenizeAutoCompleteKey();
         else if (galOp == GalOp.search)
             return galParams.tokenizeSearchKey();
-        
+
         return null;
     }
-    
+
     private static String expandKey(String tokenize, String filterTemplate, String key) throws ServiceException {
 
         if (!filterTemplate.startsWith("(")) {
             if (filterTemplate.endsWith(")"))
                 throw ServiceException.INVALID_REQUEST("Unbalanced parenthesis in filter:" + filterTemplate, null);
-            
+
             filterTemplate = "(" + filterTemplate + ")";
         }
-        
+
         String query = null;
         Map<String, String> vars = new HashMap<String, String>();
-        
+
         ZLdapFilterFactory filterFactory = ZLdapFilterFactory.getInstance();
-        
+
         if (tokenize != null) {
             String tokens[] = key.split("\\s+");
             if (tokens.length > 1) {
@@ -94,7 +94,7 @@ public class GalUtil {
                 } else {
                     throw ServiceException.FAILURE("invalid attribute value for tokenize key: " + tokenize, null);
                 }
-                
+
                 for (String t : tokens) {
                     vars.clear();
                     vars.put("s", filterFactory.encodeValue(t));
@@ -104,13 +104,13 @@ public class GalUtil {
                 query = q;
             }
         }
-        
+
         if (query == null) {
-            vars.put("s", filterFactory.encodeValue(key));
+            vars.put("s", key == null ? null : filterFactory.encodeValue(key));
             query = LdapUtil.expandStr(filterTemplate, vars);
         }
-        
+
         return query;
     }
-   
+
 }
