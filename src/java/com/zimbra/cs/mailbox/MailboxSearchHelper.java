14374c7f84fcc0fccaa45ff8192c6efebdfdf81c
Unknown Perforce User <jflanigan>
2012-09-14 18:33:46 -0500

    (split)bug: 77478
    try/finally around begin/end transaction
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=77478
    
    Copied from Perforce
     Change: 400729

--- src/java/com/zimbra/cs/mailbox/MailboxSearchHelper.java
+++ src/java/com/zimbra/cs/mailbox/MailboxSearchHelper.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011 VMware, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -27,25 +27,28 @@ import com.zimbra.cs.index.SortBy;
  * The related code is significantly refactored for IRONMAIDEN, this is just a stopgap to avoid deadlock (see bug 75182)
  */
 public class MailboxSearchHelper {
-    
+
     /**
      * Executes a DB search in a mailbox transaction; maintaining correct order of lock -> connect
      */
     public static void fetch(final List<SearchResult> results, final Mailbox mbox,
         final SortBy sort, final int offset, final int size, final IConstraints constraints, final SearchResult.ExtraData extra, final boolean inDumpster) throws ServiceException {
-        
+
         synchronized (mbox) {
-            mbox.beginTransaction("searchHelper", null);
-            DbSearch.search(results, mbox.getOperationConnection(), constraints, mbox, sort,
-                offset, size, extra, inDumpster);
-            // convert to MailItem before leaving this transaction
-            // otherwise you can poison MailItem cache with stale data
-            if (extra == SearchResult.ExtraData.MAIL_ITEM) {
-                for (SearchResult result : results) {
-                    result.extraData = mbox.toItem((MailItem.UnderlyingData) result.extraData);
+            try {
+                mbox.beginTransaction("searchHelper", null);
+                DbSearch.search(results, mbox.getOperationConnection(), constraints, mbox, sort,
+                    offset, size, extra, inDumpster);
+                // convert to MailItem before leaving this transaction
+                // otherwise you can poison MailItem cache with stale data
+                if (extra == SearchResult.ExtraData.MAIL_ITEM) {
+                    for (SearchResult result : results) {
+                        result.extraData = mbox.toItem((MailItem.UnderlyingData) result.extraData);
+                    }
                 }
+            } finally {
+                mbox.endTransaction(true);
             }
-            mbox.endTransaction(true);
         }
     }
 }
