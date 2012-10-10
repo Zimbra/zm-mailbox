151249f311d101aa7b4628abf8f14bcbbe84dda9
Unknown Perforce User <pburgu>
2012-10-10 15:52:16 -0500

    bug:77597 Add the upgrade script to create volume_blobs table and metadata column in volume table. Add UNIQUE constraint to volume_table. Up the DB version to 92. Remove the entries from volume_blobs when mailbox gets deleted. Populate the volume_blobs entries when mailbox gets moved using zmmboxmove. Add the "reset" option to zmdedupe command. Running "reset" will wipe all the entries from volume_blobs for the given volume and resets the metadata in volume table.
    https://reviewboard.eng.vmware.com/r/388895/
    
    https://reviewboard.eng.vmware.com/r/388895/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=77597
    
    Copied from Perforce
     Change: 407297

--- src/java/com/zimbra/common/util/CliUtil.java
+++ src/java/com/zimbra/common/util/CliUtil.java
@@ -15,8 +15,11 @@
 
 package com.zimbra.common.util;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 import jline.ConsoleReader;
 import jline.ConsoleReaderInputStream;
@@ -113,4 +116,22 @@ public class CliUtil {
         }
         ConsoleReaderInputStream.setIn(reader);
     }
+    
+    public static boolean confirm(String msg) {
+        System.out.print(msg + " [Y]es, [N]o: ");
+        BufferedReader in;
+        try {
+            in = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
+            String line = StringUtil.readLine(in);
+            if ("y".equalsIgnoreCase(line) || "yes".equalsIgnoreCase(line)) {
+                return true;
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
 }
