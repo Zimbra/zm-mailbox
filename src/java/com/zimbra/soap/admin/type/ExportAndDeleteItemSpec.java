e9e87edd6bdaaf072feda63956f28c1cba8ab8f4
Unknown Perforce User <gelliot>
2012-12-13 03:57:36 -0600

    bug: 74471
    ZimbraSoap updates.
    Also converted ExportAndDeleteItems handler and BlobConsistencyUtil to use JAXB instead of Element.  This should ensure that the definition and reality stay in step for that API.
    https://reviewboard.eng.vmware.com/r/410345/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=74471
    
    Copied from Perforce
     Change: 427506

---
+++ src/java/com/zimbra/soap/admin/type/ExportAndDeleteItemSpec.java
@@ -0,0 +1,71 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2012 Zimbra, Inc.
+ *
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.soap.admin.type;
+
+import com.google.common.base.Objects;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+
+import com.zimbra.common.soap.AdminConstants;
+
+@XmlAccessorType(XmlAccessType.NONE)
+public class ExportAndDeleteItemSpec {
+
+    /**
+     * @zm-api-field-tag id
+     * @zm-api-field-description ID
+     */
+    @XmlAttribute(name=AdminConstants.A_ID /* id */, required=true)
+    private final int id;
+
+    /**
+     * @zm-api-field-tag version
+     * @zm-api-field-description Version
+     */
+    @XmlAttribute(name=AdminConstants.A_VERSION_INFO_VERSION /* version */, required=true)
+    private final int version;
+
+    /**
+     * no-argument constructor wanted by JAXB
+     */
+    private ExportAndDeleteItemSpec() {
+        this(-1, -1);
+    }
+
+    private ExportAndDeleteItemSpec(int id, int version) {
+        this.id = id;
+        this.version = version;
+    }
+
+    public static ExportAndDeleteItemSpec createForIdAndVersion(int id, int version) {
+        return new ExportAndDeleteItemSpec(id, version);
+    }
+
+    public int getId() { return id; }
+    public int getVersion() { return version; }
+
+    public Objects.ToStringHelper addToStringInfo(Objects.ToStringHelper helper) {
+        return helper
+            .add("id", id)
+            .add("version", version);
+    }
+
+    @Override
+    public String toString() {
+        return addToStringInfo(Objects.toStringHelper(this)).toString();
+    }
+}
