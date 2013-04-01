108ab7716ecf06142967197736d2e2484c858204
Unknown Perforce User <jflanigan>
2013-04-01 18:56:38 -0500

    (split)bug: 81417
    Create filter to avoid Jetty ETag implementation in IncludableGzipFilter and explicitly set the value from our mod_content. We now set both X-Zimbra-ETag and ETag. In a future release we may want to have Jetty handle ETag and keep X-Zimbra-ETag for our mod_content.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=81417
    
    Copied from Perforce
     Change: 459145

--- src/java/com/zimbra/cs/dav/service/method/Get.java
+++ src/java/com/zimbra/cs/dav/service/method/Get.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2012 VMware, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -28,44 +28,52 @@ import com.zimbra.cs.dav.DavException;
 import com.zimbra.cs.dav.DavProtocol;
 import com.zimbra.cs.dav.resource.DavResource;
 import com.zimbra.cs.dav.service.DavMethod;
+import com.zimbra.cs.servlet.ETagHeaderFilter;
 
 public class Get extends DavMethod {
-	public static final String GET  = "GET";
-	public String getName() {
-		return GET;
-	}
-	protected boolean returnContent() {
-		return true;
-	}
-	public void handle(DavContext ctxt) throws DavException, IOException, ServiceException {
-		DavResource resource = ctxt.getRequestedResource();
-		HttpServletResponse resp = ctxt.getResponse();
-		String contentType = resource.getContentType(ctxt);
+    public static final String GET = "GET";
+
+    @Override
+    public String getName() {
+        return GET;
+    }
+
+    protected boolean returnContent() {
+        return true;
+    }
+
+    @Override
+    public void handle(DavContext ctxt) throws DavException, IOException, ServiceException {
+        DavResource resource = ctxt.getRequestedResource();
+        HttpServletResponse resp = ctxt.getResponse();
+        String contentType = resource.getContentType(ctxt);
         if (contentType != null) {
             ContentType ct = new ContentType(contentType);
             if (ct.getParameter(MimeConstants.P_CHARSET) == null)
                 ct.setParameter(MimeConstants.P_CHARSET, MimeConstants.P_CHARSET_UTF8);
             resp.setContentType(ct.toString());
         }
-		if (resource.hasEtag())
-			ctxt.getResponse().setHeader(DavProtocol.HEADER_ETAG, resource.getEtag());
-		
-		// in some cases getContentLength() returns an estimate, and the exact 
-		// content length is not known until DavResource.getContent() is called.
-		// the estimate is good enough for PROPFIND, but not when returning 
-		// the contents.  just leave off setting content length explicitly, 
-		// and have the servlet deal with it by doing chunking or 
-		// setting content-length header on its own.
-		
-		//resp.setContentLength(resource.getContentLength());
-		if (!returnContent() || !resource.hasContent(ctxt))
-			return;
-		resp.setHeader("Content-Disposition", "attachment");
-		if (ZimbraLog.dav.isDebugEnabled()) {
-			ZimbraLog.dav.debug("GET "+ctxt.getUri());
-			if (contentType != null && contentType.startsWith("text"))
-				ZimbraLog.dav.debug(new String(ByteUtil.getContent(resource.getContent(ctxt), 0), "UTF-8"));
-		}
-		ByteUtil.copy(resource.getContent(ctxt), true, ctxt.getResponse().getOutputStream(), false);
-	}
+        if (resource.hasEtag()) {
+            ctxt.getResponse().setHeader(DavProtocol.HEADER_ETAG, resource.getEtag());
+            ctxt.getResponse().setHeader(ETagHeaderFilter.ZIMBRA_ETAG_HEADER, resource.getEtag());
+        }
+
+        // in some cases getContentLength() returns an estimate, and the exact
+        // content length is not known until DavResource.getContent() is called.
+        // the estimate is good enough for PROPFIND, but not when returning
+        // the contents. just leave off setting content length explicitly,
+        // and have the servlet deal with it by doing chunking or
+        // setting content-length header on its own.
+
+        // resp.setContentLength(resource.getContentLength());
+        if (!returnContent() || !resource.hasContent(ctxt))
+            return;
+        resp.setHeader("Content-Disposition", "attachment");
+        if (ZimbraLog.dav.isDebugEnabled()) {
+            ZimbraLog.dav.debug("GET " + ctxt.getUri());
+            if (contentType != null && contentType.startsWith("text"))
+                ZimbraLog.dav.debug(new String(ByteUtil.getContent(resource.getContent(ctxt), 0), "UTF-8"));
+        }
+        ByteUtil.copy(resource.getContent(ctxt), true, ctxt.getResponse().getOutputStream(), false);
+    }
 }
--- src/java/com/zimbra/cs/dav/service/method/Put.java
+++ src/java/com/zimbra/cs/dav/service/method/Put.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2006, 2007, 2008, 2009, 2010 VMware, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -25,26 +25,33 @@ import com.zimbra.cs.dav.resource.Collection;
 import com.zimbra.cs.dav.resource.DavResource;
 import com.zimbra.cs.dav.resource.UrlNamespace;
 import com.zimbra.cs.dav.service.DavMethod;
+import com.zimbra.cs.servlet.ETagHeaderFilter;
 
 public class Put extends DavMethod {
-	public static final String PUT  = "PUT";
-	public String getName() {
-		return PUT;
-	}
-	public void handle(DavContext ctxt) throws DavException, IOException {
-		String user = ctxt.getUser();
-		String name = ctxt.getItem();
-		
-		if (user == null || name == null)
-			throw new DavException("invalid uri", HttpServletResponse.SC_NOT_ACCEPTABLE, null);
-		
-		Collection col = UrlNamespace.getCollectionAtUrl(ctxt, ctxt.getPath());
-		DavResource rs = col.createItem(ctxt, name);
-		if (rs.isNewlyCreated())
-			ctxt.setStatus(HttpServletResponse.SC_CREATED);
-		else
-			ctxt.setStatus(HttpServletResponse.SC_NO_CONTENT);
-		if (rs.hasEtag())
-			ctxt.getResponse().setHeader(DavProtocol.HEADER_ETAG, rs.getEtag());
-	}
+    public static final String PUT = "PUT";
+
+    @Override
+    public String getName() {
+        return PUT;
+    }
+
+    @Override
+    public void handle(DavContext ctxt) throws DavException, IOException {
+        String user = ctxt.getUser();
+        String name = ctxt.getItem();
+
+        if (user == null || name == null)
+            throw new DavException("invalid uri", HttpServletResponse.SC_NOT_ACCEPTABLE, null);
+
+        Collection col = UrlNamespace.getCollectionAtUrl(ctxt, ctxt.getPath());
+        DavResource rs = col.createItem(ctxt, name);
+        if (rs.isNewlyCreated())
+            ctxt.setStatus(HttpServletResponse.SC_CREATED);
+        else
+            ctxt.setStatus(HttpServletResponse.SC_NO_CONTENT);
+        if (rs.hasEtag()) {
+            ctxt.getResponse().setHeader(DavProtocol.HEADER_ETAG, rs.getEtag());
+            ctxt.getResponse().setHeader(ETagHeaderFilter.ZIMBRA_ETAG_HEADER, rs.getEtag());
+        }
+    }
 }
