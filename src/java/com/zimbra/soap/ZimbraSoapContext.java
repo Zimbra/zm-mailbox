597007d1053872ed2385ccb2a2c4906910a94b15
Unknown Perforce User <gelliot>
2013-04-09 11:41:16 -0500

    (split)bug:79512
    Fix NullPointerException thrown if HTTP header AuthToken expires and there is not context object in the SOAP envelope.
    Note that still returning an error - the more appropriate:
          <faultcode>soap:Client</faultcode>
          <faultstring>auth credentials have expired</faultstring>
    An alternative would be to ignore the auth token, but if the request happened not to be an AuthRequest this wouldn't be helpful.
    The original reporter of the problem noticed that their problem could be addressed by disabling HTTP cookie management.  An alternative approach would be to delete the cookie or possibly start a new session which might have the same effect depending on the HTTP library used.
    
    https://reviewboard.eng.vmware.com/r/453684/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=79512
    
    Copied from Perforce
     Change: 461454

--- src/java/com/zimbra/soap/ZimbraSoapContext.java
+++ src/java/com/zimbra/soap/ZimbraSoapContext.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 VMware, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -264,9 +264,11 @@ public final class ZimbraSoapContext {
                 if (mAuthToken.isExpired()) {
                     boolean voidOnExpired = false;
 
-                    Element eAuthTokenControl = ctxt.getOptionalElement(HeaderConstants.E_AUTH_TOKEN_CONTROL);
-                    if (eAuthTokenControl != null) {
-                        voidOnExpired = eAuthTokenControl.getAttributeBool(HeaderConstants.A_VOID_ON_EXPIRED, false);
+                    if (ctxt != null) {
+                        Element eAuthTokenControl = ctxt.getOptionalElement(HeaderConstants.E_AUTH_TOKEN_CONTROL);
+                        if (eAuthTokenControl != null) {
+                            voidOnExpired = eAuthTokenControl.getAttributeBool(HeaderConstants.A_VOID_ON_EXPIRED, false);
+                        }
                     }
 
                     if (voidOnExpired) {
