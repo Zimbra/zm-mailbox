7cb030cd930d4a5be66fed10e9ea0340c0170d07
Unknown Perforce User <pburgu>
2013-05-24 11:51:40 -0500

    bug:79364 Fixing the generated files.
    http://bugzilla.zimbra.com/show_bug.cgi?id=79364
    
    Copied from Perforce
     Change: 472727

--- src/java/com/zimbra/common/account/ZAttrProvisioning.java
+++ src/java/com/zimbra/common/account/ZAttrProvisioning.java
@@ -28,7 +28,7 @@ public class ZAttrProvisioning {
 
     ///// BEGIN-AUTO-GEN-REPLACE
 
-    /* build: 9.0.0_BETA1_1111 pburgu 20130423-1701 */
+    /* build: 9.0.0_BETA1_1111 pburgu 20130524-0946 */
 
     public static enum AccountCalendarUserType {
         RESOURCE("RESOURCE"),
