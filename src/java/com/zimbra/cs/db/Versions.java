c9012a7e7f13864e59d423275a5297542300a6b9
Unknown Perforce User <pburgu>
2013-05-28 12:09:10 -0500

    (split)bug:82423 Up the DB version to 100. Activate the AlwaysOn DB upgrade script.
    https://reviewboard.eng.vmware.com/r/472024/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=82423
    
    Copied from Perforce
     Change: 473571

--- src/java/com/zimbra/cs/db/Versions.java
+++ src/java/com/zimbra/cs/db/Versions.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 VMware, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -39,7 +39,7 @@ public final class Versions {
      *
      * UPDATE THESE TO REQUIRE RESET-WORLD TO BE RUN
      */
-    public static final int DB_VERSION = 93;
+    public static final int DB_VERSION = 100;
 
     /**
      * The INDEX_VERSION is stored into the config table of the DB when the DB is created.
