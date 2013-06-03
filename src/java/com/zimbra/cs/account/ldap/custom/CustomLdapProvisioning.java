ccf86c76db37318917be06f23bb1dcba557b759e
Unknown Perforce User <jflanigan>
2013-06-03 15:31:04 -0500

    (split)bug: 82524
    Add missing constructor to CustomLdapProvisioning.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=82524
    
    Copied from Perforce
     Change: 474500

--- src/java/com/zimbra/cs/account/ldap/custom/CustomLdapProvisioning.java
+++ src/java/com/zimbra/cs/account/ldap/custom/CustomLdapProvisioning.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2007, 2009, 2010, 2011 VMware, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -17,9 +17,17 @@ package com.zimbra.cs.account.ldap.custom;
 import com.zimbra.cs.account.ldap.LdapProvisioning;
 
 public class CustomLdapProvisioning extends LdapProvisioning {
-    
+
+    @Override
     protected void setDIT() {
         mDIT = new CustomLdapDIT(this);
     }
 
+    public CustomLdapProvisioning() {
+        super();
+    }
+
+    public CustomLdapProvisioning(CacheMode cacheMode) {
+        super(cacheMode);
+    }
 }
