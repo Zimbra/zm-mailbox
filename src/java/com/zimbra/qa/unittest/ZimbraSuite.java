869e896b6c6807f671bdafffc02b5ebf354eaadb
Unknown Perforce User <gelliot>
2013-06-17 05:06:42 -0500

    (split)bug:82767
    Tests failing under Hudson - because it relies on config file being present.
    Tests moved to RunUnitTests instead.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=82767
    
    Copied from Perforce
     Change: 475566

--- src/java/com/zimbra/qa/unittest/ZimbraSuite.java
+++ src/java/com/zimbra/qa/unittest/ZimbraSuite.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 VMware, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -68,10 +68,6 @@ public class ZimbraSuite extends TestSuite
         sClasses.add(TestPop3ImapAuth.class);
         sClasses.add(TestContacts.class);
         sClasses.add(TestTaskScheduler.class);
-
-        // XXX bburtin: commenting out TestSearch until bug 18802 is fixed
-        // sClasses.add(TestSearch.class);
-
         sClasses.add(TestSendAndReceive.class);
         sClasses.add(TestConnectionPool.class);
         sClasses.add(TestLmtp.class);
@@ -94,6 +90,7 @@ public class ZimbraSuite extends TestSuite
         sClasses.add(TestParsedMessage.class);
         sClasses.add(TestUserServlet.class);
         sClasses.add(TestWsdlServlet.class);
+        sClasses.add(TestMimeDetect.class);
         sClasses.add(TestDocument.class);
         sClasses.add(TestDocumentServer.class);
         sClasses.add(TestFileUpload.class);
