228101bee44a0773114c458ec280e5882578775d
Unknown Perforce User <pburgu>
2013-07-10 18:01:25 -0500

    bug:79364 CLI to list all servers registered in ZooKeeper. CLI to unregister a server from ZooKeeper. CLI to register local server with ZooKeeper.
    zmprov getAllActiveServers(gaas) [-v] => Lists all active servers registed in ZooKeeper
    zmprov setServerOffline(sso) {name|id} => Unregisters a server from ZooKeeper.
    zmprov setLocalServerOnline(slso)  => Registers local server with ZooKeeper.
    
    https://reviewboard.eng.vmware.com/r/485751/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=79364
    
    Copied from Perforce
     Change: 477281

--- src/java/com/zimbra/common/soap/AdminConstants.java
+++ src/java/com/zimbra/common/soap/AdminConstants.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012 VMware, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -197,6 +197,12 @@ public final class AdminConstants {
     public static final String E_EXPORT_AND_DELETE_ITEMS_RESPONSE = "ExportAndDeleteItemsResponse";
     public static final String E_DEDUPE_BLOBS_REQUEST = "DedupeBlobsRequest";
     public static final String E_DEDUPE_BLOBS_RESPONSE = "DedupeBlobsResponse";
+    public static final String E_GET_ALL_ACTIVE_SERVERS_REQUEST = "GetAllActiveServersRequest";
+    public static final String E_GET_ALL_ACTIVE_SERVERS_RESPONSE = "GetAllActiveServersResponse";
+    public static final String E_SET_SERVER_OFFLINE_REQUEST = "SetServerOfflineRequest";
+    public static final String E_SET_SERVER_OFFLINE_RESPONSE = "SetServerOfflineResponse";
+    public static final String E_SET_LOCAL_SERVER_ONLINE_REQUEST = "SetLocalServerOnlineRequest";
+    public static final String E_SET_LOCAL_SERVER_ONLINE_RESPONSE = "SetLocalServerOnlineResponse";
 
     public static final String E_CREATE_DISTRIBUTION_LIST_REQUEST = "CreateDistributionListRequest";
     public static final String E_CREATE_DISTRIBUTION_LIST_RESPONSE = "CreateDistributionListResponse";
@@ -680,6 +686,13 @@ public final class AdminConstants {
     public static final QName EXPORT_AND_DELETE_ITEMS_RESPONSE = QName.get(E_EXPORT_AND_DELETE_ITEMS_RESPONSE, NAMESPACE);
     public static final QName DEDUPE_BLOBS_REQUEST = QName.get(E_DEDUPE_BLOBS_REQUEST, NAMESPACE);
     public static final QName DEDUPE_BLOBS_RESPONSE = QName.get(E_DEDUPE_BLOBS_RESPONSE, NAMESPACE);
+    public static final QName GET_ALL_ACTIVE_SERVERS_REQUEST = QName.get(E_GET_ALL_ACTIVE_SERVERS_REQUEST, NAMESPACE);
+    public static final QName GET_ALL_ACTIVE_SERVERS_RESPONSE = QName.get(E_GET_ALL_ACTIVE_SERVERS_RESPONSE, NAMESPACE);
+
+    public static final QName SET_SERVER_OFFLINE_REQUEST = QName.get(E_SET_SERVER_OFFLINE_REQUEST, NAMESPACE);
+    public static final QName SET_SERVER_OFFLINE_RESPONSE = QName.get(E_SET_SERVER_OFFLINE_RESPONSE, NAMESPACE);
+    public static final QName SET_LOCAL_SERVER_ONLINE_REQUEST = QName.get(E_SET_LOCAL_SERVER_ONLINE_REQUEST, NAMESPACE);
+    public static final QName SET_LOCAL_SERVER_ONLINE_RESPONSE = QName.get(E_SET_LOCAL_SERVER_ONLINE_RESPONSE, NAMESPACE);
 
     public static final QName CREATE_DISTRIBUTION_LIST_REQUEST = QName.get(E_CREATE_DISTRIBUTION_LIST_REQUEST, NAMESPACE);
     public static final QName CREATE_DISTRIBUTION_LIST_RESPONSE = QName.get(E_CREATE_DISTRIBUTION_LIST_RESPONSE, NAMESPACE);
