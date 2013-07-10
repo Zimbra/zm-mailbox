c9ea154ebbc55e25d8c023d3401b93d2b26c6066
Unknown Perforce User <dywang>
2013-07-10 17:48:41 -0500

    bug: 77256 Enable Active Sync for Outlook. Add attribute to enable/disable Outlook sync via ActiveSync at account/cos level.
    http://bugzilla.zimbra.com/show_bug.cgi?id=77256
    
    Copied from Perforce
     Change: 477277

--- src/java/com/zimbra/common/account/ZAttrProvisioning.java
+++ src/java/com/zimbra/common/account/ZAttrProvisioning.java
@@ -6917,6 +6917,14 @@ public class ZAttrProvisioning {
     public static final String A_zimbraMobileNotificationEnabled = "zimbraMobileNotificationEnabled";
 
     /**
+     * Whether to permit Outlook to sync via Active Sync
+     *
+     * @since ZCS 8.0.5
+     */
+    @ZAttr(id=1453)
+    public static final String A_zimbraMobileOutlookSyncEnabled = "zimbraMobileOutlookSyncEnabled";
+
+    /**
      * whether the Bluetooth capabilities are allowed on the device. The
      * available options are Disable, HandsfreeOnly, and Allow. 0 - DISABLE 1
      * - HANDSFREE 2 - ALLOW ignored if
