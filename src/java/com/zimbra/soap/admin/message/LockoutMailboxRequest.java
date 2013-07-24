be5346a9b0241d1f73a8134c56bd74e527de18ef
Unknown Perforce User <jflanigan>
2013-07-24 15:56:57 -0500

    bug: 83096
    JAXB classes for LockoutMailbox SOAP API
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=83096
    
    Copied from Perforce
     Change: 478415

---
+++ src/java/com/zimbra/soap/admin/message/LockoutMailboxRequest.java
@@ -0,0 +1,82 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2013 VMware, Inc.
+ *
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.soap.admin.message;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.google.common.base.Objects;
+import com.zimbra.common.soap.AdminConstants;
+import com.zimbra.soap.admin.type.Name;
+
+/**
+ * @zm-api-command-auth-required true
+ * @zm-api-command-admin-auth-required true
+ * @zm-api-command-description Puts the mailbox of the specified account into maintenance lockout or removes it from maintenance lockout
+ */
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name=AdminConstants.E_LOCKOUT_MAILBOX_REQUEST)
+public class LockoutMailboxRequest {
+
+    /**
+     * @zm-api-field-tag account-email-address
+     * @zm-api-field-description Account email address
+     */
+    @XmlElement(name=AdminConstants.E_ACCOUNT /* account */, required=true)
+    private Name account;
+
+    /**
+     * @zm-api-field-tag operation
+     * @zm-api-field-description one of 'start' or 'end'
+     */
+    @XmlAttribute(name=AdminConstants.A_OPERATION)
+    private String operation;
+
+
+    private LockoutMailboxRequest() {
+    }
+
+    private LockoutMailboxRequest(Name account) {
+        setAccount(account);
+    }
+
+    public static LockoutMailboxRequest create(Name account) {
+        return new LockoutMailboxRequest(account);
+    }
+
+    public void setAccount(Name account) { this.account = account; }
+    public Name getAccount() { return account; }
+
+    public void setOperation(String operation) { this.operation = operation; }
+    public String getOperation() { return operation; }
+
+    public Objects.ToStringHelper addToStringInfo(
+                Objects.ToStringHelper helper) {
+        return helper
+            .add("account", account)
+            .add("operation", operation);
+
+    }
+
+    @Override
+    public String toString() {
+        return addToStringInfo(Objects.toStringHelper(this))
+                .toString();
+    }
+}
