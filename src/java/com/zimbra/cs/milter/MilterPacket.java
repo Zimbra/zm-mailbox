c5e609e4b1c69e48e3b6a9f71c79cadf4f30d647
Quanah Gibson-Mount <quanah@zimbra.com>
2013-08-09 18:13:31 -0500

    Integrated the following from //depot/zimbra/HELIX/...: Change 479813 by quanah@qhelix on 2013/08/09 16:12:51
    bug: 82712
    Fix copyright block
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=82712
    
    Copied from Perforce
     Change: 479815

--- src/java/com/zimbra/cs/milter/MilterPacket.java
+++ src/java/com/zimbra/cs/milter/MilterPacket.java
@@ -1,10 +1,10 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2010, 2011, 2013 Zimbra Software, LLC.
+ * Copyright (C) 2010, 2011 VMware, Inc.
  * 
  * The contents of this file are subject to the Zimbra Public License
- * Version 1.4 ("License"); you may not use this file except in
+ * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
  * 
@@ -15,35 +15,42 @@
 package com.zimbra.cs.milter;
 
 class MilterPacket {
-    private int len;
-    private byte cmd;
-    private byte[] data;
-    
+    private final int len;
+    private final byte cmd;
+    private final byte[] data;
+
     MilterPacket(int len, byte cmd, byte[] data) {
         this.len = len;
         this.cmd = cmd;
         this.data = data;
     }
- 
+
+    MilterPacket(byte cmd) {
+        this.len = 1;
+        this.cmd = cmd;
+        this.data = null;
+    }
+
     int getLength() {
         return len;
     }
-    
+
     byte getCommand() {
         return cmd;
     }
-    
+
     byte[] getData() {
         return data;
     }
-    
-    @Override public String toString() {
+
+    @Override
+    public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append(len);
         sb.append(':');
         sb.append((char)cmd);
         sb.append(':');
-        if (data != null) {           
+        if (data != null) {
             for (byte b : data) {
                 if (b > 32 &&  b < 127) {
                     sb.append((char)b);
