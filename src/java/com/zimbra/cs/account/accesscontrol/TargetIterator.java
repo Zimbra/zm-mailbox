e3745a9c9e47db18ec01467152288963bf0bcf13
Unknown Perforce User <gelliot>
2013-08-23 06:07:37 -0500

    (split)bug: 83773 getTargetIeterator needed support for AlwaysOnCluster target.
    http://bugzilla.zimbra.com/show_bug.cgi?id=83773
    
    Copied from Perforce
     Change: 481098

--- src/java/com/zimbra/cs/account/accesscontrol/TargetIterator.java
+++ src/java/com/zimbra/cs/account/accesscontrol/TargetIterator.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2008, 2009, 2010, 2011, 2012, 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -21,6 +21,7 @@ import java.util.List;
 import com.zimbra.common.account.Key;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.AlwaysOnCluster;
 import com.zimbra.cs.account.CalendarResource;
 import com.zimbra.cs.account.Config;
 import com.zimbra.cs.account.Cos;
@@ -112,8 +113,11 @@ public abstract class TargetIterator {
             iter =  new TargetIterator.ZimletTargetIterator(prov, target);
         } else if (target instanceof XMPPComponent) {
             iter =  new TargetIterator.XMPPComponentTargetIterator(prov, target);
+        } else if (target instanceof AlwaysOnCluster) {
+             iter =  new TargetIterator.AlwaysOnClusterTargetIterator(prov, target);
         } else {
-            throw ServiceException.FAILURE("internal error", null);
+            throw ServiceException.FAILURE(new StringBuilder(
+                    "internal error - no TargetIterator for ").append(target.getClass().getName()).toString(), null);
         }
 
         /*
