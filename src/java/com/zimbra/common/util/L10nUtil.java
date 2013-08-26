1f23d2ec7e452f26b4253c2f76fbf3a6372fda35
Unknown Perforce User <jflanigan>
2013-08-26 17:17:36 -0500

    bug: 83703
    Add method for unit tests to override L10n message bundle directory
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=83703
    
    Copied from Perforce
     Change: 481416

--- src/java/com/zimbra/common/util/L10nUtil.java
+++ src/java/com/zimbra/common/util/L10nUtil.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -35,6 +35,7 @@ import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.zimbra.common.localconfig.LC;
 
 public class L10nUtil {
@@ -286,6 +287,11 @@ public class L10nUtil {
         }
 
         return classLoader;
+    }
+
+    @VisibleForTesting
+    public static void setMsgClassLoader(String directory) {
+        sMsgClassLoader = getClassLoader(directory);
     }
 
     public static ClassLoader getMsgClassLoader() {
