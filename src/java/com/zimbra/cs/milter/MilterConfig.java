e73b63f2ea7af730188ca79facb9eaec1c4df6fc
Quanah Gibson-Mount <quanah@zimbra.com>
2013-09-03 14:12:31 -0500

    Integrated the following from //depot/zimbra/main/...: Change 482326 by quanah@qmain on 2013/09/03 12:11:39
    bug: 83712
    Rotate spam training log
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=83712
    
    Copied from Perforce
     Change: 482328

--- src/java/com/zimbra/cs/milter/MilterConfig.java
+++ src/java/com/zimbra/cs/milter/MilterConfig.java
@@ -14,64 +14,74 @@
  */
 package com.zimbra.cs.milter;
 
+import static com.zimbra.cs.account.ZAttrProvisioning.A_zimbraMilterNumThreads;
+import static com.zimbra.cs.account.ZAttrProvisioning.A_zimbraMilterBindAddress;
+import static com.zimbra.cs.account.ZAttrProvisioning.A_zimbraMilterBindPort;
+
 import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.util.Log;
 import com.zimbra.common.util.ZimbraLog;
-import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.server.ServerConfig;
 import com.zimbra.cs.util.Config;
 
 public class MilterConfig extends ServerConfig {
     private static final String PROTOCOL = "MILTER";
-
+    
     public MilterConfig() {
         super(PROTOCOL, false);
     }
-
-    @Override
-    public int getBindPort() {
+    
+    @Override public int getBindPort() {
         int port = LC.milter_bind_port.intValue();
-        return port != 0 ? port : getIntAttr(Provisioning.A_zimbraMilterBindPort, Config.D_MILTER_BIND_PORT);
+        return port != 0 ? port : getIntAttr(A_zimbraMilterBindPort, Config.D_MILTER_BIND_PORT);
     }
 
-    @Override
-    public String getBindAddress() {
+    @Override public String getBindAddress() {
         String addr = LC.milter_bind_address.value();
-        return addr != null ? addr : getAttr(Provisioning.A_zimbraMilterBindAddress, "127.0.0.1");
+        return addr != null ? addr : getAttr(A_zimbraMilterBindAddress, "127.0.0.1");
     }
-
+    
     @Override
-    public int getMaxThreads() {
-        return getIntAttr(Provisioning.A_zimbraMilterNumThreads, super.getMaxThreads());
+    public int getNumThreads() {
+        return getIntAttr(A_zimbraMilterNumThreads, super.getNumThreads());
     }
-
-    @Override
-    public Log getLog() {
+    
+    @Override public Log getLog() {
         return ZimbraLog.milter;
     }
 
+    // for now nio tweaks are in LC
+    
+    @Override public int getNioMaxSessions() {
+        return LC.milter_max_sessions.intValue();
+    }
+    
+    @Override public int getMaxIdleSeconds() {
+        return LC.milter_max_idle_seconds.intValue();
+    }
+    
     @Override
-    public int getMaxConnections() {
-        return getIntAttr(Provisioning.A_zimbraMilterMaxConnections, super.getMaxConnections());
+    public int getNioMaxScheduledWriteBytes() {
+        return LC.milter_max_scheduled_write_bytes.intValue();
     }
 
     @Override
-    public int getMaxIdleTime() {
-        return LC.milter_max_idle_time.intValue();
-    }
-
-    @Override
-    public int getWriteTimeout() {
+    public int getNioWriteTimeout() {
         return LC.milter_write_timeout.intValue();
     }
 
     @Override
-    public int getWriteChunkSize() {
+    public int getNioWriteChunkSize() {
         return LC.milter_write_chunk_size.intValue();
     }
 
     @Override
-    public int getThreadKeepAliveTime() {
+    public int getNioMinThreads() {
+        return LC.milter_min_threads.intValue();
+    }
+
+    @Override
+    public int getNioThreadKeepAliveTime() {
         return LC.milter_thread_keep_alive_time.intValue();
     }
 }
--- src/java/com/zimbra/cs/milter/MinaMilterEncoder.java
+++ src/java/com/zimbra/cs/milter/MinaMilterEncoder.java
@@ -14,26 +14,35 @@
  */
 package com.zimbra.cs.milter;
 
-import org.apache.mina.core.buffer.IoBuffer;
-import org.apache.mina.core.session.IoSession;
+import org.apache.mina.common.IoSession;
 import org.apache.mina.filter.codec.ProtocolEncoderAdapter;
 import org.apache.mina.filter.codec.ProtocolEncoderOutput;
+import org.apache.mina.common.ByteBuffer;
 
-final class NioMilterEncoder extends ProtocolEncoderAdapter {
+import com.zimbra.cs.mina.MinaStats;
 
-    @Override
-    public void encode(IoSession session, Object msg, ProtocolEncoderOutput out) {
+public class MinaMilterEncoder extends ProtocolEncoderAdapter {
+    private final MinaStats stats;
+    
+    MinaMilterEncoder(MinaStats stats) {
+        this.stats = stats;
+    }
+    
+    @Override public void encode(IoSession session, Object msg, ProtocolEncoderOutput out) {
         MilterPacket packet = (MilterPacket) msg;
-
-        IoBuffer buffer = IoBuffer.allocate(4 + packet.getLength(), false);
+        
+        ByteBuffer buffer = ByteBuffer.allocate(4 + packet.getLength(), false);
         buffer.setAutoExpand(true);
         buffer.putInt(packet.getLength());
-        buffer.put(packet.getCommand());
+        buffer.put(packet.getCommand());       
         byte[] data = packet.getData();
-        if (data != null && data.length > 0) {
+        if (data != null && data.length > 0)
             buffer.put(data);
-        }
         buffer.flip();
         out.write(buffer);
+        
+        if (stats != null) {
+            stats.sentBytes.addAndGet(buffer.capacity());
+        }
     }
 }
