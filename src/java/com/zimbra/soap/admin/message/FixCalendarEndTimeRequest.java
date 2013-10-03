3710e61b1d020058fe565f8f7d145572adcc4860
Unknown Perforce User <gelliot>
2013-10-03 09:26:05 -0500

    bug:84312
    More details in SOAP documentaion for FixCalendarEndTimeRequest
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=84312
    
    Copied from Perforce
     Change: 484654

--- src/java/com/zimbra/soap/admin/message/FixCalendarEndTimeRequest.java
+++ src/java/com/zimbra/soap/admin/message/FixCalendarEndTimeRequest.java
@@ -2,21 +2,18 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2011, 2012, 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
  */
 
 package com.zimbra.soap.admin.message;
-
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
 
 import java.util.Collections;
 import java.util.List;
@@ -27,6 +24,8 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.zimbra.common.soap.AdminConstants;
 import com.zimbra.soap.type.NamedElement;
 import com.zimbra.soap.type.ZmBoolean;
@@ -35,6 +34,20 @@ import com.zimbra.soap.type.ZmBoolean;
  * @zm-api-command-auth-required true
  * @zm-api-command-admin-auth-required true
  * @zm-api-command-description Fix Calendar End Times
+ * <p>
+ * Re-calculate the end times used for calendar items after changes to the recurrence expansion configuration.
+ * The current configured values can be determined from:<br />
+ * <pre>zmprov getAllConfig | grep zimbraCalendarRecurrence</pre>
+ * </p>
+ * The default values are:<br />
+ * <pre>
+ * zimbraCalendarRecurrenceDailyMaxDays: 730
+ * zimbraCalendarRecurrenceMaxInstances: 0
+ * zimbraCalendarRecurrenceMonthlyMaxMonths: 360
+ * zimbraCalendarRecurrenceOtherFrequencyMaxYears: 1
+ * zimbraCalendarRecurrenceWeeklyMaxWeeks: 520
+ * zimbraCalendarRecurrenceYearlyMaxYears: 100
+ * </pre>
  */
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlRootElement(name=AdminConstants.E_FIX_CALENDAR_END_TIME_REQUEST)
@@ -55,7 +68,7 @@ public class FixCalendarEndTimeRequest {
      * @zm-api-field-description Accounts
      */
     @XmlElement(name=AdminConstants.E_ACCOUNT, required=false)
-    private List<NamedElement> accounts = Lists.newArrayList();
+    private final List<NamedElement> accounts = Lists.newArrayList();
 
     /**
      * no-argument constructor wanted by JAXB
