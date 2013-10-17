b67e2186bf936e509e8eee60f25a1589569d53dd
Rupali Desai <rdesai@zimbra.com>
2013-10-17 05:39:07 -0500

    (split)Bug: 84099 MBS returns 451 to MTA zimbraPrefDedupeMessagesSentToSelf  to dedupeAll and a filter
    - Check if 'ignore self messages' is set to 'dedupeAll', not to call addMessageInternal
    - Tested by setting 'zimbraPrefDedupeMessagesSentToSelf' to 'dedupeAll' and creating a filter to file messages with subject 'test' to 'Junk' folder
        -- Messages are not filed to Junk folder and MBS does not return 451
    - Tested by setting 'zimbraPrefDedupeMessagesSentToSelf' to 'secondCopyifOnToOrCC' and creating a filter to file messages with subject 'test' to 'Junk' folder
        -- Messages are filed to Junk folder.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=84099
    
    Copied from Perforce
     Change: 485769

--- src/java/com/zimbra/cs/lmtpserver/ZimbraLmtpBackend.java
+++ src/java/com/zimbra/cs/lmtpserver/ZimbraLmtpBackend.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -576,6 +576,10 @@ public class ZimbraLmtpBackend implements LmtpBackend {
                                 if (dedupe(pm, mbox)) {
                                     // message was already delivered to this mailbox
                                     ZimbraLog.lmtp.info("Not delivering message with duplicate Message-ID %s", pm.getMessageID());
+                                } else if (mbox.dedupeForSelfMsg(pm)) {
+                                    ZimbraLog.mailbox.info("not delivering message, because it is a duplicate of sent message %s",
+                                        pm.getMessageID());
+
                                 } else if (recipient.getSkipFilters()) {
                                     msgId = pm.getMessageID();
                                     int folderId = Mailbox.ID_FOLDER_INBOX;
