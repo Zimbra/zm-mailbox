aaec667ae49a330d78e04a55450860f12771f189
Unknown Perforce User <gelliot>
2013-12-04 09:35:46 -0600

    bug:76968
    Support "fullConversation" attribute for SearchRequest.
    If set to true, then for conversation hits, ALL messages in the conversation are included in the results.  The default is that only messages which match the search criteria are included.
    Note that even junk and trash messages are included.  If a client switches to using "fullConversation" it may need to change handling of such messages - in my experiments with ZWC, when expanding conversations, deleted messages no longer have strike-through.
    
    Also, use JAXB for processing the request elements of Search/SearchConv and SearchMultipleMailboxes
    https://reviewboard.eng.vmware.com/r/548804/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=76968
    
    Copied from Perforce
     Change: 495172

--- src/java/com/zimbra/common/soap/MailConstants.java
+++ src/java/com/zimbra/common/soap/MailConstants.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -723,6 +723,7 @@ public final class MailConstants {
     public static final String A_CAL_EXPAND_INST_START = "calExpandInstStart";
     public static final String A_CAL_EXPAND_INST_END = "calExpandInstEnd";
     public static final String A_RESULT_MODE = "resultMode";
+    public static final String A_FULL_CONVERSATION = "fullConversation";
     public static final String A_PREFETCH = "prefetch";
     public static final String A_VERBOSE = "verbose";
     public static final String A_CSVFORMAT = "csvfmt";
