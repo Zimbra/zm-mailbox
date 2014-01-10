49b574477d1c16facbe546f82063d692b98aec65
Quanah Gibson-Mount <quanah@zimbra.com>
2014-01-09 18:44:54 -0600

    Integrated the following from //depot/zimbra/main/...: Change 496492 by quanah@qmain on 2014/01/09 18:44:21
    bug: 85603
    Fix setTimeout so that it cannot cause an integer overflow
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=85603
    
    Copied from Perforce
     Change: 496494

--- src/java/com/zimbra/cs/milter/MinaMilterDecoder.java
+++ src/java/com/zimbra/cs/milter/MinaMilterDecoder.java
@@ -14,18 +14,24 @@
  */
 package com.zimbra.cs.milter;
 
-import org.apache.mina.core.buffer.IoBuffer;
-import org.apache.mina.core.session.IoSession;
+import org.apache.mina.common.ByteBuffer;
+import org.apache.mina.common.IoSession;
 import org.apache.mina.filter.codec.CumulativeProtocolDecoder;
 import org.apache.mina.filter.codec.ProtocolDecoderOutput;
 
-final class NioMilterDecoder extends CumulativeProtocolDecoder {
+import com.zimbra.cs.mina.MinaStats;
 
-    @Override
-    public boolean doDecode(IoSession session, IoBuffer in, ProtocolDecoderOutput out) {
-        if (!in.prefixedDataAvailable(4)) {
+public class MinaMilterDecoder extends CumulativeProtocolDecoder {
+    private final MinaStats stats;
+    
+    MinaMilterDecoder(MinaStats stats) {
+        this.stats = stats;
+    }
+    
+    @Override public boolean doDecode(IoSession session, ByteBuffer in, ProtocolDecoderOutput out) {
+        if (!in.prefixedDataAvailable(4))
             return false;
-        }
+        
         int len = in.getInt();
         byte cmd = in.get();
         byte[] data = null;
@@ -35,6 +41,10 @@ final class NioMilterDecoder extends CumulativeProtocolDecoder {
         }
         MilterPacket packet = new MilterPacket(len, cmd, data);
         out.write(packet);
+        
+        if (stats != null) {
+            stats.receivedBytes.addAndGet(len + 4);
+        }
         return true;
     }
 }
