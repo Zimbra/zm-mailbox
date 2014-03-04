435cf57d90b17747b68811f4cf84e736f4fd5ff8
Gren Elliot <gren.elliot@zimbra.com>
2014-03-04 08:12:04 -0600

    (split)bug: 87194
    Unable to expand groups which use a custom URL in Admin Console
    https://reviewboard.eng.zimbra.com/r/498/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=87194
    
    Copied from Perforce
     Change: 502605

--- src/java/com/zimbra/cs/service/admin/SearchDirectory.java
+++ src/java/com/zimbra/cs/service/admin/SearchDirectory.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -18,10 +18,18 @@
  */
 package com.zimbra.cs.service.admin;
 
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import com.zimbra.common.account.Key;
 import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.AdminConstants;
+import com.zimbra.common.soap.Element;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.AccountServiceException;
 import com.zimbra.cs.account.Alias;
@@ -35,22 +43,14 @@ import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.SearchDirectoryOptions;
 import com.zimbra.cs.account.SearchDirectoryOptions.MakeObjectOpt;
 import com.zimbra.cs.account.SearchDirectoryOptions.SortOpt;
+import com.zimbra.cs.account.accesscontrol.AdminRight;
 import com.zimbra.cs.account.accesscontrol.HardRules.HardRule;
 import com.zimbra.cs.account.accesscontrol.Rights.Admin;
-import com.zimbra.cs.account.accesscontrol.AdminRight;
 import com.zimbra.cs.account.accesscontrol.TargetType;
 import com.zimbra.cs.ldap.ZLdapFilterFactory.FilterId;
 import com.zimbra.cs.session.AdminSession;
 import com.zimbra.cs.session.Session;
-import com.zimbra.common.soap.Element;
 import com.zimbra.soap.ZimbraSoapContext;
-
-import java.util.Arrays;
-import java.util.EnumSet;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * @author schemers
@@ -59,18 +59,20 @@ public class SearchDirectory extends AdminDocumentHandler {
 
     public static final String BY_NAME = "name";
     public static final String BY_ID = "id";
-    
+
     private static final String SEARCH_DIRECTORY_ACCOUNT_DATA = "SearchDirectoryAccount";
 
     public static final int MAX_SEARCH_RESULTS = LC.zimbra_directory_max_search_result.intValue();
-    
+
     /**
      * must be careful and only allow access to domain if domain admin
      */
+    @Override
     public boolean domainAuthSufficient(Map context) {
         return true;
     }
-    
+
+    @Override
     public Element handle(Element request, Map<String, Object> context) throws ServiceException {
         ZimbraSoapContext zsc = getZimbraSoapContext(context);
         Provisioning prov = Provisioning.getInstance();
@@ -99,7 +101,7 @@ public class SearchDirectory extends AdminDocumentHandler {
             (domain != null)) {
             throw ServiceException.INVALID_REQUEST("cannot specify domain with coses flag", null);
         }
-        
+
         // add zimbraMailTransport if account is requested
         // it is needed for figuring out if the account is an "external"(not yet migrated) account.
         String attrsStr = origAttrsStr;
@@ -107,15 +109,20 @@ public class SearchDirectory extends AdminDocumentHandler {
             attrsStr != null && !attrsStr.contains(Provisioning.A_zimbraMailTransport)) {
             attrsStr = attrsStr + "," + Provisioning.A_zimbraMailTransport;
         }
-        
+        if (    (   (objTypes.contains(SearchDirectoryOptions.ObjectType.distributionlists)) ||
+                    (objTypes.contains(SearchDirectoryOptions.ObjectType.dynamicgroups)) ) &&
+                (attrsStr != null) && !attrsStr.contains(Provisioning.A_memberURL)  ) {
+            attrsStr = attrsStr + "," + Provisioning.A_memberURL;
+        }
+
         String[] attrs = attrsStr == null ? null : attrsStr.split(",");
         Set<String> reqAttrs = attrs == null ? null : new HashSet(Arrays.asList(attrs));
-        
+
         Element response = zsc.createElement(AdminConstants.SEARCH_DIRECTORY_RESPONSE);
-        
+
         // if we are a domain admin only, restrict to domain
         //
-        // Note: isDomainAdminOnly *always* returns false for pure ACL based AccessManager 
+        // Note: isDomainAdminOnly *always* returns false for pure ACL based AccessManager
         if (isDomainAdminOnly(zsc)) {
             if (objTypes.contains(SearchDirectoryOptions.ObjectType.domains)) {
                 if(query != null && query.length()>0) {
@@ -153,12 +160,12 @@ public class SearchDirectory extends AdminDocumentHandler {
         }
 
         AdminAccessControl aac = AdminAccessControl.getAdminAccessControl(zsc);
-        AdminAccessControl.SearchDirectoryRightChecker rightChecker = 
+        AdminAccessControl.SearchDirectoryRightChecker rightChecker =
             new AdminAccessControl.SearchDirectoryRightChecker(aac, prov, reqAttrs);
-        
+
         List<NamedEntry> accounts;
         AdminSession session = (AdminSession) getSession(zsc, Session.Type.ADMIN);
-        
+
         SearchDirectoryOptions options = new SearchDirectoryOptions();
         options.setDomain(d);
         options.setTypes(types);
@@ -168,12 +175,12 @@ public class SearchDirectory extends AdminDocumentHandler {
         options.setSortOpt(sortAscending ? SortOpt.SORT_ASCENDING : SortOpt.SORT_DESCENDING);
         options.setSortAttr(sortBy);
         options.setConvertIDNToAscii(true); // query must be already RFC 2254 escaped
-            
-        // bug 36017. 
+
+        // bug 36017.
         // defaults, if requested, are set when accounts are paged back to the SOAP client.
         // objects returned from searchDirectory are not cached anywhere.
-        options.setMakeObjectOpt(MakeObjectOpt.NO_DEFAULTS);  
-            
+        options.setMakeObjectOpt(MakeObjectOpt.NO_DEFAULTS);
+
         if (session != null) {
             accounts = session.searchDirectory(options, offset, rightChecker);
         } else {
@@ -190,7 +197,7 @@ public class SearchDirectory extends AdminDocumentHandler {
 
             int i, limitMax = offset+limit;
             for (i=offset; i < limitMax && i < accounts.size(); i++) {
-                NamedEntry entry = (NamedEntry) accounts.get(i);
+                NamedEntry entry = accounts.get(i);
 
                 boolean applyDefault = true;
 
@@ -212,61 +219,61 @@ public class SearchDirectory extends AdminDocumentHandler {
     }
 
     private void setAccountDefaults(Account entry) throws ServiceException {
-       
+
         Boolean isDefaultSet = (Boolean)entry.getCachedData(SEARCH_DIRECTORY_ACCOUNT_DATA);
         if (isDefaultSet == null || isDefaultSet == Boolean.FALSE) {
             entry.setAccountDefaults(true);
             entry.setCachedData(SEARCH_DIRECTORY_ACCOUNT_DATA, Boolean.TRUE);
         }
     }
-    
-    static void encodeEntry(Provisioning prov, Element parent, NamedEntry entry, 
-            boolean applyDefault, Set<String> reqAttrs, AdminAccessControl aac) 
+
+    static void encodeEntry(Provisioning prov, Element parent, NamedEntry entry,
+            boolean applyDefault, Set<String> reqAttrs, AdminAccessControl aac)
     throws ServiceException {
         if (entry instanceof CalendarResource) {
-            ToXML.encodeCalendarResource(parent, (CalendarResource)entry, applyDefault, reqAttrs, 
-                    aac.getAttrRightChecker((CalendarResource)entry, 
+            ToXML.encodeCalendarResource(parent, (CalendarResource)entry, applyDefault, reqAttrs,
+                    aac.getAttrRightChecker(entry,
                     EnumSet.of(HardRule.DELEGATED_ADMIN_CANNOT_ACCESS_GLOBAL_ADMIN))); // bug 64357));
         } else if (entry instanceof Account) {
-            ToXML.encodeAccount(parent, (Account)entry, applyDefault, true, reqAttrs, 
-                    aac.getAttrRightChecker((Account)entry, 
+            ToXML.encodeAccount(parent, (Account)entry, applyDefault, true, reqAttrs,
+                    aac.getAttrRightChecker(entry,
                     EnumSet.of(HardRule.DELEGATED_ADMIN_CANNOT_ACCESS_GLOBAL_ADMIN))); // bug 64357));
         } else if (entry instanceof DistributionList) {
-            GetDistributionList.encodeDistributionList(parent, (DistributionList)entry, false, 
-                    false, reqAttrs, aac.getAttrRightChecker((DistributionList)entry));
+            GetDistributionList.encodeDistributionList(parent, (DistributionList)entry, false,
+                    false, reqAttrs, aac.getAttrRightChecker(entry));
         } else if (entry instanceof DynamicGroup) {
             // TODO: can combine DistributionList and DynamicGroup after aac.getAttrRightChecker
             // is fixed/implemented for DynamicGroup
-            GetDistributionList.encodeDistributionList(parent, (DynamicGroup)entry, false, 
+            GetDistributionList.encodeDistributionList(parent, (DynamicGroup)entry, false,
                     false, reqAttrs, null);  // TODO: FIXME (aac.getAttrRightChecker)!!!
         } else if (entry instanceof Alias) {
             encodeAlias(parent, prov, (Alias)entry, reqAttrs);
         } else if (entry instanceof Domain) {
-            GetDomain.encodeDomain(parent, (Domain)entry, applyDefault, reqAttrs, 
-                    aac.getAttrRightChecker((Domain)entry));
+            GetDomain.encodeDomain(parent, (Domain)entry, applyDefault, reqAttrs,
+                    aac.getAttrRightChecker(entry));
         } else if (entry instanceof Cos) {
-            GetCos.encodeCos(parent, (Cos)entry, reqAttrs, aac.getAttrRightChecker((Cos)entry));
+            GetCos.encodeCos(parent, (Cos)entry, reqAttrs, aac.getAttrRightChecker(entry));
         }
     }
 
-    private static void encodeAlias(Element e, Provisioning prov, Alias a, Set<String> reqAttrs) 
+    private static void encodeAlias(Element e, Provisioning prov, Alias a, Set<String> reqAttrs)
     throws ServiceException {
         Element ealias = e.addElement(AdminConstants.E_ALIAS);
         ealias.addAttribute(AdminConstants.A_NAME, a.getUnicodeName());
         ealias.addAttribute(AdminConstants.A_ID, a.getId());
         ealias.addAttribute(AdminConstants.A_TARGETNAME, a.getTargetUnicodeName(prov));
-        
+
         TargetType tt = a.getTargetType(prov);
         if (tt != null) {
             ealias.addAttribute(AdminConstants.A_TYPE, tt.getCode());
         }
-        
+
         Map attrs = a.getUnicodeAttrs();
-        
+
         ToXML.encodeAttrs(ealias, attrs, reqAttrs, null); // don't have/need an AttrRightChecker for alias
     }
-  
-    
+
+
     @Override
     public void docRights(List<AdminRight> relatedRights, List<String> notes) {
         relatedRights.add(Admin.R_getAccount);
@@ -279,7 +286,7 @@ public class SearchDirectory extends AdminDocumentHandler {
         relatedRights.add(Admin.R_listDistributionList);
         relatedRights.add(Admin.R_listDomain);
         relatedRights.add(Admin.R_listCos);
-        
+
         notes.add(AdminRightCheckPoint.Notes.LIST_ENTRY);
     }
 }
