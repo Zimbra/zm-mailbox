c40af9eb308928dd6e4eb6b15f2ed6eb6ab4da34
Gren Elliot <gren.elliot@zimbra.com>
2014-03-18 05:57:15 -0500

    bug:69984
    CalDAV calendar-auto-schedule support (partial)
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=69984
    
    Copied from Perforce
     Change: 504564

--- src/java/com/zimbra/common/localconfig/DebugConfig.java
+++ src/java/com/zimbra/common/localconfig/DebugConfig.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -179,6 +179,11 @@ public final class DebugConfig {
     public static final boolean enableThisAndFuture = value("debug_enable_calendar_thisandfuture", false);
     public static final boolean caldavAllowAttendeeForOrganizer =
             value("debug_caldav_allow_attendee_for_organizer", false);
+
+    /** TODO: Replace with a Config key when caldav-auto-schedule fully working in a way similar to how
+                    Provisioning.A_zimbraCalendarCalDavDisableScheduling is treated */
+    public static final boolean enableExperimentalCaldavAutoSchedule =
+            value("debug_caldav_enable_experimental_caldav_auto_schedule", false);
 
     public static boolean certAuthCaptureClientCertificate =
         value("debug_certauth_capture_client_certificate", false);
