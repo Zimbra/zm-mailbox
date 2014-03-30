06c521a4e077e24f306fec616b17edcdab2750ed
Greg Solovyev <greg@zimbra.com>
2014-03-29 19:47:00 -0500

    bug 88182  - do not send long lists of domains back and forth between UI and server. Instead, have the server load the list of domains that current admin has access to. This improves performance for delegated admins that manage large numbers of domains  - batch up CountObjectRequests and send them asynchronously  - to avoid an extra try/catch - add a boolean "hasRight" method AdminAccessControl
    http://bugzilla.zimbra.com/show_bug.cgi?id=88182
    
    Copied from Perforce
     Change: 506511

--- src/java/com/zimbra/soap/admin/message/CountObjectsResponse.java
+++ src/java/com/zimbra/soap/admin/message/CountObjectsResponse.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2012, 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -19,29 +19,37 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import com.zimbra.common.soap.AdminConstants;
 
-@XmlRootElement(name=AdminConstants.E_COUNT_OBJECTS_RESPONSE)
+@XmlRootElement(name = AdminConstants.E_COUNT_OBJECTS_RESPONSE)
 public class CountObjectsResponse {
 
     /**
      * @zm-api-field-tag num-objects
      * @zm-api-field-description Number of objects of the requested type
      */
-    @XmlAttribute(name=AdminConstants.A_NUM, required=true)
+    @XmlAttribute(name = AdminConstants.A_NUM, required = true)
     private long num;
+
+    @XmlAttribute(name = AdminConstants.A_TYPE, required = true)
+    private String type;
 
     /**
      * no-argument constructor wanted by JAXB
      */
     @SuppressWarnings("unused")
     public CountObjectsResponse() {
-        this(0);
+        this(0,"");
     }
 
-    public CountObjectsResponse(long num) {
+    public CountObjectsResponse(long num, String type) {
         this.num = num;
+        this.type = type;
     }
 
     public long getNum() {
         return num;
+    }
+
+    public String getType() {
+        return type;
     }
 }
