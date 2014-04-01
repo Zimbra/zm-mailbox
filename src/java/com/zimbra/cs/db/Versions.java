b6a70747d8d54fd0fc61400fb8e9ed7a9dc2cfbb
Ilya Raykin <raykini@zimbra.com>
2014-03-31 20:59:25 -0500

    (split)Bug: 85196
    Perl script to iterate over mailboxes and set charset to utf8 in all dumpster tables. Changing db version to 102.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=85196
    
    Copied from Perforce
     Change: 506776

--- src/java/com/zimbra/cs/db/Versions.java
+++ src/java/com/zimbra/cs/db/Versions.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -39,7 +39,7 @@ public final class Versions {
      *
      * UPDATE THESE TO REQUIRE RESET-WORLD TO BE RUN
      */
-    public static final int DB_VERSION = 101;
+    public static final int DB_VERSION = 102;
 
     /**
      * The INDEX_VERSION is stored into the config table of the DB when the DB is created.
