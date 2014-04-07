3f91e93a8a1c6e5628fcd2232002e87cd58b563a
Unknown Perforce User <dywang>
2014-04-07 18:53:25 -0500

    bug: 88084 Make local heap size configurable, no size limit for disk cache.
    http://bugzilla.zimbra.com/show_bug.cgi?id=88084
    
    Copied from Perforce
     Change: 508030

--- src/java/com/zimbra/common/localconfig/LC.java
+++ src/java/com/zimbra/common/localconfig/LC.java
@@ -1062,6 +1062,7 @@ public final class LC {
     public static final KnownKey zimbra_activesync_search_max_results = KnownKey.newKey(500);
     public static final KnownKey zimbra_activesync_general_cache_size = KnownKey.newKey(500); //active device number
     public static final KnownKey zimbra_activesync_parallel_sync_enabled = KnownKey.newKey(false);
+    public static final KnownKey zimbra_activesync_syncstate_item_cache_heap_size = KnownKey.newKey("10M"); //e.g. 10M,10G
 
     public static final KnownKey zimbra_slow_logging_enabled = KnownKey.newKey(false);
     public static final KnownKey zimbra_slow_logging_threshold = KnownKey.newKey(5000);
