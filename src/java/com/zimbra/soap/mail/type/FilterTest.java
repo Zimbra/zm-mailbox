3ceff3c740266eecc0cfb06a9bde34164523e6b3
Ilya Raykin <raykini@zimbra.com>
2014-04-21 15:52:35 -0500

    Bug: 86255
    Added filters for three Community categories: Requests, Content, and Connections.
    
    CommunityAbstractTest.java:
    - abstract class wrapping AbstractTest to be subclassed by Community filters
    - defines notification groups (not sure if it's better to put these in their individual concrete classes, or keep them in one place)
    - implements a protected method checkHeaderValue to check the Community notification type header against some set of IDs
    
    CommunityConnectionsTest.java:
     - implements executeBasic to call checkHeaderValue with connection notification types
    
    CommunityContentTest.java:
     - implements executeBasic to call checkHeaderValue with content notification types
    
    CommunityRequestsTest.java:
     - implements executeBasic to call checkHeaderValue with request notification types
    
    JsieveConfigMapHandler.java:
    - registered the above filters under the names "community_connections", "community_content", and "community_requests"
    
    ZFilterCondition.java:
    - added the three filters as inner subclasses of ZFilterCondition
    
    FilterTest.java:
    - added the three filters as inner subclasses of FilterTest
    
    CommunityTestTest.java
    - test cases for the three new filters. Modeled after test cases for the other filters, including the awkward name.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=86255
    
    Copied from Perforce
     Change: 509310

--- src/java/com/zimbra/soap/mail/type/FilterTest.java
+++ src/java/com/zimbra/soap/mail/type/FilterTest.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2011, 2012, 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -20,9 +20,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-import com.google.common.base.Objects;
-import com.google.common.collect.Lists;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
@@ -31,6 +28,8 @@ import javax.xml.bind.annotation.XmlEnum;
 
 import org.codehaus.jackson.annotate.JsonPropertyOrder;
 
+import com.google.common.base.Objects;
+import com.google.common.collect.Lists;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.MailConstants;
 import com.zimbra.soap.type.ZmBoolean;
@@ -821,4 +820,15 @@ public class FilterTest {
     public static final class TwitterTest extends FilterTest {
     }
 
+    @XmlAccessorType(XmlAccessType.NONE)
+    public static final class CommunityRequestsTest extends FilterTest {
+    }
+
+    @XmlAccessorType(XmlAccessType.NONE)
+    public static final class CommunityContentTest extends FilterTest {
+    }
+
+    @XmlAccessorType(XmlAccessType.NONE)
+    public static final class CommunityConnectionsTest extends FilterTest {
+    }
 }
