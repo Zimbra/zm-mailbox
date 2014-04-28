908054555bf2ba0702abcd20edce083f938ebb8c
Unknown Perforce User <drauschenbach>
2014-04-28 12:44:41 -0500

    Remove auto-generated TODO cruft from code.
    Copied from Perforce
     Change: 509885

--- src/java/com/zimbra/common/mime/shim/JavaMailMimeBodyPart.java
+++ src/java/com/zimbra/common/mime/shim/JavaMailMimeBodyPart.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2010, 2011, 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -291,7 +291,6 @@ public class JavaMailMimeBodyPart extends MimeBodyPart implements JavaMailShim {
     @Override
     public String[] getContentLanguage() throws MessagingException {
         if (ZPARSER) {
-            // TODO Auto-generated method stub
             return super.getContentLanguage();
         } else {
             return super.getContentLanguage();
--- src/java/com/zimbra/common/mime/shim/JavaMailMimeMultipart.java
+++ src/java/com/zimbra/common/mime/shim/JavaMailMimeMultipart.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2010, 2011, 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -204,7 +204,6 @@ public class JavaMailMimeMultipart extends MimeMultipart implements JavaMailShim
     @Override
     public synchronized boolean isComplete() throws MessagingException {
         if (ZPARSER) {
-            // TODO Auto-generated method stub
             return super.isComplete();
         } else {
             return super.isComplete();
