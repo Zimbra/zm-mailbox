ef3df1566b7ee529a8d3ae1472e1145818939696
Greg Solovyev <greg@zimbra.com>
2014-04-29 19:35:39 -0500

    (split)bug 89574 break the tests down in two sets
    http://bugzilla.zimbra.com/show_bug.cgi?id=89574
    
    Copied from Perforce
     Change: 510123

--- src/java/com/zimbra/qa/unittest/TestSearchSortByDate.java
+++ src/java/com/zimbra/qa/unittest/TestSearchSortByDate.java
@@ -11,7 +11,9 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 import com.zimbra.client.ZMailbox;
+import com.zimbra.client.ZMessage;
 import com.zimbra.client.ZSearchParams;
+import com.zimbra.client.ZSearchParams.Cursor;
 import com.zimbra.common.account.Key.AccountBy;
 import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.service.ServiceException;
@@ -21,8 +23,8 @@ import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.cs.mailbox.Message;
 import com.zimbra.soap.type.SearchSortBy;
 
-public class TestLongSearchResultSets extends TestCase {
-    private static final String NAME_PREFIX = TestLongSearchResultSets.class.getSimpleName();
+public class TestSearchSortByDate extends TestCase {
+    private static final String NAME_PREFIX = TestSearchSortByDate.class.getSimpleName();
     private static final String RECIPIENT = "search_test_1";
     private static final String RECIPIENT2 = "search_test_2";
     private static final String SENDER = "user1";
@@ -47,12 +49,14 @@ public class TestLongSearchResultSets extends TestCase {
         attrs.put("displayName", "TestAccount unit test user 2");
         Provisioning.getInstance().createAccount(TestUtil.getAddress(RECIPIENT2), PASSWORD, attrs);
     }
+
     @Test
-    public void testConversations()  {
+    public void testMessagesSortDateAsc()  {
+
         try {
             Mailbox recieverMbox = TestUtil.getMailbox(RECIPIENT);
             long timestamp = System.currentTimeMillis() - 3600*60*1000;
-            int numMessages = 3207;
+            int numMessages = 3309;
             int firstLimit = 100;
             int incLimit = 50;
             int offset = 0;
@@ -61,131 +65,13 @@ public class TestLongSearchResultSets extends TestCase {
                 Message msg = TestUtil.addMessage(recieverMbox, TestUtil.getAddress(RECIPIENT), TestUtil.getAddress(SENDER), NAME_PREFIX + " testing bug " + i, String.format("this message contains a search string %s which we are searching for and a number %d and timestamp %d", SEARCH_STRING, i,timestamp), timestamp);
                 expectedIds.add(Integer.toString(msg.getId()));
                 timestamp +=1000;
-                Thread.sleep(100);
+               // Thread.sleep(100);
             }
-            Collections.reverse(expectedIds);
+           // Collections.reverse(expectedIds);
             Thread.sleep(100);
             ZMailbox zmbx = TestUtil.getZMailbox(RECIPIENT);
             ZSearchParams searchParams = new ZSearchParams("in:inbox " + SEARCH_STRING);
-            searchParams.setSortBy(SearchSortBy.dateDesc);
-            searchParams.setLimit(firstLimit);
-            searchParams.setTypes(ZSearchParams.TYPE_CONVERSATION);
-            List<String> msgIds = TestUtil.searchMessageIds(zmbx, searchParams);
-            assertEquals(firstLimit,msgIds.size());
-            int gotMessages = msgIds.size();
-            List<String> seenIds = new ArrayList<String>();
-            seenIds.addAll(msgIds);
-            int recCount = 1;
-            offset+=firstLimit;
-       //     int numDups = 0;
-            while(gotMessages > 0) {
-                searchParams = new ZSearchParams("in:inbox " + SEARCH_STRING);
-                searchParams.setSortBy(SearchSortBy.dateDesc);
-                searchParams.setLimit(incLimit);
-                searchParams.setTypes(ZSearchParams.TYPE_CONVERSATION);
-                searchParams.setOffset(offset);
-                msgIds = TestUtil.searchMessageIds(zmbx, searchParams);
-                recCount++;
-                gotMessages = msgIds.size();
-                int resCount = 0;
-                for(String szId : msgIds) {
-                    assertFalse(String.format("Request %d, result %d, encountered duplicate ID %s. Previously seen at %d", recCount, resCount, szId, seenIds.indexOf(szId)), seenIds.contains(szId));
-                    seenIds.add(szId);
-                    resCount++;
-                }
-                Thread.sleep(100); //jetty sometimes crashes on Mac when bombarded with request without a timeout
-                offset+=incLimit;
-            }
-            assertEquals("Returned incorrect number of conversations", numMessages, seenIds.size());
-        } catch (ServiceException e) {
-            e.printStackTrace();
-            fail(e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail(e.getMessage());
-        }
-    }
-
-    @Test
-    public void testConversations2()  {
-        try {
-            Mailbox recieverMbox = TestUtil.getMailbox(RECIPIENT2);
-            long timestamp = System.currentTimeMillis() - 3600*60*1000;
-            int numMessages = 3207;
-            int firstLimit = 100;
-            int incLimit = 50;
-            int offset = 0;
-            ArrayList<String> expectedIds = new ArrayList<String>();
-            for(int i=0;i<numMessages;i++) {
-                Message msg = TestUtil.addMessage(recieverMbox, TestUtil.getAddress(RECIPIENT2), TestUtil.getAddress(SENDER), NAME_PREFIX + " testing bug " + i, String.format("this message contains a search string %s which we are searching for and a number %d and timestamp %d", SEARCH_STRING, i,timestamp), timestamp);
-                expectedIds.add(Integer.toString(msg.getId()));
-                timestamp +=1000;
-                Thread.sleep(100);
-            }
-            Collections.reverse(expectedIds);
-            Thread.sleep(100);
-            ZMailbox zmbx = TestUtil.getZMailbox(RECIPIENT2);
-            ZSearchParams searchParams = new ZSearchParams("in:inbox " + SEARCH_STRING);
-            searchParams.setSortBy(SearchSortBy.dateDesc);
-            searchParams.setLimit(firstLimit);
-            searchParams.setTypes(ZSearchParams.TYPE_CONVERSATION);
-            List<String> msgIds = TestUtil.searchMessageIds(zmbx, searchParams);
-            assertEquals(firstLimit,msgIds.size());
-            int gotMessages = msgIds.size();
-            List<String> seenIds = new ArrayList<String>();
-            seenIds.addAll(msgIds);
-            int recCount = 1;
-            offset+=firstLimit;
-            while(gotMessages > 0) {
-                searchParams = new ZSearchParams("in:inbox " + SEARCH_STRING);
-                searchParams.setSortBy(SearchSortBy.dateDesc);
-                searchParams.setLimit(incLimit);
-                searchParams.setTypes(ZSearchParams.TYPE_CONVERSATION);
-                searchParams.setOffset(offset);
-                msgIds = TestUtil.searchMessageIds(zmbx, searchParams);
-                recCount++;
-                gotMessages = msgIds.size();
-                int resCount = 0;
-                for(String szId : msgIds) {
-                    assertFalse(String.format("Request %d, result %d, encountered duplicate ID %s. Previously seen at %d", recCount, resCount, szId, seenIds.indexOf(szId)), seenIds.contains(szId));
-                    seenIds.add(szId);
-                    resCount++;
-                }
-                Thread.sleep(100); //jetty sometimes crashes on Mac when bombarded with request without a timeout
-                offset+=incLimit;
-            }
-            assertEquals("Returned incorrect number of conversations", numMessages, seenIds.size());
-        } catch (ServiceException e) {
-            e.printStackTrace();
-            fail(e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail(e.getMessage());
-        }
-    }
-
-    @Test
-    public void testMessages()  {
-
-        try {
-            Mailbox recieverMbox = TestUtil.getMailbox(RECIPIENT);
-            long timestamp = System.currentTimeMillis() - 3600*60*1000;
-            int numMessages = 2257;
-            int firstLimit = 100;
-            int incLimit = 50;
-            int offset = 0;
-            ArrayList<String> expectedIds = new ArrayList<String>();
-            for(int i=0;i<numMessages;i++) {
-                Message msg = TestUtil.addMessage(recieverMbox, TestUtil.getAddress(RECIPIENT), TestUtil.getAddress(SENDER), NAME_PREFIX + " testing bug " + i, String.format("this message contains a search string %s which we are searching for and a number %d and timestamp %d", SEARCH_STRING, i,timestamp), timestamp);
-                expectedIds.add(Integer.toString(msg.getId()));
-                timestamp +=1000;
-                Thread.sleep(100);
-            }
-            Collections.reverse(expectedIds);
-            Thread.sleep(100);
-            ZMailbox zmbx = TestUtil.getZMailbox(RECIPIENT);
-            ZSearchParams searchParams = new ZSearchParams("in:inbox " + SEARCH_STRING);
-            searchParams.setSortBy(SearchSortBy.dateDesc);
+            searchParams.setSortBy(SearchSortBy.dateAsc);
             searchParams.setLimit(firstLimit);
             searchParams.setTypes(ZSearchParams.TYPE_MESSAGE);
             List<String> msgIds = TestUtil.searchMessageIds(zmbx, searchParams);
@@ -197,7 +83,7 @@ public class TestLongSearchResultSets extends TestCase {
             offset+=firstLimit;
             while(gotMessages > 0) {
                 searchParams = new ZSearchParams("in:inbox " + SEARCH_STRING);
-                searchParams.setSortBy(SearchSortBy.dateDesc);
+                searchParams.setSortBy(SearchSortBy.dateAsc);
                 searchParams.setLimit(incLimit);
                 searchParams.setTypes(ZSearchParams.TYPE_MESSAGE);
                 searchParams.setOffset(offset);
@@ -210,7 +96,7 @@ public class TestLongSearchResultSets extends TestCase {
                     seenIds.add(szId);
                     resCount++;
                 }
-                Thread.sleep(100); //jetty sometimes crashes on Mac when bombarded with request without a timeout
+                //Thread.sleep(100); //jetty sometimes crashes on Mac when bombarded with request without a timeout
                 offset+=incLimit;
             }
             for(int i=0;i<expectedIds.size();i++) {
@@ -227,57 +113,63 @@ public class TestLongSearchResultSets extends TestCase {
     }
 
     @Test
-    public void testMessages2()  {
+    public void testMessagesCloseTimestamps()  {
 
         try {
-            Mailbox recieverMbox = TestUtil.getMailbox(RECIPIENT2);
-            long timestamp = System.currentTimeMillis() - 3600*60*1000;
-            int numMessages = 2257;
+            Mailbox recieverMbox = TestUtil.getMailbox(RECIPIENT);
+            long timestamp = System.currentTimeMillis();// - 3600*60*1000;
+            int numMessages = 2207;
             int firstLimit = 100;
             int incLimit = 50;
             int offset = 0;
             ArrayList<String> expectedIds = new ArrayList<String>();
             for(int i=0;i<numMessages;i++) {
-                Message msg = TestUtil.addMessage(recieverMbox, TestUtil.getAddress(RECIPIENT2), TestUtil.getAddress(SENDER), NAME_PREFIX + " testing bug " + i, String.format("this message contains a search string %s which we are searching for and a number %d and timestamp %d", SEARCH_STRING, i,timestamp), timestamp);
+                Message msg = TestUtil.addMessage(recieverMbox, TestUtil.getAddress(RECIPIENT), TestUtil.getAddress(SENDER), NAME_PREFIX + " testing bug " + i, String.format("this message contains a search string %s which we are searching for and a number %d and timestamp %d", SEARCH_STRING, i,timestamp), timestamp);
                 expectedIds.add(Integer.toString(msg.getId()));
-                timestamp +=1000;
-                Thread.sleep(100);
             }
+
+            for(int i=0;i<numMessages;i++) {
+                Message msg = TestUtil.addMessage(recieverMbox, TestUtil.getAddress(RECIPIENT), TestUtil.getAddress(SENDER), NAME_PREFIX + " testing bug " + i, "this message does not contains a search string which we are searching for", timestamp);
+            }
+
             Collections.reverse(expectedIds);
             Thread.sleep(100);
-            ZMailbox zmbx = TestUtil.getZMailbox(RECIPIENT2);
+            ZMailbox zmbx = TestUtil.getZMailbox(RECIPIENT);
             ZSearchParams searchParams = new ZSearchParams("in:inbox " + SEARCH_STRING);
             searchParams.setSortBy(SearchSortBy.dateDesc);
             searchParams.setLimit(firstLimit);
             searchParams.setTypes(ZSearchParams.TYPE_MESSAGE);
-            List<String> msgIds = TestUtil.searchMessageIds(zmbx, searchParams);
-            assertEquals(firstLimit,msgIds.size());
-            int gotMessages = msgIds.size();
+            List<ZMessage> resultSet = TestUtil.search(zmbx, searchParams);
+            assertEquals(firstLimit,resultSet.size());
+            int gotMessages = resultSet.size();
             List<String> seenIds = new ArrayList<String>();
-            seenIds.addAll(msgIds);
+            ZMessage lastHit = null;
+            for(ZMessage m : resultSet) {
+                seenIds.add(m.getId());
+                lastHit = m;
+            }
             int recCount = 1;
             offset+=firstLimit;
             while(gotMessages > 0) {
+                searchParams.setCursor((new Cursor(lastHit.getId(), Long.toString(lastHit.getReceivedDate())) ));
                 searchParams = new ZSearchParams("in:inbox " + SEARCH_STRING);
                 searchParams.setSortBy(SearchSortBy.dateDesc);
                 searchParams.setLimit(incLimit);
                 searchParams.setTypes(ZSearchParams.TYPE_MESSAGE);
                 searchParams.setOffset(offset);
-                msgIds = TestUtil.searchMessageIds(zmbx, searchParams);
+                resultSet = TestUtil.search(zmbx, searchParams);
                 recCount++;
-                gotMessages = msgIds.size();
+                gotMessages = resultSet.size();
                 int resCount = 0;
-                for(String szId : msgIds) {
-                    assertFalse(String.format("Request %d, result %d, encountered duplicate ID %s. Previously seen at %d", recCount, resCount, szId, seenIds.indexOf(szId)), seenIds.contains(szId));
-                    seenIds.add(szId);
+                for(ZMessage msg : resultSet) {
+                    assertFalse(String.format("Request %d, result %d, encountered duplicate ID %s. Previously seen at %d", recCount, resCount, msg.getId(), seenIds.indexOf(msg.getId())), seenIds.contains(msg.getId()));
+                    seenIds.add(msg.getId());
                     resCount++;
+                    lastHit = msg;
                 }
-                Thread.sleep(100); //jetty sometimes crashes on Mac when bombarded with request without a timeout
+               // Thread.sleep(100); //jetty sometimes crashes on Mac when bombarded with request without a timeout
                 offset+=incLimit;
             }
-            for(int i=0;i<expectedIds.size();i++) {
-                assertEquals("IDs at index " + i + " do not match", expectedIds.get(i),seenIds.get(i) );
-             }
             assertEquals("Returned incorrect number of messages", numMessages, seenIds.size());
         } catch (ServiceException e) {
             e.printStackTrace();
@@ -288,6 +180,73 @@ public class TestLongSearchResultSets extends TestCase {
         }
     }
 
+    @Test
+    public void testMessagesCloseTimestamps2()  {
+
+        try {
+            Mailbox recieverMbox = TestUtil.getMailbox(RECIPIENT2);
+            long timestamp = System.currentTimeMillis();// - 3600*60*1000;
+            int numMessages = 4307;
+            int firstLimit = 100;
+            int incLimit = 50;
+            int offset = 0;
+            ArrayList<String> expectedIds = new ArrayList<String>();
+            for(int i=0;i<numMessages;i++) {
+                Message msg = TestUtil.addMessage(recieverMbox, TestUtil.getAddress(RECIPIENT2), TestUtil.getAddress(SENDER), NAME_PREFIX + " testing bug " + i, String.format("this message contains a search string %s which we are searching for and a number %d and timestamp %d", SEARCH_STRING, i,timestamp), timestamp);
+                expectedIds.add(Integer.toString(msg.getId()));
+                timestamp +=10;
+            }
+
+            for(int i=0;i<numMessages;i++) {
+                Message msg = TestUtil.addMessage(recieverMbox, TestUtil.getAddress(RECIPIENT), TestUtil.getAddress(SENDER), NAME_PREFIX + " testing bug " + i, "this message does not contains a search string which we are searching for", timestamp);
+            }
+            Collections.reverse(expectedIds);
+            Thread.sleep(100);
+            ZMailbox zmbx = TestUtil.getZMailbox(RECIPIENT2);
+            ZSearchParams searchParams = new ZSearchParams("in:inbox " + SEARCH_STRING);
+            searchParams.setSortBy(SearchSortBy.dateDesc);
+            searchParams.setLimit(firstLimit);
+            searchParams.setTypes(ZSearchParams.TYPE_MESSAGE);
+            List<ZMessage> resultSet = TestUtil.search(zmbx, searchParams);
+            assertEquals(firstLimit,resultSet.size());
+            int gotMessages = resultSet.size();
+            List<String> seenIds = new ArrayList<String>();
+            ZMessage lastHit = null;
+            for(ZMessage m : resultSet) {
+                seenIds.add(m.getId());
+                lastHit = m;
+            }
+            int recCount = 1;
+            offset+=firstLimit;
+            while(gotMessages > 0) {
+                searchParams.setCursor((new Cursor(lastHit.getId(), Long.toString(lastHit.getReceivedDate())) ));
+                searchParams = new ZSearchParams("in:inbox " + SEARCH_STRING);
+                searchParams.setSortBy(SearchSortBy.dateDesc);
+                searchParams.setLimit(incLimit);
+                searchParams.setTypes(ZSearchParams.TYPE_MESSAGE);
+                searchParams.setOffset(offset);
+                resultSet = TestUtil.search(zmbx, searchParams);
+                recCount++;
+                gotMessages = resultSet.size();
+                int resCount = 0;
+                for(ZMessage msg : resultSet) {
+                    assertFalse(String.format("Request %d, result %d, encountered duplicate ID %s. Previously seen at %d", recCount, resCount, msg.getId(), seenIds.indexOf(msg.getId())), seenIds.contains(msg.getId()));
+                    seenIds.add(msg.getId());
+                    resCount++;
+                    lastHit = msg;
+                }
+               // Thread.sleep(100); //jetty sometimes crashes on Mac when bombarded with request without a timeout
+                offset+=incLimit;
+            }
+            assertEquals("Returned incorrect number of messages", numMessages, seenIds.size());
+        } catch (ServiceException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
 
     @Override
     public void tearDown()
