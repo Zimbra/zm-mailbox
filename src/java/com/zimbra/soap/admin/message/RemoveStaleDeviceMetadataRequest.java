3949fadcb698c227ee0887696343d3c6722d82bf
Unknown Perforce User <dywang>
2014-05-06 14:03:21 -0500

    bug: 79969 Mobile metadata retention -Added timer task to periodically remove stale mailbox_metadata data. By default it runs every 30 days at 1AM to remove stale metadata belong to devices whose last usage is 180 days ago. -Added soap support so admin can run it immediately.
    http://bugzilla.zimbra.com/show_bug.cgi?id=79969
    
    Copied from Perforce
     Change: 510741

---
+++ src/java/com/zimbra/soap/admin/message/RemoveStaleDeviceMetadataRequest.java
@@ -0,0 +1,52 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2014 Zimbra Software, LLC.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.4 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.soap.admin.message;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.google.common.base.Objects;
+import com.zimbra.common.soap.SyncAdminConstants;
+
+@XmlRootElement(name=SyncAdminConstants.E_REMOVE_STALE_DEVICE_METADATA_REQUEST)
+public class RemoveStaleDeviceMetadataRequest {
+
+    @XmlAttribute(name=SyncAdminConstants.A_LAST_USED_DATE_OLDER_THAN, required=false)
+    private int lastUsedDateOlderThan;
+
+    /**
+     * no-argument constructor wanted by JAXB
+     */
+    @SuppressWarnings("unused")
+    private RemoveStaleDeviceMetadataRequest() {
+    }
+
+    private RemoveStaleDeviceMetadataRequest(int days) {
+        this.lastUsedDateOlderThan = days;
+    }
+
+    public int getLastUsedDateOlderThan() {
+        return lastUsedDateOlderThan;
+    }
+
+    public void setLastUsedDateOlderThan(int lastUsedDateOlderThan) {
+        this.lastUsedDateOlderThan = lastUsedDateOlderThan;
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this).add("lastUsedDateOlderThan", this.lastUsedDateOlderThan).toString();
+    }
+}
---
+++ src/java/com/zimbra/soap/admin/message/RemoveStaleDeviceMetadataResponse.java
@@ -0,0 +1,26 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2014 Zimbra Software, LLC.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.4 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.soap.admin.message;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.zimbra.common.soap.SyncAdminConstants;
+
+@XmlRootElement(name=SyncAdminConstants.E_REMOVE_STALE_DEVICE_METADATA_RESPONSE)
+public class RemoveStaleDeviceMetadataResponse {
+
+    public RemoveStaleDeviceMetadataResponse() {}
+
+}
