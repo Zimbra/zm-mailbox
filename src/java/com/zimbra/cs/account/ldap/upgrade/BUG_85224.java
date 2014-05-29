e4977e861ae64dff5d608a1cb0961d9416063261
Unknown Perforce User <kpariani>
2014-05-29 14:23:36 -0500

    (split)Bug 85224 - SSL Forward Secrecy configuration for ZCS Adding the upgrade step in java to update the default value of zimbraReverseProxySSLCiphers on a 8.0 -> 8.5 upgrade
    http://bugzilla.zimbra.com/show_bug.cgi?id=85224
    
    Copied from Perforce
     Change: 512914

---
+++ src/java/com/zimbra/cs/account/ldap/upgrade/BUG_85224.java
@@ -0,0 +1,65 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2013 Zimbra Software, LLC.
+ *
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.4 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.account.ldap.upgrade;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.account.Entry;
+import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.ldap.LdapClient;
+import com.zimbra.cs.ldap.LdapServerType;
+import com.zimbra.cs.ldap.LdapUsage;
+import com.zimbra.cs.ldap.ZLdapContext;
+
+public class BUG_85224 extends UpgradeOp {
+
+    private static final String ATTR_NAME = Provisioning.A_zimbraReverseProxySSLCiphers;
+    private static final String OLD_VALUE = "RC4:HIGH:!aNULL:!MD5:!kEDH:!AD:!SSLv2";
+    private static final String NEW_VALUE = "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:ECDHE-RSA-RC4-SHA:ECDHE-ECDSA-RC4-SHA:AES128:AES256:RC4-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!3DES:!MD5:!PSK";
+
+    @Override
+    void doUpgrade() throws ServiceException {
+        ZLdapContext zlc = LdapClient.getContext(LdapServerType.MASTER, LdapUsage.UPGRADE);
+        try {
+            doGlobalConfig(zlc);
+        } finally {
+            LdapClient.closeContext(zlc);
+        }
+    }
+
+    private void doGlobalConfig(ZLdapContext zlc) throws ServiceException {
+        doEntry(zlc, prov.getConfig());
+    }
+
+    private void doEntry(ZLdapContext zlc, Entry entry) throws ServiceException {
+        String entryName = entry.getLabel();
+
+        printer.println();
+        printer.println("------------------------------");
+        printer.println("Checking " + ATTR_NAME + " on " + entryName);
+
+        String curValue = entry.getAttr(ATTR_NAME, NEW_VALUE);
+        if (curValue == OLD_VALUE) {
+            Map<String, Object> attrs = new HashMap<String, Object>();
+            printer.println("Changing " + ATTR_NAME + " on " + entryName + " from " + OLD_VALUE + " to " + NEW_VALUE);
+            attrs.put(Provisioning.A_zimbraReverseProxySSLCiphers, NEW_VALUE);
+            modifyAttrs(entry, attrs);
+        } else {
+            printer.println("Current value of " + ATTR_NAME + " on " + entryName + " is " + curValue + " - not changed");
+        }
+    }
+}
--- src/java/com/zimbra/cs/account/ldap/upgrade/UpgradeTask.java
+++ src/java/com/zimbra/cs/account/ldap/upgrade/UpgradeTask.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2009, 2010, 2011, 2012, 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -70,7 +70,8 @@ public enum UpgradeTask {
     BUG_75450(BUG_75450.class),
     BUG_75650(BUG_75650.class),
     BUG_76427(BUG_76427.class),
-    BUG_81385(BUG_81385.class);
+    BUG_81385(BUG_81385.class),
+    BUG_85224(BUG_85224.class);
 
     private static final String NAME_PREFIX = "BUG_";
 
