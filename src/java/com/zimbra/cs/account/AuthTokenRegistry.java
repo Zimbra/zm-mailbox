c8883a8c08ff1a559737a28b60ca460d3864682d
Greg Solovyev <greg@zimbra.com>
2014-06-09 15:24:37 -0500

    (split)bug 83550 missed a file
    http://bugzilla.zimbra.com/show_bug.cgi?id=83550
    
    Copied from Perforce
     Change: 513854

---
+++ src/java/com/zimbra/cs/account/AuthTokenRegistry.java
@@ -0,0 +1,92 @@
+package com.zimbra.cs.account;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import com.zimbra.common.localconfig.LC;
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.Log;
+import com.zimbra.common.util.LogFactory;
+import com.zimbra.cs.account.soap.SoapProvisioning;
+import com.zimbra.cs.util.Zimbra;
+import com.zimbra.soap.admin.message.RefreshRegisteredAuthTokensRequest;
+
+/**
+ *
+ * @author gsolovyev
+ * a registry of authtokens that have been deregistered (logged out) and need to be broadcasted to other servers.
+ * This class implements storage of tokens as a queue limited in size by {@link LC#zimbra_deregistered_authtoken_queue_size}
+ * and a {@link TimerTask} task responsible for broadcasting the queue to other servers.
+ * TODO: switch to using a global shared registry such as Redis or a subscription based notification mechanism such as RabbitMQ
+ */
+public final class AuthTokenRegistry {
+    private static final Log mLOG = LogFactory.getLog(AuthTokenRegistry.class);
+    //queue of deregistered authtokens
+    final private static ConcurrentLinkedQueue<ZimbraAuthToken> deregisteredOutAuthTokens = new ConcurrentLinkedQueue<ZimbraAuthToken>();
+
+    /**
+     * adds a token to the queue of deregistered tokens
+     * @param token
+     */
+    final public static void addTokenToQueue(ZimbraAuthToken token) {
+        while(deregisteredOutAuthTokens.size() > LC.zimbra_deregistered_authtoken_queue_size.intValue() && !deregisteredOutAuthTokens.isEmpty()) {
+            //throw out oldest tokens to make space
+            deregisteredOutAuthTokens.remove();
+        }
+        deregisteredOutAuthTokens.add(token);
+    }
+
+    /**
+     * starts up the {@link TimerTask} for broadcasting the queue to other servers
+     * @param interval
+     */
+    public static void startup(long interval) {
+        Zimbra.sTimer.schedule(new SendTokensTimerTask(), interval, interval);
+    }
+
+    private static final class SendTokensTimerTask extends TimerTask {
+
+        SendTokensTimerTask()  { }
+
+        @Override
+        public void run() {
+
+            mLOG.debug("Broadcasting dergistered authtokens");
+            //get a snapshot of the queue so that we are not holding logins and logouts
+            Object[] copy = deregisteredOutAuthTokens.toArray();
+
+            //if we have any tokens to report send them over to all servers
+            if(copy.length > 0) {
+                mLOG.debug("Found some dergistered authtokens to broadcast");
+
+                /* Remove the snapshot from the queue.
+                *  Since this block is not synchronized new tokens may have been added to the queue and some may have been pushed out due to size limitation
+                */
+                deregisteredOutAuthTokens.removeAll(Arrays.asList(copy));
+
+                //send the snapshot to other servers
+                try {
+                    List<Server> mailServers = Provisioning.getInstance().getAllServers(Provisioning.SERVICE_MAILBOX);
+                    SoapProvisioning soapProv = SoapProvisioning.getAdminInstance();
+                    soapProv.soapZimbraAdminAuthenticate();
+                    RefreshRegisteredAuthTokensRequest req = new RefreshRegisteredAuthTokensRequest();
+                    for(Object ztoken : copy) {
+                        if(!((ZimbraAuthToken)ztoken).isExpired()) {//no need to broadcast expired tokens, since they will be cleaned up automatically
+                            req.addToken(((ZimbraAuthToken)ztoken).getEncoded());
+                        }
+                    }
+                    for(Server server : mailServers) {
+                        if(server.isLocalServer()) {
+                            continue;
+                        }
+                        soapProv.invokeJaxb(req, server.getName());
+                    }
+                } catch (ServiceException | AuthTokenException e) {
+                    mLOG.error("Failed to broadcast deregistered authtokens", e);
+                }
+            }
+        }
+    }
+}
