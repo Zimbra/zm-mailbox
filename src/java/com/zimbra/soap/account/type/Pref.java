a6abcba6236fd916c087c5585a039bcf2769ca3e
Gren Elliot <gren.elliot@zimbra.com>
2014-06-11 03:39:04 -0500

    bug: 86864
    Delegate's forwarded invite not being recognized if attendee is not auto-adding calendar invites.
    TestUtil.waitForMessage now checks every 100ms instead of every 500.
    https://reviewboard.eng.zimbra.com/r/1513/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=86864
    
    Copied from Perforce
     Change: 513981

--- src/java/com/zimbra/soap/account/type/Pref.java
+++ src/java/com/zimbra/soap/account/type/Pref.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2010, 2011, 2012, 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -58,6 +58,10 @@ public class Pref implements KeyAndValue {
     public Pref(String name, String value) {
         setName(name);
         setValue(value);
+    }
+
+    public static Pref createPrefWithNameAndValue(String name, String value) {
+        return new Pref(name, value);
     }
 
     public String getName() { return name; }
