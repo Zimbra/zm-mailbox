94190205d358889e8421bbb521826d7621b81ba0
Unknown Perforce User <kpariani>
2014-06-20 15:47:54 -0500

    Bug 92399 - Briefcase and calendar folders are not accessible with REST URL in split server environment Removing isZimbraServiceSplitEnabled(zimbraServiceInstalled) function and its reference. We always need to check for zimbraServiceEnabled values to determine split
    http://bugzilla.zimbra.com/show_bug.cgi?id=92399
    
    Copied from Perforce
     Change: 515059

--- src/java/com/zimbra/common/util/WebSplitUtil.java
+++ src/java/com/zimbra/common/util/WebSplitUtil.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -64,11 +64,6 @@ public class WebSplitUtil {
         	ZimbraLog.misc.debug("webclient split enabled = false");
             return false;
         }
-    }
-
-    public static boolean isZimbraServiceSplitEnabled(List<String> zimbraServiceInstalled) {
-        servicesEnabled = zimbraServiceInstalled;
-        return isZimbraServiceSplitEnabled();
     }
 
     private static boolean allServicesEnabled() {
