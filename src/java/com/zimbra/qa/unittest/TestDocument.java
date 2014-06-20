79204c7df24f8d69860b23dd4f44e3d4888241f0
Greg Solovyev <greg@zimbra.com>
2014-06-20 18:33:46 -0500

    (split)bug 92427  - compare account type as Strings  - do not check registration of a "public" token
    http://bugzilla.zimbra.com/show_bug.cgi?id=92427
    
    Copied from Perforce
     Change: 515081

--- src/java/com/zimbra/qa/unittest/TestDocument.java
+++ src/java/com/zimbra/qa/unittest/TestDocument.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2009, 2010, 2011, 2012, 2013 Zimbra Software, LLC.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.4 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -20,11 +20,17 @@ import java.util.List;
 
 import junit.framework.TestCase;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.GetMethod;
+
 import com.zimbra.client.ZDocument;
 import com.zimbra.client.ZItem;
 import com.zimbra.client.ZMailbox;
 import com.zimbra.client.ZSearchParams;
 import com.zimbra.common.account.Key;
+import com.zimbra.common.httpclient.HttpClientUtil;
+import com.zimbra.common.util.ZimbraHttpConnectionManager;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.mailbox.ACL;
@@ -43,7 +49,7 @@ public class TestDocument extends TestCase {
     throws Exception {
         cleanUp();
     }
-    
+
     /**
      * Tests documents created with the {@code Note} flag set.
      */
@@ -56,12 +62,12 @@ public class TestDocument extends TestCase {
         ZDocument note = TestUtil.createDocument(mbox, folderId, NAME_PREFIX + "-note.txt", "text/plain", "note".getBytes(), true);
         String flags = Character.toString(ZItem.Flag.note.getFlagChar());
         mbox.updateItem(note.getId(), null, null, flags, null);
-        
+
         // Confirm that the Note flag is set when getting the documents.
         doc = mbox.getDocument(doc.getId());
         assertEquals(null, doc.getFlags());
         assertEquals(flags, note.getFlags());
-        
+
         // Test searching for notes.
         List<String> ids = TestUtil.search(mbox, "in:briefcase tag:\\note", ZSearchParams.TYPE_DOCUMENT);
         assertEquals(1, ids.size());
@@ -78,7 +84,7 @@ public class TestDocument extends TestCase {
         String filename = NAME_PREFIX + "-testMoveNote.txt";
         Account acct = Provisioning.getInstance().get(Key.AccountBy.name, TestUtil.getAddress(USER_NAME));
         Account acct2 = Provisioning.getInstance().get(Key.AccountBy.name, TestUtil.getAddress(USER2_NAME));
-        
+
         // Create a note.
         ZMailbox zmbx = TestUtil.getZMailbox(USER_NAME);
         String folderId = Integer.toString(Mailbox.ID_FOLDER_BRIEFCASE);
@@ -89,41 +95,41 @@ public class TestDocument extends TestCase {
 
         Mailbox remoteMbox = MailboxManager.getInstance().getMailboxByAccount(acct2);
         Mailbox mbox1 = MailboxManager.getInstance().getMailboxByAccount(acct);
-        
+
         // Clean up test data for user2.
         TestUtil.deleteTestData(USER2_NAME, NAME_PREFIX);
-        
+
         // reset the access if there are any present.
         remoteMbox.revokeAccess(null, Mailbox.ID_FOLDER_BRIEFCASE, acct.getId());
         mbox1.revokeAccess(null, Mailbox.ID_FOLDER_BRIEFCASE, acct2.getId());
-        
+
         // Give write permissions on user2's Briefcase for user1 and user1's Briefcase for user2.
         remoteMbox.grantAccess(null, Mailbox.ID_FOLDER_BRIEFCASE, acct.getId(), ACL.GRANTEE_USER,(short) (ACL.RIGHT_READ | ACL.RIGHT_WRITE | ACL.RIGHT_INSERT), null);
         mbox1.grantAccess(null, Mailbox.ID_FOLDER_BRIEFCASE, acct2.getId(), ACL.GRANTEE_USER,(short) (ACL.RIGHT_READ | ACL.RIGHT_WRITE | ACL.RIGHT_INSERT), null);
-        
+
         // move the note to user2
         zmbx.moveItem(note.getId(), acct2.getId() + ":" + Mailbox.ID_FOLDER_BRIEFCASE, null);
         ZMailbox remoteZmbx = TestUtil.getZMailbox(USER2_NAME);
         String idStr = TestUtil.search(remoteZmbx, "in:briefcase " + filename , ZSearchParams.TYPE_DOCUMENT).get(0);
         Document doc = remoteMbox.getDocumentById(null, Integer.parseInt(idStr));
-        
+
         // make sure moved document has note flag.
         assertEquals(note.getFlags(), doc.getFlagString());
-        
+
         // move the note back to user1
         remoteZmbx.moveItem(String.valueOf(doc.getId()), acct.getId() + ":" + Mailbox.ID_FOLDER_BRIEFCASE, null);
-        
+
         // reset the access
         remoteMbox.revokeAccess(null, Mailbox.ID_FOLDER_BRIEFCASE, acct.getId());
         mbox1.revokeAccess(null, Mailbox.ID_FOLDER_BRIEFCASE, acct2.getId());
         idStr = TestUtil.search(zmbx, "in:briefcase " + filename , ZSearchParams.TYPE_DOCUMENT).get(0);
-        
+
         doc = mbox1.getDocumentById(null, Integer.parseInt(idStr));
-        
+
         // make sure moved document has note flag.
         assertEquals(note.getFlags(), doc.getFlagString());
     }
-    
+
     /**
      * Tests deletion of blob when document revision is deleted.
      */
@@ -148,7 +154,7 @@ public class TestDocument extends TestCase {
         assertTrue(file1.exists());
         assertFalse(file2.exists());
     }
-    
+
     /**
      * Tests deletion of blobs when document revisions are deleted.
      */
@@ -173,24 +179,45 @@ public class TestDocument extends TestCase {
         assertFalse(file1.exists());
         assertFalse(file2.exists());
     }
-    
+
     /**
      * Tests the content-type based on file extension.
      */
-
- public void testContentType()
+    public void testContentType()
     throws Exception {
         // Create two documents.
         ZMailbox mbox = TestUtil.getZMailbox(USER_NAME);
         String folderId = Integer.toString(Mailbox.ID_FOLDER_BRIEFCASE);
         ZDocument doc1 = TestUtil.createDocument(mbox, folderId, NAME_PREFIX + "-docOne.doc", "application/octet-stream", "doc1".getBytes());
         ZDocument doc2 = TestUtil.createDocument(mbox, folderId, NAME_PREFIX + "-docTwo.xls", "application/ms-tnef", "doc2".getBytes());
-        
+
         // Confirm that the content-type changed based on file extension
         assertEquals("application/msword", doc1.getContentType());
         assertEquals("application/vnd.ms-excel", doc2.getContentType());
     }
 
+    /**
+     * Test REST access to publicly shared folder
+     * @throws Exception
+     */
+    public void testPublicShare()
+    throws Exception {
+        Mailbox mbox = TestUtil.getMailbox(USER_NAME);
+        String folderName = NAME_PREFIX + "testPublicSharing";
+        Folder.FolderOptions fopt = new Folder.FolderOptions().setDefaultView(MailItem.Type.DOCUMENT);
+        Folder folder = mbox.createFolder(null, "/" + folderName, fopt);
+        Document doc = mbox.createDocument(null, folder.getId(), "test2.txt", "text/plain", NAME_PREFIX, "testPublicSharing", new ByteArrayInputStream("A Test String".getBytes()));
+        mbox.grantAccess(null, folder.getId(), null, ACL.GRANTEE_PUBLIC, ACL.stringToRights("rw"), null);
+
+        String URL = TestUtil.getBaseUrl()+"/home/" + mbox.getAccount().getName()+"/"+folderName;
+        HttpClient eve = ZimbraHttpConnectionManager.getInternalHttpConnMgr().newHttpClient();
+        GetMethod get = new GetMethod(URL);
+        int statusCode = HttpClientUtil.executeMethod(eve, get);
+        assertEquals("This request should succeed. Getting status code " + statusCode, HttpStatus.SC_OK,statusCode);
+        String respStr = get.getResponseBodyAsString();
+        assertFalse("Should not contain AUTH_EXPIRED", respStr.contains("AUTH_EXPIRED"));
+        assertTrue("Should contain shared content ", respStr.contains("test2.txt"));
+    }
     @Override public void tearDown()
     throws Exception {
         cleanUp();
@@ -200,7 +227,7 @@ public class TestDocument extends TestCase {
     throws Exception {
         TestUtil.deleteTestData(USER_NAME, NAME_PREFIX);
     }
-    
+
     public static void main(String[] args)
     throws Exception {
         TestUtil.cliSetup();
