f4848f3d7263434e1028487c9d09b5e4d4596ae1
Rupali Desai <rdesai@zimbra.com>
2014-06-24 03:08:11 -0500

    Bug: 83547
    - Modified code to return CSRF token in the Auth response as well as HTTP response header
    - Modified generateCsrfToken method to throw ServiceException
    
    http://wiki.eng.zimbra.com/index.php/JPCSRF_Bug83547
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=83547
    
    Copied from Perforce
     Change: 515232

--- src/java/com/zimbra/soap/account/message/AuthResponse.java
+++ src/java/com/zimbra/soap/account/message/AuthResponse.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2010, 2011, 2012, 2013 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -33,7 +33,6 @@ import com.zimbra.common.soap.HeaderConstants;
 import com.zimbra.soap.account.type.Attr;
 import com.zimbra.soap.account.type.Pref;
 import com.zimbra.soap.account.type.Session;
-
 import com.zimbra.soap.json.jackson.annotate.ZimbraJsonAttribute;
 import com.zimbra.soap.json.jackson.annotate.ZimbraKeyValuePairs;
 
@@ -43,10 +42,11 @@ import com.zimbra.soap.json.jackson.annotate.ZimbraKeyValuePairs;
    <authToken>...</authToken>
    <lifetime>...</lifetime>
    <session .../>
-   <refer>{mail-host}</refer>  
+   <refer>{mail-host}</refer>
    [<prefs><pref name="{name}" modified="{modified-time}">{value}</pref>...</prefs>]
    [<attrs><attr name="{name}">{value}</a>...</attrs>]
    [<skin>{skin-name}</skin>]
+   [<csrfToken>{csrf-token}</csrfToken>]
  </AuthResponse>
 /**
  * @zm-api-response-description Response to account authentication request.
@@ -84,7 +84,14 @@ public class AuthResponse {
      */
     @XmlElement(name=AccountConstants.E_SKIN /* skin */, required=false)
     private String skin;
-    
+
+    /**
+     * @zm-api-field-description if client is CSRF token enabled , the CSRF token
+     * Returned only when client says it is CSRF enabled .
+     */
+    @XmlElement(name=HeaderConstants.E_CSRFTOKEN /* CSRF token*/, required=false)
+    private String csrfToken;
+
     /**
      * @zm-api-field-description Requested preference settings.
      */
@@ -92,7 +99,7 @@ public class AuthResponse {
     @XmlElementWrapper(name=AccountConstants.E_PREFS /* prefs */)
     @XmlElement(name=AccountConstants.E_PREF /* pref */)
     private List<Pref> prefs = new ArrayList<Pref>();
-    
+
     /**
      * @zm-api-field-description Requested attribute settings.  Only attributes that are allowed to be returned by
      * GetInfo will be returned by this call
@@ -101,30 +108,30 @@ public class AuthResponse {
     @XmlElementWrapper(name=AccountConstants.E_ATTRS /* attrs */)
     @XmlElement(name=AccountConstants.E_ATTR /* attr */)
     private List<Attr> attrs = new ArrayList<Attr>();
-    
+
     public AuthResponse() {
     }
-    
+
     public AuthResponse(String authToken, long lifetime) {
         setAuthToken(authToken);
         setLifetime(lifetime);
     }
-    
+
     public String getAuthToken() { return authToken; }
     public AuthResponse setAuthToken(String authToken) { this.authToken = authToken; return this; }
-    
+
     public long getLifetime() { return lifetime; }
     public AuthResponse setLifetime(long lifetime) { this.lifetime = lifetime; return this; }
-    
+
     public Session getSession() { return session; }
     public AuthResponse setSession(Session session) { this.session = session; return this; }
-    
+
     public String getRefer() { return refer; }
     public AuthResponse setRefer(String refer) { this.refer = refer; return this; }
-    
+
     public String getSkin() { return skin; }
     public AuthResponse setSkin(String skin) { this.skin = skin; return this; }
-    
+
     public List<Pref> getPrefs() { return Collections.unmodifiableList(prefs); }
     public AuthResponse setPrefs(Collection<Pref> prefs) {
         this.prefs.clear();
@@ -133,7 +140,7 @@ public class AuthResponse {
         }
         return this;
     }
-    
+
     public List<Attr> getAttrs() { return Collections.unmodifiableList(attrs); }
     public AuthResponse setAttrs(Collection<Attr> attrs) {
         this.attrs.clear();
@@ -146,8 +153,26 @@ public class AuthResponse {
     public Multimap<String, String> getAttrsMultimap() {
         return Attr.toMultimap(attrs);
     }
-    
+
     public Multimap<String, String> getPrefsMultimap() {
         return Pref.toMultimap(prefs);
     }
+
+
+    /**
+     * @return the csrfToken
+     */
+    public String getCsrfToken() {
+        return csrfToken;
+    }
+
+
+    /**
+     * @param csrfToken the csrfToken to set
+     */
+    public void setCsrfToken(String csrfToken) {
+        this.csrfToken = csrfToken;
+    }
+
+
 }
