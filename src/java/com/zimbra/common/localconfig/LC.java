b639c1a5efb1709386d4722b4c4bd04424e00dff
Rupali Desai <rdesai@zimbra.com>
2014-06-30 03:56:01 -0500

    Bug: 92247 Unable to sync Mac mail
    - Fixed the way EWS service URL is constructed. It now uses by default URLUtil.GetPublicUrlForDomain and uses LC key for sites that need to return home mailbox server
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=92247
    
    Copied from Perforce
     Change: 515880

--- src/java/com/zimbra/common/localconfig/LC.java
+++ src/java/com/zimbra/common/localconfig/LC.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -1239,6 +1239,9 @@ public final class LC {
         KnownKey.newKey("/opt/zimbra/lib/ext/zimbraews/");
     public static final KnownKey ews_service_log_file =
         KnownKey.newKey("/opt/zimbra/log/ews.log");
+
+    public static final KnownKey zimbra_ews_autodiscover_use_service_url =
+        KnownKey.newKey(false);
 
     static {
         // Automatically set the key name with the variable name.
