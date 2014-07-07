5e765ab6b1a7a3902a69cc343f8a37f4a910305a
Gren Elliot <gren.elliot@zimbra.com>
2014-07-07 14:35:55 -0500

    bug:92900
    enhanced JAXB for SOAP Header Context used in requests.
    Based on code in ZimbraSoapContext constructor and SoapTransport.generateSoapMessage
    
    One limitation.  Doesn't support attributes on <authToken>.  This was poorly designed - using the text body of <authToken> as the value but also allowing sub-elements for attributes leads to major usability issues in JAXB.   (You can't have @XmlElement and @XmlValue in the same class...)  If it becomes important in the future, suggest we allow specifying the authToken value in an attribute as an alternative and requiring that when using attributes on the auth token.
    https://reviewboard.eng.zimbra.com/r/1871/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=92900
    
    Copied from Perforce
     Change: 516775

--- src/java/com/zimbra/common/soap/HeaderConstants.java
+++ src/java/com/zimbra/common/soap/HeaderConstants.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -34,6 +34,8 @@ public final class HeaderConstants {
     public static final String E_NO_NOTIFY  = "nonotify";
     public static final String E_NO_SESSION = "nosession";
     public static final String E_SESSION    = "session";
+    @Deprecated
+    public static final String E_SESSION_ID    = "sessionId";
     public static final String A_ACCOUNT_ID = "acct";
     public static final String A_ID         = "id";
     public static final String A_PROXIED    = "proxy";
--- src/java/com/zimbra/common/soap/SoapTransport.java
+++ src/java/com/zimbra/common/soap/SoapTransport.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -341,10 +341,12 @@ public abstract class SoapTransport {
         if (mContext != null) {
             String sid = mContext.getAttribute(HeaderConstants.E_SESSION, null);
             // be backwards-compatible for sanity-preservation purposes
-            if (sid == null)
-                mContext.getAttribute("sessionId", null);
-            if (sid != null)
+            if (sid == null) {
+                mContext.getAttribute(HeaderConstants.E_SESSION_ID, null);
+            }
+            if (sid != null) {
                 mSessionId = sid;
+            }
         }
 
         if (proto.isFault(e)) {
