9580cb56ca20a3a3fb48609c15291344ed157abf
Unknown Perforce User <jflanigan>
2014-07-18 19:02:15 -0500

    (split)bug: 93666
    Add admin auth requirement to GetVersionInfo. Delay sending this request from admin console until user is logged in.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=93666
    
    Copied from Perforce
     Change: 518188

--- src/java/com/zimbra/cs/service/admin/GetVersionInfo.java
+++ src/java/com/zimbra/cs/service/admin/GetVersionInfo.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2005, 2006, 2007, 2009, 2010, 2011, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -23,15 +23,15 @@ import java.util.Map;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
 import com.zimbra.common.util.StringUtil;
+import com.zimbra.cs.account.accesscontrol.AdminRight;
+import com.zimbra.cs.util.BuildInfo;
 import com.zimbra.soap.ZimbraSoapContext;
 import com.zimbra.soap.admin.message.GetVersionInfoResponse;
 import com.zimbra.soap.admin.type.VersionInfo;
 
-import com.zimbra.cs.account.accesscontrol.AdminRight;
-import com.zimbra.cs.util.BuildInfo;
-
 public class GetVersionInfo extends AdminDocumentHandler {
 
+    @Override
     public Element handle(Element request, Map<String, Object> context)
     throws ServiceException {
         ZimbraSoapContext lc = getZimbraSoapContext(context);
@@ -41,12 +41,12 @@ public class GetVersionInfo extends AdminDocumentHandler {
         if (!StringUtil.isNullOrEmpty(BuildInfo.TYPE)) {
             fullVersionInfo = fullVersionInfo + "." + BuildInfo.TYPE;
             versionInfo.setType(BuildInfo.TYPE);
-        }    
+        }
         versionInfo.setVersion(fullVersionInfo);
         versionInfo.setRelease(BuildInfo.RELEASE);
         versionInfo.setBuildDate(BuildInfo.DATE);
         versionInfo.setHost(BuildInfo.HOST);
-        
+
         versionInfo.setMajorVersion(BuildInfo.MAJORVERSION);
         versionInfo.setMinorVersion(BuildInfo.MINORVERSION);
         versionInfo.setMicroVersion(BuildInfo.MICROVERSION);
@@ -55,10 +55,12 @@ public class GetVersionInfo extends AdminDocumentHandler {
         return lc.jaxbToElement(resp);
     }
 
+    @Override
     public boolean needsAdminAuth(Map<String, Object> context) {
-        return false;
+        return true;
     }
 
+    @Override
     public boolean needsAuth(Map<String, Object> context) {
         return false;
     }
