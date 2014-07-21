fe8a4a3855b63e5647b059cb851793f299b3058b
Unknown Perforce User <jflanigan>
2014-07-21 12:56:57 -0500

    (split)bug: 93666
    Fix test case.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=93666
    
    Copied from Perforce
     Change: 518301

--- src/java/com/zimbra/qa/unittest/TestSoap.java
+++ src/java/com/zimbra/qa/unittest/TestSoap.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -21,15 +21,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import junit.framework.TestCase;
 
+import com.zimbra.client.ZAuthResult;
+import com.zimbra.client.ZFolder;
+import com.zimbra.client.ZMailbox;
+import com.zimbra.common.account.Key.AccountBy;
 import com.zimbra.common.auth.ZAuthToken;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.AccountConstants;
@@ -37,24 +39,21 @@ import com.zimbra.common.soap.AdminConstants;
 import com.zimbra.common.soap.Element;
 import com.zimbra.common.soap.SoapFaultException;
 import com.zimbra.common.soap.SoapHttpTransport;
-import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.common.soap.SoapTransport;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.Server;
-import com.zimbra.common.account.Key.AccountBy;
 import com.zimbra.cs.ldap.LdapConstants;
-import com.zimbra.client.ZAuthResult;
-import com.zimbra.client.ZFolder;
-import com.zimbra.client.ZMailbox;
+import com.zimbra.qa.unittest.prov.soap.SoapTest;
 
 public class TestSoap
 extends TestCase {
 
     private static final String USER_NAME = "user1";
     private static final String NAME_PREFIX = TestSoap.class.getSimpleName();
-    
+
     private String mOriginalSoapRequestMaxSize;
     private String mOriginalSoapExposeVersion;
-    
+
     @Override
     public void setUp()
     throws Exception {
@@ -63,18 +62,18 @@ extends TestCase {
         mOriginalSoapExposeVersion = server.getAttr(Provisioning.A_zimbraSoapExposeVersion, "");
         cleanUp();
     }
-    
+
     public void testSoapRequestMaxSize()
     throws Exception {
         StringBuilder messageBody = new StringBuilder();
         for (int i = 1; i <= 100; i++) {
             messageBody.append("Morey Amsterdam was a great man.  Morey Amsterdam was not a sandwich.\r\n");
         }
-        
+
         setSoapRequestMaxSize(100000);
         ZMailbox mbox = TestUtil.getZMailbox(USER_NAME);
         TestUtil.sendMessage(mbox, USER_NAME, NAME_PREFIX + " 1", messageBody.toString());
-        
+
         setSoapRequestMaxSize(1000);
         try {
             TestUtil.sendMessage(mbox, USER_NAME, NAME_PREFIX + " 2", messageBody.toString());
@@ -91,12 +90,12 @@ extends TestCase {
     throws Exception {
         SoapHttpTransport transport = new SoapHttpTransport(TestUtil.getSoapUrl());
         Element request = Element.create(transport.getRequestProtocol(), AccountConstants.GET_VERSION_INFO_REQUEST);
-        
+
         // Test with version exposed
         TestUtil.setServerAttr(Provisioning.A_zimbraSoapExposeVersion, LdapConstants.LDAP_TRUE);
         Element response = transport.invoke(request);
         validateSoapVersionResponse(response);
-        
+
         // Test with version not exposed
         TestUtil.setServerAttr(Provisioning.A_zimbraSoapExposeVersion, LdapConstants.LDAP_FALSE);
         request = Element.create(transport.getRequestProtocol(), AccountConstants.GET_VERSION_INFO_REQUEST);
@@ -107,29 +106,29 @@ extends TestCase {
             assertEquals(ServiceException.PERM_DENIED, e.getCode());
         }
     }
-    
+
     /**
      * Tests the AdminService version of GetInfoRequest.
      */
     public void testAdminGetInfoRequest()
     throws Exception {
-        SoapHttpTransport transport = new SoapHttpTransport(TestUtil.getAdminSoapUrl());
+        SoapTransport transport = SoapTest.authAdmin("admin");
         Element request = Element.create(transport.getRequestProtocol(), AdminConstants.GET_VERSION_INFO_REQUEST);
         Element response = transport.invoke(request);
         validateSoapVersionResponse(response);
     }
-    
+
     private void validateSoapVersionResponse(Element response)
     throws ServiceException {
         assertEquals(AccountConstants.GET_VERSION_INFO_RESPONSE.getName(), response.getName());
-        
+
         Element info = response.getElement(AccountConstants.E_VERSION_INFO_INFO);
         assertNotNull(info.getAttribute(AccountConstants.A_VERSION_INFO_DATE));
         assertNotNull(info.getAttribute(AccountConstants.A_VERSION_INFO_HOST));
         assertNotNull(info.getAttribute(AccountConstants.A_VERSION_INFO_RELEASE));
         assertNotNull(info.getAttribute(AccountConstants.A_VERSION_INFO_VERSION));
     }
-    
+
     /**
      * Confirms that attrs and prefs are selected when specified in {@link ZMailbox} options.
      */
@@ -143,7 +142,7 @@ extends TestCase {
         options.setUri(TestUtil.getSoapUrl());
         ZMailbox mbox = runAuthTest(options);
         ZAuthToken authToken = mbox.getAuthToken();
-        
+
         // Test auth token auth.
         options = new ZMailbox.Options();
         options.setAuthToken(authToken);
@@ -151,7 +150,7 @@ extends TestCase {
         options.setUri(TestUtil.getSoapUrl());
         runAuthTest(options);
     }
-    
+
     private ZMailbox runAuthTest(ZMailbox.Options options)
     throws Exception {
         List<String> attrNames = Arrays.asList(
@@ -160,18 +159,18 @@ extends TestCase {
         List<String> prefNames = Arrays.asList(
             Provisioning.A_zimbraPrefComposeFormat,
             Provisioning.A_zimbraPrefAutoSaveDraftInterval);
-        
+
         options.setAttrs(attrNames);
         options.setPrefs(prefNames);
         ZMailbox mbox = ZMailbox.getMailbox(options);
-        
+
         ZAuthResult auth = mbox.getAuthResult();
         Map<String, List<String>> attrs = auth.getAttrs();
         Map<String, List<String>> prefs = auth.getPrefs();
-        
+
         assertEquals(attrNames.size(), attrs.size());
         assertEquals(prefNames.size(), prefs.size());
-        
+
         for (String attrName : attrNames) {
             assertTrue(attrs.containsKey(attrName));
         }
@@ -180,7 +179,7 @@ extends TestCase {
         }
         return mbox;
     }
-    
+
     public void testGetFolders()
     throws Exception {
         ZMailbox.Options options = new ZMailbox.Options();
@@ -190,7 +189,7 @@ extends TestCase {
         options.setUri(TestUtil.getSoapUrl());
         options.setNoSession(true);
         ZMailbox mbox = ZMailbox.getMailbox(options);
-        
+
         ZFolder inbox = mbox.getFolderByPath("/Inbox");
         assertEquals("Inbox", inbox.getName());
     }
@@ -270,7 +269,7 @@ extends TestCase {
             .append("</SOAO-ENV:Envelope>\n");
         String responseString = doLowLevelRequest(new URL(TestUtil.getSoapUrl() + "/WibbleRequest"), req.toString());
         assertTrue("Response should be a SOAP 1.1 fault", responseString.startsWith(
-                "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">" + 
+                "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
                 "<soap:Body><soap:Fault><faultcode>soap:Client</faultcode><faultstring>parse error:"));
     }
 
@@ -281,17 +280,17 @@ extends TestCase {
         TestUtil.setServerAttr(Provisioning.A_zimbraSoapExposeVersion, mOriginalSoapExposeVersion);
         cleanUp();
     }
-    
+
     private void cleanUp()
     throws Exception {
         TestUtil.deleteTestData(USER_NAME, NAME_PREFIX);
     }
-    
+
     private void setSoapRequestMaxSize(int numBytes)
     throws Exception {
         TestUtil.setServerAttr(Provisioning.A_zimbraSoapRequestMaxSize, Integer.toString(numBytes));
     }
-    
+
     public static void main(String[] args)
     throws Exception {
         TestUtil.cliSetup();
