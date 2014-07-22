79db4e60a7db69661e0bde09e34c2a93f3223ff0
Unknown Perforce User <jflanigan>
2014-07-22 16:08:23 -0500

    (split)bug: 93670
    Require admin auth for PingRequest and GetLicenseInfoRequest. Do not see any call sites where these are used (with or without auth).
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=93670
    
    Copied from Perforce
     Change: 518472

--- src/java/com/zimbra/cs/service/admin/GetLicenseInfo.java
+++ src/java/com/zimbra/cs/service/admin/GetLicenseInfo.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2005, 2006, 2007, 2009, 2010, 2011, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -20,19 +20,19 @@ package com.zimbra.cs.service.admin;
 import java.util.List;
 import java.util.Map;
 
+import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
+import com.zimbra.cs.account.accesscontrol.AdminRight;
 import com.zimbra.soap.ZimbraSoapContext;
 import com.zimbra.soap.admin.message.GetLicenseInfoResponse;
 import com.zimbra.soap.admin.type.LicenseExpirationInfo;
-
-import com.zimbra.common.localconfig.LC;
-import com.zimbra.cs.account.accesscontrol.AdminRight;
 
 public class GetLicenseInfo extends AdminDocumentHandler {
 
     static final String TRIAL_EXPIRATION_DATE_KEY = "trial_expiration_date";
 
+    @Override
     public Element handle(Element request, Map<String, Object> context)
     throws ServiceException {
         ZimbraSoapContext lc = getZimbraSoapContext(context);
@@ -43,14 +43,6 @@ public class GetLicenseInfo extends AdminDocumentHandler {
         GetLicenseInfoResponse resp =
             new GetLicenseInfoResponse(expirationInfo);
         return lc.jaxbToElement(resp);
-    }
-
-    public boolean needsAdminAuth(Map<String, Object> context) {
-        return false;
-    }
-
-    public boolean needsAuth(Map<String, Object> context) {
-        return false;
     }
 
     @Override
--- src/java/com/zimbra/cs/service/admin/Ping.java
+++ src/java/com/zimbra/cs/service/admin/Ping.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2004, 2005, 2006, 2007, 2009, 2010, 2011, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -25,9 +25,9 @@ import java.util.Map;
 
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
+import com.zimbra.cs.account.accesscontrol.AdminRight;
 import com.zimbra.soap.ZimbraSoapContext;
 import com.zimbra.soap.admin.message.PingResponse;
-import com.zimbra.cs.account.accesscontrol.AdminRight;
 
 /**
  * @author schemers
@@ -37,24 +37,13 @@ public class Ping extends AdminDocumentHandler {
     /* (non-Javadoc)
       * @see com.zimbra.soap.DocumentHandler#handle(org.dom4j.Element, java.util.Map)
       */
+    @Override
     public Element handle(Element request, Map<String, Object> context)
     throws ServiceException {
         ZimbraSoapContext lc = getZimbraSoapContext(context);
         return lc.jaxbToElement(new PingResponse());
     }
 
-    public boolean needsAuth(Map<String, Object> context) {
-        // return false because this may be called from Perl which
-        // doesn't have auth token
-        return false;
-    }
-
-    public boolean needsAdminAuth(Map<String, Object> context) {
-        // return false because this may be called from Perl which
-        // doesn't have auth token
-        return false;
-    }
-    
     @Override
     public void docRights(List<AdminRight> relatedRights, List<String> notes) {
         notes.add(AdminRightCheckPoint.Notes.ALLOW_ALL_ADMINS);
