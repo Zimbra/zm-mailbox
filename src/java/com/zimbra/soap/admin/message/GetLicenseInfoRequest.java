a53d00c31e8433d7e05b135827073db20cff61ed
Unknown Perforce User <jflanigan>
2014-07-22 16:08:23 -0500

    bug: 93670
    Require admin auth for PingRequest and GetLicenseInfoRequest. Do not see any call sites where these are used (with or without auth).
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=93670
    
    Copied from Perforce
     Change: 518472

--- src/java/com/zimbra/soap/admin/message/GetLicenseInfoRequest.java
+++ src/java/com/zimbra/soap/admin/message/GetLicenseInfoRequest.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2011, 2012, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -22,8 +22,8 @@ import javax.xml.bind.annotation.XmlRootElement;
 import com.zimbra.common.soap.AdminConstants;
 
 /**
- * @zm-api-command-auth-required false
- * @zm-api-command-admin-auth-required false - Always provide license information
+ * @zm-api-command-auth-required true
+ * @zm-api-command-admin-auth-required true
  * @zm-api-command-description Get License information
  */
 @XmlRootElement(name=AdminConstants.E_GET_LICENSE_INFO_REQUEST)
