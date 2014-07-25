44392849633890d115713c19b99a8646c18b9a8f
Rupali Desai <rdesai@zimbra.com>
2014-07-25 12:03:09 -0500

    (split)Bug: 94066 Logged out after reading message
    - The cause of this was that while passing AuthToken between servers we turn off the CSRF bit, we also re encode the Auth Token when this happens. This resulted in the CSRF validation failure, as we cannot find a CSRF token for the new AuthToken
    -Fixed this by cloning the AuthToken and then turning off the CSRF bit in this new instance, this leaves the original token unchanged.
    -Verified changes by deploying in dev and calling getCsrfUnsecuredAuthToken() in SoapEngine, verified in the original auth token CSRF bit is unmodified while the authtoken returned from getCsrfUnsecuredAuthToken()  has CSRF bit turned off.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=94066
    
    Copied from Perforce
     Change: 518975

--- src/java/com/zimbra/cs/account/AuthToken.java
+++ src/java/com/zimbra/cs/account/AuthToken.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -33,6 +33,7 @@ import com.zimbra.common.auth.ZAuthToken;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
 import com.zimbra.common.util.ByteUtil;
+import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.account.auth.AuthMechanism.AuthMech;
 
 
@@ -65,13 +66,23 @@ public abstract class AuthToken {
      * running within the same system using the auth token inside an op/soap context.
      *
      * @param authToken
-     * @return
+     * @return clone of an existing Auth token
      */
-    public static AuthToken getCsrfUnsecuredAuthToken(AuthToken authToken) {
+    public static AuthToken getCsrfUnsecuredAuthToken(AuthToken authToken)  {
+
         if (authToken.isCsrfTokenEnabled()) {
-            authToken.setCsrfTokenEnabled(false);
+            AuthToken token;
+            try {
+                token = (AuthToken) authToken.clone();
+                token.setCsrfTokenEnabled(false);
+                return token;
+            } catch (CloneNotSupportedException e) {
+                ZimbraLog.misc.debug("Error cloning auth token.", e);
+                return null;
+            }
+        } else {
+            return authToken;
         }
-        return authToken;
     }
 
     @Override
