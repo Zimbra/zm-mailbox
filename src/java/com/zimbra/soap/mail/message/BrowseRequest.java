cc1c25e5654ad34236d21f96671b5a3c7463d95c
Gren Elliot <gren.elliot@zimbra.com>
2014-09-18 10:24:58 -0500

    bug:94843
    TestJaxb.testBrowseRequestObjectsBadRegex() failing.
    This was caused by there being no index hits for messages containing objects.  I believe this is probably because indexing occurs on the lmtpinjected messages before the zimlets are configured.
    Test updated to add a message containing an object to ensure at least one hit.  Also fixed bug recently introduced which didn't cleanup data properly.
    Documentation updated as I for one wasn't clear what an object was in this context.
    Some minor code tidyup and logging added in the code that looks for objects.
    Note that submitted Bug 95624 to reflect the fact that BrowseResponse can be nonsense for objects like com_zimbra_url
    https://reviewboard.eng.zimbra.com/r/2495/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=94843
    
    Copied from Perforce
     Change: 523625

--- src/java/com/zimbra/soap/mail/message/BrowseRequest.java
+++ src/java/com/zimbra/soap/mail/message/BrowseRequest.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2011, 2012, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -17,12 +17,12 @@
 
 package com.zimbra.soap.mail.message;
 
-import com.google.common.base.Objects;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.google.common.base.Objects;
 import com.zimbra.common.soap.MailConstants;
 
 /**
@@ -57,6 +57,8 @@ public class BrowseRequest {
     /**
      * @zm-api-field-tag browse-by-domains|attachments|objects
      * @zm-api-field-description Browse by setting - <b>domains|attachments|objects</b>
+     * <br/>where <b>objects</b> means objects in message body content recognized by Zimlets via &lt;contentObject&gt;
+     * configuration.
      */
     @XmlAttribute(name=MailConstants.A_BROWSE_BY, required=true)
     private final String browseBy;
