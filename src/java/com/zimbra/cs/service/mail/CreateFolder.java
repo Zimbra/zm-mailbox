ac1e89f4ff4d07da4e7db30f1580cba077d6f417
Unknown Perforce User <jflanigan>
2014-09-18 17:24:23 -0500

    (split)bug: 95572
    If parent folder is not specified treat the folder name as a path.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=95572
    
    Copied from Perforce
     Change: 523682

--- src/java/com/zimbra/cs/service/mail/CreateFolder.java
+++ src/java/com/zimbra/cs/service/mail/CreateFolder.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -94,7 +94,11 @@ public class CreateFolder extends MailDocumentHandler {
             }
         } catch (ServiceException se) {
             if (se.getCode() == MailServiceException.ALREADY_EXISTS && fetchIfExists) {
-                folder = mbox.getFolderByName(octxt, iidParent.getId(), name);
+                if (iidParent != null) {
+                    folder = mbox.getFolderByName(octxt, iidParent.getId(), name);
+                } else {
+                    folder = mbox.getFolderByPath(octxt, name);
+                }
                 alreadyExisted = true;
             } else {
                 throw se;
