556918158e4e55193e55700d8a1938e4f70262e5
Gren Elliot <gren.elliot@zimbra.com>
2014-10-28 19:46:57 -0500

    bug:96145
    zmtzdata2ical generates incorrect VTIMEZONE for Europe/Moscow and others where rule has only just come into effect.
    Problem was that the Day type used is multipurpose and the compareTo method doesn't work correctly for ON comparisons, so, if the reference date for the end of relevance for a rule is recent (within the same month) we incorrectly think it still applies.
    https://reviewboard.eng.zimbra.com/r/2869/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=96145
    
    Copied from Perforce
     Change: 527206

--- src/java/com/zimbra/common/calendar/ZoneInfoParser.java
+++ src/java/com/zimbra/common/calendar/ZoneInfoParser.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2008, 2009, 2010, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -381,8 +381,12 @@ public class ZoneInfoParser {
                 }
             }
             comp = dtIndex[0] - dtIndex[1];
-            if (comp != 0)
+            if (comp != 0) {
                 return comp;
+            }
+            if (DayType.ON.equals(mType)) {
+                return mDate - other.getDate();
+            }
 
             return Weekday.toInt(mWeekday) - Weekday.toInt(other.getWeekday());
         }
