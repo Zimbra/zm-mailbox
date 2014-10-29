4f3bcd218bdefc26f861fbac8ff0add42feb126b
Gren Elliot <gren.elliot@zimbra.com>
2014-10-29 06:19:49 -0500

    bug:96145
    Copy tool source from main to ensure we have tools that can process more recent tzdata distributions
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=96145
    
    Copied from Perforce
     Change: 527244

--- src/java/com/zimbra/common/calendar/ZoneInfo2iCalendar.java
+++ src/java/com/zimbra/common/calendar/ZoneInfo2iCalendar.java
@@ -1,15 +1,17 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2008, 2009, 2010, 2011, 2012, 2013 Zimbra Software, LLC.
- *
- * The contents of this file are subject to the Zimbra Public License
- * Version 1.4 ("License"); you may not use this file except in
- * compliance with the License.  You may obtain a copy of the License at
- * http://www.zimbra.com/license.
- *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * Copyright (C) 2008, 2009, 2010, 2011, 2012, 2013, 2014 Zimbra, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
  * ***** END LICENSE BLOCK *****
  */
 package com.zimbra.common.calendar;
@@ -683,9 +685,13 @@ public class ZoneInfo2iCalendar {
                         continue;
                     // Skip some known non data files: *.tab, *.sh and "factory".
                     String name = file.getName();
-                    if (name.endsWith(".tab") || name.endsWith(".sh") || name.endsWith(".awk") ||
+                    if (name.endsWith(".tab") || name.endsWith(".sh") || name.endsWith(".awk") ||  name.endsWith(".pl") ||
                             (name.startsWith(".") || name.endsWith(".swp")) ||  // ignore editor temporary files
-                            name.equalsIgnoreCase("Makefile") || name.equalsIgnoreCase("README") ||
+                            name.equalsIgnoreCase("CONTRIBUTING") ||
+                            name.equalsIgnoreCase("Makefile") || 
+                            name.equalsIgnoreCase("NEWS") ||
+                            name.equalsIgnoreCase("README") ||
+                            name.equalsIgnoreCase("Theory") ||
                             name.equalsIgnoreCase("factory") ||
                             name.equalsIgnoreCase("leap-seconds.list")) {
                         continue;
