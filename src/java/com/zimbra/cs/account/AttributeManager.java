e8a3c4eeaacd58032ad3ef53bccf6426867df74a
Unknown Perforce User <jflanigan>
2014-12-01 16:49:01 -0600

    (split)bug: none
    Add logging to capture initialization errors.
    
    Copied from Perforce
     Change: 530527

--- src/java/com/zimbra/cs/account/AttributeManager.java
+++ src/java/com/zimbra/cs/account/AttributeManager.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -174,7 +174,7 @@ public class AttributeManager {
                         mInstance = (AttributeManager) ExtensionUtil.findClass(className).getDeclaredConstructor(String.class).newInstance(dir);
                     }
                 } catch (Exception e) {
-                    ZimbraLog.account.debug("could not instantiate AttributeManager interface of class '" + className + "'; defaulting to AttributeManager");
+                    ZimbraLog.account.error("could not instantiate AttributeManager interface of class '" + className + "'; defaulting to AttributeManager", e);
                 }
             }
             if (mInstance == null) {
