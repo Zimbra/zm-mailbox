7a1bd09e2787980fc36048f6d4191541e6b2925a
Sanjeev Kumar <sankumar@zimbra.com>
2014-12-28 23:56:03 -0600

    bug: 96883    Moved more attributes from lc to ldap
       All the review comments from review request 3283 are also handled.
       zmupgrade.pm 
           updated zimbraAcl names to zimbraAdminAcl 
           removed zimbraAntiSpamBackupRetention
       zimbra-attrs.xml
           Added description for affectesServices
           Fixed the missing space in description from zimbraImapMaxConsecutiveError.
           Added bytes in description of zimbraImapWriteChunkSize
       LC.java
           Removed commented code.
           AttributeInfo.java 
       Added missing services in enum list of affectsServices.
       //TODO decide all the correct values for affectsServices. Once Jeff gives go head, will make changes as per decision.
       //These are semantic values currently just to give extra information to us. They are not used by any module.
       //So this doesn't impact anything currently.
    
    Testing:
       ant -DskipTests=true runs fine
       mvn test for ZimbraServer runs fine
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=96883
    
    Copied from Perforce
     Change: 532882

--- src/java/com/zimbra/soap/mail/message/WaitSetRequest.java
+++ src/java/com/zimbra/soap/mail/message/WaitSetRequest.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2011, 2012, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -16,10 +16,6 @@
  */
 
 package com.zimbra.soap.mail.message;
-
-import com.google.common.base.Objects;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
 
 import java.util.Collections;
 import java.util.List;
@@ -31,6 +27,9 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.google.common.base.Objects;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.zimbra.common.soap.MailConstants;
 import com.zimbra.soap.type.Id;
 import com.zimbra.soap.type.WaitSetAddSpec;
@@ -59,9 +58,9 @@ import com.zimbra.soap.type.ZmBoolean;
  * zimbra_waitset_default_request_timeout,
  * zimbra_waitset_min_request_timeout,
  * zimbra_waitset_max_request_timeout,
- * zimbra_admin_waitset_default_request_timeout,
- * zimbra_admin_waitset_min_request_timeout, and
- * zimbra_admin_waitset_max_request_timeout
+ * zimbraAdminWaitsetDefaultRequestTimeout,
+ * zimbraAdminWaitsetMinRequestTimeout, and
+ * zimbraAdminWaitsetMaxRequestTimeout
  * </pre>
  * <p>
  * WaitSet: scalable mechanism for listening for changes to one or more accounts
