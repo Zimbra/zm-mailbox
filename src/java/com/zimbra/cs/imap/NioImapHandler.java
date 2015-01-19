001feddbd8346029f6765208c9a693f941ce79f5
Unknown Perforce User <jflanigan>
2015-01-19 15:43:26 -0600

    (split)bug: 97370
    Only unset the log context after the entire NIO message (buffer) has been consumed. 
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=97370
    
    Copied from Perforce
     Change: 534508

--- src/java/com/zimbra/cs/imap/NioImapHandler.java
+++ src/java/com/zimbra/cs/imap/NioImapHandler.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -64,7 +64,6 @@ final class NioImapHandler extends ImapHandler implements NioHandler {
                     dropConnection();
                 }
             } finally {
-                ZimbraLog.clearContext();
                 if (request != null) {
                     request.cleanup();
                     request = null;
