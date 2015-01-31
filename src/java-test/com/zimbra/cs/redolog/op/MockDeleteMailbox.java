407caa7a18e0f9825fc4f383834bbe56a3015770
Unknown Perforce User <jflanigan>
2015-01-30 19:33:51 -0600

    (split)bug: 97610
    New mock class missed on prior checkin.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=97610
    
    Copied from Perforce
     Change: 535809

---
+++ src/java-test/com/zimbra/cs/redolog/op/MockDeleteMailbox.java
@@ -0,0 +1,62 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2015 Zimbra, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.cs.redolog.op;
+
+import java.io.IOException;
+
+import com.zimbra.cs.redolog.RedoLogOutput;
+import com.zimbra.cs.redolog.TransactionId;
+
+/**
+ * Mock redo op for testing. Exposes serverId and other internals for testing purposes
+ *
+ */
+public class MockDeleteMailbox extends DeleteMailbox {
+
+
+    public MockDeleteMailbox() {
+        super();
+    }
+
+    public MockDeleteMailbox(int mailboxId) {
+        super(mailboxId);
+    }
+
+    public void setServerId(String serverId) {
+        this.serverId = serverId;
+    }
+
+    public static String getLocalServerId() {
+        return RedoableOp.LOCAL_SERVER_ID;
+    }
+
+    @Override
+    public void setTransactionId(TransactionId txnId) {
+        super.setTransactionId(txnId);
+    }
+
+    public void serializeOperation(RedoLogOutput out) throws IOException {
+        super.serializeHeader(out);
+        super.serializeData(out);
+    }
+
+    @Override
+    public void setTimestamp(long currentTimeMillis) {
+        super.setTimestamp(currentTimeMillis);
+    }
+}
