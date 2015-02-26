cc09843a39ada8636be1afa9f73775f95b2b3ed6
Unknown Perforce User <drauschenbach>
2015-02-26 11:36:25 -0600

    Bug 96429 - New Consul service locator
    Refine the mailstore soap service health check script by checking the webapp service with a soap request. A new PingRequest is made available in the urn:zimbraAccount ns, that is allowed from unauthenticated localhost callers only. The previous health check used curl to test the root Jetty container, which would have passed even if the mailstore webapp was not deployed to jetty.
    
    Switched the heath check script from bash over to Python and a proper arg parser lib, to avoid having to support a legacy of positional arguments.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=96429
    
    Copied from Perforce
     Change: 538193

---
+++ src/java/com/zimbra/soap/account/message/PingRequest.java
@@ -0,0 +1,38 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2015 Zimbra, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.soap.account.message;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import com.zimbra.common.soap.AdminConstants;
+
+/**
+ * @zm-api-command-auth-required false
+ * @zm-api-command-admin-auth-required false
+ * @zm-api-command-description Ping the service from localhost to perform a basic health check.
+ */
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name=AdminConstants.E_PING_REQUEST)
+@XmlType(propOrder = {})
+public class PingRequest {
+    public PingRequest() {
+    }
+}
