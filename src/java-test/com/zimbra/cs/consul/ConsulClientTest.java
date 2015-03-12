4b4e9ae11b0e5c6a01a59a8751cb846f5f1ceda5
Unknown Perforce User <jflanigan>
2015-03-11 19:22:41 -0500

    bug: 98073
    Add capabilities to ConsulClient for leader election
    - create, destroy sessions
    - elect leader
    - wait for leader change
    - get session info
    - get node info
    
    Register zimbra-redolog service upon startup, and attempt to obtain leadership. Reject POST requests when not leader.
    Use leader/service location to assign url for HttpRedoLogManager.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=98073
    
    Copied from Perforce
     Change: 539305

--- src/java-test/com/zimbra/cs/consul/ConsulClientTest.java
+++ src/java-test/com/zimbra/cs/consul/ConsulClientTest.java
@@ -21,14 +21,20 @@ import java.util.Random;
 
 import junit.framework.Assert;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.RandomStringUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.JavaType;
+import org.junit.After;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.common.net.HostAndPort;
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.consul.NodeInfoResponse.NodeInfo;
+import com.zimbra.cs.consul.ServiceListResponse.Service;
 import com.zimbra.qless.JSON;
 
 /**
@@ -46,14 +52,33 @@ public final class ConsulClientTest {
         }
     }
 
+    private String TEST_SERVICE_PREFIX = "zimbra-consulclienttest-";
+
+    @After
+    public void tearDown() throws Exception {
+        ServiceListResponse list = consulClient.listAgentServices();
+        if (list != null && list.getServicesById() != null) {
+            for (String key : list.getServicesById().keySet()) {
+                if (key.startsWith(TEST_SERVICE_PREFIX)) {
+                    consulClient.agentDeregister(key);
+                }
+            }
+        }
+    }
+
+    private CatalogRegistration.Service createService() {
+        CatalogRegistration.Service service = new CatalogRegistration.Service();
+        service.name = TEST_SERVICE_PREFIX + RandomStringUtils.randomAlphanumeric(10);
+        service.id = service.name + new Random().nextInt(1000);
+        service.port = 8000 + new Random().nextInt(1000);
+        return service;
+    }
+
     @Test
     public void catalogRegisterCRUD() throws IOException {
 
         // Prepare test data
-        CatalogRegistration.Service service = new CatalogRegistration.Service();
-        service.name = RandomStringUtils.randomAlphanumeric(10);
-        service.id = service.name + new Random().nextInt(1000);
-        service.port = 8000 + new Random().nextInt(1000);
+        CatalogRegistration.Service service = createService();
 
         // Register
         consulClient.agentRegister(service);
@@ -93,5 +118,169 @@ public final class ConsulClientTest {
         Assert.assertEquals("Agent alive and reachable", check.output);
         Assert.assertEquals("", check.serviceID);
         Assert.assertEquals("", check.serviceName);
+    }
+
+    @Test
+    public void listServices() throws IOException {
+        CatalogRegistration.Service service = createService();
+        consulClient.agentRegister(service);
+
+        ServiceListResponse list = consulClient.listAgentServices();
+        Assert.assertNotNull(list.getServicesById());
+        Assert.assertTrue(list.getServicesById().size() > 0);
+
+        Assert.assertTrue(list.getServicesById().containsKey(service.id));
+        ServiceListResponse.Service fromResp = list.getServicesById().get(service.id);
+        Assert.assertNotNull(fromResp);
+        Assert.assertEquals(service.id, fromResp.id);
+        Assert.assertEquals(service.name, fromResp.service);
+    }
+
+    @Test
+    public void electLeader() throws IOException {
+        CatalogRegistration.Service service = createService();
+
+        LeaderResponse leader = consulClient.findLeader(service);
+        Assert.assertNull(leader);
+
+        SessionResponse session = consulClient.createSession(service.id, 120, null);
+        Assert.assertNotNull(session);
+        Assert.assertNotNull(session.id);
+
+        String someData = RandomStringUtils.randomAlphanumeric(100);
+
+        boolean isLeader = consulClient.acquireLeadership(service.name, session.id, someData);
+        Assert.assertTrue(isLeader);
+
+        //try again; can't reacquire
+        isLeader = consulClient.acquireLeadership(service.name, session.id, someData);
+        Assert.assertFalse(isLeader);
+
+        leader = consulClient.findLeader(service);
+        Assert.assertNotNull(leader);
+        Assert.assertEquals(session.id, leader.sessionId);
+
+        Assert.assertEquals(someData, new String(Base64.decodeBase64(leader.value), "UTF-8"));
+
+        //now release
+        consulClient.releaseLeadership(service.name, session.id);
+        //and get again; should be non-null, but no session
+        leader = consulClient.findLeader(service);
+        Assert.assertNotNull(leader);
+        Assert.assertNull(leader.sessionId);
+    }
+
+    @Test
+    public void getSessionNodeInfo() throws IOException {
+        CatalogRegistration.Service service = createService();
+        consulClient.agentRegister(service);
+
+        SessionResponse session = consulClient.createSession(service.id, 120, null);
+        Assert.assertNotNull(session);
+        Assert.assertNotNull(session.id);
+
+        SessionResponse info = consulClient.getSessionInfo(session.id);
+        Assert.assertNotNull(info);
+        Assert.assertNotNull(info.id);
+        Assert.assertEquals(session.id, info.id);
+        Assert.assertNotNull(info.nodeName);
+
+        NodeInfoResponse nodeInfoResponse = consulClient.getNodeInfo(info.nodeName);
+        NodeInfo nodeInfo = nodeInfoResponse.node;
+        Assert.assertNotNull(nodeInfo);
+        Assert.assertNotNull(nodeInfo.address);
+        Assert.assertNotNull(nodeInfo.nodeName);
+
+        HostAndPort hp = HostAndPort.fromString(nodeInfo.address);
+        Assert.assertNotNull(hp);
+        Assert.assertNotNull(hp.getHostText());
+
+        ServiceListResponse services = nodeInfoResponse.services;
+        Assert.assertNotNull(services);
+        Assert.assertNotNull(services.getServicesById());
+        Service nodeResponseService = services.getServicesById().get(service.id);
+        Assert.assertNotNull(nodeResponseService);
+        Assert.assertNotNull(nodeResponseService.service);
+        Assert.assertNotNull(nodeResponseService.id);
+    }
+
+    @Test
+    public void createCheckedSession() throws IOException {
+        CatalogRegistration.Service service = createService();
+
+        LeaderResponse leader = consulClient.findLeader(service);
+        Assert.assertNull(leader);
+
+        List<String> checks = new ArrayList<String>();
+        checks.add("serfHealth");
+        SessionResponse session = consulClient.createSession(service.id, 0, checks);
+        Assert.assertNotNull(session);
+        Assert.assertNotNull(session.id);
+        consulClient.deleteSession(session.id);
+    }
+
+    @Test
+    public void ttlSession() throws IOException {
+        CatalogRegistration.Service service = createService();
+
+        SessionResponse session = consulClient.createSession(service.id, 120, null);
+        consulClient.renewSession(session.id);
+    }
+
+    private class FinalStringWrapper {
+        private String value;
+        public FinalStringWrapper(String value) {
+            super();
+            this.value = value;
+        }
+    }
+
+    @Test
+    public void blockForLeaderChange() throws IOException, InterruptedException {
+        CatalogRegistration.Service service = createService();
+
+        SessionResponse session = consulClient.createSession(service.id, 120, null);
+        //and should now be able to acquire again
+        boolean isLeader = consulClient.acquireLeadership(service.name, session.id, "");
+        Assert.assertTrue(isLeader);
+        final FinalStringWrapper finalStringWrapper = new FinalStringWrapper("foo");
+        //other tests - blocking leader change
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    finalStringWrapper.value = consulClient.waitForLeaderChange(service, session.id);
+                } catch (IOException e) {
+                    ZimbraLog.test.error("ioexception waiting for leader", e);
+                }
+            }
+        };
+        t.start();
+
+        //drop leadership; should trigger the change
+        consulClient.releaseLeadership(service.name, session.id);
+        int maxwait = 100000;
+        t.join(maxwait);
+        Assert.assertNull(finalStringWrapper.value);
+        Assert.assertFalse(t.isAlive());
+
+        t = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    finalStringWrapper.value = consulClient.waitForLeaderChange(service, null);
+                } catch (IOException e) {
+                    ZimbraLog.test.error("ioexception waiting for leader", e);
+                }
+            }
+        };
+        t.start();
+
+        //acquire with new session
+        SessionResponse session2 = consulClient.createSession(service.id, 120, null);
+        isLeader = consulClient.acquireLeadership(service.name, session2.id, "");
+        t.join(maxwait);
+        Assert.assertEquals(session2.id, finalStringWrapper.value);
+        Assert.assertFalse(t.isAlive());
     }
 }
