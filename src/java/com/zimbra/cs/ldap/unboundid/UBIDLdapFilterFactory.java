d9325e12675a3feb6daceca4c39752d23e8e745f
Gren Elliot <gren.elliot@zimbra.com>
2015-03-25 11:50:53 -0500

    (split)bug: 91793
    CountObjects slow for Domain Admin.
    If have more than 100 domains, use 3 threads to do the counting concurrently.
    
    getAllDomains now caches the domains as long as the cache is big enough 
    i.e. Localconfig setting ldap_cache_domain_maxsize is larger than the number of domains
    (This is what is recommended at http://wiki.zimbra.com/wiki/OpenLDAP_Performance_Tuning_8.0 - at least if the number of domains is less than 30000).
    
    This speeds up later CountObjects calls - which is useful for Admin Console which does 5 CountObjectsRequests at startup.
    
    Use JAXB in the handler.  Fixed up the JAXB to fit with actual handler behavior - can specify multiple domains.
    
    https://reviewboard.eng.zimbra.com/r/4457/
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=91793
    
    Copied from Perforce
     Change: 540467

--- src/java/com/zimbra/cs/ldap/unboundid/UBIDLdapFilterFactory.java
+++ src/java/com/zimbra/cs/ldap/unboundid/UBIDLdapFilterFactory.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2011, 2012, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -16,6 +16,7 @@
  */
 package com.zimbra.cs.ldap.unboundid;
 
+import java.util.Collection;
 import java.util.List;
 
 import com.google.common.collect.Lists;
@@ -844,6 +845,16 @@ public class UBIDLdapFilterFactory extends ZLdapFilterFactory {
                 Filter.createANDFilter(
                         Filter.createEqualityFilter(Provisioning.A_zimbraId, id),
                         FILTER_ALL_DOMAINS));
+    }
+
+    @Override
+    public ZLdapFilter domainsByIds(Collection<String> ids) {
+        List<Filter> filters = Lists.newArrayList();
+        for (String id : ids) {
+            filters.add(Filter.createEqualityFilter(Provisioning.A_zimbraId, id));
+        }
+        return new UBIDLdapFilter(FilterId.DOMAINS_BY_IDS,
+                Filter.createANDFilter(FILTER_ALL_DOMAINS, Filter.createORFilter(Filter.createORFilter(filters))));
     }
 
     @Override
