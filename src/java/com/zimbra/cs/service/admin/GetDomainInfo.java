714571c6891934ad4f4984407ac0a378a7bd6040
Prashant Surana <psurana@zimbra.com>
2015-04-15 01:46:10 -0500

    (split)bug:95546 Cannot change theme colors via the Admin Console. added following required attributes in GetDomainInfoRequest without admin auth.
                ZAttrProvisioning.A_zimbraSkinBackgroundColor,
                ZAttrProvisioning.A_zimbraSkinForegroundColor,
                ZAttrProvisioning.A_zimbraSkinSecondaryColor,
                ZAttrProvisioning.A_zimbraSkinSelectionColor
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=95546
    
    Copied from Perforce
     Change: 542070

--- src/java/com/zimbra/cs/service/admin/GetDomainInfo.java
+++ src/java/com/zimbra/cs/service/admin/GetDomainInfo.java
@@ -49,7 +49,11 @@ public class GetDomainInfo extends AdminDocumentHandler {
             ZAttrProvisioning.A_zimbraWebClientLogoutURLAllowedUA,
             ZAttrProvisioning.A_zimbraWebClientLogoutURLAllowedIP,
             ZAttrProvisioning.A_zimbraWebClientMaxInputBufferLength,
-            ZAttrProvisioning.A_zimbraWebClientStaySignedInDisabled);
+            ZAttrProvisioning.A_zimbraWebClientStaySignedInDisabled,
+            ZAttrProvisioning.A_zimbraSkinBackgroundColor,
+            ZAttrProvisioning.A_zimbraSkinForegroundColor,
+            ZAttrProvisioning.A_zimbraSkinSecondaryColor,
+            ZAttrProvisioning.A_zimbraSkinSelectionColor);
 
     @Override
     public Element handle(Element request, Map<String, Object> context) throws ServiceException {
