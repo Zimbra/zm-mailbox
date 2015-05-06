d35c7ace1bfcc296af0770ea73319c3639e62b1f
Greg Solovyev <greg@zimbra.com>
2015-05-06 16:22:37 -0500

    (split)bug 97791 more unit tests for share notifications to test scenarions with multiple shares
    https://bugzilla.zimbra.com/show_bug.cgi?id=97791
    
    Copied from Perforce
     Change: 544020

--- src/java/com/zimbra/qa/unittest/TestShareNotifications.java
+++ src/java/com/zimbra/qa/unittest/TestShareNotifications.java
@@ -2,6 +2,7 @@ package com.zimbra.qa.unittest;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.List;
 
@@ -11,7 +12,10 @@ import org.junit.Test;
 
 import com.zimbra.client.ZFolder;
 import com.zimbra.client.ZMailbox;
+import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.util.ProvisioningUtil;
 import com.zimbra.soap.mail.message.FolderActionRequest;
 import com.zimbra.soap.mail.message.FolderActionResponse;
 import com.zimbra.soap.mail.message.GetShareNotificationsRequest;
@@ -29,16 +33,21 @@ import com.zimbra.soap.mail.type.ShareNotificationInfo;
 public class TestShareNotifications  {
     private String SENDER_NAME="sender_TestShareNotifications";
     private String RECIPIENT_NAME="recip_TestShareNotifications";
-    private String CAL_NAME = "cal1_TestShareNotifications";
-    
+    private String CAL_NAME1 = "cal1_TestShareNotifications";
+    private String CAL_NAME2 = "cal2_TestShareNotifications";
+    private String CONTACTS_NAME1 = "contacts1_TestShareNotifications";
+    private boolean originalLCSetting = false;
     @Before
     public void setUp() throws Exception {
+        originalLCSetting = ProvisioningUtil.getServerAttribute(Provisioning.A_zimbraIndexManualCommit, true);
+        Provisioning.getInstance().getLocalServer().setIndexManualCommit(true);
         cleanUp();
     }
     
     @After
     public void tearDown() throws Exception {
         cleanUp();
+        Provisioning.getInstance().getLocalServer().setIndexManualCommit(originalLCSetting);
     }
     
     private void cleanUp() throws Exception {
@@ -51,11 +60,11 @@ public class TestShareNotifications  {
     }
             
     @Test            
-    public void testGetShareNotifications() throws Exception {
+    public void testCalendarShareNotification() throws Exception {
         Account senderAccount = TestUtil.createAccount(SENDER_NAME);
         Account recipientAccount = TestUtil.createAccount(RECIPIENT_NAME);
         ZMailbox mbox = TestUtil.getZMailbox(SENDER_NAME);
-        ZFolder newCal = TestUtil.createFolder(mbox, CAL_NAME, ZFolder.View.appointment);
+        ZFolder newCal = TestUtil.createFolder(mbox, CAL_NAME1, ZFolder.View.appointment);
         String  calendarId = newCal.getId();
         FolderActionSelector action = new FolderActionSelector(calendarId, "grant");
         ActionGrantSelector grant = new ActionGrantSelector("r", "usr");
@@ -70,6 +79,164 @@ public class TestShareNotifications  {
         shareNotificationReq.addEmailAddress(new EmailAddrInfo(recipientAccount.getMail()));
         SendShareNotificationResponse resp = mbox.invokeJaxb(shareNotificationReq);
         assertNotNull("ShareNotificationResponse is null", resp);
+        
+        try {
+            TestUtil.getMailbox(RECIPIENT_NAME).index.waitForIndexing(3000);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+            fail("should not be getting an exception ");
+        }
+        
+        ZMailbox recipientMbox = TestUtil.getZMailbox(RECIPIENT_NAME);
+        GetShareNotificationsRequest getShareNotificationReq = new GetShareNotificationsRequest();
+        GetShareNotificationsResponse sharesResp = recipientMbox.invokeJaxb(getShareNotificationReq);
+        assertNotNull("GetShareNotificationsResponse is null", sharesResp);
+        List<ShareNotificationInfo> shares = sharesResp.getShares();
+        assertTrue("should have exactly one share notification", shares.size() == 1);
+        assertNotNull("share grantor is null", shares.get(0).getGrantor());
+        assertTrue("share grantor is not " + senderAccount.getMail(), senderAccount.getMail().equalsIgnoreCase(shares.get(0).getGrantor().getEmail()));
+    }
+    
+    @Test            
+    public void testMultipleCalendarShareNotifications() throws Exception {
+        Account senderAccount = TestUtil.createAccount(SENDER_NAME);
+        Account recipientAccount = TestUtil.createAccount(RECIPIENT_NAME);
+        ZMailbox mbox = TestUtil.getZMailbox(SENDER_NAME);
+        //create and share the first calendar
+        ZFolder newCal = TestUtil.createFolder(mbox, CAL_NAME1, ZFolder.View.appointment);
+        String  calendarId = newCal.getId();
+        FolderActionSelector action = new FolderActionSelector(calendarId, "grant");
+        ActionGrantSelector grant = new ActionGrantSelector("r", "usr");
+        grant.setDisplayName(recipientAccount.getName());
+        grant.setPassword("");
+        action.setGrant(grant);
+        FolderActionRequest folderActionReq = new FolderActionRequest(action);
+        FolderActionResponse folderActionResp = mbox.invokeJaxb(folderActionReq);
+        assertNotNull("FolderActionResponse is null", folderActionResp);
+        SendShareNotificationRequest shareNotificationReq = new SendShareNotificationRequest();
+        shareNotificationReq.setItem(new com.zimbra.soap.type.Id(calendarId));
+        shareNotificationReq.addEmailAddress(new EmailAddrInfo(recipientAccount.getMail()));
+        SendShareNotificationResponse resp = mbox.invokeJaxb(shareNotificationReq);
+        assertNotNull("ShareNotificationResponse is null", resp);
+        
+        //create and share the second calendar
+        ZFolder newCal2 = TestUtil.createFolder(mbox, CAL_NAME2, ZFolder.View.appointment);
+        String  calendarId2 = newCal2.getId();
+        action = new FolderActionSelector(calendarId2, "grant");
+        grant = new ActionGrantSelector("r", "usr");
+        grant.setDisplayName(recipientAccount.getName());
+        grant.setPassword("");
+        action.setGrant(grant);
+        folderActionReq = new FolderActionRequest(action);
+        folderActionResp = mbox.invokeJaxb(folderActionReq);
+        assertNotNull("FolderActionResponse is null", folderActionResp);
+        shareNotificationReq = new SendShareNotificationRequest();
+        shareNotificationReq.setItem(new com.zimbra.soap.type.Id(calendarId2));
+        shareNotificationReq.addEmailAddress(new EmailAddrInfo(recipientAccount.getMail()));
+        resp = mbox.invokeJaxb(shareNotificationReq);
+        assertNotNull("ShareNotificationResponse is null", resp);
+        
+        try {
+            TestUtil.getMailbox(RECIPIENT_NAME).index.waitForIndexing(3000);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+            fail("should not be getting an exception ");
+        }
+        
+        ZMailbox recipientMbox = TestUtil.getZMailbox(RECIPIENT_NAME);
+        GetShareNotificationsRequest getShareNotificationReq = new GetShareNotificationsRequest();
+        GetShareNotificationsResponse sharesResp = recipientMbox.invokeJaxb(getShareNotificationReq);
+        assertNotNull("GetShareNotificationsResponse is null", sharesResp);
+        List<ShareNotificationInfo> shares = sharesResp.getShares();
+        assertTrue("should have exactly two share notification", shares.size() == 2);
+        assertNotNull("share grantor is null", shares.get(0).getGrantor());
+        assertTrue("share grantor is not " + senderAccount.getMail(), senderAccount.getMail().equalsIgnoreCase(shares.get(0).getGrantor().getEmail()));
+    }
+    
+    @Test            
+    public void testContactAndCalendarShareNotifications() throws Exception {
+        Account senderAccount = TestUtil.createAccount(SENDER_NAME);
+        Account recipientAccount = TestUtil.createAccount(RECIPIENT_NAME);
+        ZMailbox mbox = TestUtil.getZMailbox(SENDER_NAME);
+        //create and share the first calendar
+        ZFolder newCal = TestUtil.createFolder(mbox, CAL_NAME1, ZFolder.View.appointment);
+        String  calendarId = newCal.getId();
+        FolderActionSelector action = new FolderActionSelector(calendarId, "grant");
+        ActionGrantSelector grant = new ActionGrantSelector("r", "usr");
+        grant.setDisplayName(recipientAccount.getName());
+        grant.setPassword("");
+        action.setGrant(grant);
+        FolderActionRequest folderActionReq = new FolderActionRequest(action);
+        FolderActionResponse folderActionResp = mbox.invokeJaxb(folderActionReq);
+        assertNotNull("FolderActionResponse is null", folderActionResp);
+        SendShareNotificationRequest shareNotificationReq = new SendShareNotificationRequest();
+        shareNotificationReq.setItem(new com.zimbra.soap.type.Id(calendarId));
+        shareNotificationReq.addEmailAddress(new EmailAddrInfo(recipientAccount.getMail()));
+        SendShareNotificationResponse resp = mbox.invokeJaxb(shareNotificationReq);
+        assertNotNull("ShareNotificationResponse is null", resp);
+        
+        //create and share the second calendar
+        ZFolder newContactsFolder = TestUtil.createFolder(mbox, CONTACTS_NAME1, ZFolder.View.contact);
+        String  contactsFolderId = newContactsFolder.getId();
+        action = new FolderActionSelector(contactsFolderId, "grant");
+        grant = new ActionGrantSelector("r", "usr");
+        grant.setDisplayName(recipientAccount.getName());
+        grant.setPassword("");
+        action.setGrant(grant);
+        folderActionReq = new FolderActionRequest(action);
+        folderActionResp = mbox.invokeJaxb(folderActionReq);
+        assertNotNull("FolderActionResponse is null", folderActionResp);
+        shareNotificationReq = new SendShareNotificationRequest();
+        shareNotificationReq.setItem(new com.zimbra.soap.type.Id(contactsFolderId));
+        shareNotificationReq.addEmailAddress(new EmailAddrInfo(recipientAccount.getMail()));
+        resp = mbox.invokeJaxb(shareNotificationReq);
+        assertNotNull("ShareNotificationResponse is null", resp);
+        
+        try {
+            TestUtil.getMailbox(RECIPIENT_NAME).index.waitForIndexing(3000);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+            fail("should not be getting an exception ");
+        }
+        
+        ZMailbox recipientMbox = TestUtil.getZMailbox(RECIPIENT_NAME);
+        GetShareNotificationsRequest getShareNotificationReq = new GetShareNotificationsRequest();
+        GetShareNotificationsResponse sharesResp = recipientMbox.invokeJaxb(getShareNotificationReq);
+        assertNotNull("GetShareNotificationsResponse is null", sharesResp);
+        List<ShareNotificationInfo> shares = sharesResp.getShares();
+        assertTrue("should have exactly two share notification", shares.size() == 2);
+        assertNotNull("share grantor is null", shares.get(0).getGrantor());
+        assertTrue("share grantor is not " + senderAccount.getMail(), senderAccount.getMail().equalsIgnoreCase(shares.get(0).getGrantor().getEmail()));
+    }
+    
+    @Test            
+    public void testContactShareNotification() throws Exception {
+        Account senderAccount = TestUtil.createAccount(SENDER_NAME);
+        Account recipientAccount = TestUtil.createAccount(RECIPIENT_NAME);
+        ZMailbox mbox = TestUtil.getZMailbox(SENDER_NAME);
+        //create and share the first calendar
+        ZFolder newContactsFolder = TestUtil.createFolder(mbox, CAL_NAME1, ZFolder.View.contact);
+        String  contactsFolderId = newContactsFolder.getId();
+        FolderActionSelector action = new FolderActionSelector(contactsFolderId, "grant");
+        ActionGrantSelector grant = new ActionGrantSelector("r", "usr");
+        grant.setDisplayName(recipientAccount.getName());
+        grant.setPassword("");
+        action.setGrant(grant);
+        FolderActionRequest folderActionReq = new FolderActionRequest(action);
+        FolderActionResponse folderActionResp = mbox.invokeJaxb(folderActionReq);
+        assertNotNull("FolderActionResponse is null", folderActionResp);
+        SendShareNotificationRequest shareNotificationReq = new SendShareNotificationRequest();
+        shareNotificationReq.setItem(new com.zimbra.soap.type.Id(contactsFolderId));
+        shareNotificationReq.addEmailAddress(new EmailAddrInfo(recipientAccount.getMail()));
+        SendShareNotificationResponse resp = mbox.invokeJaxb(shareNotificationReq);
+        assertNotNull("ShareNotificationResponse is null", resp);
+    
+        try {
+            TestUtil.getMailbox(RECIPIENT_NAME).index.waitForIndexing(3000);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+            fail("should not be getting an exception ");
+        }
         
         ZMailbox recipientMbox = TestUtil.getZMailbox(RECIPIENT_NAME);
         GetShareNotificationsRequest getShareNotificationReq = new GetShareNotificationsRequest();
