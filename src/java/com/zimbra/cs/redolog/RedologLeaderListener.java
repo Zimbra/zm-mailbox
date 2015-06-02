3debf4549015d20d80ca83e16f03a5d65c1155bc
Unknown Perforce User <jflanigan>
2015-06-02 15:13:06 -0500

    (split)bug: 99772
    Suppress logging for repeated exceptions; if cause is same as previous only output at debug level. Backoff retries starting with 100ms pause up to 32000ms pause (exposed in DebugConfig.redologListenerBackoffMin/Max in case timing needs further tweaks). Reduce log level for HttpMethodDirector since it outputs a few lines for each connection refused.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=99772
    
    Copied from Perforce
     Change: 546355

--- src/java/com/zimbra/cs/redolog/RedologLeaderListener.java
+++ src/java/com/zimbra/cs/redolog/RedologLeaderListener.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang.StringUtils;
 
 import com.zimbra.common.consul.CatalogRegistration.Service;
 import com.zimbra.common.consul.ConsulClient;
+import com.zimbra.common.localconfig.DebugConfig;
 import com.zimbra.common.servicelocator.ServiceLocator;
 import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.redolog.LeaderChangeListener.LeaderStateChange;
@@ -73,6 +74,9 @@ public class RedologLeaderListener {
         stopped = true;
     }
 
+    private static final long MAX_BACKOFF = DebugConfig.redologListenerBackoffMax;
+    private static final long MIN_BACKOFF = DebugConfig.redologListenerBackoffMin;
+
     public void init() {
         consulClient = Zimbra.getAppContext().getBean(ConsulClient.class);
         serviceLocator = Zimbra.getAppContext().getBean(ServiceLocator.class);
@@ -80,6 +84,8 @@ public class RedologLeaderListener {
         Thread t = new Thread("redolog-leader-listener") {
             @Override
             public void run() {
+                String lastExceptionMsg = null;
+                long backoffTime = MIN_BACKOFF;
                 while (!stopped) {
                     try {
                         String sessionIdBefore = getLeaderSessionId();
@@ -105,8 +111,24 @@ public class RedologLeaderListener {
                                 }
                             }
                         }
+                        backoffTime = MIN_BACKOFF;
                     } catch (Exception e) {
-                        ZimbraLog.redolog.warn("Exception waiting for leader change", e);
+                        //only log warning for new exceptions, to avoid flooding log
+                        if ((lastExceptionMsg == null && e.getMessage() != null) || (lastExceptionMsg != null && !lastExceptionMsg.equals(e.getMessage()))) {
+                            ZimbraLog.redolog.warn("Exception waiting for leader change", e);
+                            lastExceptionMsg = e.getMessage();
+                        } else {
+                            ZimbraLog.redolog.debug("Exception waiting for leader change", e);
+                        }
+                        //pause before retry
+                        try {
+                            ZimbraLog.redolog.debug("pausing %dms before retry", backoffTime);
+                            Thread.sleep(backoffTime);
+                            if (backoffTime < MAX_BACKOFF) {
+                                backoffTime = Math.min(MAX_BACKOFF, backoffTime * 2);
+                            }
+                        } catch (InterruptedException ie) {
+                        }
                     }
                 }
             }
