67a4b4442fe964d943c171e13f1b949b99f3db72
Unknown Perforce User <jflanigan>
2015-06-22 16:36:41 -0500

    (split)bug: 99671
    Invalidate cached properties on failure, so they do not remain in subsequent requests.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=99671
    
    Copied from Perforce
     Change: 548381

--- src/java/com/zimbra/cs/zimlet/ZimletUserProperties.java
+++ src/java/com/zimbra/cs/zimlet/ZimletUserProperties.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2005, 2006, 2007, 2009, 2010, 2011, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -83,7 +83,12 @@ public final class ZimletUserProperties {
         for (Prop prop: properties) {
             props[index++] = prop.getSerialization();
         }
-        Provisioning.getInstance().modifyAttrs(account,
+        try {
+            Provisioning.getInstance().modifyAttrs(account,
                 Collections.singletonMap(Provisioning.A_zimbraZimletUserProperties, props));
+        } catch (ServiceException se) {
+            CACHE.invalidate(account.getId());
+            throw se;
+        }
     }
 }
