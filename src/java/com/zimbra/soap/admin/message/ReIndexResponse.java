dd2f5f28173422588800c6ea754056d5cb917fa6
Greg Solovyev <greg@zimbra.com>
2015-06-26 13:24:09 -0500

    bug 95536 - implement reindexing mail items chronologically by day (from newest to oldest) - add ability to trigger parallel reindexing of multiple mailboxes - add tests for new reindexing functionality - add LDAP attributes for controlling timeouts and wait times that are used when talking to shared indexing queue and Solr - do not automatically kick off reindexing of a mailbox during in-place upgrade. This should be done by a separate call that will trigger parallel reindexing of all mailboxes after an upgrade  - when restoring a pre-KISS mailbox, kick off reindexing unless skipSearchIndex flag is specified  - when not using solr cloud, use one thread for inxing service. Using multiple threads against single Solr index running on the same machine does not speed up indexing.  - reIndexById does not need to be recorded in Redolog, because indexing retries are handled by redis-backed indexing queue adapter  - remove indexing code that is no longer being used
    https://bugzilla.zimbra.com/show_bug.cgi?id=95536
    
    Copied from Perforce
     Change: 548910

--- src/java/com/zimbra/soap/admin/message/ReIndexResponse.java
+++ src/java/com/zimbra/soap/admin/message/ReIndexResponse.java
@@ -17,32 +17,22 @@
 
 package com.zimbra.soap.admin.message;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.google.common.collect.Iterables;
 import com.zimbra.common.soap.AdminConstants;
 import com.zimbra.soap.admin.type.ReindexProgressInfo;
 
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlRootElement(name = AdminConstants.E_REINDEX_RESPONSE)
 public class ReIndexResponse {
-
-    /**
-     * @zm-api-field-tag statusCode
-     * @zm-api-field-description Status code - one of:
-      <ul> 
-      <li>-3 - re-indexing failed</li>
-      <li>-2 - re-indexing was interrupted, because indexing queue is full</li>
-      <li>-1 - re-indexing was aborted with a "cancel" action </li>
-      <li>0 - re-indexing is not running/has not been started</li>
-      <li>1 - re-indexing is actively running</li>
-      <li>2 - re-indexing task is complete</li> 
-     */
-    @XmlAttribute(name = AdminConstants.A_STATUS_CODE, required = true)
-    private final Integer statusCode;
 
     /**
      * @zm-api-field-tag status
@@ -53,35 +43,40 @@ public class ReIndexResponse {
     private final String status;
 
     /**
-     * @zm-api-field-description Information about reindexing progress
+     * @zm-api-field-description Aggregate re-indexing progress for legacy SOAP clients
      */
     @XmlElement(name = AdminConstants.E_PROGRESS, required = false)
     private final ReindexProgressInfo progress;
+
+    /**
+     * @zm-api-field-tag mbox
+     * @zm-api-field-description Per mailbox re-indexing progress
+     */
+    @XmlElement(name = AdminConstants.E_MAILBOX, required = false)
+    private List<ReindexProgressInfo> mbox;
 
     /**
      * no-argument constructor wanted by JAXB
      */
     @SuppressWarnings("unused")
     private ReIndexResponse() {
-        this((Integer) null, (String) null, (ReindexProgressInfo) null);
+        this((String) null, (ReindexProgressInfo) null);
     }
 
-    public ReIndexResponse(Integer statusCode) {
-        this(statusCode, (String) null, (ReindexProgressInfo) null);
+    public ReIndexResponse(String status) {
+        this(status, (ReindexProgressInfo) null);
     }
 
-    public ReIndexResponse(Integer statusCode, String status) {
-        this(statusCode, status, (ReindexProgressInfo) null);
-    }
-
-    public ReIndexResponse(Integer statusCode, String status, ReindexProgressInfo progress) {
-        this.statusCode = statusCode;
+    public ReIndexResponse(String status, ReindexProgressInfo progress) {
         this.status = status;
         this.progress = progress;
+        this.mbox = new ArrayList<ReindexProgressInfo>();
     }
 
-    public Integer getStatusCode() {
-        return statusCode;
+    public ReIndexResponse(String status, ReindexProgressInfo progress, List<ReindexProgressInfo> mailboxes) {
+        this.status = status;
+        this.progress = progress;
+        this.mbox = mailboxes;
     }
 
     public String getStatus() {
@@ -90,5 +85,22 @@ public class ReIndexResponse {
 
     public ReindexProgressInfo getProgress() {
         return progress;
+    }
+
+    public void addMbox(ReindexProgressInfo mailboxProgress) {
+        this.mbox.add(mailboxProgress);
+    }
+
+    public void setMbox(Iterable<ReindexProgressInfo> mailboxes) {
+        this.mbox.clear();
+        if(mailboxes != null) {
+            if(mailboxes != null) {
+                Iterables.addAll(this.mbox, mailboxes);
+            }
+        }
+    }
+
+    public List<ReindexProgressInfo> getMbox() {
+        return this.mbox;
     }
 }
