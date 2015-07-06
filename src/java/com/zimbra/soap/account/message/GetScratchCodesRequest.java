09a42bc984e4e9e88eec20d6ed734dc6e53d3e87
Ilya Raykin <raykini@zimbra.com>
2015-07-06 08:29:59 -0500

    Bug: 99603
    New GetScratchCodesRequest endpoint that returns all currently available scratch codes for the account.
    
    GetScratchCodes.java:
     - SOAP endpoint
    
    GetScratchCodesRequest/Response.java:
     - JAXB definitions
    
    AccountConstants.java:
     - new constants
    
    AccountService.java:
     - registered new endpoint
    
    JaxbUtil.java:
     - registered new JAXB classes
    
    TestTwoFactorAuth.java:
     - new unit test for getting scratch codes
    
    soap.txt:
     - new endpoint documentation
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=99603
    
    Copied from Perforce
     Change: 549607

---
+++ src/java/com/zimbra/soap/account/message/GetScratchCodesRequest.java
@@ -0,0 +1,10 @@
+package com.zimbra.soap.account.message;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.zimbra.common.soap.AccountConstants;
+
+@XmlRootElement(name=AccountConstants.E_GET_SCRATCH_CODES_REQUEST)
+public class GetScratchCodesRequest {
+
+}
