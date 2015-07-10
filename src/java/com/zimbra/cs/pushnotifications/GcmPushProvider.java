016c4e892d6f22df907a9f8b1f1934f98c6e442e
Rohan Ambasta <rambasta@zimbra.com>
2015-07-10 02:37:24 -0500

    Bug: 100122 - ZMG: Enable OAuth for SMTP for external data source
    Merging using JUDASPRIEST_to_main
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=100122
    
    Copied from Perforce
     Change: 550224

--- src/java/com/zimbra/cs/pushnotifications/GcmPushProvider.java
+++ src/java/com/zimbra/cs/pushnotifications/GcmPushProvider.java
@@ -26,6 +26,7 @@ import org.apache.commons.httpclient.methods.StringRequestEntity;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.zimbra.common.httpclient.HttpClientUtil;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.util.StringUtil;
 import com.zimbra.common.util.ZimbraHttpConnectionManager;
--- src/java/com/zimbra/cs/pushnotifications/PushNotificationListener.java
+++ src/java/com/zimbra/cs/pushnotifications/PushNotificationListener.java
@@ -36,7 +36,7 @@ import com.zimbra.cs.session.PendingModifications;
 import com.zimbra.cs.session.PendingModifications.Change;
 import com.zimbra.cs.session.PendingModifications.ModificationKey;
 
-public class PushNotificationListener extends MailboxListener {
+public class PushNotificationListener implements MailboxListener {
 
     public static final ImmutableSet<MailboxOperation> EVENTS = ImmutableSet.of(
         MailboxOperation.MoveItem, MailboxOperation.RenameItem, MailboxOperation.RenameItemPath,
@@ -46,7 +46,7 @@ public class PushNotificationListener extends MailboxListener {
         MailItem.Type.FOLDER, MailItem.Type.MESSAGE);
 
     @Override
-    public Set<MailItem.Type> registerForItemTypes() {
+    public Set<MailItem.Type> notifyForItemTypes() {
         return ITEMTYPES;
     }
 
