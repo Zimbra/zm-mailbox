1ba9269600c5b7b7de2067ee10ede085e5593890
Gren Elliot <gren.elliot@zimbra.com>
2015-08-06 04:08:25 -0500

    Bug:99825
    Whether public shares are visible outside a domain should be configurable.
    Introduced new LC key public_share_advertising_scope
    This setting affects the behavior of the account namespace SOAP GetShareInfoRequest when an owner is not specified in the request.
    Possible values:
           "samePrimaryDomain" - Accounts can only see public shares advertised by accounts whose primary domain
                                 name matches the primary domain name of the requesting account.
           "all"               - Accounts with this set can only see public shares advertised by any accounts.
           "none"              - Accounts with this set cannot get details of any public shares.
    Note that this doesn't prevent mounting of shares which are discovered out of band (e.g. via invitation messages).  Shares which are already in use will also be reported.
    
    Changed GetShareInfo handler to used JAXB.
    https://reviewboard.eng.zimbra.com/r/5788/
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=99825
    
    Copied from Perforce
     Change: 552160

--- src/java/com/zimbra/soap/type/ShareInfo.java
+++ src/java/com/zimbra/soap/type/ShareInfo.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2011, 2012, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -21,10 +21,14 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 
+import org.codehaus.jackson.annotate.JsonPropertyOrder;
+
 import com.zimbra.common.soap.AccountConstants;
 import com.zimbra.common.soap.MailConstants;
 
 @XmlAccessorType(XmlAccessType.NONE)
+@JsonPropertyOrder({ "ownerId", "ownerEmail", "ownerName", "folderId", "folderUuid", "folderPath", "view", "rights",
+    "granteeType", "granteeId", "granteeName", "granteeDisplayName", "mid" })
 public class ShareInfo {
 
     /**
