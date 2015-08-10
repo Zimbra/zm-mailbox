41ed3f380c3f8d5fcd8de07dd1591187b9e7056c
Unknown Perforce User <jflanigan>
2015-08-10 13:56:48 -0500

    Integrate change 552500 from main
    bug: 100679
    
    Add zimlet rewrite to zmtlsctl which is called by zmmailboxdctl.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=100679
    
    Copied from Perforce
     Change: 552501

--- src/java/com/zimbra/cs/pushnotifications/filters/MessageFileIntoFilter.java
+++ src/java/com/zimbra/cs/pushnotifications/filters/MessageFileIntoFilter.java
@@ -49,7 +49,7 @@ public class MessageFileIntoFilter implements Filter {
 
     /*
      * (non-Javadoc)
-     *
+     * 
      * @see com.zimbra.cs.pushnotifications.filters.Filter#apply()
      */
     @Override
