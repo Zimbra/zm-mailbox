8ba92773a8c91bc1f3163d1dd9211940251f1698
Ilya Raykin <raykini@zimbra.com>
2015-08-24 09:51:51 -0500

    (split)Bug: 98633
    TwoFactorAuthStatus attribute callback now takes into account the other attribute changes that are happening in the same ModifyAccount call, allowing multiple 2FA attributes to be changed without a conflict error being thrown.
    
    This should also fix bug 98632.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=98633
    
    Copied from Perforce
     Change: 553569

--- src/java/com/zimbra/cs/account/callback/TwoFactorAuthStatus.java
+++ src/java/com/zimbra/cs/account/callback/TwoFactorAuthStatus.java
@@ -2,6 +2,7 @@ package com.zimbra.cs.account.callback;
 
 import java.util.Map;
 
+import com.zimbra.common.account.ProvisioningConstants;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.AttributeCallback;
@@ -16,28 +17,61 @@ public class TwoFactorAuthStatus extends AttributeCallback {
         boolean setting = ((String) attrValue).equalsIgnoreCase("true");
         if (entry instanceof Account) {
             Account account = (Account) entry;
-            if (attrName.equals(Provisioning.A_zimbraFeatureTwoFactorAuthRequired) && !account.isFeatureTwoFactorAuthAvailable()
+            if (attrName.equals(Provisioning.A_zimbraFeatureTwoFactorAuthRequired) && !is2faAvailable(account, attrsToModify)
                     && setting) {
                 throw ServiceException.FAILURE("cannot make two-factor auth required because is not available on this account", null);
-            } else if (attrName.equals(Provisioning.A_zimbraFeatureTwoFactorAuthAvailable) && account.isFeatureTwoFactorAuthRequired()
+            } else if (attrName.equals(Provisioning.A_zimbraFeatureTwoFactorAuthAvailable) && is2faRequired(account, attrsToModify)
                     && !setting) {
                 throw ServiceException.FAILURE("cannot make two-factor auth unavailable because it is currently required on the account", null);
-            } else if (attrName.equals(Provisioning.A_zimbraTwoFactorAuthEnabled) && !account.isFeatureTwoFactorAuthAvailable() &&
-                    setting) {
+            } else if (attrName.equals(Provisioning.A_zimbraTwoFactorAuthEnabled) && !is2faAvailable(account, attrsToModify)
+                    && setting) {
                 throw ServiceException.FAILURE("cannot enable two-factor auth because it is not available on this account", null);
             }
         } else if (entry instanceof Cos) {
             Cos cos = (Cos) entry;
-            if (attrName.equals(Provisioning.A_zimbraFeatureTwoFactorAuthRequired) && !cos.isFeatureTwoFactorAuthAvailable()
+            if (attrName.equals(Provisioning.A_zimbraFeatureTwoFactorAuthRequired) && !is2faAvailable(cos, attrsToModify)
                     && setting) {
                 throw ServiceException.FAILURE("cannot make two-factor auth required because it is not available on this COS", null);
-            } else if (attrName.equals(Provisioning.A_zimbraFeatureTwoFactorAuthAvailable) && cos.isFeatureTwoFactorAuthRequired()
+            } else if (attrName.equals(Provisioning.A_zimbraFeatureTwoFactorAuthAvailable) && is2faRequired(cos, attrsToModify)
                     && !setting) {
                 throw ServiceException.FAILURE("cannot make two-factor auth unavailable because it is currently required on the COS", null);
             } else if (attrName.equals(Provisioning.A_zimbraFeatureTwoFactorAuthAvailable) && setting) {
                 cos.unsetTwoFactorAuthLastReset();
             }
         }
+    }
+
+    private boolean setting2faAttr(Map attrs, String attr, String value) {
+        String attrValue = (String) attrs.get(Provisioning.A_zimbraFeatureTwoFactorAuthAvailable);
+        return attrValue != null ? attrValue.equals(value) : false;
+    }
+
+    private boolean is2faAvailable(Account account, Map attrs) {
+        boolean alreadyAvailable = account.isFeatureTwoFactorAuthAvailable();
+        boolean settingNow = setting2faAttr(attrs, Provisioning.A_zimbraFeatureTwoFactorAuthAvailable, ProvisioningConstants.TRUE);
+        boolean unsettingNow = setting2faAttr(attrs, Provisioning.A_zimbraFeatureTwoFactorAuthAvailable, ProvisioningConstants.FALSE);
+        return alreadyAvailable ? !unsettingNow : settingNow;
+    }
+
+    private boolean is2faAvailable(Cos cos, Map attrs) {
+        boolean alreadyAvailable = cos.isFeatureTwoFactorAuthAvailable();
+        boolean settingNow = setting2faAttr(attrs, Provisioning.A_zimbraFeatureTwoFactorAuthAvailable, ProvisioningConstants.TRUE);
+        boolean unsettingNow = setting2faAttr(attrs, Provisioning.A_zimbraFeatureTwoFactorAuthAvailable, ProvisioningConstants.FALSE);
+        return alreadyAvailable ? !unsettingNow : settingNow;
+    }
+
+    private boolean is2faRequired(Account account, Map attrs) {
+        boolean alreadyRequired = account.isFeatureTwoFactorAuthRequired();
+        boolean settingNow = setting2faAttr(attrs, Provisioning.A_zimbraFeatureTwoFactorAuthRequired, ProvisioningConstants.TRUE);
+        boolean unsettingNow = setting2faAttr(attrs, Provisioning.A_zimbraFeatureTwoFactorAuthRequired, ProvisioningConstants.FALSE);
+        return alreadyRequired ? !unsettingNow : settingNow;
+    }
+
+    private boolean is2faRequired(Cos cos, Map attrs) {
+        boolean alreadyRequired = cos.isFeatureTwoFactorAuthRequired();
+        boolean settingNow = setting2faAttr(attrs, Provisioning.A_zimbraFeatureTwoFactorAuthRequired, ProvisioningConstants.TRUE);
+        boolean unsettingNow = setting2faAttr(attrs, Provisioning.A_zimbraFeatureTwoFactorAuthRequired, ProvisioningConstants.FALSE);
+        return alreadyRequired ? !unsettingNow : settingNow;
     }
 
     @Override
