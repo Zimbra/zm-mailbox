40cff6dfb319eda13a5b7084d9965e9743f1180a
Gren Elliot <gren.elliot@zimbra.com>
2015-09-26 09:33:18 -0500

    (split)Bug: 100965
    New delegated admin can not edit account on same server till the cache get flushed
    Refactored FlushCache to use JAXB and for much of the functionality to be usable from other handlers.
    If a domain is the target of a GrantRight then now flush the cache entry for that on all servers.  Had thought that the cache that needed flushing was the "acl" cache but testing suggests that it is actually the domain cached entry that is the problem.
    
    Removed unused flushChacheOnServer introduced in change 153737 for Bug 35205 but never actually called by anything
    
    https://reviewboard.eng.zimbra.com/r/6261/
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=100965
    
    Copied from Perforce
     Change: 555817

--- src/java/com/zimbra/cs/service/admin/FlushCache.java
+++ src/java/com/zimbra/cs/service/admin/FlushCache.java
@@ -16,7 +16,6 @@
  */
 package com.zimbra.cs.service.admin;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -26,13 +25,10 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-import com.zimbra.common.account.Key;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.AdminConstants;
 import com.zimbra.common.soap.Element;
-import com.zimbra.common.soap.Element.XMLElement;
 import com.zimbra.common.soap.SoapHttpTransport;
-import com.zimbra.common.util.FileUtil;
 import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.account.CacheExtension;
 import com.zimbra.cs.account.Provisioning;
@@ -47,9 +43,14 @@ import com.zimbra.cs.util.SkinUtil;
 import com.zimbra.cs.util.WebClientL10nUtil;
 import com.zimbra.cs.util.WebClientServiceUtil;
 import com.zimbra.cs.zimlet.ZimletUtil;
+import com.zimbra.soap.JaxbUtil;
 import com.zimbra.soap.SoapServlet;
 import com.zimbra.soap.ZimbraSoapContext;
+import com.zimbra.soap.admin.message.FlushCacheRequest;
+import com.zimbra.soap.admin.message.FlushCacheResponse;
+import com.zimbra.soap.admin.type.CacheEntrySelector;
 import com.zimbra.soap.admin.type.CacheEntryType;
+import com.zimbra.soap.admin.type.CacheSelector;
 
 public class FlushCache extends AdminDocumentHandler {
 
@@ -68,24 +69,26 @@ public class FlushCache extends AdminDocumentHandler {
     @Override
     public Element handle(Element request, Map<String, Object> context) throws ServiceException {
         ZimbraSoapContext zsc = getZimbraSoapContext(context);
+        FlushCacheRequest req = JaxbUtil.elementToJaxb(request);
+        doFlushCache(this, context, req);
+        return zsc.jaxbToElement(new FlushCacheResponse());
+    }
 
+    public static void doFlushCache(AdminDocumentHandler handler, Map<String, Object> context, FlushCacheRequest req)
+    throws ServiceException {
+        ZimbraSoapContext zsc = getZimbraSoapContext(context);
         Server localServer = Provisioning.getInstance().getLocalServer();
-        checkRight(zsc, context, localServer, Admin.R_flushCache);
+        handler.checkRight(zsc, context, localServer, Admin.R_flushCache);
 
-        Element eCache = request.getElement(AdminConstants.E_CACHE);
-        String typeStr = eCache.getAttribute(AdminConstants.A_TYPE);
-        boolean allServers = eCache.getAttributeBool(AdminConstants.A_ALLSERVERS, false);
-
-        String[] types = typeStr.split(",");
-
+        CacheSelector cacheSelector = req.getCache();
+        boolean allServers = cacheSelector.isAllServers();
+        String[] types = cacheSelector.getTypes().split(",");
         for (String type : types) {
             CacheEntryType cacheType = null;
-
             try {
                 cacheType = CacheEntryType.fromString(type);
-                doFlush(context, cacheType, eCache);
+                doFlush(context, cacheType, cacheSelector);
             } catch (ServiceException e) {
-
                 if (cacheType == null) {
                     // see if it is a registered extension
                     CacheExtension ce = CacheExtension.getHandler(type);
@@ -99,82 +102,79 @@ public class FlushCache extends AdminDocumentHandler {
                 }
             }
         }
-
         if (allServers) {
-            flushCacheOnAllServers(zsc, request);
+            flushCacheOnAllServers(zsc, req);
         }
-
-        Element response = zsc.createElement(AdminConstants.FLUSH_CACHE_RESPONSE);
-        return response;
     }
 
-    private void doFlush(Map<String, Object> context, CacheEntryType cacheType, Element eCache) throws ServiceException {
+    private static void doFlush(Map<String, Object> context, CacheEntryType cacheType, CacheSelector cacheSelector)
+    throws ServiceException {
 
-		String mailURL = Provisioning.getInstance().getLocalServer().getMailURL();
-		switch (cacheType) {
-			case acl:
-				PermissionCache.invalidateCache();
-				break;
-			case all:
-				flushLdapCache(cacheType, eCache);
-				Provisioning.getInstance().refreshValidators(); // refresh other bits of cached license data
-				break;
-			case galgroup:
-				GalGroup.flushCache(getCacheEntries(eCache));
-				break;
-			case uistrings:
-                if (WebClientServiceUtil.isServerInSplitMode()) {
-                    WebClientServiceUtil.flushUistringsCache();
-                } else {
-                    FlushCache.sendFlushRequest(context, mailURL, RES_AJXMSG_JS);
-                    FlushCache.sendFlushRequest(context, "/zimbraAdmin", RES_AJXMSG_JS);
-                }
-				break;
-			case skin:
-                SkinUtil.flushCache();
-                if (!WebClientServiceUtil.isServerInSplitMode()) {
-                    FlushCache.sendFlushRequest(context, mailURL, JS_SKIN_JS);
-                }
-				break;
-			case locale:
-                WebClientL10nUtil.flushCache();
-				break;
-			case license:
-				flushLdapCache(CacheEntryType.config, eCache); // refresh global config for parsed license
-				Provisioning.getInstance().refreshValidators(); // refresh other bits of cached license data
-				break;
-			case zimlet:
-			    ZimletUtil.flushDiskCache(context);
-                if (!WebClientServiceUtil.isServerInSplitMode()) {
-                    flushAllZimlets(context);
-                }
-				// fall through to also flush ldap entries
-			default:
-				flushLdapCache(cacheType, eCache);
-		}
-	}
+        String mailURL = Provisioning.getInstance().getLocalServer().getMailURL();
+        switch (cacheType) {
+        case acl:
+            PermissionCache.invalidateCache();
+            break;
+        case all:
+            flushLdapCache(cacheType, cacheSelector);
+            Provisioning.getInstance().refreshValidators(); // refresh other bits of cached license data
+            break;
+        case galgroup:
+            GalGroup.flushCache(getCacheEntries(cacheSelector));
+            break;
+        case uistrings:
+            if (WebClientServiceUtil.isServerInSplitMode()) {
+                WebClientServiceUtil.flushUistringsCache();
+            } else {
+                FlushCache.sendFlushRequest(context, mailURL, RES_AJXMSG_JS);
+                FlushCache.sendFlushRequest(context, "/zimbraAdmin", RES_AJXMSG_JS);
+            }
+            break;
+        case skin:
+            SkinUtil.flushCache();
+            if (!WebClientServiceUtil.isServerInSplitMode()) {
+                FlushCache.sendFlushRequest(context, mailURL, JS_SKIN_JS);
+            }
+            break;
+        case locale:
+            WebClientL10nUtil.flushCache();
+            break;
+        case license:
+            flushLdapCache(CacheEntryType.config, cacheSelector); // refresh global config for parsed license
+            Provisioning.getInstance().refreshValidators(); // refresh other bits of cached license data
+            break;
+        case zimlet:
+            ZimletUtil.flushDiskCache(context);
+            if (!WebClientServiceUtil.isServerInSplitMode()) {
+                flushAllZimlets(context);
+            }
+            // fall through to also flush ldap entries
+        default:
+            flushLdapCache(cacheType, cacheSelector);
+        }
+    }
 
     public static void flushAllZimlets(Map<String, Object> context) {
         FlushCache.sendFlushRequest(context, "/service", "/zimlet/res/all.js");
     }
 
-    private CacheEntry[] getCacheEntries(Element eCache) throws ServiceException {
-        List<Element> eEntries = eCache.listElements(AdminConstants.E_ENTRY);
-        CacheEntry[] entries = null;
-        if (eEntries.size() > 0) {
-            entries = new CacheEntry[eEntries.size()];
-            int i = 0;
-            for (Element eEntry : eEntries) {
-                entries[i++] = new CacheEntry(Key.CacheEntryBy.valueOf(eEntry.getAttribute(AdminConstants.A_BY)),
-                        eEntry.getText());
-            }
+    private static CacheEntry[] getCacheEntries(CacheSelector cacheSelector) throws ServiceException {
+        List<CacheEntrySelector> cacheEntrySelectors = cacheSelector.getEntries();
+        if (cacheEntrySelectors.size() < 1) {
+            return null;
         }
-
+        CacheEntry[] entries = new CacheEntry[cacheEntrySelectors.size()];
+            int i = 0;
+            for (CacheEntrySelector cacheEntrySelector : cacheEntrySelectors) {
+                entries[i++] = new CacheEntry(
+                        cacheEntrySelector.getBy().toKeyCacheEntryBy(), cacheEntrySelector.getKey());
+            }
         return entries;
     }
 
-    private void flushLdapCache(CacheEntryType cacheType, Element eCache) throws ServiceException {
-        CacheEntry[] entries = getCacheEntries(eCache);
+    private static void flushLdapCache(CacheEntryType cacheType, CacheSelector cacheSelector)
+    throws ServiceException {
+        CacheEntry[] entries = getCacheEntries(cacheSelector);
         Provisioning.getInstance().flushCache(cacheType, entries);
     }
 
@@ -212,50 +212,33 @@ public class FlushCache extends AdminDocumentHandler {
         }
     }
 
-    public static void flushChacheOnServer(Server server,ZimbraSoapContext zsc,String cacheType) throws ServiceException, IOException {
-        String adminUrl = URLUtil.getAdminURL(server, AdminConstants.ADMIN_SERVICE_URI);
-        SoapHttpTransport mTransport = new SoapHttpTransport(adminUrl);
-        mTransport.setAuthToken(zsc.getRawAuthToken());
-        XMLElement req = new XMLElement(AdminConstants.FLUSH_CACHE_REQUEST);
-        req.addElement(AdminConstants.E_CACHE).addAttribute(AdminConstants.A_TYPE, cacheType);
-        mTransport.invoke(req);
-    }
-
-    private void flushCacheOnAllServers(ZimbraSoapContext zsc, Element origReq) throws ServiceException {
+    private static void flushCacheOnAllServers(ZimbraSoapContext zsc, FlushCacheRequest req) throws ServiceException {
+        req.getCache().setAllServers(false);  // make sure we don't go round in loops
+        Element request = JaxbUtil.jaxbToElement(req);
 
         Provisioning prov = Provisioning.getInstance();
         String localServerId = prov.getLocalServer().getId();
 
         for (Server server : prov.getAllMailClientServers()) {
-
             if (localServerId.equals(server.getId())) {
                 continue;
             }
 
-            ZimbraLog.misc.debug("Flushing cache on server: " + server.getName());
-
-            Element req = origReq.clone();
-            Element eCache = req.getElement(AdminConstants.E_CACHE);
-            eCache.addAttribute(AdminConstants.A_ALLSERVERS, false);
-
+            ZimbraLog.misc.debug("Flushing cache on server: %s", server.getName());
             String adminUrl = URLUtil.getAdminURL(server, AdminConstants.ADMIN_SERVICE_URI);
             SoapHttpTransport mTransport = new SoapHttpTransport(adminUrl);
             mTransport.setAuthToken(zsc.getRawAuthToken());
 
             try {
-                mTransport.invoke(req);
-            } catch (ServiceException e) {
+                mTransport.invoke(request);
+            } catch (ServiceException | IOException e) {
                 // log and continue
-                ZimbraLog.misc.warn("Encountered exception while FlushCache on server: " + server.getName() +
-                        ", skip and continue with the next server", e);
-            } catch (IOException e) {
-                // log and continue
-                ZimbraLog.misc.warn("Encountered exception while FlushCache on server: " + server.getName() +
-                        ", skip and continue with the next server", e);
+                ZimbraLog.misc.warn(
+                        "Encountered exception during FlushCache on server '%s', skip & continue with the next server",
+                        server.getName(), e);
             }
         }
     }
-
 
     @Override
     public void docRights(List<AdminRight> relatedRights, List<String> notes) {
