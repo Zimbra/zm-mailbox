caa924027c30e5c837b5ed9bcc3234b0e8015e25
Greg Solovyev <greg@zimbra.com>
2015-10-16 02:24:59 -0500

    (split)bug none formatting cleanup
    Copied from Perforce
     Change: 557540

--- src/java/com/zimbra/cs/service/admin/CollectConfigFiles.java
+++ src/java/com/zimbra/cs/service/admin/CollectConfigFiles.java
@@ -36,49 +36,50 @@ import com.zimbra.cs.rmgmt.RemoteManager;
 import com.zimbra.cs.rmgmt.RemoteResult;
 import com.zimbra.cs.servlet.ZimbraServlet;
 
+@SuppressWarnings("serial")
 public class CollectConfigFiles extends ZimbraServlet {
-	private static final String P_HOST = "host";
-	private static final String DOWNLOAD_CONTENT_TYPE = "application/x-compressed";
-	@Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+    private static final String P_HOST = "host";
+    private static final String DOWNLOAD_CONTENT_TYPE = "application/x-compressed";
 
-		try {
-			//check the auth token
-			AuthToken authToken = getAdminAuthTokenFromCookie(req, resp);
-			if (authToken == null) {
-			    resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-			}
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-			if(!authToken.isAdmin()) {
-			    resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-			}
-			//take the host name
-			Provisioning prov = Provisioning.getInstance();
-			String hostName = req.getParameter(P_HOST);
-			Server server = prov.get(Key.ServerBy.name, hostName);
-			if (server == null) {
-				throw ServiceException.INVALID_REQUEST("server with name " + hostName + " could not be found", null);
-			}
-			//call RemoteManager
-			RemoteManager rmgr = RemoteManager.getRemoteManager(server);
-			RemoteResult rr = rmgr.execute(RemoteCommands.COLLECT_CONFIG_FILES);
-			//stream the data
-			resp.setContentType(DOWNLOAD_CONTENT_TYPE);
-            ContentDisposition cd = new ContentDisposition(Part.INLINE).setParameter("filename", hostName+".conf.tgz");
+        try {
+            // check the auth token
+            AuthToken authToken = getAdminAuthTokenFromCookie(req, resp);
+            if (authToken == null) {
+                resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            }
+
+            if (!authToken.isAdmin()) {
+                resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            }
+            // take the host name
+            Provisioning prov = Provisioning.getInstance();
+            String hostName = req.getParameter(P_HOST);
+            Server server = prov.get(Key.ServerBy.name, hostName);
+            if (server == null) {
+                throw ServiceException.INVALID_REQUEST("server with name " + hostName + " could not be found", null);
+            }
+            // call RemoteManager
+            RemoteManager rmgr = RemoteManager.getRemoteManager(server);
+            RemoteResult rr = rmgr.execute(RemoteCommands.COLLECT_CONFIG_FILES);
+            // stream the data
+            resp.setContentType(DOWNLOAD_CONTENT_TYPE);
+            ContentDisposition cd = new ContentDisposition(Part.INLINE)
+                    .setParameter("filename", hostName + ".conf.tgz");
             resp.addHeader("Content-Disposition", cd.toString());
-			ByteUtil.copy(new ByteArrayInputStream(rr.getMStdout()), true, resp.getOutputStream(), false);
-		} catch (ServiceException e) {
-			returnError(resp, e);
-        	return;
-		}
+            ByteUtil.copy(new ByteArrayInputStream(rr.getMStdout()), true, resp.getOutputStream(), false);
+        } catch (ServiceException e) {
+            returnError(resp, e);
+            return;
+        }
 
-	}
+    }
 
-	@Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		doGet(req, resp);
-	}
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
 
 }
