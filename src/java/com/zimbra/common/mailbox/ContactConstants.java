20fc6568315cffbccbb865aa4710b77ed538b03f
Chinmay Pathak <pathakc@zimbra.com>
2015-10-21 07:58:17 -0500

    Bug : 101348
    Unwanted contact data is generated from email.
    
    The code is written to generate fileAs and fullName metadata fields from email, if email contains .(dot) or _(underscore). Now when device sends fileAs field in request for creating contact, fileAs metadata is created from request, but if device doesn't send fileAs field, code creates fileAs metadata field from email.(ParsedAddress.java->parse()). So when fileAs is created from email, it may be wrong.
    
    So the code has been changed to not to generate contact fields from email address. If fileAs is sent in a request from device, then it is saved as fileAs. If request has both fileAs and email address with coment part, then fileAs will be stored from request and email address comment part will be stored as commentForEmailAddress.
    
    Copied from Perforce
     Change: 557841

--- src/java/com/zimbra/common/mailbox/ContactConstants.java
+++ src/java/com/zimbra/common/mailbox/ContactConstants.java
@@ -122,7 +122,6 @@ public final class ContactConstants {
     public static final String A_imAddress2 = Attr.imAddress2.name();
     public static final String A_imAddress3 = Attr.imAddress3.name();
     public static final String A_zimbraId = Attr.zimbraId.name();
-    public static final String A_commentForEmailAddress = Attr.commentForEmailAddress.name();
 
     // Comcast specific fields
     public static final String A_homeAddress = Attr.homeAddress.name();
@@ -259,8 +258,7 @@ public final class ContactConstants {
         vCardUID,
         vCardXProps,
         zimbraId,
-        outlookProps,
-        commentForEmailAddress;
+        outlookProps;
 
 
         public static Attr fromString(String s) throws ServiceException {
