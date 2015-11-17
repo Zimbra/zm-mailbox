959dff5c4c099d7e3a103e7a98f80ececad90e12
Ilya Raykin <raykini@zimbra.com>
2015-11-17 12:14:51 -0600

    (split)Bug: 98015
    The scenario in this bug was caused by the algorithm for determining the Content-Transfer-Encoding yielding a different value for the replied/forwarded message than for the original message. This happens because the replied/forwarded message has added headers in the content, which increase the content length, which decreases the proportion of non-ASCII characters to below 1/4, which switches the encoding from 8bit to QP.
    
    The solution is to inherit the known CTE header from the original message. This fix updates the creation of MIME messages from content passed through SendMsgRequest to inherit the CTE header from the copy of the message on disk. This applies to either replies or forwards, scenarios where SendMsgRequest includes the "origid" parameter. If the ParseMimeMessage.parseMimeMsgSoap() method is passed an optional new ItemId parameter, a copy of the existing MIME message will be loaded from the mailbox. Then, the setContent() method that recursively assembles the MIME parts will attempt to set the Content-Transfer-Encoding header of each part to that of the corresponding part on the local message.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=98015
    
    Copied from Perforce
     Change: 559840

--- src/java/com/zimbra/cs/service/mail/SendMsg.java
+++ src/java/com/zimbra/cs/service/mail/SendMsg.java
@@ -2,11 +2,11 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014 Zimbra, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -166,7 +166,7 @@ public final class SendMsg extends MailDocumentHandler {
                     Message msg = mbox.getMessageById(octxt, iidDraft.getId());
                     mm = msg.getMimeMessage(false);
                 } else {
-                    mm = ParseMimeMessage.parseMimeMsgSoap(zsc, octxt, mbox, msgElem, null, mimeData);
+                    mm = ParseMimeMessage.parseMimeMsgSoap(zsc, octxt, mbox, msgElem, null, iidOrigId, mimeData);
                 }
 
                 savedMsgId = doSendMessage(octxt, mbox, mm, mimeData.uploads, iidOrigId, replyType, identityId,
