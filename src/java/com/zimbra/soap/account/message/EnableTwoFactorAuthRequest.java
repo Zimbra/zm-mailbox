6f0b503b1b3b2cc52acc012ccb17952526996467
Ilya Raykin <raykini@zimbra.com>
2016-03-01 12:31:54 -0600

    Bug: 103962
    Merging
    
    //depot/zimbra/JUDASPRIEST/...
    
    to //depot/zimbra/main/...
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=103962
    
    Copied from Perforce
     Change: 567128

--- src/java/com/zimbra/soap/account/message/EnableTwoFactorAuthRequest.java
+++ src/java/com/zimbra/soap/account/message/EnableTwoFactorAuthRequest.java
@@ -1,11 +1,13 @@
 package com.zimbra.soap.account.message;
 
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 import com.zimbra.common.soap.AccountConstants;
 import com.zimbra.soap.account.type.AuthToken;
+import com.zimbra.soap.type.ZmBoolean;
 
 @XmlRootElement(name=AccountConstants.E_ENABLE_TWO_FACTOR_AUTH_REQUEST)
 @XmlType(propOrder = {})
@@ -34,6 +36,12 @@ public class EnableTwoFactorAuthRequest {
     @XmlElement(name=AccountConstants.E_TWO_FACTOR_CODE, required=false)
     private String twoFactorCode;
 
+    /**
+     * @zm-api-field-description Whether the client supports the CSRF token
+     */
+    @XmlAttribute(name=AccountConstants.A_CSRF_SUPPORT, required=false)
+    private ZmBoolean csrfSupported;
+
     public String getPassword() { return password; }
     public EnableTwoFactorAuthRequest setPassword(String password) { this.password = password; return this; }
 
@@ -45,4 +53,11 @@ public class EnableTwoFactorAuthRequest {
 
     public AuthToken getAuthToken() { return authToken; }
     public EnableTwoFactorAuthRequest setAuthToken(AuthToken authToken) { this.authToken = authToken; return this; }
+
+    public ZmBoolean getCsrfSupported() { return csrfSupported; }
+
+    public EnableTwoFactorAuthRequest setCsrfSupported(Boolean csrfSupported) {
+        this.csrfSupported = ZmBoolean.fromBool(csrfSupported);
+        return this;
+    }
 }
