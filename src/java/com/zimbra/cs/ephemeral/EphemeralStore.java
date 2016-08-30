389498acc8d4aa8aa38cf8bcae012d2b4350fdb0
ilya <raykini@zimbra.com>
2016-08-30 10:46:25 -0700

    removed obsolete Location inner class

--- src/java/com/zimbra/cs/ephemeral/EphemeralStore.java
+++ src/java/com/zimbra/cs/ephemeral/EphemeralStore.java
@@ -108,21 +108,4 @@ public class EphemeralStore {
         }
         return singleton;
     }
-
-    /**
-     * This abstract class represents a hierarchical specification of the
-     * location of ephemeral attributes.
-     *
-     * The only method, getLocation(), returns an array of Strings representing
-     * a hierarchy under which the key/value pair is stored. It is up to
-     * the @EphemeralBackend implementation to decide how to use this,
-     * be it accessing the appropriate database, constructing a key hierarchy,
-     * or something else.
-     *
-     * @author iraykin
-     *
-     */
-    public static abstract class Location {
-        public abstract String[] getLocation();
-    }
 }
