3ae4e79750b9f318c3ffac1e9620752159c98c86
ilya <raykini@zimbra.com>
2016-08-31 08:34:45 -0700

    redesigned ephemeral store

--- src/java/com/zimbra/cs/ephemeral/EphemeralStore.java
+++ src/java/com/zimbra/cs/ephemeral/EphemeralStore.java
@@ -1,120 +1,157 @@
 package com.zimbra.cs.ephemeral;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.extension.ExtensionUtil;
+import com.zimbra.cs.util.Zimbra;
 
-/** Singleton representing the entry point into ephemeral storage.
- * Routes requests to the specified @EphemeralBackend.
+/**
+ * Abstract class representing ephemeral storage.
+ *
  * @author iraykin
  *
  */
-public class EphemeralStore {
-    private static EphemeralStore singleton;
-    private EphemeralBackend backend;
+public abstract class EphemeralStore {
 
-    private EphemeralStore(EphemeralBackend backend) {
-        this.backend = backend;
-    }
+    private static Map<String, String> factories = new HashMap<String, String>();
+    private static Factory factory;
 
-    public EphemeralResult get(String key, EphemeralLocation location)
-            throws ServiceException {
-        backend.open();
-        try {
-            return backend.get(key, location);
-        } finally {
-            backend.close();
+    /**
+     * Get the value for a key. If key does not exist, returns an empty
+     * EphemeralResult instance.
+     *
+     * @param key
+     * @param location
+     * @return
+     * @throws ServiceException
+     */
+    public abstract EphemeralResult get(String key, EphemeralLocation location)
+            throws ServiceException;
+
+    /**
+     * Set a value for a key if the key the does not exist, or overwrites
+     * otherwise. If the key is multi-valued, all original values are deleted.
+     *
+     * @param input
+     * @param location
+     * @throws ServiceException
+     */
+    public abstract void set(EphemeralInput attribute, EphemeralLocation location)
+            throws ServiceException;
+
+    /**
+     * Set a value for a key if the key the does not exist, or add another value
+     * otherwise. If this value already exists, the expiration is updated if one
+     * is provided.
+     *
+     * @param input
+     * @param location
+     * @throws ServiceException
+     */
+    public abstract void update(EphemeralInput attribute, EphemeralLocation location)
+            throws ServiceException;
+
+    /**
+     * Delete the specified key.
+     *
+     * @param key
+     * @param location
+     * @throws ServiceException
+     */
+    public abstract void delete(String key, EphemeralLocation location)
+            throws ServiceException;
+
+    /**
+     * Delete specified value for a key. If the value does not exist, do
+     * nothing. If no values for the key remain, delete the key.
+     *
+     * @param key
+     * @param value
+     * @param location
+     * @throws ServiceException
+     */
+    public abstract void deleteValue(String key, String value, EphemeralLocation location)
+            throws ServiceException;
+
+    /**
+     * Delete keys that have passed their expiration. If the backend natively
+     * supports key expiry, this may do nothing.
+     *
+     * @param key
+     * @param location
+     * @throws ServiceException
+     */
+    public abstract boolean hasKey(String key, EphemeralLocation location)
+            throws ServiceException;
+
+
+    /**
+     * Check whether the specified key exists.
+     *
+     * @param key
+     * @param location
+     * @return
+     * @throws ServiceException
+     */
+    public abstract void purgeExpired(String key, EphemeralLocation location)
+            throws ServiceException;
+
+    public static void registerFactory(String prefix, String klass) {
+        if (factories.containsKey(prefix)) {
+            ZimbraLog.ephemeral.warn("Replacing ephemeral backend factory class '%s' registered for '%s' with '%s'",
+                    factories.get(prefix), prefix, klass);
         }
+        factories.put(prefix,  klass);
     }
 
-    public void set(EphemeralInput attribute, EphemeralLocation location)
-            throws ServiceException {
-        backend.open();
-        try {
-            backend.set(attribute, location);
-        } finally {
-            backend.close();
+    public static final void setFactory(String factoryClassName) {
+        if (factoryClassName == null) {
+            Zimbra.halt("no EphemeralStore specified");
         }
-    }
-
-    public void update(EphemeralInput attribute, EphemeralLocation location)
-            throws ServiceException {
-        backend.open();
+        Class<? extends Factory> factoryClass = null;
         try {
-            backend.update(attribute, location);
-        } finally {
-            backend.close();
-        }
-    }
-
-    public void delete(String key, EphemeralLocation location) throws ServiceException {
-        backend.open();
-        try {
-            backend.delete(key, location);
-        } finally {
-            backend.close();
-        }
-    }
-
-    public void deleteValue(String key, String value, EphemeralLocation location)
-            throws ServiceException {
-        backend.open();
-        try {
-            backend.deleteValue(key, value, location);
-        } finally {
-            backend.close();
-        }
-    }
-
-    public boolean hasKey(String key, EphemeralLocation location) throws ServiceException {
-        backend.open();
-        try {
-            return backend.hasKey(key, location);
-        } finally {
-            backend.close();
-        }
-    }
-
-    public void purgeExpired(String key, EphemeralLocation location)
-            throws ServiceException {
-        backend.open();
-        try {
-            backend.purgeExpired(key, location);
-        } finally {
-            backend.close();
-        }
-    }
-
-    public static EphemeralStore getInstance() throws ServiceException {
-        if (singleton == null) {
-            synchronized (EphemeralStore.class) {
-                if (singleton == null) {
-                    EphemeralBackend backend;
-                    String className = Provisioning.getInstance().getConfig().getEphemeralStorageClassName();
-                    if (className != null && !className.equals("")) {
-                        Class<?> klass = null;
-                        try {
-                            try {
-                                klass = Class.forName(className);
-                            } catch (ClassNotFoundException cnfe) {
-                                klass = ExtensionUtil.findClass(className);
-                            }
-                            backend = (EphemeralBackend) klass.newInstance();
-                        } catch (Exception e) {
-                            //TODO: use default backend
-                            throw ServiceException.FAILURE("no EphemeralBackend specified", null);
-
-                        }
-                    } else {
-                        //TODO: use default backend
-                        throw ServiceException.FAILURE("no EphemeralBackend specified", null);
-                    }
-                    ZimbraLog.ephemeral.info("Using EphemeralBackend %s", backend.getClass().getCanonicalName());
-                    singleton = new EphemeralStore(backend);
-                }
+            factoryClass = Class.forName(factoryClassName).asSubclass(Factory.class);
+        } catch (ClassNotFoundException cnfe) {
+            try {
+                factoryClass = ExtensionUtil.findClass(factoryClassName).asSubclass(Factory.class);
+            } catch (ClassNotFoundException cnfe2) {
+                Zimbra.halt("Unable to find EphemeralStore factory " + factoryClassName, cnfe2);
             }
         }
-        return singleton;
+        setFactory(factoryClass);
+    }
+
+    public static final void setFactory(Class<? extends Factory> factoryClass) {
+        try {
+            factory = factoryClass.newInstance();
+            ZimbraLog.ephemeral.info("using ephemeral store factory %s", factoryClass.getDeclaringClass().getSimpleName());
+        } catch (InstantiationException | IllegalAccessException e) {
+            Zimbra.halt("Unable to initialize EphemeralStore factory " + factoryClass.getDeclaringClass().getSimpleName(), e);
+        }
+    }
+
+    public static Factory getFactory() throws ServiceException {
+        if (factory == null) {
+            String factoryClass = null;
+            String url = Provisioning.getInstance().getConfig().getEphemeralBackendURL();
+            if (url != null) {
+                String[] tokens = url.split(":");
+                if (tokens != null && tokens.length > 0) {
+                    factoryClass = factories.get(tokens[0]);
+                }
+            }
+            setFactory(factoryClass);
+        }
+        return factory;
+    }
+
+    public static interface Factory {
+
+        EphemeralStore getStore();
+
     }
 }
