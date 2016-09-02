9682e62b37a959b1fca354a589117e64403a9857
ilya <raykini@zimbra.com>
2016-09-02 10:28:03 -0700

    changes to EphemeralKeyValuePair

--- src/java/com/zimbra/cs/ephemeral/EphemeralKeyValuePair.java
+++ src/java/com/zimbra/cs/ephemeral/EphemeralKeyValuePair.java
@@ -1,27 +1,21 @@
 package com.zimbra.cs.ephemeral;
 
 
-/** Represents an key/value pair, as well as an optional expiration time.
- * This class is the output of @AttributeEncoder.decode(), which makes it an
- * intermediate representation of the data. One or more of these are wrapped
- * in an @EphemeralResult.
+/**
+ * Represents a key/value pair returned by @AttributeEncoder.decode().
+ * This is an intermediate representation of the data.
+ * One or more of these are wrapped in an @EphemeralResult.
  *
  * @author iraykin
  *
  */
 class EphemeralKeyValuePair {
-    private String key;
-    private String value;
-    private Long expires;
+    protected String key;
+    protected String value;
 
     public EphemeralKeyValuePair(String key, String value) {
-        this(key, value, null);
-    }
-
-    public EphemeralKeyValuePair(String key, String value, Long expires) {
         this.key = key;
         this.value = value;
-        this.expires = expires;
     }
 
     public String getKey() {
@@ -30,9 +24,5 @@ class EphemeralKeyValuePair {
 
     public String getValue() {
         return value;
-    }
-
-    public Long getExpires() {
-        return expires;
     }
 }
