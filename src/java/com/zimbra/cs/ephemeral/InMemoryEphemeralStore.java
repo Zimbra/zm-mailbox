b4ec9d08a289cfbc3f3ddafc44cfcd2fc7b04720
ilya <raykini@zimbra.com>
2016-09-02 08:23:29 -0700

    using guava multimap

--- src/java/com/zimbra/cs/ephemeral/InMemoryEphemeralStore.java
+++ src/java/com/zimbra/cs/ephemeral/InMemoryEphemeralStore.java
@@ -1,43 +1,40 @@
 package com.zimbra.cs.ephemeral;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import com.zimbra.common.service.ServiceException;
-import com.zimbra.common.util.StringUtil;
 
 public class InMemoryEphemeralStore extends EphemeralStore {
 
-    private Map<String, Map<String, Object>> storeMap;
+    private Map<String, Multimap<String, String>> storeMap;
 
     public InMemoryEphemeralStore() {
-        storeMap = new HashMap<String, Map<String, Object>>();
+        storeMap = new HashMap<String, Multimap<String, String>>();
         setAttributeEncoder(new ExpirationEncoder());
     }
 
     @Override
     public EphemeralResult get(String key, EphemeralLocation target)
             throws ServiceException {
-        Map<String, Object> map = getSpecifiedMap(target);
-        Object value = map.get(key);
-        if (value == null) {
+        Multimap<String, String> map = getSpecifiedMap(target);
+        Collection<String> values = map.get(key);
+        if (values.isEmpty()) {
             return EphemeralResult.emptyResult(key);
-        } else if (value instanceof String) {
-            EphemeralKeyValuePair entry = getAttributeEncoder().decode(key, (String) value);
-            return new EphemeralResult(entry);
-        } else if (value instanceof String[]) {
-            String[] values = (String[]) value;
-            EphemeralKeyValuePair[] entries = new EphemeralKeyValuePair[values.length];
-            for (int i = 0; i < values.length; i++) {
-                entries[i] = getAttributeEncoder().decode(key, values[i]);
+        } else {
+            EphemeralKeyValuePair[] entries = new EphemeralKeyValuePair[values.size()];
+            int i = 0;
+            for (String v: values) {
+                entries[i] = getAttributeEncoder().decode(key, v);
+                i++;
             }
             return new EphemeralResult(entries);
-        } else {
-            throw ServiceException.FAILURE("map values can only be of type String or String[]", null);
         }
     }
 
@@ -46,106 +43,73 @@ public class InMemoryEphemeralStore extends EphemeralStore {
             throws ServiceException {
         String key = attribute.getKey();
         String value = getAttributeEncoder().encode(attribute, target).getSecond();
-        Map<String, Object> map = getSpecifiedMap(target);
-        map.remove(key);
-        StringUtil.addToMultiMap(map, key, value);
+        Multimap<String, String> map = getSpecifiedMap(target);
+        map.removeAll(key);
+        map.put(key, value);
     }
 
     @Override
     public void update(EphemeralInput attribute, EphemeralLocation target)
             throws ServiceException {
-        Map<String, Object> map = getSpecifiedMap(target);
+        Multimap<String, String> map = getSpecifiedMap(target);
         String key = attribute.getKey();
         String value = getAttributeEncoder().encode(attribute, target).getSecond();
-        StringUtil.addToMultiMap(map, key, value);
+        map.put(key, value);
     }
 
     @Override
     public void delete(String key, EphemeralLocation target)
             throws ServiceException {
-        Map<String, Object> map = getSpecifiedMap(target);
-        map.remove(key);
+        Multimap<String, String> map = getSpecifiedMap(target);
+        map.removeAll(key);
     }
 
     @Override
     public void deleteValue(String key, String valueToDelete, EphemeralLocation target)
             throws ServiceException {
-        Map<String, Object> map = getSpecifiedMap(target);
-        Object value = map.get(key);
-        if (value == null) {
-            return;
-        }
-        String[] values;
-        if (value instanceof String) {
-            values = new String[]{(String) value};
-        } else if (value instanceof String[]) {
-            values = (String[]) value;
-        } else {
-            throw ServiceException.FAILURE("map values can only be of type String or String[]", null);
-        }
-        List<String> valuesAfterRemoval = new ArrayList<String>();
+        Multimap<String, String> map = getSpecifiedMap(target);
+        Collection<String> values = map.get(key);
+        List<String> toDelete = new LinkedList<String>();
         for (String v: values) {
-            if (!getAttributeEncoder().decode(key, v).getValue().equals(valueToDelete)) {
-                valuesAfterRemoval.add(v);
+            if (getAttributeEncoder().decode(key, v).getValue().equals(valueToDelete)) {
+                toDelete.add(v);
             }
         }
-        if (valuesAfterRemoval.isEmpty()) {
-            map.remove(key);
-        } else if (valuesAfterRemoval.size() == 1) {
-            map.put(key, valuesAfterRemoval.get(0));
-        } else {
-            map.put(key, valuesAfterRemoval.toArray(new String[valuesAfterRemoval.size()]));
+        for (String v: toDelete) {
+            map.remove(key, v);
         }
     }
 
     @Override
     public void purgeExpired(String key, EphemeralLocation target)
             throws ServiceException {
-        Map<String, Object> map = getSpecifiedMap(target);
-        Object value = map.get(key);
-        if (value == null) { return; }
-        String[] values;
-        if (value instanceof String) {
-            values = new String[] {(String) value};
-        } else if (value instanceof String[]) {
-            values = (String[]) value;
-        } else {
-            throw ServiceException.FAILURE("map values can only be of type String or String[]", null);
-        }
-        List<String> purged = new LinkedList<String>();
-        List<String> remaining = new LinkedList<String>();
+        Multimap<String, String> map = getSpecifiedMap(target);
+        Collection<String> values = map.get(key);
+        List<String> toDelete = new LinkedList<String>();
         for (String v: values) {
              EphemeralKeyValuePair entry = getAttributeEncoder().decode(key, v);
              if (entry.getExpires() < System.currentTimeMillis()) {
-                 purged.add(entry.getValue());
-             } else {
-                 remaining.add(v);
+                 toDelete.add(v);
              }
         }
-        if (purged.isEmpty()) {
-            return;
-        } else if (remaining.isEmpty()) {
-            map.remove(key);
-        } else if (remaining.size() == 1) {
-            map.put(key, remaining.get(0));
-        } else {
-            map.put(key, remaining.toArray(new String[remaining.size()]));
+        for (String v: toDelete) {
+            map.remove(key, v);
         }
     }
 
     @Override
     public boolean hasKey(String keyName, EphemeralLocation target)
             throws ServiceException {
-        Map<String, Object> map = getSpecifiedMap(target);
+        Multimap<String, String> map = getSpecifiedMap(target);
         return map.containsKey(keyName);
     }
 
-    private Map<String, Object> getSpecifiedMap(EphemeralLocation target) {
+    private Multimap<String, String> getSpecifiedMap(EphemeralLocation target) {
         String[] locationHierarchy = target.getLocation();
         String storeKey = Joiner.on("|").join(locationHierarchy);
-        Map<String, Object> store = storeMap.get(storeKey);
+        Multimap<String, String> store = storeMap.get(storeKey);
         if (store == null) {
-            store = new HashMap<String, Object>();
+            store = ArrayListMultimap.create();
             storeMap.put(storeKey, store);
         }
         return store;
