04aa147f23d4878aa4d0c7c81a767d96ee7dc9eb
Shrikant <sprasad@zimbra.com>
2016-09-23 14:59:13 +0530

    BugBug106457 --add constants for GetCertificateInfo api

--- src/java/com/zimbra/common/soap/SmimeConstants.java
+++ src/java/com/zimbra/common/soap/SmimeConstants.java
@@ -6,12 +6,29 @@ public class SmimeConstants {
 
     public static final String E_SEND_SECURE_MSG_REQUEST = "SendSecureMsgRequest";
     public static final String E_SEND_SECURE_MSG_RESPONSE = "SendSecureMsgResponse";
+    public static final String E_GET_CERT_INFO_REQUEST = "GetCertificateInfoRequest";
+    public static final String E_GET_CERT_INFO_RESPONSE = "GetCertificateInfoResponse";
 
     public static final QName SEND_SECURE_MSG_REQUEST = QName.get(E_SEND_SECURE_MSG_REQUEST, MailConstants.NAMESPACE);
     public static final QName SEND_SECURE_MSG_RESPONSE = QName.get(E_SEND_SECURE_MSG_RESPONSE, MailConstants.NAMESPACE);
+    public static final QName GET_CERT_INFO_REQUEST = QName.get(E_GET_CERT_INFO_REQUEST, MailConstants.NAMESPACE);
+    public static final QName GET_CERT_INFO_RESPONSE = QName.get(E_GET_CERT_INFO_RESPONSE, MailConstants.NAMESPACE);
 
     public static final String A_SIGN = "sign";
     public static final String A_ENCRYPT = "encrypt";
     public static final String A_CERT_ID = "certId";
+
+    public static final String E_CERTIFICATE = "certificate";
+    public static final String E_SUBJECT_DN = "subjectDn";
+    public static final String E_ISSUER_DN = "issuerDn";
+    public static final String E_SERIAL_NO = "serialNo";
+    public static final String E_ALGORITHM = "algorithm";
+    public static final String E_VALIDITY = "validity";
+    public static final String E_SIGNATURE = "signature";
+    public static final String E_START_DATE = "startDate";
+    public static final String E_END_DATE = "endDate";
+    public static final String A_PUB_CERT_ID = "pubCertId";
+    public static final String A_PVT_KEY_ID = "pvtKeyId";
+    public static final String A_DEFAULT = "default";
 
 }
