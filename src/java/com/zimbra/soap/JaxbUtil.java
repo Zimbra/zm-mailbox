032597197a5748edd339dede3874906a918380cd
rohan.ambasta <rohan.ambasta@synacor.com>
2016-09-27 15:41:34 +0530

    Bug-106454 SMIME: saveCertificate SOAP extension API

--- src/java/com/zimbra/soap/JaxbUtil.java
+++ src/java/com/zimbra/soap/JaxbUtil.java
@@ -1055,7 +1055,9 @@ public final class JaxbUtil {
             com.zimbra.soap.mail.message.SendSecureMsgRequest.class,
             com.zimbra.soap.mail.message.SendSecureMsgResponse.class,
             com.zimbra.soap.account.message.GetCertificateInfoRequest.class,
-            com.zimbra.soap.account.message.GetCertificateInfoResponse.class
+            com.zimbra.soap.account.message.GetCertificateInfoResponse.class,
+            com.zimbra.soap.account.message.SaveCertificateRequest.class,
+            com.zimbra.soap.account.message.SaveCertificateResponse.class
         };
 
         try {
