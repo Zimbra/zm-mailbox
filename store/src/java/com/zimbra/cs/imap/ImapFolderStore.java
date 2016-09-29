68a25c95a6f97201a1528290f3a4b84d793aa8be
Gren Elliot <gren.elliot@synacor.com>
2016-09-29 11:34:14 +0100

    ZMS-135:Mailbox access refactoring phase 1
    New interfaces ImapMailboxStore and ImapFolderStore.
    Plan is to wrap Mailbox/ZMailbox and Folder/ZFolder inside implementors
    of these interfaces, so that rest of the IMAP code doesn't need to care
    which is being used.

---
+++ store/src/java/com/zimbra/cs/imap/ImapFolderStore.java
@@ -0,0 +1,36 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2016 Synacor, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.imap;
+
+import com.zimbra.client.ZFolder;
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.mailbox.Folder;
+
+public interface ImapFolderStore {
+    public static ImapFolderStore get(Object folder) throws ServiceException {
+        if (folder == null) {
+            return null;
+        }
+        if (folder instanceof Folder) {
+            return new LocalImapFolderStore((Folder) folder);
+        }
+        if (folder instanceof ZFolder) {
+            return new RemoteImapFolderStore((ZFolder) folder);
+        }
+        return null; // TODO or throw an exception?
+    }
+}
