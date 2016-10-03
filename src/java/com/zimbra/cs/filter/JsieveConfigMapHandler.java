ca8e68b8e6040a28db90b0cde7ff09682956d16f
Rupali Desai <rupali.desai@synacor.com>
2016-10-03 10:25:14 +0530

    code cleanup and revert

--- src/java/com/zimbra/cs/filter/JsieveConfigMapHandler.java
+++ src/java/com/zimbra/cs/filter/JsieveConfigMapHandler.java
@@ -164,17 +164,4 @@ public class JsieveConfigMapHandler {
         return isNotifyActionRFCCompliant;
     }
     
-//	/**
-//	 * @return true if zimbraSieveFeatureVariablesEnabled is true
-//	 */
-//	private static boolean isVariablesExtAvailable() {
-//		boolean variablesExtAvailable = false;
-//		try {
-//			variablesExtAvailable = Provisioning.getInstance().getConfig()
-//					.getBooleanAttr(Provisioning.A_zimbraSieveFeatureVariablesEnabled, false);
-//		} catch (ServiceException e) {
-//			ZimbraLog.filter.info("Error initializing the sieve variables extension.", e);
-//		}
-//		return variablesExtAvailable;
-//	}
 }
