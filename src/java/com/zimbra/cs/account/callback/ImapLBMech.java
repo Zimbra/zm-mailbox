c936495b08d8cdbe2d00387f4c455c221325cd47
Gordon Tillman <gordyt@gmail.com>
2016-10-04 07:52:46 -0500

    ZMS-147 - zimbraImapLoadBalancingAlgorithm
    - Added attribute to zimbra-attrs.  Following the same
      pattern as what was used for zimbraAuthMech.
    - Added validation callback stub that allows any value
      for the time being.  ZMS-148 will update that to perform
      actual validation.

---
+++ src/java/com/zimbra/cs/account/callback/ImapLBMech.java
@@ -0,0 +1,60 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2010, 2011, 2013, 2014, 2016 Synacor, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.account.callback;
+
+import java.util.Map;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.account.AttributeCallback;
+import com.zimbra.cs.account.Entry;
+import com.zimbra.cs.account.auth.AuthMechanism;
+
+public class ImapLBMech extends AttributeCallback {
+
+    @Override
+    public void preModify(CallbackContext context, String attrName, Object attrValue, Map attrsToModify, Entry entry)
+    throws ServiceException {	
+
+        String lbMech;
+
+        SingleValueMod mod = singleValueMod(attrName, attrValue);
+        if (mod.setting()) {
+            lbMech = mod.value();
+
+            boolean valid = false;
+
+            if (lbMech == null) {
+                valid = true;
+            } else {
+            	// TODO (ZMS-148): Implement IMAP server load balancing class and wire it in.
+            	valid = true;
+            }
+
+            if (!valid) {
+                throw ServiceException.INVALID_REQUEST("invalid value: " + lbMech, null);
+            }
+        }
+
+    }
+
+
+    @Override
+    public void postModify(CallbackContext context, String attrName, Entry entry) {
+    }
+
+}
