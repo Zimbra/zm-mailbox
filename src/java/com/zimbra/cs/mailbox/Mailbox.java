b14a4b1ccbcd36ec29c5189f7ddf2b3ae0a01b45
Shrikant <sprasad@zimbra.com>
2016-10-12 15:23:58 +0530

    Bug106503 - save certificate

--- src/java/com/zimbra/cs/mailbox/Mailbox.java
+++ src/java/com/zimbra/cs/mailbox/Mailbox.java
@@ -284,6 +284,7 @@ public class Mailbox {
     //This id should be incremented if any new ID_FOLDER_* is added.
     public static final int HIGHEST_SYSTEM_ID = 18;
     public static final int FIRST_USER_ID = 256;
+    public static final int ID_FOLDER_SMIME_CERT  = 19;
 
 
     public static final String CONF_PREVIOUS_MAILBOX_IDS = "prev_mbox_ids";
@@ -2175,6 +2176,8 @@ public class Mailbox {
                             MailItem.Type.MESSAGE, 0, MailItem.DEFAULT_COLOR_RGB, null, null, null);
             Folder.create(ID_FOLDER_BRIEFCASE, UUIDUtil.generateUUID(), this, userRoot, "Briefcase", system,
                             MailItem.Type.DOCUMENT, 0, MailItem.DEFAULT_COLOR_RGB, null, null, null);
+            Folder.create(ID_FOLDER_SMIME_CERT, UUIDUtil.generateUUID(), this, userRoot, "SmimeCertificates", system,
+                    MailItem.Type.SMIME_CERTIFICATE, 0, MailItem.DEFAULT_COLOR_RGB, null, null, null);
         } finally {
             lock.release();
         }
@@ -8799,6 +8802,17 @@ public class Mailbox {
         }
     }
 
+   public Document createDocument(OperationContext octxt, int folderId, String filename, String mimeType,
+            String author, String description, boolean descEnabled, InputStream data, MailItem.Type type)
+    throws ServiceException {
+        try {
+            ParsedDocument pd = new ParsedDocument(data, filename, mimeType, System.currentTimeMillis(), author, description, descEnabled);
+            return createDocument(octxt, folderId, pd, type, 0);
+        } catch (IOException ioe) {
+            throw ServiceException.FAILURE("error writing document blob", ioe);
+        }
+    }
+
     public Document createDocument(OperationContext octxt, int folderId, ParsedDocument pd, MailItem.Type type,
             int flags)
     throws IOException, ServiceException {
@@ -8807,7 +8821,7 @@ public class Mailbox {
 
     public Document createDocument(OperationContext octxt, int folderId, ParsedDocument pd, MailItem.Type type,
             int flags, MailItem parent, CustomMetadata custom, boolean indexing)
-    throws IOException, ServiceException {
+    throws IOException, ServiceException {    
         StoreManager sm = StoreManager.getInstance();
         StagedBlob staged = sm.stage(pd.getBlob(), this);
 
@@ -8830,6 +8844,9 @@ public class Mailbox {
                     break;
                 case WIKI:
                     doc = WikiItem.create(itemId, uuid, getFolderById(folderId), pd.getFilename(), pd, null);
+                    break;
+                case SMIME_CERTIFICATE:
+                    doc = SmimeCertificate.create(itemId, uuid, getFolderById(folderId), pd.getFilename(), pd.getContentType(), pd, custom, flags, parent);
                     break;
                 default:
                     throw MailServiceException.INVALID_TYPE(type.toString());
