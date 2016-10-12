a5fc9a8df1458091a8f256bfdc9656d0411ff776
Gren Elliot <gren.elliot@synacor.com>
2016-10-12 17:35:36 +0100

    ZMS-135:GrantGranteeType.asByte(), modifyFolderGrant
    MailboxStore.modifyFolderGrant interface method added
    GrantGranteeType gains asByte method to make a clear link between the
    enum values and ACL constants.

--- common/src/java/com/zimbra/common/mailbox/GrantGranteeType.java
+++ common/src/java/com/zimbra/common/mailbox/GrantGranteeType.java
@@ -27,39 +27,45 @@ import com.zimbra.common.service.ServiceException;
 public enum GrantGranteeType {
     // case must match protocol - keep in sync with com.zimbra.client.ZGrant.GranteeType and
     // com.zimbra.soap.type.GrantGranteeType
-    //     (which differ slightly from com.zimbra.cs.account.accesscontrol.GranteeType)
+    //         (Note:differs slightly from com.zimbra.cs.account.accesscontrol.GranteeType)
+        usr((byte)1),     /** access is granted to an authenticated user */
+        grp((byte)2),     /** access is granted to a group of users */
+        all((byte)3),     /** access is granted to all authenticated users */
+        dom((byte)4),     /** access is granted to all users in a domain */
+        cos((byte)5),     /** access is granted to users on a cos */
+        pub((byte)6),     /** access is granted to public. no authentication needed. */
+        guest((byte)7),   /** access is granted to a non-Zimbra email address and a password */
+        key((byte)8);     /** access is granted to a non-Zimbra email address and an accesskey */
+
+        private byte byteEquiv;
+
+        GrantGranteeType(byte asByte) {
+            byteEquiv = asByte;
+        }
+
         /**
-         * access is granted to an authenticated user
+         * @return zm-store ACL byte constant value equivalent
+         *         usr     --> GRANTEE_USER
+         *         grp     --> GRANTEE_GROUP
+         *         all     --> GRANTEE_AUTHUSER
+         *         dom     --> GRANTEE_DOMAIN
+         *         cos     --> GRANTEE_COS
+         *         pub     --> GRANTEE_PUBLIC
+         *         guest   --> GRANTEE_GUEST
+         *         key     --> GRANTEE_KEY
          */
-        usr,
-        /**
-         * access is granted to a group of users
-         */
-        grp,
-        /**
-         * access is granted to users on a cos
-         */
-        cos,
-        /**
-         * access is granted to public. no authentication needed.
-         */
-        pub,
-        /**
-         * access is granted to all authenticated users
-         */
-        all,
-        /**
-         * access is granted to all users in a domain
-         */
-        dom,
-        /**
-         * access is granted to a non-Zimbra email address and a password
-         */
-        guest,
-        /**
-         * access is granted to a non-Zimbra email address and an accesskey
-         */
-        key;
+        public byte asByte() {
+            return byteEquiv;
+        }
+
+        public static GrantGranteeType fromByte(byte byteValue) {
+            for (GrantGranteeType gt :GrantGranteeType.values()) {
+                if (gt.asByte() == byteValue) {
+                    return gt;
+                }
+            }
+            throw new IllegalArgumentException("Unrecognised GranteeType byte equvalent :" + byteValue);
+        }
 
     public static GrantGranteeType fromString(String s) throws ServiceException {
         try {
