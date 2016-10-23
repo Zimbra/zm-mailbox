4696c814a8e371bc2dec7ac6f9fe4543e0da5970
Shrikant <sprasad@zimbra.com>
2016-10-23 10:59:31 +0530

    Bug107011 --add constants for parsed subject/issuer details and subject/issuer alt names

--- src/java/com/zimbra/common/soap/SmimeConstants.java
+++ src/java/com/zimbra/common/soap/SmimeConstants.java
@@ -38,24 +38,48 @@ public class SmimeConstants {
     public static final String A_CERT_ID = "certId";
     public static final String A_CERTIFICATE_PASSWORD = "password";
     public static final String A_REPLACE_ID = "replaceId";
+    public static final String A_PUB_CERT_ID = "pubCertId";
+    public static final String A_PVT_KEY_ID = "pvtKeyId";
+    public static final String A_DEFAULT = "default";
 
     public static final String E_CERTIFICATE = "certificate";
     public static final String E_EMAIL_ADDR = "emailAddress";
-    public static final String E_SUBJECT_DN = "subjectDn";
-    public static final String E_ISSUER_DN = "issuerDn";
+    public static final String E_SUBJECT_DN = "issuedTo";
+    public static final String E_ISSUER_DN = "issuedBy";
     public static final String E_SERIAL_NO = "serialNo";
     public static final String E_ALGORITHM = "algorithm";
     public static final String E_VALIDITY = "validity";
     public static final String E_SIGNATURE = "signature";
     public static final String E_START_DATE = "startDate";
     public static final String E_END_DATE = "endDate";
-    public static final String A_PUB_CERT_ID = "pubCertId";
-    public static final String A_PVT_KEY_ID = "pvtKeyId";
-    public static final String A_DEFAULT = "default";
+    public static final String E_SUBJECT_ALT_NAME = "sujectAltName";
+    public static final String E_ISSUER_ALT_NAME = "issuerAltName";
+
+    //distinguished name
+    public static final String E_COUNTRY = "c";
+    public static final String E_STATE = "st";
+    public static final String E_CITY = "l";
+    public static final String E_ORG = "o";
+    public static final String E_ORG_UNIT = "ou";
+    public static final String E_COMMON_NAME = "cn";
+
+    //alt names
+    public static final String E_OTHER_NAME = "otherName";
+    public static final String E_RFC822_NAME = "rfc822Name";
+    public static final String E_DNS_NAME = "dNSName";
+    public static final String E_X400ADDRESS = "x400Address";
+    public static final String E_DIRECTORY_NAME = "directoryName";
+    public static final String E_EDI_PARTY_NAME ="ediPartyName";
+    public static final String E_URI = "uniformResourceIdentifier";
+    public static final String E_IP_ADDRESS = "iPAddress";
+    public static final String E_REGISTERED_ID = "registeredID";
 
     public static final String PUB_CERT = "pubCert";
     public static final String PVT_KEY = "pvtKey";
     public static final String ALIAS = "alias";
     public static final String CERT_FOLDER_NAME = "-smimecertificates";
-
+    public static final String SUBJECT_DN_KEY = "it_";
+    public static final String ISSUER_DN_KEY = "ib_";
+    public static final String SUBJECT_ALT_NAME_KEY = "san_";
+    public static final String ISSUER_ALT_NAME_KEY = "ian_";
 }
