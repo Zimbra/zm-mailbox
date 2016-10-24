e936e9ddd9e4575db9cee901ce87919cd1f039a3
ilya <raykini@zimbra.com>
2016-10-24 12:03:14 -0700

    added resetimapuid constant

--- common/src/java/com/zimbra/common/soap/MailConstants.java
+++ common/src/java/com/zimbra/common/soap/MailConstants.java
@@ -1230,7 +1230,7 @@ public final class MailConstants {
     public static final String A_SESSION = "session";
 
     public static final String A_ACTION = "action";
-    
+
     public static final String OP_TAG = "tag";
     public static final String OP_FLAG = "flag";
     public static final String OP_PRIORITY = "priority";
@@ -1249,4 +1249,5 @@ public final class MailConstants {
     public static final String OP_UNLOCK = "unlock";
     public static final String OP_INHERIT = "inherit";
     public static final String OP_MUTE = "mute";
+    public static final String OP_RESET_IMAP_UID = "resetimapuid";
 }
