2ae5b83e5340a3675d4007e2fe6bafeedcde3b4e
Greg Solovyev <greg@zimbra.com>
2016-10-24 15:39:39 -0700

    implement ListSubscription handler, register ListSubscriptionRequest/Response with JAXB

--- soap/src/java/com/zimbra/soap/account/message/ListSubscriptionsRequest.java
+++ soap/src/java/com/zimbra/soap/account/message/ListSubscriptionsRequest.java
@@ -1,7 +1,5 @@
 package com.zimbra.soap.account.message;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import com.zimbra.common.soap.AccountConstants;
@@ -11,9 +9,7 @@ import com.zimbra.common.soap.AccountConstants;
  * @zm-api-command-admin-auth-required false
  * @zm-api-command-description Return a list of subscribed folder names
  */
-@XmlAccessorType(XmlAccessType.NONE)
 @XmlRootElement(name=AccountConstants.E_LIST_SUBSCRIPTIONS_REQUEST)
-
 public class ListSubscriptionsRequest {
-
+    public ListSubscriptionsRequest() {}
 }
