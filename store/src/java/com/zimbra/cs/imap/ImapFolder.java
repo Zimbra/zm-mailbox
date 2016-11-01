45461e5d00501868650ac52468c76205eef576cb
Gren Elliot <gren.elliot@synacor.com>
2016-11-01 09:26:17 +0000

    ZMS-145:ImapListener interface
    Introduced the new ImapListener interface, which is implemented by
    ImapSession and the new ImapRemoteListener.
    
    Did some IMAP refactoring to use ImapListener objects instead
    of ImapSession objects.  As needed, added methods to ImapListener,
    leaving them throwing exceptions for now in ImapRemoteListener.

--- store/src/java/com/zimbra/cs/imap/ImapFolder.java
+++ store/src/java/com/zimbra/cs/imap/ImapFolder.java
@@ -63,7 +63,7 @@ public final class ImapFolder implements ImapSession.ImapFolderData, java.io.Ser
 
     // attributes of the folder itself, irrespective of the session state
     private transient ImapMailboxStore mailboxStore;
-    private transient ImapListener folderListener;
+    private transient ImapListener session;
     private transient ImapPath path;
     private transient SessionData sessionData;
     private transient Map<Integer, ImapMessage> messageIds;
@@ -143,9 +143,9 @@ public final class ImapFolder implements ImapSession.ImapFolderData, java.io.Ser
         imap.addAttribute("folder", path.asImapPath()).addAttribute("query", query);
     }
 
-    void setFolderListener(ImapListener value) {
-        assert(folderListener == null || folderListener == value || sessionData == null);
-        folderListener = value;
+    void setSession(ImapListener value) {
+        assert(session == null || session == value || sessionData == null);
+        session = value;
     }
 
     SessionData getSessionData() {
@@ -449,8 +449,8 @@ public final class ImapFolder implements ImapSession.ImapFolderData, java.io.Ser
                 idx--;
             } else {
                 ZimbraLog.imap.warn("message added out of order occurs before message which is already visible to client. Must renumber %s", i4msg);
-                folderListener.incrementRenumber(i4msg);
-                if (folderListener.isFailedRenumber(i4msg)) {
+                ((ImapSession)session).incrementRenumber(i4msg);
+                if (((ImapSession)session).isFailedRenumber(i4msg)) {
                     throw new ImapRenumberException();
                 }
                 //prev has higher UID, but it has already been displayed to client
@@ -1076,9 +1076,9 @@ public final class ImapFolder implements ImapSession.ImapFolderData, java.io.Ser
         return removed;
     }
 
-    void restore(ImapListener listener, SessionData sdata) throws ImapSessionClosedException, ServiceException {
-        folderListener = listener;
-        MailboxStore sessMbox = folderListener.getMailbox();
+    void restore(ImapListener sess, SessionData sdata) throws ImapSessionClosedException, ServiceException {
+        session = sess;
+        MailboxStore sessMbox = session.getMailbox();
         if (sessMbox == null) {
             mailboxStore = null;
             throw new ImapSessionClosedException();
@@ -1170,11 +1170,11 @@ public final class ImapFolder implements ImapSession.ImapFolderData, java.io.Ser
 
         added.sort();
         boolean recent = true;
-        for (ImapListener i4listener : mailboxStore.getListeners()) {
+        for (ImapListener i4session : mailboxStore.getListeners()) {
             // added messages are only \Recent if we're the first IMAP session notified about them
-            if (i4listener == folderListener) {
+            if (i4session == session) {
                 break;
-            } else if (i4listener.isWritable() && (i4listener.getFolderId() == folderId)) {
+            } else if (i4session.isWritable() && (i4session.getFolderId() == folderId)) {
                 recent = false;
                 break;
             }
