09c5bdf09947c2ae1491d5618558187d27b8c08e
Greg Solovyev <fiddlestring@gmail.com>
2016-11-03 16:23:31 -0700

    Remove ImapMailboxStore::getId, because it is not called anywhere. Also, not sure why IMAP would need to use database ID of a mailbox - it should really only use Account IDs

--- store/src/java/com/zimbra/cs/imap/RemoteImapMailboxStore.java
+++ store/src/java/com/zimbra/cs/imap/RemoteImapMailboxStore.java
@@ -179,11 +179,6 @@ public class RemoteImapMailboxStore extends ImapMailboxStore {
     }
 
     @Override
-    public int getId() {
-        throw new UnsupportedOperationException("RemoteImapMailboxStore method not supported yet");
-    }
-
-    @Override
     public MailboxStore getMailboxStore() {
         return zMailbox;
     }
