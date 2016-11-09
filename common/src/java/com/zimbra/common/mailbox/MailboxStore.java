39cf52f455855acea6a7dbca6bb154ca08641c69
Gren Elliot <gren.elliot@synacor.com>
2016-11-09 16:18:49 +0000

    ZMS-145:new ZimbraQueryHitResults search(...)

--- common/src/java/com/zimbra/common/mailbox/MailboxStore.java
+++ common/src/java/com/zimbra/common/mailbox/MailboxStore.java
@@ -79,4 +79,5 @@ public interface MailboxStore {
     public void setTags(OpContext octxt, Collection<ItemIdentifier> itemIds, int flags, Collection<String> tags)
             throws ServiceException;
     public ZimbraSearchParams createSearchParams(String queryString);
+    public ZimbraQueryHitResults search(OpContext octx, ZimbraSearchParams params) throws ServiceException;
 }
