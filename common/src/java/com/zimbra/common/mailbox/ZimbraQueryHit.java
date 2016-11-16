119a4db94c7822c887856bcf19f7cc7c1a8882f1
Gren Elliot <gren.elliot@synacor.com>
2016-11-16 16:22:29 +0000

    ZMS-204: ZimbraQueryHit new interface methods.
    Added getMailItemType() getImapUid() getFlagBitmask() getTags()

--- common/src/java/com/zimbra/common/mailbox/ZimbraQueryHit.java
+++ common/src/java/com/zimbra/common/mailbox/ZimbraQueryHit.java
@@ -22,4 +22,8 @@ public interface ZimbraQueryHit {
     public int getItemId() throws ServiceException;
     public int getParentId() throws ServiceException;
     public int getModifiedSequence() throws ServiceException;
+    public MailItemType getMailItemType() throws ServiceException;
+    public int getImapUid() throws ServiceException;
+    public int getFlagBitmask() throws ServiceException;
+    public String[] getTags() throws ServiceException;
 }
