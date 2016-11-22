c0ecada3f145410a064ebe1d12feb29f25add081
Greg Solovyev <fiddlestring@gmail.com>
2016-11-21 18:12:47 -0800

    clean imports

--- store/src/java/com/zimbra/qa/unittest/TestImapClient.java
+++ store/src/java/com/zimbra/qa/unittest/TestImapClient.java
@@ -24,16 +24,11 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.net.Socket;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
