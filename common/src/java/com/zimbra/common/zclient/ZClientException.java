a30eaa2ccb64915d36643406ca157f2307516f3a
Gren Elliot <gren.elliot@synacor.com>
2016-11-30 16:24:26 +0000

    ZMS-224:NO_SUCH_CONTACT/NO_SUCH_MSG

--- common/src/java/com/zimbra/common/zclient/ZClientException.java
+++ common/src/java/com/zimbra/common/zclient/ZClientException.java
@@ -68,12 +68,44 @@ public class ZClientException extends ServiceException {
     public static class ZClientNoSuchItemException extends ZClientException {
         private static final long serialVersionUID = 6322647788472338821L;
 
+        public ZClientNoSuchItemException(String msg, Throwable cause) {
+            super(msg, CLIENT_ERROR, SENDERS_FAULT, cause);
+        }
+
+        public ZClientNoSuchItemException(int id, Throwable cause) {
+            super("no such item: "+ id, CLIENT_ERROR, SENDERS_FAULT, cause);
+        }
+
         public ZClientNoSuchItemException(int id) {
-            super("no such item: "+ id, CLIENT_ERROR, SENDERS_FAULT, null);
+            this(id, null);
+        }
+    }
+
+    public static class ZClientNoSuchMsgException extends ZClientNoSuchItemException {
+        private static final long serialVersionUID = 7916158633910054737L;
+
+        public ZClientNoSuchMsgException(int id, Throwable cause) {
+            super("no such message: "+ id, cause);
+        }
+    }
+
+    public static class ZClientNoSuchContactException extends ZClientNoSuchItemException {
+        private static final long serialVersionUID = -6622409008893327197L;
+
+        public ZClientNoSuchContactException(int id, Throwable cause) {
+            super("no such contact: "+ id, cause);
         }
     }
 
     public static ZClientException NO_SUCH_ITEM(int id) {
         return new ZClientNoSuchItemException(id);
+    }
+
+    public static ZClientException NO_SUCH_CONTACT(int id, Throwable cause) {
+        return new ZClientNoSuchContactException(id, cause);
+    }
+
+    public static ZClientException NO_SUCH_MSG(int id, Throwable cause) {
+        return new ZClientNoSuchMsgException(id, cause);
     }
 }
