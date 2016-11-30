dbb935ac3a15dd139e31a639acfda2fe2192ac48
Gren Elliot <gren.elliot@synacor.com>
2016-11-30 17:14:23 +0000

    ZMS-244:ImapServerListener - basic scaffold

---
+++ store/src/java/com/zimbra/cs/imap/ImapServerListener.java
@@ -0,0 +1,31 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2016 Synacor, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.imap;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.account.Server;
+
+public class ImapServerListener {
+    final Server server;
+
+    ImapServerListener(Server svr) {
+        this.server = svr;
+    }
+
+    public void addListener(ImapRemoteSession listener) throws ServiceException {
+    }
+}
