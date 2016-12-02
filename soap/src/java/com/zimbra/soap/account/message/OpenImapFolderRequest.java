4d2f751eb13e6e9177c8354822f1e1afd0d555f9
ilya <raykini@zimbra.com>
2016-12-02 10:57:52 -0800

    OpenImapFolder pagination

--- soap/src/java/com/zimbra/soap/account/message/OpenImapFolderRequest.java
+++ soap/src/java/com/zimbra/soap/account/message/OpenImapFolderRequest.java
@@ -14,15 +14,33 @@ public class OpenImapFolderRequest {
         setFolderId(folderId);
     }
 
+    /**
+     * @zm-api-field-description The ID of the folder to open
+     */
     @XmlAttribute(name=MailConstants.A_FOLDER /* l */, required=true)
     private String folderId;
 
-    public void setFolderId(String id) {
-        this.folderId = id;
-    }
+    /**
+     * @zm-api-field-description The maximum number of results to return
+     */
+    @XmlAttribute(name=MailConstants.A_QUERY_LIMIT /* limit */, required=true)
+    private Integer limit;
 
-    public String getFolderId() {
-        return folderId;
-    }
+    /**
+     * @zm-api-field-description Specifies the 0-based offset into the message list
+     */
+    @XmlAttribute(name=MailConstants.A_QUERY_OFFSET /* offset */, required=false)
+    private Integer offset;
 
+    public void setFolderId(String id) { this.folderId = id; }
+
+    public String getFolderId() { return folderId; }
+
+    public void setLimit(Integer limit) { this.limit = limit; }
+
+    public Integer getLimit() { return limit; }
+
+    public void setOffset(Integer offset) { this.offset = offset; }
+
+    public Integer getOffset() { return offset == null ? 0 : offset; }
 }
