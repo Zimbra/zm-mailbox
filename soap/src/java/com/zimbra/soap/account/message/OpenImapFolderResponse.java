6fe7baf1bcc377d406c600e7e96731f0e4093541
ilya <raykini@zimbra.com>
2016-12-02 10:57:52 -0800

    OpenImapFolder pagination

--- soap/src/java/com/zimbra/soap/account/message/OpenImapFolderResponse.java
+++ soap/src/java/com/zimbra/soap/account/message/OpenImapFolderResponse.java
@@ -3,19 +3,30 @@ package com.zimbra.soap.account.message;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import com.zimbra.common.soap.AccountConstants;
 import com.zimbra.common.soap.MailConstants;
+import com.zimbra.soap.type.ZmBoolean;
 
 @XmlRootElement(name=AccountConstants.E_OPEN_IMAP_FOLDER_RESPONSE)
 public class OpenImapFolderResponse {
 
     public OpenImapFolderResponse() {}
 
-    @XmlElement(name=MailConstants.E_MSG, required=true)
+    /**
+     * @zm-api-field-description list of ImapMessageInfo elements
+     */
+    @XmlElement(name=MailConstants.E_MSG, /* msg */ required=true)
     private List<ImapMessageInfo> messages = new LinkedList<ImapMessageInfo>();
+
+    /**
+     * @zm-api-field-description whether there are more imap messages remaining in the folder
+     */
+    @XmlAttribute(name=MailConstants.A_QUERY_MORE /* more */, required=false)
+    private ZmBoolean hasMore;
 
     public void addImapMessageInfo(ImapMessageInfo info) {
         messages.add(info);
@@ -23,5 +34,12 @@ public class OpenImapFolderResponse {
 
     public List<ImapMessageInfo> getImapMessageInfo() {
         return messages;
+    }
+
+    public void setHasMore(Boolean bool) {
+        hasMore = ZmBoolean.fromBool(bool);
+    }
+    public boolean getHasMore() {
+        return ZmBoolean.toBool(hasMore, false);
     }
 }
