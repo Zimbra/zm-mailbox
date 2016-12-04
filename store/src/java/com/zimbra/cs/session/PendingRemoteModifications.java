0d48d6d8a6b26595f3fd13f3af79eac172f3189d
Gordon Tillman <gordon.tillman@synacor.com>
2016-12-04 13:01:11 -0600

    ZMS-265 Refactor ModificationKey
    - PendingModifications.ModificationKey
      - No longer final
      - Remove ctor with MailItem
    - PendingLocalModifications.ModificationKey
      - New subclass
      - Move ctor with MailItem here
    - PendingRemoteModifications.ModificationKey
      - New subclass
      - Add ctor with ZBaseItem

--- store/src/java/com/zimbra/cs/session/PendingRemoteModifications.java
+++ store/src/java/com/zimbra/cs/session/PendingRemoteModifications.java
@@ -17,6 +17,31 @@
 package com.zimbra.cs.session;
 
 import com.zimbra.client.ZBaseItem;
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.ZimbraLog;
 
 public final class PendingRemoteModifications extends PendingModifications<ZBaseItem> {
+
+    public static final class ModificationKey extends PendingModifications.ModificationKey {
+
+        /*
+         * TODO - Looks like we will be having to deal with ServiceExceptions in
+         * numerous places in PendingRemoteModifications. See if there is a
+         * cleaner way to handle them.
+         */
+        public ModificationKey(ZBaseItem item) {
+            super("", 0);
+            String actId = null;
+            int idInMbox = 0;
+            try {
+                actId = item.getMailbox().getAccountId();
+                idInMbox = item.getIdInMailbox();
+            } catch (ServiceException e) {
+                ZimbraLog.mailbox.warn("error retrieving account id or id in mailbox", e);
+            }
+            setAccountId(actId);
+            setItemId(Integer.valueOf(idInMbox));
+        }
+    }
+
 }
