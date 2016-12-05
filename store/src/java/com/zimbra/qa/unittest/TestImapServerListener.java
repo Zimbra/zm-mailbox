862cf9f2d8150297bbd977579931635b2eceaa03
Greg Solovyev <fiddlestring@gmail.com>
2016-12-05 14:31:14 -0800

    add asynchronous invocation to Soapprovisioning

--- store/src/java/com/zimbra/qa/unittest/TestImapServerListener.java
+++ store/src/java/com/zimbra/qa/unittest/TestImapServerListener.java
@@ -14,11 +14,14 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.google.common.collect.Maps;
+import com.zimbra.client.ZMailbox;
 import com.zimbra.common.account.ProvisioningConstants;
 import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.mailbox.MailboxStore;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.AdminConstants;
+import com.zimbra.common.soap.SoapTransport;
+import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.Server;
@@ -30,11 +33,20 @@ import com.zimbra.cs.imap.ImapHandler;
 import com.zimbra.cs.imap.ImapListener;
 import com.zimbra.cs.imap.ImapMailboxStore;
 import com.zimbra.cs.imap.ImapPath;
+import com.zimbra.cs.imap.ImapRemoteSession;
 import com.zimbra.cs.imap.ImapServerListener;
 import com.zimbra.cs.imap.ImapServerListenerPool;
 import com.zimbra.cs.imap.ImapProxy.ZimbraClientAuthenticator;
+import com.zimbra.cs.imap.ImapSessionClosedException;
+import com.zimbra.cs.imap.RemoteImapMailboxStore;
 import com.zimbra.cs.mailclient.auth.AuthenticatorFactory;
 import com.zimbra.cs.security.sasl.ZimbraAuthenticator;
+import com.zimbra.cs.session.PendingModifications;
+import com.zimbra.cs.session.Session;
+import com.zimbra.soap.JaxbUtil;
+import com.zimbra.soap.admin.message.QueryWaitSetRequest;
+import com.zimbra.soap.admin.message.QueryWaitSetResponse;
+import com.zimbra.soap.admin.type.WaitSetInfo;
 
 public class TestImapServerListener {
     private static final String LOCAL_USER_NAME = "TestImapServerListener-localuser";
@@ -113,14 +125,32 @@ public class TestImapServerListener {
     }
 
     @Test
-    public void testRegisterUnregister() throws ServiceException {
+    public void testNotify() throws Exception {
         Assume.assumeNotNull(remoteServer);
         Assume.assumeNotNull(remoteAccount);
-        MailboxStore mboxStore = TestUtil.getZMailbox(REMOTE_USER_NAME);
+        ZMailbox mboxStore = TestUtil.getZMailbox(REMOTE_USER_NAME);
+        ImapServerListener remoteListener = ImapServerListenerPool.getInstance().get(mboxStore);
+        RemoteImapMailboxStore imapStore = new RemoteImapMailboxStore(mboxStore);
+        ImapCredentials creds = new ImapCredentials(remoteAccount);
+        ImapPath path = new ImapPath("INBOX", creds);
+        byte params = 0;
+        ImapHandler handler = new MockImapHandler().setCredentials(creds);
+        ImapFolder i4folder = new ImapFolder(path, params, handler);
+        MockImapListener session =new MockImapListener(imapStore, i4folder, handler);
+        remoteListener.addListener(session);
+        TestUtil.addMessage(mboxStore, "TestImapServerListener - testNotify");
+        assertTrue("Expected session to be triggered", session.wasTriggered());
+        remoteListener.removeListener(session);
+    }
+    @Test
+    public void testRegisterUnregister() throws ServiceException, IOException {
+        Assume.assumeNotNull(remoteServer);
+        Assume.assumeNotNull(remoteAccount);
+        ZMailbox mboxStore = TestUtil.getZMailbox(REMOTE_USER_NAME);
         ImapServerListener remoteListener = ImapServerListenerPool.getInstance().get(mboxStore);
         assertNotNull("ImapServerListener instance should not be null", remoteListener);
         
-        ImapMailboxStore imapStore = ImapMailboxStore.get(mboxStore);
+        RemoteImapMailboxStore imapStore = new RemoteImapMailboxStore(mboxStore);
         assertNotNull("ImapMailboxStore instance should not be null", imapStore);
         ImapCredentials creds = new ImapCredentials(remoteAccount);
         ImapPath path = new ImapPath("INBOX", creds);
@@ -132,15 +162,29 @@ public class TestImapServerListener {
         assertNotNull("ImapFolder.getCredentials() should not return null", i4folder.getCredentials());
         assertNotNull("ImapFolder.getPath() should not return null", i4folder.getPath());
         
-        ImapListener session = imapStore.createListener(i4folder, handler);
+        ImapRemoteSession session = (ImapRemoteSession) imapStore.createListener(i4folder, handler);
         assertNotNull("ImapListener instance should not be null", session);
         assertFalse("Expecting ImapServerListener::isListeningOn to return false before calling addListener", remoteListener.isListeningOn(session.getTargetAccountId()));
+        assertNull("Should not have a waitset before registering first listener", remoteListener.getWSId());
         remoteListener.addListener(session);
         assertTrue("Expecting ImapServerListener::isListeningOn to return true after calling addListener", remoteListener.isListeningOn(session.getTargetAccountId()));
+        assertNotNull("Should have a waitset after registering first listener", remoteListener.getWSId());
+        QueryWaitSetRequest req = new QueryWaitSetRequest(remoteListener.getWSId());
+        SoapTransport transport = TestUtil.getAdminSoapTransport(remoteServer);
+        QueryWaitSetResponse resp = JaxbUtil.elementToJaxb(transport.invoke(JaxbUtil.jaxbToElement(req)));
+        assertNotNull(resp);
+        List<WaitSetInfo> wsInfoList = resp.getWaitsets();
+        assertNotNull(wsInfoList);
+        assertFalse(wsInfoList.isEmpty());
+        assertEquals(1, wsInfoList.size());
+        WaitSetInfo wsInfo = wsInfoList.get(0);
+        assertNotNull(wsInfo);
+        assertEquals(remoteListener.getWSId(), wsInfo.getWaitSetId());
         remoteListener.removeListener(session);
         assertFalse("Expecting ImapServerListener::isListeningOn to return false after calling removeListener", remoteListener.isListeningOn(session.getTargetAccountId()));
+        assertNull("Should not have a waitset after removing last listener", remoteListener.getWSId());
     }
-    
+
     class MockImapHandler extends ImapHandler {
 
         MockImapHandler() {
@@ -180,6 +224,22 @@ public class TestImapServerListener {
         @Override
         protected InetSocketAddress getLocalAddress() {
             return new InetSocketAddress("localhost", 0);
+        }
+    }
+    
+    class MockImapListener extends ImapRemoteSession {
+        private boolean triggered = false;
+        MockImapListener(ImapMailboxStore store, ImapFolder i4folder, ImapHandler handler) throws ServiceException {
+            super(store, i4folder, handler);
+            // TODO Auto-generated constructor stub
+        }
+
+        public void signalAccountChange() {
+            triggered = true;
+        }
+
+        public boolean wasTriggered() {
+            return triggered;
         }
     }
 }
