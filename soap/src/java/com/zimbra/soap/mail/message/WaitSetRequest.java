925ff37ddc20418274a6408156bf9a6bc9c070a7
Gren Elliot <gren.elliot@synacor.com>
2016-12-11 21:37:06 +0000

    ZMS-276:WaitSets monitor folders
    Changes to WaitSet related JAXB files to allow monitoring
    a subset of folders only.
    
    Some changes facilitate using JAXB files in server handlers whilst
    sharing code between different handlers

--- soap/src/java/com/zimbra/soap/mail/message/WaitSetRequest.java
+++ soap/src/java/com/zimbra/soap/mail/message/WaitSetRequest.java
@@ -17,10 +17,6 @@
 
 package com.zimbra.soap.mail.message;
 
-import com.google.common.base.Objects;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
-
 import java.util.Collections;
 import java.util.List;
 
@@ -31,7 +27,11 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.google.common.base.Objects;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.zimbra.common.soap.MailConstants;
+import com.zimbra.soap.base.WaitSetReq;
 import com.zimbra.soap.type.Id;
 import com.zimbra.soap.type.WaitSetAddSpec;
 import com.zimbra.soap.type.ZmBoolean;
@@ -40,7 +40,7 @@ import com.zimbra.soap.type.ZmBoolean;
  * @zm-api-command-auth-required true
  * @zm-api-command-admin-auth-required false
  * @zm-api-command-description WaitSetRequest optionally modifies the wait set and checks for any notifications.
- * If <b>block</b> is set and there are no notificatins, then this API will BLOCK until there is data.
+ * If <b>block</b> is set and there are no notifications, then this API will BLOCK until there is data.
  * <p>
  * Client should always set 'seq' to be the highest known value it has received from the server.  The server will use
  * this information to retransmit lost data.
@@ -51,7 +51,7 @@ import com.zimbra.soap.type.ZmBoolean;
  * If the client does not send a last known sync token, then notification is based on change since last Wait
  * (or change since &lt;add> if this is the first time Wait has been called with the account)
  * </p><p>
- * The client may specifiy a custom timeout-length for their request if they know something about the particular
+ * The client may specify a custom timeout-length for their request if they know something about the particular
  * underlying network.  The server may or may not honor this request (depending on server configured max/min values).
  * See LocalConfig values:
  * </p>
@@ -69,7 +69,7 @@ import com.zimbra.soap.type.ZmBoolean;
  */
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlRootElement(name=MailConstants.E_WAIT_SET_REQUEST)
-public class WaitSetRequest {
+public class WaitSetRequest implements WaitSetReq {
 
     /**
      * @zm-api-field-tag waitset-id
@@ -105,6 +105,7 @@ public class WaitSetRequest {
      * <tr> <td> <b>d</b> </td> <td> documents </td> </tr>
      * <tr> <td> <b>all</b> </td> <td> all types (equiv to "f,m,c,a,t,d") </td> </tr>
      * </table>
+     * <p>This is used if <b>types</b> isn't specified for an account</p>
      */
     @XmlAttribute(name=MailConstants.A_DEFTYPES /* defTypes */, required=false)
     private String defaultInterests;
@@ -121,21 +122,21 @@ public class WaitSetRequest {
      */
     @XmlElementWrapper(name=MailConstants.E_WAITSET_ADD /* add */, required=false)
     @XmlElement(name=MailConstants.E_A /* a */, required=false)
-    private List<WaitSetAddSpec> addAccounts = Lists.newArrayList();
+    private final List<WaitSetAddSpec> addAccounts = Lists.newArrayList();
 
     /**
      * @zm-api-field-description Waitsets to update
      */
     @XmlElementWrapper(name=MailConstants.E_WAITSET_UPDATE /* update */, required=false)
     @XmlElement(name=MailConstants.E_A /* a */, required=false)
-    private List<WaitSetAddSpec> updateAccounts = Lists.newArrayList();
+    private final List<WaitSetAddSpec> updateAccounts = Lists.newArrayList();
 
     /**
      * @zm-api-field-description Waitsets to remove
      */
     @XmlElementWrapper(name=MailConstants.E_WAITSET_REMOVE /* remove */, required=false)
     @XmlElement(name=MailConstants.E_A /* a */, required=false)
-    private List<Id> removeAccounts = Lists.newArrayList();
+    private final List<Id> removeAccounts = Lists.newArrayList();
 
     /**
      * no-argument constructor wanted by JAXB
@@ -150,11 +151,15 @@ public class WaitSetRequest {
         this.lastKnownSeqNo = lastKnownSeqNo;
     }
 
+    @Override
     public void setBlock(Boolean block) { this.block = ZmBoolean.fromBool(block); }
+    @Override
     public void setDefaultInterests(String defaultInterests) {
         this.defaultInterests = defaultInterests;
     }
+    @Override
     public void setTimeout(Long timeout) { this.timeout = timeout; }
+    @Override
     public void setAddAccounts(Iterable <WaitSetAddSpec> addAccounts) {
         this.addAccounts.clear();
         if (addAccounts != null) {
@@ -162,11 +167,13 @@ public class WaitSetRequest {
         }
     }
 
+    @Override
     public WaitSetRequest addAddAccount(WaitSetAddSpec addAccount) {
         this.addAccounts.add(addAccount);
         return this;
     }
 
+    @Override
     public void setUpdateAccounts(Iterable <WaitSetAddSpec> updateAccounts) {
         this.updateAccounts.clear();
         if (updateAccounts != null) {
@@ -174,11 +181,13 @@ public class WaitSetRequest {
         }
     }
 
+    @Override
     public WaitSetRequest addUpdateAccount(WaitSetAddSpec updateAccount) {
         this.updateAccounts.add(updateAccount);
         return this;
     }
 
+    @Override
     public void setRemoveAccounts(Iterable <Id> removeAccounts) {
         this.removeAccounts.clear();
         if (removeAccounts != null) {
@@ -186,22 +195,31 @@ public class WaitSetRequest {
         }
     }
 
+    @Override
     public WaitSetRequest addRemoveAccount(Id removeAccount) {
         this.removeAccounts.add(removeAccount);
         return this;
     }
 
+    @Override
     public String getWaitSetId() { return waitSetId; }
+    @Override
     public String getLastKnownSeqNo() { return lastKnownSeqNo; }
-    public Boolean getBlock() { return ZmBoolean.toBool(block); }
+    @Override
+    public Boolean getBlock() { return ZmBoolean.toBool(block, false); }
+    @Override
     public String getDefaultInterests() { return defaultInterests; }
+    @Override
     public Long getTimeout() { return timeout; }
+    @Override
     public List<WaitSetAddSpec> getAddAccounts() {
         return Collections.unmodifiableList(addAccounts);
     }
+    @Override
     public List<WaitSetAddSpec> getUpdateAccounts() {
         return Collections.unmodifiableList(updateAccounts);
     }
+    @Override
     public List<Id> getRemoveAccounts() {
         return Collections.unmodifiableList(removeAccounts);
     }
