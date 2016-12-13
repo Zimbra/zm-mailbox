9e14cc5988ff7abb9d19c1652af6542862b574eb
Gren Elliot <gren.elliot@synacor.com>
2016-12-13 11:36:41 +0000

    ZMS-276:Use Set for folderInterests not List

--- soap/src/java/com/zimbra/soap/type/WaitSetAddSpec.java
+++ soap/src/java/com/zimbra/soap/type/WaitSetAddSpec.java
@@ -18,7 +18,7 @@
 package com.zimbra.soap.type;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.Set;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -27,7 +27,7 @@ import javax.xml.bind.annotation.XmlTransient;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Objects;
-import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.zimbra.common.soap.MailConstants;
 
 @XmlAccessorType(XmlAccessType.NONE)
@@ -73,7 +73,7 @@ public class WaitSetAddSpec {
     @XmlAttribute(name=MailConstants.A_TYPES /* types */, required=false)
     private String interests;
 
-    private final List <Integer> folderInterests = Lists.newArrayList();
+    private final Set<Integer> folderInterests = Sets.newHashSet();
 
     public WaitSetAddSpec() {
     }
@@ -103,7 +103,7 @@ public class WaitSetAddSpec {
     }
 
     @XmlTransient
-    public List<Integer> getFolderInterestsAsList() { return folderInterests; }
+    public Set<Integer> getFolderInterestsAsSet() { return folderInterests; }
 
     public WaitSetAddSpec setFolderInterests(Integer... folderIds) {
         this.folderInterests.clear();
