4e4b71c188855c43e4023c6d864eae3f2c773e91
Gren Elliot <gren.elliot@synacor.com>
2016-12-14 15:50:54 +0000

    ZMS-293:Honor requested SOAP reply format
    Use `zsc.jaxbToElement(res)` instead of `JaxbUtil.jaxbToElement(res)`
    
    `zsc.jaxbToElement(res)` is actually a wrapper for:
    
        JaxbUtil.jaxbToElement(res, zsc.getResponseProtocol().getFactory());
    
    i.e. It takes note of the requested response protocol (XML or JSON)
         whereas `JaxbUtil.jaxbToElement(res)` always returns XML.
    
    Note:Some of these handlers did actually do the correct:
        JaxbUtil.jaxbToElement(res, zsc.getResponseProtocol().getFactory())
    but those were changed as well.

--- store/src/java/com/zimbra/cs/service/admin/FlushCache.java
+++ store/src/java/com/zimbra/cs/service/admin/FlushCache.java
@@ -214,6 +214,7 @@ public class FlushCache extends AdminDocumentHandler {
 
     private static void flushCacheOnAllServers(ZimbraSoapContext zsc, FlushCacheRequest req) throws ServiceException {
         req.getCache().setAllServers(false);  // make sure we don't go round in loops
+        Element request = zsc.jaxbToElement(req);
 
         Provisioning prov = Provisioning.getInstance();
         String localServerId = prov.getLocalServer().getId();
@@ -223,7 +224,6 @@ public class FlushCache extends AdminDocumentHandler {
                 continue;
             }
 
-            Element request = JaxbUtil.jaxbToElement(req);
             ZimbraLog.misc.debug("Flushing cache on server: %s", server.getName());
             String adminUrl = URLUtil.getAdminURL(server, AdminConstants.ADMIN_SERVICE_URI);
             SoapHttpTransport mTransport = new SoapHttpTransport(adminUrl);
