d4d6707b8493f04db9c2cd4dd4dd9381438fd41b
Gren Elliot <gren.elliot@synacor.com>
2016-12-15 14:46:12 +0000

    eclipse cleanup

--- store/src/java/com/zimbra/cs/service/admin/QueryWaitSet.java
+++ store/src/java/com/zimbra/cs/service/admin/QueryWaitSet.java
@@ -25,7 +25,6 @@ import com.zimbra.common.soap.AdminConstants;
 import com.zimbra.common.soap.Element;
 import com.zimbra.common.soap.MailConstants;
 import com.zimbra.cs.account.accesscontrol.AdminRight;
-import com.zimbra.cs.account.accesscontrol.Rights.Admin;
 import com.zimbra.cs.session.IWaitSet;
 import com.zimbra.cs.session.WaitSetMgr;
 import com.zimbra.soap.ZimbraSoapContext;
@@ -39,16 +38,16 @@ public class QueryWaitSet extends AdminDocumentHandler {
     @Override
     public Element handle(Element request, Map<String, Object> context)
     throws ServiceException {
-        
+
         ZimbraSoapContext zsc = getZimbraSoapContext(context);
         WaitSetMgr.checkRightForAllAccounts(zsc); // must be a global admin
-        
+
         Element response = zsc.createElement(AdminConstants.QUERY_WAIT_SET_RESPONSE);
-        
+
         String waitSetId = request.getAttribute(MailConstants.A_WAITSET_ID, null);
-        
+
         List<IWaitSet> sets;
-        
+
         if (waitSetId != null) {
             sets = new ArrayList<IWaitSet>(1);
             IWaitSet ws = WaitSetMgr.lookup(waitSetId);
@@ -66,7 +65,7 @@ public class QueryWaitSet extends AdminDocumentHandler {
         }
         return response;
     }
-    
+
     @Override
     public void docRights(List<AdminRight> relatedRights, List<String> notes) {
         notes.add(AdminRightCheckPoint.Notes.SYSTEM_ADMINS_ONLY);
