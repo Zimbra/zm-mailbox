40432cf6f0c0b021fc006522cb60842f72e0225d
ilya <raykini@zimbra.com>
2016-12-20 13:44:08 -0800

    fixed capitalization in class names

--- soap/src/java/com/zimbra/soap/account/message/BeginTrackingIMAPRequest.java
+++ soap/src/java/com/zimbra/soap/account/message/BeginTrackingIMAPRequest.java
@@ -5,6 +5,6 @@ import javax.xml.bind.annotation.XmlRootElement;
 import com.zimbra.common.soap.AccountConstants;
 
 @XmlRootElement(name=AccountConstants.E_BEGIN_TRACKING_IMAP_REQUEST)
-public class BeginTrackingImapRequest {
-    public BeginTrackingImapRequest() {};
+public class BeginTrackingIMAPRequest {
+    public BeginTrackingIMAPRequest() {};
 }
