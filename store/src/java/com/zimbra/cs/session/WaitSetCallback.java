81ad818c3c43fa7a6c3101c9f79d9f5e6eedd5ed
Gren Elliot <gren.elliot@synacor.com>
2016-12-20 18:03:39 +0000

    ZMS-276:WaitSet ChangedFolders in response
    Main aim of change is to report folder IDs changed in WaitSet responses
    when they are known.  If changedFolders is not in the response, should
    assume that changes may affect all monitored folders.
    * WaitSetCallback now an Object based on `WaitSetRequest.Callback` instead of being an interface
      See no value in having to change method signatures in 2 places without good reason.
      `dataReady` gains a `changedFolders` arg
    * WaitSetRequest - preloadMailboxes method to avoid duplicate code

--- store/src/java/com/zimbra/cs/session/WaitSetCallback.java
+++ store/src/java/com/zimbra/cs/session/WaitSetCallback.java
@@ -17,11 +17,75 @@
 package com.zimbra.cs.session;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
-/**
- * User-supplied callback which is set by doWait() and which is called when one 
- * or more of the waiting sessions has new data.
- */
-public interface WaitSetCallback {
-    void dataReady(IWaitSet ws, String seqNo, boolean cancelled, List<WaitSetError> errors, String[] signalledAccounts);
+import com.google.common.base.Objects;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.servlet.continuation.ResumeContinuationListener;
+
+public class WaitSetCallback {
+
+    public boolean completed = false;
+    public boolean canceled;
+    public Set<String> signalledAccounts;
+    public Map<String /*accountId*/, Set<Integer /* folderId */>> changedFolderIds;
+    public IWaitSet waitSet;
+    public String seqNo;
+    public IWaitSet ws;
+    public List<WaitSetError> errors = Lists.newArrayList();
+    public ResumeContinuationListener continuationResume;
+
+    public void dataReady(IWaitSet wset, String seqNum, boolean setCanceled, List<WaitSetError> inErrors,
+            Set<String> signalledAccts, Map<String /*accountId*/, Set<Integer /* folderId */>> changedFolders) {
+        boolean trace = ZimbraLog.session.isTraceEnabled();
+        synchronized(this) {
+            if (inErrors != null && inErrors.size() > 0) {
+                errors.addAll(inErrors);
+            }
+            this.waitSet = wset;
+            this.canceled = setCanceled;
+            this.signalledAccounts = (signalledAccts == null) ? Sets.newHashSetWithExpectedSize(0)
+                                                              : Sets.newCopyOnWriteArraySet(signalledAccts);
+            if (changedFolders == null) {
+                this.changedFolderIds = Maps.newHashMapWithExpectedSize(0);
+            } else {
+                this.changedFolderIds = Maps.newHashMapWithExpectedSize(changedFolders.size());
+                for (Entry<String, Set<Integer>> entry : changedFolders.entrySet()) {
+                    this.changedFolderIds.put(entry.getKey(), Sets.newCopyOnWriteArraySet(entry.getValue()));
+                }
+            }
+            this.seqNo = seqNum;
+            this.completed = true;
+            ZimbraLog.session.debug("dataReady called. %s", this);
+            if (continuationResume != null) {
+                if (trace) {
+                    ZimbraLog.session.trace("dataReady calling resumeIfSuspended(). %s", this);
+                }
+                continuationResume.resumeIfSuspended();
+            }
+        }
+        if (trace) {
+            ZimbraLog.session.trace("dataReady DONE. %s", this);
+        }
+    }
+    public void dataReadySetCanceled(IWaitSet wset, String seqNum) {
+        dataReady(wset, seqNum, true, null, null, null);
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this)
+                .add("seqNo", seqNo)
+                .add("completed", completed)
+                .add("canceled", canceled)
+                .add("signalledAccounts", signalledAccounts)
+                .add("errors", errors)
+                .add("hashCode()", hashCode())
+                .toString();
+    }
 }
