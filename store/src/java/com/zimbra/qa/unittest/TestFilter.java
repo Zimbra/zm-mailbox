7d5b6bbddb68134c49918105576c162f22e2ea6c
Greg Solovyev <fiddlestring@gmail.com>
2016-12-30 18:50:16 -0500

    Upgrade to JUnit4. Remove dependency on user1 and user2. Check for test data files and add an informative message if they are missing. Make sure test accounts are created locally

--- store/src/java/com/zimbra/qa/unittest/TestFilter.java
+++ store/src/java/com/zimbra/qa/unittest/TestFilter.java
@@ -133,8 +133,12 @@ public final class TestFilter {
     public void setUp() throws Exception {
         cleanUp();
 
-        user1 = TestUtil.createAccount(USER_NAME);
-        TestUtil.createAccount(REMOTE_USER_NAME);
+        Map<String, Object> attrs = Maps.newHashMap();
+        attrs.put(Provisioning.A_zimbraMailHost, localServer.getServiceHostname());
+        user1 = TestUtil.createAccount(USER_NAME, attrs);
+        attrs = Maps.newHashMap();
+        attrs.put(Provisioning.A_zimbraMailHost, localServer.getServiceHostname());
+        TestUtil.createAccount(REMOTE_USER_NAME, attrs);
         mMbox = TestUtil.getZMailbox(USER_NAME);
         mTag1 = mMbox.createTag(TAG1_NAME, null);
         mTag2 = mMbox.createTag(TAG2_NAME, null);
