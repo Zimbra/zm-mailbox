b9d5891cc8217a879f4b4f9fd5cb1f328d8e71e5
chinmay15 <15.chinmay@gmail.com>
2017-01-03 11:09:38 +0530

    ZCS-367 : Get/ModifyFilter SIEVE SOAP API (ADMIN)

---
+++ src/java/com/zimbra/soap/admin/message/GetFilterRulesResponse.java
@@ -0,0 +1,75 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011, 2012, 2013, 2014 Zimbra, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.soap.admin.message;
+
+import com.google.common.base.Objects;
+import com.google.common.collect.Lists;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.zimbra.common.soap.AdminConstants;
+import com.zimbra.soap.json.jackson.annotate.ZimbraJsonArrayForWrapper;
+import com.zimbra.soap.mail.type.FilterRule;
+
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name=AdminConstants.E_GET_FILTER_RULES_RESPONSE)
+public final class GetFilterRulesResponse {
+
+    /**
+     * @zm-api-field-description Filter rules
+     */
+    @ZimbraJsonArrayForWrapper
+    @XmlElementWrapper(name=AdminConstants.E_FILTER_RULES /* filterRules */, required=true)
+    @XmlElement(name=AdminConstants.E_FILTER_RULE /* filterRule */, required=false)
+    private final List<FilterRule> rules = Lists.newArrayList();
+
+    public GetFilterRulesResponse() {
+    }
+
+    public void setFilterRules(Collection<FilterRule> list) {
+        rules.clear();
+        if (list != null) {
+            rules.addAll(list);
+        }
+    }
+
+    public void addFilterRule(FilterRule rule) {
+        rules.add(rule);
+    }
+
+    public void addFilterRule(Collection<FilterRule> list) {
+        rules.addAll(list);
+    }
+
+    public List<FilterRule> getFilterRules() {
+        return Collections.unmodifiableList(rules);
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this).add("rules", rules).toString();
+    }
+}
