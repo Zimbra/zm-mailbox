d6eb4bea463731d00be25240d069e5f43d444796
Greg Solovyev <fiddlestring@gmail.com>
2017-01-03 19:19:54 -0500

    address Gren's comment and remove redundant initialization code

--- store/src/java/com/zimbra/qa/unittest/TestDbUtil.java
+++ store/src/java/com/zimbra/qa/unittest/TestDbUtil.java
@@ -54,9 +54,7 @@ public class TestDbUtil {
 
     @Before
     public void setUp() throws Exception {
-        Map<String, Object> attrs = Maps.newHashMap();
-        attrs.put(Provisioning.A_zimbraMailHost, localServer.getServiceHostname());
-        TestUtil.createAccount(USER_NAME, attrs);
+        TestUtil.createAccount(USER_NAME);
         mbox = TestUtil.getMailbox(USER_NAME);
     }
 
