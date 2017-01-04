0b2252a17dcc87eb844b24113cea3a273d00e52a
Gordon Tillman <gordon.tillman@synacor.com>
2017-01-04 13:54:02 -0600

    ZMS-289 JAXB classes for RecordIMAPSession*

---
+++ soap/src/java/com/zimbra/soap/mail/message/RecordIMAPSessionResponse.java
@@ -0,0 +1,57 @@
+package com.zimbra.soap.mail.message;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.google.common.base.Objects;
+import com.zimbra.common.soap.MailConstants;
+
+@XmlRootElement(name=MailConstants.E_RECORD_IMAP_SESSION_RESPONSE)
+public class RecordIMAPSessionResponse {
+    /**
+     * @zm-api-field-description ID of last item created in mailbox
+     */
+    @XmlElement(name=MailConstants.A_ID /* id */, required=true)
+    private int lastItemId;
+
+    /**
+     * @zm-api-field-description UUID of the affected Folder
+     */
+    @XmlElement(name=MailConstants.A_FOLDER_UUID /* luuid */, required=true)
+    private String folderUuid;
+
+    public RecordIMAPSessionResponse() { }
+
+    public RecordIMAPSessionResponse(int lastItemId, String folderUuid) {
+        setLastItemId(lastItemId);
+        setFolderUuid(folderUuid);
+    }
+
+    public String getFolderUuid() {
+        return folderUuid;
+    }
+
+    public int getLastItemId() {
+        return lastItemId;
+    }
+
+    public void setFolderUuid (String folderUuid) {
+        this.folderUuid = folderUuid;
+    }
+
+    public void setLastItemId(int lastItemId) {
+        this.lastItemId = lastItemId;
+    }
+
+    public Objects.ToStringHelper addToStringInfo(Objects.ToStringHelper helper) {
+        return helper
+                .add(MailConstants.A_ID, getLastItemId())
+                .add(MailConstants.A_FOLDER_UUID, getFolderUuid());
+
+    }
+
+    @Override
+    public String toString() {
+        return addToStringInfo(Objects.toStringHelper(this)).toString();
+    }
+}
