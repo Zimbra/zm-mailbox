1dae829b84073205ce40a4ca0ee273eae84c56f8
Gren Elliot <gren.elliot@synacor.com>
2017-01-07 21:42:55 +0000

    ZMS-263ZMailbox getTransportURI/getResource
    Introduced getTransportURI so have visibility of other URIs besides
    the REST API - for instance the content servlet.
    
    Factored out reusable code in getRESTResource to getResource

--- client/src/java/com/zimbra/client/ZMailbox.java
+++ client/src/java/com/zimbra/client/ZMailbox.java
@@ -22,6 +22,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLConnection;
@@ -2915,6 +2916,43 @@ public class ZMailbox implements ToZJSONObject, MailboxStore {
         }
     }
 
+    public URI getTransportURI(String relativePath) throws ServiceException {
+        String pathPrefix = (relativePath.startsWith("/")) ? "" : "/";
+        try {
+            URI uri = new URI(mTransport.getURI());
+            return  uri.resolve(pathPrefix + relativePath);
+        } catch (URISyntaxException e) {
+            throw ZClientException.CLIENT_ERROR("unable to parse URI: "+mTransport.getURI(), e);
+        }
+    }
+
+    public InputStream getResource(URI uri) throws ServiceException {
+        return getResource(uri, null, null, getTimeout());
+    }
+
+    private InputStream getResource(URI uri, String startTimeArg, String endTimeArg, int msecTimeout)
+    throws ServiceException {
+        GetMethod get = null;
+        try {
+            HttpClient client = getHttpClient(uri);
+            get = new GetMethod(uri.toString());
+            if (msecTimeout > -1) {
+                get.getParams().setSoTimeout(msecTimeout);
+            }
+            int statusCode = HttpClientUtil.executeMethod(client, get);
+            // parse the response
+            if (statusCode == HttpServletResponse.SC_OK) {
+                return new GetMethodInputStream(get);
+            } else {
+                String msg = String.format("GET from %s failed, status=%d.  %s", uri, statusCode, get.getStatusText());
+                throw ServiceException.FAILURE(msg, null);
+            }
+        } catch (IOException e) {
+            String msg = String.format("Unable to get resource from '%s' : %s", uri, e.getMessage());
+            throw ZClientException.IO_ERROR(msg, e);
+        }
+    }
+
     /**
      * returns a rest URL relative to this mailbox.
      * @param relativePath a relative path (i.e., "/Calendar", "Inbox?fmt=rss", etc).
@@ -2984,11 +3022,8 @@ public class ZMailbox implements ToZJSONObject, MailboxStore {
 
     private InputStream getRESTResource(String relativePath, String startTimeArg, String endTimeArg,
             int msecTimeout, String alternateUrl)
-                    throws ServiceException {
-        GetMethod get = null;
+    throws ServiceException {
         URI uri = null;
-
-        int statusCode;
         try {
             if (startTimeArg != null) {
                 String encodedArg = URLEncoder.encode(startTimeArg, "UTF-8");
@@ -3006,32 +3041,13 @@ public class ZMailbox implements ToZJSONObject, MailboxStore {
                     relativePath = relativePath + "&end=" + encodedArg;
                 }
             }
-
-            uri = getRestURI(relativePath, alternateUrl);
-            HttpClient client = getHttpClient(uri);
-
-            get = new GetMethod(uri.toString());
-
-            if (msecTimeout > -1) {
-                get.getParams().setSoTimeout(msecTimeout);
-            }
-
-            statusCode = HttpClientUtil.executeMethod(client, get);
-            // parse the response
-            if (statusCode == HttpServletResponse.SC_OK) {
-                return new GetMethodInputStream(get);
-            } else {
-                String msg = String.format("GET from %s failed, status=%d.  %s", uri.toString(), statusCode, get.getStatusText());
-                throw ServiceException.FAILURE(msg, null);
-            }
-        } catch (IOException e) {
-            String fromUri = "";
-            if (uri != null) {
-                fromUri = " from " + uri.toString();
-            }
-            String msg = String.format("Unable to get REST resource%s: %s", fromUri, e.getMessage());
+        } catch (UnsupportedEncodingException e) {
+            String msg = String.format("Unable to get REST resource relativePath=%s start=%s end=%s: %s",
+                    relativePath, startTimeArg, endTimeArg, e.getMessage());
             throw ZClientException.IO_ERROR(msg, e);
         }
+        uri = getRestURI(relativePath, alternateUrl);
+        return getResource(uri, startTimeArg, endTimeArg, msecTimeout);
     }
 
     /**
