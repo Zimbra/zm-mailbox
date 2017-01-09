0c445d5935d2e57f4823ba644126ba7a7c5251c4
Travis McLane <travis.mclane@synacor.com>
2017-01-09 16:46:57 -0600

    ZMS-322 implement TestRemoteImap::testIdleNotification

--- store/src/java/com/zimbra/qa/unittest/TestRemoteImap.java
+++ store/src/java/com/zimbra/qa/unittest/TestRemoteImap.java
@@ -10,6 +10,9 @@ import java.io.IOException;
 import java.sql.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 import javax.mail.MessagingException;
 import javax.security.auth.login.LoginException;
@@ -44,15 +47,18 @@ import com.zimbra.cs.mailclient.auth.AuthenticatorFactory;
 import com.zimbra.cs.mailclient.imap.AppendResult;
 import com.zimbra.cs.mailclient.imap.Body;
 import com.zimbra.cs.mailclient.imap.BodyStructure;
+import com.zimbra.cs.mailclient.imap.CAtom;
 import com.zimbra.cs.mailclient.imap.Envelope;
 import com.zimbra.cs.mailclient.imap.Flags;
 import com.zimbra.cs.mailclient.imap.IDInfo;
 import com.zimbra.cs.mailclient.imap.ImapConfig;
 import com.zimbra.cs.mailclient.imap.ImapConnection;
+import com.zimbra.cs.mailclient.imap.ImapResponse;
 import com.zimbra.cs.mailclient.imap.ListData;
 import com.zimbra.cs.mailclient.imap.Literal;
 import com.zimbra.cs.mailclient.imap.MailboxInfo;
 import com.zimbra.cs.mailclient.imap.MessageData;
+import com.zimbra.cs.mailclient.imap.ResponseHandler;
 import com.zimbra.cs.security.sasl.ZimbraAuthenticator;
 import com.zimbra.cs.service.AuthProvider;
 import com.zimbra.cs.service.AuthProviderException;
@@ -400,5 +406,52 @@ public class TestRemoteImap {
         assertNotNull(body);
         Literal imapData = (Literal) body.getData();
         assertEquals(MessageBuilder.DEFAULT_MESSAGE_BODY, imapData.toString());
+    }
+
+    @Test
+    public void testIdleNotification() throws Exception {
+        ImapConnection connection2 = connect(imapServer);
+
+        connection.select("INBOX");
+
+        Flags flags = Flags.fromSpec("afs");
+        Date date = new Date(System.currentTimeMillis());
+        String subject = "TestRemoteImap-testIdleNotification";
+        ZMailbox zmbox = TestUtil.getZMailbox(USER);
+        TestUtil.addMessage(zmbox, subject, "1", null);
+        Literal msg = TestImap.message(100000);
+        final AtomicLong exists = new AtomicLong(-1);
+        final AtomicLong recent = new AtomicLong(-1);
+        final CountDownLatch doneSignal = new CountDownLatch(1);
+
+        connection.idle(new ResponseHandler() {
+            @Override
+            public void handleResponse(ImapResponse res) {
+                if (res.getCCode() == CAtom.EXISTS) {
+                    exists.set(res.getNumber());
+                }
+                if (res.getCCode() == CAtom.RECENT) {
+                    recent.set(1);
+                }
+            }
+        });
+        Assert.assertTrue("Connection is not idling when it should be", connection.isIdling());
+
+        try {
+	        AppendResult res = connection2.append("INBOX", flags, date, msg);
+	        Assert.assertNotNull(res);
+
+	        try {
+	            doneSignal.await(5, TimeUnit.SECONDS);
+	        } catch (Exception e) {
+	            Assert.fail("Wait interrupted. ");
+	        }
+
+	        Assert.assertEquals("Connection was not notified of new items", 1, recent.get());
+	        Assert.assertEquals("Connection was not notified of existing items", 1, exists.get());
+
+        } finally {
+            msg.dispose();
+        }
     }
 }
