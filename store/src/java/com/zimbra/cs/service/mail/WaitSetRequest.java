87fd21fdd321dc42217a27859fbfa2e9fe39bed3
Greg Solovyev <fiddlestring@gmail.com>
2017-01-09 19:18:40 -0500

    add reporting of created items using new SOAP types.

--- store/src/java/com/zimbra/cs/service/mail/WaitSetRequest.java
+++ store/src/java/com/zimbra/cs/service/mail/WaitSetRequest.java
@@ -33,8 +33,10 @@ import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import com.zimbra.client.ZBaseItem;
 import com.zimbra.common.account.Key.AccountBy;
 import com.zimbra.common.localconfig.LC;
+import com.zimbra.common.mailbox.BaseItemInfo;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
 import com.zimbra.common.soap.MailConstants;
@@ -53,12 +55,17 @@ import com.zimbra.cs.session.WaitSetCallback;
 import com.zimbra.cs.session.WaitSetError;
 import com.zimbra.cs.session.WaitSetMgr;
 import com.zimbra.cs.session.WaitSetSession;
+import com.zimbra.cs.session.PendingModifications.Change;
+import com.zimbra.cs.session.PendingModifications.ModificationKey;
 import com.zimbra.soap.SoapServlet;
 import com.zimbra.soap.ZimbraSoapContext;
+import com.zimbra.soap.account.message.ImapMessageInfo;
 import com.zimbra.soap.base.WaitSetReq;
 import com.zimbra.soap.base.WaitSetResp;
 import com.zimbra.soap.mail.message.WaitSetResponse;
+import com.zimbra.soap.mail.type.CreateItemNotification;
 import com.zimbra.soap.mail.type.ItemSpec;
+import com.zimbra.soap.mail.type.ModifyNotification;
 import com.zimbra.soap.mail.type.PendingFolderModifications;
 import com.zimbra.soap.type.AccountWithModifications;
 import com.zimbra.soap.type.Id;
@@ -286,39 +293,45 @@ public class WaitSetRequest extends MailDocumentHandler {
                 PendingModifications accountMods = cb.pendingModifications.get(signalledAccount);
                 if(accountMods!= null && accountMods.created != null) {
                     for(Object mod : accountMods.created.values()) {
-                        if(mod instanceof MailItem) {
-                            Integer folderId = ((MailItem)mod).getFolderId();
+                        if(mod instanceof BaseItemInfo) {
+                            Integer folderId = ((BaseItemInfo)mod).getFolderIdInMailbox();
                             if(folderInterests != null && !folderInterests.contains(folderId)) {
                                 continue;
                             }
-                            getFolderMods(folderId, folderMap).addCreatedItem(getItemSpec((MailItem)mod, Integer.toString(folderId)));
+                            getFolderMods(folderId, folderMap).addCreatedItem(getCreatedItemSOAP((BaseItemInfo)mod));
                         }
                     }
                 }
 
-                if(accountMods!= null && accountMods.modified != null) {
+                //TODO: process item modifications
+                /*if(accountMods!= null && accountMods.modified != null) {
                     for(Object mod : accountMods.modified.values()) {
-                        if(mod instanceof MailItem) {
-                            Integer folderId = ((MailItem)mod).getFolderId();
-                            if(folderInterests != null && !folderInterests.contains(folderId)) {
-                                continue;
+                        if(mod instanceof Change) {
+                            Integer folderId = ((Change)mod).getFolderId();    
+                            Object what = ((Change) mod).what;
+                            if(what != null && what instanceof BaseItemInfo) {
+                                BaseItemInfo itemInfo = (BaseItemInfo)what;
+                                if(folderInterests != null && !folderInterests.contains(folderId)) {
+                                    continue;
+                                }
+                                getFolderMods(folderId, folderMap).addModifiedItem(getModifiedItemSOAP(itemInfo, ((Change) mod).why));
                             }
-                            getFolderMods(folderId, folderMap).addModifiedItem(getItemSpec((MailItem)mod, Integer.toString(folderId)));
                         }
                     }
-                }
+                }*/
 
-                if(accountMods!= null && accountMods.deleted != null) {
+                //TODO: process deletes
+                /*if(accountMods!= null && accountMods.deleted != null) {
                     for(Object mod : accountMods.deleted.values()) {
                         if(mod instanceof MailItem) {
                             Integer folderId = ((MailItem)mod).getFolderId();
                             if(folderInterests != null && !folderInterests.contains(folderId)) {
                                 continue;
                             }
-                            getFolderMods(folderId, folderMap).addDeletedItem(getItemSpec((MailItem)mod, Integer.toString(folderId)));;
+                            //getFolderMods(folderId, folderMap).addDeletedItem(getDeletedItemSOAP((MailItem)mod, Integer.toString(folderId)));;
                         }
                     }
-                }
+                }*/
                 if(folderInterests!= null && !folderInterests.isEmpty() && !folderMap.isEmpty()) {
                     //interested only in specific folders
                     if(expand) {
@@ -338,6 +351,24 @@ public class WaitSetRequest extends MailDocumentHandler {
         resp.setErrors(encodeErrors(cb.errors));
     }
 
+    private static CreateItemNotification getCreatedItemSOAP(BaseItemInfo mod) throws ServiceException {
+        String tags = mod.getTags() == null ? null : Joiner.on(",").join(mod.getTags());
+        ImapMessageInfo messageInfo = new ImapMessageInfo(mod.getIdInMailbox(), mod.getImapUid(), mod.getMailItemType().toString(), mod.getFlagBitmask(), tags);
+        return new CreateItemNotification(messageInfo);
+    }
+
+    private static ModifyNotification getModifiedItemSOAP(BaseItemInfo mod, int reason) throws ServiceException {
+        String tags = mod.getTags() == null ? null : Joiner.on(",").join(mod.getTags());
+        ImapMessageInfo messageInfo = new ImapMessageInfo(mod.getIdInMailbox(), mod.getImapUid(), mod.getMailItemType().toString(), mod.getFlagBitmask(), tags);
+        return new ModifyNotification.ModifyItemNotification(messageInfo, reason);
+    }
+
+    /*private static ModifyNotification getDeletedItemSOAP(int itemId) throws ServiceException {
+        String tags = mod.getTags() == null ? null : Joiner.on(",").join(mod.getTags());
+        ImapMessageInfo messageInfo = new ImapMessageInfo(mod.getIdInMailbox(), mod.getImapUid(), mod.getMailItemType().toString(), mod.getFlagBitmask(), tags);
+        return new ModifyNotification.DeleteItemNotification(itemId);
+    }*/
+    
     private static ItemSpec getItemSpec(MailItem mod, String folderId) {
         ItemSpec item = new ItemSpec();
         item.setFolder(folderId);
