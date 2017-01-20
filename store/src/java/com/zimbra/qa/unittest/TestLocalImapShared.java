2e52516ac94389ce1ff92901cfcdeda80533857a
Gren Elliot <gren.elliot@synacor.com>
2017-01-20 18:32:33 +0000

    Docs on how shared tests work

--- store/src/java/com/zimbra/qa/unittest/TestLocalImapShared.java
+++ store/src/java/com/zimbra/qa/unittest/TestLocalImapShared.java
@@ -10,6 +10,14 @@ import com.zimbra.common.localconfig.ConfigException;
 import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.service.ServiceException;
 
+/**
+ * This is a shell test for Local IMAP tests that does the necessary configuration to select
+ * the local variant of IMAP access.
+ *
+ * The Remote equivalent is {@Link TestRemoteImapShared}
+ *
+ * The actual tests that are run are in {@link SharedImapTests}
+ */
 public class TestLocalImapShared extends SharedImapTests {
     private static boolean saved_imap_always_use_remote_store;
 
