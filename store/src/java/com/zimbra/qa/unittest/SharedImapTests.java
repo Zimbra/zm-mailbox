5927f9a15df8d2d497d0ab27be9c21be1504e415
Gren Elliot <gren.elliot@synacor.com>
2017-01-20 18:31:48 +0000

    ZMS-322:Add SharedImapTests.testIdleNotification
    Moved `testIdleNotification()` to `SharedImapTests` so that it can be
    used from  both `TestLocalImapShared` and `TestRemoteImapShared`

--- store/src/java/com/zimbra/qa/unittest/SharedImapTests.java
+++ store/src/java/com/zimbra/qa/unittest/SharedImapTests.java
@@ -2,12 +2,18 @@ package com.zimbra.qa.unittest;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.mail.MessagingException;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.zimbra.client.ZFolder;
@@ -18,14 +24,22 @@ import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.Server;
 import com.zimbra.cs.account.soap.SoapProvisioning;
+import com.zimbra.cs.mailclient.imap.AppendResult;
 import com.zimbra.cs.mailclient.imap.Body;
 import com.zimbra.cs.mailclient.imap.BodyStructure;
 import com.zimbra.cs.mailclient.imap.Envelope;
+import com.zimbra.cs.mailclient.imap.Flags;
 import com.zimbra.cs.mailclient.imap.ImapConfig;
 import com.zimbra.cs.mailclient.imap.ImapConnection;
+import com.zimbra.cs.mailclient.imap.ImapResponse;
+import com.zimbra.cs.mailclient.imap.Literal;
 import com.zimbra.cs.mailclient.imap.MailboxInfo;
 import com.zimbra.cs.mailclient.imap.MessageData;
+import com.zimbra.cs.mailclient.imap.ResponseHandler;
 
+/**
+ * Definitions of tests used from {@Link TestLocalImapShared} and {@Link TestRemoteImapShared}
+ */
 public abstract class SharedImapTests {
     private static final String USER = "SharedImapTests-user";
     private static final String PASS = "test123";
@@ -101,5 +115,64 @@ public abstract class SharedImapTests {
         Body[] body = md.getBodySections();
         assertNotNull("body", body);
         assertEquals(1, body.length);
+    }
+
+    @Test
+    public void testIdleNotification() throws IOException, ServiceException, MessagingException {
+        final ImapConnection connection1 = connect(imapServer);
+        connection1.login(PASS);
+        connection1.select("INBOX");
+        ImapConnection connection2 = connect(imapServer);
+        connection2.login(PASS);
+        try {
+            Flags flags = Flags.fromSpec("afs");
+            Date date = new Date(System.currentTimeMillis());
+            String subject = "SharedImapTest-testIdleNotification";
+            ZMailbox zmbox = TestUtil.getZMailbox(USER);
+            TestUtil.addMessage(zmbox, subject, "1", null);
+            Literal msg = TestImap.message(1000);
+            final AtomicBoolean gotExists = new AtomicBoolean(false);
+            final AtomicBoolean gotRecent = new AtomicBoolean(false);
+            final CountDownLatch doneSignal = new CountDownLatch(1);
+
+            // Kick off an IDLE command - which will be processed in another thread until we call stopIdle()
+            connection1.idle(new ResponseHandler() {
+                @Override
+                public void handleResponse(ImapResponse res) {
+                    if ("* 1 EXISTS".equals(res.toString())) {
+                        gotExists.set(true);
+                    }
+                    if ("* 1 RECENT".equals(res.toString())) {
+                        gotRecent.set(true);
+                    }
+                    if (gotExists.get() && gotRecent.get()) {
+                        doneSignal.countDown();
+                    }
+                }
+            });
+            assertTrue("Connection is not idling when it should be", connection1.isIdling());
+
+            try {
+                AppendResult res = connection2.append("INBOX", flags, date, msg);
+                assertNotNull(res);
+            } finally {
+                msg.dispose();
+            }
+
+            try {
+                doneSignal.await(10, TimeUnit.SECONDS);
+            } catch (Exception e) {
+                Assert.fail("Wait interrupted. ");
+            }
+            assertTrue("Connection is not idling when it should be", connection1.isIdling());
+            connection1.stopIdle();
+            assertTrue("Connection is idling when it should NOT be", !connection1.isIdling());
+            MailboxInfo mboxInfo = connection1.getMailboxInfo();
+            assertEquals("Connection was not notified of correct number of existing items", 1, mboxInfo.getExists());
+            assertEquals("Connection was not notified of correct number of recent items", 1, mboxInfo.getRecent());
+        } finally {
+            connection1.close();
+            connection2.close();
+        }
     }
 }
