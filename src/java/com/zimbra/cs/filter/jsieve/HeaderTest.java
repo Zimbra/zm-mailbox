e5f3d68e4f278c306da620b6f6fb3ebeec5c8668
Yasuko Komiyama <yasuko.komiyama@synacor.com>
2017-01-26 07:09:26 -0500

    ZCS-555/Bug-107330:SIEVE:Variable evaluated twice
    SIEVE: when variable "dollar" is assigned to '$', filter
    rule "*${dollar}{sample}*" does not assign ${1} ${2} correctly
    
    [Bug]
    When the following filter is defined:
    ```
    set "dollar" "$";
    set "sample" "test text";
    if header :matches :comparator "i;ascii-casemap" "Subject" "*${dollar}{sample}*" {
      log "1: [${1}]";
      log "2: [${2}]";
    }
    ```
    and when the triggering message has a header
    | Subject: abc${sample}xyz test text 123
    
    then the if-header test matches the Subject header, and it
    returns TRUE, but it does not assign the ${1} and ${2} correctly;
    it mistakenly assigned "abctest textxyz " and " 123" respectively.
    
    It seems at first when the Subject value is compared, the match
    pattern "*${sample}*" is used, but after that when the value of
    the variables ${1} and ${2} are composed, the previous match
    pattern is replaced one more time with the variable "sample",
    therefore the match pattern "*test text*" is used.
    
    Correctly, the following log events are expected:
    | 1: [abc]
    | 2: [xyz test text 123]
    
    [Root cause]
    When preparing the pattern text for the :matches operation, the
    variable in the match pattern was replaced twice, just before
    comparing the target string with the pattern (by
    HeaderTest.replaceVariables() in HeaderTest.executeBasic()),
    and when putting the matched strings into the ${1} or ${2} variable.
    
    [Fix]
    Removed the second variable replacement.
    
    [Unit test]
    The following unit tests are passed successfully:
    test-com.zimbra.cs.filter.AddHeaderTest
    test-com.zimbra.cs.filter.DeleteHeaderTest
    test-com.zimbra.cs.filter.EnvelopeTest
    test-com.zimbra.cs.filter.FilterUtilTest
    test-com.zimbra.cs.filter.NotifyMailtoTest
    test-com.zimbra.cs.filter.ReplaceHeaderTest
    test-com.zimbra.cs.filter.RuleManagerAdminFilterTest
    test-com.zimbra.cs.filter.SetVariableTest
    test-com.zimbra.cs.filter.FileIntoCopyTest
    test-com.zimbra.cs.filter.TagTest
    test-com.zimbra.cs.filter.RedirectCopyTest
    test-com.zimbra.cs.filter.FlaggedTestTest
    test-com.zimbra.cs.filter.RelationalExtensionTest
    
    [Affected area]
    MBS - SIEVE engine - Header/match test
    
    (cherry picked from commit 755d2b9870ccf8895227678f321e2b273fbf5881)
    
    Conflicts:
    	src/java-test/com/zimbra/cs/filter/SetVariableTest.java

--- src/java/com/zimbra/cs/filter/jsieve/HeaderTest.java
+++ src/java/com/zimbra/cs/filter/jsieve/HeaderTest.java
@@ -356,9 +356,6 @@ public class HeaderTest extends Header {
             for (String sourceStr : values) {
                 for (Object key : keys) {
                     String keyStr = ((String) key);
-                    if (SetVariable.isVariablesExtAvailable(mailAdapter)) {
-                        keyStr = FilterUtil.replaceVariables(mailAdapter.getVariables(), mailAdapter.getMatchedValues(), keyStr);
-                    }
                     String regex = FilterUtil.sieveToJavaRegex(keyStr);
                     Matcher matcher = Pattern.compile(regex, Pattern.CASE_INSENSITIVE).matcher(sourceStr);
                     int grpCount = matcher.groupCount();
