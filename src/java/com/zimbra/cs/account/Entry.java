48594a537e39b0c572dfb729726a31e3411f8f8e
ilya <raykini@zimbra.com>
2017-02-08 16:25:08 -0800

    not including ephemeral attrs by default

--- src/java/com/zimbra/cs/account/Entry.java
+++ src/java/com/zimbra/cs/account/Entry.java
@@ -346,7 +346,7 @@ public abstract class Entry implements ToZJSONObject {
     }
 
     public Map<String, Object> getAttrs(boolean applyDefaults) {
-        return getAttrs(applyDefaults, true);
+        return getAttrs(applyDefaults, false);
     }
 
     public Map<String, Object> getAttrs(boolean applyDefaults, boolean includeEphemeral) {
