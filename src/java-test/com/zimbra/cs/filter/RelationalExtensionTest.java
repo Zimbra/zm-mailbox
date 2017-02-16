Zimbra/zm-mailbox

commit 3fdc9d8ec22028c6c744e4d42016c9cef7d50c19

Revert "resolving conflicts"
This reverts commit d3e1c65dba9db0ab4e97224ad92ad70b45f5e9dc, reversing
changes made to 945e385eb7b80a09dedc4a3c93f965057c418a0f.

src/java/com/zimbra/cs/lmtpserver/LmtpReply.java
@@ -51,7 +51,6 @@ public enum LmtpReply {
     INVALID_SIZE_PARAMETER(501, "5.5.4", "Syntax error in SIZE parameter"),
 
     NO_SUCH_USER(550, "5.1.1", "No such user here"),
-    PERMANENT_MESSAGE_REFUSED(550, "5.7.1", "Message rejected"),
     PERMANENT_FAILURE_OVER_QUOTA(552, "5.2.2", "Over quota"),
     PERMANENT_FAILURE(554, "5.0.0", "Permanent message delivery failure");
     

commit 066b66e8932f743847b45862f2ae2b27206eda40

Merging files from 'dev' to 'sieve'
The commit 3fdc9d8ec22028c6c744e4d42016c9cef7d50c19 accidentally
deleted multiple files from the 'sieve' branch on Sep 27th, 2016.
Most of them were recovered by the commit c203929392138f0aac2c33025faa1d1a551703bd
on Jan 23rd, 2017, but still these four files untouched was left untouched.

Now the source of each file is checked out from the commit by
```
git checkout --patch d3e1c65dba9db0ab4e97224ad92ad70b45f5e9dc <filename>
```
(merging the changes as of the commit d3e1c65dba9 which was one commit
before the 3fdc9d8ec22), or
```
git checkout d3e1c65dba9db0ab4e97224ad92ad70b45f5e9dc <filename>
```
(copying the file as of the commit d3e1c65dba9).

After applying the changes, the following unit test are passed successfully:
com.zimbra.cs.filter.AddHeaderTest
com.zimbra.cs.filter.DeleteHeaderTest
com.zimbra.cs.filter.EnvelopeTest
com.zimbra.cs.filter.FilterUtilTest
com.zimbra.cs.filter.ReplaceHeaderTest
com.zimbra.cs.filter.RuleManagerAdminFilterTest
com.zimbra.cs.filter.SetVariableTest
com.zimbra.cs.filter.FileIntoCopyTest
com.zimbra.cs.filter.TagTest
com.zimbra.cs.filter.FlaggedTestTest
com.zimbra.cs.filter.RelationalExtensionTest
com.zimbra.cs.filter.RedirectCopyTest

And the following unit test is passed as well but one test case
testNotify_variable() (due to the known bug on the test itself).
com.zimbra.cs.filter.NotifyMailtoTest

src/java/com/zimbra/cs/lmtpserver/LmtpReply.java
@@ -51,6 +51,7 @@ public enum LmtpReply {
     INVALID_SIZE_PARAMETER(501, "5.5.4", "Syntax error in SIZE parameter"),
 
     NO_SUCH_USER(550, "5.1.1", "No such user here"),
+    PERMANENT_MESSAGE_REFUSED(550, "5.7.1", "Message rejected"),
     PERMANENT_FAILURE_OVER_QUOTA(552, "5.2.2", "Over quota"),
     PERMANENT_FAILURE(554, "5.0.0", "Permanent message delivery failure");
     

src/java/com/zimbra/cs/lmtpserver/ZimbraLmtpBackend.java
@@ -45,6 +45,7 @@ import com.zimbra.common.lmtp.LmtpProtocolException;
 import com.zimbra.common.localconfig.DebugConfig;
 import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.mime.Rfc822ValidationInputStream;
+import com.zimbra.common.service.DeliveryServiceException;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.util.BufferStream;
 import com.zimbra.common.util.ByteUtil;
@@ -612,8 +613,8 @@ public class ZimbraLmtpBackend implements LmtpBackend {
                                     // file if Mailbox.addMessageInternal() closes it.
                                     pm.getMessageID();
                                     addedMessageIds = RuleManager.applyRulesToIncomingMessage(
-                                            null, mbox, pm, (int) blob.getRawSize(), rcptEmail, sharedDeliveryCtxt,
-                                            Mailbox.ID_FOLDER_INBOX, false);
+                                            null, mbox, pm, (int) blob.getRawSize(), rcptEmail, env, sharedDeliveryCtxt,
+                                            Mailbox.ID_FOLDER_INBOX, false, true);
                                 } else {
                                     pm.getMessageID();
                                     DeliveryOptions dopt = new DeliveryOptions().setFolderId(Mailbox.ID_FOLDER_INBOX);
@@ -667,6 +668,9 @@ public class ZimbraLmtpBackend implements LmtpBackend {
                                 envSender, rcptEmail);
                         reply = LmtpReply.PERMANENT_FAILURE;
                     }
+                } catch (DeliveryServiceException e) {
+                    ZimbraLog.lmtp.info("rejecting message from=%s,to=%s: sieve filter rule", envSender, rcptEmail);
+                    reply = LmtpReply.PERMANENT_MESSAGE_REFUSED;
                 } catch (ServiceException e) {
                     if (e.getCode().equals(MailServiceException.QUOTA_EXCEEDED)) {
                         ZimbraLog.lmtp.info("rejecting message from=%s,to=%s: overquota", envSender, rcptEmail);

src/java-test/com/zimbra/cs/filter/ErejectTest.java
@@ -1,0 +1,196 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2016 Synacor, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.filter;
+
+import static org.junit.Assert.fail;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.google.common.collect.Maps;
+import com.zimbra.common.account.Key;
+import com.zimbra.common.service.DeliveryServiceException;
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.filter.jsieve.ErejectException;
+import com.zimbra.cs.lmtpserver.LmtpAddress;
+import com.zimbra.cs.lmtpserver.LmtpEnvelope;
+import com.zimbra.cs.mailbox.DeliveryContext;
+import com.zimbra.cs.mailbox.MailItem;
+import com.zimbra.cs.mailbox.Mailbox;
+import com.zimbra.cs.mailbox.MailboxManager;
+import com.zimbra.cs.mailbox.MailboxTestUtil;
+import com.zimbra.cs.mailbox.Message;
+import com.zimbra.cs.mailbox.OperationContext;
+import com.zimbra.cs.mime.ParsedMessage;
+import com.zimbra.cs.service.mail.SendMsgTest.DirectInsertionMailboxManager;
+import com.zimbra.cs.service.util.ItemId;
+
+public class ErejectTest {
+
+    private static String sampleBaseMsg = "Received: from edge01e.zimbra.com ([127.0.0.1])\n"
+            + "\tby localhost (edge01e.zimbra.com [127.0.0.1]) (amavisd-new, port 10032)\n"
+            + "\twith ESMTP id DN6rfD1RkHD7; Fri, 24 Jun 2016 01:45:31 -0400 (EDT)\n"
+            + "Received: from localhost (localhost [127.0.0.1])\n"
+            + "\tby edge01e.zimbra.com (Postfix) with ESMTP id 9245B13575C;\n"
+            + "\tFri, 24 Jun 2016 01:45:31 -0400 (EDT)\n"
+            + "from: test2@zimbra.com\n"
+            + "Subject: example\n"
+            + "to: test@zimbra.com\n";
+
+    private String filterScript = "require [\"ereject\"];\n"
+            + "if header :contains \"from\" \"test2@zimbra.com\" {\n"
+            + "  ereject text:\r\n"
+            + "I am not taking mail from you, and I don’t\n"
+            + "want your birdseed, either!\r\n"
+            + ".\r\n"
+            + "  ;\n"
+            + "}";
+
+    @BeforeClass
+    public static void init() throws Exception {
+        MailboxTestUtil.initServer();
+        MailboxTestUtil.clearData();
+        Provisioning prov = Provisioning.getInstance();
+
+        Map<String, Object> attrs = Maps.newHashMap();
+        prov.createDomain("zimbra.com", attrs);
+
+        attrs = Maps.newHashMap();
+        attrs.put(Provisioning.A_zimbraId, UUID.randomUUID().toString());
+        prov.createAccount("test@zimbra.com", "secret", attrs);
+
+        attrs = Maps.newHashMap();
+        attrs.put(Provisioning.A_zimbraId, UUID.randomUUID().toString());
+        prov.createAccount("test2@zimbra.com", "secret", attrs);
+
+        // this MailboxManager does everything except actually send mail
+        MailboxManager.setInstance(new DirectInsertionMailboxManager());
+
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        MailboxTestUtil.clearData();
+    }
+
+    /*
+     * applyRulesToIncomingMessage() should throw an exception to cancel the message delivery.
+     * No message is delivered.
+     *
+     * The following error will be logged:
+     * ERROR - Evaluation failed. Reason: 'ereject' action refuses delivery of a message. Sieve rule evaluation is cancelled
+     */
+    @Test
+    public void test() {
+        Account acct1 = null;
+        Mailbox mbox1 = null;
+
+        try {
+            acct1 = Provisioning.getInstance().get(Key.AccountBy.name, "test@zimbra.com");
+            mbox1 = MailboxManager.getInstance().getMailboxByAccount(acct1);
+            RuleManager.clearCachedRules(acct1);
+
+            LmtpEnvelope env = new LmtpEnvelope();
+            LmtpAddress sender = new LmtpAddress("<test2@zimbra.com>", new String[] { "BODY", "SIZE" }, null);
+            LmtpAddress recipient = new LmtpAddress("<test@zimbra.com>", null, null);
+            env.setSender(sender);
+            env.addLocalRecipient(recipient);
+
+            acct1.setMailSieveScript(filterScript);
+            RuleManager.applyRulesToIncomingMessage(
+                    new OperationContext(mbox1), mbox1, new ParsedMessage(
+                    sampleBaseMsg.getBytes(), false), 0, acct1.getName(),
+                    env, new DeliveryContext(),
+                    Mailbox.ID_FOLDER_INBOX, true);
+        } catch (DeliveryServiceException e) {
+            if (e.getCause() instanceof ErejectException){
+                try {
+                    List<Integer> items = mbox1.getItemIds(null, Mailbox.ID_FOLDER_INBOX)
+                            .getIds(MailItem.Type.MESSAGE);
+                    Assert.assertEquals(null, items);
+                } catch (Exception ex) {
+                    fail("No exception should be thrown: " + ex.getMessage());
+                }
+            } else {
+                fail("No exception other than DeliveryServiceException/ErejectException should be thrown: " + e.getMessage());
+            }
+        } catch (Exception e) {
+             fail("No exception should be thrown: " + e.getMessage());
+        }
+    }
+    
+    /*
+     * applyRulesToIncomingMessage() should throw an exception to cancel the message delivery.
+     * No message is delivered.
+     *
+     * The following error will be logged:
+     * ERROR - Evaluation failed. Reason: 'ereject' action refuses delivery of a message. Sieve rule evaluation is cancelled
+     */
+    @Test
+    public void testThatSenderRcdUnDeliveredEmail() {
+        Account acct1 = null;
+        Mailbox mbox1 = null;
+        
+        Account acct2 = null;
+        Mailbox mbox2 = null;
+
+        try {
+            acct1 = Provisioning.getInstance().get(Key.AccountBy.name, "test@zimbra.com");
+            mbox1 = MailboxManager.getInstance().getMailboxByAccount(acct1);
+            RuleManager.clearCachedRules(acct1);
+            
+            acct2 = Provisioning.getInstance().get(Key.AccountBy.name, "test2@zimbra.com");
+            mbox2 = MailboxManager.getInstance().getMailboxByAccount(acct2);
+
+            LmtpEnvelope env = new LmtpEnvelope();
+            LmtpAddress sender = new LmtpAddress("<test2@zimbra.com>", new String[] { "BODY", "SIZE" }, null);
+            LmtpAddress recipient = new LmtpAddress("<test@zimbra.com>", null, null);
+            env.setSender(sender);
+            env.addLocalRecipient(recipient);
+
+            acct1.setMailSieveScript(filterScript);
+            RuleManager.applyRulesToIncomingMessage(
+                    new OperationContext(mbox1), mbox1, new ParsedMessage(
+                    sampleBaseMsg.getBytes(), false), 0, acct1.getName(),
+                    env, new DeliveryContext(),
+                    Mailbox.ID_FOLDER_INBOX, true);
+        } catch (DeliveryServiceException e) {
+            if (e.getCause() instanceof ErejectException){
+                try {
+                    List<Integer> items = mbox1.getItemIds(null, Mailbox.ID_FOLDER_INBOX)
+                            .getIds(MailItem.Type.MESSAGE);
+                    Assert.assertEquals(null, items);
+                } catch (Exception ex) {
+                	ex.printStackTrace();
+                    fail("No exception should be thrown: " + ex.getMessage());
+                }
+            } else {
+                fail("No exception other than DeliveryServiceException/ErejectException should be thrown: " + e.getMessage());
+            }
+        } catch (Exception e) {
+             fail("No exception should be thrown: " + e.getMessage());
+        }
+    }
+}

src/java-test/com/zimbra/cs/filter/RelationalExtensionTest.java
@@ -1,0 +1,331 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2016 Synacor, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.filter;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.zimbra.common.util.ArrayUtil;
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.MockProvisioning;
+import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.filter.RuleManager;
+import com.zimbra.cs.lmtpserver.LmtpAddress;
+import com.zimbra.cs.lmtpserver.LmtpEnvelope;
+import com.zimbra.cs.mailbox.DeliveryContext;
+import com.zimbra.cs.mailbox.Mailbox;
+import com.zimbra.cs.mailbox.MailboxManager;
+import com.zimbra.cs.mailbox.MailboxTestUtil;
+import com.zimbra.cs.mailbox.Message;
+import com.zimbra.cs.mailbox.OperationContext;
+import com.zimbra.cs.mime.ParsedMessage;
+import com.zimbra.cs.service.util.ItemId;
+
+public class RelationalExtensionTest {
+    private static String sampleMsg = "Received: from edge01e.zimbra.com ([127.0.0.1])\n"
+            + "\tby localhost (edge01e.zimbra.com [127.0.0.1]) (amavisd-new, port 10032)\n"
+            + "\twith ESMTP id DN6rfD1RkHD7; Fri, 24 Jun 2016 01:45:31 -0400 (EDT)\n"
+            + "Received: from localhost (localhost [127.0.0.1])\n"
+            + "\tby edge01e.zimbra.com (Postfix) with ESMTP id 9245B13575C;\n"
+            + "\tFri, 24 Jun 2016 01:45:31 -0400 (EDT)\n"
+            + "x-priority: 1\n"
+            + "from: xyz@example.com\n"
+            + "Subject: =?ISO-2022-JP?B?GyRCJDMkcyRLJEEkTxsoQg==?=\n"
+            + "to: foo@example.com, baz@example.com\n"
+            + "cc: qux@example.com\n";
+
+    @BeforeClass
+    public static void init() throws Exception {
+        MailboxTestUtil.initServer();
+        Provisioning prov = Provisioning.getInstance();
+        prov.createAccount("test@zimbra.com", "secret",
+                new HashMap<String, Object>());
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        MailboxTestUtil.clearData();
+    }
+
+    @Test
+    public void testCountAddressNumericGE1() {
+        // RFC 5231 6. Example first example would evaluate to true
+        String filterScript = "if address :count \"ge\" :comparator \"i;ascii-numeric\" "
+                + "[\"to\", \"cc\"] [\"3\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, "Priority");
+    }
+
+    @Test
+    public void testCountAddressNumericGE2() {
+        // RFC 5231 6. Example second example would evaluate to false
+        String filterScript = "if anyof (address :count \"ge\" :comparator \"i;ascii-numeric\"\n"
+                + "[\"to\"] [\"3\"],\n"
+                + "address :count \"ge\" :comparator \"i;ascii-numeric\"\n"
+                + "[\"cc\"] [\"3\"] )\n"
+                + "{" + "tag \"Priority\";}";
+        doTest(filterScript, null);
+    }
+
+    @Test
+    public void testCountHeaderNumericGE1() {
+        // RFC 5231 6. Example third example would evaluate to false
+        String filterScript = "require [\"fileinto\", \"tag\", \"flag\", \"log\", \"relational\"];\n"
+                + "if header :count \"ge\" :comparator \"i;ascii-numeric\" "
+                + "[\"received\"] [\"3\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, null);
+    }
+
+    @Test
+    public void testCountHeaderNumericGE2() {
+        // RFC 5231 6. Example fourth example would evaluate to true
+        String filterScript = "require [\"fileinto\", \"tag\", \"flag\", \"log\", \"relational\"];\n"
+                + "if header :count \"ge\" :comparator \"i;ascii-numeric\" "
+                + "[\"received\", \"subject\"] [\"3\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, "Priority");
+    }
+
+    @Test
+    public void testCountHeaderNumericGE3() {
+        // RFC 5231 6. Example fifth example would evaluate to false
+        String filterScript = "require [\"fileinto\", \"tag\", \"flag\", \"log\", \"relational\"];\n"
+                + "if header :count \"ge\" :comparator \"i;ascii-numeric\" "
+                + "[\"to\", \"cc\"] [\"3\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, null);
+    }
+
+    @Test
+    public void testValueAddressNumericGT() {
+        // invalid comparison
+        String filterScript = "require [\"fileinto\", \"tag\", \"flag\", \"log\", \"relational\"];\n"
+                + "if address :value \"gt\" :comparator \"i;ascii-numeric\" "
+                + "[\"to\"] [\"0\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, null);
+    }
+
+    @Test
+    public void testValueAddressCasemapGT() {
+        // "from" address starts with 'N'-'Z'
+        String filterScript = "if address :value \"gt\" :comparator \"i;ascii-casemap\" "
+                + "[\"from\"] [\"M\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, "Priority");
+    }
+
+    @Test
+    public void testValueHeaderNumericGT() {
+        // RFC 5231 7. Extended Example (modified)
+        String filterScript = "require [\"fileinto\", \"tag\", \"flag\", \"log\", \"relational\"];\n"
+                + "if header :value \"gt\" :comparator \"i;ascii-numeric\" "
+                + "[\"x-priority\"] [\"3\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, null);
+    }
+
+    @Test
+    public void testValueHeadeNumericrLT() {
+        // RFC 5231 7. Extended Example
+        String filterScript = "require [\"fileinto\", \"tag\", \"flag\", \"log\", \"relational\"];\n"
+                + "if header :value \"lt\" :comparator \"i;ascii-numeric\" "
+                + "[\"x-priority\"] [\"3\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, "Priority");
+    }
+
+    @Test
+    public void testValueHeaderNumericLE() {
+        // RFC 5231 7. Extended Example (modified)
+        String filterScript = "require [\"fileinto\", \"tag\", \"flag\", \"log\", \"relational\"];\n"
+                + "if header :value \"le\" :comparator \"i;ascii-numeric\" "
+                + "[\"x-priority\"] [\"3\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, "Priority");
+    }
+
+    @Test
+    public void testValueHeaderNumericEQ() {
+        // RFC 5231 7. Extended Example (modified)
+        String filterScript = "require [\"fileinto\", \"tag\", \"flag\", \"log\", \"relational\"];\n"
+                + "if header :value \"eq\" :comparator \"i;ascii-numeric\" "
+                + "[\"x-priority\"] [\"1\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, "Priority");
+    }
+
+    @Test
+    public void testValueHeaderNumericNE() {
+        // RFC 5231 7. Extended Example (modified)
+        String filterScript = "require [\"fileinto\", \"tag\", \"flag\", \"log\", \"relational\"];\n"
+                + "if header :value \"ne\" :comparator \"i;ascii-numeric\" "
+                + "[\"x-priority\"] [\"1\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, null);
+    }
+
+    @Test
+    public void testValueHeaderCasemapGT() {
+        // RFC 5231 7. Extended Example (modified)
+        String filterScript = "require [\"fileinto\", \"tag\", \"flag\", \"log\", \"relational\"];\n"
+                + "if header :value \"gt\" :comparator \"i;ascii-casemap\" "
+                + "[\"from\"] [\"M\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, "Priority");
+    }
+
+    @Test
+    public void testValueHeaderCasemapI18NGT() {
+        // RFC 5231 7. Extended Example (modified)
+        String filterScript = "require [\"fileinto\", \"tag\", \"flag\", \"log\", \"relational\"];\n"
+                + "if header :value \"gt\" :comparator \"i;ascii-casemap\" "
+                + "[\"subject\"] [\"か\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, "Priority");
+    }
+
+    @Test
+    public void testBadFormat_nokeys() {
+        String filterScript = "require [\"fileinto\", \"tag\", \"flag\", \"log\", \"relational\"];\n"
+                + "if header :value \"gt\" :comparator \"i;ascii-casemap\" "
+                + "[\"subject\"] :foo {" + "tag \"Priority\";}";
+        /*
+         * The following error will occur:
+         * org.apache.jsieve.exception.SyntaxException: Expecting a StringList of keys Line 2 column 1.
+         */
+        doTest(filterScript, null);
+    }
+
+    @Test
+    public void testBadFormat_noTestName() {
+        String filterScript = "require [\"fileinto\", \"tag\", \"flag\", \"log\", \"relational\"];\n"
+                + "if relational :value \"gt\" :comparator \"i;ascii-casemap\" "
+                + "[\"subject\"] [\"test\"] {" + "tag \"Priority\";}";
+        /*
+         * The following error will occur:
+         * org.apache.jsieve.exception.SyntaxException: Found unexpected arguments. Line 2 column 1.
+         */
+        doTest(filterScript, null);
+    }
+
+    @Test
+    public void testCountEnvelopeToNumericEQ() {
+        // RFC 5231 10. Security Considerations
+        // An implementation MUST ensure that the test for envelope "to" only
+        // reflects the delivery to the current user.
+        String filterScript = "if envelope :count \"eq\" :comparator \"i;ascii-numeric\" "
+                + "[\"TO\"] [\"1\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, "Priority");
+    }
+
+    @Test
+    public void testCountEnvelopeToNumericGT() {
+        // RFC 5231 10. Security Considerations
+        // An implementation MUST ensure that the test for envelope "to" only
+        // reflects the delivery to the current user.  Using this test, it MUST
+        // not be possible for a user to determine if this message was delivered
+        // to someone else.
+        // The number of sample LMTP RCPT TO addresses is 2, but the number of
+        // address to be evaluated for the "envelope" test should be 1.
+        String filterScript = "if envelope :count \"gt\" :comparator \"i;ascii-numeric\" "
+                + "[\"to\"] [\"1\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, null);
+    }
+
+    @Test
+    public void testValueEnvelopeFromNumericGT() {
+        // invalid comparison
+        String filterScript = "if envelope :value \"gt\" :comparator \"i;ascii-numeric\" "
+                + "[\"from\"] [\"1\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, null);
+    }
+
+    @Test
+    public void testValueEnvelopeCasemapGT() {
+        // LMTP MAIL FROM envelope (<abc@zimbra.com>) does not start 'N'-'Z'
+        String filterScript = "if envelope :value \"gt\" :comparator \"i;ascii-casemap\" "
+                + "[\"from\"] [\"M\"] {" + "tag \"Priority\";}";
+        doTest(filterScript, null);
+    }
+
+    @Test
+    public void testValueEnvelopeCasemap() {
+        // LMTP MAIL FROM envelope (<abc@zimbra.com>) matchs the all upper case string
+        // RFC 5228 Section 2.7.3. "i;ascii-casemap" comparator which treats uppercase and lowercase
+        //   characters in the US-ASCII subset of UTF-8 as the same
+        String filterScript = "require [\"fileinto\", \"tag\", \"flag\", \"log\", \"relational\"];\n"
+                + "if envelope :value \"eq\" :comparator \"i;ascii-casemap\" "
+                + "[\"from\"] \"ABC@ZIMBRA.COM\" {" + "tag \"Priority\";}";
+        doTest(filterScript, "Priority");
+    }
+
+    @Test
+    public void testValueEnvelopeOctet() {
+        // LMTP MAIL FROM envelope (<abc@zimbra.com>) does not match the all upper case string
+        // RFC 5228 Section 2.7.3. i;octet comparator simply compares octets
+        String filterScript = "require [\"fileinto\", \"tag\", \"flag\", \"log\", \"relational\"];\n"
+                + "if envelope :value \"eq\" :comparator \"i;octet\" "
+                + "[\"from\"] \"ABC@ZIMBRA.COM\" {" + "tag \"Priority\";}";
+        doTest(filterScript, null);
+    }
+
+    @Test
+    public void testBadFormat_invalidEnvHeaderName() {
+        String filterScript = "if envelope :value \"gt\" :comparator \"i;ascii-casemap\" "
+                + "[\"AUTH\"] [\"M\"] {" + "tag \"Priority\";}";
+         // The following error will occur:
+         // org.apache.jsieve.exception.SyntaxException: Unexpected header name as a value for <envelope-part>: 'AUTH'
+        doTest(filterScript, null);
+    }
+
+    @Test
+    public void testValueEnvelopeFromNumeric_AllUpperCase() {
+        String filterScript = "IF ENVELOPE :COUNT \"EQ\" :COMPARATOR \"I;ASCII-NUMERIC\" "
+                + "[\"TO\"] [\"1\"] {" + "TAG \"Priority\";}";
+        doTest(filterScript, "Priority");
+    }
+
+    private void doTest(String filterScript, String expectedResult) {
+        try {
+            LmtpEnvelope env = setEnvelopeInfo();
+            Account account = Provisioning.getInstance().getAccount(
+                    MockProvisioning.DEFAULT_ACCOUNT_ID);
+            RuleManager.clearCachedRules(account);
+            Mailbox mbox = MailboxManager.getInstance().getMailboxByAccount(account);
+
+            account.setMailSieveScript(filterScript);
+            List<ItemId> ids = RuleManager.applyRulesToIncomingMessage(
+                    new OperationContext(mbox), mbox,
+                    new ParsedMessage(sampleMsg.getBytes(), false), 0,
+                    account.getName(), env,
+                    new DeliveryContext(),
+                    Mailbox.ID_FOLDER_INBOX, true);
+            Assert.assertEquals(1, ids.size());
+            Message msg = mbox.getMessageById(null, ids.get(0).getId());
+            Assert.assertEquals(expectedResult, ArrayUtil.getFirstElement(msg.getTags()));
+        } catch (Exception e) {
+            fail("No exception should be thrown");
+        }
+    }
+
+    private LmtpEnvelope setEnvelopeInfo() {
+        LmtpEnvelope env = new LmtpEnvelope();
+        LmtpAddress sender = new LmtpAddress("<abc@zimbra.com>", new String[] { "BODY", "SIZE" }, null);
+        LmtpAddress recipient1 = new LmtpAddress("<xyz@zimbra.com>", null, null);
+        LmtpAddress recipient2 = new LmtpAddress("<uvw@zimbra.com>", null, null);
+        env.setSender(sender);
+        env.addLocalRecipient(recipient1);
+        env.addLocalRecipient(recipient2);
+        return env;
+    }
+}