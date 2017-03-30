efce0a0eee189c3b9d49565d6f2df8aed12ab0cb
Gren Elliot <gren.elliot@synacor.com>
2017-03-30 17:32:06 +0100

    Avoid NPE when modifyLdapAttrs null entry

--- store/src/java/com/zimbra/cs/account/ldap/LdapProvisioning.java
+++ store/src/java/com/zimbra/cs/account/ldap/LdapProvisioning.java
@@ -628,6 +628,9 @@ public class LdapProvisioning extends LdapProv implements CacheAwareProvisioning
     private void modifyLdapAttrs(Entry entry, ZLdapContext initZlc,
             Map<String, ? extends Object> attrs)
             throws ServiceException {
+        if (null == entry) {
+            throw ServiceException.FAILURE("unable to modify attrs on null Entry", null);
+        }
         ZLdapContext zlc = initZlc;
         try {
             if (zlc == null) {
@@ -642,8 +645,7 @@ public class LdapProvisioning extends LdapProv implements CacheAwareProvisioning
             throw AccountServiceException.INVALID_ATTR_VALUE(
                     "invalid attr value: " + e.getMessage(), e);
         } catch (ServiceException e) {
-            throw ServiceException.FAILURE("unable to modify attrs: "
-                    + e.getMessage(), e);
+            throw ServiceException.FAILURE("unable to modify attrs: " + e.getMessage(), e);
         } finally {
             if (zlc != null) {
                 refreshEntry(entry, zlc);
