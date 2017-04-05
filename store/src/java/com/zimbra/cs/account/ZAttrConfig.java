49c25adef79d0e0ec02798cbe4748cc3db6e5c62
Gordon Tillman <gordon.tillman@synacor.com>
2017-04-05 12:35:55 -0500

    Regenerated getters/setters

--- store/src/java/com/zimbra/cs/account/ZAttrConfig.java
+++ store/src/java/com/zimbra/cs/account/ZAttrConfig.java
@@ -48220,7 +48220,7 @@ public abstract class ZAttrConfig extends Entry {
      *
      * @since ZCS 8.8.0
      */
-    @ZAttr(id=3010)
+    @ZAttr(id=3012)
     public int getOpenImapFolderRequestChunkSize() {
         return getIntAttr(Provisioning.A_zimbraOpenImapFolderRequestChunkSize, 1000, true);
     }
@@ -48234,7 +48234,7 @@ public abstract class ZAttrConfig extends Entry {
      *
      * @since ZCS 8.8.0
      */
-    @ZAttr(id=3010)
+    @ZAttr(id=3012)
     public void setOpenImapFolderRequestChunkSize(int zimbraOpenImapFolderRequestChunkSize) throws com.zimbra.common.service.ServiceException {
         HashMap<String,Object> attrs = new HashMap<String,Object>();
         attrs.put(Provisioning.A_zimbraOpenImapFolderRequestChunkSize, Integer.toString(zimbraOpenImapFolderRequestChunkSize));
@@ -48251,7 +48251,7 @@ public abstract class ZAttrConfig extends Entry {
      *
      * @since ZCS 8.8.0
      */
-    @ZAttr(id=3010)
+    @ZAttr(id=3012)
     public Map<String,Object> setOpenImapFolderRequestChunkSize(int zimbraOpenImapFolderRequestChunkSize, Map<String,Object> attrs) {
         if (attrs == null) attrs = new HashMap<String,Object>();
         attrs.put(Provisioning.A_zimbraOpenImapFolderRequestChunkSize, Integer.toString(zimbraOpenImapFolderRequestChunkSize));
@@ -48266,7 +48266,7 @@ public abstract class ZAttrConfig extends Entry {
      *
      * @since ZCS 8.8.0
      */
-    @ZAttr(id=3010)
+    @ZAttr(id=3012)
     public void unsetOpenImapFolderRequestChunkSize() throws com.zimbra.common.service.ServiceException {
         HashMap<String,Object> attrs = new HashMap<String,Object>();
         attrs.put(Provisioning.A_zimbraOpenImapFolderRequestChunkSize, "");
@@ -48282,7 +48282,7 @@ public abstract class ZAttrConfig extends Entry {
      *
      * @since ZCS 8.8.0
      */
-    @ZAttr(id=3010)
+    @ZAttr(id=3012)
     public Map<String,Object> unsetOpenImapFolderRequestChunkSize(Map<String,Object> attrs) {
         if (attrs == null) attrs = new HashMap<String,Object>();
         attrs.put(Provisioning.A_zimbraOpenImapFolderRequestChunkSize, "");
