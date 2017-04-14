94b2aa66b7bbd0578bb26fc0fd553da573b15dba
Chinmay Pathak <15.chinmay@gmail.com>
2017-04-14 19:30:50 +0530

    ZCS-1238 : SIEVE: Match variable not getting replaced within header name of replaceheader

--- store/src/java-test/com/zimbra/cs/filter/ReplaceHeaderTest.java
+++ store/src/java-test/com/zimbra/cs/filter/ReplaceHeaderTest.java
@@ -1299,4 +1299,43 @@ public class ReplaceHeaderTest {
         }
     }
 
+    /*
+     * Replace header with new name from the matching condition
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testReplaceHeaderNewNameFromCondition() {
+        try {
+            String filterScript = "require [\"editheader\", \"variables\"];\n"
+                    + "replaceheader :newname \"${1}\" :newvalue \"${2}\" :matches \"X-Test-Header\" \"t*t*\";\n";
+            Account acct1 = Provisioning.getInstance().get(Key.AccountBy.name, "test@zimbra.com");
+            Mailbox mbox1 = MailboxManager.getInstance().getMailboxByAccount(acct1);
+
+            RuleManager.clearCachedRules(acct1);
+
+            acct1.setMailSieveScript(filterScript);
+            RuleManager.applyRulesToIncomingMessage(
+                    new OperationContext(mbox1), mbox1, new ParsedMessage(
+                            sampleBaseMsg.getBytes(), false), 0, acct1.getName(),
+                            null, new DeliveryContext(),
+                            Mailbox.ID_FOLDER_INBOX, true);
+            Integer itemId = mbox1.getItemIds(null, Mailbox.ID_FOLDER_INBOX).getIds(MailItem.Type.MESSAGE).get(0);
+            Message message = mbox1.getMessageById(null, itemId);
+            boolean matchNotFound = false;
+            boolean matchFound = false;
+            for (Enumeration<Header> enumeration = message.getMimeMessage().getAllHeaders(); enumeration.hasMoreElements();) {
+                Header header = enumeration.nextElement();
+                if ("X-Test-Header".equals(header.getName()) && "test1".equals(header.getValue())) {
+                    matchNotFound = true;
+                }
+                if ("es".equals(header.getName()) && "1".equals(header.getValue())) {
+                    matchFound = true;
+                }
+            }
+            Assert.assertFalse(matchNotFound);
+            Assert.assertTrue(matchFound);
+        } catch (Exception e) {
+            fail("No exception should be thrown: " + e.getMessage());
+        }
+    }
 }
