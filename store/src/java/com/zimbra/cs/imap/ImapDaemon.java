511b1bb5aa47e8d5819efe2a8d1e1e38fe44eebc
Gordon Tillman <gordon.tillman@synacor.com>
2017-05-08 12:15:15 -0500

    ZCS-1344 Cleanup logging in ImapDaemon

--- store/src/java/com/zimbra/cs/imap/ImapDaemon.java
+++ store/src/java/com/zimbra/cs/imap/ImapDaemon.java
@@ -56,13 +56,13 @@ public class ImapDaemon {
             imapServer = startImapServer(false);
             cnt += 1;
         } else {
-            System.err.println(Provisioning.A_zimbraRemoteImapServerEnabled + " is FALSE");
+            ZimbraLog.imap.info("%s is FALSE", Provisioning.A_zimbraRemoteImapServerEnabled);
         }
         if (isEnabled(Provisioning.A_zimbraRemoteImapSSLServerEnabled)) {
             imapSSLServer = startImapServer(true);
             cnt += 1;
         } else {
-            System.err.println(Provisioning.A_zimbraRemoteImapSSLServerEnabled + " is FALSE");
+            ZimbraLog.imap.info("%s is FALSE", Provisioning.A_zimbraRemoteImapSSLServerEnabled);
         }
 
         return cnt;
@@ -102,19 +102,23 @@ public class ImapDaemon {
                           }
                         });
                 } else {
-                    System.err.println("ImapDaemon: No servers started. Check zimbraRemoteImapServerEnabled and zimbraRemoteImapSSLServerEnabled");
-                    System.exit(1);
+                    errorExit("ImapDaemon: No servers started. Check zimbraRemoteImapServerEnabled and zimbraRemoteImapSSLServerEnabled");
                 }
                 if(!isMemberOfPool()) {
-                    System.err.println("ImapDaemon: This server not member of pool. Check zimbraReverseProxyUpstreamImapServers.");
+                    errorExit("ImapDaemon: This server not member of pool. Check zimbraReverseProxyUpstreamImapServers.");
                 }
             } else {
-                System.err.println("ImapDaemon: imap service is not enabled on this server. Check zimbraServiceEnabled.");
-                System.exit(1);
+                errorExit("ImapDaemon: imap service is not enabled on this server. Check zimbraServiceEnabled.");
             }
         } catch (Exception e) {
             System.err.println("ImapDaemon: " + e);
         }
+    }
+
+    private static void errorExit(String msg) {
+        ZimbraLog.imap.warn(msg);
+        System.err.println(msg);
+        System.exit(1);
     }
 
     private static boolean isEnabled(String key) throws ServiceException {
