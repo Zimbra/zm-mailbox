1b9ad8de6f589d9a44227a90760283941d7c0857
Greg Solovyev <fiddlestring@gmail.com>
2017-05-10 16:39:43 -0700

    move imap soap handlers to zimbraMail namespace

--- store/src/java/com/zimbra/cs/service/mail/ResetRecentMessageCount.java
+++ store/src/java/com/zimbra/cs/service/mail/ResetRecentMessageCount.java
@@ -1,16 +1,14 @@
-package com.zimbra.cs.service.account;
+package com.zimbra.cs.service.mail;
 
 import java.util.Map;
-import java.util.Set;
 
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
 import com.zimbra.cs.account.Account;
-import com.zimbra.cs.util.AccountUtil;
 import com.zimbra.soap.ZimbraSoapContext;
-import com.zimbra.soap.mail.message.ListIMAPSubscriptionsResponse;
+import com.zimbra.soap.mail.message.ResetRecentMessageCountResponse;
 
-public class ListIMAPSubscriptions extends AccountDocumentHandler {
+public class ResetRecentMessageCount extends MailDocumentHandler {
 
     @Override
     public Element handle(Element request, Map<String, Object> context) throws ServiceException {
@@ -21,9 +19,7 @@ public class ListIMAPSubscriptions extends AccountDocumentHandler {
             throw ServiceException.PERM_DENIED("can not access account");
         }
 
-        Set<String> subs = AccountUtil.parseConfig(getRequestedMailbox(zsc).getConfig(getOperationContext(zsc, context), AccountUtil.SN_IMAP));
-        ListIMAPSubscriptionsResponse resp = new ListIMAPSubscriptionsResponse();
-        resp.setSubscriptions(subs);
-        return zsc.jaxbToElement(resp);
+        getRequestedMailbox(zsc).resetRecentMessageCount(getOperationContext(zsc, context));
+        return zsc.jaxbToElement(new ResetRecentMessageCountResponse());
     }
 }
