a083445051d038769ab3b484dc1ff00ae5e92fc0
Greg Solovyev <fiddlestring@gmail.com>
2017-05-10 16:46:19 -0700

    move IMAP request/response constants to MailConstants

--- soap/src/java/com/zimbra/soap/mail/message/GetLastItemIdInMailboxRequest.java
+++ soap/src/java/com/zimbra/soap/mail/message/GetLastItemIdInMailboxRequest.java
@@ -2,14 +2,14 @@ package com.zimbra.soap.mail.message;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
-import com.zimbra.common.soap.AccountConstants;
+import com.zimbra.common.soap.MailConstants;
 
 /**
  * @zm-api-command-auth-required true
  * @zm-api-command-admin-auth-required false
  * @zm-api-command-description Returns the last ID assigned to an item successfully created in the mailbox
  */
-@XmlRootElement(name=AccountConstants.E_GET_LAST_ITEM_ID_IN_MAILBOX_REQUEST)
+@XmlRootElement(name=MailConstants.E_GET_LAST_ITEM_ID_IN_MAILBOX_REQUEST)
 public class GetLastItemIdInMailboxRequest {
 
     public GetLastItemIdInMailboxRequest() {}
