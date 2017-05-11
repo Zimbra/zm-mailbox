45757bf263b52f8942b6425f4723c3ddcedd7fdc
Gordon Tillman <gordon.tillman@synacor.com>
2017-05-11 17:55:11 -0500

    ZCS-1344 Added localconfig options for zimbra-imapd JVM
    Added the following to allow the zmimapdctl script to configure
    appropriate options for the JVM.
    
    - imapd_java_options
    - imapd_java_heap_size
    - imapd_java_heap_new_size_percent

--- common/src/java/com/zimbra/common/localconfig/LC.java
+++ common/src/java/com/zimbra/common/localconfig/LC.java
@@ -732,6 +732,12 @@ public final class LC {
     public static final KnownKey imapd_keystore = KnownKey.newKey("/opt/zimbra/conf/imapd.keystore");
     @Supported
     public static final KnownKey imapd_keystore_password = KnownKey.newKey("${mailboxd_keystore_password}");
+    @Supported
+    public static final KnownKey imapd_java_options = KnownKey.newKey("");
+    @Supported
+    public static final KnownKey imapd_java_heap_size = KnownKey.newKey("");
+    @Supported
+    public static final KnownKey imapd_java_heap_new_size_percent = KnownKey.newKey("${mailboxd_java_heap_new_size_percent}");
 
     public static final KnownKey pop3_write_timeout = KnownKey.newKey(10);
     public static final KnownKey pop3_thread_keep_alive_time = KnownKey.newKey(60);
