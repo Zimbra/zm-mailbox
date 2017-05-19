b10d39003b68f3f42bb229135c3569af8a153bca
Gren Elliot <gren.elliot@synacor.com>
2017-05-19 17:52:22 +0100

    ZMS-262:ToXML callers don't ask for imapUid 2 ways
    Was using separate wantImapUid arg even when had fields arg.
    Now require caller to adjust fields as needed.

--- store/src/java-test/com/zimbra/cs/mailbox/MessageTest.java
+++ store/src/java-test/com/zimbra/cs/mailbox/MessageTest.java
@@ -163,7 +163,7 @@ public final class MessageTest {
         // Make sure that the post flag is serialized to XML.
         Element eMsg = ToXML.encodeMessageAsMIME(new XMLElement("test"), new ItemIdFormatter(), (OperationContext)null,
                 msg, (String)null /* part */, false /* mustInline */, false /* mustNotInline */,
-                false /* serializeType */, false /* wantImapUid */);
+                false /* serializeType */, ToXML.NOTIFY_FIELDS);
 
         Assert.assertEquals("^", eMsg.getAttribute(MailConstants.A_FLAGS));
 
