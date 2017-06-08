31f471c080c3ebbde18df6c8ad2bdc31cff4a60a
Gordon Tillman <gordon.tillman@synacor.com>
2017-06-08 13:52:16 -0500

    ZCS-1580 Add ImapTransientStoreManager
    The `ImapTransientStoreManager` is intended for use with the
    zimbra-imapd service. It needs to be able to store temporary blobs
    whilst assembling the body of an APPEND message.

---
+++ store/src/java/com/zimbra/cs/store/external/ImapTransientStoreManager.java
@@ -0,0 +1,89 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2017 Synacor, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.cs.store.external;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.zimbra.common.localconfig.LC;
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.FileUtil;
+import com.zimbra.cs.mailbox.Mailbox;
+
+/**
+ * Simple ExternalStoreManager implementation that is intended for use in storing
+ * transient blobs.
+ *
+ * <p>This StoreManager is used by the {@link com.zimbra.cs.imap.ImapDaemon} while
+ * constructing blobs for an APPEND operation.  The blobs are deleted when the
+ * APPEND is finalized.
+ *
+ */
+public class ImapTransientStoreManager extends ExternalStoreManager {
+
+    protected File baseDirectory;
+
+    @Override
+    public void startup() throws IOException, ServiceException {
+        startup(LC.imapd_tmp_directory.value());
+    }
+
+    public void startup(String baseDirectoryPath) throws IOException, ServiceException {
+        super.startup();
+        baseDirectory = new File(baseDirectoryPath);
+        FileUtil.mkdirs(baseDirectory);
+    }
+
+    @Override
+    public void shutdown() {
+        super.shutdown();
+    }
+
+    @Override
+    public String writeStreamToStore(InputStream in, long actualSize, Mailbox mbox)
+            throws IOException, ServiceException {
+        File destFile = createBlobFile(mbox);
+        FileUtil.copy(in, false, destFile);
+        return destFile.getCanonicalPath();
+    }
+
+    @Override
+    public InputStream readStreamFromStore(String locator, Mailbox mbox) throws IOException {
+        return new FileInputStream(locator);
+    }
+
+    @Override
+    public boolean deleteFromStore(String locator, Mailbox mbox) throws IOException {
+        File deleteFile = new File(locator);
+        return deleteFile.delete();
+    }
+
+    @Override
+    public boolean supports(StoreFeature feature) {
+        return feature == StoreFeature.CENTRALIZED ? false : super.supports(feature);
+    }
+
+    private File createBlobFile(Mailbox mbox) throws IOException {
+        synchronized (this) {
+            return File.createTempFile(mbox.getAccountId(), ".msg", baseDirectory);
+        }
+    }
+
+}
