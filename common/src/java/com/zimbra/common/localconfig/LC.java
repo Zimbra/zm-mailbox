c31dc41c0fbdea14edbbdb50f5ebd02c976b1115
Gordon Tillman <gordon.tillman@synacor.com>
2017-06-08 13:50:22 -0500

    ZCS-1580 Add new LC imapd_tmp_directory
    This new localconfig attribute is used by the ImapTransientStoreManager.

--- common/src/java/com/zimbra/common/localconfig/LC.java
+++ common/src/java/com/zimbra/common/localconfig/LC.java
@@ -738,6 +738,8 @@ public final class LC {
     public static final KnownKey imapd_java_heap_size = KnownKey.newKey("");
     @Supported
     public static final KnownKey imapd_java_heap_new_size_percent = KnownKey.newKey("${mailboxd_java_heap_new_size_percent}");
+    @Supported
+    public static final KnownKey imapd_tmp_directory = KnownKey.newKey("${zimbra_tmp_directory}/imapd");
 
     public static final KnownKey pop3_write_timeout = KnownKey.newKey(10);
     public static final KnownKey pop3_thread_keep_alive_time = KnownKey.newKey(60);
