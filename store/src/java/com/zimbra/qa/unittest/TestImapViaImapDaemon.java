7cd7c921225614db8be578d48cdf945096b5c324
ilya <raykini@zimbra.com>
2017-07-05 09:23:23 -0700

    testClearDaemonCache IMAP test

--- store/src/java/com/zimbra/qa/unittest/TestImapViaImapDaemon.java
+++ store/src/java/com/zimbra/qa/unittest/TestImapViaImapDaemon.java
@@ -5,10 +5,20 @@ import java.io.IOException;
 import org.dom4j.DocumentException;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
+import com.zimbra.common.account.Key.CacheEntryBy;
 import com.zimbra.common.localconfig.ConfigException;
 import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.Log;
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.imap.ImapProxy.ZimbraClientAuthenticator;
+import com.zimbra.cs.mailclient.auth.AuthenticatorFactory;
+import com.zimbra.cs.mailclient.imap.ImapConfig;
+import com.zimbra.cs.mailclient.imap.ImapConnection;
+import com.zimbra.cs.security.sasl.ZimbraAuthenticator;
+import com.zimbra.cs.service.AuthProvider;
 
 /**
  * Test the IMAP server provided by the IMAP daemon, doing the necessary configuration to make it work.
@@ -38,5 +48,23 @@ public class TestImapViaImapDaemon extends SharedImapTests {
     @Override
     protected int getImapPort() {
         return imapServer.getRemoteImapBindPort();
+    }
+
+    @Test
+    public void testClearDaemonCache() throws Exception {
+        AuthenticatorFactory authFactory = new AuthenticatorFactory();
+        authFactory.register(ZimbraAuthenticator.MECHANISM, ZimbraClientAuthenticator.class);
+        Account acct = TestUtil.getAccount(USER);
+        ImapConfig config = new ImapConfig(imapHostname);
+        config.setMechanism(ZimbraAuthenticator.MECHANISM);
+        config.setAuthenticatorFactory(authFactory);
+        config.setPort(imapPort);
+        config.setAuthenticationId(USER);
+        config.getLogger().setLevel(Log.Level.trace);
+        connection = new ImapConnection(config);
+        connection.connect();
+        connection.authenticate(AuthProvider.getAuthToken(acct).getEncoded());
+        connection.flushCache(CacheEntryType.account, CacheEntryBy.name, acct.getName(), acct.getName());
+        connection.flushCache(CacheEntryType.config);
     }
 }
