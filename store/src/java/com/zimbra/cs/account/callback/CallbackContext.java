2b911b94ee4667f3400841ad95f2ebf5f182042d
ilya <raykini@zimbra.com>
2017-07-08 21:01:58 -0700

    setting zimbraPreviousEphemeralBackendURL when changing backend

--- store/src/java/com/zimbra/cs/account/callback/CallbackContext.java
+++ store/src/java/com/zimbra/cs/account/callback/CallbackContext.java
@@ -30,48 +30,49 @@ public class CallbackContext {
         CREATE,
         MODIFY;
     };
-    
+
     public enum DataKey {
         MAX_SIGNATURE_LEN,
         MAIL_FORWARDING_ADDRESS_MAX_LEN,
         MAIL_FORWARDING_ADDRESS_MAX_NUM_ADDRS,
         MAIL_WHITELIST_MAX_NUM_ENTRIES,
         MAIL_BLACKLIST_MAX_NUM_ENTRIES,
+        PREV_EPHEMERAL_BACKEND_URL;
     };
 
     // whether the entry is being created
     private final Op op;
-    
+
     // named of the entry being created
     private String creatingEntryName;
-    
+
     // set of AttributeCallback marked themselves done
     private Set<Class<? extends AttributeCallback>> done = Sets.newHashSet();
-    
+
     // data map
     private Map<DataKey, String> dataMap = Maps.newHashMap();
-    
+
     public CallbackContext(Op op) {
         this.op = op;
     }
-    
+
     public boolean isCreate() {
         return op == Op.CREATE;
     }
-    
+
     public void setCreatingEntryName(String name) {
         creatingEntryName = name;
     }
-    
+
     // name of the entry being creating
     public String getCreatingEntryName() {
         return creatingEntryName;
     }
-    
+
     private void setDone(Class<? extends AttributeCallback> callback) {
         done.add(callback);
     }
-    
+
     public boolean isDoneAndSetIfNot(Class<? extends AttributeCallback> callback) {
         boolean isDone = done.contains(callback);
         if (!isDone) {
@@ -79,13 +80,13 @@ public class CallbackContext {
         }
         return isDone;
     }
-    
+
     public void setData(DataKey key, String value) {
         dataMap.put(key, value);
     }
-    
+
     public String getData(DataKey key) {
         return dataMap.get(key);
     }
-    
+
 }
