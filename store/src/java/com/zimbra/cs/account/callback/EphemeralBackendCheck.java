56825d6a63993e05b0053d4f5413434c4cbece7b
ilya <raykini@zimbra.com>
2017-07-31 17:58:34 -0700

    URL change logic handled by EphemeralBackendMigrationRules

--- store/src/java/com/zimbra/cs/account/callback/EphemeralBackendCheck.java
+++ store/src/java/com/zimbra/cs/account/callback/EphemeralBackendCheck.java
@@ -1,7 +1,9 @@
 package com.zimbra.cs.account.callback;
 
+import java.text.SimpleDateFormat;
 import java.util.Map;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Strings;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.util.Log.Level;
@@ -11,14 +13,19 @@ import com.zimbra.cs.account.Config;
 import com.zimbra.cs.account.Entry;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.callback.CallbackContext.DataKey;
+import com.zimbra.cs.account.callback.EphemeralBackendCheck.MigrationStateHelper.Reason;
 import com.zimbra.cs.ephemeral.EphemeralStore;
-import com.zimbra.cs.ephemeral.EphemeralStore.Factory;
 import com.zimbra.cs.ephemeral.migrate.AttributeMigration;
 import com.zimbra.cs.ephemeral.migrate.MigrationInfo;
 import com.zimbra.cs.ephemeral.migrate.MigrationInfo.Status;
 import com.zimbra.cs.extension.ExtensionUtil;
 
 public class EphemeralBackendCheck extends AttributeCallback {
+
+    private static MigrationStateHelper.Factory helperFactory;
+    static {
+        setHelperFactory(new ZimbraMigrationStateHelper.Factory());
+    }
 
     @Override
     public void preModify(CallbackContext context, String attrName,
@@ -35,7 +42,7 @@ public class EphemeralBackendCheck extends AttributeCallback {
                     EphemeralStore.clearFactory();
                     return;
                 }
-                Factory factory = EphemeralStore.getFactory(backend);
+                EphemeralStore.Factory factory = EphemeralStore.getFactory(backend);
                 if (factory == null) {
                     // Probably called from zmprov in LDAP mode, so need to setup any Ephemeral Store extensions
                     Level savedEphem = ZimbraLog.ephemeral.getLevel();
@@ -70,27 +77,14 @@ public class EphemeralBackendCheck extends AttributeCallback {
         }
     }
 
+    public static void setHelperFactory(MigrationStateHelper.Factory factory) {
+        helperFactory = factory;
+    }
+
+
     private void checkMigration(String URL) throws ServiceException {
-        /*
-         * The only scenario that explicitly disallows changing the backend URL is if the migration is currently
-         * marked is IN_PROGRESS.
-         */
-        MigrationInfo info = AttributeMigration.getMigrationInfo();
-        Status curStatus = info.getStatus();
-        String migrationURL = info.getURL();
-        if (curStatus == Status.IN_PROGRESS) {
-            throw ServiceException.FAILURE(String.format("attribute migration to '%s' is currently in progress, cannot change backend URL", migrationURL), null);
-        } else if (curStatus == Status.NONE) {
-          //no record of migration running
-            ZimbraLog.ephemeral.warn("No record of an attribute migration exists; data may not have been migrated to this backend");
-        } else if (curStatus == Status.FAILED && URL.equals(migrationURL)) {
-            //migration to this URL failed
-            ZimbraLog.ephemeral.warn("Previous attribute migration to '%s' did not succeed; data may not have been migrated to this backend", migrationURL);
-        } else if (!URL.equals(migrationURL)) {
-            //ephemeral backend is changed to one that does not match the migration target
-            ZimbraLog.ephemeral.warn("New backend '%s' does not match current migration URL '%s'; data may not have been migrated to this backend.\n"
-                    + "Ephemeral data will be forwarded to '%s' until migration info is reset", URL, migrationURL, migrationURL);
-        }
+        MigrationStateHelper helper = helperFactory.getHelper();
+        new EphemeralBackendMigrationRules(helper).checkCanChangeURL(URL);
     }
 
     private void resetMigrationInfo(String newURL) throws ServiceException {
@@ -125,5 +119,109 @@ public class EphemeralBackendCheck extends AttributeCallback {
             ZimbraLog.ephemeral.error("unable to reset attribute migration info", e);
         }
         AttributeMigration.clearConfigCacheOnAllServers(false);
+    }
+
+
+    /**
+     * Class encapsulating the rules governing whether the backend URL can be changed given
+     * what is known about the current state of ephemeral attribute migration
+     */
+    @VisibleForTesting
+    public static class EphemeralBackendMigrationRules {
+        private MigrationStateHelper helper;
+
+        public EphemeralBackendMigrationRules(MigrationStateHelper helper) {
+            this.helper = helper;
+        }
+
+
+        public void checkCanChangeURL(String newURL) throws ServiceException {
+            /*
+             * The only scenario that explicitly disallows changing the backend URL is if the migration is currently
+             * marked is IN_PROGRESS.
+             */
+            helper.setURL(newURL);
+            MigrationInfo info = AttributeMigration.getMigrationInfo();
+            Status curStatus = info.getStatus();
+            String migrationURL = info.getURL();
+            if (curStatus == Status.IN_PROGRESS) {
+                helper.deny();
+            } else if (curStatus == Status.NONE) {
+                helper.warn(Reason.NO_MIGRATION);
+            } else if (curStatus == Status.FAILED && newURL.equals(migrationURL)) {
+                helper.warn(Reason.MIGRATION_ERROR);
+            } else if (!newURL.equals(migrationURL)) {
+                helper.warn(Reason.URL_MISMATCH);
+            } else {
+                helper.allow();
+            }
+        }
+    }
+
+    /**
+     * Helper interface specifying how the outcomes of checkMigration() are handled
+     */
+    public static abstract class MigrationStateHelper {
+
+        public static enum Reason {NO_MIGRATION, MIGRATION_ERROR, URL_MISMATCH }
+
+        protected String URL;
+
+        private void setURL(String newURL) {
+            URL = newURL;
+        }
+
+        public abstract void deny() throws ServiceException;
+        public abstract void warn(Reason reason);
+        public abstract void allow();
+
+        public static interface Factory {
+            public MigrationStateHelper getHelper() throws ServiceException;
+        }
+    }
+
+    public static class ZimbraMigrationStateHelper extends MigrationStateHelper {
+
+        private MigrationInfo info;
+
+        public ZimbraMigrationStateHelper() throws ServiceException {
+            this.info = AttributeMigration.getMigrationInfo();
+        }
+
+        @Override
+        public void allow() {
+            ZimbraLog.ephemeral.info("Successfully changed backend URL to %s", URL);
+        }
+
+        @Override
+        public void deny() throws ServiceException {
+            String started = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(info.getDate());
+            throw ServiceException.FAILURE(String.format("Cannot change the backend URL; attribute migration to %s is currently in progress (started %s)", info.getURL(), started), null);
+        }
+
+        @Override
+        public void warn(Reason reason) {
+            switch(reason) {
+            case MIGRATION_ERROR:
+                ZimbraLog.ephemeral.warn("Previous attribute migration to %s did not succeed; data may not have been migrated to this backend", URL);
+                break;
+            case NO_MIGRATION:
+                ZimbraLog.ephemeral.warn("No record of an attribute migration exists; data may not have been migrated to backend at %s", URL);
+                break;
+            case URL_MISMATCH:
+                ZimbraLog.ephemeral.warn("Provided URL %s does not match current migration URL %s; data may not have been migrated to this backend.\n"
+                        + "Ephemeral data will be forwarded to %s until migration info is reset or the backend is changed to match it", URL, info.getURL(), info.getURL());
+
+            }
+        }
+
+        public static class Factory implements MigrationStateHelper.Factory {
+
+            @Override
+            public MigrationStateHelper getHelper() throws ServiceException {
+                return new ZimbraMigrationStateHelper();
+            }
+        }
+
     }
 }
