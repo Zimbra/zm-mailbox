c1576ad7e2627378ab4ea451c765f8ca81c13b07
Gren Elliot <gren.elliot@synacor.com>
2017-08-11 11:24:28 +0100

    ZCS-2288:new ZSharedFolder.getTargetItemIdentifier

--- client/src/java/com/zimbra/client/ZSharedFolder.java
+++ client/src/java/com/zimbra/client/ZSharedFolder.java
@@ -17,7 +17,9 @@
 
 package com.zimbra.client;
 
+import com.zimbra.common.mailbox.ItemIdentifier;
 import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.soap.mail.type.Folder;
 
 public class ZSharedFolder extends ZFolder {
@@ -36,6 +38,18 @@ public class ZSharedFolder extends ZFolder {
 
     public String getTargetId() {
         return targetId;
+    }
+
+    public ItemIdentifier getTargetItemIdentifier() {
+        ItemIdentifier fId;
+        try {
+            fId = new ItemIdentifier(targetId, null);
+            return fId;
+        } catch (ServiceException e) {
+            ZimbraLog.mailbox.debug("Problem understanding folderId '%s' - assume hidden", targetId, e);
+            throw new RuntimeException(
+                    String.format("Problem understanding folderId '%s' - assume hidden", targetId), e);
+        }
     }
 
     @Override
