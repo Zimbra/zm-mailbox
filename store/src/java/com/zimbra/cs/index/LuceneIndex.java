0f4f9ad6236b215e676e509bc6e65d7aceeaa2d3
ilya <raykini@zimbra.com>
2017-09-05 15:39:38 -0700

    Lucene-based search history indexing

--- store/src/java/com/zimbra/cs/index/LuceneIndex.java
+++ store/src/java/com/zimbra/cs/index/LuceneIndex.java
@@ -886,6 +886,16 @@ public final class LuceneIndex extends IndexStore {
             }
         }
 
+        @Override
+        public void addDocument(IndexDocument doc) throws IOException {
+            synchronized (doc) {
+                Document luceneDoc = doc.toDocument();
+                if (ZimbraLog.index.isTraceEnabled()) {
+                    ZimbraLog.index.trace("Adding lucene document %s", luceneDoc.toString());
+                }
+                writer.get().addDocument(luceneDoc);
+            }
+        }
         /**
          * Deletes documents.
          * <p>
@@ -895,8 +905,14 @@ public final class LuceneIndex extends IndexStore {
          */
         @Override
         public void deleteDocument(List<Integer> ids) throws IOException {
+            deleteDocument(ids, LuceneFields.L_MAILBOX_BLOB_ID);
+        }
+
+        @Override
+        public void deleteDocument(List<Integer> ids, String fieldName)
+                throws IOException {
             for (Integer id : ids) {
-                Term term = new Term(LuceneFields.L_MAILBOX_BLOB_ID, id.toString());
+                Term term = new Term(fieldName, id.toString());
                 writer.get().deleteDocuments(term);
                 ZimbraLog.index.debug("Deleted documents id=%d", id);
             }
