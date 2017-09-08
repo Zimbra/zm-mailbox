1d46f7f4062db478295c9382e872a45d44680eda
Greg Solovyev <fiddlestring@gmail.com>
2017-09-08 16:35:14 -0700

    update to JUnit4

--- store/src/java/com/zimbra/qa/unittest/server/TestDataSourceServer.java
+++ store/src/java/com/zimbra/qa/unittest/server/TestDataSourceServer.java
@@ -16,13 +16,18 @@
  */
 package com.zimbra.qa.unittest.server;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
 
 import com.zimbra.client.ZFolder;
 import com.zimbra.client.ZImapDataSource;
 import com.zimbra.client.ZMailbox;
-import com.zimbra.cs.account.Account;
-import com.zimbra.cs.account.Cos;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.Server;
 import com.zimbra.cs.datasource.DataSourceManager;
@@ -31,55 +36,32 @@ import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.cs.mailbox.ScheduledTask;
 import com.zimbra.qa.unittest.TestUtil;
 import com.zimbra.soap.type.DataSource.ConnectionType;
+public class TestDataSourceServer {
 
-public class TestDataSourceServer extends TestCase {
+    @Rule
+    public TestName testInfo = new TestName();
 
-    private static final String USER_NAME = "user1";
-    private static final String TEST_USER_NAME = "testdatasource";
-    private static final String NAME_PREFIX = TestDataSourceServer.class.getSimpleName();
+    protected static String USER_NAME = null;
+    protected String testId;
     
-    private String mOriginalAccountPollingInterval;
-    private String mOriginalAccountPop3PollingInterval;
-    private String mOriginalAccountImapPollingInterval;
-    
-    private String mOriginalCosPollingInterval;
-    private String mOriginalCosPop3PollingInterval;
-    private String mOriginalCosImapPollingInterval;
-    
-    public void setUp()
-    throws Exception {
+    @Before
+    public void setUp() throws Exception {
+        testId = String.format("%s-%s-%d", this.getClass().getSimpleName(), testInfo.getMethodName(), (int)Math.abs(Math.random()*100));
+        USER_NAME = String.format("%s-user", testId).toLowerCase();
         cleanUp();
-
-        // Remember original polling intervals.
-        Account account = TestUtil.getAccount(USER_NAME);
-        Cos cos = account.getCOS();
-        mOriginalAccountPollingInterval = account.getAttr(Provisioning.A_zimbraDataSourcePollingInterval, false);
-        if (mOriginalAccountPollingInterval == null) {
-            mOriginalAccountPollingInterval = "";
-        }
-        mOriginalAccountPop3PollingInterval = account.getAttr(Provisioning.A_zimbraDataSourcePop3PollingInterval, false);
-        if (mOriginalAccountPop3PollingInterval == null) {
-            mOriginalAccountPop3PollingInterval = "";
-        }
-        mOriginalAccountImapPollingInterval = account.getAttr(Provisioning.A_zimbraDataSourceImapPollingInterval, false);
-        if (mOriginalAccountImapPollingInterval == null) {
-            mOriginalAccountImapPollingInterval = "";
-        }
-        
-        mOriginalCosPollingInterval = cos.getAttr(Provisioning.A_zimbraDataSourcePollingInterval, "");
-        mOriginalCosPop3PollingInterval = cos.getAttr(Provisioning.A_zimbraDataSourcePop3PollingInterval, "");
-        mOriginalCosImapPollingInterval = cos.getAttr(Provisioning.A_zimbraDataSourceImapPollingInterval, "");
+        TestUtil.createAccount(USER_NAME);
     }
-    
+
+    @Test
     public void testScheduling()
     throws Exception {
         // Create data source.
         ZMailbox zmbox = TestUtil.getZMailbox(USER_NAME);
-        ZFolder folder = TestUtil.createFolder(zmbox, "/" + NAME_PREFIX + "-testScheduling");
+        ZFolder folder = TestUtil.createFolder(zmbox, "/testScheduling");
         Provisioning prov = Provisioning.getInstance();
         Server server = prov.getLocalServer();
         int port = server.getImapBindPort();
-        ZImapDataSource zds = new ZImapDataSource(NAME_PREFIX + " testScheduling", true, "localhost", port,
+        ZImapDataSource zds = new ZImapDataSource("testScheduling", true, "localhost", port,
             "user2", "test123", folder.getId(), ConnectionType.cleartext);
         String dsId = zmbox.createDataSource(zds);
         
@@ -122,26 +104,14 @@ public class TestDataSourceServer extends TestCase {
         }
     }
     
-    public void tearDown()
-    throws Exception {
-        // Reset original polling intervals.
-        Account account = TestUtil.getAccount(USER_NAME);
-        Cos cos = account.getCOS();
-        
-        account.setDataSourcePollingInterval(mOriginalAccountPollingInterval);
-        account.setDataSourcePop3PollingInterval(mOriginalAccountPop3PollingInterval);
-        account.setDataSourceImapPollingInterval(mOriginalAccountImapPollingInterval);
-        
-        cos.setDataSourcePollingInterval(mOriginalCosPollingInterval);
-        cos.setDataSourcePop3PollingInterval(mOriginalCosPop3PollingInterval);
-        cos.setDataSourceImapPollingInterval(mOriginalCosImapPollingInterval);
-        
+    @After
+    public void tearDown() throws Exception {
         cleanUp();
     }
     
-    public void cleanUp()
-    throws Exception {
-        TestUtil.deleteAccount(TEST_USER_NAME);
-        TestUtil.deleteTestData(USER_NAME, NAME_PREFIX);
+    private void cleanUp() throws Exception {
+        if (USER_NAME != null) {
+            TestUtil.deleteAccountIfExists(USER_NAME);
+        }
     }
 }
