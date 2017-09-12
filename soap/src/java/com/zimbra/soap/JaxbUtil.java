d569eb8e2bcb3f472392dcb291bc7c8a91ccf5d1
sbluhm <github.com@bluhm-de.com>
2017-09-12 10:58:56 +0200

    Update JaxbUtil.java

--- soap/src/java/com/zimbra/soap/JaxbUtil.java
+++ soap/src/java/com/zimbra/soap/JaxbUtil.java
@@ -1096,23 +1096,7 @@ public final class JaxbUtil {
             com.zimbra.soap.account.message.GetSmimeCertificateInfoRequest.class,
             com.zimbra.soap.account.message.GetSmimeCertificateInfoResponse.class,
             com.zimbra.soap.account.message.SaveSmimeCertificateRequest.class,
-            com.zimbra.soap.account.message.SaveSmimeCertificateResponse.class,
-
-            //IMAP
-            com.zimbra.soap.mail.message.ListIMAPSubscriptionsRequest.class,
-            com.zimbra.soap.mail.message.ListIMAPSubscriptionsResponse.class,
-            com.zimbra.soap.mail.message.SaveIMAPSubscriptionsRequest.class,
-            com.zimbra.soap.mail.message.SaveIMAPSubscriptionsResponse.class,
-            com.zimbra.soap.mail.message.ResetRecentMessageCountRequest.class,
-            com.zimbra.soap.mail.message.ResetRecentMessageCountResponse.class,
-            com.zimbra.soap.mail.message.OpenIMAPFolderRequest.class,
-            com.zimbra.soap.mail.message.OpenIMAPFolderResponse.class,
-            com.zimbra.soap.mail.message.GetModifiedItemsIDsRequest.class,
-            com.zimbra.soap.mail.message.GetModifiedItemsIDsResponse.class,
-            com.zimbra.soap.mail.message.GetLastItemIdInMailboxRequest.class,
-            com.zimbra.soap.mail.message.GetLastItemIdInMailboxResponse.class,
-            com.zimbra.soap.mail.message.BeginTrackingIMAPRequest.class,
-            com.zimbra.soap.mail.message.BeginTrackingIMAPResponse.class
+            com.zimbra.soap.account.message.SaveSmimeCertificateResponse.class
         };
 
         try {
