e910e619451d676bf0f3275a9979c35e12b5ea7f
ilya <raykini@zimbra.com>
2017-09-14 08:41:31 -0700

    updated description of zimbraSearchHistoryAge

--- store/src/java/com/zimbra/cs/account/ZAttrAccount.java
+++ store/src/java/com/zimbra/cs/account/ZAttrAccount.java
@@ -55766,9 +55766,10 @@ public abstract class ZAttrAccount  extends MailTarget {
     /**
      * Search history entries older than this will not show up in search
      * suggestions and will not be included in the count used for
-     * zimbraNumSearchesForSavedSearchPrompt. If purging is enabled, these
-     * entries will be purged as part of a mailbox purge.. Must be in valid
-     * duration format: {digits}{time-unit}. digits: 0-9, time-unit:
+     * zimbraNumSearchesForSavedSearchPrompt or as part of
+     * GetSearchHistoryResponse/SearchSuggestResponse. If purging is enabled,
+     * these entries will be purged as part of a mailbox purge.. Must be in
+     * valid duration format: {digits}{time-unit}. digits: 0-9, time-unit:
      * [hmsd]|ms. h - hours, m - minutes, s - seconds, d - days, ms -
      * milliseconds. If time unit is not specified, the default is
      * s(seconds).
@@ -55789,9 +55790,10 @@ public abstract class ZAttrAccount  extends MailTarget {
     /**
      * Search history entries older than this will not show up in search
      * suggestions and will not be included in the count used for
-     * zimbraNumSearchesForSavedSearchPrompt. If purging is enabled, these
-     * entries will be purged as part of a mailbox purge.. Must be in valid
-     * duration format: {digits}{time-unit}. digits: 0-9, time-unit:
+     * zimbraNumSearchesForSavedSearchPrompt or as part of
+     * GetSearchHistoryResponse/SearchSuggestResponse. If purging is enabled,
+     * these entries will be purged as part of a mailbox purge.. Must be in
+     * valid duration format: {digits}{time-unit}. digits: 0-9, time-unit:
      * [hmsd]|ms. h - hours, m - minutes, s - seconds, d - days, ms -
      * milliseconds. If time unit is not specified, the default is
      * s(seconds).
@@ -55808,9 +55810,10 @@ public abstract class ZAttrAccount  extends MailTarget {
     /**
      * Search history entries older than this will not show up in search
      * suggestions and will not be included in the count used for
-     * zimbraNumSearchesForSavedSearchPrompt. If purging is enabled, these
-     * entries will be purged as part of a mailbox purge.. Must be in valid
-     * duration format: {digits}{time-unit}. digits: 0-9, time-unit:
+     * zimbraNumSearchesForSavedSearchPrompt or as part of
+     * GetSearchHistoryResponse/SearchSuggestResponse. If purging is enabled,
+     * these entries will be purged as part of a mailbox purge.. Must be in
+     * valid duration format: {digits}{time-unit}. digits: 0-9, time-unit:
      * [hmsd]|ms. h - hours, m - minutes, s - seconds, d - days, ms -
      * milliseconds. If time unit is not specified, the default is
      * s(seconds).
@@ -55830,9 +55833,10 @@ public abstract class ZAttrAccount  extends MailTarget {
     /**
      * Search history entries older than this will not show up in search
      * suggestions and will not be included in the count used for
-     * zimbraNumSearchesForSavedSearchPrompt. If purging is enabled, these
-     * entries will be purged as part of a mailbox purge.. Must be in valid
-     * duration format: {digits}{time-unit}. digits: 0-9, time-unit:
+     * zimbraNumSearchesForSavedSearchPrompt or as part of
+     * GetSearchHistoryResponse/SearchSuggestResponse. If purging is enabled,
+     * these entries will be purged as part of a mailbox purge.. Must be in
+     * valid duration format: {digits}{time-unit}. digits: 0-9, time-unit:
      * [hmsd]|ms. h - hours, m - minutes, s - seconds, d - days, ms -
      * milliseconds. If time unit is not specified, the default is
      * s(seconds).
@@ -55853,9 +55857,10 @@ public abstract class ZAttrAccount  extends MailTarget {
     /**
      * Search history entries older than this will not show up in search
      * suggestions and will not be included in the count used for
-     * zimbraNumSearchesForSavedSearchPrompt. If purging is enabled, these
-     * entries will be purged as part of a mailbox purge.. Must be in valid
-     * duration format: {digits}{time-unit}. digits: 0-9, time-unit:
+     * zimbraNumSearchesForSavedSearchPrompt or as part of
+     * GetSearchHistoryResponse/SearchSuggestResponse. If purging is enabled,
+     * these entries will be purged as part of a mailbox purge.. Must be in
+     * valid duration format: {digits}{time-unit}. digits: 0-9, time-unit:
      * [hmsd]|ms. h - hours, m - minutes, s - seconds, d - days, ms -
      * milliseconds. If time unit is not specified, the default is
      * s(seconds).
@@ -55874,9 +55879,10 @@ public abstract class ZAttrAccount  extends MailTarget {
     /**
      * Search history entries older than this will not show up in search
      * suggestions and will not be included in the count used for
-     * zimbraNumSearchesForSavedSearchPrompt. If purging is enabled, these
-     * entries will be purged as part of a mailbox purge.. Must be in valid
-     * duration format: {digits}{time-unit}. digits: 0-9, time-unit:
+     * zimbraNumSearchesForSavedSearchPrompt or as part of
+     * GetSearchHistoryResponse/SearchSuggestResponse. If purging is enabled,
+     * these entries will be purged as part of a mailbox purge.. Must be in
+     * valid duration format: {digits}{time-unit}. digits: 0-9, time-unit:
      * [hmsd]|ms. h - hours, m - minutes, s - seconds, d - days, ms -
      * milliseconds. If time unit is not specified, the default is
      * s(seconds).
