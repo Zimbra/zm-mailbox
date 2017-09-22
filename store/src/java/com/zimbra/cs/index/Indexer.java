fc33a000481112afe45c0b507608555190acbc9b
ilya <raykini@zimbra.com>
2017-09-22 16:40:05 -0700

    updated index classes to Lucene/Solr 6.6

--- store/src/java/com/zimbra/cs/index/Indexer.java
+++ store/src/java/com/zimbra/cs/index/Indexer.java
@@ -20,8 +20,10 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.util.List;
 
+import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.mailbox.Folder;
 import com.zimbra.cs.mailbox.MailItem;
+import com.zimbra.cs.mailbox.Mailbox.IndexItemEntry;
 
 /**
  * Abstraction of index write operations.
@@ -33,16 +35,12 @@ import com.zimbra.cs.mailbox.MailItem;
 public interface Indexer extends Closeable {
 
     /**
-     * Adds index documents.
-     */
-    void addDocument(Folder folder, MailItem item, List<IndexDocument> docs) throws IOException;
-
-    /**
      * Deletes index documents.
      *
      * @param ids list of item IDs to delete
+     * @throws ServiceException
      */
-    void deleteDocument(List<Integer> ids) throws IOException;
+    void deleteDocument(List<Integer> ids) throws IOException, ServiceException;
 
     /**
      * Compacts the index by expunging all the deletes.
@@ -59,13 +57,29 @@ public interface Indexer extends Closeable {
     int maxDocs();
 
     /**
-     * Index a stand-alone document
-     * Currently used to index search history items
+     * Index an IndexDocument derived from a search history item
      */
-    void addDocument(IndexDocument doc) throws IOException;
+    void addSearchHistoryDocument(IndexDocument doc) throws IOException, ServiceException;
+
+    /**
+     * Adds single MailItem to index. A MailItem may contain multiple IndexDocuments.
+     * @param MailItem
+     * @param list of IndexDocument docs
+     * @throws ServiceException
+     * @throws TemporaryIndexingException
+     */
+    void addDocument(MailItem item,List<IndexDocument> docs) throws IOException, ServiceException;
 
     /**
      * Deletes index documents by the specified field name
      */
-    void deleteDocument(List<Integer> ids, String fieldName) throws IOException;
+    void deleteDocument(List<Integer> ids, String fieldName) throws IOException, ServiceException;
+
+    /**
+     * Adds multiple MailItems to index. Each MailItem may contain multiple IndexDocuments
+     * @param entries encapsulated in IndexItemEntry class
+     * @throws IOException
+     * @throws ServiceException
+     */
+    void add(List<IndexItemEntry> entries) throws IOException, ServiceException;
 }
