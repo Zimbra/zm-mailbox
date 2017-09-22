d0bd1c4bddb161aea45d5d9b560449eaededc000
ilya <raykini@zimbra.com>
2017-09-22 16:09:48 -0700

    Indexing queue classes

---
+++ store/src/java-test/com/zimbra/cs/index/queue/IndexingServiceTest.java
@@ -0,0 +1,298 @@
+package com.zimbra.cs.index.queue;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.MockProvisioning;
+import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.mailbox.DeliveryOptions;
+import com.zimbra.cs.mailbox.MailItem;
+import com.zimbra.cs.mailbox.Mailbox;
+import com.zimbra.cs.mailbox.MailboxManager;
+import com.zimbra.cs.mailbox.MailboxTestUtil;
+import com.zimbra.cs.mime.ParsedMessage;
+import com.zimbra.qa.unittest.TestUtil;
+
+public class IndexingServiceTest {
+
+    @BeforeClass
+    public static void init() throws Exception {
+        MailboxTestUtil.initServer();
+        Provisioning prov = Provisioning.getInstance();
+        prov.createAccount("test@zimbra.com", "secret", new HashMap<String, Object>());
+
+        //use local queue for testing
+        //Provisioning.getInstance().getLocalServer().setIndexingQueueProvider("com.zimbra.cs.index.LocalIndexingQueueAdapter");
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        cleanup();
+        //indexing service should not be running at the beginning of these tests
+        IndexingService.getInstance().shutDown();
+    }
+
+    private void cleanup() throws Exception {
+        IndexingQueueAdapter queueAdapter = IndexingQueueAdapter.getFactory().getAdapter();
+        if(queueAdapter != null) {
+            queueAdapter.drain();
+            queueAdapter.clearAllTaskCounts();
+        }
+        MailboxTestUtil.clearData();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        cleanup();
+    }
+
+    @AfterClass
+    public static void destroy() throws Exception {
+        Account acc = Provisioning.getInstance().getAccountByName("test@zimbra.com");
+        acc.deleteAccount();
+        IndexingService.getInstance().shutDown();
+        Provisioning.getInstance().getLocalServer().setIndexManualCommit(true);
+    }
+
+    @Test
+    public void testAsyncIndex() throws Exception {
+        Provisioning.getInstance().getLocalServer().setIndexManualCommit(true);
+        Provisioning.getInstance().getLocalServer().setReindexBatchSize(10);
+        Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(MockProvisioning.DEFAULT_ACCOUNT_ID);
+        DeliveryOptions dopt = new DeliveryOptions().setFolderId(Mailbox.ID_FOLDER_INBOX);
+
+        //the only real-life case when Zimbra is indexing multiple items via a shared queue is re-indexing. Here we will simulate this scenario
+        List<MailItem> mailItems = new ArrayList<MailItem>();
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Shall I compare thee to a summer's day".getBytes(), false), dopt, null));
+
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Thou art more lovely and more temperate".getBytes(), false), dopt, null));
+
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Rough winds do shake the darling buds of May".getBytes(), false), dopt, null));
+
+        //MailboxTestUtil.waitForIndexing(mbox);
+        List<Integer> ids = TestUtil.search(mbox, "from:greg", MailItem.Type.MESSAGE);
+        Assert.assertEquals(3, ids.size());
+
+        mbox.index.deleteIndex();
+        ids = TestUtil.search(mbox, "from:greg", MailItem.Type.MESSAGE);
+        Assert.assertEquals(0, ids.size());
+
+        mbox.lock.lock();
+        assertTrue("MailboxIndex.add should return TRUE", mbox.index.queue(mailItems, false));
+        mbox.lock.release();
+
+        //start indexing service
+        IndexingService.getInstance().startUp();
+
+        mbox.index.getIndexStore().waitForIndexCommit(1000);
+
+        ids = TestUtil.search(mbox, "from:greg", MailItem.Type.MESSAGE);
+        assertEquals(3, ids.size());
+    }
+
+    @Test
+    public void testDeletedItem() throws Exception {
+        Provisioning.getInstance().getLocalServer().setIndexManualCommit(true);
+        Provisioning.getInstance().getLocalServer().setReindexBatchSize(10);
+        Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(MockProvisioning.DEFAULT_ACCOUNT_ID);
+        DeliveryOptions dopt = new DeliveryOptions().setFolderId(Mailbox.ID_FOLDER_INBOX);
+
+        //the only real-life case when Zimbra is indexing multiple items via a shared queue is re-indexing. Here we will simulate this scenario
+        List<MailItem> mailItems = new ArrayList<MailItem>();
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Shall I compare thee to a summer's day".getBytes(), false), dopt, null));
+
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Thou art more lovely and more temperate".getBytes(), false), dopt, null));
+
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Rough winds do shake the darling buds of May".getBytes(), false), dopt, null));
+
+        mbox.index.getIndexStore().waitForIndexCommit(1000);
+        List<Integer> ids = TestUtil.search(mbox, "from:greg", MailItem.Type.MESSAGE);
+        Assert.assertEquals("should find 3 items", 3, ids.size());
+
+        mbox.index.deleteIndex();
+        ids = TestUtil.search(mbox, "from:greg", MailItem.Type.MESSAGE);
+        Assert.assertEquals(0, ids.size());
+        mbox.delete(null, mailItems.get(0).getId(), MailItem.Type.MESSAGE);
+        mbox.lock.lock();
+        assertTrue("MailboxIndex.add should return TRUE", mbox.index.queue(mailItems, false));
+        mbox.lock.release();
+
+        //start indexing service
+        IndexingService.getInstance().startUp();
+
+        mbox.index.getIndexStore().waitForIndexCommit(1000);
+
+        ids = TestUtil.search(mbox, "from:greg", MailItem.Type.MESSAGE);
+        assertEquals(2, ids.size());
+    }
+
+    @Test
+    public void testInvalidItem() throws Exception {
+        Provisioning.getInstance().getLocalServer().setIndexManualCommit(true);
+        Provisioning.getInstance().getLocalServer().setReindexBatchSize(10);
+        Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(MockProvisioning.DEFAULT_ACCOUNT_ID);
+        DeliveryOptions dopt = new DeliveryOptions().setFolderId(Mailbox.ID_FOLDER_INBOX);
+
+        //the only real-life case when Zimbra is indexing multiple items via a shared queue is re-indexing. Here we will simulate this scenario
+        List<MailItem> mailItems = new ArrayList<MailItem>();
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Shall I compare thee to a summer's day".getBytes(), false), dopt, null));
+
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Thou art more lovely and more temperate".getBytes(), false), dopt, null));
+
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Rough winds do shake the darling buds of May".getBytes(), false), dopt, null));
+
+        mbox.index.getIndexStore().waitForIndexCommit(1000);
+        List<Integer> ids = TestUtil.search(mbox, "from:greg", MailItem.Type.MESSAGE);
+        Assert.assertEquals("should find 3 items", 3, ids.size());
+
+        mbox.index.deleteIndex();
+        ids = TestUtil.search(mbox, "from:greg", MailItem.Type.MESSAGE);
+        Assert.assertEquals(0, ids.size());
+        mbox.delete(null, mailItems.get(0).getId(), MailItem.Type.MESSAGE);
+        int [] deletedIds = {mailItems.get(0).getId()};
+        mbox.deleteFromDumpster(null, deletedIds);
+        mbox.lock.lock();
+        assertTrue("MailboxIndex.add should return TRUE", mbox.index.queue(mailItems, false));
+        mbox.lock.release();
+
+        //start indexing service
+        IndexingService.getInstance().startUp();
+
+        mbox.index.getIndexStore().waitForIndexCommit(1000);
+
+        ids = TestUtil.search(mbox, "from:greg", MailItem.Type.MESSAGE);
+        assertEquals(2, ids.size());
+    }
+
+    @Test
+    public void testAsyncDeleteAllFromIndex() throws Exception {
+        Provisioning.getInstance().getLocalServer().setIndexManualCommit(true);
+        Provisioning.getInstance().getLocalServer().setReindexBatchSize(10);
+        Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(MockProvisioning.DEFAULT_ACCOUNT_ID);
+        DeliveryOptions dopt = new DeliveryOptions().setFolderId(Mailbox.ID_FOLDER_INBOX);
+
+        //the only real-life case when Zimbra is indexing multiple items via a shared queue is re-indexing. Here we will simulate this scenario
+        List<MailItem> mailItems = new ArrayList<MailItem>();
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Shall I compare thee to a summer's day".getBytes(), false), dopt, null));
+
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Thou art more lovely and more temperate".getBytes(), false), dopt, null));
+
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Rough winds do shake the darling buds of May".getBytes(), false), dopt, null));
+
+        mbox.index.getIndexStore().waitForIndexCommit(1000);
+        List<Integer> ids = TestUtil.search(mbox, "from:greg", MailItem.Type.MESSAGE);
+        Assert.assertEquals("should find 3 items", 3, ids.size());
+
+        //queue items for deletion from index
+        IndexingQueueAdapter queueAdapter = IndexingQueueAdapter.getFactory().getAdapter();
+        DeleteFromIndexTaskLocator itemLocator = new DeleteFromIndexTaskLocator(ids, mbox.getAccountId(), mbox.getId(), mbox.getSchemaGroupId());
+        queueAdapter.put(itemLocator);
+
+        //start indexing service
+        IndexingService.getInstance().startUp();
+
+        mbox.index.getIndexStore().waitForIndexCommit(1000);
+
+        ids = TestUtil.search(mbox, "from:greg", MailItem.Type.MESSAGE);
+        assertEquals("should not be ale to find any items after deletion from indes", 0, ids.size());
+    }
+
+    @Test
+    public void testAsyncDeleteOneFromIndex() throws Exception {
+        Provisioning.getInstance().getLocalServer().setIndexManualCommit(true);
+        Provisioning.getInstance().getLocalServer().setReindexBatchSize(10);
+        Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(MockProvisioning.DEFAULT_ACCOUNT_ID);
+        DeliveryOptions dopt = new DeliveryOptions().setFolderId(Mailbox.ID_FOLDER_INBOX);
+
+        //the only real-life case when Zimbra is indexing multiple items via a shared queue is re-indexing. Here we will simulate this scenario
+        List<MailItem> mailItems = new ArrayList<MailItem>();
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Shall I compare thee to a summer's day".getBytes(), false), dopt, null));
+
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Thou art more lovely and more temperate".getBytes(), false), dopt, null));
+
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Rough winds do shake the darling buds of May".getBytes(), false), dopt, null));
+
+        mbox.index.getIndexStore().waitForIndexCommit(1000);
+        List<Integer> ids = TestUtil.search(mbox, "from:greg", MailItem.Type.MESSAGE);
+        Assert.assertEquals("should find 3 items", 3, ids.size());
+
+        //queue items for deletion from index
+        IndexingQueueAdapter queueAdapter = IndexingQueueAdapter.getFactory().getAdapter();
+        DeleteFromIndexTaskLocator itemLocator = new DeleteFromIndexTaskLocator(ids.get(0), mbox.getAccountId(), mbox.getId(), mbox.getSchemaGroupId());
+        queueAdapter.put(itemLocator);
+        //start indexing service
+        IndexingService.getInstance().startUp();
+
+        mbox.index.getIndexStore().waitForIndexCommit(1000);
+
+        ids = TestUtil.search(mbox, "from:greg", MailItem.Type.MESSAGE);
+        Assert.assertEquals("should find 2 items", 2, ids.size());
+    }
+
+    @Test
+    public void testAsyncDeleteSomeFromIndex() throws Exception {
+        Provisioning.getInstance().getLocalServer().setIndexManualCommit(true);
+        Provisioning.getInstance().getLocalServer().setReindexBatchSize(10);
+        Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(MockProvisioning.DEFAULT_ACCOUNT_ID);
+        DeliveryOptions dopt = new DeliveryOptions().setFolderId(Mailbox.ID_FOLDER_INBOX);
+
+        //the only real-life case when Zimbra is indexing multiple items via a shared queue is re-indexing. Here we will simulate this scenario
+        List<MailItem> mailItems = new ArrayList<MailItem>();
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Shall I compare thee to a summer's day".getBytes(), false), dopt, null));
+
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Thou art more lovely and more temperate".getBytes(), false), dopt, null));
+
+        mailItems.add(mbox.addMessage(null, new ParsedMessage(
+                "From: greg@zimbra.com\r\nTo: test@zimbra.com\r\nSubject: Rough winds do shake the darling buds of May".getBytes(), false), dopt, null));
+
+        mbox.index.getIndexStore().waitForIndexCommit(1000);
+        List<Integer> ids = TestUtil.search(mbox, "from:greg", MailItem.Type.MESSAGE);
+        Assert.assertEquals("should find 3 items", 3, ids.size());
+
+        //queue items for deletion from index
+        List<Integer> idsToDelete = Lists.newArrayList();
+        idsToDelete.add(ids.get(0));
+        idsToDelete.add(ids.get(1));
+        IndexingQueueAdapter queueAdapter = IndexingQueueAdapter.getFactory().getAdapter();
+        DeleteFromIndexTaskLocator itemLocator = new DeleteFromIndexTaskLocator(idsToDelete, mbox.getAccountId(), mbox.getId(), mbox.getSchemaGroupId());
+        queueAdapter.put(itemLocator);
+
+        //start indexing service
+        IndexingService.getInstance().startUp();
+
+        mbox.index.getIndexStore().waitForIndexCommit(1000);
+
+        ids = TestUtil.search(mbox, "from:greg", MailItem.Type.MESSAGE);
+        Assert.assertEquals("should find 1 item", 1, ids.size());
+    }
+}
---
+++ store/src/java/com/zimbra/cs/index/queue/LocalIndexingQueueAdapter.java
@@ -0,0 +1,205 @@
+package com.zimbra.cs.index.queue;
+
+import java.util.HashMap;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.mailbox.ReIndexStatus;
+import com.zimbra.cs.util.ProvisioningUtil;
+
+/**
+ * @author Greg Solovyev
+ * Default implementation of the indexing queue for
+ * non-cluster environment
+ */
+public class LocalIndexingQueueAdapter extends IndexingQueueAdapter {
+    private final ArrayBlockingQueue<AbstractIndexingTasksLocator> itemQueue;
+    private final HashMap<String, Integer> totalCounters;
+    private final HashMap<String, Integer> succeededCounters;
+    private final HashMap<String, Integer> failedCounters;
+    private final HashMap<String, Integer> taskStatus;
+
+    public LocalIndexingQueueAdapter() {
+        int queueSize = ProvisioningUtil.getServerAttribute(Provisioning.A_zimbraIndexingQueueMaxSize, 10000);
+        itemQueue = new ArrayBlockingQueue<AbstractIndexingTasksLocator>(queueSize);
+        totalCounters = new HashMap<String, Integer>();
+        succeededCounters = new HashMap<String, Integer>();
+        failedCounters = new HashMap<String, Integer>();
+        taskStatus = new HashMap<String, Integer>();
+    }
+
+    /**
+     * Add an item to the tail of the queue. If the underlying queue is full
+     * this call blocks until there is space in the queue.
+     *
+     * @param {@link com.zimbra.cs.index.AbstractIndexingTasksLocator} item
+     */
+    @Override
+    public boolean put(AbstractIndexingTasksLocator item) {
+        try {
+            itemQueue.put(item);
+        } catch (InterruptedException e) {
+            ZimbraLog.index.error("failed to queue items for indexing to mailbox %d", item.getMailboxID(), e);
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Add an item to the tail of the queue. If the underlying queue is full
+     * this call returns FALSE.
+     *
+     * @param {@link com.zimbra.cs.index.AbstractIndexingTasksLocator} item
+     * @return TRUE if the item was successfully added/FALSE otherwise.
+     */
+    @Override
+    public boolean add(AbstractIndexingTasksLocator item) throws ServiceException {
+        try {
+            return itemQueue.add(item);
+        } catch (IllegalStateException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Return the next element from the queue and remove it from the queue.
+     * If no element is available, return null
+     *
+     * @return {@link com.zimbra.cs.index.AbstractIndexingTasksLocator}
+     * @throws InterruptedException
+     */
+    @Override
+    public AbstractIndexingTasksLocator take()  {
+        try {
+            return itemQueue.take();
+        } catch (InterruptedException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Return the next element in the queue and keep it in the queue
+     *
+     * @return {@link com.zimbra.cs.index.AbstractIndexingTasksLocator}
+     */
+    @Override
+    public AbstractIndexingTasksLocator peek() {
+        return itemQueue.peek();
+    }
+
+    @Override
+    public boolean hasMoreItems() {
+        return !itemQueue.isEmpty();
+    }
+
+    @Override
+    public void drain() {
+        itemQueue.clear();
+        totalCounters.clear();
+        succeededCounters.clear();
+        failedCounters.clear();
+        taskStatus.clear();
+    }
+
+    @Override
+    public synchronized void incrementSucceededMailboxTaskCount(String accountId, int val) {
+        Integer currentCount = succeededCounters.get(accountId);
+        if (currentCount == null) {
+            currentCount = 0;
+        }
+        succeededCounters.put(accountId, currentCount + val);
+        checkStatus(accountId);
+    }
+
+    private void checkStatus(String accountId) {
+        if (getTaskStatus(accountId) != ReIndexStatus.STATUS_ABORTED
+                && (getSucceededMailboxTaskCount(accountId) + getFailedMailboxTaskCount(accountId)) >= getTotalMailboxTaskCount(accountId)) {
+            setTaskStatus(accountId, ReIndexStatus.STATUS_DONE);
+        }
+    }
+
+    @Override
+    public synchronized int getSucceededMailboxTaskCount(String accountId) {
+        Integer currentCount = succeededCounters.get(accountId);
+        return currentCount == null ? 0 : currentCount;
+    }
+
+    @Override
+    public synchronized void deleteMailboxTaskCounts(String accountId) {
+        totalCounters.remove(accountId);
+        succeededCounters.remove(accountId);
+        failedCounters.remove(accountId);
+    }
+
+    @Override
+    public synchronized void clearAllTaskCounts() {
+        totalCounters.clear();
+        succeededCounters.clear();
+        failedCounters.clear();
+    }
+
+    @Override
+    public void setTotalMailboxTaskCount(String accountId, int val) {
+        totalCounters.put(accountId, val);
+    }
+
+    @Override
+    public void setSucceededMailboxTaskCount(String accountId, int val) {
+        succeededCounters.put(accountId, val);
+    }
+
+    @Override
+    public int getTotalMailboxTaskCount(String accountId) {
+        Integer val = totalCounters.get(accountId);
+        return val == null ? 0 : val;
+    }
+
+    @Override
+    public void incrementFailedMailboxTaskCount(String accountId, int numItems) {
+        Integer currentCount = failedCounters.get(accountId);
+        if (currentCount == null) {
+            currentCount = 0;
+        }
+        failedCounters.put(accountId, currentCount + numItems);
+        checkStatus(accountId);
+    }
+
+    @Override
+    public int getFailedMailboxTaskCount(String accountId) {
+        Integer currentCount = failedCounters.get(accountId);
+        return currentCount == null ? 0 : currentCount;
+    }
+
+    @Override
+    public int getTaskStatus(String accountId) {
+        Integer status = taskStatus.get(accountId);
+        return status == null ? ReIndexStatus.STATUS_IDLE : status;
+    }
+
+    @Override
+    public void setTaskStatus(String accountId, int status) {
+        taskStatus.put(accountId, status);
+    }
+
+    @Override
+    public void setFailedMailboxTaskCount(String accountId, int val) {
+        failedCounters.put(accountId, val);
+    }
+
+    public static class Factory implements IndexingQueueAdapter.Factory {
+
+        private static LocalIndexingQueueAdapter instance = null;
+
+        @Override
+        public IndexingQueueAdapter getAdapter() {
+            synchronized(Factory.class) {
+                if (instance == null){
+                    instance = new LocalIndexingQueueAdapter();
+                }
+            }
+            return instance;
+        }
+    }
+}
