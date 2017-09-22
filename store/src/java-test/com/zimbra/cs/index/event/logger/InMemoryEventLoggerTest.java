fc27cf4c409b6f97089b6b8bdac4409fa0f4a7ec
varun.risbud <varun.risbud@synacor.com>
2017-09-22 14:42:26 -0400

    Added a new log category and changed the event interface to be more generic

--- store/src/java-test/com/zimbra/cs/index/event/logger/InMemoryEventLoggerTest.java
+++ store/src/java-test/com/zimbra/cs/index/event/logger/InMemoryEventLoggerTest.java
@@ -1,39 +1,30 @@
 package com.zimbra.cs.index.event.logger;
 
 import com.zimbra.common.service.ServiceException;
-import com.zimbra.cs.account.Account;
-import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.index.event.Event;
-import com.zimbra.cs.mailbox.Mailbox;
-import com.zimbra.cs.mailbox.MailboxManager;
-import com.zimbra.cs.mailbox.MailboxTestUtil;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.Collections;
-import java.util.HashMap;
 
 public class InMemoryEventLoggerTest {
-    private static Provisioning prov;
-    private static Account acct;
-    private static Mailbox mbox;
+    private EventLogger logger;
 
-    @BeforeClass
-    public static void init() throws Exception {
-        MailboxTestUtil.initServer();
-        prov = Provisioning.getInstance();
-        acct = prov.createAccount("test@zimbra.com", "test123", new HashMap<>());
-        mbox = MailboxManager.getInstance().getMailboxByAccountId(acct.getId());
+    @Before
+    public void setup() throws ServiceException {
+        EventLogger.setEventLogger(InMemoryEventLogger.class);
+        logger = EventLogger.getEventLogger();
     }
 
     @Test
     public void testLogging() {
-        Event event = new Event(mbox.getAccountId(), Event.EventType.SENT, System.currentTimeMillis(), "test@zimbra.com", "test1@zimbra.com", Collections.EMPTY_MAP);
-        InMemoryEventLogger inMemoryEventLogger = new InMemoryEventLogger();
-        inMemoryEventLogger.log(event);
+        Event event = new Event(Event.EventType.SENT, System.currentTimeMillis(), Collections.EMPTY_MAP);
+        logger.log(event);
+        InMemoryEventLogger inMemoryEventLogger = (InMemoryEventLogger) logger;
         Assert.assertNotNull(inMemoryEventLogger.getLogs());
         Assert.assertEquals(1, inMemoryEventLogger.getLogs().size());
+        Assert.assertNotNull(inMemoryEventLogger.getLogs().get(0));
+        Assert.assertEquals(Event.EventType.SENT, inMemoryEventLogger.getLogs().get(0).getEventType());
     }
 }
