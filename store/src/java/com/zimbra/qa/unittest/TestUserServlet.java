03eded3a04ae733cb62acd97459f6f07b7e58a38
Gren Elliot <gren.elliot@synacor.com>
2017-10-02 16:25:11 +0100

    ZCS-3083:TestUserServlet ValidationException move
    ValidationException is now in a different package

--- store/src/java/com/zimbra/qa/unittest/TestUserServlet.java
+++ store/src/java/com/zimbra/qa/unittest/TestUserServlet.java
@@ -67,7 +67,6 @@ import com.zimbra.cs.util.JMSession;
 import net.fortuna.ical4j.data.CalendarOutputter;
 import net.fortuna.ical4j.model.Date;
 import net.fortuna.ical4j.model.Property;
-import net.fortuna.ical4j.model.ValidationException;
 import net.fortuna.ical4j.model.component.VEvent;
 import net.fortuna.ical4j.model.parameter.FmtType;
 import net.fortuna.ical4j.model.parameter.Value;
@@ -77,6 +76,7 @@ import net.fortuna.ical4j.model.property.CalScale;
 import net.fortuna.ical4j.model.property.ProdId;
 import net.fortuna.ical4j.model.property.Uid;
 import net.fortuna.ical4j.model.property.Version;
+import net.fortuna.ical4j.validate.ValidationException;
 
 public class TestUserServlet{
 
