9ba46cbe171aaab6ab18fccd94b397dcbff8cc08
ilya <raykini@zimbra.com>
2017-10-03 12:45:38 -0700

    updated license blocks

--- store/src/java/com/zimbra/cs/index/TermInfo.java
+++ store/src/java/com/zimbra/cs/index/TermInfo.java
@@ -11,7 +11,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License along with this program.
- * If not, see <http://www.gnu.org/licenses/>.
+ * If not, see <https://www.gnu.org/licenses/>.
  * ***** END LICENSE BLOCK *****
  */
 package com.zimbra.cs.index;
