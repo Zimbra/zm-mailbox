422def962c24303c99b7376762c7ad91381b98f3
ilya <raykini@zimbra.com>
2017-10-04 09:49:05 -0700

    Solr LDAP attrs targeted to 8.8.6

--- common/src/java/com/zimbra/common/account/ZAttrProvisioning.java
+++ common/src/java/com/zimbra/common/account/ZAttrProvisioning.java
@@ -8153,7 +8153,7 @@ public class ZAttrProvisioning {
      * The maximum number of items that can be held in memory while queued
      * for indexing
      *
-     * @since ZCS 8.8.1
+     * @since ZCS 8.8.6
      */
     @ZAttr(id=3030)
     public static final String A_zimbraIndexingQueueMaxSize = "zimbraIndexingQueueMaxSize";
@@ -8165,7 +8165,7 @@ public class ZAttrProvisioning {
      * hours, m - minutes, s - seconds, d - days, ms - milliseconds. If time
      * unit is not specified, the default is s(seconds).
      *
-     * @since ZCS 8.8.1
+     * @since ZCS 8.8.6
      */
     @ZAttr(id=3033)
     public static final String A_zimbraIndexingQueuePollingInterval = "zimbraIndexingQueuePollingInterval";
@@ -8178,7 +8178,7 @@ public class ZAttrProvisioning {
      * minutes, s - seconds, d - days, ms - milliseconds. If time unit is not
      * specified, the default is s(seconds).
      *
-     * @since ZCS 8.8.1
+     * @since ZCS 8.8.6
      */
     @ZAttr(id=3036)
     public static final String A_zimbraIndexingQueueTimeout = "zimbraIndexingQueueTimeout";
@@ -8191,7 +8191,7 @@ public class ZAttrProvisioning {
      * milliseconds. If time unit is not specified, the default is
      * s(seconds).
      *
-     * @since ZCS 8.8.1
+     * @since ZCS 8.8.6
      */
     @ZAttr(id=3027)
     public static final String A_zimbraIndexPollingInterval = "zimbraIndexPollingInterval";
@@ -8200,7 +8200,7 @@ public class ZAttrProvisioning {
      * Maximum number of threads for re-indexing. Re-index threads are not
      * pooled.
      *
-     * @since ZCS 8.8.1
+     * @since ZCS 8.8.6
      */
     @ZAttr(id=3032)
     public static final String A_zimbraIndexReIndexThreads = "zimbraIndexReIndexThreads";
@@ -8215,7 +8215,7 @@ public class ZAttrProvisioning {
      * milliseconds. If time unit is not specified, the default is
      * s(seconds).
      *
-     * @since ZCS 8.8.1
+     * @since ZCS 8.8.6
      */
     @ZAttr(id=3026)
     public static final String A_zimbraIndexReplicationTimeout = "zimbraIndexReplicationTimeout";
@@ -8223,7 +8223,7 @@ public class ZAttrProvisioning {
     /**
      * The number of terms SOLR will cache during term enumeration queries
      *
-     * @since ZCS 8.8.1
+     * @since ZCS 8.8.6
      */
     @ZAttr(id=3024)
     public static final String A_zimbraIndexTermsCacheSize = "zimbraIndexTermsCacheSize";
@@ -8231,7 +8231,7 @@ public class ZAttrProvisioning {
     /**
      * Number of threads used for indexing items from the queue
      *
-     * @since ZCS 8.8.1
+     * @since ZCS 8.8.6
      */
     @ZAttr(id=3031)
     public static final String A_zimbraIndexThreads = "zimbraIndexThreads";
@@ -8246,7 +8246,7 @@ public class ZAttrProvisioning {
      * URL should be set to that of the ZooKeeper host managing the SOLR
      * cluster.
      *
-     * @since ZCS 8.8.1
+     * @since ZCS 8.8.6
      */
     @ZAttr(id=3023)
     public static final String A_zimbraIndexURL = "zimbraIndexURL";
@@ -9510,7 +9510,7 @@ public class ZAttrProvisioning {
      * to index a document fails, the server will put the document into the
      * indexing queue for additional indexing attempts.
      *
-     * @since ZCS 8.8.1
+     * @since ZCS 8.8.6
      */
     @ZAttr(id=3034)
     public static final String A_zimbraMaxIndexingRetries = "zimbraMaxIndexingRetries";
@@ -14322,7 +14322,7 @@ public class ZAttrProvisioning {
     /**
      * Maximum number of items to put into a single reindexing task.
      *
-     * @since ZCS 8.8.1
+     * @since ZCS 8.8.6
      */
     @ZAttr(id=3035)
     public static final String A_zimbraReindexBatchSize = "zimbraReindexBatchSize";
@@ -16183,7 +16183,7 @@ public class ZAttrProvisioning {
      * The number of times the server will attempt to connect to a SOLR core.
      * 1 is the recommended setting.
      *
-     * @since ZCS 8.8.1
+     * @since ZCS 8.8.6
      */
     @ZAttr(id=3025)
     public static final String A_zimbraSolrMaxRetries = "zimbraSolrMaxRetries";
@@ -16192,7 +16192,7 @@ public class ZAttrProvisioning {
      * Limit for how many replicas of the same SOLR Collection are allowed to
      * be created on each node.
      *
-     * @since ZCS 8.8.1
+     * @since ZCS 8.8.6
      */
     @ZAttr(id=3037)
     public static final String A_zimbraSolrMaxShardsPerNode = "zimbraSolrMaxShardsPerNode";
@@ -16202,7 +16202,7 @@ public class ZAttrProvisioning {
      * mailbox. This value is used in replicationFactor parameter when
      * creating Solr Collections in SolrCloud with via Collections API.
      *
-     * @since ZCS 8.8.1
+     * @since ZCS 8.8.6
      */
     @ZAttr(id=3028)
     public static final String A_zimbraSolrReplicationFactor = "zimbraSolrReplicationFactor";
@@ -17452,7 +17452,7 @@ public class ZAttrProvisioning {
      * minutes, s - seconds, d - days, ms - milliseconds. If time unit is not
      * specified, the default is s(seconds).
      *
-     * @since ZCS 8.8.1
+     * @since ZCS 8.8.6
      */
     @ZAttr(id=3029)
     public static final String A_zimbraZKClientTimeout = "zimbraZKClientTimeout";
