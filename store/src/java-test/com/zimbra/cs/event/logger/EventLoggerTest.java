d73af256d907cf3c04c7c457572d54eff5583070
varun.risbud <varun.risbud@synacor.com>
2017-10-10 17:55:04 -0400

    Added the shutdown method for the Event Logger

--- store/src/java-test/com/zimbra/cs/event/logger/EventLoggerTest.java
+++ store/src/java-test/com/zimbra/cs/event/logger/EventLoggerTest.java
@@ -7,10 +7,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class EventLoggerTest {
     EventLogger eventLogger = EventLogger.getEventLogger();
-    EventLogHandler mock2 = Mockito.mock(EventLogHandler.class);
-    EventLogHandler mock1 = Mockito.mock(EventLogHandler.class);
+    EventLogHandler mock2 = Mockito.mock(InMemoryEventLogHandler.class);
+    EventLogHandler mock1 = Mockito.mock(InMemoryEventLogHandler.class);
 
     @Before
     public void init() {
@@ -18,18 +21,61 @@ public class EventLoggerTest {
     }
 
     @Test
-    public void testEventLogger() {
-        eventLogger.registerEventLogHandler(mock1);
+    public void testEventLogger() throws InterruptedException {
+        eventLogger.registerEventLogHandler(new EventLogHandler() {
+            private AtomicInteger eventCount = new AtomicInteger(0);
+            @Override
+            public void log(Event event) {
+                eventCount.getAndIncrement();
+            }
+
+            @Override
+            public void shutdown() {
+                System.out.println("Logger 1 " + eventCount.get());
+            }
+        });
 
         Event event = new Event("eventLoggerAccountId", Event.EventType.SENT, System.currentTimeMillis());
 
-        eventLogger.log(event);
-        Mockito.verify(mock1, Mockito.times(1)).log(event);
+        Runnable producer = () -> {
+            Random generator = new Random(System.currentTimeMillis());
+            for (int i = 0; i < 100; i++) {
+                eventLogger.log(event);
+                if(generator.nextBoolean()) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        new Thread(producer).start();
 
-        eventLogger.registerEventLogHandler(mock2);
-        eventLogger.log(event);
-        Mockito.verify(mock1, Mockito.times(2)).log(event);
-        Mockito.verify(mock2, Mockito.times(1)).log(event);
+        //eventLogger.log(event);
+        //Mockito.verify(mock1, Mockito.times(1)).log(event);
+
+        eventLogger.registerEventLogHandler(new EventLogHandler() {
+            private AtomicInteger eventCount = new AtomicInteger(0);
+            @Override
+            public void log(Event event) {
+                eventCount.getAndIncrement();
+            }
+
+            @Override
+            public void shutdown() {
+                System.out.println("Logger 2 " + eventCount.get());
+            }
+        });
+
+        new Thread(producer).start();
+        //eventLogger.log(event);
+        //Mockito.verify(mock1, Mockito.times(2)).log(event);
+        //Mockito.verify(mock2, Mockito.times(1)).log(event);
+        eventLogger.shutdownEventNotifierExecutor();
+        Thread.sleep(5000);
+        eventLogger.startupEventNotifierExecutor();
+        eventLogger.shutdowEventLogger();
     }
 
     @Test
