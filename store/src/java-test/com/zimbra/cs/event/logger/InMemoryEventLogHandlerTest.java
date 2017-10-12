808ca3232ad8df84011e22fbff13f37f70301088
varun.risbud <varun.risbud@synacor.com>
2017-10-12 16:23:45 -0400

    Added test cases for EventLogger. Fixed minor issues

--- store/src/java-test/com/zimbra/cs/event/logger/InMemoryEventLogHandlerTest.java
+++ store/src/java-test/com/zimbra/cs/event/logger/InMemoryEventLogHandlerTest.java
@@ -1,27 +1,44 @@
 package com.zimbra.cs.event.logger;
 
+import com.google.common.collect.Maps;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.event.Event;
 import org.junit.*;
 import org.mockito.Mockito;
 
 import java.util.Collections;
+import java.util.HashMap;
 
 public class InMemoryEventLogHandlerTest {
-    InMemoryEventLogHandler inMemoryEventLogHandler = new InMemoryEventLogHandler();
 
     @Before
     public void init() throws ServiceException {
-        EventLogger.getEventLogger().unregisterAllEventLogHandlers();
+        EventLogger.unregisterAllHandlerFactories();
     }
 
     @Test
-    public void testLogging() {
-        EventLogger eventLogger = EventLogger.getEventLogger();
-        eventLogger.registerEventLogHandler(inMemoryEventLogHandler);
+    public void testInMemoryEventLogHandler() {
+        EventLogger.ConfigProvider mockConfigProvider = Mockito.mock(EventLogger.ConfigProvider.class);
+        HashMap<String, String> testConfigMap = Maps.newHashMap();
+        testConfigMap.put("inMemoryEventLogHandlerFactory", "");
+        Mockito.doReturn(testConfigMap).when(mockConfigProvider).getHandlerConfig();
 
+        //Setting number of threads in executor service as 2
+        Mockito.doReturn(2).when(mockConfigProvider).getNumThreads();
+
+
+        //Creating mock InMemoryEventLogHandler.Factory so it can return a spy instance of InMemoryEventLogHandler
+        EventLogHandler.Factory inMemoryEventLogHandlerFactory = new InMemoryEventLogHandler.Factory();
+        EventLogger.registerHandlerFactory("inMemoryEventLogHandlerFactory", inMemoryEventLogHandlerFactory);
+
+        EventLogger eventLogger = EventLogger.getEventLogger(mockConfigProvider);
         Event event = new Event("InMemoryEventLoggerTestAccountId", Event.EventType.SENT, System.currentTimeMillis(), Collections.EMPTY_MAP);
         eventLogger.log(event);
+
+        eventLogger.startupEventNotifierExecutor();
+        eventLogger.shutdownEventLogger();
+
+        InMemoryEventLogHandler inMemoryEventLogHandler = (InMemoryEventLogHandler) inMemoryEventLogHandlerFactory.createHandler("");
 
         Assert.assertNotNull(inMemoryEventLogHandler.getLogs());
         Assert.assertEquals(1, inMemoryEventLogHandler.getLogs().size());
@@ -32,6 +49,6 @@ public class InMemoryEventLogHandlerTest {
 
     @AfterClass
     public static void cleanup() {
-        EventLogger.getEventLogger().unregisterAllEventLogHandlers();
+        EventLogger.unregisterAllHandlerFactories();
     }
 }
