e288761059d3eb5da26c5c47564b10c871b49ff2
ilya <raykini@zimbra.com>
2017-10-13 08:37:13 -0700

    allow multiple event handlers for same factory

--- store/src/java/com/zimbra/cs/account/callback/EventLoggerCallback.java
+++ store/src/java/com/zimbra/cs/account/callback/EventLoggerCallback.java
@@ -2,10 +2,13 @@ package com.zimbra.cs.account.callback;
 
 import java.util.Map;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.account.AttributeCallback;
+import com.zimbra.cs.account.Config;
 import com.zimbra.cs.account.Entry;
 import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.account.Server;
 import com.zimbra.cs.event.logger.EventLogger;
 
 public class EventLoggerCallback extends AttributeCallback {
@@ -13,11 +16,33 @@ public class EventLoggerCallback extends AttributeCallback {
     @Override
     public void preModify(CallbackContext context, String attrName, Object attrValue, Map attrsToModify, Entry entry) throws ServiceException {
         if (attrName.equalsIgnoreCase(Provisioning.A_zimbraEventLoggingBackends)) {
-            String backend = (String) attrValue;
-            String[] tokens = backend.split(":");
-            String backendName = tokens[0];
-            if (!EventLogger.isFactoryRegistered(backendName)) {
-                throw ServiceException.FAILURE(String.format("'%s' does not correspond to a registered EventLogHandler Factory", backendName), null);
+            String newBackend = (String) attrValue;
+            String[] tokens = newBackend.split(":", 2);
+            String newBackendName = tokens[0];
+            String newBackendConfig = tokens[1];
+            //check that a corresponding EventLogHandler Factory exists
+            if (!EventLogger.isFactoryRegistered(newBackendName)) {
+                throw ServiceException.FAILURE(String.format("'%s' does not correspond to a registered EventLogHandler Factory", newBackendName), null);
+            }
+            //check that a handler with the same config is not already registered
+            checkExistingHandlers(entry, newBackendName, newBackendConfig);
+        }
+    }
+
+    private void checkExistingHandlers(Entry entry, String newBackendName, String newBackendConfig) throws ServiceException {
+        String[] curBackends = null;
+        if (entry instanceof Server) {
+            curBackends = ((Server) entry).getEventLoggingBackends();
+        } else if (entry instanceof Config) {
+            curBackends = ((Config) entry).getEventLoggingBackends();
+        }
+        if (curBackends != null) {
+            for (String curBackend: curBackends) {
+                String[] toks = curBackend.split(":", 2);
+                if (toks[0].equalsIgnoreCase(newBackendName) && toks[1].equalsIgnoreCase(newBackendConfig)) {
+                    String msg = String.format("Event log handler '%s' is already registered with config '%s'", newBackendName, newBackendConfig);
+                    throw ServiceException.FAILURE(msg,  null);
+                }
             }
         }
     }
