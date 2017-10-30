19ba818c2f326c2f4877d89123a170c06cf64bda
ilya <raykini@zimbra.com>
2017-10-30 13:05:36 -0700

    deleting event data on datasource deletion

--- store/src/java/com/zimbra/cs/event/logger/EventLogHandler.java
+++ store/src/java/com/zimbra/cs/event/logger/EventLogHandler.java
@@ -3,7 +3,10 @@ package com.zimbra.cs.event.logger;
 import com.zimbra.cs.event.Event;
 
 public interface EventLogHandler {
+
     void log(Event event);
+
+    boolean acceptsInternalEvents();
 
     void shutdown();
 
