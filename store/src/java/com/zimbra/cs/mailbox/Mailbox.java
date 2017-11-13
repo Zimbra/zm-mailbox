75825826deb55d5206740af7cd3d7f190d290e78
ilya <raykini@zimbra.com>
2017-11-13 13:32:17 -0800

    moved MessageCallbackContext to new class

--- store/src/java/com/zimbra/cs/mailbox/Mailbox.java
+++ store/src/java/com/zimbra/cs/mailbox/Mailbox.java
@@ -10734,45 +10734,6 @@ public class Mailbox implements MailboxStore {
         }
     }
 
-    public static class MessageCallbackContext {
-        private String dsId;
-        private String recipient;
-        private Long timestamp;
-        private MessageCallback.Type type;
-
-        public MessageCallbackContext(MessageCallback.Type type) {
-            this.type = type;
-        }
-
-        public MessageCallback.Type getCallbackType() {
-            return type;
-        }
-
-        public String getDataSourceId() {
-            return dsId;
-        }
-
-        public String getRecipient() {
-            return recipient;
-        }
-
-        public Long getTimestamp() {
-            return timestamp;
-        }
-
-        public void setDataSourceId(String dsId) {
-            this.dsId = dsId;
-        }
-
-        public void setRecipient(String recipient) {
-            this.recipient = recipient;
-        }
-
-        public void setTimestamp(Long timestamp) {
-            this.timestamp = timestamp;
-        }
-    }
-
     public interface MessageCallback {
 
         public static enum Type {
