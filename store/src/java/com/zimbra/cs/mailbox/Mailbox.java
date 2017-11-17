f2229c4abcad68d968d5f5d5d7ae98dcddd15716
Israel Arteaga <israel.arteaga.mx@gmail.com>
2017-11-17 16:10:36 -0600

    Refactor Distributed lock in a addMessage method

--- store/src/java/com/zimbra/cs/mailbox/Mailbox.java
+++ store/src/java/com/zimbra/cs/mailbox/Mailbox.java
@@ -6173,9 +6173,7 @@ public class Mailbox implements MailboxStore {
             localMsgMarkedRead = true;
         }
 
-        //rlock = new RedissonRedLock(distributedMailboxLock.getRedissonInstance().getLock("lock"));
-        //rlock.lock();
-        dLock.lock();
+        lock.lock();
         try {
             try {
                 Message message =  addMessageInternal(octxt, pm, folderId, noICal, flags, tags, conversationId,
@@ -6195,8 +6193,7 @@ public class Mailbox implements MailboxStore {
                 sm.quietDelete(staged);
             }
         } finally {
-            //rlock.unlock();
-        		dLock.release();
+            lock.release();
             ZimbraPerf.STOPWATCH_MBOX_ADD_MSG.stop(start);
         }
     }
