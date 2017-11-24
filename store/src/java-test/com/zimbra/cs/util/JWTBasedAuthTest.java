3db82e7d05aebd55b631ce59540bb45cf2111b54
Shrikant <shrikant.prasad@synacor.com>
2017-11-24 10:27:40 +0530

    ZCS-3258:Modify existing JWT validation implementation

--- store/src/java-test/com/zimbra/cs/util/JWTBasedAuthTest.java
+++ store/src/java-test/com/zimbra/cs/util/JWTBasedAuthTest.java
@@ -1,24 +1,22 @@
 package com.zimbra.cs.util;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.StringUtils;
 import org.easymock.EasyMock;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
+import com.google.common.primitives.Bytes;
 import com.zimbra.common.account.Key;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
 import com.zimbra.common.soap.HeaderConstants;
-import com.zimbra.common.util.Constants;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.AccountServiceException.AuthFailedServiceException;
 import com.zimbra.cs.account.AuthToken;
@@ -28,7 +26,7 @@ import com.zimbra.cs.account.AuthTokenException;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.mailbox.MailboxTestUtil;
 import com.zimbra.cs.service.AuthProvider;
-import com.zimbra.cs.service.account.Auth;
+import com.zimbra.cs.service.util.JWTUtil;
 import com.zimbra.soap.SoapServlet;
 
 import io.jsonwebtoken.Claims;
@@ -45,87 +43,110 @@ public class JWTBasedAuthTest {
     }
 
     @Test
-    public void testGenerateJWT() throws ServiceException, AuthTokenException {
-        Account acct = Provisioning.getInstance().get(Key.AccountBy.name, "test@zimbra.com");
-        AuthToken at = AuthProvider.getAuthToken(acct, TokenType.JWT);
-        validateJWT(at, acct.getId());
+    public void testGenerateJWTusingProvider() {
+        Account acct;
+        try {
+            acct = Provisioning.getInstance().get(Key.AccountBy.name, "test@zimbra.com");
+            AuthToken at = AuthProvider.getAuthToken(acct, TokenType.JWT);
+            validateJWT(at, acct.getId());
+        } catch (ServiceException e) {
+            Assert.fail("testGenerateJWTusingProvider failed");
+        }
+
     }
 
     @Test
-    public void testAccountAndUsageJWT() throws ServiceException, AuthTokenException {
-        Account acct = Provisioning.getInstance().get(Key.AccountBy.name, "test@zimbra.com");
-        AuthToken at = AuthProvider.getAuthToken(acct, Usage.TWO_FACTOR_AUTH, TokenType.JWT);
-        validateJWT(at, acct.getId());
+    public void testAccountAndUsageJWT() {
+        Account acct;
+        try {
+            acct = Provisioning.getInstance().get(Key.AccountBy.name, "test@zimbra.com");
+            AuthToken at = AuthProvider.getAuthToken(acct, Usage.TWO_FACTOR_AUTH, TokenType.JWT);
+            validateJWT(at, acct.getId());
+        } catch (ServiceException e) {
+            Assert.fail("testAccountAndUsageJWT failed");
+       }
     }
 
     @Test
-    public void testAccountAndExpiresJWT() throws ServiceException, AuthTokenException {
-        Account acct = Provisioning.getInstance().get(Key.AccountBy.name, "test@zimbra.com");
-        AuthToken at = AuthProvider.getAuthToken(acct, 0, TokenType.JWT);
-        validateJWT(at, acct.getId());
-    }
-
-    // positive case
-    @Ignore
-    @Test
-    public void testValdiateAndCreateNewJwtAuthToken() throws ServiceException, AuthTokenException {
-        Provisioning prov = Provisioning.getInstance();
-        Account acct = prov.get(Key.AccountBy.name, "test@zimbra.com");
-        AuthToken at = AuthProvider.getAuthToken(acct, 0, TokenType.JWT);
-        String token = at.getEncoded();
-        Claims claims = Auth.validateJWT(token);
-        Account authTokenAcct = prov.getAccountById(claims.getSubject());
-        AuthToken newAt = AuthProvider.getAuthToken(authTokenAcct, TokenType.JWT);
-        Assert.assertNotNull(newAt);
-        Assert.assertNotSame(at, newAt);
-        Assert.assertSame(acct, authTokenAcct);
-        validateJWT(newAt, authTokenAcct.getId());
+    public void testAccountAndExpiresJWT() {
+        Account acct;
+        try {
+            acct = Provisioning.getInstance().get(Key.AccountBy.name, "test@zimbra.com");
+            AuthToken at = AuthProvider.getAuthToken(acct, 0, TokenType.JWT);
+            validateJWT(at, acct.getId());
+        } catch (ServiceException e) {
+            Assert.fail("testAccountAndExpiresJWT failed");
+        }
     }
 
     // negative case
     @Test
-    public void testNegativeValdiateAndCreateNewJwtAuthToken() throws ServiceException, AuthTokenException {
+    public void testNegativeValdiate() {
         String token = "abc.dev.xyz";
         try {
-            Auth.validateJWT(token);
+            JWTUtil.validateJWT(token, "abcd");
         } catch(AuthFailedServiceException afse) {
-            Assert.assertTrue(afse.getReason().equals("Malformed JWT received"));
+            Assert.assertTrue(afse.getReason().equals("invalid jwt"));
+        } catch (ServiceException e) {
+            Assert.fail("testNegativeValdiate failed");
         }
     }
 
-    private void validateJWT(AuthToken at, String acctId) throws ServiceException, AuthTokenException {
-        String jwt = at.getEncoded();
-        String[] jwtClaims = jwt.split("\\.");
-        String jwtBody = StringUtils.newStringUtf8(Base64.decodeBase64(jwtClaims[1]));
-        Assert.assertTrue(jwtBody.contains(acctId));
+    private void validateJWT(AuthToken at, String acctId) {
+        String jwt;
+        try {
+            jwt = at.getEncoded();
+            String[] jwtClaims = jwt.split("\\.");
+            String jwtBody = StringUtils.newStringUtf8(Base64.decodeBase64(jwtClaims[1]));
+            Assert.assertTrue(jwtBody.contains(acctId));
+        } catch (AuthTokenException e) {
+            Assert.fail("validation failed");
+        }
 
     }
 
-    @Ignore
     @Test
-    public void testJWTSoapContextUsage() throws ServiceException, AuthTokenException {
-        Account acct = Provisioning.getInstance().get(Key.AccountBy.name, "test@zimbra.com");
-        AuthToken at = AuthProvider.getAuthToken(acct, 0, TokenType.JWT);
-        String jwt = at.getEncoded();
-        Element ctxt = EasyMock.createMock(Element.class);
-        EasyMock.expect(ctxt.getAttribute(HeaderConstants.E_JWT_TOKEN, null)).andReturn(jwt);
-        EasyMock.replay(ctxt);
-        AuthToken jwt_at = AuthProvider.getAuthToken(ctxt, null, TokenType.JWT);
-        assertEquals(acct.getId(), jwt_at.getAccountId());
+    public void testGenerateAndValidateJWT() {
+        Account acct;
+        try {
+            acct = Provisioning.getInstance().get(Key.AccountBy.name, "test@zimbra.com");
+            String salt = "s1";
+            String salts ="s2|s3|s1";
+            byte[] jwtKey = Bytes.concat(JWTUtil.getTokenKey(), salt.getBytes());
+            long issuedAt = System.currentTimeMillis();
+            long expires = issuedAt + 3600000;
+            String jwt = JWTUtil.generateJWT(jwtKey, salt, issuedAt, expires, acct);
+            Claims claims = JWTUtil.validateJWT(jwt, salts);
+            Assert.assertEquals(acct.getId(), claims.getSubject());
+        } catch (ServiceException e) {
+            e.printStackTrace();
+            Assert.fail("testGenerateAndValidateJWT failed");
+        }
     }
 
-    @Ignore
     @Test
-    public void testJWTHeaderUsage() throws ServiceException, AuthTokenException {
-        Account acct = Provisioning.getInstance().get(Key.AccountBy.name, "test@zimbra.com");
-        AuthToken at = AuthProvider.getAuthToken(acct, 0, TokenType.JWT);
-        String jwt = at.getEncoded();
+    public void testGetSaltSoapContext() {
+        Element soapCtxt = EasyMock.createMock(Element.class);
+        String salt = "s1";
+        EasyMock.expect(soapCtxt.getAttribute(HeaderConstants.E_JWT_SALT, null)).andReturn(salt);
+        EasyMock.replay(soapCtxt);
+        String saltFound = JWTUtil.getSalt(soapCtxt, null);
+        Assert.assertEquals(salt, saltFound);
+    }
+
+    @Test
+    public void testGetSaltCookie() {
         HttpServletRequest req = EasyMock.createMock(HttpServletRequest.class);
-        EasyMock.expect(req.getHeader(Constants.AUTH_HEADER)).andReturn("Bearer " + jwt);
+        Cookie cookies[] =  new Cookie[1];
+        String salt = "s1";
+        Cookie cookie = new Cookie("ZM_JWT", salt);
+        cookie.setHttpOnly(true);
+        cookies[0] = cookie;
+        EasyMock.expect(req.getCookies()).andReturn(cookies);
         EasyMock.replay(req);
         Map<String, Object> engineCtxt = new HashMap<String, Object>();
         engineCtxt.put(SoapServlet.SERVLET_REQUEST, req);
-        AuthToken jwt_at = AuthProvider.getAuthToken(null, engineCtxt, TokenType.JWT);
-        assertEquals(acct.getId(), jwt_at.getAccountId());
+        String saltFound = JWTUtil.getSalt(null, engineCtxt);
+        Assert.assertEquals(salt, saltFound);
     }
 }
