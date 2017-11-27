f68dc5419f2e5536e35eb618a08a4835ab92df79
varun.risbud <varun.risbud@synacor.com>
2017-11-27 14:10:22 -0500

    Added the unit test

--- store/src/java/com/zimbra/cs/event/analytics/contact/ContactAnalytics.java
+++ store/src/java/com/zimbra/cs/event/analytics/contact/ContactAnalytics.java
@@ -1,16 +1,9 @@
 package com.zimbra.cs.event.analytics.contact;
 
 import com.zimbra.common.service.ServiceException;
-import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.event.EventStore;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAdjusters;
-import java.time.temporal.WeekFields;
-import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 
 public class ContactAnalytics {
     public enum ContactFrequencyTimeRange {
@@ -35,5 +28,9 @@ public class ContactAnalytics {
 
     public static List<ContactFrequencyGraphDataPoint> getContactFrequencyGraph(String contact, ContactFrequencyGraphTimeRange timeRange, EventStore eventStore) throws ServiceException {
         return eventStore.getContactFrequencyGraph(contact, timeRange);
+    }
+
+    public static Double getPercentageOpenedEmails(String contact, EventStore eventStore) throws ServiceException {
+        return eventStore.getPercentageOpenedEmails(contact);
     }
 }
