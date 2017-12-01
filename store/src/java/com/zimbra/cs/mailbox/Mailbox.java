c26175f2f3c49bf3cc5f526920b01f3422002dfc
Eric Schoonover <eric@teamsodo.com>
2017-11-30 16:29:14 -0800

    replace removed call to lock.lock() in beginTransaction logic

--- store/src/java/com/zimbra/cs/mailbox/Mailbox.java
+++ store/src/java/com/zimbra/cs/mailbox/Mailbox.java
@@ -9906,6 +9906,7 @@ public class Mailbox implements MailboxStore {
             boolean write = this.lock.isWriteLock() || requiresWriteLock();
             assert recorder == null || write;
             assert !Thread.holdsLock(this) : "use MailboxLock";
+            this.lock.lock();
             if (!write && requiresWriteLock()) {
                 //another call must have purged the cache.
                 //the lock.lock() call should have resulted in write lock already
