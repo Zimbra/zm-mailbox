fe574b726daa1db49f143516603a31a472c503c6
varun.risbud <varun.risbud@synacor.com>
2017-12-01 14:37:44 -0500

    Refactored the code to remove solr specific data associated with the ContactAnalytics::TimeRange enum. Removed TimeRange enum and added seperate enums for contact frequency count and contact frequency graph.

--- store/src/java/com/zimbra/cs/event/analytics/contact/ContactAnalytics.java
+++ store/src/java/com/zimbra/cs/event/analytics/contact/ContactAnalytics.java
@@ -13,51 +13,12 @@ import java.util.List;
 import java.util.Locale;
 
 public class ContactAnalytics {
-    public enum TimeRange {
-        LAST_DAY("NOW-1DAY", "NOW", "+1HOUR", "HOUR"),
-        LAST_WEEK("NOW-7DAY", "NOW", "+1DAY", "DAY"),
-        LAST_MONTH("NOW-1MONTH", "NOW", "+1DAY", "DAY"),
-        CURRENT_MONTH("+1DAY", "DAY"),
-        LAST_SIX_MONTHS("+7DAY", "DAY"),
-        CURRENT_YEAR("+1MONTH", "MONTH"),
-        FOREVER("1970-01-01T00:00:00.001Z", "NOW", "+1YEAR", "YEAR");
+    public enum ContactFrequencyTimeRange {
+        LAST_DAY, LAST_WEEK, LAST_MONTH, FOREVER
+    }
 
-        private String solrStartDate = "";
-        private String solrEndDate = "";
-        private String solrAggregationUnit;
-        private String solrAggregationRoundingUnit;
-
-        TimeRange(String solrAggregationUnit, String solrAggregationRoundingUnit) {
-            this.solrAggregationUnit = solrAggregationUnit;
-            this.solrAggregationRoundingUnit = solrAggregationRoundingUnit;
-        }
-
-        TimeRange(String solrStartDate, String solrEndDate, String solrAggregationUnit, String solrAggregationRoundingUnit) {
-            this.solrStartDate = solrStartDate;
-            this.solrEndDate = solrEndDate;
-            this.solrAggregationUnit = solrAggregationUnit;
-            this.solrAggregationRoundingUnit = solrAggregationRoundingUnit;
-        }
-
-        public String getSolrStartDate() {
-            return solrStartDate;
-        }
-
-        public String getSolrEndDate() {
-            return solrEndDate;
-        }
-
-        public String getSolrAggregationUnit() {
-            return solrAggregationUnit;
-        }
-
-        public String getSolrAggregationRoundingUnit() {
-            return solrAggregationRoundingUnit;
-        }
-
-        public String getSolrAggregationBucket() {
-            return solrAggregationUnit + "/" + solrAggregationRoundingUnit;
-        }
+    public enum ContactFrequencyGraphTimeRange {
+        CURRENT_MONTH, LAST_SIX_MONTHS, CURRENT_YEAR
     }
 
     public enum ContactFrequencyEventType {
@@ -65,14 +26,14 @@ public class ContactAnalytics {
     }
 
     public static Long getContactFrequency(String contact, EventStore eventStore) throws ServiceException {
-        return eventStore.getContactFrequencyCount(contact, ContactFrequencyEventType.COMBINED, TimeRange.FOREVER);
+        return eventStore.getContactFrequencyCount(contact, ContactFrequencyEventType.COMBINED, ContactFrequencyTimeRange.FOREVER);
     }
 
-    public static Long getContactFrequency(String contact, EventStore eventStore, ContactFrequencyEventType eventType, TimeRange timeRange) throws ServiceException {
+    public static Long getContactFrequency(String contact, EventStore eventStore, ContactFrequencyEventType eventType, ContactFrequencyTimeRange timeRange) throws ServiceException {
         return eventStore.getContactFrequencyCount(contact, eventType, timeRange);
     }
 
-    public static List<ContactFrequencyGraphDataPoint> getContactFrequencyGraph(String contact, ContactAnalytics.TimeRange timeRange, EventStore eventStore) throws ServiceException {
+    public static List<ContactFrequencyGraphDataPoint> getContactFrequencyGraph(String contact, ContactFrequencyGraphTimeRange timeRange, EventStore eventStore) throws ServiceException {
         return eventStore.getContactFrequencyGraph(contact, timeRange);
     }
 }
