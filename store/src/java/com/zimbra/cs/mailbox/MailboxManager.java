5473f37d9c300e5c7f04d83016a3513736b0ea68
Eric Schoonover <eric@teamsodo.com>
2017-12-05 23:14:14 -0800

    fix MailboxManager

--- store/src/java/com/zimbra/cs/mailbox/MailboxManager.java
+++ store/src/java/com/zimbra/cs/mailbox/MailboxManager.java
@@ -861,7 +861,7 @@ public class MailboxManager {
         CreateMailbox redoRecorder = new CreateMailbox(account.getId());
 
         Mailbox mbox = null;
-        Mailbox.MailboxTransaction t = null;
+        Mailbox.MailboxTransaction mboxTransaction = null;
         DbConnection conn = DbPool.getConnection();
         try {
             CreateMailbox redoPlayer = (octxt == null ? null : (CreateMailbox) octxt.getPlayer());
@@ -892,7 +892,7 @@ public class MailboxManager {
             mbox.setGalSyncMailbox(isGalSyncAccount);
             // the existing Connection is used for the rest of this transaction...
             try (final MailboxLock l = mbox.lock(true)) {
-                t = mbox.new MailboxTransaction("createMailbox", octxt, l, redoRecorder, conn);
+                mboxTransaction = mbox.new MailboxTransaction("createMailbox", octxt, l, redoRecorder, conn);
 
                 if (created) {
                     // create the default folders
@@ -904,7 +904,7 @@ public class MailboxManager {
                 cacheMailbox(mbox);
                 redoRecorder.setMailboxId(mbox.getId());
 
-                t.commit();
+                mboxTransaction.commit();
             }
         } catch (ServiceException e) {
             // Log exception here, just in case.  If badness happens during rollback
@@ -919,7 +919,7 @@ public class MailboxManager {
         } finally {
             try {
                 if (t != null) {
-                    t.close();
+                    mboxTransaction.close();
                 } else {
                     conn.rollback();
                 }
