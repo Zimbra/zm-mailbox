3a3e48b4615a129f5e665ff018f80e819ac87b42
Yasuko Komiyama <yasuko.komiyama@synacor.com>
2017-12-12 17:57:21 +0900

    ZCS-3456(3):Attendee's status isn't updated
    Remove unused import.

--- store/src/java-test/com/zimbra/cs/mailbox/calendar/ZAttendeeTest.java
+++ store/src/java-test/com/zimbra/cs/mailbox/calendar/ZAttendeeTest.java
@@ -21,7 +21,6 @@ import java.io.InputStream;
 import java.util.Iterator;
 import java.util.List;
 
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
