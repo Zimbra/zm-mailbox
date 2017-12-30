e654e830ea5861a00d51127397dd92c922640136
ilya <raykini@zimbra.com>
2017-12-29 16:09:18 -0800

    GetSmartFolders SOAP API

---
+++ soap/src/java/com/zimbra/soap/mail/message/GetSmartFoldersRequest.java
@@ -0,0 +1,8 @@
+package com.zimbra.soap.mail.message;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.zimbra.common.soap.MailConstants;
+
+@XmlRootElement(name=MailConstants.E_GET_SMART_FOLDERS_REQUEST)
+public class GetSmartFoldersRequest {}
