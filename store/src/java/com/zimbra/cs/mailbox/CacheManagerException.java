39686219f7fd1f7ad4d3f4a6f0659c8bc5746eca
Israel Arteaga <israel.arteaga.mx@gmail.com>
2018-01-04 17:33:58 -0600

    disable MailboxManager cache when running HA mailbox

---
+++ store/src/java/com/zimbra/cs/mailbox/CacheManagerException.java
@@ -0,0 +1,7 @@
+package com.zimbra.cs.mailbox;
+
+public class CacheManagerException extends RuntimeException {
+    public CacheManagerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
