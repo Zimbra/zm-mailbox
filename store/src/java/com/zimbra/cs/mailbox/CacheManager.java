b6136702e2de8a1b3ef9aafa09615c8b9f95f236
Israel Arteaga <israel.arteaga.mx@gmail.com>
2018-01-04 17:33:58 -0600

    disable MailboxManager cache when running HA mailbox

---
+++ store/src/java/com/zimbra/cs/mailbox/CacheManager.java
@@ -0,0 +1,41 @@
+package com.zimbra.cs.mailbox;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public interface CacheManager {
+
+    Collection<Integer> getMailboxIds();
+
+    Set<String> getAccountIds();
+
+    Set<Map.Entry<String, Integer>> getMailboxIdsByAccountId();
+
+    int getMailboxCount();
+
+    Integer getMailboxKey(String accountId);
+
+    void removeMailboxId(String accountId);
+
+    Object retrieveFromCache(int mailboxId, boolean trackGC, MailboxManager mailboxManager) throws MailServiceException;
+
+    void cacheMailbox(int mailboxId, MailboxMaintenance maint);
+
+    Object getMailbox(int mailboxId);
+
+    Mailbox cacheMailbox(Mailbox mailbox, MailboxManager mailboxManager);
+
+    void removeMailbox(int mailboxId);
+
+    void cacheAccount(String accountId, int mailboxId);
+
+    int getMailboxCacheSize();
+
+    Collection<Object> getAllLoadedMailboxes();
+
+    Set<Map.Entry<Integer, Object>> getMailboxesById();
+
+    void clearCache();
+
+}
