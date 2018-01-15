cb9dd5b2565866b6c7131cdabdf4ca6eb835138d
ilya <raykini@zimbra.com>
2018-01-15 11:00:18 -0800

    Codacy suggestions

--- store/src/java/com/zimbra/cs/ml/query/MessageClassificationQuery.java
+++ store/src/java/com/zimbra/cs/ml/query/MessageClassificationQuery.java
@@ -18,7 +18,7 @@ public class MessageClassificationQuery extends AbstractClassificationQuery<Mess
         return classifierId;
     }
 
-    String getReceiverAddress() {
+    public String getReceiverAddress() {
         return receiverAddr;
     }
 
