872c175e5242e293f6498fe05250e7f0acae138f
Eric Schoonover <eric@teamsodo.com>
2018-01-15 22:49:12 -0800

    rename CacheManager -> MailboxCacheManager

--- store/src/java/com/zimbra/cs/mailbox/MailboxCacheManager.java
+++ store/src/java/com/zimbra/cs/mailbox/MailboxCacheManager.java
@@ -4,7 +4,7 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
-public interface CacheManager {
+public interface MailboxCacheManager {
 
     Collection<Integer> getMailboxIds();
 
