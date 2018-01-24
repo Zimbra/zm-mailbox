fd2aa03e8458841dc87b82850c3d49889e58a540
Gordon Tillman <gordyt@gmail.com>
2018-01-24 09:27:00 -0600

    ZCS-4004 Update to initial fix
    Replaced call to `this.getMailboxOrNull`, as we need to have
    a MailboxStore.  Original implementation was breaking the
    `TestImapViaEmbeddedRemote` SOAP tests.

--- store/src/java/com/zimbra/cs/imap/ImapListener.java
+++ store/src/java/com/zimbra/cs/imap/ImapListener.java
@@ -698,7 +698,7 @@ public abstract class ImapListener extends Session {
 
     protected ImapFolder reload() throws ImapSessionClosedException {
         // ZESC-460, ZCS-4004: Ensure mailbox was not modified by another thread
-        Mailbox mbox = this.getMailboxOrNull();
+        MailboxStore mbox = mailbox;
         if (mbox == null) {
             throw new ImapSessionClosedException();
         }
@@ -808,7 +808,7 @@ public abstract class ImapListener extends Session {
     public void updateAccessTime() {
         super.updateAccessTime();
         // ZESC-460, ZCS-4004: Ensure mailbox was not modified by another thread
-        Mailbox mbox = this.getMailboxOrNull();
+        MailboxStore mbox = mailbox;
         if (mbox == null) {
             return;
         }
