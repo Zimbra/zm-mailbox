aec76452375b41474f7d24d7a26bbfda79581c82
Gren Elliot <gren.elliot@synacor.com>
2018-01-25 10:42:27 +0000

    ZCS-3810:TestImapVia* delete @Ignores
    TestImapViaEmbeddedRemote - stop ignoring tests
    TestImapViaImapDaemon - stop ignoring tests

--- store/src/java/com/zimbra/qa/unittest/TestImapViaEmbeddedRemote.java
+++ store/src/java/com/zimbra/qa/unittest/TestImapViaEmbeddedRemote.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import org.dom4j.DocumentException;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
 
 import com.zimbra.common.localconfig.ConfigException;
 import com.zimbra.common.localconfig.LC;
@@ -37,26 +35,5 @@ public class TestImapViaEmbeddedRemote extends SharedImapTests {
     @Override
     protected int getImapPort() {
         return imapServer.getImapBindPort();
-    }
-
-    @Override
-    @SuppressWarnings("PMD")  // checking done in called methods
-    @Ignore("ZCS-3810 - range searches are broken on remote IMAP")
-    @Test
-    /**
-     * this is ignored until ZCS-3810 is fixed
-     */
-    public void testUidRangeSearch() throws Exception {
-
-    }
-    @Override
-    @SuppressWarnings("PMD")  // checking done in called methods
-    @Ignore("ZCS-3810 - virtual folders (search folders) return only up to 1000 items")
-    @Test
-    /**
-     * this is ignored until ZCS-3810 is fixed
-     */
-    public void testUidRangeSearchOnVirtualFolder() throws Exception {
-
     }
 }
