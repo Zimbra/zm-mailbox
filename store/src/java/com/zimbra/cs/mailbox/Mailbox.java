e8e92ee9b3aba3bc611aff044b92aa8911b68614
Eric Schoonover <eric@teamsodo.com>
2018-01-31 11:50:52 -0800

    fixing bad rebase change, add back success=false

--- store/src/java/com/zimbra/cs/mailbox/Mailbox.java
+++ store/src/java/com/zimbra/cs/mailbox/Mailbox.java
@@ -5927,6 +5927,7 @@ public class Mailbox implements MailboxStore {
 
         Threader threader = pm.getThreader(this);
         String subject = pm.getNormalizedSubject();
+        boolean success = false;
 
         try (final MailboxTransaction t = new MailboxTransaction("addMessage", octxt, l, redoRecorder)) {
             if (isRedo) {
