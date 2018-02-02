9a96d8cf76c0901c9f7eeab5d380e1545943e9c1
Koichi Kato <koichi.kato@synacor.com>
2018-02-02 14:51:22 +0900

    NICPS-30: added zimbraPrefDisplayTimeInMailList to zimbra-attrs.xml

--- common/src/java/com/zimbra/common/account/ZAttrProvisioning.java
+++ common/src/java/com/zimbra/common/account/ZAttrProvisioning.java
@@ -12792,6 +12792,14 @@ public class ZAttrProvisioning {
     public static final String A_zimbraPrefDisplayExternalImages = "zimbraPrefDisplayExternalImages";
 
     /**
+     * Display received/sent time in mail list
+     *
+     * @since ZCS 8.8.7
+     */
+    @ZAttr(id=3021)
+    public static final String A_zimbraPrefDisplayTimeInMailList = "zimbraPrefDisplayTimeInMailList";
+
+    /**
      * Specifies the meaning of an external sender. &quot;ALL&quot; means
      * users whose domain doesn&#039;t match the recipient&#039;s or
      * zimbraInternalSendersDomain. &quot;ALLNOTINAB&quot; means
