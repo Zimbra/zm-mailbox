eb45f144aadc465ac893ccc488a9fec76e3f9c0c
Eric Schoonover <eric@teamsodo.com>
2018-02-05 18:17:52 -0800

    json serialize/deserialize tests

---
+++ soap/src/java-test/com/zimbra/soap/mail/type/ImapMessageInfoTest.java
@@ -0,0 +1,31 @@
+package com.zimbra.soap.mail.type;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import junit.framework.Assert;
+
+public class ImapMessageInfoTest {
+
+	private ObjectMapper mapper = new ObjectMapper();
+
+	@Test
+	public void serializesToJsonTest() throws Exception {
+		ImapMessageInfo imapMessageInfo = new ImapMessageInfo(1, 2, "type", 1, "tags");
+		String json = mapper.writeValueAsString(imapMessageInfo);
+		System.out.println(json);
+		String expected = "{\"id\":1,\"imapUid\":2,\"type\":\"type\",\"flags\":1,\"tags\":\"tags\"}";
+		Assert.assertEquals(expected, json);
+	}
+
+	@Test
+	public void deserializesFromJsonTest() throws Exception {
+		ImapMessageInfo imapMessageInfo = null;
+		String expected = "{\"id\":1,\"imapUid\":2,\"type\":\"type\",\"flags\":1,\"tags\":\"tags\"}";
+		imapMessageInfo = mapper.readValue(expected, ImapMessageInfo.class);
+		System.out.println(imapMessageInfo);
+		Assert.assertNotNull(imapMessageInfo);
+	}
+
+}
