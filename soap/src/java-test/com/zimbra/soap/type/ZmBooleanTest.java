716c0e36fc527029461b60c01c1fcc55f2033cf3
Eric Schoonover <eric@teamsodo.com>
2018-02-05 18:17:52 -0800

    json serialize/deserialize tests

---
+++ soap/src/java-test/com/zimbra/soap/type/ZmBooleanTest.java
@@ -0,0 +1,26 @@
+package com.zimbra.soap.type;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import junit.framework.Assert;
+
+public class ZmBooleanTest {
+
+	private ObjectMapper mapper = new ObjectMapper();
+
+	@Test
+	public void serializesToJsonTest() throws Exception {
+		String json = mapper.writeValueAsString(ZmBoolean.ONE);
+		System.out.println(json);
+		Assert.assertEquals("true", json.toLowerCase());
+	}
+
+	@Test
+	public void deserializesFromJsonTest() throws Exception {
+		ZmBoolean zmBoolean = mapper.readValue(String.valueOf(true), ZmBoolean.class);
+		System.out.println(zmBoolean);
+		Assert.assertEquals(zmBoolean == ZmBoolean.ONE || zmBoolean == ZmBoolean.TRUE, true);
+	}
+}
