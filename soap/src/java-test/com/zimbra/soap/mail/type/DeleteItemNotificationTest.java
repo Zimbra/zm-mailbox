fb33972f458dcb5e164b916757475242a1921f0b
Eric Schoonover <eric@teamsodo.com>
2018-02-05 18:17:52 -0800

    json serialize/deserialize tests

---
+++ soap/src/java-test/com/zimbra/soap/mail/type/DeleteItemNotificationTest.java
@@ -0,0 +1,31 @@
+package com.zimbra.soap.mail.type;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import junit.framework.Assert;
+
+public class DeleteItemNotificationTest {
+
+	private ObjectMapper mapper = new ObjectMapper();
+
+	@Test
+	public void serializesToJsonTest() throws Exception {
+		DeleteItemNotification deleteItemNotification = new DeleteItemNotification(1," Atype");
+		String json = mapper.writeValueAsString(deleteItemNotification);
+		System.out.println(json);
+		String expected = "{\"id\":1,\"type\":\" Atype\"}";
+		Assert.assertEquals(expected, json);
+	}
+
+	@Test
+	public void deserializesFromJsonTest() throws Exception {
+		DeleteItemNotification deleteItemNotification = null;
+		String expected = "{\"id\":1,\"type\":\" Atype\"}";
+		deleteItemNotification = mapper.readValue(expected, DeleteItemNotification.class);
+		System.out.println(deleteItemNotification);
+		Assert.assertNotNull(deleteItemNotification);
+	}
+	
+}
