617ec1aa34312429ee0f10fe8c2e3a71c8e1f4f3
Sneha Patil <sneha.patil@synacor.com>
2018-02-09 14:36:34 +0530

    ZCS-3954:Modify SearchAction to support updating read/unread flag

--- soap/src/java/com/zimbra/soap/mail/type/BulkAction.java
+++ soap/src/java/com/zimbra/soap/mail/type/BulkAction.java
@@ -29,7 +29,7 @@ public class BulkAction {
 
     @XmlEnum
     public static enum Operation {
-        move;
+        move, read, unread;
 
         public static Operation fromString(String s) throws ServiceException {
             try {
@@ -45,6 +45,8 @@ public class BulkAction {
      * @zm-api-field-description Operation to perform
      * <table>
      * <tr><td><b>move</b>           </td><td>move the search result to specified folder location</td></tr>
+     * <tr><td><b>read</b>           </td><td>mark the search result as read</td></tr>
+     * <tr><td><b>unread</b>         </td><td>mark the search result as unread</td></tr>
      * </table>
      */
     @XmlAttribute(name = MailConstants.A_OPERATION /* op */, required = true)
@@ -52,7 +54,8 @@ public class BulkAction {
 
     /**
      * @zm-api-field-tag folder-path
-     * @zm-api-field-description Folder pathname
+     * @zm-api-field-description Required if op="move". Folder pathname where
+     *                           all matching items should be moved.
      */
     @XmlAttribute(name = MailConstants.A_FOLDER /* l */, required = false)
     private String folder;
