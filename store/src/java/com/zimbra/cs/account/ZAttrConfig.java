4f483e55202f93637db7abc30a1111e69bcc80ab
Maki Watanabe <maki.watanabe@synacor.com>
2018-02-19 20:19:14 +0900

    NICPS-59/Bug 34202 - STARTTLS support for outbound mail sent by mailbox server Add a new ldap attribute zimbraSmtpStartTlsMode (on,off,only) to control mailbox server behavior on sending out mails.

--- store/src/java/com/zimbra/cs/account/ZAttrConfig.java
+++ store/src/java/com/zimbra/cs/account/ZAttrConfig.java
@@ -68849,9 +68849,9 @@ public abstract class ZAttrConfig extends Entry {
     }
 
     /**
-     * Configures the starttls mode on sending messages from mailboxd to MTA
-     * on - uses starttls if the peer MTA supports off - doesn&#039;t use
-     * starttls only - requires starttls to send messages
+     * Configures the starttls mode on sending messages from mailboxd to MTA.
+     * ( on - uses starttls if the peer MTA supports. off - doesn&#039;t use
+     * starttls. only - requires starttls to send messages. )
      *
      * <p>Valid values: [on, off, only]
      *
@@ -68992,108 +68992,6 @@ public abstract class ZAttrConfig extends Entry {
     public Map<String,Object> unsetSmtpStartTlsMode(Map<String,Object> attrs) {
         if (attrs == null) attrs = new HashMap<String,Object>();
         attrs.put(Provisioning.A_zimbraSmtpStartTlsMode, "");
-        return attrs;
-    }
-
-    /**
-     * Configures trusted hosts for sending mail from mailboxd to MTA with
-     * starttls. If set to &quot;*&quot;, all hosts are trusted. If set to a
-     * whitespace separated list of hosts, those hosts are trusted.
-     * Otherwise, trust depends on the certificate the server presents. This
-     * attribute does not affect data source SMTP connections as those are
-     * controlled by zimbraDataSourceSmtpHost,
-     * zimbraDataSourceSmtpConnectionType, etc.
-     *
-     * @return zimbraSmtpStartTlsTrustedHosts, or "*" if unset
-     *
-     * @since ZCS 8.8.6
-     */
-    @ZAttr(id=5004)
-    public String getSmtpStartTlsTrustedHosts() {
-        return getAttr(Provisioning.A_zimbraSmtpStartTlsTrustedHosts, "*", true);
-    }
-
-    /**
-     * Configures trusted hosts for sending mail from mailboxd to MTA with
-     * starttls. If set to &quot;*&quot;, all hosts are trusted. If set to a
-     * whitespace separated list of hosts, those hosts are trusted.
-     * Otherwise, trust depends on the certificate the server presents. This
-     * attribute does not affect data source SMTP connections as those are
-     * controlled by zimbraDataSourceSmtpHost,
-     * zimbraDataSourceSmtpConnectionType, etc.
-     *
-     * @param zimbraSmtpStartTlsTrustedHosts new value
-     * @throws com.zimbra.common.service.ServiceException if error during update
-     *
-     * @since ZCS 8.8.6
-     */
-    @ZAttr(id=5004)
-    public void setSmtpStartTlsTrustedHosts(String zimbraSmtpStartTlsTrustedHosts) throws com.zimbra.common.service.ServiceException {
-        HashMap<String,Object> attrs = new HashMap<String,Object>();
-        attrs.put(Provisioning.A_zimbraSmtpStartTlsTrustedHosts, zimbraSmtpStartTlsTrustedHosts);
-        getProvisioning().modifyAttrs(this, attrs);
-    }
-
-    /**
-     * Configures trusted hosts for sending mail from mailboxd to MTA with
-     * starttls. If set to &quot;*&quot;, all hosts are trusted. If set to a
-     * whitespace separated list of hosts, those hosts are trusted.
-     * Otherwise, trust depends on the certificate the server presents. This
-     * attribute does not affect data source SMTP connections as those are
-     * controlled by zimbraDataSourceSmtpHost,
-     * zimbraDataSourceSmtpConnectionType, etc.
-     *
-     * @param zimbraSmtpStartTlsTrustedHosts new value
-     * @param attrs existing map to populate, or null to create a new map
-     * @return populated map to pass into Provisioning.modifyAttrs
-     *
-     * @since ZCS 8.8.6
-     */
-    @ZAttr(id=5004)
-    public Map<String,Object> setSmtpStartTlsTrustedHosts(String zimbraSmtpStartTlsTrustedHosts, Map<String,Object> attrs) {
-        if (attrs == null) attrs = new HashMap<String,Object>();
-        attrs.put(Provisioning.A_zimbraSmtpStartTlsTrustedHosts, zimbraSmtpStartTlsTrustedHosts);
-        return attrs;
-    }
-
-    /**
-     * Configures trusted hosts for sending mail from mailboxd to MTA with
-     * starttls. If set to &quot;*&quot;, all hosts are trusted. If set to a
-     * whitespace separated list of hosts, those hosts are trusted.
-     * Otherwise, trust depends on the certificate the server presents. This
-     * attribute does not affect data source SMTP connections as those are
-     * controlled by zimbraDataSourceSmtpHost,
-     * zimbraDataSourceSmtpConnectionType, etc.
-     *
-     * @throws com.zimbra.common.service.ServiceException if error during update
-     *
-     * @since ZCS 8.8.6
-     */
-    @ZAttr(id=5004)
-    public void unsetSmtpStartTlsTrustedHosts() throws com.zimbra.common.service.ServiceException {
-        HashMap<String,Object> attrs = new HashMap<String,Object>();
-        attrs.put(Provisioning.A_zimbraSmtpStartTlsTrustedHosts, "");
-        getProvisioning().modifyAttrs(this, attrs);
-    }
-
-    /**
-     * Configures trusted hosts for sending mail from mailboxd to MTA with
-     * starttls. If set to &quot;*&quot;, all hosts are trusted. If set to a
-     * whitespace separated list of hosts, those hosts are trusted.
-     * Otherwise, trust depends on the certificate the server presents. This
-     * attribute does not affect data source SMTP connections as those are
-     * controlled by zimbraDataSourceSmtpHost,
-     * zimbraDataSourceSmtpConnectionType, etc.
-     *
-     * @param attrs existing map to populate, or null to create a new map
-     * @return populated map to pass into Provisioning.modifyAttrs
-     *
-     * @since ZCS 8.8.6
-     */
-    @ZAttr(id=5004)
-    public Map<String,Object> unsetSmtpStartTlsTrustedHosts(Map<String,Object> attrs) {
-        if (attrs == null) attrs = new HashMap<String,Object>();
-        attrs.put(Provisioning.A_zimbraSmtpStartTlsTrustedHosts, "");
         return attrs;
     }
 
