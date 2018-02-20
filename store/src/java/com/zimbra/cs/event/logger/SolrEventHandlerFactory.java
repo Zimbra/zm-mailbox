7a37d5da67d6ff32d50b36ef666d436c22ecd9e1
ilya <raykini@zimbra.com>
2018-02-20 08:53:43 -0800

    updated solr event logger to support sharded indexes

--- store/src/java/com/zimbra/cs/event/logger/SolrEventHandlerFactory.java
+++ store/src/java/com/zimbra/cs/event/logger/SolrEventHandlerFactory.java
@@ -1,35 +1,23 @@
 package com.zimbra.cs.event.logger;
 
 import com.zimbra.common.service.ServiceException;
-import com.zimbra.common.util.ZimbraLog;
-import com.zimbra.cs.account.Provisioning;
-import com.zimbra.cs.account.Server;
 import com.zimbra.cs.event.logger.BatchingEventLogger.BatchingHandlerFactory;
-import com.zimbra.cs.index.solr.AccountCollectionLocator;
 import com.zimbra.cs.index.solr.JointCollectionLocator;
+import com.zimbra.cs.index.solr.JointCollectionLocator.IndexNameFunc;
 import com.zimbra.cs.index.solr.SolrCollectionLocator;
-import com.zimbra.cs.index.solr.SolrConstants;
+import com.zimbra.cs.index.solr.SolrUtils;
 
 public abstract class SolrEventHandlerFactory extends BatchingHandlerFactory {
 
-    protected SolrCollectionLocator getCoreLocator() {
-        SolrCollectionLocator locator;
-        try {
-            Server server = Provisioning.getInstance().getLocalServer();
-            switch(server.getEventSolrIndexType()) {
-                case account:
-                    locator = new AccountCollectionLocator(SolrConstants.EVENT_CORE_NAME_OR_PREFIX);
-                    break;
-                case combined:
-                default:
-                    locator = new JointCollectionLocator(SolrConstants.EVENT_CORE_NAME_OR_PREFIX);
-                    break;
+    protected SolrCollectionLocator getCoreLocator() throws ServiceException {
 
+        IndexNameFunc indexNameFunc = new IndexNameFunc() {
+            @Override
+            public String getIndexName(String accountId) throws ServiceException {
+                return SolrUtils.getEventIndexName(accountId);
             }
-        } catch (ServiceException e) {
-            ZimbraLog.event.error("unable to determine event index type; defaulting to JointCollectionLocator", e);
-            locator = new JointCollectionLocator(SolrConstants.EVENT_CORE_NAME_OR_PREFIX);
-        }
-        return locator;
+        };
+
+        return new JointCollectionLocator(indexNameFunc);
     }
 }
