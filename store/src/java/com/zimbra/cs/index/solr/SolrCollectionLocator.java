3b2b015c610d5a244420f3b11e3e5f5ede15fa3d
ilya <raykini@zimbra.com>
2018-02-20 08:04:33 -0800

    compositeId routing in SolrCloudHelper and SolrCollectionLocator classes

--- store/src/java/com/zimbra/cs/index/solr/SolrCollectionLocator.java
+++ store/src/java/com/zimbra/cs/index/solr/SolrCollectionLocator.java
@@ -1,16 +1,22 @@
 package com.zimbra.cs.index.solr;
 
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.common.SolrInputDocument;
+
+import com.google.common.base.Joiner;
+import com.zimbra.common.service.ServiceException;
 
 /**
  * Helper class that routes requests to the appropriate Solr index
  */
 public abstract class SolrCollectionLocator {
 
+    protected static final Joiner idJoiner = Joiner.on("_");
+
     /**
      * Return the name of the Solr collection used to store documents for the given account ID
      */
-    abstract String getCoreName(String accountId);
+    abstract String getIndexName(String accountId) throws ServiceException;
 
     /**
      * Optionally post-process the Solr document appropriately
@@ -21,4 +27,11 @@ public abstract class SolrCollectionLocator {
      * Whether this collection needs an account filter
      */
     abstract boolean needsAccountFilter();
+
+    /**
+     * Optionally post-process a query to make it appropriate for the Solr topology
+     */
+    abstract void finalizeQuery(SolrQuery query, String accountId);
+
+    abstract String getSolrId(String accountId, Object... idParts);
 }
