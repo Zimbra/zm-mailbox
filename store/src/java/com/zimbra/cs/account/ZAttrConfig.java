dfe6e5521fb02fb838daafdf5ff6518a66998a96
Maki Watanabe <maki.watanabe@synacor.com>
2018-03-02 13:20:22 +0900

    NICPS-59/Bug 34202 Update attribute description

--- store/src/java/com/zimbra/cs/account/ZAttrConfig.java
+++ store/src/java/com/zimbra/cs/account/ZAttrConfig.java
@@ -69295,9 +69295,11 @@ public abstract class ZAttrConfig extends Entry {
     /**
      * Configures trusted hosts sending mail from mailboxd to MTA with
      * starttls. If set to &quot;*&quot;, all hosts are trusted. If set to a
-     * whitespace separated list of hosts, those hosts are trusted. This
-     * parameter doesn&#039;t affect on smtp sessions to
-     * zimbraDataSourceSmtpHost.
+     * whitespace separated list of hosts, those hosts are trusted.
+     * Otherwise, trust depends on the certificate the server presents. This
+     * attribute does not affect data source SMTP connections as those are
+     * controlled by zimbraDataSourceSmtpHost,
+     * zimbraDataSourceSmtpConnectionType, etc.
      *
      * @return zimbraSmtpStartTlsTrustedHosts, or "*" if unset
      *
@@ -69311,9 +69313,11 @@ public abstract class ZAttrConfig extends Entry {
     /**
      * Configures trusted hosts sending mail from mailboxd to MTA with
      * starttls. If set to &quot;*&quot;, all hosts are trusted. If set to a
-     * whitespace separated list of hosts, those hosts are trusted. This
-     * parameter doesn&#039;t affect on smtp sessions to
-     * zimbraDataSourceSmtpHost.
+     * whitespace separated list of hosts, those hosts are trusted.
+     * Otherwise, trust depends on the certificate the server presents. This
+     * attribute does not affect data source SMTP connections as those are
+     * controlled by zimbraDataSourceSmtpHost,
+     * zimbraDataSourceSmtpConnectionType, etc.
      *
      * @param zimbraSmtpStartTlsTrustedHosts new value
      * @throws com.zimbra.common.service.ServiceException if error during update
@@ -69330,9 +69334,11 @@ public abstract class ZAttrConfig extends Entry {
     /**
      * Configures trusted hosts sending mail from mailboxd to MTA with
      * starttls. If set to &quot;*&quot;, all hosts are trusted. If set to a
-     * whitespace separated list of hosts, those hosts are trusted. This
-     * parameter doesn&#039;t affect on smtp sessions to
-     * zimbraDataSourceSmtpHost.
+     * whitespace separated list of hosts, those hosts are trusted.
+     * Otherwise, trust depends on the certificate the server presents. This
+     * attribute does not affect data source SMTP connections as those are
+     * controlled by zimbraDataSourceSmtpHost,
+     * zimbraDataSourceSmtpConnectionType, etc.
      *
      * @param zimbraSmtpStartTlsTrustedHosts new value
      * @param attrs existing map to populate, or null to create a new map
@@ -69350,9 +69356,11 @@ public abstract class ZAttrConfig extends Entry {
     /**
      * Configures trusted hosts sending mail from mailboxd to MTA with
      * starttls. If set to &quot;*&quot;, all hosts are trusted. If set to a
-     * whitespace separated list of hosts, those hosts are trusted. This
-     * parameter doesn&#039;t affect on smtp sessions to
-     * zimbraDataSourceSmtpHost.
+     * whitespace separated list of hosts, those hosts are trusted.
+     * Otherwise, trust depends on the certificate the server presents. This
+     * attribute does not affect data source SMTP connections as those are
+     * controlled by zimbraDataSourceSmtpHost,
+     * zimbraDataSourceSmtpConnectionType, etc.
      *
      * @throws com.zimbra.common.service.ServiceException if error during update
      *
@@ -69368,9 +69376,11 @@ public abstract class ZAttrConfig extends Entry {
     /**
      * Configures trusted hosts sending mail from mailboxd to MTA with
      * starttls. If set to &quot;*&quot;, all hosts are trusted. If set to a
-     * whitespace separated list of hosts, those hosts are trusted. This
-     * parameter doesn&#039;t affect on smtp sessions to
-     * zimbraDataSourceSmtpHost.
+     * whitespace separated list of hosts, those hosts are trusted.
+     * Otherwise, trust depends on the certificate the server presents. This
+     * attribute does not affect data source SMTP connections as those are
+     * controlled by zimbraDataSourceSmtpHost,
+     * zimbraDataSourceSmtpConnectionType, etc.
      *
      * @param attrs existing map to populate, or null to create a new map
      * @return populated map to pass into Provisioning.modifyAttrs
