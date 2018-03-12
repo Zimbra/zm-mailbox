16603aee8f814dfa6237c3adbea06db48d10562e
nilesh pawar <nilesh.pawar@synacor.com>
2018-03-12 06:13:47 +0000

    NICPS-41: Fix string initialization

--- store/src/java/com/zimbra/cs/servlet/RecaptchaServlet.java
+++ store/src/java/com/zimbra/cs/servlet/RecaptchaServlet.java
@@ -16,22 +16,21 @@ import javax.servlet.http.*;
 
 public class RecaptchaServlet extends HttpServlet {
 
-  protected void processRequest(HttpServletRequest request, 
-                                HttpServletResponse response) 
+  protected void processRequest(HttpServletRequest request,
+                                HttpServletResponse response)
                  throws ServletException, IOException {
 
     int width = 150;
     int height = 50;
-	
 
     char data[][] = {
-        { 'j', '2', 'e', 'x', 'z', 'P' }		
+        { 'j', '2', 'e', 'x', 'z', 'P' }
     };
 
 	String tmp = generateSessionKey(6);
 	data[0] = tmp.toCharArray();
 
-    BufferedImage bufferedImage = new BufferedImage(width, height, 
+    BufferedImage bufferedImage = new BufferedImage(width, height,
                   BufferedImage.TYPE_INT_RGB);
 
     Graphics2D g2d = bufferedImage.createGraphics();
@@ -43,12 +42,12 @@ public class RecaptchaServlet extends HttpServlet {
            RenderingHints.KEY_ANTIALIASING,
            RenderingHints.VALUE_ANTIALIAS_ON);
 
-    rh.put(RenderingHints.KEY_RENDERING, 
+    rh.put(RenderingHints.KEY_RENDERING,
            RenderingHints.VALUE_RENDER_QUALITY);
 
     g2d.setRenderingHints(rh);
 
-    GradientPaint gp = new GradientPaint(0, 0, 
+    GradientPaint gp = new GradientPaint(0, 0,
     Color.gray, 0, height/2, Color.white, true);
 
     g2d.setPaint(gp);
@@ -62,7 +61,7 @@ public class RecaptchaServlet extends HttpServlet {
     String captcha = String.copyValueOf(data[index]);
     request.getSession().setAttribute("captcha", captcha );
 
-    int x = 0; 
+    int x = 0;
     int y = 0;
 
     for (int i=0; i<data[index].length; i++) {
@@ -77,31 +76,31 @@ public class RecaptchaServlet extends HttpServlet {
     OutputStream os = response.getOutputStream();
     ImageIO.write(bufferedImage, "png", os);
     os.close();
-  } 
+  }
 
 
-  protected void doGet(HttpServletRequest request, 
+  protected void doGet(HttpServletRequest request,
                        HttpServletResponse response)
                            throws ServletException, IOException {
       processRequest(request, response);
-  } 
+  }
 
 
-  protected void doPost(HttpServletRequest request, 
+  protected void doPost(HttpServletRequest request,
                         HttpServletResponse response)
                             throws ServletException, IOException {
       processRequest(request, response);
   }
-  
+
   public String generateSessionKey(int length){
-		String alphabet = new String("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"); //9
+		String alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 		int n = alphabet.length();
 
-		String result = new String(); 
+		String result = "";
 		Random r = new Random();
 
 		for (int i=0; i<length; i++)
-			result = result + alphabet.charAt(r.nextInt(n)); //13
+			result = result + alphabet.charAt(r.nextInt(n));
 
 		return result;
   }
