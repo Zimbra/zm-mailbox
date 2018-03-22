657c0b6d529d44e8ab62789c4edb1b905cecc767
Gren Elliot <gren.elliot@synacor.com>
2018-03-22 20:46:11 +0000

    ZCS-4276:Mailbox.getReadLockAndLockIt changes
    Made more like the current lock() implementation in that lock type can
    be promoted to a write lock if appropriate.

--- store/src/java/com/zimbra/cs/mailbox/Mailbox.java
+++ store/src/java/com/zimbra/cs/mailbox/Mailbox.java
@@ -9787,7 +9787,7 @@ public class Mailbox implements MailboxStore {
     }
 
     public MailboxLock getReadLockAndLockIt() {
-        return lockFactory.acquiredReadLock();
+        return requiresWriteLock() ? lockFactory.acquiredWriteLock() : lockFactory.acquiredReadLock();
     }
 
     @Override
