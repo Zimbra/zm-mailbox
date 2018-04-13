5554d0cdb57cf04d82f4cc3491fad73de9c639e7
ilya <raykini@zimbra.com>
2018-04-13 11:13:52 -0700

    LocalItemCache subclasses MapItemCache

--- store/src/java/com/zimbra/cs/mailbox/cache/LocalItemCache.java
+++ store/src/java/com/zimbra/cs/mailbox/cache/LocalItemCache.java
@@ -1,6 +1,5 @@
 package com.zimbra.cs.mailbox.cache;
 
-import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -8,82 +7,30 @@ import com.googlecode.concurrentlinkedhashmap.ConcurrentLinkedHashMap;
 import com.zimbra.cs.mailbox.MailItem;
 import com.zimbra.cs.mailbox.Mailbox;
 
-public class LocalItemCache extends ItemCache {
-    private final Map<Integer /* id */, MailItem> mapById;
-    private final Map<String /* uuid */, Integer /* id */> uuid2id;
+public class LocalItemCache extends MapItemCache<MailItem> {
 
-    public LocalItemCache(Mailbox mbox) {
-        super(mbox);
-        mapById = new ConcurrentLinkedHashMap.Builder<Integer, MailItem>().maximumWeightedCapacity(
-                        Mailbox.MAX_ITEM_CACHE_WITH_LISTENERS).build();
-        uuid2id = new ConcurrentHashMap<String, Integer>(Mailbox.MAX_ITEM_CACHE_WITH_LISTENERS);
+    public LocalItemCache(Mailbox mbox, Map<Integer, MailItem> itemMap, Map<String, Integer> uuidMap) {
+        super(mbox, itemMap, uuidMap);
     }
 
     @Override
-    public void put(MailItem item) {
-        int id = item.getId();
-        mapById.put(id, item);
-        String uuid = item.getUuid();
-        if (uuid != null) {
-            uuid2id.put(uuid, id);
-        }
+    protected MailItem toCacheValue(MailItem item) {
+        return item;
     }
 
     @Override
-    public MailItem get(int id) {
-        return mapById.get(id);
-    }
-
-    @Override
-    public MailItem get(String uuid) {
-        // Always fetch item from mapById map to preserve LRU's access time ordering.
-        Integer id = uuid2id.get(uuid);
-        return id != null ? mapById.get(id) : null;
-    }
-
-    @Override
-    public MailItem remove(MailItem item) {
-        return remove(item.getId());
-    }
-
-    @Override
-    public MailItem remove(int id) {
-        MailItem removed = mapById.remove(id);
-        if (removed != null) {
-            String uuid = removed.getUuid();
-            if (uuid != null) {
-                uuid2id.remove(uuid);
-            }
-        }
-        return removed;
-    }
-
-    @Override
-    public boolean contains(MailItem item) {
-        return mapById.containsKey(item.getId());
-    }
-
-    @Override
-    public Collection<MailItem> values() {
-        return mapById.values();
-    }
-
-    @Override
-    public int size() {
-        return mapById.size();
-    }
-
-    @Override
-    public void clear() {
-        mapById.clear();
-        uuid2id.clear();
+    protected MailItem fromCacheValue(MailItem value) {
+        return value;
     }
 
     public static class Factory implements ItemCache.Factory {
 
         @Override
         public ItemCache getItemCache(Mailbox mbox) {
-            return new LocalItemCache(mbox);
+            Map<Integer, MailItem> itemMap = new ConcurrentLinkedHashMap.Builder<Integer, MailItem>().maximumWeightedCapacity(
+                    Mailbox.MAX_ITEM_CACHE_WITH_LISTENERS).build();
+            Map<String, Integer> uuidMap = new ConcurrentHashMap<>(Mailbox.MAX_ITEM_CACHE_WITH_LISTENERS);
+            return new LocalItemCache(mbox, itemMap, uuidMap);
         }
     }
 }
