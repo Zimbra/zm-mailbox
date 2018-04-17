7cae805791d5a3c46aef7fdb48cf6945ad8f9d6f
ilya <raykini@zimbra.com>
2018-04-17 14:02:16 -0700

    Removed IdProvider and references

--- store/src/java/com/zimbra/cs/util/Zimbra.java
+++ store/src/java/com/zimbra/cs/util/Zimbra.java
@@ -62,12 +62,9 @@ import com.zimbra.cs.index.IndexStore;
 import com.zimbra.cs.index.queue.IndexingService;
 import com.zimbra.cs.index.solr.SolrIndex;
 import com.zimbra.cs.iochannel.MessageChannel;
-import com.zimbra.cs.mailbox.ContactBackupThread;
-import com.zimbra.cs.mailbox.IdProvider;
 import com.zimbra.cs.mailbox.MailboxManager;
 import com.zimbra.cs.mailbox.MailboxState;
 import com.zimbra.cs.mailbox.PurgeThread;
-import com.zimbra.cs.mailbox.RedisIdProvider;
 import com.zimbra.cs.mailbox.RedisMailboxState;
 import com.zimbra.cs.mailbox.ScheduledTaskManager;
 import com.zimbra.cs.mailbox.acl.AclPushTask;
@@ -399,8 +396,6 @@ public final class Zimbra {
                 ZimbraPerf.initialize(ZimbraPerf.ServerID.ZIMBRA);
             }
         }
-
-        IdProvider.setFactory(new RedisIdProvider.Factory());
 
         ExtensionUtil.postInitAll();
 
