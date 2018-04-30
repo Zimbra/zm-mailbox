7bb25ff004517013e76428bb27e284ada640f40f
Jayasree Garapati <jgarapati@synacor.com>
2018-04-30 14:16:30 +0000

    NICPS-153: ZAttrProvisioning.java fixed typo

--- common/src/java/com/zimbra/common/account/ZAttrProvisioning.java
+++ common/src/java/com/zimbra/common/account/ZAttrProvisioning.java
@@ -5963,12 +5963,13 @@ public class ZAttrProvisioning {
     public static final String A_zimbraDumpsterUserVisibleAge = "zimbraDumpsterUserVisibleAge";
 
     /**
-     * Header title on Email Printout
+     * This attribute is used to enable/disable zimbra headers on email
+     * printouts
      *
      * @since ZCS 8.8.6
      */
     @ZAttr(id=5007)
-    public static final String A_zimbraEmailPrintoutHeader = "zimbraEmailPrintoutHeader";
+    public static final String A_zimbraEmailPrintoutHeaderEnabled = "zimbraEmailPrintoutHeaderEnabled";
 
     /**
      * EmptyFolderOpTimeout is the time in seconds for which empty folder
