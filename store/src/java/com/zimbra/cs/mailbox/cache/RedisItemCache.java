10759d73fc4f3a7e5a4c7ff32736375367b7c195
ilya <raykini@zimbra.com>
2018-04-30 12:58:02 -0700

    consistent redis cache key naming

--- store/src/java/com/zimbra/cs/mailbox/cache/RedisItemCache.java
+++ store/src/java/com/zimbra/cs/mailbox/cache/RedisItemCache.java
@@ -26,7 +26,7 @@ public class RedisItemCache extends MapItemCache<String> {
 
     private void initFolderTagBucket() {
         RedissonClient client = RedissonClientHolder.getInstance().getRedissonClient();
-        folderTagBucket = client.getBucket(String.format("%s:FOLDERS_TAGS", mbox.getAccountId()));
+        folderTagBucket = client.getBucket(String.format("FOLDERS_TAGS:%s", mbox.getAccountId()));
     }
 
     @Override
@@ -70,8 +70,8 @@ public class RedisItemCache extends MapItemCache<String> {
         @Override
         public ItemCache getItemCache(Mailbox mbox) {
             RedissonClient client = RedissonClientHolder.getInstance().getRedissonClient();
-            String itemMapName = String.format("%s:id", mbox.getAccountId());
-            String uuidMapName = String.format("%s:uuid", mbox.getAccountId());
+            String itemMapName = String.format("ITEMS_BY_ID:%s", mbox.getAccountId());
+            String uuidMapName = String.format("ITEMS_BY_UUID:%s", mbox.getAccountId());
             RMap<Integer, String> itemMap = client.getMap(itemMapName);
             RMap<String, Integer> uuidMap = client.getMap(uuidMapName);
             return new RedisItemCache(mbox, itemMap, uuidMap);
