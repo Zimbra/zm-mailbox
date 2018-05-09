328c6626e4d3824cd4f141570401bbb04d425396
Travis McLane <travis.mclane@synacor.com>
2018-05-09 14:12:38 -0500

    ZCS-4921 implement PubSubService
    - implement new PubSubService which can be utilized to send messags to
      any process in the system which is also using the PubSubService
    - implement PubSubMsg base class to represent the most basic type of
      pub/sub message in the system
    - implement FlushCacheMsg to use in the broadcast system to ask all
      servers to flush their local caches
    - RedisPubSub is the cluster-wide pub/sub implementation
    - MemoryPubSub is intended for unit-tests primarily
    
    Note: Any subclasses of PubSubMsg are required to have an EMPTY default
    constructor that DOES NOT call the PubSubMsg default constructor or it
    seems that the JSON serializer/deserializer in Jackson fails to
    operate correctly.

---
+++ store/src/java/com/zimbra/cs/pubsub/PubSub.java
@@ -0,0 +1,107 @@
+package com.zimbra.cs.pubsub;
+
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.pubsub.message.PubSubMsg;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class PubSub {
+
+    private ConcurrentHashMap<String, CopyOnWriteArraySet<ListenerCallback>> listenerMap;
+
+    public PubSub() {
+        ZimbraLog.pubsub.debug("PubSub()");
+        startup();
+    }
+
+    public void startup() {
+        if (listenerMap == null) {
+            ZimbraLog.pubsub.debug("PubSub.startup()");
+            listenerMap = new ConcurrentHashMap<>();
+        }
+    }
+
+    /**
+     * Adds a new ListenerCallback to the specified channel
+     * @param channel name of the channel
+     * @param callback instance of a ListenerCallback to be called on receipt of a message on the channel
+     */
+    public void addListener(String channel, ListenerCallback callback) {
+        ZimbraLog.pubsub.debug("PubSub.addListener(%s) - '%s'", channel, callback);
+        CopyOnWriteArraySet<ListenerCallback> listeners = listenerMap.get(channel);
+        if (listeners == null) {
+            listeners = new CopyOnWriteArraySet<>();
+        }
+        listeners.add(callback);
+        CopyOnWriteArraySet<ListenerCallback> previousValue = listenerMap.putIfAbsent(channel, listeners);
+        if (previousValue != null) {
+            previousValue.add(callback);
+        }
+    }
+
+    /**
+     * Removes the specified ListenerCallback from the channel
+     * @param channel name of the channel
+     * @param callback ListenerCallback to be removed from the channel
+     * @return true if callback list is empty after removal false if not
+     */
+    public boolean removeListener(String channel, ListenerCallback callback) {
+        ZimbraLog.pubsub.debug("PubSub.removeListener(%s) - '%s'", channel, callback);
+        CopyOnWriteArraySet<ListenerCallback> listeners = listenerMap.get(channel);
+        if (listeners == null) {
+            return true;
+        }
+        if (listeners.contains(callback)) {
+            listeners.remove(callback);
+        }
+        return listeners.isEmpty();
+    }
+
+    /**
+     * Processes and delivers messages to all registered ListenerCallback(s)
+     * @param channel name of the channel
+     * @param msg PubSubMsg object containing the message received from the channel
+     */
+    public void onMessage(String channel, PubSubMsg msg) {
+        ZimbraLog.pubsub.debug("PubSub.onMessage(%s, %s) - %s", channel, msg, msg.getClass());
+        CopyOnWriteArraySet<ListenerCallback> listeners = listenerMap.get(channel);
+        if (listeners == null) {
+            return;
+        }
+        for (ListenerCallback listener: listeners) {
+            listener.onMessage(channel, msg);
+        }
+    }
+
+    /**
+     * Publishes a message to listeners of a channel
+     * @param channel name of the channel
+     * @param msg PubSubMsg to send to all registered listeners
+     */
+    public void publish(String channel, PubSubMsg msg) {
+        ZimbraLog.pubsub.debug("PubSub.publish(%s, %s)", channel, msg);
+        this.onMessage(channel, msg);
+    }
+
+    /**
+     * Retrieve the list of ListenerCallbacks for the channel
+     * @param channel name of the channel
+     * @return list of subscribed listeners for the channel
+     */
+    public List<ListenerCallback> getListeners(String channel) {
+        CopyOnWriteArraySet<ListenerCallback> listeners = listenerMap.get(channel);
+        if (listeners == null) {
+            listeners = new CopyOnWriteArraySet<>();
+        }
+        return new ArrayList<ListenerCallback>(listeners);
+    }
+
+    public static abstract class Factory {
+        protected abstract PubSub initPubSub();
+    }
+}
---
+++ store/src/java/com/zimbra/cs/pubsub/PubSubService.java
@@ -0,0 +1,93 @@
+package com.zimbra.cs.pubsub;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.pubsub.message.FlushCacheMsg;
+import com.zimbra.cs.pubsub.message.PubSubMsg;
+import com.zimbra.cs.service.admin.FlushCache;
+
+public class PubSubService {
+    public static String BROADCAST = "ZMC-BROADCAST";
+
+    private static PubSubService sInstance;
+    private static PubSub.Factory factory;
+
+    private PubSub pubsub;
+
+    protected PubSubService(PubSub pSub) {
+        pubsub = pSub;
+    }
+
+    public static void setInstance(PubSubService instance) {
+        PubSubService.sInstance = instance;
+    }
+
+    public static void setFactory(PubSub.Factory factory) {
+        PubSubService.factory = factory;
+    }
+
+    public void setPubSub(PubSub pubSub) {
+        pubsub = pubSub;
+    }
+
+    /**
+     * Called by Zimbra.startup to start up the PubSub service.
+     */
+    public void startup() {
+        pubsub.startup();
+        PubSubService.getInstance().addListener(BROADCAST, new BroadcastCallback());
+    }
+
+    /**
+     * Adds a new ListenerCallback to the channel
+     * @param channel channel name
+     * @param callback ListenerCallback to be notified of message arrival
+     */
+    public void addListener(String channel, ListenerCallback callback) {
+        pubsub.addListener(channel, callback);
+    }
+
+    public void removeListener(String channel, ListenerCallback callback) {
+        pubsub.removeListener(channel, callback);
+    }
+
+    /**
+     * Publishes a PubSubMsg to all listeners of the BROADCAST topic
+     * @param msg message to be broadcast
+     */
+    public void broadcast(PubSubMsg msg) {
+        publish(BROADCAST, msg);
+    }
+
+    /**
+     * Publishes a PubSubMsg to all listeners of the channel specified
+     * @param channel channel to publish
+     * @param msg message to be sent on the channel
+     */
+    public void publish(String channel, PubSubMsg msg) {
+        pubsub.publish(channel, msg);
+    }
+
+    public static PubSubService getInstance() {
+        if (sInstance == null) {
+            sInstance = new PubSubService(PubSubService.factory.initPubSub());
+        }
+        return sInstance;
+    }
+
+    public class BroadcastCallback extends ListenerCallback {
+        @Override
+        public void onMessage(String channel, PubSubMsg msg) {
+            ZimbraLog.pubsub.debug("PubSubService.BroadcastCallback.onMessage(%s) = %s", channel, msg);
+            if (msg instanceof FlushCacheMsg) {
+                ZimbraLog.pubsub.debug("FlushCacheMsg received: calling FlushCache.doFlush(selector)");
+                try {
+                    FlushCache.doFlush(((FlushCacheMsg)msg).getSelector());
+                } catch (ServiceException e) {
+                    ZimbraLog.pubsub.warn("Exception encountered processing '%s' message. \n%s", msg, e);
+                }
+            }
+        }
+    }
+
+}
