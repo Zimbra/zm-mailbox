8784b95db1236c718939e5f55f9d5202c9cf21e1
ilya <raykini@zimbra.com>
2018-05-14 22:36:59 -0700

    redis-backed folder/tag cache for persistent objects

---
+++ store/src/java/com/zimbra/cs/mailbox/cache/RedisFolderCache.java
@@ -0,0 +1,74 @@
+package com.zimbra.cs.mailbox.cache;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.mailbox.Folder;
+import com.zimbra.cs.mailbox.MailItem;
+import com.zimbra.cs.mailbox.MailItem.UnderlyingData;
+import com.zimbra.cs.mailbox.Mailbox;
+import com.zimbra.cs.mailbox.RedissonClientHolder;
+
+public class RedisFolderCache extends RedisSharedStateCache<Folder> implements FolderCache {
+
+    private RMap<String, Integer> uuid2IdMap;
+
+    public RedisFolderCache(Mailbox mbox) {
+        super(mbox, new LocalFolderCache());
+        RedissonClient client = RedissonClientHolder.getInstance().getRedissonClient();
+        uuid2IdMap = client.getMap(String.format("FOLDER_UUID2ID:%s", mbox.getAccountId()));
+    }
+
+    @Override
+    public void put(Folder folder) {
+        super.put(folder);
+        if (folder.getUuid() != null) {
+            uuid2IdMap.fastPut(folder.getUuid(), folder.getId());
+        }
+    }
+
+    @Override
+    protected Folder construct(int itemId, Map<String, Object> map) {
+        UnderlyingData ud = mapToUnderlyingData(itemId, map);
+        try {
+            return (Folder) MailItem.constructItem(mbox, ud, true);
+        } catch (ServiceException e) {
+            ZimbraLog.mailbox.error("unable to reconstruct Folder from Redis Live Object cache", e);
+            return null;
+        }
+    }
+
+    @Override
+    public Folder remove(int folderId) {
+        Folder removed = super.remove(folderId);
+        if (removed != null && removed.getUuid() != null) {
+            uuid2IdMap.fastRemove(removed.getUuid());
+        }
+        return removed;
+    }
+
+    @Override
+    public int size() {
+        return uuid2IdMap.size();
+    }
+
+    @Override
+    protected Integer getIdForStringKey(String uuid) {
+        return uuid2IdMap.get(uuid);
+    }
+
+    @Override
+    protected String getMapName(String accountId, int itemId) {
+        return String.format("%s:FOLDER:%d", accountId, itemId);
+    }
+
+    @Override
+    protected Collection<Integer> getAllIds() {
+        return uuid2IdMap.readAllValues();
+    }
+}
---
+++ store/src/java/com/zimbra/cs/mailbox/cache/RedisTagCache.java
@@ -0,0 +1,88 @@
+package com.zimbra.cs.mailbox.cache;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.mailbox.MailItem;
+import com.zimbra.cs.mailbox.MailItem.UnderlyingData;
+import com.zimbra.cs.mailbox.Mailbox;
+import com.zimbra.cs.mailbox.RedissonClientHolder;
+import com.zimbra.cs.mailbox.Tag;
+import com.zimbra.cs.mailbox.TagState;
+
+public class RedisTagCache extends RedisSharedStateCache<Tag> implements TagCache {
+
+    private RMap<String, Integer> name2Id;
+
+    public RedisTagCache(Mailbox mbox) {
+        super(mbox, new LocalTagCache());
+        RedissonClient client = RedissonClientHolder.getInstance().getRedissonClient();
+        name2Id = client.getMap(String.format("TAG_NAME2ID:%s", mbox.getAccountId()));
+    }
+
+    private LocalTagCache getLocalCache() {
+        return (LocalTagCache) localCache;
+    }
+
+    @Override
+    public Tag remove(String tagName) {
+        Tag removed = getLocalCache().remove(tagName);
+        if (removed != null) {
+            remove(removed.getId());
+        }
+        return removed;
+    }
+
+    @Override
+    public Tag remove(int tagId) {
+        Tag removed = super.remove(tagId);
+        if (removed != null ) {
+            name2Id.fastRemove(removed.getName().toLowerCase());
+        }
+        return removed;
+    }
+
+    @Override
+    public boolean contains(String tagName) {
+        return getLocalCache().contains(tagName);
+    }
+
+    @Override
+    public void put(Tag tag) {
+        super.put(tag);
+        name2Id.fastPut(tag.getName().toLowerCase(), tag.getId());
+    }
+
+    @Override
+    protected Tag construct(int tagId, Map<String, Object> map) {
+        UnderlyingData ud = mapToUnderlyingData(tagId, map);
+        try {
+            Tag tag = (Tag) MailItem.constructItem(mbox, ud, true);
+            tag.setIsImapVisible(boolVal(map, TagState.F_IMAP_VISIBLE));
+            return tag;
+        } catch (ServiceException e) {
+            ZimbraLog.mailbox.error("unable to reconstruct Tag from RMap", e);
+            return null;
+        }
+    }
+
+    @Override
+    protected Integer getIdForStringKey(String key) {
+        return name2Id.get(key);
+    }
+
+    @Override
+    protected String getMapName(String accountId, int itemId) {
+        return String.format("%s:TAG:%d", accountId, itemId);
+    }
+
+    @Override
+    protected Collection<Integer> getAllIds() {
+        return name2Id.readAllValues();
+    }
+}
