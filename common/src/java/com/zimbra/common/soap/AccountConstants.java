1598be94caf808647afac196641a7fb16c244e17
Shrikant <shrikant.prasad@synacor.com>
2018-05-28 12:58:49 +0530

    ZCS-4800:Modify AuthRequest to support recovery code based authentication

--- common/src/java/com/zimbra/common/soap/AccountConstants.java
+++ common/src/java/com/zimbra/common/soap/AccountConstants.java
@@ -335,6 +335,7 @@ public class AccountConstants {
     public static final String E_ID = "id";
     public static final String E_PROFILE_IMAGE_ID = "profileImageId";
     public static final String E_PASSWORD = "password";
+    public static final String E_RECOVERY_CODE = "recoveryCode";
     public static final String E_OLD_PASSWORD = "oldPassword";
     public static final String A_SECTIONS = "sections";
     public static final String E_PREF = "pref";
