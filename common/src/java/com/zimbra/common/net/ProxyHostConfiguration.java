9d9e69affbbe82aecc1b91de9c518842093dffe2
rupalid <rtdesai@gmail.com>
2018-07-09 15:30:50 +0530

    ZCS-4845_1 changes in common for httpclient upgrade (#678)
    * ZCS-4845_1 changes in common for hhtpclient upgrade
    
    * ZCS-4845_1 Fixing codacy reported issues an dother fixes
    
    * ZCS-4845_1 Fixing issues fund in code review

--- common/src/java/com/zimbra/common/net/ProxyHostConfiguration.java
+++ common/src/java/com/zimbra/common/net/ProxyHostConfiguration.java
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2011, 2013, 2014, 2016 Synacor, Inc.
+ * Copyright (C) 2011, 2013, 2014, 2016, 2018 Synacor, Inc.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
@@ -16,16 +16,14 @@
  */
 package com.zimbra.common.net;
 
-import org.apache.commons.httpclient.HostConfiguration;
 /**
  * HostConfiguration that includes proxy username/password
  */
-public class ProxyHostConfiguration extends HostConfiguration {
+public class ProxyHostConfiguration  {
     private String username;
     private String password;
-    public ProxyHostConfiguration(HostConfiguration hc) {
-        super(hc);
-    }
+    private String proxyHost;
+    private int proxyPort;
     public String getUsername() {
         return username;
     }
@@ -38,4 +36,21 @@ public class ProxyHostConfiguration extends HostConfiguration {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getProxyHost() {
+        return proxyHost;
+    }
+
+    public void setProxyHost(String proxyHost) {
+        this.proxyHost = proxyHost;
+    }
+
+    public int getProxyPort() {
+        return proxyPort;
+    }
+
+    public void setProxyPort(int proxyPort) {
+        this.proxyPort = proxyPort;
+    }
+
 }
--- common/src/java/com/zimbra/common/net/SecureProtocolSocketFactoryWrapper.java
+++ common/src/java/com/zimbra/common/net/SecureProtocolSocketFactoryWrapper.java
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2010, 2013, 2014, 2016 Synacor, Inc.
+ * Copyright (C) 2010, 2013, 2014, 2016, 2018 Synacor, Inc.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
@@ -16,14 +16,20 @@
  */
 package com.zimbra.common.net;
 
-import org.apache.commons.httpclient.protocol.SecureProtocolSocketFactory;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 import javax.net.ssl.SSLSocketFactory;
-import java.io.IOException;
-import java.net.Socket;
 
-class SecureProtocolSocketFactoryWrapper
-    extends ProtocolSocketFactoryWrapper implements SecureProtocolSocketFactory {
+import org.apache.http.HttpHost;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
+import org.apache.http.protocol.HttpContext;
+
+class SecureProtocolSocketFactoryWrapper extends ProtocolSocketFactoryWrapper 
+implements LayeredConnectionSocketFactory {
     
     private SSLSocketFactory factory;
 
@@ -34,5 +40,51 @@ class SecureProtocolSocketFactoryWrapper
     
     public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException {
         return factory.createSocket(socket, host, port, autoClose);
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.http.conn.socket.ConnectionSocketFactory#connectSocket(int, java.net.Socket, org.apache.http.HttpHost, java.net.InetSocketAddress, java.net.InetSocketAddress, org.apache.http.protocol.HttpContext)
+     */
+    @Override
+    public Socket connectSocket(int connectTimeout, Socket sock, HttpHost host,
+        InetSocketAddress remoteAddress, InetSocketAddress localAddress, HttpContext context)
+        throws IOException {
+
+        sock.bind(localAddress);
+        sock.connect(remoteAddress, connectTimeout);
+        return sock;
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.http.conn.socket.ConnectionSocketFactory#createSocket(org.apache.http.protocol.HttpContext)
+     */
+    @Override
+    public Socket createSocket(HttpContext context) throws IOException {
+        if (context != null) {
+            HttpClientContext clientContext = HttpClientContext.adapt(context);
+            HttpHost  host = clientContext.getTargetHost();
+            return factory.createSocket(host.getHostName(), host.getPort());
+        } 
+        return factory.createSocket();
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.http.conn.socket.LayeredConnectionSocketFactory#createLayeredSocket(java.net.Socket, java.lang.String, int, org.apache.http.protocol.HttpContext)
+     */
+    @Override
+    public Socket createLayeredSocket(Socket socket,  String target, int port, HttpContext context)
+        throws IOException, UnknownHostException {
+        if (socket == null) {
+            if (context != null) {
+                HttpClientContext clientContext = HttpClientContext.adapt(context);
+                HttpHost  host = clientContext.getTargetHost();
+                return factory.createSocket(host.getHostName(), host.getPort());
+            } else {
+                return  factory.createSocket(target, port);
+            }
+        } else {
+            return  factory.createSocket(socket, target, port, true);
+        }
     }
 }
