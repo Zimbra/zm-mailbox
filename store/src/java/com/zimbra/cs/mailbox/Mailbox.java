afc09840dbb5aebe5037649422dd24cb3e51867d
Gren Elliot <gren@fatkudu.net>
2018-09-07 02:10:27 +0100

    ZCS-5899:rm dup. Mailbox.resetDefaultCalendarId()

--- store/src/java/com/zimbra/cs/mailbox/Mailbox.java
+++ store/src/java/com/zimbra/cs/mailbox/Mailbox.java
@@ -9475,13 +9475,6 @@ public class Mailbox implements MailboxStore {
         // do nothing
     }
 
-    /*
-     * This method resets defalult calendar id to ID_FOLDER_CALENDAR in pref
-     */
-    public void resetDefaultCalendarId() throws ServiceException {
-        getAccount().setPrefDefaultCalendarId(ID_FOLDER_CALENDAR);
-    }
-
     private SearchHistory getSearchHistory() throws ServiceException {
         return SearchHistory.getFactory().getSearchHistory(getAccount());
     }
@@ -10060,9 +10053,7 @@ public class Mailbox implements MailboxStore {
         return copy;
     }
 
-    /*
-	 * This method resets defalult calendar id to ID_FOLDER_CALENDAR in pref
-     */
+    /** This method resets default calendar id to ID_FOLDER_CALENDAR in pref */
     public void resetDefaultCalendarId() throws ServiceException {
         getAccount().setPrefDefaultCalendarId(ID_FOLDER_CALENDAR);
     }
