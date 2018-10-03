b582807d30b88b5145533e1e02953e19b5a61583
ilya <raykini@zimbra.com>
2018-10-03 15:54:58 -0700

    Mailbox.close() handles uncaught Exceptions

--- store/src/java/com/zimbra/cs/mailbox/Mailbox.java
+++ store/src/java/com/zimbra/cs/mailbox/Mailbox.java
@@ -9899,6 +9899,9 @@ public class Mailbox implements MailboxStore {
                     } catch (ServiceException e) {
                         exception = e;
                         this.rollback();
+                    } catch (Exception e2) {
+                        exception = ServiceException.FAILURE("uncaught exception during transaction close, rolling back transaction", e2);
+                        this.rollback();
                     }
                 }
 
