2cce8fee13a5273a52cc4ddc16ae718aeb0fe040
ilya <raykini@zimbra.com>
2018-10-04 13:25:49 -0700

    MailboxLock interface declares ServiceException

--- common/src/java/com/zimbra/common/mailbox/MailboxLockFactory.java
+++ common/src/java/com/zimbra/common/mailbox/MailboxLockFactory.java
@@ -1,5 +1,7 @@
 package com.zimbra.common.mailbox;
 
+import com.zimbra.common.service.ServiceException;
+
 /**
  * Autocloseable so that close() can be called on any resources used to gain access to a
  * locking framework for an instance of the factory.
@@ -9,15 +11,15 @@ public interface MailboxLockFactory extends AutoCloseable {
 
     MailboxLock writeLock();
 
-    MailboxLock acquiredWriteLock();
+    MailboxLock acquiredWriteLock() throws ServiceException;
 
-    MailboxLock acquiredReadLock();
+    MailboxLock acquiredReadLock() throws ServiceException;
 
     /**
      * Number of holds on this lock by the current thread (sum of read and write locks)
      * @return holds or <code>0</code> if this lock is not held by current thread
      */
-    int getHoldCount();
+    int getHoldCount() throws ServiceException;
 
     @Deprecated
     MailboxLock lock(boolean write);
