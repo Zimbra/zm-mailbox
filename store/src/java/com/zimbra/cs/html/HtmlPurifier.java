da7ef911e9bdec9ac8dec90c9113f8a982b4835b
Rupali Desai <rupali.desai@synacor.com>
2018-10-10 09:26:17 +0530

    ZCS-5696 Defanger fix for attachment with XSS

--- store/src/java/com/zimbra/cs/html/HtmlPurifier.java
+++ store/src/java/com/zimbra/cs/html/HtmlPurifier.java
@@ -34,9 +34,13 @@ public class HtmlPurifier extends Purifier {
 
     private static final Pattern VALID_IMG_TAG = Pattern.compile(
         DebugConfig.defangOwaspValidImgTag, Pattern.CASE_INSENSITIVE);
-    private static final PolicyFactory sanitizer = Sanitizers.IMAGES;
+    private static final PolicyFactory sanitizer = Sanitizers.FORMATTING.and(Sanitizers.IMAGES);
     private static final Pattern IMG_SKIP_OWASPSANITIZE = Pattern.compile(
         DebugConfig.defangImgSkipOwaspSanitize, Pattern.CASE_INSENSITIVE);
+    private static final Pattern VALID_SCRIPT_TAG = Pattern.compile(
+        DebugConfig.defangOwaspJavaScript, Pattern.CASE_INSENSITIVE);
+    private static final Pattern VALID_ALERT_TAG = Pattern.compile(
+        DebugConfig.defangOwaspAlert, Pattern.CASE_INSENSITIVE);
 
     /* (non-Javadoc)
      * @see org.cyberneko.html.filters.Purifier#purifyText(org.apache.xerces.xni.XMLString)
@@ -50,6 +54,10 @@ public class HtmlPurifier extends Purifier {
         }
 
         if (VALID_IMG_TAG.matcher(temp).find()) {
+            temp = sanitizer.sanitize(temp);
+        }
+
+        if (VALID_ALERT_TAG.matcher(temp).find() || VALID_SCRIPT_TAG.matcher(temp).find()) {
             temp = sanitizer.sanitize(temp);
         }
 
