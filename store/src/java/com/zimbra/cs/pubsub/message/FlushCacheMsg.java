36753f512f599058ec1d45e6d20d8a58effc4d75
Gren Elliot <gren.elliot@synacor.com>
2018-11-16 12:06:59 +0000

    ZCS-6183:FlushCacheMsg use JSON String
    Use a JSON string instead of a `CacheSelector` object as the default
    serialization used by Redis can't cope with deserialization of
    `CacheSelector`

--- store/src/java/com/zimbra/cs/pubsub/message/FlushCacheMsg.java
+++ store/src/java/com/zimbra/cs/pubsub/message/FlushCacheMsg.java
@@ -1,31 +1,38 @@
 package com.zimbra.cs.pubsub.message;
 
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.soap.admin.type.CacheSelector;
+import com.zimbra.soap.json.JacksonUtil;
 
 /**
  * Contains the CacheSelector payload of a FlushCacheRequest (SOAP).
  * Allows for resubmitting the request over the PubSub channel.
  */
 public class FlushCacheMsg extends PubSubMsg {
-    private CacheSelector selector;
+    private String selector;
 
     // Default for serialization - don't remove
     public FlushCacheMsg() { }
 
     public FlushCacheMsg(CacheSelector selector) {
-        this.selector = selector;
+        setSelector(selector);
     }
 
-    public void setSelector(CacheSelector selector) {
-        this.selector = selector;
+    public void setSelector(CacheSelector cacheSelector) {
+        try {
+            selector = JacksonUtil.jaxbToJsonNonSoapApi(cacheSelector);
+        } catch (ServiceException e) {
+            ZimbraLog.cache.info("FlushCacheMsg.setSelector(%s) - problem serializing, so using null", cacheSelector);
+            selector = null;
+        }
     }
 
     public CacheSelector getSelector() {
-        return this.selector;
+        return JacksonUtil.jsonToJaxbNonSoapApi(selector, CacheSelector.class);
     }
 
     public String toString() {
-        return String.format("FlushCacheMsg[targetService=%s, selector=%s]",  this.getTargetService(), this.selector);
+        return String.format("FlushCacheMsg[targetService=%s, selector=%s]",  getTargetService(), selector);
     }
-
 }
