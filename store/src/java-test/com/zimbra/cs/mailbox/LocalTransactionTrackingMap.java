8022ca9bb76f3e93fb8c33af50307c9bc75d2a0d
ilya <raykini@zimbra.com>
2019-01-02 13:36:04 -0800

    more control over TransactionAware caching/access behavior

--- store/src/java-test/com/zimbra/cs/mailbox/LocalTransactionTrackingMap.java
+++ store/src/java-test/com/zimbra/cs/mailbox/LocalTransactionTrackingMap.java
@@ -4,16 +4,16 @@ import java.util.Map;
 
 public class LocalTransactionTrackingMap<K, V> extends TransactionAwareMap<K, V> {
 
-    public LocalTransactionTrackingMap(Map<K, V> map, TransactionCacheTracker cacheTracker, String mapName) {
-        super(mapName, cacheTracker, new LocalGetter<>(mapName, map));
+    public LocalTransactionTrackingMap(Map<K, V> map, TransactionCacheTracker cacheTracker, String mapName, ReadPolicy readPolicy, WritePolicy writePolicy) {
+        super(mapName, cacheTracker, new LocalGetter<>(mapName, map), readPolicy, writePolicy);
     }
 
-    private static class LocalGetter<K, V> extends GreedyGetter<Map<K, V>> {
+    private static class LocalGetter<K, V> extends GreedyCachingGetter<Map<K, V>> {
 
         private Map<K, V> map;
 
         public LocalGetter(String objectName, Map<K, V> map) {
-            super(objectName);
+            super(objectName, CachePolicy.SINGLE_VALUE);
             this.map = map;
         }
 
