a3db870c5e63d4039e619dd10d2880fa27271ce6
Vishnu kant Jangid <vishnukant.jangid@synacor.com>
2019-02-11 12:15:52 +0530

    NICPS-535: Fixed codacy-bot comments

--- store/src/java/com/zimbra/cs/service/mail/SendInviteReply.java
+++ store/src/java/com/zimbra/cs/service/mail/SendInviteReply.java
@@ -429,7 +429,7 @@ public class SendInviteReply extends CalendarRequest {
         }
         
 		try {
-			if(!StringUtil.isNullOrEmpty(verbStr) && "ACCEPT".equals(verbStr)) {
+			if("ACCEPT".equals(verbStr)) {
 				CalItemReminderService.scheduleReminder(new CalItemSmsReminderTask(), calItem);
 			}
 		} catch(Exception ex) {
