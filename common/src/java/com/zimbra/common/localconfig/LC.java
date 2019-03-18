0a99b25b094ffa12d4b7f9f475495a09d098b907
Stephan DeSouza <sdesouza@synacor.com>
2019-03-18 10:45:44 -0400

    ZBUG-913 Remove extra whitespace.

--- common/src/java/com/zimbra/common/localconfig/LC.java
+++ common/src/java/com/zimbra/common/localconfig/LC.java
@@ -1300,7 +1300,6 @@ public final class LC {
     // Feed Manager comma-separated blacklist. addresses can be CIDR notation, or single ip. no wild-cards (default blacklist private networks)
     public static final KnownKey zimbra_feed_manager_blacklist = KnownKey.newKey("10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,fd00::/8");
 
-
     static {
         // Automatically set the key name with the variable name.
         for (Field field : LC.class.getFields()) {
