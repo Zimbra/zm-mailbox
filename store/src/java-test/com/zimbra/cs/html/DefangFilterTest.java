aa2a6078c31f50380cabb3a439b8a4bf5ba49422
Chinmay Pathak <15.chinmay@gmail.com>
2019-04-09 14:24:16 +0530

    ZCS-6871 Make OWASP filtering configurable
    Added unit test
    
    updated comment as per review comment
    
    Resolving review comments

--- store/src/java-test/com/zimbra/cs/html/DefangFilterTest.java
+++ store/src/java-test/com/zimbra/cs/html/DefangFilterTest.java
@@ -42,6 +42,7 @@ import com.zimbra.cs.mailbox.MailboxTestUtil;
 import com.zimbra.cs.mime.MPartInfo;
 import com.zimbra.cs.mime.Mime;
 import com.zimbra.cs.mime.ParsedMessage;
+import com.zimbra.cs.service.mail.ToXML;
 import com.zimbra.cs.servlet.ZThreadLocal;
 import com.zimbra.soap.RequestContext;
 
@@ -52,11 +53,12 @@ import com.zimbra.soap.RequestContext;
  *
  */
 public class DefangFilterTest {
-    private static final String EMAIL_BASE_DIR = "./data/unittest/email/";
+    private static String EMAIL_BASE_DIR = "./data/unittest/email/";
     @BeforeClass
     public static void init() throws Exception {
         MailboxTestUtil.initServer();
         Provisioning prov = Provisioning.getInstance();
+        prov.getConfig().setUseOwaspHtmlSanitizer(false);
         Account acct = prov.createAccount("test@in.telligent.com", "secret", new HashMap<String, Object>());
     }
 
@@ -1463,10 +1465,25 @@ public class DefangFilterTest {
      */
     @Test
     public void testzbug736Mime1() throws Exception {
+        Provisioning.getInstance().getConfig().setUseOwaspHtmlSanitizer(false);
         String fileName = "zbug736_2.txt";
         InputStream htmlStream = getHtmlBody(fileName);
         String result = DefangFactory.getDefanger(MimeConstants.CT_TEXT_HTML).defang(htmlStream,
             true);
         Assert.assertTrue(result != null);
+    }
+
+    /**
+     * Checking zimbraUseOwaspHtmlSanitizer is true
+     * @throws Exception
+     */
+    @Test
+    public void testzcs6871OwaspDefanger() throws Exception {
+        BrowserDefang defanger2 = DefangFactory.getDefanger(null);
+        Assert.assertFalse(defanger2 instanceof OwaspHtmlSanitizer);
+
+        Provisioning.getInstance().getConfig().setUseOwaspHtmlSanitizer(true);
+        BrowserDefang defanger = DefangFactory.getDefanger(null);
+        Assert.assertTrue(defanger instanceof OwaspHtmlSanitizer);
     }
 }
