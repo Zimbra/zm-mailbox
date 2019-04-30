f9b86f322eefaf27e0b40eded367bca712f9c1dc
rupalid <rtdesai@gmail.com>
2019-04-30 16:37:49 +0530

    ZCS7167 Fixing issues found in SOAP automation (#892)
    * ZCS7167 Fixing issues found in SOAP automation
    
    * ZCS-7167 Fixing  issue found in SOAP automation testing

--- common/src/java/com/zimbra/common/net/ProtocolSocketFactoryWrapper.java
+++ common/src/java/com/zimbra/common/net/ProtocolSocketFactoryWrapper.java
@@ -19,6 +19,7 @@ package com.zimbra.common.net;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 import javax.net.SocketFactory;
 
@@ -36,7 +37,7 @@ class ProtocolSocketFactoryWrapper implements ConnectionSocketFactory {
     }
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see
      * org.apache.http.conn.socket.ConnectionSocketFactory#createSocket(org.
      * apache.http.protocol.HttpContext)
@@ -46,14 +47,14 @@ class ProtocolSocketFactoryWrapper implements ConnectionSocketFactory {
         if (context != null) {
             HttpClientContext clientContext = HttpClientContext.adapt(context);
             HttpHost  host = clientContext.getTargetHost();
-            factory.createSocket(host.getHostName(), host.getPort());
-        } 
+            return createSocketFromHostInfo(host);
+        }
         return factory.createSocket();
     }
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see
      * org.apache.http.conn.socket.ConnectionSocketFactory#connectSocket(int,
      * java.net.Socket, org.apache.http.HttpHost, java.net.InetSocketAddress,
@@ -67,15 +68,38 @@ class ProtocolSocketFactoryWrapper implements ConnectionSocketFactory {
         if (context != null) {
             HttpClientContext clientContext = HttpClientContext.adapt(context);
             timeout = clientContext.getConnection().getSocketTimeout();
-        } 
-       
+        }
+
+
         if (timeout > 0) {
-            sock.bind(localAddress);
-            sock.connect(remoteAddress, connectTimeout);
+            if (sock != null && !sock.isBound()) {
+                sock.bind(localAddress);
+                sock.connect(remoteAddress, timeout);
+            }
             return sock;
         } else {
             sock.connect(remoteAddress, connectTimeout);
             return sock;
+        }
+    }
+
+    /**
+     * @param host
+     * @return
+     * @throws IOException
+     * @throws UnknownHostException
+     */
+    public Socket createSocketFromHostInfo(HttpHost host) throws IOException, UnknownHostException {
+        if(host.getPort() == -1) {
+            if (host.getSchemeName().equalsIgnoreCase("http")) {
+               return  factory.createSocket(host.getHostName(), 80);
+            } else if (host.getSchemeName().equalsIgnoreCase("https")) {
+               return  factory.createSocket(host.getHostName(), 443);
+            } else {
+                throw new IOException("Unknown scheme for connecting  to host, Received  +  host.toHostString()");
+            }
+        } else {
+           return  factory.createSocket(host.getHostName(), host.getPort());
         }
     }
 
