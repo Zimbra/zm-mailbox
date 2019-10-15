c914f7e445d6ac07ff14912ad38f58d86cc689e7
Chinmay Pathak <15.chinmay@gmail.com>
2019-10-16 00:31:11 +0530

    ZCS-7983 Add expiry to individual Zimbra licensed features

--- common/src/java/com/zimbra/common/soap/AccountConstants.java
+++ common/src/java/com/zimbra/common/soap/AccountConstants.java
@@ -427,6 +427,7 @@ public class AccountConstants {
     public static final String A_KEY = "key";
     public static final String A_N = "n";
     public static final String A_NAME = "name";
+    public static final String A_EXPIRY = "expiry";
     public static final String A_ID = "id";
     public static final String A_ID_ONLY = "idOnly";
     public static final String A_IS_DL = "isDL";
