d380967cb61768581a728c3b877191069fa0322d
Shashi Kant <shashikantsantosh@gmail.com>
2019-11-05 17:06:58 +0530

    ZCS-8028: Conf changes to support current deployment

--- common/src/java/com/zimbra/common/localconfig/LC.java
+++ common/src/java/com/zimbra/common/localconfig/LC.java
@@ -268,7 +268,7 @@ public final class LC {
     public static final KnownKey zimbra_index_rfc822address_max_token_count = KnownKey.newKey(512);
 
     @Supported
-    public static final KnownKey zimbra_index_num_collections = KnownKey.newKey(2);
+    public static final KnownKey zimbra_index_num_collections = KnownKey.newKey(1);
 
     @Supported
     public static final KnownKey zimbra_index_collections_prefix = KnownKey.newKey("mailbox_index_");
