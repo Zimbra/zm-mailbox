1d1e9c7aed1f514e0c91a6c96d1a2afc04df8581
rupalid <rtdesai@gmail.com>
2019-11-11 08:52:20 +0530

    Code review comments

--- store/src/java-test/com/zimbra/cs/mailbox/ContactTest.java
+++ store/src/java-test/com/zimbra/cs/mailbox/ContactTest.java
@@ -341,7 +341,6 @@ import com.zimbra.cs.util.ZTestWatchman;
     @Test
     public void testZCS6232WithNullEmail() throws Exception {
         Account account = Provisioning.getInstance().getAccountByName("test6232@zimbra.com");
-        // mocking the group not to have view permission
         PowerMockito.stub(PowerMockito.method(GalGroupInfoProvider.class, "getGroupInfo"))
             .toReturn(GroupInfo.IS_GROUP);
         Assert.assertTrue(ToXML.hasDLViewRight(null, account, account));
