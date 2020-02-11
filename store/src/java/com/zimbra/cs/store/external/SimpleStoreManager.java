aa2eab3f5e10611b550325cb5e963b5bd8d841c9
Nilesh Pawar <nilesh.pawar@synacor.com>
2020-02-11 14:57:38 +0530

    ZBUG-1315: Fixes after soap test

--- store/src/java/com/zimbra/cs/store/external/SimpleStoreManager.java
+++ store/src/java/com/zimbra/cs/store/external/SimpleStoreManager.java
@@ -126,32 +126,4 @@ public class SimpleStoreManager extends ExternalStoreManager {
             return super.supports(feature);
         }
     }
-
-	@Override
-	public MailboxBlob copy(MailboxBlob src, Mailbox destMbox, int destMsgId, long destRevision)
-			throws IOException, ServiceException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public MailboxBlob link(StagedBlob src, Mailbox destMbox, int destMsgId, long destRevision)
-			throws IOException, ServiceException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public MailboxBlob renameTo(StagedBlob src, Mailbox destMbox, int destMsgId, long destRevision)
-			throws IOException, ServiceException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public MailboxBlob getMailboxBlob(Mailbox mbox, int itemId, long revision, String locator, boolean validate)
-			throws ServiceException {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
