65ca538b35c0adeb3c80ecf683fa5cd854d19990
Chinmay Pathak <15.chinmay@gmail.com>
2020-02-29 15:55:51 +0530

    ZCS-8500 calendar add and edit
    edit and sync state fixes
    
    (cherry picked from commit 773c42190ec477ec6562e38d561b55828ccf8af6)

--- common/src/java/com/zimbra/common/zmime/ZMimeMessage.java
+++ common/src/java/com/zimbra/common/zmime/ZMimeMessage.java
@@ -27,7 +27,6 @@ import javax.mail.Address;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
