5f4e2f502421e08d96dd6694d54d29676f98c9fd
ilya <raykini@zimbra.com>
2020-03-16 12:37:14 -0700

    set waitingForSearch on delegate auth

--- store/src/java/com/zimbra/cs/service/admin/DelegateAuth.java
+++ store/src/java/com/zimbra/cs/service/admin/DelegateAuth.java
@@ -20,6 +20,12 @@
  */
 package com.zimbra.cs.service.admin;
 
+import java.util.List;
+import java.util.Map;
+
+import com.zimbra.common.account.Key;
+import com.zimbra.common.account.Key.AccountBy;
+import com.zimbra.common.account.ZAttrProvisioning.DelayedIndexStatus;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.AdminConstants;
 import com.zimbra.common.soap.Element;
@@ -27,16 +33,14 @@ import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.AccountServiceException;
 import com.zimbra.cs.account.AuthToken;
-import com.zimbra.cs.account.AuthTokenException;
 import com.zimbra.cs.account.Provisioning;
-import com.zimbra.common.account.Key.AccountBy;
+import com.zimbra.cs.account.Provisioning.CacheEntry;
 import com.zimbra.cs.account.accesscontrol.AdminRight;
 import com.zimbra.cs.account.accesscontrol.Rights.Admin;
 import com.zimbra.cs.service.AuthProvider;
+import com.zimbra.cs.util.AccountUtil;
 import com.zimbra.soap.ZimbraSoapContext;
-
-import java.util.List;
-import java.util.Map;
+import com.zimbra.soap.admin.type.CacheEntryType;
 
 /**
  * @author schemers
@@ -48,11 +52,13 @@ public class DelegateAuth extends AdminDocumentHandler {
 
     public static final String BY_NAME = "name";
     public static final String BY_ID = "id";
-    
+
+    @Override
     public boolean domainAuthSufficient(Map context) {
         return true;
     }
 
+    @Override
     public Element handle(Element request, Map<String, Object> context) throws ServiceException {
         ZimbraSoapContext zsc = getZimbraSoapContext(context);
 
@@ -61,9 +67,9 @@ public class DelegateAuth extends AdminDocumentHandler {
         String value = a.getText();
 
         long lifetime = request.getAttributeLong(AdminConstants.A_DURATION, DEFAULT_AUTH_LIFETIME) * 1000;
-        
+
         Provisioning prov = Provisioning.getInstance();
-        
+
         Account account = null;
 
         if (key.equals(BY_NAME)) {
@@ -76,14 +82,14 @@ public class DelegateAuth extends AdminDocumentHandler {
 
         if (account == null)
             throw AccountServiceException.NO_SUCH_ACCOUNT(value);
-        
+
         checkAdminLoginAsRight(zsc, prov, account);
-        
+
         ZimbraLog.security.info(ZimbraLog.encodeAttrs(
-                new String[] {"cmd", "DelegateAuth","accountId", account.getId(),"accountName", account.getName()})); 
+                new String[] {"cmd", "DelegateAuth","accountId", account.getId(),"accountName", account.getName()}));
 
         Element response = zsc.createElement(AdminConstants.DELEGATE_AUTH_RESPONSE);
-        long maxLifetime = account.getTimeInterval(Provisioning.A_zimbraAuthTokenLifetime, DEFAULT_AUTH_LIFETIME*1000); 
+        long maxLifetime = account.getTimeInterval(Provisioning.A_zimbraAuthTokenLifetime, DEFAULT_AUTH_LIFETIME*1000);
 
         // take the min of requested lifetime vs maxLifetime
         long expires = System.currentTimeMillis()+ Math.min(lifetime, maxLifetime);
@@ -95,23 +101,57 @@ public class DelegateAuth extends AdminDocumentHandler {
         AuthToken at = AuthProvider.getAuthToken(account, expires, false, adminAcct);
         at.encodeAuthResp(response, true);
         response.addAttribute(AdminConstants.E_LIFETIME, lifetime, Element.Disposition.CONTENT);
+        handleDelayedIndex(account);
         return response;
     }
-    
+
     @Override
     public void docRights(List<AdminRight> relatedRights, List<String> notes) {
         relatedRights.add(Admin.R_adminLoginAs);
         relatedRights.add(Admin.R_adminLoginCalendarResourceAs);
         notes.add(AdminRightCheckPoint.Notes.ADMIN_LOGIN_AS);
     }
-    
+
     /*
     public static void main(String args[]) throws ServiceException, AuthTokenException {
         Account acct = Provisioning.getInstance().getAccountByName("user2@slapshot.example.zimbra.com");
-        Account admin = Provisioning.getInstance().getAccountByName("admin@slapshot.example.zimbra.com");        
+        Account admin = Provisioning.getInstance().getAccountByName("admin@slapshot.example.zimbra.com");
         AuthToken at = new AuthToken(acct, System.currentTimeMillis()+DEFAULT_AUTH_LIFETIME*1000, false, admin);
         String token = at.getEncoded();
         System.out.println(token);
     }
     */
+
+    private void handleDelayedIndex(Account acct) {
+        // delegate auth isn't guaranteed to run on the the target account's affinity pod, so we have to
+        // make sure we don't have stale LDAP attributes
+        boolean isLocal;
+        try {
+            isLocal = Provisioning.onLocalServer(acct);
+        } catch (ServiceException e) {
+            isLocal = false;
+        }
+        if (!isLocal) {
+            //flush account cache first
+            CacheEntry accountEntry = new CacheEntry(Key.CacheEntryBy.id, acct.getId());
+            try {
+                Provisioning.getInstance().flushCache(CacheEntryType.account, new CacheEntry[] {accountEntry});
+            } catch (ServiceException e) {
+                ZimbraLog.index.warn("error flushing cache on non-local account %s during delegated auth", acct.getName(), e);
+            }
+        }
+        if (acct.isFeatureDelayedIndexEnabled() && acct.getDelayedIndexStatus() == DelayedIndexStatus.suppressed) {
+            try {
+                acct.setDelayedIndexStatus(DelayedIndexStatus.waitingForSearch);
+                if (!isLocal) {
+                    AccountUtil.broadcastFlushCache(acct);
+                    ZimbraLog.index.info("updated delayed index status for non-local %s to waitingForSearch and broadcast flushCache", acct.getName());
+                } else {
+                    ZimbraLog.index.info("updated delayed index status for %s to waitingForSearch", acct.getName());
+                }
+            } catch (ServiceException e) {
+                ZimbraLog.index.error("error updating delayed index status for account %s after delegate auth", acct.getName(), e);
+            }
+        }
+    }
 }
