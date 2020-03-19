01a819686198f207c531fb66d04454db96f63375
robert.durgin@synacor.com <1226643+RBDurgin@users.noreply.github.com>
2020-03-19 13:59:01 -0400

    ZCS-8832 - Previously GetAggregateQuotaUsageOnServer was Loading all accounts from a server into a single array list before summing up quota usage for accounts, which was causing memory overhead and requests to time out. Process has changed to do the following 1. Look up domains per server. 2. For each domain, invoke searchAccountsOnServer, with a custom visitor that increments quota usage inside the visit() method. Also updated the account search options to only return the needed fields: zimbraId and domainId.
    ZCS-8832 Fix license block.

--- store/src/java/com/zimbra/cs/service/admin/GetAggregateQuotaUsageOnServer.java
+++ store/src/java/com/zimbra/cs/service/admin/GetAggregateQuotaUsageOnServer.java
@@ -1,20 +1,28 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
+ *
  * Zimbra Collaboration Suite Server
+ *
  * Copyright (C) 2011, 2013, 2014, 2016 Synacor, Inc.
  *
  * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software Foundation,
- * version 2 of the License.
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 2 of the License.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License along with this program.
- * If not, see <https://www.gnu.org/licenses/>.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
  * ***** END LICENSE BLOCK *****
  */
 package com.zimbra.cs.service.admin;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.AdminConstants;
@@ -30,10 +38,6 @@ import com.zimbra.cs.account.accesscontrol.AccessControlUtil;
 import com.zimbra.cs.mailbox.MailboxManager;
 import com.zimbra.soap.ZimbraSoapContext;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class GetAggregateQuotaUsageOnServer extends AdminDocumentHandler {
 
     public Element handle(Element request, Map<String, Object> context) throws ServiceException {
@@ -42,40 +46,90 @@ public class GetAggregateQuotaUsageOnServer extends AdminDocumentHandler {
             throw ServiceException.PERM_DENIED("only global admin is allowed");
         }
 
-        Map<String, Long> domainAggrQuotaUsed = new HashMap<String, Long>();
+        final Map<String, Long> domainAggrQuotaUsed = new HashMap<String, Long>();
 
         SearchAccountsOptions searchOpts = new SearchAccountsOptions();
         searchOpts.setIncludeType(IncludeType.ACCOUNTS_ONLY);
         searchOpts.setMakeObjectOpt(MakeObjectOpt.NO_DEFAULTS);
 
+        final String[] returnAttrs = { Provisioning.A_zimbraId, Provisioning.A_zimbraDomainId };
+        searchOpts.setReturnAttrs(returnAttrs);
+
         Provisioning prov = Provisioning.getInstance();
-        List<NamedEntry> accounts = prov.searchAccountsOnServer(prov.getLocalServer(), searchOpts);
-        Map<String, Long> acctQuotaUsed = MailboxManager.getInstance().getMailboxSizes(accounts);
-        for (NamedEntry ne : accounts) {
-            if (!(ne instanceof Account)) {
-                continue;
-            }
-            Account acct = (Account) ne;
-            Long acctQuota = acctQuotaUsed.get(acct.getId());
-            if (acctQuota == null) {
-                acctQuota = 0L;
-            }
-            String domainId = acct.getDomainId();
-            if (domainId == null) {
-                continue;
-            }
-            Long aggrQuota = domainAggrQuotaUsed.get(domainId);
-            domainAggrQuotaUsed.put(domainId, aggrQuota == null ? acctQuota : aggrQuota + acctQuota);
+        final List<Domain> domains = prov.getAllDomains();
+
+        for (Domain domain : domains) {
+            searchOpts = new SearchAccountsOptions(domain);
+            searchOpts.setIncludeType(IncludeType.ACCOUNTS_ONLY);
+            searchOpts.setMakeObjectOpt(MakeObjectOpt.NO_DEFAULTS);
+            searchOpts.setReturnAttrs(returnAttrs);
+            searchOpts.setDomain(domain);
+            DomainQuotaVisitor visitor = new DomainQuotaVisitor();
+            prov.searchAccountsOnServer(prov.getLocalServer(), searchOpts, visitor);
+
+            final Long quotaUsed = visitor.getQuotaUsed();
+            domainAggrQuotaUsed.put(domain.getId(), quotaUsed);
         }
 
-        Element response = zsc.createElement(AdminConstants.GET_AGGR_QUOTA_USAGE_ON_SERVER_RESPONSE);
+        final Element response = zsc.createElement(AdminConstants.GET_AGGR_QUOTA_USAGE_ON_SERVER_RESPONSE);
         for (String domainId : domainAggrQuotaUsed.keySet()) {
-            Domain domain = prov.getDomainById(domainId);
-            Element domainElt = response.addElement(AdminConstants.E_DOMAIN);
+            final Domain domain = prov.getDomainById(domainId);
+            final Element domainElt = response.addNonUniqueElement(AdminConstants.E_DOMAIN);
             domainElt.addAttribute(AdminConstants.A_NAME, domain.getName());
             domainElt.addAttribute(AdminConstants.A_ID, domainId);
             domainElt.addAttribute(AdminConstants.A_QUOTA_USED, domainAggrQuotaUsed.get(domainId));
         }
+
         return response;
+    }
+
+    public static class DomainQuotaVisitor implements NamedEntry.Visitor {
+
+        private static final int DEFAULT_BATCH_SIZE = 250;
+
+        private final List<NamedEntry> accounts = new ArrayList<NamedEntry>();
+        private final int batchSize;
+        private Long quotaUsed = 0L;
+        private int count = 0;
+
+        public DomainQuotaVisitor() {
+            this.batchSize = DEFAULT_BATCH_SIZE;
+        }
+
+        public DomainQuotaVisitor(int batchSize) {
+            this.batchSize = batchSize;
+        }
+
+        @Override
+        public void visit(NamedEntry entry) throws ServiceException {
+            if (!(entry instanceof Account)) {
+                return;
+            }
+
+            this.accounts.add(entry);
+
+            count++;
+            if (count % batchSize == 0) {
+
+                final Map<String, Long> batchAcctQuotaUsed = MailboxManager.getInstance().getMailboxSizes(accounts);
+
+                for (NamedEntry ne : accounts) {
+                    final Account acct = (Account) ne;
+                    Long acctQuota = batchAcctQuotaUsed.get(acct.getId());
+                    if (acctQuota == null) {
+                        acctQuota = 0L;
+                    }
+
+                    this.quotaUsed += acctQuota;
+                }
+
+                this.accounts.clear();
+            }
+        }
+
+        public Long getQuotaUsed() {
+            return quotaUsed;
+        }
+
     }
 }
