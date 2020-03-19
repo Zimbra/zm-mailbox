083bffacd97c887892d7f33ec7798f488ee404c6
Vishnu <vishnukant.jangid@synacor.com>
2020-03-20 02:09:06 +0530

    NICPS-738: Changes for VC response

---
+++ soap/src/java/com/zimbra/soap/account/message/GetVCMeetingInfoRequest.java
@@ -0,0 +1,65 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011, 2012, 2013, 2014, 2016 Synacor, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.soap.account.message;
+
+import com.google.common.base.MoreObjects;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.zimbra.common.soap.AccountConstants;
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.soap.type.AccountSelector;
+
+/**
+ * @zm-api-command-auth-required true
+ * @zm-api-command-admin-auth-required false
+ * @zm-api-command-description Get Information about an account
+ */
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name=AccountConstants.E_GET_VC_MEETING_INFO_REQUEST)
+public class GetVCMeetingInfoRequest {
+
+    /**
+     * @zm-api-field-description Use to identify the account
+     */
+    @XmlElement(name=AccountConstants.E_ACCOUNT, required=true)
+    private final AccountSelector account;
+
+    /**
+     * no-argument constructor wanted by JAXB
+     */
+    @SuppressWarnings("unused")
+    private GetVCMeetingInfoRequest() {
+        this((AccountSelector) null);
+    }
+
+    public GetVCMeetingInfoRequest(AccountSelector account) {
+        this.account = account;
+    }
+
+    public AccountSelector getAccount() { return account; }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .add("account", account)
+            .toString();
+    }
+}
---
+++ soap/src/java/com/zimbra/soap/account/message/GetVCMeetingInfoResponse.java
@@ -0,0 +1,78 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011, 2012, 2013, 2014, 2015, 2016 Synacor, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.soap.account.message;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import com.google.common.base.MoreObjects;
+import com.zimbra.common.soap.AccountConstants;
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.soap.json.jackson.annotate.ZimbraJsonAttribute;
+
+/**
+ * @zm-api-response-description Provides a limited amount of information about the requested account.
+ * <br />
+ * Note: there are some minor differences between the Admin and Account versions of GetAccountInfoResponse.
+ */
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name=AccountConstants.E_GET_VC_MEETING_INFO_RESPONSE)
+@XmlType(propOrder = {"vcResponse"})
+public class GetVCMeetingInfoResponse {
+
+    
+    /**
+     * @zm-api-field-description Response for vc meeting. e.g.:
+     * <pre>
+     *     http://server:7070/service/soap/
+     * </pre>
+     */
+    @XmlElement(name=AccountConstants.VC_RESPONSE /* vcResponse */, required=true) 
+    @ZimbraJsonAttribute
+    private String vcResponse;
+
+    public String getVcResponse() {
+		return vcResponse;
+	}
+
+
+	public void setVcResponse(String vcResponse) {
+		this.vcResponse = vcResponse;
+	}
+
+	/**
+     * no-argument constructor wanted by JAXB
+     */
+    @SuppressWarnings("unused")
+    public GetVCMeetingInfoResponse() {
+    }
+
+    
+    public MoreObjects.ToStringHelper addToStringInfo(MoreObjects.ToStringHelper helper) {
+        return helper
+            .add("vcResponse", vcResponse);
+    }
+
+    @Override
+    public String toString() {
+        return addToStringInfo(MoreObjects.toStringHelper(this)).toString();
+    }
+}
