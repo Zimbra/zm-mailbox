1fe42d3d7f5a0ec0b2f7951e10062869ca604510
Jyotiranjan <jrjena136@gmail.com>
2020-03-27 18:43:39 +0530

    zcs-8835: Mark ResumeDeviceRequest as deprecated and create new API AllowDeviceRequest

--- soap/src/java/com/zimbra/soap/admin/message/AllowDeviceRequest.java
+++ soap/src/java/com/zimbra/soap/admin/message/AllowDeviceRequest.java
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2012, 2013, 2014, 2016, 2020 Synacor, Inc.
+ * Copyright (C) 2020 Synacor, Inc.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
@@ -30,37 +30,36 @@ import com.zimbra.soap.admin.type.DeviceId;
 import com.zimbra.soap.type.AccountSelector;
 
 /**
- * @zm-api-command-deprecation-info Note: <b>ResumeDeviceRequest</b> is deprecated. See <b>AllowDeviceRequest</b>.
  * @zm-api-command-auth-required true
  * @zm-api-command-admin-auth-required true
- * @zm-api-command-description Resume sync with a device or all devices attached to an account if currently suspended.
+ * @zm-api-command-description Allow sync with a device or all devices attached to an account if currently suspended.
  * This will cause a policy reset, but will not reset sync data.
  */
 @XmlAccessorType(XmlAccessType.NONE)
-@XmlRootElement(name=SyncAdminConstants.E_RESUME_DEVICE_REQUEST)
-public class ResumeDeviceRequest {
+@XmlRootElement(name=SyncAdminConstants.E_ALLOW_DEVICE_REQUEST)
+public class AllowDeviceRequest {
 
     /**
      * @zm-api-field-description Account selector
      */
-    @XmlElement(name=AdminConstants.E_ACCOUNT, required=true)
+    @XmlElement(name=AdminConstants.E_ACCOUNT, required=false)
     private AccountSelector account;
 
     /**
      * @zm-api-field-tag device-id
      * @zm-api-field-description Device ID
      */
-    @XmlElement(name=SyncConstants.E_DEVICE, required=false)
+    @XmlElement(name=SyncConstants.E_DEVICE, required=true)
     private DeviceId deviceId;
 
     /**
      * no-argument constructor wanted by JAXB
      */
     @SuppressWarnings("unused")
-    private ResumeDeviceRequest() {
+    private AllowDeviceRequest() {
     }
 
-    public ResumeDeviceRequest(AccountSelector account) {
+    public AllowDeviceRequest(AccountSelector account) {
         this.account = account;
     }
 
--- soap/src/java/com/zimbra/soap/sync/message/AllowDeviceResponse.java
+++ soap/src/java/com/zimbra/soap/sync/message/AllowDeviceResponse.java
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2011, 2012, 2013, 2014, 2016, 2020 Synacor, Inc.
+ * Copyright (C) 2020 Synacor, Inc.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
@@ -14,7 +14,6 @@
  * If not, see <https://www.gnu.org/licenses/>.
  * ***** END LICENSE BLOCK *****
  */
-
 package com.zimbra.soap.sync.message;
 
 import com.google.common.base.MoreObjects;
@@ -24,39 +23,27 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import com.zimbra.common.soap.SyncConstants;
-import com.zimbra.soap.sync.type.DeviceId;
+import com.zimbra.soap.sync.type.DeviceStatusInfo;
 
-/**
- * @zm-api-command-deprecation-info Note: <b>ResumeDeviceRequest</b> is deprecated. See <b>AllowDeviceRequest</b>.
- * @zm-api-command-network-edition
- * @zm-api-command-auth-required true
- * @zm-api-command-admin-auth-required false
- * @zm-api-command-description Resume sync with a device if currently suspended.
- * This will cause a policy reset, but will not reset sync data.
- */
 @XmlAccessorType(XmlAccessType.NONE)
-@XmlRootElement(name=SyncConstants.E_RESUME_DEVICE_REQUEST)
-public class ResumeDeviceRequest {
+@XmlRootElement(name=SyncConstants.E_ALLOW_DEVICE_RESPONSE)
+public class AllowDeviceResponse {
 
     /**
-     * @zm-api-field-description Specify the device to resume
+     * @zm-api-field-description Device status information
      */
-    @XmlElement(name=SyncConstants.E_DEVICE /* device */, required=true)
-    private final DeviceId device;
+    @XmlElement(name=SyncConstants.E_DEVICE /* device */, required=false)
+    private DeviceStatusInfo device;
 
-    /**
-     * no-argument constructor wanted by JAXB
-     */
-    @SuppressWarnings("unused")
-    private ResumeDeviceRequest() {
-        this((DeviceId) null);
-    }
+    public AllowDeviceResponse() {}
 
-    public ResumeDeviceRequest(DeviceId device) {
+    public void setDevice(DeviceStatusInfo device) {
         this.device = device;
     }
 
-    public DeviceId getDevice() { return device; }
+    public DeviceStatusInfo getDevice() {
+        return device;
+    }
 
     public MoreObjects.ToStringHelper addToStringInfo(MoreObjects.ToStringHelper helper) {
         return helper.add("device", device);
