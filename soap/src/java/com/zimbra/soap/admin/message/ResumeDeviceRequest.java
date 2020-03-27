adbfef56187a84804a5365b24e0160c87361e589
Jyotiranjan <jrjena136@gmail.com>
2020-03-27 16:02:49 +0530

    Revert "zcs-8835:rename ResumeDeviceRequest"
    This reverts commit 6bdd51eb48ea70b27db9b0064687c70c7978e9e9.

--- soap/src/java/com/zimbra/soap/admin/message/ResumeDeviceRequest.java
+++ soap/src/java/com/zimbra/soap/admin/message/ResumeDeviceRequest.java
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2012, 2013, 2014, 2016, 2020 Synacor, Inc.
+ * Copyright (C) 2012, 2013, 2014, 2016 Synacor, Inc.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
@@ -36,30 +36,30 @@ import com.zimbra.soap.type.AccountSelector;
  * This will cause a policy reset, but will not reset sync data.
  */
 @XmlAccessorType(XmlAccessType.NONE)
-@XmlRootElement(name=SyncAdminConstants.E_ALLOW_DEVICE_REQUEST)
-public class AllowDeviceRequest {
+@XmlRootElement(name=SyncAdminConstants.E_RESUME_DEVICE_REQUEST)
+public class ResumeDeviceRequest {
 
     /**
      * @zm-api-field-description Account selector
      */
-    @XmlElement(name=AdminConstants.E_ACCOUNT, required=false)
+    @XmlElement(name=AdminConstants.E_ACCOUNT, required=true)
     private AccountSelector account;
 
     /**
      * @zm-api-field-tag device-id
      * @zm-api-field-description Device ID
      */
-    @XmlElement(name=SyncConstants.E_DEVICE, required=true)
+    @XmlElement(name=SyncConstants.E_DEVICE, required=false)
     private DeviceId deviceId;
 
     /**
      * no-argument constructor wanted by JAXB
      */
     @SuppressWarnings("unused")
-    private AllowDeviceRequest() {
+    private ResumeDeviceRequest() {
     }
 
-    public AllowDeviceRequest(AccountSelector account) {
+    public ResumeDeviceRequest(AccountSelector account) {
         this.account = account;
     }
 
--- soap/src/java/com/zimbra/soap/admin/message/ResumeDeviceResponse.java
+++ soap/src/java/com/zimbra/soap/admin/message/ResumeDeviceResponse.java
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2011, 2012, 2013, 2014, 2016, 2020 Synacor, Inc.
+ * Copyright (C) 2012, 2013, 2014, 2016 Synacor, Inc.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
@@ -15,35 +15,55 @@
  * ***** END LICENSE BLOCK *****
  */
 
-package com.zimbra.soap.sync.message;
+package com.zimbra.soap.admin.message;
 
-import com.google.common.base.MoreObjects;
+import java.util.Collections;
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.zimbra.common.soap.SyncAdminConstants;
 import com.zimbra.common.soap.SyncConstants;
-import com.zimbra.soap.sync.type.DeviceStatusInfo;
+import com.zimbra.soap.admin.type.DeviceStatusInfo;
 
 @XmlAccessorType(XmlAccessType.NONE)
-@XmlRootElement(name=SyncConstants.E_ALLOW_DEVICE_RESPONSE)
-public class AllowDeviceResponse {
+@XmlRootElement(name=SyncAdminConstants.E_RESUME_DEVICE_RESPONSE)
+@XmlType(propOrder = {})
+public class ResumeDeviceResponse {
 
     /**
-     * @zm-api-field-description Device status information
+     * @zm-api-field-description Information about device status
      */
     @XmlElement(name=SyncConstants.E_DEVICE /* device */, required=false)
-    private DeviceStatusInfo device;
+    private List<DeviceStatusInfo> devices = Lists.newArrayList();
 
-    public AllowDeviceResponse() {
+    public ResumeDeviceResponse() {
     }
 
-    public void setDevice(DeviceStatusInfo device) { this.device = device; }
-    public DeviceStatusInfo getDevice() { return device; }
+    public void setDevices(Iterable<DeviceStatusInfo> devices) {
+        this.devices.clear();
+        if (devices != null) {
+            Iterables.addAll(this.devices, devices);
+        }
+    }
+
+    public void addDevice(DeviceStatusInfo device) {
+        this.devices.add(device);
+    }
+
+    public List<DeviceStatusInfo> getDevices() {
+        return Collections.unmodifiableList(devices);
+    }
 
     public MoreObjects.ToStringHelper addToStringInfo(MoreObjects.ToStringHelper helper) {
-        return helper.add("device", device);
+        return helper.add("devices", devices);
     }
 
     @Override
