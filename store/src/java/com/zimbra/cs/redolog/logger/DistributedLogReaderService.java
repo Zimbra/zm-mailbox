89d0f4b955add72ac58f27405009f31a6238abe5
Shashi Kant <shashikantsantosh@gmail.com>
2020-04-23 17:41:24 +0530

    ZCS-9027: Implementation for DistributedLogReaderService

---
+++ store/src/java/com/zimbra/cs/redolog/logger/DistributedLogReaderService.java
@@ -0,0 +1,70 @@
+package com.zimbra.cs.redolog.logger;
+
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+import org.redisson.api.RStream;
+import org.redisson.api.RedissonClient;
+import org.redisson.api.StreamMessageId;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.zimbra.common.localconfig.LC;
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.mailbox.RedissonClientHolder;
+
+public class DistributedLogReaderService {
+    private static RedissonClient client;
+    private RStream<String, String> stream;
+    private ExecutorService executorService;
+    private static String group;
+    private static String consumer;
+    private volatile boolean running = false;
+    private static DistributedLogReaderService instance = null;
+
+    private DistributedLogReaderService() {}
+
+    public static DistributedLogReaderService getInstance() {
+        if (instance == null) {
+            synchronized(DistributedLogReaderService.class) {
+                if (instance == null) {
+                    instance = new DistributedLogReaderService();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public synchronized void startUp() {
+        running = true;
+        group = LC.redis_streams_redo_log_group.value();
+        consumer = "log-reader-c1";
+        client = RedissonClientHolder.getInstance().getRedissonClient();
+        stream = client.getStream(LC.redis_streams_redo_log_stream.value());
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("DistributedLog-Reader-Thread-%d").build();
+        executorService = Executors.newSingleThreadExecutor(namedThreadFactory);
+        executorService.execute(new LogMonitor());
+    }
+
+    class LogMonitor implements Runnable {
+        @Override
+        public void run() {
+            ZimbraLog.redolog.info("Started Log queue monitoring thread %s", Thread.currentThread().getName());
+            while (running) {
+                ZimbraLog.redolog.debug("Iterating %s", Thread.currentThread().getName());
+                Map<StreamMessageId, Map<String, String>> logs = stream.readGroup(group, consumer,1, 0, TimeUnit.SECONDS, StreamMessageId.NEVER_DELIVERED);
+                for(Map.Entry<StreamMessageId, Map<String, String>> m:logs.entrySet()){  
+                    stream.ack(group, (StreamMessageId) m.getKey());
+                    Map<String, String> fields = (Map<String, String>) m.getValue();
+
+                    for(Map.Entry<String, String> m1:fields.entrySet()){ 
+                        ZimbraLog.redolog.debug("Key:%s Value:%s", m1.getKey(), m1.getValue());
+                    }
+                }
+            }
+        }
+        
+    }
+}
