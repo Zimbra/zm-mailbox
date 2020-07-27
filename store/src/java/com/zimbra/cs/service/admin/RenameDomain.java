06be15c1391d6bbf4a7cb08aabe26b17de0d79e0
Prashant Surana <p.k.surana@gmail.com>
2020-07-27 19:57:10 +0530

    Updated the response.

--- store/src/java/com/zimbra/cs/service/admin/RenameDomain.java
+++ store/src/java/com/zimbra/cs/service/admin/RenameDomain.java
@@ -2,17 +2,17 @@ package com.zimbra.cs.service.admin;
 
 import java.util.Map;
 
+import com.zimbra.common.account.Key.DomainBy;
 import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.soap.AdminConstants;
 import com.zimbra.common.soap.Element;
 import com.zimbra.cs.account.AccountServiceException;
 import com.zimbra.cs.account.Domain;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.ldap.LdapProv;
 import com.zimbra.cs.account.ldap.LdapProvisioning;
-import com.zimbra.soap.JaxbUtil;
 import com.zimbra.soap.ZimbraSoapContext;
 import com.zimbra.soap.admin.message.RenameDomainRequest;
-import com.zimbra.soap.admin.message.RenameDomainResponse;
 
 public class RenameDomain extends AdminDocumentHandler {
 
@@ -32,7 +32,9 @@ public class RenameDomain extends AdminDocumentHandler {
             throw ServiceException.PERM_DENIED("can not access domain, domain is in " + domain.getDomainStatusAsString() + " status");
         LdapProv lp = LdapProvisioning.getInst();
         lp.renameDomain(domain.getId(), req.getName());
-        RenameDomainResponse res = new RenameDomainResponse();
-        return JaxbUtil.jaxbToElement(res);
+        Element response = zsc.createElement(AdminConstants.E_RENAME_DOMAIN_RESPONSE);
+        domain = lp.getDomain(DomainBy.id, domain.getId(), false);
+        GetDomain.encodeDomain(response, domain);
+        return response;
     }
 }
