5af77b835ce553f419f94e52a11eff43263a4d6d
Jyotiranjan <jrjena136@gmail.com>
2020-08-05 12:46:51 +0530

    ZBUG-1555:Can not reset the Two factor setup (zimbraTwoFactorAuthSecret) from trusted device clients

--- store/src/java/com/zimbra/cs/service/admin/ModifyAccount.java
+++ store/src/java/com/zimbra/cs/service/admin/ModifyAccount.java
@@ -227,6 +227,20 @@ public class ModifyAccount extends AdminDocumentHandler {
         try {
             // pass in true to checkImmutable
             prov.modifyAttrs(account, attrs, true);
+
+            // If request is for `zimbraTwoFactorAuthEnabled = FALSE` and both zimbraFeatureTwoFactorAuthAvailable and zimbraFeatureTwoFactorAuthRequired is set to TRUE
+            // for the user, clear all the trusted devices so that the user would re-setup 2FA in the next login after LDAP updation is successful.
+            if (attrs.containsKey(Provisioning.A_zimbraTwoFactorAuthEnabled)) {
+                String value = (String) attrs.get(Provisioning.A_zimbraTwoFactorAuthEnabled);
+                if (!StringUtil.isNullOrEmpty(value) && value.equalsIgnoreCase("false") && account.isFeatureTwoFactorAuthAvailable() && account.isFeatureTwoFactorAuthRequired()) {
+                    String []trustedDevices = account.getTwoFactorAuthTrustedDevices();
+                    if (trustedDevices != null && trustedDevices.length > 0) {
+                        for (String encoded : trustedDevices) {
+                            account.removeTwoFactorAuthTrustedDevices(encoded);
+                        }
+                    }
+                }
+            }
         } catch (ServiceException se) {
             if (rollbackOnFailure) {
                 ZimbraLog.account.debug("Exception occured while modifying account in zimbra for %s, roll back listener updates.", account.getMail());
