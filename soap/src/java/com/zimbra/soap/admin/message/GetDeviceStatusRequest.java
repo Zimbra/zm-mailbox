c3771956bf8610db5780d46245984d5436b9da18
Chinmay Pathak <15.chinmay@gmail.com>
2020-10-22 18:42:17 +0530

    ZCS-9960 Update ABQ API to have Pagination and Server side filtering

--- soap/src/java/com/zimbra/soap/admin/message/GetDeviceStatusRequest.java
+++ soap/src/java/com/zimbra/soap/admin/message/GetDeviceStatusRequest.java
@@ -17,10 +17,10 @@
 
 package com.zimbra.soap.admin.message;
 
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import com.google.common.base.MoreObjects;
 import com.zimbra.common.soap.AdminConstants;
 import com.zimbra.common.soap.SyncAdminConstants;
 import com.zimbra.common.soap.SyncConstants;
@@ -37,16 +37,32 @@ import com.zimbra.soap.type.AccountSelector;
 public class GetDeviceStatusRequest {
 
     /**
+     * @zm-api-field-tag offset
+     * @zm-api-field-description Offset for the resultant list of the devices. Offset and Limit both should be sent together in request.
+     */
+    @XmlAttribute(name=SyncConstants.A_OFFSET /* offset */, required=false)
+    private int offset = 0;
+
+    /**
+     * @zm-api-field-tag limit
+     * @zm-api-field-description Number of the devices you want in the resultant list of the devices. Offset and Limit both should be sent together in request.
+     */
+    @XmlAttribute(name=SyncConstants.A_LIMIT /* limit */, required=false)
+    private int limit = 0;
+
+    /**
+     * @zm-api-field-tag account
      * @zm-api-field-description Account
      */
-    @XmlElement(name=AdminConstants.E_ACCOUNT, required = false)
+    @XmlElement(name=AdminConstants.E_ACCOUNT /* account */, required = false)
     private AccountSelector account;
 
     /**
+     * @zm-api-field-tag device
      * @zm-api-field-description Device id
      */
-    @XmlElement(name = SyncConstants.E_DEVICE, required = false)
-    private DeviceId deviceId;
+    @XmlElement(name = SyncConstants.E_DEVICE /* device */, required = false)
+    private DeviceId device;
 
     /**
      * @zm-api-field-tag device-status
@@ -54,6 +70,34 @@ public class GetDeviceStatusRequest {
      */
     @XmlElement(name=SyncConstants.E_STATUS /* status */, required = false)
     private Byte status;
+
+    /**
+     * @zm-api-field-tag device-name
+     * @zm-api-field-description Device name
+     */
+    @XmlElement(name=SyncConstants.E_DEVICE_NAME /* deviceName */, required = false)
+    private String deviceName;
+
+    /**
+     * @zm-api-field-tag device-type
+     * @zm-api-field-description Device type
+     */
+    @XmlElement(name=SyncConstants.E_DEVICE_TYPE /* deviceType */, required = false)
+    private String deviceType;
+
+    /**
+     * @zm-api-field-tag device-last-used
+     * @zm-api-field-description Device last used
+     */
+    @XmlElement(name=SyncConstants.E_DEVICE_LAST_USED /* deviceLastUsed */, required = false)
+    private String deviceLastUsed;
+
+    /**
+     * @zm-api-field-tag device-sync-version
+     * @zm-api-field-description Device sync version
+     */
+    @XmlElement(name=SyncConstants.E_DEVICE_SYNC_VERSION /* deviceSyncVersion */, required = false)
+    private String deviceSyncVersion;
 
     /**
      * no-argument constructor wanted by JAXB
@@ -68,17 +112,53 @@ public class GetDeviceStatusRequest {
     }
 
     public GetDeviceStatusRequest(AccountSelector account, DeviceId deviceId, Byte status) {
+        this(0, 0, account, deviceId, status, null, null, null, null);
+    }
+
+    /**
+     * @param account
+     * @param device
+     * @param status
+     * @param deviceName
+     * @param deviceType
+     * @param deviceLastUsed
+     * @param deviceSyncVersion
+     */
+    public GetDeviceStatusRequest(int offset, int limit, AccountSelector account, DeviceId device, Byte status, String deviceName,
+            String deviceType, String deviceLastUsed, String deviceSyncVersion) {
+        this.offset = offset;
+        this.limit = limit;
         this.account = account;
-        this.deviceId = deviceId;
+        this.device = device;
         this.status = status;
+        this.deviceName = deviceName;
+        this.deviceType = deviceType;
+        this.deviceLastUsed = deviceLastUsed;
+        this.deviceSyncVersion = deviceSyncVersion;
     }
 
-    public DeviceId getDeviceId() {
-        return this.deviceId;
+    public int getOffset() {
+        return offset;
     }
 
-    public void setDeviceId(DeviceId deviceId) {
-        this.deviceId = deviceId;
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public DeviceId getDevice() {
+        return device;
+    }
+
+    public void setDevice(DeviceId device) {
+        this.device = device;
     }
 
     public AccountSelector getAccount() {
@@ -97,8 +177,42 @@ public class GetDeviceStatusRequest {
         this.status = status;
     }
 
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public String getDeviceLastUsed() {
+        return deviceLastUsed;
+    }
+
+    public void setDeviceLastUsed(String deviceLastUsed) {
+        this.deviceLastUsed = deviceLastUsed;
+    }
+
+    public String getDeviceSyncVersion() {
+        return deviceSyncVersion;
+    }
+
+    public void setDeviceSyncVersion(String deviceSyncVersion) {
+        this.deviceSyncVersion = deviceSyncVersion;
+    }
+
     @Override
     public String toString() {
-        return MoreObjects.toStringHelper(this).add("account", this.account).add("device", this.deviceId).add("status", this.status).toString();
+        return "GetDeviceStatusRequest [account=" + account + ", device=" + device + ", status=" + status
+                + ", deviceName=" + deviceName + ", deviceType=" + deviceType + ", deviceLastUsed=" + deviceLastUsed
+                + ", deviceSyncVersion=" + deviceSyncVersion + "]";
     }
 }
