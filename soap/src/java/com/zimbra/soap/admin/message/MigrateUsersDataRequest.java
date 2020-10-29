c3f0d9e69f95ee7bb962498479cdb9c58060e660
Jyotiranjan <jrjena136@gmail.com>
2020-10-29 16:52:41 +0530

    ZCS-9805: Added request and response parameters

--- soap/src/java/com/zimbra/soap/admin/message/MigrateUsersDataRequest.java
+++ soap/src/java/com/zimbra/soap/admin/message/MigrateUsersDataRequest.java
@@ -1,5 +1,40 @@
 package com.zimbra.soap.admin.message;
 
+import java.util.Collections;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.zimbra.common.soap.AdminConstants;
+import com.zimbra.soap.admin.type.MigrationInfo;
+
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name = AdminConstants.E_MIGRATE_USERS_DATA_REQUEST)
 public class MigrateUsersDataRequest {
 
+    @XmlElement(name = "migrate", required = true)
+    private List<MigrationInfo> migrate = Lists.newArrayList();
+
+    public MigrateUsersDataRequest() {
+    }
+
+    public void setMigrationInfo(Iterable <MigrationInfo> migrationInfo) {
+        this.migrate.clear();
+        if (migrationInfo != null) {
+            Iterables.addAll(this.migrate, migrationInfo);
+        }
+    }
+
+    public void addMigrationInfo(MigrationInfo migrationInfo) {
+        this.migrate.add(migrationInfo);
+    }
+
+    public List <MigrationInfo> getMigrationList() {
+        return Collections.unmodifiableList(migrate);
+    }
 }
--- soap/src/java/com/zimbra/soap/admin/message/MigrateUsersDataResponse.java
+++ soap/src/java/com/zimbra/soap/admin/message/MigrateUsersDataResponse.java
@@ -1,5 +1,33 @@
 package com.zimbra.soap.admin.message;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.zimbra.common.soap.AdminConstants;
+
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name = AdminConstants.E_MIGRATE_USERS_DATA_RESPONSE)
 public class MigrateUsersDataResponse {
+
+    @XmlAttribute(name = "migId", required = false)
+    private String id;
+
+    public MigrateUsersDataResponse() {
+        this(null);
+    }
+
+    public MigrateUsersDataResponse(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
 }
