1e1b668fbc6d6c3eaca06ff66e2babaaa5a81fa3
Jyotiranjan <jrjena136@gmail.com>
2020-11-10 19:21:58 +0530

    ZCS-9850: removed user password from request

--- soap/src/java/com/zimbra/soap/admin/type/MigrationInfo.java
+++ soap/src/java/com/zimbra/soap/admin/type/MigrationInfo.java
@@ -16,9 +16,6 @@ public class MigrationInfo {
     @XmlAttribute(name = AdminConstants.A_SOURCE_USER, required = true)
     private String sourceUser;
 
-    @XmlAttribute(name = AdminConstants.A_SOURCE_USER_PASSWORD, required = false)
-    private String sourceUserPassword;
-
     /**
      * @zm-api-field-tag targetUser
      * @zm-api-field-description Target system user account to where data will be migrated
@@ -35,12 +32,11 @@ public class MigrationInfo {
     private String typesOfData;
 
     public MigrationInfo() {
-        this ((String) null, (String) null, (String) null, (String) null);
+        this ((String) null, (String) null, (String) null);
     }
 
-    public MigrationInfo(String sUser, String sPass, String tUser, String types) {
+    public MigrationInfo(String sUser, String tUser, String types) {
         this.sourceUser = sUser;
-        this.sourceUserPassword = sPass;
         this.targetUser = tUser;
         this.typesOfData = types;
     }
@@ -51,14 +47,6 @@ public class MigrationInfo {
 
     public void setSourceUser(String sourceUser) {
         this.sourceUser = sourceUser;
-    }
-
-    public String getSourceUserPassword() {
-        return sourceUserPassword;
-    }
-
-    public void setSourceUserPassword(String sourceUserPassword) {
-        this.sourceUserPassword = sourceUserPassword;
     }
 
     public String getTargetUser() {
