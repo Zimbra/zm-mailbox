f4a0a4948610e88c5f3692002f3fd77498120bdd
Yogesh Dasi <yogesh.dasi@synacor.com>
2020-12-21 22:40:56 +0530

    ZCS-10146: adding admin rights and account harvesting check

--- store/src/java/com/zimbra/cs/service/admin/ResetAccountPassword.java
+++ store/src/java/com/zimbra/cs/service/admin/ResetAccountPassword.java
@@ -18,8 +18,14 @@ package com.zimbra.cs.service.admin;
 
 import java.util.Map;
 
+import com.google.common.base.Strings;
+import com.zimbra.common.account.Key.AccountBy;
 import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.soap.AccountConstants;
 import com.zimbra.common.soap.Element;
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.account.accesscontrol.Rights.Admin;
 import com.zimbra.soap.ZimbraSoapContext;
 import com.zimbra.soap.admin.message.ResetAccountPasswordResponse;
 
@@ -29,7 +35,47 @@ public class ResetAccountPassword extends AdminDocumentHandler {
     @Override
     public Element handle(Element request, Map<String, Object> context) throws ServiceException {
         zsc = getZimbraSoapContext(context);
+        Element a = request.getElement(AccountConstants.E_ACCOUNT);
+        String key = a.getAttribute(AccountConstants.A_BY);
+        String value = a.getText();
+
+        if (Strings.isNullOrEmpty(value)) {
+            throw ServiceException.INVALID_REQUEST(
+                "no text specified for the " + AccountConstants.E_ACCOUNT + " element", null);
+        }
+        Provisioning prov = Provisioning.getInstance();
+        Account account = prov.get(AccountBy.fromString(key), value, zsc.getAuthToken());
+
+        // prevent directory harvest attack, mask no such account as permission denied
+        if (account == null)
+            throw ServiceException.PERM_DENIED("can not access account");
+
+        checkAccountRights(zsc, account);
+
         ResetAccountPasswordResponse response = new ResetAccountPasswordResponse();
         return zsc.jaxbToElement(response);
+    }
+
+    /*
+     * returns whether password strength policies should be enforced for the authed user
+     *
+     * returns false if user can setAccountPassword
+     * returns true if user cannot setAccountPassword but can changeAccountPassword
+     *
+     * throws PERM_DENIED if user doesn't have either right
+     */
+    private boolean checkAccountRights(ZimbraSoapContext zsc, Account acct)
+            throws ServiceException {
+        try {
+            checkAccountRight(zsc, acct, Admin.R_setAccountPassword);
+            return false;
+        } catch (ServiceException e) {
+            if (ServiceException.PERM_DENIED.equals(e.getCode())) {
+                checkAccountRight(zsc, acct, Admin.R_changeAccountPassword);
+                return true;
+            } else {
+                throw e;
+            }
+        }
     }
 }
