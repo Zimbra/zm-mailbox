7d9cf189cafa2721ae0f84652acddc0be06839f0
log2akshat <akshat-pg8@iiitmk.ac.in>
2021-01-06 00:41:00 +0530

    Fixing ResetPasswordResponse that started sending
    authtoken in soap response in dryRun support implementation.

--- store/src/java/com/zimbra/cs/account/ldap/LdapProvisioning.java
+++ store/src/java/com/zimbra/cs/account/ldap/LdapProvisioning.java
@@ -6261,6 +6261,11 @@ public class LdapProvisioning extends LdapProv implements CacheAwareProvisioning
             (ch >=123 && ch <= 126);  // { | } ~
     }
 
+    public void resetPassword(Account acct, String newPassword, boolean dryRun) 
+            throws ServiceException {
+        setPassword(acct, newPassword, true, dryRun);
+    }
+
     void setPassword(Account acct, String newPassword, boolean enforcePolicy, boolean dryRun)
     throws ServiceException {
 
@@ -6285,7 +6290,6 @@ public class LdapProvisioning extends LdapProv implements CacheAwareProvisioning
         }
 
         Map<String, Object> attrs = new HashMap<String, Object>();
-
         int enforceHistory = acct.getIntAttr(Provisioning.A_zimbraPasswordEnforceHistory, 0);
         if (enforceHistory > 0) {
             String[] newHistory = updateHistory(
@@ -6294,8 +6298,9 @@ public class LdapProvisioning extends LdapProv implements CacheAwareProvisioning
                     enforceHistory);
             attrs.put(Provisioning.A_zimbraPasswordHistory, newHistory);
 
-            if (enforcePolicy || dryRun)
+            if (enforcePolicy || dryRun) {
                 checkHistory(newPassword, newHistory);
+            }
         }
 
         if (dryRun) {
@@ -11576,10 +11581,5 @@ public class LdapProvisioning extends LdapProv implements CacheAwareProvisioning
             LdapClient.closeContext(zlc);
         }
     }
-
-	@Override
-	public void resetPassword(Account acct, String newPassword, boolean dryRun) throws ServiceException {
-		// TODO Auto-generated method stub	
-	}
 
 }
