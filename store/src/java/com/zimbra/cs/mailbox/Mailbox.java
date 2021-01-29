f0f5911ea27202f54741842320f237e1ad5b189c
Prashant Surana <p.k.surana@gmail.com>
2021-01-29 10:24:21 +0530

    ZCS-10237: Invalidate folderTag snapshot cache on RecalculateMailbox

--- store/src/java/com/zimbra/cs/mailbox/Mailbox.java
+++ store/src/java/com/zimbra/cs/mailbox/Mailbox.java
@@ -1810,6 +1810,7 @@ public class Mailbox implements MailboxStore {
     private void clearFolderCache() throws ServiceException {
         try(FolderTagCacheLock lock = ftCacheReadLock.lock()) {
             mFolderCache = null;
+            folderTagSnapshot.invalidate();
         }
         // Remove from memcached cache
         try {
