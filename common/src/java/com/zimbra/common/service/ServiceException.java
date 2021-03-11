f418b05e7fa4f3bca5b8157a2fcdb699f06e8cd2
log2akshat <akshat-pg8@iiitmk.ac.in>
2021-03-12 00:39:23 +0530

    ZCS-8658:Fixed messages to use %s style for strings
    as per java standards.

--- common/src/java/com/zimbra/common/service/ServiceException.java
+++ common/src/java/com/zimbra/common/service/ServiceException.java
@@ -293,7 +293,8 @@ public class ServiceException extends Exception {
     }
 
     public static ServiceException ERROR_WHILE_PARSING_UPLOAD(String message, Throwable cause) {
-        return new ServiceException("ioexception during upload: "+message, ERROR_WHILE_PARSING_UPLOAD, RECEIVERS_FAULT, cause);
+        return new ServiceException(
+                String.format("ioexception during upload: %s", message), ERROR_WHILE_PARSING_UPLOAD, RECEIVERS_FAULT, cause);
     }
 
     /**
@@ -307,14 +308,16 @@ public class ServiceException extends Exception {
      * The request was invalid as datasource with the specified Id is not present.
      */
     public static ServiceException INVALID_DATASOURCE_ID(String message, Throwable cause) {
-        return new ServiceException("wrong datasource id: "+message, INVALID_DATASOURCE_ID, SENDERS_FAULT, cause);
+        return new ServiceException(
+                String.format("wrong datasource id: %s", message), INVALID_DATASOURCE_ID, SENDERS_FAULT, cause);
     }
 
     /**
      * The request was invalid as datasource datasource SMTP is not enabled.
      */
     public static ServiceException DATASOURCE_SMTP_DISABLED(String message, Throwable cause) {
-        return new ServiceException("datasource smtp not enabled: "+message, DATASOURCE_SMTP_DISABLED, SENDERS_FAULT, cause);
+        return new ServiceException(
+                String.format("datasource smtp not enabled: %s", message), DATASOURCE_SMTP_DISABLED, SENDERS_FAULT, cause);
     }
 
     /**
@@ -329,7 +332,8 @@ public class ServiceException extends Exception {
     }
 
     public static ServiceException FIXING_SENDMSG_FOR_SENTBY_PARSE_ERROR(String message, Throwable cause) {
-        return new ServiceException("parse error for SENT-BY: "+message, INTERNAL_ERROR, SENDERS_FAULT, cause);
+        return new ServiceException(
+                String.format("parse error for SENT-BY: %s", message), INTERNAL_ERROR, SENDERS_FAULT, cause);
     }
 
     public static ServiceException RESOURCE_UNREACHABLE(String message, Throwable cause, Argument... arguments) {
