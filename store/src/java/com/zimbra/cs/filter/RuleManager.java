028aa0224e6339d0113a2a5d7a7144c1a0f20567
Fulvio Scapin <fs@zextras.com>
2021-03-14 21:53:57 +0100

    Corrected a couple of incorrect format expansions

--- store/src/java/com/zimbra/cs/filter/RuleManager.java
+++ store/src/java/com/zimbra/cs/filter/RuleManager.java
@@ -634,7 +634,7 @@ public final class RuleManager {
             try {
                 node = parse(script);
             } catch (ParseException e) {
-                ZimbraLog.filter.warn("Unable to update filter rules with new folder path '%s'.", e);
+                ZimbraLog.filter.warn("Unable to update filter rules with new folder path '%s'.", newPath, e);
                 return;
             }
             FolderRenamer renamer = new FolderRenamer(originalPath, newPath);
