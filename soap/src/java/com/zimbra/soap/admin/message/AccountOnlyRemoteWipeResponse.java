fde0f7809c4b8ca473b8974b6a87bbe7069fbbbc
Chinmay Pathak <15.chinmay@gmail.com>
2021-03-15 15:51:09 +0530

    Merge branch 'bugfix/zcs-9628' into develop
    (cherry picked from commit 6bd148320d7ed7bbc0cb2de7ec4fab3751e490e5)

---
+++ soap/src/java/com/zimbra/soap/admin/message/AccountOnlyRemoteWipeResponse.java
@@ -0,0 +1,73 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2021 Synacor, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.soap.admin.message;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.zimbra.common.soap.SyncAdminConstants;
+import com.zimbra.common.soap.SyncConstants;
+import com.zimbra.soap.admin.type.DeviceStatusInfo;
+
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name=SyncAdminConstants.E_ACCOUNT_ONLY_REMOTE_WIPE_RESPONSE)
+@XmlType(propOrder = {})
+public class AccountOnlyRemoteWipeResponse {
+
+    /**
+     * @zm-api-field-description Information about device status
+     */
+    @XmlElement(name=SyncConstants.E_DEVICE /* device */, required=false)
+    private List<DeviceStatusInfo> devices = Lists.newArrayList();
+
+    public AccountOnlyRemoteWipeResponse() {
+    }
+
+    public void setDevices(Iterable<DeviceStatusInfo> devices) {
+        this.devices.clear();
+        if (devices != null) {
+            Iterables.addAll(this.devices, devices);
+        }
+    }
+
+    public void addDevice(DeviceStatusInfo device) {
+        this.devices.add(device);
+    }
+
+    public List<DeviceStatusInfo> getDevices() {
+        return Collections.unmodifiableList(devices);
+    }
+
+    public MoreObjects.ToStringHelper addToStringInfo(MoreObjects.ToStringHelper helper) {
+        return helper.add("devices", devices);
+    }
+
+    @Override
+    public String toString() {
+        return addToStringInfo(MoreObjects.toStringHelper(this)).toString();
+    }
+}
