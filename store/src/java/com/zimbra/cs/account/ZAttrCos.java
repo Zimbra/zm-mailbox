ef705e40affe52e76f2989ce410a44b3a0d31805
log2akshat <akshat-pg8@iiitmk.ac.in>
2021-04-20 22:30:48 +0530

    ZCS-10443: Modified zimbraFileUploadMaxSizePerFile
    ldap attribute to be used for file uploads restrictions
    other than e-mail. Now, the old defaultCOSValue is
    removed as it's value should be less than the ldap
    attribute zimbraFileUploadMaxSize. If the default
    value is not set then upload size is limited by
    zimbraFileUploadMaxSize which is global.
    Also, added accountInfo flag to zimbraFileUploadMaxSizePerFile.

--- store/src/java/com/zimbra/cs/account/ZAttrCos.java
+++ store/src/java/com/zimbra/cs/account/ZAttrCos.java
@@ -16446,19 +16446,23 @@ public abstract class ZAttrCos extends NamedEntry {
     }
 
     /**
-     * Maximum size in bytes for each attachment.
+     * Maximum size in bytes for each attachment for the file uploads. If the
+     * default value is not set then upload size is limited by
+     * zimbraFileUploadMaxSize.
      *
-     * @return zimbraFileUploadMaxSizePerFile, or 2147483648 if unset
+     * @return zimbraFileUploadMaxSizePerFile, or 0 if unset
      *
      * @since ZCS 8.0.0
      */
     @ZAttr(id=1350)
     public long getFileUploadMaxSizePerFile() {
-        return getLongAttr(Provisioning.A_zimbraFileUploadMaxSizePerFile, 2147483648L, true);
+        return getLongAttr(Provisioning.A_zimbraFileUploadMaxSizePerFile, 0L, true);
     }
 
     /**
-     * Maximum size in bytes for each attachment.
+     * Maximum size in bytes for each attachment for the file uploads. If the
+     * default value is not set then upload size is limited by
+     * zimbraFileUploadMaxSize.
      *
      * @param zimbraFileUploadMaxSizePerFile new value
      * @throws com.zimbra.common.service.ServiceException if error during update
@@ -16473,7 +16477,9 @@ public abstract class ZAttrCos extends NamedEntry {
     }
 
     /**
-     * Maximum size in bytes for each attachment.
+     * Maximum size in bytes for each attachment for the file uploads. If the
+     * default value is not set then upload size is limited by
+     * zimbraFileUploadMaxSize.
      *
      * @param zimbraFileUploadMaxSizePerFile new value
      * @param attrs existing map to populate, or null to create a new map
@@ -16489,7 +16495,9 @@ public abstract class ZAttrCos extends NamedEntry {
     }
 
     /**
-     * Maximum size in bytes for each attachment.
+     * Maximum size in bytes for each attachment for the file uploads. If the
+     * default value is not set then upload size is limited by
+     * zimbraFileUploadMaxSize.
      *
      * @throws com.zimbra.common.service.ServiceException if error during update
      *
@@ -16503,7 +16511,9 @@ public abstract class ZAttrCos extends NamedEntry {
     }
 
     /**
-     * Maximum size in bytes for each attachment.
+     * Maximum size in bytes for each attachment for the file uploads. If the
+     * default value is not set then upload size is limited by
+     * zimbraFileUploadMaxSize.
      *
      * @param attrs existing map to populate, or null to create a new map
      * @return populated map to pass into Provisioning.modifyAttrs
