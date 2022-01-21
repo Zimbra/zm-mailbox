d042d99a23d9a0e7bf56cd0c4767c9ce0640ca37
Yogesh Dasi <yogesh.dasi@synacor.com>
2022-01-21 09:59:47 +0530

    Merge branch 'bugfix/ZCS-11145' into develop

--- common/src/java/com/zimbra/common/localconfig/LC.java
+++ common/src/java/com/zimbra/common/localconfig/LC.java
@@ -1438,6 +1438,8 @@ public final class LC {
     @Supported
     public static final KnownKey zimbra_remote_cmd_channel_timeout_min = KnownKey.newKey(10);
 
+    public static final KnownKey invite_ignore_x_alt_description = KnownKey.newKey(true);
+
     static {
         // Automatically set the key name with the variable name.
         for (Field field : LC.class.getFields()) {
