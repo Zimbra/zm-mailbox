be8fe7961db677c72704aa6c9edb1ef242eb151c
rupeshtelus <96474570+rupeshtelus@users.noreply.github.com>
2022-06-09 11:21:32 +0530

    Merge branch bugfix/ZBUG-2547 into develop #1277

--- store/src/java/com/zimbra/cs/html/owasp/OwaspHtmlSanitizer.java
+++ store/src/java/com/zimbra/cs/html/owasp/OwaspHtmlSanitizer.java
@@ -16,6 +16,8 @@
  */
 package com.zimbra.cs.html.owasp;
 
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.concurrent.Callable;
 
 import org.owasp.html.Handler;
@@ -36,9 +38,45 @@ public class OwaspHtmlSanitizer implements Callable<String> {
     private String vHost;
 
     public OwaspHtmlSanitizer(String html, boolean neuterImages, String vHost) {
-        this.html = html;
+        // fix to check open tags & remove
+        this.html = checkUnbalancedTags(html);
         this.neuterImages = neuterImages;
         this.vHost = vHost;
+    }
+    
+    /** 
+     * A method to check & remove unbalanced tags which may have started, not ended
+     * 
+     * @return the Formatted Html after removing unbalanced tags
+     */
+    private String checkUnbalancedTags(String html) {
+        if (StringUtil.isNullOrEmpty(html)) {
+            return html;
+        }
+        ArrayList<String> tags = new ArrayList<String>();
+        // in tags array we can add multiple tags, so for each tag write start &
+        // end with a & separator like(<!--&-->)
+        tags.add("<!--&-->");
+        String formattedHtml = "";
+        for (String str : tags) {
+            String tagArr[] = str.split("&");
+            String start = tagArr[0];
+            String end = tagArr[1];
+            String[] arrOfStr = html.split(start);
+            for (String strAfterSplit : arrOfStr) {
+                 if (!strAfterSplit.equals("") && strAfterSplit.contains(end)) {
+                     formattedHtml = formattedHtml + start + strAfterSplit;
+                 } else {
+                     formattedHtml = formattedHtml + strAfterSplit;
+                 }
+            }
+            html = formattedHtml;
+            formattedHtml = "";
+        }
+        if (html.contains("portal-page-1>div>div{padding-top:5px;}") && !html.split(">div>div")[1].equals("")) {
+            html = html.split(">div>div")[0] + "div>div" + html.split(">div>div")[1];
+        }
+        return html;
     }
 
     private PolicyFactory POLICY_DEFINITION;
