467285e394fe51c44d78a9512cc2fb5da370f51d
Carlos Romero <carlos.romero03@telusinternational.com>
2022-06-22 17:32:10 -0700

    ZCS-11427: Need new SOAP API for Storing S3 config in ldap attribute(global)

---
+++ common/src/java/com/zimbra/common/soap/GlobalExternalStoreConfigConstants.java
@@ -0,0 +1,22 @@
+package com.zimbra.common.soap;
+
+public final class GlobalExternalStoreConfigConstants {
+
+    public static final String E_GLOBAL_S3_BUCKET_CONFIGURATIONS = "global/s3BucketConfigurations";
+    public static final String A_S3_GLOBAL_BUCKET_UUID = "globalBucketUUID";
+    public static final String A_S3_BUCKET_NAME = "bucketName";
+    public static final String A_S3_STORE_PROVIDER = "storeProvider";
+    public static final String A_S3_PROTOCOL = "protocol";
+    public static final String A_S3_STORE_TYPE = "storeType";
+    public static final String A_S3_ACCESS_KEY = "accessKey";
+    public static final String A_S3_SECRET_KEY = "secretKey";
+    public static final String A_S3_REGION = "region";
+    public static final String A_S3_DESTINATION_PATH = "destinationPath";
+    public static final String A_S3_URL = "url";
+    public static final String A_S3_NAMESPACE = "namespace";
+    public static final String A_S3_PROXY_PORT = "proxyPort";
+    public static final String A_S3_ACCOUNT = "account";
+    public static final String A_S3_ACCOUNT_PORT = "accountPort";
+    public static final String A_S3_BUCKET_STATUS = "bucketStatus";
+
+}
