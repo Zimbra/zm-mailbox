74912d6b5e0e3beeb9ea19e8ee568dd71f25483e
Indraneel Patil <indraneel.patil@synacor.com>
2022-06-30 16:53:21 +0530

    ZCS-11428 Modify Volumes SOAP API to support S3 volume type.
    - Introduced external volume info DTO
    - Honoring store_type column while creating volume
    - Modified create/delete/getAllvolumes/getVolume/modifyVolume API's to support S3 volume type

--- store/src/java/com/zimbra/cs/service/admin/GetAllVolumes.java
+++ store/src/java/com/zimbra/cs/service/admin/GetAllVolumes.java
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2005, 2006, 2007, 2009, 2010, 2011, 2013, 2014, 2016 Synacor, Inc.
+ * Copyright (C) 2005, 2006, 2007, 2009, 2010, 2011, 2013, 2014, 2016, 2022 Synacor, Inc.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
@@ -18,18 +18,27 @@ package com.zimbra.cs.service.admin;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.soap.AdminConstants;
+import com.zimbra.common.soap.Element;
+import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.accesscontrol.AdminRight;
 import com.zimbra.cs.account.accesscontrol.Rights.Admin;
 import com.zimbra.cs.volume.Volume;
 import com.zimbra.cs.volume.VolumeManager;
-import com.zimbra.common.service.ServiceException;
-import com.zimbra.common.soap.Element;
 import com.zimbra.soap.JaxbUtil;
 import com.zimbra.soap.ZimbraSoapContext;
 import com.zimbra.soap.admin.message.GetAllVolumesRequest;
 import com.zimbra.soap.admin.message.GetAllVolumesResponse;
+import com.zimbra.soap.admin.type.VolumeInfo;
+import com.zimbra.soap.admin.type.VolumeExternalInfo;
+import com.zimbra.util.ExternalVolumeInfoHandler;
 
 public final class GetAllVolumes extends AdminDocumentHandler {
 
@@ -46,7 +55,36 @@ public final class GetAllVolumes extends AdminDocumentHandler {
 
         GetAllVolumesResponse resp = new GetAllVolumesResponse();
         for (Volume vol : VolumeManager.getInstance().getAllVolumes()) {
-            resp.addVolume(vol.toJAXB());
+            VolumeInfo volInfo = vol.toJAXB();
+
+            if (vol.getStoreType().equals(Volume.StoreType.EXTERNAL)) {
+                VolumeExternalInfo volExtInfo = new VolumeExternalInfo();
+                ExternalVolumeInfoHandler extVolInfoHandler = new ExternalVolumeInfoHandler(Provisioning.getInstance());
+
+                try {
+                    JSONObject properties = extVolInfoHandler.readServerProperties(volInfo.getId());
+                    String volumePrefix = properties.getString(AdminConstants.A_VOLUME_VOLUME_PREFIX);
+                    String globalBucketConfigId = properties.getString(AdminConstants.A_VOLUME_GLB_BUCKET_CONFIG_ID);
+                    String storageType = properties.getString(AdminConstants.A_VOLUME_STORAGE_TYPE);
+                    Boolean useInFrequentAccess = Boolean.valueOf(properties.getString(AdminConstants.A_VOLUME_USE_IN_FREQ_ACCESS));
+                    Boolean useIntelligentTiering = Boolean.valueOf(properties.getString(AdminConstants.A_VOLUME_USE_INTELLIGENT_TIERING));
+                    int useInFrequentAccessThreshold = Integer.parseInt(properties.getString(AdminConstants.A_VOLUME_USE_IN_FREQ_ACCESS_THRESHOLD));
+
+                    volExtInfo.setVolumePrefix(volumePrefix);
+                    volExtInfo.setGlobalBucketConfigurationId(globalBucketConfigId);
+                    volExtInfo.setStorageType(storageType);
+                    volExtInfo.setUseInFrequentAccess(useInFrequentAccess);
+                    volExtInfo.setUseIntelligentTiering(useIntelligentTiering);
+                    volExtInfo.setUseInFrequentAccessThreshold(useInFrequentAccessThreshold);
+                    volInfo.setVolumeExternalInfo(volExtInfo);
+                } catch (ServiceException e) {
+                    ZimbraLog.store.error("Error while processing GetAllVolumesRequest", e);
+                    throw e;
+                } catch (JSONException e) {
+                    throw ServiceException.FAILURE("Error while reading server properties", null);
+                }
+            }
+            resp.addVolume(volInfo);
         }
         return resp;
     }
