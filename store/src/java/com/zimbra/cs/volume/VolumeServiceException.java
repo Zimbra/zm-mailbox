6e01a80383a9c8a3a1f94831c48c8309c177bbb0
Indraneel Patil <indraneel.patil@synacor.com>
2022-06-30 16:53:21 +0530

    ZCS-11428 Modify Volumes SOAP API to support S3 volume type. - Introduced external volume info DTO - Honoring store_type column while creating volume - Modified create/delete/getAllvolumes/getVolume/modifyVolume API's to support S3 volume type
    Co-authored-by: Rakeshkumar More <rakeshkumar.more@PNQM1010-RAMORE.local>

--- store/src/java/com/zimbra/cs/volume/VolumeServiceException.java
+++ store/src/java/com/zimbra/cs/volume/VolumeServiceException.java
@@ -22,19 +22,31 @@ import com.zimbra.common.service.ServiceException;
 public final class VolumeServiceException extends ServiceException {
     private static final long serialVersionUID = -3596326510079311719L;
 
-    public static final String BAD_CURRVOL_CONFIG            = "volume.BAD_CURRVOL_CONFIG";
-    public static final String NO_SUCH_VOLUME                = "volume.NO_SUCH_VOLUME";
-    public static final String NO_SUCH_PATH                  = "volume.NO_SUCH_PATH";
-    public static final String ALREADY_EXISTS                = "volume.ALREADY_EXISTS";
-    public static final String ID_OUT_OF_RANGE               = "volume.ID_OUT_OF_RANGE";
-    public static final String CANNOT_DELETE_VOLUME_IN_USE   = "volume.CANNOT_DELETE_VOLUME_IN_USE";
-    public static final String WRONG_TYPE_CURRVOL            = "volume.WRONG_TYPE_CURRVOL";
-    public static final String CANNOT_DELETE_CURRVOL         = "volume.CANNOT_DELETE_CURRVOL";
-    public static final String CANNOT_CHANGE_TYPE_OF_CURRVOL = "volume.CANNOT_CHANGE_TYPE_OF_CURRVOL";
-    public static final String INVALID_REQUEST               = "volume.INVALID_REQUEST";
-    public static final String NOT_ABSOLUTE_PATH             = "volume.NOT_ABSOLUTE_PATH";
-    public static final String SUBDIR_OF_ANOTHER_VOLUME      = "volume.SUBDIR_OF_ANOTHER_VOLUME";
-    public static final String INVALID_METADATA              = "volume.INVALID_METADATA";
+    public static final String BAD_CURRVOL_CONFIG                           = "volume.BAD_CURRVOL_CONFIG";
+    public static final String NO_SUCH_VOLUME                               = "volume.NO_SUCH_VOLUME";
+    public static final String NO_SUCH_PATH                                 = "volume.NO_SUCH_PATH";
+    public static final String ALREADY_EXISTS                               = "volume.ALREADY_EXISTS";
+    public static final String ID_OUT_OF_RANGE                              = "volume.ID_OUT_OF_RANGE";
+    public static final String CANNOT_DELETE_VOLUME_IN_USE                  = "volume.CANNOT_DELETE_VOLUME_IN_USE";
+    public static final String WRONG_TYPE_CURRVOL                           = "volume.WRONG_TYPE_CURRVOL";
+    public static final String CANNOT_DELETE_CURRVOL                        = "volume.CANNOT_DELETE_CURRVOL";
+    public static final String CANNOT_CHANGE_TYPE_OF_CURRVOL                = "volume.CANNOT_CHANGE_TYPE_OF_CURRVOL";
+    public static final String INVALID_REQUEST                              = "volume.INVALID_REQUEST";
+    public static final String NOT_ABSOLUTE_PATH                            = "volume.NOT_ABSOLUTE_PATH";
+    public static final String SUBDIR_OF_ANOTHER_VOLUME                     = "volume.SUBDIR_OF_ANOTHER_VOLUME";
+    public static final String INVALID_METADATA                             = "volume.INVALID_METADATA";
+    public static final String BAD_VOLUME_STORE_TYPE                        = "volume.BAD_STORE_TYPE";
+    public static final String BAD_VOLUME_PATH                              = "volume.BAD_PATH";
+    public static final String BAD_VOLUME_NAME                              = "volume.BAD_NAME";
+    public static final String BAD_VOLUME_TYPE                              = "volume.BAD_TYPE";
+    public static final String BAD_VOLUME_COMPRESSION_THRESHOLD             = "volume.BAD_COMPRESSION_THRESHOLD";
+    public static final String BAD_VOLUME_CURRENT                           = "volume.BAD_CURRENT";
+    public static final String BAD_VOLUME_COMPRESS_BLOBS                    = "volume.BAD_COMPRESS_BLOBS";
+    public static final String BAD_VOLUME_USE_IN_FREQUENT_ACCESS            = "volume.BAD_USE_IN_FREQUENT_ACCESS";
+    public static final String BAD_VOLUME_USE_INTELLIGENT_TIERING           = "volume.BAD_USE_INTELLIGENT_TIERING";
+    public static final String BAD_VOLUME_STORAGE_TYPE                      = "volume.BAD_STORAGE_TYPE";
+    public static final String BAD_VOLUME_USE_IN_FREQUENT_ACCESS_THRESHOLD  = "volume.BAD_USE_IN_FREQUENT_ACCESS_THRESHOLD";
+    public static final String BAD_VOLUME_GLOBAL_BUCKET_ID                  = "volume.BAD_GLOBAL_BUCKET_ID";
 
     private VolumeServiceException(String message, String code, boolean isReceiversFault) {
         super(message, code, isReceiversFault);
@@ -103,8 +115,12 @@ public final class VolumeServiceException extends ServiceException {
                 anotherVol.getId() + ", path=" + anotherVol.getRootPath() + ")",
                 SUBDIR_OF_ANOTHER_VOLUME, SENDERS_FAULT, null);
     }
-    
+
     public static VolumeServiceException INVALID_METADATA(Throwable cause) {
         return new VolumeServiceException("could not decode metadata", INVALID_METADATA, true, cause);
+    }
+
+    public static VolumeServiceException INVALID_REQUEST(String msg, String error) {
+        return new VolumeServiceException("invalid request: " + msg, error, SENDERS_FAULT, null);
     }
 }
