cb99c3d5d9cc22f5fa99e489e3b481d763289e57
Indraneel Patil <indraneel.patil@synacor.com>
2022-06-30 16:53:21 +0530

    ZCS-11428 Modify Volumes SOAP API to support S3 volume type.
    - Introduced external volume info DTO
    - Honoring store_type column while creating volume
    - Modified create/delete/getAllvolumes/getVolume/modifyVolume API's to support S3 volume type

--- store/src/java/com/zimbra/cs/service/admin/DeleteVolume.java
+++ store/src/java/com/zimbra/cs/service/admin/DeleteVolume.java
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2005, 2006, 2007, 2009, 2010, 2011, 2013, 2014, 2016 Synacor, Inc.
+ * Copyright (C) 2005, 2006, 2007, 2009, 2010, 2011, 2013, 2014, 2016, 2022 Synacor, Inc.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
@@ -20,17 +20,22 @@ package com.zimbra.cs.service.admin;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONException;
+
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
+import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.accesscontrol.AdminRight;
 import com.zimbra.cs.account.accesscontrol.Rights.Admin;
 import com.zimbra.cs.store.StoreManager;
+import com.zimbra.cs.volume.Volume;
 import com.zimbra.cs.volume.VolumeManager;
 import com.zimbra.soap.JaxbUtil;
 import com.zimbra.soap.ZimbraSoapContext;
 import com.zimbra.soap.admin.message.DeleteVolumeRequest;
 import com.zimbra.soap.admin.message.DeleteVolumeResponse;
+import com.zimbra.util.ExternalVolumeInfoHandler;
 
 public final class DeleteVolume extends AdminDocumentHandler {
 
@@ -45,12 +50,23 @@ public final class DeleteVolume extends AdminDocumentHandler {
         checkRight(zsc, ctx, Provisioning.getInstance().getLocalServer(), Admin.R_manageVolume);
 
         VolumeManager mgr = VolumeManager.getInstance();
-        mgr.getVolume(req.getId()); // make sure the volume exists before doing anything heavyweight...
+        Volume vol = mgr.getVolume(req.getId()); // make sure the volume exists before doing anything heavyweight...
         StoreManager storeManager = StoreManager.getInstance();
         if (storeManager.supports(StoreManager.StoreFeature.CUSTOM_STORE_API, String.valueOf(req.getId()))) {
           throw ServiceException.INVALID_REQUEST("Operation unsupported, use zxsuite to delete this volume", null);
         }
         mgr.delete(req.getId());
+        try {
+            if (vol.getStoreType().equals(Volume.StoreType.EXTERNAL)) {
+                ExternalVolumeInfoHandler extVolInfoHandler = new ExternalVolumeInfoHandler(Provisioning.getInstance());
+                extVolInfoHandler.deleteServerProperties(req.getId());
+            }
+        } catch (ServiceException e) {
+            ZimbraLog.store.error("Error while processing DeleteVolumeRequest", e);
+            throw e;
+        } catch (JSONException e) {
+            throw ServiceException.FAILURE("Error while deleting server properties", null);
+        }
         return new DeleteVolumeResponse();
     }
 
@@ -58,5 +74,4 @@ public final class DeleteVolume extends AdminDocumentHandler {
     public void docRights(List<AdminRight> relatedRights, List<String> notes) {
         relatedRights.add(Admin.R_manageVolume);
     }
-
 }
