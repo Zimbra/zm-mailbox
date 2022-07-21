43e7ca9cac39dc3971a70f727217d43a7dd1564f
rupeshtelus <rupesh.baral@telusinternational.com>
2022-07-21 13:20:58 +0530

    removed space

--- common/src/java/com/zimbra/common/soap/AccountConstants.java
+++ common/src/java/com/zimbra/common/soap/AccountConstants.java
@@ -644,5 +644,4 @@ public class AccountConstants {
     public static final String E_SKIN_SELECTION_COLOR = "zimbraSkinSelectionColor";
     public static final String E_SKIN_FAVICON = "zimbraSkinFavicon";
     public static final String E_HOSTNAME = "hostname";
-    
 }
