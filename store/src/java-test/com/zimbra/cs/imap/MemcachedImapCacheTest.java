4d4f47e90558368ba12b16e3da12006f215b5281
kevinmolina01 <107272354+kevinmolina01@users.noreply.github.com>
2022-08-17 13:22:07 -0600

    ZCS-11935: Check and Fix exceptions logged in build logs for test failures (#1369)

--- store/src/java-test/com/zimbra/cs/imap/MemcachedImapCacheTest.java
+++ store/src/java-test/com/zimbra/cs/imap/MemcachedImapCacheTest.java
@@ -54,7 +54,7 @@ public class MemcachedImapCacheTest {
      */
     @Before
     public void setUp() throws Exception {
-        MailboxTestUtil.initProvisioning("store/");
+        MailboxTestUtil.initProvisioning(MailboxTestUtil.getZimbraServerDir(null));
     }
 
     /**
