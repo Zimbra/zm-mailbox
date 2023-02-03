c7e2da687b7f0e97eeefb0cfa103486b4a0e0cb3
Shruti Gupta <shruti.gupta@synacor.com>
2023-02-03 11:31:56 +0530

    ZCS-12876|ZCS-12878 logging the message ID when the discard filter is called in the mailbox logs. *Changed the log level of IMAP SEARCH command to debug

--- store/src/java/com/zimbra/cs/filter/jsieve/Discard.java
+++ store/src/java/com/zimbra/cs/filter/jsieve/Discard.java
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2011, 2013, 2014, 2016 Synacor, Inc.
+ * Copyright (C) 2011, 2013, 2014, 2016, 2023 Synacor, Inc.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
@@ -16,6 +16,7 @@
  */
 package com.zimbra.cs.filter.jsieve;
 
+import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.filter.ZimbraMailAdapter;
 import org.apache.jsieve.Arguments;
 import org.apache.jsieve.Block;
@@ -29,8 +30,14 @@ public class Discard extends org.apache.jsieve.commands.Discard {
 
     @Override
     protected Object executeBasic(MailAdapter mail, Arguments arguments, Block block, SieveContext context) throws SieveException {
-        if (!(mail instanceof ZimbraMailAdapter))
+        if (!(mail instanceof ZimbraMailAdapter)) {
             return null;
+        }
+        for (String msgId : mail.getHeader("Message-ID")) {
+            if (!msgId.isEmpty()) {
+                ZimbraLog.filter.info("Discarding Message: Message-ID=%s", msgId);
+            }
+        }
         ((ZimbraMailAdapter) mail).setDiscardActionPresent();
         return super.executeBasic(mail, arguments, block, context);
     }
