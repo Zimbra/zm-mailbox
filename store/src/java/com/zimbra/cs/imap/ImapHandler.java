3d0e0b102b21e014582f236ec53a998f451fb83d
Shruti Gupta <shruti.gupta@synacor.com>
2023-02-03 11:31:56 +0530

    ZCS-12876|ZCS-12878 logging the message ID when the discard filter is called in the mailbox logs. *Changed the log level of IMAP SEARCH command to debug

--- store/src/java/com/zimbra/cs/imap/ImapHandler.java
+++ store/src/java/com/zimbra/cs/imap/ImapHandler.java
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016 Synacor, Inc.
+ * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2023 Synacor, Inc.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
@@ -3757,7 +3757,7 @@ public abstract class ImapHandler {
             } else {
                 search = '(' + i4folder.getQuery() + ") " + search;
             }
-            ZimbraLog.imap.info("[ search is: " + search + " ]");
+            ZimbraLog.imap.debug("[ search is: " + search + " ]");
         } finally {
             mbox.unlock();
         }
