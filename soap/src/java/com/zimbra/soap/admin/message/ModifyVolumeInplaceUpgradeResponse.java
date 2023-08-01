5453989a580a55100069ed949cf9bf1e774a2f57
Vishnukant Jangid <vishnukant.jangid@PNQM0028-NISIHA-4.local>
2023-08-01 08:15:35 +0530

    ZCS-13547: Inplace migration implementation

---
+++ soap/src/java/com/zimbra/soap/admin/message/ModifyVolumeInplaceUpgradeResponse.java
@@ -0,0 +1,26 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2023 Synacor, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.soap.admin.message;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.zimbra.common.soap.AdminConstants;
+
+@XmlRootElement(name=AdminConstants.E_MODIFY_VOLUME_INPLACE_UPGRADE_RESPONSE)
+public class ModifyVolumeInplaceUpgradeResponse {
+}
