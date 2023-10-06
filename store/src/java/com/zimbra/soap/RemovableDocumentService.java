15c8c948585910483d08826899462609cda8b775
Rakeshkumar More <rakeshkumar.more@synacor.com>
2023-10-06 10:38:47 +0530

    ZCS-13901 method for unloading extension

---
+++ store/src/java/com/zimbra/soap/RemovableDocumentService.java
@@ -0,0 +1,22 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2023 Synacor, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.soap;
+
+public interface RemovableDocumentService extends DocumentService{
+    void deregisterHandlers(DocumentDispatcher dispatcher);
+}
