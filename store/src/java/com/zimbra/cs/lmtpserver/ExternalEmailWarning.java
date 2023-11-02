2864f1b5cdc48f575d696e6891794b69dbe1f6c0
chitranshu198 <76155458+chitranshu198@users.noreply.github.com>
2023-11-02 16:21:19 +0530

    ZBUG-3132 : Changes to fix missing external email warning issue for all external domains

--- store/src/java/com/zimbra/cs/lmtpserver/ExternalEmailWarning.java
+++ store/src/java/com/zimbra/cs/lmtpserver/ExternalEmailWarning.java
@@ -22,6 +22,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang.StringEscapeUtils;
 
 import com.zimbra.common.localconfig.LC;
@@ -151,9 +152,20 @@ public class ExternalEmailWarning {
 
     // message must be RTF822 compliant: CRLFCRLF sequence is required to determine
     // where does the header meet the body of the text/html part
-    private static final String CONTENT_TYPE_TEXT_HTML_REGEX = "Content-Type: text/html.*?\\r\\n\\r\\n.*?<body.*?>";
+    private static final String CONTENT_TYPE_TEXT_HTML_REGEX = "Content-Type: text/html.*?\\r\\n\\r\\n";
     private static final Pattern CONTENT_TYPE_TEXT_HTML_PATTERN = Pattern.compile(CONTENT_TYPE_TEXT_HTML_REGEX,
             Pattern.CASE_INSENSITIVE + Pattern.DOTALL);
+    private static final String CONTENT_TYPE_TEXT_PLAIN_BASE64_ENCODED_REGEX = "Content-Type: text/plain.*?base64\\r\\n\\r\\n";
+    private static final Pattern CONTENT_TYPE_TEXT_PLAIN_BASE64_ENCODED_PATTERN = Pattern.compile(CONTENT_TYPE_TEXT_PLAIN_BASE64_ENCODED_REGEX,
+            Pattern.CASE_INSENSITIVE + Pattern.DOTALL);
+    private static final String CONTENT_TYPE_TEXT_HTML_BASE64_ENCODED_REGEX = "Content-Type: text/html.*?base64\\r\\n\\r\\n";
+    private static final Pattern CONTENT_TYPE_TEXT_HTML_BASE64_ENCODED_PATTERN = Pattern.compile(CONTENT_TYPE_TEXT_HTML_BASE64_ENCODED_REGEX,
+            Pattern.CASE_INSENSITIVE + Pattern.DOTALL);
+    private static final String CONTAINS_BODY_TAG=".*?<body.*?>";
+    private static final Pattern CONTAINS_BODY_TAG_PATTERN = Pattern.compile(CONTAINS_BODY_TAG,
+            Pattern.CASE_INSENSITIVE + Pattern.DOTALL);
+    private final String MIME_BOUNDARY_REGEX = "Content-Type: multipart/alternative;.*?boundary=\"";
+    private final Pattern MIME_BOUNDARY_PATTERN=Pattern.compile(MIME_BOUNDARY_REGEX, Pattern.CASE_INSENSITIVE + Pattern.DOTALL);
 
     /**
      * Updates the string representation of a mime message in RFC822 format with the
@@ -172,27 +184,132 @@ public class ExternalEmailWarning {
     public String getUpdatedContent(String content) {
         if (content != null) {
             final Matcher ctTextPlainMatcher = CONTENT_TYPE_TEXT_PLAIN_PATTERN.matcher(content);
-            if (ctTextPlainMatcher.find()) {
+            final Matcher ctTextPlainBase64EncodedMatcher = CONTENT_TYPE_TEXT_PLAIN_BASE64_ENCODED_PATTERN.matcher(content);
+            if (ctTextPlainBase64EncodedMatcher.find()) {
+                String mimeBoundary = getMimeBoundary(content);
+                if (mimeBoundary != null) {
+                    final int plainTextBodyStart = ctTextPlainBase64EncodedMatcher.end();
+                    String decodedPlainTextContentMime = decodeMimeSection(content, plainTextBodyStart, mimeBoundary);
+                    if (decodedPlainTextContentMime != null) {
+                        decodedPlainTextContentMime = appendPlainTextWarning(decodedPlainTextContentMime, plainTextBodyStart);
+                        content = encodeMimeSection(decodedPlainTextContentMime, plainTextBodyStart, mimeBoundary);
+                    }
+                }
+            } else if (ctTextPlainMatcher.find()) {
                 final int end = ctTextPlainMatcher.end();
-                final StringBuilder sb = new StringBuilder();
-                sb.append(content.substring(0, end));
-                if (getTextPlainWarning() != null)
-                    sb.append(getTextPlainWarning());
-                sb.append(content.substring(end));
-                content = sb.toString();
+                content = appendPlainTextWarning(content, end);
             }
             final Matcher ctTextHtmlMatcher = CONTENT_TYPE_TEXT_HTML_PATTERN.matcher(content);
-            if (ctTextHtmlMatcher.find()) {
-                final int end = ctTextHtmlMatcher.end();
-                final StringBuilder sb = new StringBuilder();
-                sb.append(content.substring(0, end));
-                if (getTextHtmlWarning() != null)
-                    sb.append(getTextHtmlWarning());
-                sb.append(content.substring(end));
-                content = sb.toString();
+            final Matcher ctTextHtmlBase64EncodedMatcher = CONTENT_TYPE_TEXT_HTML_BASE64_ENCODED_PATTERN.matcher(content);
+            if (ctTextHtmlBase64EncodedMatcher.find()) {
+                String mimeBoundary = getMimeBoundary(content);
+                if (mimeBoundary != null) {
+                    final int htmlBodyStart = ctTextHtmlBase64EncodedMatcher.end();
+                    String decodedHtmlContentMime = decodeMimeSection(content, htmlBodyStart, mimeBoundary);
+                    decodedHtmlContentMime = getUpdatedHtmlSection(decodedHtmlContentMime, htmlBodyStart);
+                    content = encodeMimeSection(decodedHtmlContentMime, htmlBodyStart, mimeBoundary);
+                }
+            } else if (ctTextHtmlMatcher.find()) {
+                final int htmlBodyStart = ctTextHtmlMatcher.end();
+                content = getUpdatedHtmlSection(content, htmlBodyStart);
             }
+
+        }
+        return content;
+    }
+    /**
+     * Updates the text/html section of MIME
+     * Checks weather html section contains <body> tag and appends the warning, else appends the warning at start of section
+     * */
+    public String getUpdatedHtmlSection(String content, int htmlBodyStartIndex) {
+        Matcher containsBody = CONTAINS_BODY_TAG_PATTERN.matcher(content.substring(htmlBodyStartIndex));
+        if (containsBody.find()) {
+            final int end = containsBody.end() + htmlBodyStartIndex;
+            content = appendHtmlWarning(content, end);
+        } else {
+            final int end = htmlBodyStartIndex;
+            content = appendHtmlWarning(content, end);
         }
         return content;
     }
 
+    /**
+     * Appends HTML warning at a specific index and returns the MIME
+     * */
+    public String appendHtmlWarning(String content, int index) {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(content.substring(0, index));
+        if (getTextHtmlWarning() != null)
+            sb.append(getTextHtmlWarning());
+        sb.append(content.substring(index));
+        content = sb.toString();
+        return content;
+    }
+
+    /**
+     * Appends plain text warning at a specific index and returns the MIME
+     * */
+    public String appendPlainTextWarning(String content, int index) {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(content.substring(0, index));
+        if (getTextPlainWarning() != null)
+            sb.append(getTextPlainWarning());
+        sb.append(content.substring(index));
+        content = sb.toString();
+        return content;
+    }
+
+    /**
+     * Decodes a specific encoded MIME section and return the MIME
+     * Uses MIME boundary to find end of section
+     * */
+    public String decodeMimeSection(String content, int sectionStartIndex, String boundary) {
+        Base64 base64 = new Base64();
+        int endOfSection = content.indexOf("--"+boundary,sectionStartIndex);
+        if (endOfSection !=-1) {
+            String encodedSection = content.substring(sectionStartIndex, endOfSection);
+            String decodedSection = new String(base64.decode(encodedSection.getBytes()));
+            StringBuilder sb = new StringBuilder();
+            sb.append(content.substring(0,sectionStartIndex));
+            sb.append(decodedSection);
+            sb.append("\r\n");
+            sb.append("\r\n"+content.substring(endOfSection));
+            content = sb.toString();
+            return content;
+        }
+        return null;
+    }
+
+    /**
+     * Encodes a specific MIME section with base64 encoding and returns the MIME
+     * Uses MIME boundary to find end of section
+     * */
+    public String encodeMimeSection(String content, int sectionStartIndex, String boundary) {
+        Base64 base64 = new Base64();
+        int endOfSection = content.indexOf("--"+boundary,sectionStartIndex);
+        if (endOfSection !=-1) {
+            String decodedSection = content.substring(sectionStartIndex, endOfSection);
+            String encodedSection = new String(base64.encode(decodedSection.getBytes()));
+            StringBuilder sb = new StringBuilder();
+            sb.append(content.substring(0,sectionStartIndex));
+            sb.append(encodedSection);
+            sb.append("\r\n");
+            sb.append("\r\n"+content.substring(content.indexOf("--",sectionStartIndex)));
+            content = sb.toString();
+            return content;
+        }
+        return null;
+    }
+
+    /**
+     * Extracts the boundary/delimiter of a MIME
+     * */
+    public String getMimeBoundary(String content) {
+        final Matcher boundryMatcher = MIME_BOUNDARY_PATTERN.matcher(content);
+        if (boundryMatcher.find()) {
+            int boundaryStart = boundryMatcher.end();
+            return content.substring(boundaryStart,content.indexOf("\"",boundaryStart));
+        }
+        return null;
+    }
 }
