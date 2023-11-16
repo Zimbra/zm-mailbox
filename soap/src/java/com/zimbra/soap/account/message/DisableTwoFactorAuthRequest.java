c403272a0eaf5a654029636b11a1397697ea871a
Preshita Agrawal <preshita.agrawal@synacor.com>
2023-11-17 00:26:19 +0530

    ZCS-14058 Update EnableTwoFactorAuthRequest and DisableTwoFactorAuthRequest to address email method

--- soap/src/java/com/zimbra/soap/account/message/DisableTwoFactorAuthRequest.java
+++ soap/src/java/com/zimbra/soap/account/message/DisableTwoFactorAuthRequest.java
@@ -2,7 +2,7 @@
  * ***** BEGIN LICENSE BLOCK *****
  *
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2015, 2016 Synacor, Inc.
+ * Copyright (C) 2015, 2016, 2023 Synacor, Inc.
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
@@ -18,6 +18,7 @@
  */
 package com.zimbra.soap.account.message;
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
@@ -26,4 +27,18 @@ import com.zimbra.common.soap.AccountConstants;
 @XmlRootElement(name=AccountConstants.E_DISABLE_TWO_FACTOR_AUTH_REQUEST)
 @XmlType(propOrder = {})
 public class DisableTwoFactorAuthRequest {
+    /**
+     * @zm-api-field-description 2FA method to be disabled
+     */
+    @XmlElement(name=AccountConstants.E_METHOD, required=false)
+    private String method;
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
 }
