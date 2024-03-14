8045570a6ab24853df15ef3ca3f3707ab858163d
miteshsavani810 <mitesh.savani@synacor.com>
2024-03-14 11:59:58 +0530

    PREAPPS-7711: Add new LDAP attributes that require to disable Classic UI access - Added new zimbraClassicWebClientDisabled attribute at domain and config level.

--- store/src/java/com/zimbra/cs/account/ZAttrConfig.java
+++ store/src/java/com/zimbra/cs/account/ZAttrConfig.java
@@ -37959,6 +37959,88 @@ public abstract class ZAttrConfig extends Entry {
     }
 
     /**
+     * Whether to enable/disable the classic web client option on the login
+     * screen. Default value is FALSE, which makes the classic client option
+     * available.
+     *
+     * @return zimbraClassicWebClientDisabled, or false if unset
+     *
+     * @since ZCS 10.1.0
+     */
+    @ZAttr(id=4125)
+    public boolean isClassicWebClientDisabled() {
+        return getBooleanAttr(Provisioning.A_zimbraClassicWebClientDisabled, false, true);
+    }
+
+    /**
+     * Whether to enable/disable the classic web client option on the login
+     * screen. Default value is FALSE, which makes the classic client option
+     * available.
+     *
+     * @param zimbraClassicWebClientDisabled new value
+     * @throws com.zimbra.common.service.ServiceException if error during update
+     *
+     * @since ZCS 10.1.0
+     */
+    @ZAttr(id=4125)
+    public void setClassicWebClientDisabled(boolean zimbraClassicWebClientDisabled) throws com.zimbra.common.service.ServiceException {
+        HashMap<String,Object> attrs = new HashMap<String,Object>();
+        attrs.put(Provisioning.A_zimbraClassicWebClientDisabled, zimbraClassicWebClientDisabled ? TRUE : FALSE);
+        getProvisioning().modifyAttrs(this, attrs);
+    }
+
+    /**
+     * Whether to enable/disable the classic web client option on the login
+     * screen. Default value is FALSE, which makes the classic client option
+     * available.
+     *
+     * @param zimbraClassicWebClientDisabled new value
+     * @param attrs existing map to populate, or null to create a new map
+     * @return populated map to pass into Provisioning.modifyAttrs
+     *
+     * @since ZCS 10.1.0
+     */
+    @ZAttr(id=4125)
+    public Map<String,Object> setClassicWebClientDisabled(boolean zimbraClassicWebClientDisabled, Map<String,Object> attrs) {
+        if (attrs == null) attrs = new HashMap<String,Object>();
+        attrs.put(Provisioning.A_zimbraClassicWebClientDisabled, zimbraClassicWebClientDisabled ? TRUE : FALSE);
+        return attrs;
+    }
+
+    /**
+     * Whether to enable/disable the classic web client option on the login
+     * screen. Default value is FALSE, which makes the classic client option
+     * available.
+     *
+     * @throws com.zimbra.common.service.ServiceException if error during update
+     *
+     * @since ZCS 10.1.0
+     */
+    @ZAttr(id=4125)
+    public void unsetClassicWebClientDisabled() throws com.zimbra.common.service.ServiceException {
+        HashMap<String,Object> attrs = new HashMap<String,Object>();
+        attrs.put(Provisioning.A_zimbraClassicWebClientDisabled, "");
+        getProvisioning().modifyAttrs(this, attrs);
+    }
+
+    /**
+     * Whether to enable/disable the classic web client option on the login
+     * screen. Default value is FALSE, which makes the classic client option
+     * available.
+     *
+     * @param attrs existing map to populate, or null to create a new map
+     * @return populated map to pass into Provisioning.modifyAttrs
+     *
+     * @since ZCS 10.1.0
+     */
+    @ZAttr(id=4125)
+    public Map<String,Object> unsetClassicWebClientDisabled(Map<String,Object> attrs) {
+        if (attrs == null) attrs = new HashMap<String,Object>();
+        attrs.put(Provisioning.A_zimbraClassicWebClientDisabled, "");
+        return attrs;
+    }
+
+    /**
      * Value for postconf address_verify_negative_refresh_time
      *
      * @return zimbraMtaAddressVerifyNegativeRefreshTime, or "10m" if unset
