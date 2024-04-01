5d58ca1f2f3fd2eb8ff8108598150115b2bce41f
UmaShankar Avagadda <umagmrit@gmail.com>
2024-04-01 14:29:10 +0000

    ZCS-14844:add preinst.sh,postinst.sh for restrict write access for /opt/zimbra/jetty_base/webapps

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-if lsattr -d /path/to/directory | grep -q 'i'; then
+if lsattr -d /opt/zimbra/jetty_base/webapps/ | grep -q 'i'; then
     echo "Immutable attribute already set on the /opt/zimbra/jetty_base/webapps/ (zimbra-mbox-war)"
+    lsattr -d /opt/zimbra/jetty_base/webapps/
 else
 	echo "**** apply restrict write access for /opt/zimbra/jetty_base/webapps/ (zimbra-mbox-war)****"
+	lsattr -d /opt/zimbra/jetty_base/webapps/
 	chattr +i -R /opt/zimbra/jetty_base/webapps/
 fi
--- scripts/preinst.sh
+++ scripts/preinst.sh
@@ -2,5 +2,6 @@
 # Check if the immutable attribute is already set on the directory
 if lsattr -d /opt/zimbra/jetty_base/webapps/ | grep -q 'i'; then
 	echo "**** remove restrict write access for /opt/zimbra/jetty_base/webapps/ (zimbra-mbox-war)****"
+	lsattr -d /opt/zimbra/jetty_base/webapps/
 	chattr -i -R /opt/zimbra/jetty_base/webapps/
 fi
