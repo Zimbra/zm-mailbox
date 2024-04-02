5fcf924770cfe90b91cdcfc698c02ed1b7735a84
UmaShankar Avagadda <umagmrit@gmail.com>
2024-04-02 04:24:59 +0000

    ZCS-14844:add preinst.sh,postinst.sh for restrict write access for /opt/zimbra/jetty_base/webapps

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-if lsattr -d /opt/zimbra/jetty_base/webapps/ | grep -qE '\b[i]\b'; then
-	echo "Immutable attribute already set on the /opt/zimbra/jetty_base/webapps/ (zimbra-mbox-war)"
-	lsattr -d /opt/zimbra/jetty_base/webapps/
-else
-	echo "**** apply restrict write access for /opt/zimbra/jetty_base/webapps/ (zimbra-mbox-war) ****"
-	lsattr -d /opt/zimbra/jetty_base/webapps/
-	chattr +i -R /opt/zimbra/jetty_base/webapps/
+if [ -d "/opt/zimbra/jetty_base/webapps" ]; then
+	if lsattr -d /opt/zimbra/jetty_base/webapps/ | grep -qE '\b[i]\b'; then
+		echo "Immutable attribute already set on the /opt/zimbra/jetty_base/webapps/ (zimbra-mbox-war)"
+		lsattr -d /opt/zimbra/jetty_base/webapps/
+	else
+		echo "**** apply restrict write access for /opt/zimbra/jetty_base/webapps/ (zimbra-mbox-war) ****"
+		lsattr -d /opt/zimbra/jetty_base/webapps/
+		chattr +i -R /opt/zimbra/jetty_base/webapps/
+	fi
 fi
--- scripts/preinst.sh
+++ scripts/preinst.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-if lsattr -d /opt/zimbra/jetty_base/webapps/ | grep -qE '\b[i]\b'; then
-	echo "**** remove restrict write access for /opt/zimbra/jetty_base/webapps/ (zimbra-mbox-war) ****"
-	lsattr -d /opt/zimbra/jetty_base/webapps/
-	chattr -i -R /opt/zimbra/jetty_base/webapps/
+if [ -d "/opt/zimbra/jetty_base/webapps" ]; then
+	if lsattr -d /opt/zimbra/jetty_base/webapps/ | grep -qE '\b[i]\b'; then
+		echo "**** remove restrict write access for /opt/zimbra/jetty_base/webapps/ (zimbra-mbox-war) ****"
+		lsattr -d /opt/zimbra/jetty_base/webapps/
+		chattr -i -R /opt/zimbra/jetty_base/webapps/
+	fi
 fi
