3590b6d1c18d43eda63c0585b3303a990a9588ff
viditZim <vidit.patni@synacor.com>
2025-10-13 15:36:13 +0530

    ZCS-18141 Modified since version to 10.1.13

--- store/src/java/com/zimbra/cs/account/ZAttrConfig.java
+++ store/src/java/com/zimbra/cs/account/ZAttrConfig.java
@@ -18413,7 +18413,7 @@ public abstract class ZAttrConfig extends Entry {
      *
      * @return zimbraExternalEmailWarningMessage, or "This message originated outside of your organization." if unset
      *
-     * @since ZCS 10.1.12
+     * @since ZCS 10.1.13
      */
     @ZAttr(id=4139)
     public String getExternalEmailWarningMessage() {
@@ -18426,7 +18426,7 @@ public abstract class ZAttrConfig extends Entry {
      * @param zimbraExternalEmailWarningMessage new value
      * @throws com.zimbra.common.service.ServiceException if error during update
      *
-     * @since ZCS 10.1.12
+     * @since ZCS 10.1.13
      */
     @ZAttr(id=4139)
     public void setExternalEmailWarningMessage(String zimbraExternalEmailWarningMessage) throws com.zimbra.common.service.ServiceException {
@@ -18442,7 +18442,7 @@ public abstract class ZAttrConfig extends Entry {
      * @param attrs existing map to populate, or null to create a new map
      * @return populated map to pass into Provisioning.modifyAttrs
      *
-     * @since ZCS 10.1.12
+     * @since ZCS 10.1.13
      */
     @ZAttr(id=4139)
     public Map<String,Object> setExternalEmailWarningMessage(String zimbraExternalEmailWarningMessage, Map<String,Object> attrs) {
@@ -18456,7 +18456,7 @@ public abstract class ZAttrConfig extends Entry {
      *
      * @throws com.zimbra.common.service.ServiceException if error during update
      *
-     * @since ZCS 10.1.12
+     * @since ZCS 10.1.13
      */
     @ZAttr(id=4139)
     public void unsetExternalEmailWarningMessage() throws com.zimbra.common.service.ServiceException {
@@ -18471,7 +18471,7 @@ public abstract class ZAttrConfig extends Entry {
      * @param attrs existing map to populate, or null to create a new map
      * @return populated map to pass into Provisioning.modifyAttrs
      *
-     * @since ZCS 10.1.12
+     * @since ZCS 10.1.13
      */
     @ZAttr(id=4139)
     public Map<String,Object> unsetExternalEmailWarningMessage(Map<String,Object> attrs) {
@@ -18977,7 +18977,7 @@ public abstract class ZAttrConfig extends Entry {
      *
      * @return zimbraFeatureExternalEmailWarningEnabled, or false if unset
      *
-     * @since ZCS 10.1.12
+     * @since ZCS 10.1.13
      */
     @ZAttr(id=4138)
     public boolean isFeatureExternalEmailWarningEnabled() {
@@ -18990,7 +18990,7 @@ public abstract class ZAttrConfig extends Entry {
      * @param zimbraFeatureExternalEmailWarningEnabled new value
      * @throws com.zimbra.common.service.ServiceException if error during update
      *
-     * @since ZCS 10.1.12
+     * @since ZCS 10.1.13
      */
     @ZAttr(id=4138)
     public void setFeatureExternalEmailWarningEnabled(boolean zimbraFeatureExternalEmailWarningEnabled) throws com.zimbra.common.service.ServiceException {
@@ -19006,7 +19006,7 @@ public abstract class ZAttrConfig extends Entry {
      * @param attrs existing map to populate, or null to create a new map
      * @return populated map to pass into Provisioning.modifyAttrs
      *
-     * @since ZCS 10.1.12
+     * @since ZCS 10.1.13
      */
     @ZAttr(id=4138)
     public Map<String,Object> setFeatureExternalEmailWarningEnabled(boolean zimbraFeatureExternalEmailWarningEnabled, Map<String,Object> attrs) {
@@ -19020,7 +19020,7 @@ public abstract class ZAttrConfig extends Entry {
      *
      * @throws com.zimbra.common.service.ServiceException if error during update
      *
-     * @since ZCS 10.1.12
+     * @since ZCS 10.1.13
      */
     @ZAttr(id=4138)
     public void unsetFeatureExternalEmailWarningEnabled() throws com.zimbra.common.service.ServiceException {
@@ -19035,7 +19035,7 @@ public abstract class ZAttrConfig extends Entry {
      * @param attrs existing map to populate, or null to create a new map
      * @return populated map to pass into Provisioning.modifyAttrs
      *
-     * @since ZCS 10.1.12
+     * @since ZCS 10.1.13
      */
     @ZAttr(id=4138)
     public Map<String,Object> unsetFeatureExternalEmailWarningEnabled(Map<String,Object> attrs) {
@@ -29353,78 +29353,6 @@ public abstract class ZAttrConfig extends Entry {
     public Map<String,Object> unsetLicensePreExpiryReminderSentDetails(Map<String,Object> attrs) {
         if (attrs == null) attrs = new HashMap<String,Object>();
         attrs.put(Provisioning.A_zimbraLicensePreExpiryReminderSentDetails, "");
-        return attrs;
-    }
-
-    /**
-     * Package Usage Reporting Scheduler last successful run date
-     *
-     * @return zimbraPackageUsageReportingLastSuccessfulRunDetails, or null if unset
-     *
-     * @since ZCS 10.1.12
-     */
-    @ZAttr(id=4145)
-    public String getPackageUsageReportingLastSuccessfulRunDetails() {
-        return getAttr(Provisioning.A_zimbraPackageUsageReportingLastSuccessfulRunDetails, null, true);
-    }
-
-    /**
-     * Package Usage Reporting Scheduler last successful run date
-     *
-     * @param zimbraPackageUsageReportingLastSuccessfulRunDetails new value
-     * @throws com.zimbra.common.service.ServiceException if error during update
-     *
-     * @since ZCS 10.1.12
-     */
-    @ZAttr(id=4145)
-    public void setPackageUsageReportingLastSuccessfulRunDetails(String zimbraPackageUsageReportingLastSuccessfulRunDetails) throws com.zimbra.common.service.ServiceException {
-        HashMap<String,Object> attrs = new HashMap<String,Object>();
-        attrs.put(Provisioning.A_zimbraPackageUsageReportingLastSuccessfulRunDetails, zimbraPackageUsageReportingLastSuccessfulRunDetails);
-        getProvisioning().modifyAttrs(this, attrs);
-    }
-
-    /**
-     * Package Usage Reporting Scheduler last successful run date
-     *
-     * @param zimbraPackageUsageReportingLastSuccessfulRunDetails new value
-     * @param attrs existing map to populate, or null to create a new map
-     * @return populated map to pass into Provisioning.modifyAttrs
-     *
-     * @since ZCS 10.1.12
-     */
-    @ZAttr(id=4145)
-    public Map<String,Object> setPackageUsageReportingLastSuccessfulRunDetails(String zimbraPackageUsageReportingLastSuccessfulRunDetails, Map<String,Object> attrs) {
-        if (attrs == null) attrs = new HashMap<String,Object>();
-        attrs.put(Provisioning.A_zimbraPackageUsageReportingLastSuccessfulRunDetails, zimbraPackageUsageReportingLastSuccessfulRunDetails);
-        return attrs;
-    }
-
-    /**
-     * Package Usage Reporting Scheduler last successful run date
-     *
-     * @throws com.zimbra.common.service.ServiceException if error during update
-     *
-     * @since ZCS 10.1.12
-     */
-    @ZAttr(id=4145)
-    public void unsetPackageUsageReportingLastSuccessfulRunDetails() throws com.zimbra.common.service.ServiceException {
-        HashMap<String,Object> attrs = new HashMap<String,Object>();
-        attrs.put(Provisioning.A_zimbraPackageUsageReportingLastSuccessfulRunDetails, "");
-        getProvisioning().modifyAttrs(this, attrs);
-    }
-
-    /**
-     * Package Usage Reporting Scheduler last successful run date
-     *
-     * @param attrs existing map to populate, or null to create a new map
-     * @return populated map to pass into Provisioning.modifyAttrs
-     *
-     * @since ZCS 10.1.12
-     */
-    @ZAttr(id=4145)
-    public Map<String,Object> unsetPackageUsageReportingLastSuccessfulRunDetails(Map<String,Object> attrs) {
-        if (attrs == null) attrs = new HashMap<String,Object>();
-        attrs.put(Provisioning.A_zimbraPackageUsageReportingLastSuccessfulRunDetails, "");
         return attrs;
     }
 
@@ -56011,7 +55939,7 @@ public abstract class ZAttrConfig extends Entry {
      *
      * @return zimbraPURDomainDetailsCollectionEnabled, or false if unset
      *
-     * @since ZCS 10.1.12
+     * @since ZCS 10.1.13
      */
     @ZAttr(id=4146)
     public boolean isPURDomainDetailsCollectionEnabled() {
@@ -56024,7 +55952,7 @@ public abstract class ZAttrConfig extends Entry {
      * @param zimbraPURDomainDetailsCollectionEnabled new value
      * @throws com.zimbra.common.service.ServiceException if error during update
      *
-     * @since ZCS 10.1.12
+     * @since ZCS 10.1.13
      */
     @ZAttr(id=4146)
     public void setPURDomainDetailsCollectionEnabled(boolean zimbraPURDomainDetailsCollectionEnabled) throws com.zimbra.common.service.ServiceException {
@@ -56040,7 +55968,7 @@ public abstract class ZAttrConfig extends Entry {
      * @param attrs existing map to populate, or null to create a new map
      * @return populated map to pass into Provisioning.modifyAttrs
      *
-     * @since ZCS 10.1.12
+     * @since ZCS 10.1.13
      */
     @ZAttr(id=4146)
     public Map<String,Object> setPURDomainDetailsCollectionEnabled(boolean zimbraPURDomainDetailsCollectionEnabled, Map<String,Object> attrs) {
@@ -56054,7 +55982,7 @@ public abstract class ZAttrConfig extends Entry {
      *
      * @throws com.zimbra.common.service.ServiceException if error during update
      *
-     * @since ZCS 10.1.12
+     * @since ZCS 10.1.13
      */
     @ZAttr(id=4146)
     public void unsetPURDomainDetailsCollectionEnabled() throws com.zimbra.common.service.ServiceException {
@@ -56069,12 +55997,84 @@ public abstract class ZAttrConfig extends Entry {
      * @param attrs existing map to populate, or null to create a new map
      * @return populated map to pass into Provisioning.modifyAttrs
      *
-     * @since ZCS 10.1.12
+     * @since ZCS 10.1.13
      */
     @ZAttr(id=4146)
     public Map<String,Object> unsetPURDomainDetailsCollectionEnabled(Map<String,Object> attrs) {
         if (attrs == null) attrs = new HashMap<String,Object>();
         attrs.put(Provisioning.A_zimbraPURDomainDetailsCollectionEnabled, "");
+        return attrs;
+    }
+
+    /**
+     * Package Usage Reporting Scheduler last successful run date
+     *
+     * @return zimbraPackageUsageReportingLastSuccessfulRunDetails, or null if unset
+     *
+     * @since ZCS 10.1.13
+     */
+    @ZAttr(id=4145)
+    public String getPackageUsageReportingLastSuccessfulRunDetails() {
+        return getAttr(Provisioning.A_zimbraPackageUsageReportingLastSuccessfulRunDetails, null, true);
+    }
+
+    /**
+     * Package Usage Reporting Scheduler last successful run date
+     *
+     * @param zimbraPackageUsageReportingLastSuccessfulRunDetails new value
+     * @throws com.zimbra.common.service.ServiceException if error during update
+     *
+     * @since ZCS 10.1.13
+     */
+    @ZAttr(id=4145)
+    public void setPackageUsageReportingLastSuccessfulRunDetails(String zimbraPackageUsageReportingLastSuccessfulRunDetails) throws com.zimbra.common.service.ServiceException {
+        HashMap<String,Object> attrs = new HashMap<String,Object>();
+        attrs.put(Provisioning.A_zimbraPackageUsageReportingLastSuccessfulRunDetails, zimbraPackageUsageReportingLastSuccessfulRunDetails);
+        getProvisioning().modifyAttrs(this, attrs);
+    }
+
+    /**
+     * Package Usage Reporting Scheduler last successful run date
+     *
+     * @param zimbraPackageUsageReportingLastSuccessfulRunDetails new value
+     * @param attrs existing map to populate, or null to create a new map
+     * @return populated map to pass into Provisioning.modifyAttrs
+     *
+     * @since ZCS 10.1.13
+     */
+    @ZAttr(id=4145)
+    public Map<String,Object> setPackageUsageReportingLastSuccessfulRunDetails(String zimbraPackageUsageReportingLastSuccessfulRunDetails, Map<String,Object> attrs) {
+        if (attrs == null) attrs = new HashMap<String,Object>();
+        attrs.put(Provisioning.A_zimbraPackageUsageReportingLastSuccessfulRunDetails, zimbraPackageUsageReportingLastSuccessfulRunDetails);
+        return attrs;
+    }
+
+    /**
+     * Package Usage Reporting Scheduler last successful run date
+     *
+     * @throws com.zimbra.common.service.ServiceException if error during update
+     *
+     * @since ZCS 10.1.13
+     */
+    @ZAttr(id=4145)
+    public void unsetPackageUsageReportingLastSuccessfulRunDetails() throws com.zimbra.common.service.ServiceException {
+        HashMap<String,Object> attrs = new HashMap<String,Object>();
+        attrs.put(Provisioning.A_zimbraPackageUsageReportingLastSuccessfulRunDetails, "");
+        getProvisioning().modifyAttrs(this, attrs);
+    }
+
+    /**
+     * Package Usage Reporting Scheduler last successful run date
+     *
+     * @param attrs existing map to populate, or null to create a new map
+     * @return populated map to pass into Provisioning.modifyAttrs
+     *
+     * @since ZCS 10.1.13
+     */
+    @ZAttr(id=4145)
+    public Map<String,Object> unsetPackageUsageReportingLastSuccessfulRunDetails(Map<String,Object> attrs) {
+        if (attrs == null) attrs = new HashMap<String,Object>();
+        attrs.put(Provisioning.A_zimbraPackageUsageReportingLastSuccessfulRunDetails, "");
         return attrs;
     }
 
