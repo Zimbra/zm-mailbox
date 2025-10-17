6025789f7c4cecc7f7ab561ca43f306154c3643e
Sonali Jagtap <sonali.jagtap@synacor.com>
2025-10-17 23:37:18 +0530

    ZBUG-5144: Added MathML MIME type handling and defanger to prevent script execution

--- store/src/java/com/zimbra/cs/html/DefangFactory.java
+++ store/src/java/com/zimbra/cs/html/DefangFactory.java
@@ -71,10 +71,11 @@ public class DefangFactory {
             return htmlDefang;
         }
 
-        if(contentTypeLowerCase.startsWith(MimeConstants.CT_TEXT_XML) ||
-           contentTypeLowerCase.startsWith(MimeConstants.CT_APPLICATION_XHTML) ||
-           contentTypeLowerCase.startsWith(MimeConstants.CT_IMAGE_SVG) ||
-           contentTypeLowerCase.startsWith(MimeConstants.CT_TEXT_XML_LEGACY)) {
+        if (contentTypeLowerCase.startsWith(MimeConstants.CT_TEXT_XML) ||
+                contentTypeLowerCase.startsWith(MimeConstants.CT_APPLICATION_XHTML) ||
+                contentTypeLowerCase.startsWith(MimeConstants.CT_IMAGE_SVG) ||
+                contentTypeLowerCase.startsWith(MimeConstants.CT_TEXT_XML_LEGACY) ||
+                contentTypeLowerCase.startsWith(MimeConstants.CT_APPLICATION_MATHML)) {
             return xhtmlDefang;
         }
         return noopDefang;
