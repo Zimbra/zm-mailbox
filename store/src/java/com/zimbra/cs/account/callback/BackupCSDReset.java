6ff18ac0e86e484222f43d22b241a14c4668a4ff
Gopal-Moolchandani <142574581+Gopal-Moolchandani@users.noreply.github.com>
2026-03-31 18:56:22 +0530

    10.1.17 ZCS 18558 : Modifying the zimbraBackupDeduplication atrribute as global config level updates the zimbraBackupCrossSessionDedupeEnabled attribute at server level
    Refactored

--- store/src/java/com/zimbra/cs/account/callback/BackupCSDReset.java
+++ store/src/java/com/zimbra/cs/account/callback/BackupCSDReset.java
@@ -1,9 +1,9 @@
 package com.zimbra.cs.account.callback;
 
 import com.zimbra.common.service.ServiceException;
-import com.zimbra.common.util.Log;
 import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.account.AttributeCallback;
+import com.zimbra.cs.account.Config;
 import com.zimbra.cs.account.Entry;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.Server;
@@ -21,10 +21,15 @@ public class BackupCSDReset extends AttributeCallback {
     public void postModify(CallbackContext context, String attrName, Entry entry) {
         try {
             Provisioning prov = Provisioning.getInstance();
-            Server server = prov.getLocalServer();
             Map<String, String> attrs = new HashMap<String, String>(1);
             attrs.put(Provisioning.A_zimbraBackupCSDReset, "TRUE");
-            prov.modifyAttrs(server, attrs);
+            if (entry instanceof Server) {
+                Server server = (Server) entry;
+                prov.modifyAttrs(server, attrs);
+            } else if (entry instanceof Config) {
+                Config config = prov.getConfig();
+                prov.modifyAttrs(config, attrs);
+            }
         } catch (ServiceException e) {
             ZimbraLog.misc.warn("unable to fetch the local server", e);
         }
