1fdd5d32ad23064a55bedc23e361cbfe6abe49d0
ashwinsahu19 <ashwin.sahu@synacor.com>
2026-06-22 10:09:04 +0530

    ZCS-19476 : Create IRopcCutomAuth class and register new extension on ZimbraCustomAuth ZCS-19476 : refactored

---
+++ store/src/java/com/zimbra/cs/account/auth/IRopcCustomAuth.java
@@ -0,0 +1,47 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite, Network Edition.
+ * Copyright (C) 2026 Synacor, Inc.  All Rights Reserved.
+ * ***** END LICENSE BLOCK *****
+ */
+
+package com.zimbra.cs.account.auth;
+
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.auth.AuthContext.Protocol;
+import java.util.List;
+import java.util.Map;
+
+public class IRopcCustomAuth extends ZimbraCustomAuth {
+
+    private static final String EXTENSION_NAME = "idp-ropc";
+
+    @Override
+    public void authenticate(Account acct, String password,
+                             Map<String, Object> context, List<String> args) throws Exception {
+
+        String user = acct.getName();
+        Protocol proto = (Protocol) context.get(AuthContext.AC_PROTOCOL);
+        String deviceId = (String) context.get(AuthContext.AC_DEVICE_ID);
+
+        ZimbraLog.account.debug("%s auth mech initiated for user : %s", EXTENSION_NAME, user);
+
+        boolean isAuthenticated = executeRopcAuth();
+
+        if (isAuthenticated) {
+            ZimbraLog.account.debug("Auth successful for user %s", user);
+        } else {
+            ZimbraLog.account.debug("Auth failed for user %s", user);
+        }
+    }
+
+    protected boolean executeRopcAuth() {
+        return true;
+    }
+
+    @Override
+    public boolean checkPasswordAging() {
+        return false;
+    }
+}
