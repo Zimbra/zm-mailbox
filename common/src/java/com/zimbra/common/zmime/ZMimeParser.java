39de12c880e242d7b6c313a0443635d9795bde96
Subrat Behera <subrat.behera-ctr@synacor.com>
2026-09-21 17:06:04 +0530

    ZBUG-5493: Replace wildcard import with specific java.util imports

--- common/src/java/com/zimbra/common/zmime/ZMimeParser.java
+++ common/src/java/com/zimbra/common/zmime/ZMimeParser.java
@@ -21,7 +21,12 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 import javax.mail.MessagingException;
 import javax.mail.Session;
