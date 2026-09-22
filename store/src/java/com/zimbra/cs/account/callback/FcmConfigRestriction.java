cf0efa7039e9c285c5baf8764914bb06a541fc55
Ashish Gupta <ashishkhms@gmail.com>
2026-09-22 12:50:49 +0530

    ZCS-20697 enabled restriction on the ldap attribute while domain creation

--- store/src/java/com/zimbra/cs/account/callback/FcmConfigRestriction.java
+++ store/src/java/com/zimbra/cs/account/callback/FcmConfigRestriction.java
@@ -18,41 +18,27 @@
 package com.zimbra.cs.account.callback;
 
 import com.zimbra.common.service.ServiceException;
-import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.AttributeCallback;
 import com.zimbra.cs.account.Config;
-import com.zimbra.cs.account.Cos;
 import com.zimbra.cs.account.Domain;
 import com.zimbra.cs.account.Entry;
 import java.util.Map;
 
 public class FcmConfigRestriction extends AttributeCallback {
-    /**
-     * Resolves a human-readable configuration level name from the given entry type.
-     *
-     * @param entry the LDAP entry being modified
-     * @return a lowercase label identifying the entry level (e.g. "domain", "cos", "account", "server")
-     */
-    private String resolveEntryLevel(Entry entry) {
-        if (entry instanceof Domain) {
-            return "domain";
-        } else if (entry instanceof Cos) {
-            return "cos (class of service)";
-        } else if (entry instanceof Account) {
-            return "account";
-        }
-        return entry.getClass().getSimpleName().toLowerCase();
-    }
 
     @Override
     public void preModify(CallbackContext context, String attrName, Object value,
             Map attrsToModify, Entry entry) throws ServiceException {
-        if (!(entry instanceof Config)) {
-            String level = resolveEntryLevel(entry);
+        if (isDomainCreateContext(context) || !(entry instanceof Config)) {
             throw ServiceException.PERM_DENIED(
-                    String.format("'%s' cannot be configured at the %s level. "
-                            + "Please use global config instead.", attrName, level));
+                    String.format("'%s' can only be configured at the global config level.", attrName));
         }
+    }
+
+    private boolean isDomainCreateContext(CallbackContext context) {
+        return context != null
+                && context.isCreate()
+                && Domain.class.equals(context.getCreatingEntryType());
     }
 
     @Override
